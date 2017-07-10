9dfc7f97e83d3665ff128e4041d7bde770770fab
aardschok <wikoreman@gmail.com>
2017-07-10 11:57:28 +0200

    added naming convention validator

--- colorbleed.bat
+++
@@ -1,4 +0,0 @@
-:: Set paths to ensure plugins have access to the inhouse tools
-
-set PYTHONPATH=%PYTHONPATH%;P:\pipeline\dev\git\cb;
-set PYTHONPATH=%PYTHONPATH%;P:\pipeline\dev\git\cbra;
--- colorbleed/plugins/maya/publish/validate_naming_convention.py
+++ colorbleed/plugins/maya/publish/validate_naming_convention.py
@@ -7,6 +7,7 @@ import colorbleed.api
 class ValidateNamingConvention(pyblish.api.InstancePlugin):
 
     label = ""
+    families = ["colorbleed.model"]
     host = ["maya"]
     actions = [colorbleed.api.SelectInvalidAction]
 
