5eaf66c55cba67638571079f65ce2acd555cb8b1
aardschok <wikoreman@gmail.com>
2017-07-26 10:51:28 +0200

    added post-integrate plugin - temp

--- colorbleed/plugins/publish/integrate_asset.py
+++ colorbleed/plugins/publish/integrate_asset.py
@@ -47,34 +47,14 @@ class IntegrateAsset(pyblish.api.InstancePlugin):
         # get needed data
         traffic = instance.data["traffic"]
         representations = instance.data["representations"]
-        version_folder = instance.data["versionFolder"]
-        family = instance.data["family"]
-        resources = instance.data("resources", [])
 
         self.log.info("Registering {} items".format(len(representations)))
         io.insert_many(representations)
 
         # moving files
-        for src_dest in traffic:
-            src, dest = src_dest
+        for src, dest in traffic:
             self.log.info("Copying file .. {} -> {}".format(src, dest))
             self.copy_file(src, dest)
-
-        if family == "colorbleed.texture":
-            try:
-                lib.remap_resource_nodes(resources, folder=version_folder)
-            except Exception as e:
-                self.log.error(e)
-
-        if family == "colorbleed.lookdev":
-            try:
-                tmp_dir = lib.maya_temp_folder()
-                resource_file = os.path.join(tmp_dir, "resources.json")
-                with open(resource_file, "r") as f:
-                    resources = json.load(f)
-                lib.remap_resource_nodes(resources)
-            except Exception as e:
-                self.log.error(e)
 
         self.log.info("Removing temporary files and folders ...")
         stagingdir = instance.data["stagingDir"]
