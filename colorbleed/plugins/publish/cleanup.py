e0d9cecf7c0325cdcfb738dab585d5addd38fd98
aardschok <wikoreman@gmail.com>
2017-08-18 16:05:21 +0200

    initial commit for cleanup plugin, non active

---
+++ colorbleed/plugins/publish/cleanup.py
@@ -0,0 +1,20 @@
+import os
+
+from pyblish import api
+
+
+class CleanUp(api.InstancePlugin):
+    """Cleans up the staging directory after a successful publish
+
+    """
+
+    order = api.IntegratorOrder + 10
+
+    def process(self, instance):
+        return
+
+    def clean_up(self, instance):
+        staging_dir = instance.get("stagingDir", None)
+        if staging_dir and os.path.exists(staging_dir):
+            self.log.info("Removing temporary folder ...")
+            os.rmdir(staging_dir)
