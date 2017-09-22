492a7f468ad3045e4ba05444c5c6f54f5e6b6644
aardschok <wikoreman@gmail.com>
2017-09-22 11:38:58 +0200

    added Maya Ascii loader

---
+++ colorbleed/plugins/maya/load/load_mayaascii.py
@@ -0,0 +1,34 @@
+from avalon import api
+
+
+class MayaAsciiLoader(api.Loader):
+    """Load the model"""
+
+    families = ["colorbleed.mayaAscii"]
+    representations = ["ma"]
+
+    label = "Reference Maya Ascii"
+    order = -10
+    icon = "code-fork"
+    color = "orange"
+
+    def process(self, name, namespace, context, data):
+
+        import maya.cmds as cmds
+        from avalon import maya
+
+        # Create a readable namespace
+        # Namespace should contain asset name and counter
+        # TEST_001{_descriptor} where `descriptor` can be `_abc` for example
+        assetname = "{}_".format(namespace.split("_")[0])
+        namespace = maya.unique_namespace(assetname, format="%03d")
+
+        with maya.maintained_selection():
+            nodes = cmds.file(self.fname,
+                              namespace=namespace,
+                              reference=True,
+                              returnNewNodes=True,
+                              groupReference=True,
+                              groupName="{}:{}".format(namespace, name))
+
+        self[:] = nodes
