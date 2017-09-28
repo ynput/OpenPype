df287f411c22cc830cdb97805412469975247889
Roy Nieterau <roy_nieterau@hotmail.com>
2017-09-28 14:43:16 +0200

    Add to docstring why this workaround plug-in is implemented

--- colorbleed/plugins/maya/publish/collect_model.py
+++ colorbleed/plugins/maya/publish/collect_model.py
@@ -7,6 +7,11 @@ class CollectModelData(pyblish.api.InstancePlugin):
     """Collect model data
 
     Ensures always only a single frame is extracted (current frame).
+    
+    Note:
+        This is a workaround so that the `colorbleed.model` family can use the
+        same pointcache extractor implementation as animation and pointcaches.
+        This always enforces the "current" frame to be published.
 
     """
 
