3a4d89c389ee779dddd53b9eec30a94ea4c93655
aardschok <wikoreman@gmail.com>
2017-10-11 14:26:02 +0200

    integrate is all string, sorting transforms

--- colorbleed/plugins/maya/publish/collect_setdress.py
+++ colorbleed/plugins/maya/publish/collect_setdress.py
@@ -5,6 +5,8 @@ from maya import cmds, mel
 from avalon import maya as amaya
 from colorbleed.maya import lib
 
+import pprint
+
 
 class CollectSetDress(pyblish.api.InstancePlugin):
     """Collect all relevant setdress items
@@ -32,31 +34,23 @@ class CollectSetDress(pyblish.api.InstancePlugin):
 
         for container in containers:
             members = cmds.sets(container["objectName"], query=True)
-            transforms = lib.get_container_transfroms(container, members)
-            root = lib.get_container_transfroms(container, members, root=True)
+            transforms = lib.get_container_transforms(container, members)
+            root = lib.get_container_transforms(container, transforms,
+                                                root=True)
             if root not in instance_lookup:
                 continue
 
-            representation_id = container["representation"]
-            shapes = [m for m in members if
-                      cmds.objectType(m, isAType="shape") is True]
-
-            look_ids = [self.get_look_id(shape) for shape in shapes]
-
-            # Support for re-opened setdress scenes where the only connected
-            # transform node is the root node. This is due to how references
-            # are loaded in combination with the containers.
-
-            matrix_data = self.get_matrix_data(transforms)
+            # retrieve all matrix data
+            matrix_data = self.get_matrix_data(sorted(transforms))
 
             # Gather info for new data entry
             reference_node = cmds.ls(members, type="reference")[0]
             namespace = cmds.referenceQuery(reference_node, namespace=True)
+            representation_id = container["representation"]
             data[representation_id].append({
                  "loader": container["loader"],
                  "matrix": matrix_data,
-                 "namespace": namespace,
-                 "look_id": look_ids
+                 "namespace": namespace
             })
 
         instance.data["scenedata"] = dict(data)
@@ -66,18 +60,12 @@ class CollectSetDress(pyblish.api.InstancePlugin):
 
     def get_matrix_data(self, members):
 
-        matrix_data = []
-        for member in members:
-            self.log.info(member)
+        matrix_data = {}
+        for idx, member in enumerate(members):
             matrix = cmds.xform(member, query=True, matrix=True)
-            matrix_data.append(matrix)
+            if matrix == lib.DEFAULT_MATRIX:
+                continue
+
+            matrix_data[str(idx)] = matrix
 
         return matrix_data
-
-    def get_look_id(self, node):
-        """Get the look id of the assigned shader"""
-        shad_engine = cmds.listConnections(node, type="shadingEngine")
-        if not shad_engine or shad_engine == "initialShadingEngine":
-            return
-
-        return cmds.getAttr("{}.cbId".format(shad_engine[0])).split(":")[0]
