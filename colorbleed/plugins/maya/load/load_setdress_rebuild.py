afae0fa893828503c904737d095043a347ae4517
aardschok <wikoreman@gmail.com>
2017-10-20 18:11:35 +0200

    removed site.addsitedir, use import instead

--- colorbleed/plugins/maya/load/load_setdress_rebuild.py
+++ colorbleed/plugins/maya/load/load_setdress_rebuild.py
@@ -1,8 +1,4 @@
-import site
-
 from avalon import api
-
-site.addsitedir(r"C:\Users\User\Documents\development\research\setdress")
 
 
 class SetDressRebuild(api.Loader):
@@ -17,18 +13,10 @@ class SetDressRebuild(api.Loader):
 
     def process(self, name, namespace, context, data):
 
-        # Hack
-        import sys
+        import setdress_api
 
-        p = r"C:\Users\User\Documents\development\research\setdress"
-        if p not in sys.path:
-            sys.path.insert(0, p)
-
-        import loader
-        reload(loader)
-
-        containers = loader.load_package(filepath=self.fname,
-                                         name=name,
-                                         namespace=namespace)
+        containers = setdress_api.load_package(filepath=self.fname,
+                                               name=name,
+                                               namespace=namespace)
 
         self[:] = containers
