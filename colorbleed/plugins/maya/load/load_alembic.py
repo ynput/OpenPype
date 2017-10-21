1ca84cb94ad799a62a9dc21f8369c2155234c577
Roy Nieterau <roy_nieterau@hotmail.com>
2017-10-21 12:05:16 +0200

    Refactor loaders

--- colorbleed/plugins/maya/load/load_alembic.py
+++ colorbleed/plugins/maya/load/load_alembic.py
@@ -1,7 +1,8 @@
 from avalon import api
+import avalon.maya.pipeline
 
 
-class AnimationLoader(api.Loader):
+class AbcLoader(api.Loader):
     """Specific loader of Alembic for the avalon.animation family"""
 
     families = ["colorbleed.animation",
@@ -29,27 +30,27 @@ class AnimationLoader(api.Loader):
         self[:] = nodes
 
 
-class SetDressAlembicLoader(api.Loader):
-    """Load the setdress as alembic"""
-
-    families = ["colorbleed.setdress"]
-    label = "Reference Alembic"
-    representations = ["abc"]
-    order = -10
-    icon = "code-fork"
-    color = "orange"
-
-    def process(self, name, namespace, context, data):
-
-        import maya.cmds as cmds
-
-        cmds.loadPlugin("AbcImport.mll", quiet=True)
-        nodes = cmds.file(self.fname,
-                          namespace=namespace,
-                          sharedReferenceFile=False,
-                          groupReference=True,
-                          groupName="{}:{}".format(namespace, name),
-                          reference=True,
-                          returnNewNodes=True)
-
-        self[:] = nodes
+# class SetDressAlembicLoader(avalon.maya.pipeline.ReferenceLoader):
+#     """Load the setdress as alembic"""
+#
+#     families = ["colorbleed.setdress"]
+#     label = "Reference Alembic"
+#     representations = ["abc"]
+#     order = -10
+#     icon = "code-fork"
+#     color = "orange"
+#
+#     def process(self, name, namespace, context, data):
+#
+#         import maya.cmds as cmds
+#
+#         cmds.loadPlugin("AbcImport.mll", quiet=True)
+#         nodes = cmds.file(self.fname,
+#                           namespace=namespace,
+#                           sharedReferenceFile=False,
+#                           groupReference=True,
+#                           groupName="{}:{}".format(namespace, name),
+#                           reference=True,
+#                           returnNewNodes=True)
+#
+#         self[:] = nodes
