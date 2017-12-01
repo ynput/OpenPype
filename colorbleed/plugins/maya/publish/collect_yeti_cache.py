155bb334859db6a338ebbeae811f4ab51849e1a9
aardschok <wikoreman@gmail.com>
2017-12-01 15:01:13 +0100

    updated data for recreating nodes to include transform data

--- colorbleed/plugins/maya/publish/collect_yeti_cache.py
+++ colorbleed/plugins/maya/publish/collect_yeti_cache.py
@@ -2,6 +2,7 @@ from maya import cmds
 
 import pyblish.api
 
+from colorbleed.maya import lib
 
 SETTINGS = {"renderDensity",
             "renderWidth",
@@ -22,11 +23,28 @@ class CollectYetiCache(pyblish.api.InstancePlugin):
     def process(self, instance):
 
         # Collect fur settings
-        settings = {}
-        for node in cmds.ls(instance, type="pgYetiMaya"):
-            settings[node] = {}
+        settings = {"nodes": []}
+
+        # Get yeti nodes and their transforms
+        yeti_shapes = cmds.ls(instance, type="pgYetiMaya")
+
+        for shape in yeti_shapes:
+            shape_data = {"transform": None,
+                          "name": shape}
+
+            # Get specific node attributes
             for attr in SETTINGS:
-                current = cmds.getAttr("%s.%s" % (node, attr))
-                settings[node][attr] = current
+                current = cmds.getAttr("%s.%s" % (shape, attr))
+                shape_data[attr] = current
+
+            # Get transform data
+            parent = cmds.listRelatives(shape, parent=True)[0]
+            transform_data = {"name": parent,
+                              "cbId": lib.get_id(parent)}
+
+            # Store transform data
+            shape_data["transform"] = transform_data
+
+            settings["nodes"].append(shape_data)
 
         instance.data["fursettings"] = settings
