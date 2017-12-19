71587db6cd403351a3b710a6b40f4a055b01c9d7
Roy Nieterau <roy_nieterau@hotmail.com>
2017-12-19 11:48:53 +0100

    Improve versioning logic and move the versioning logic to lib.py

--- colorbleed/lib.py
+++ colorbleed/lib.py
@@ -1,3 +1,5 @@
+import os
+import re
 import logging
 
 from .vendor import pather
@@ -90,3 +92,55 @@ def update_task_from_path(path):
     if changes:
         log.info("Updating work task to: %s", context)
         avalon.api.update_current_task(**changes)
+
+
+def _rreplace(s, a, b, n=1):
+    """Replace a with b in string s from right side n times"""
+    return b.join(s.rsplit(a, n))
+
+
+def version_up(filepath):
+    """Version up filepath to a new non-existing version.
+
+    Parses for a version identifier like `_v001` or `.v001`
+    When no version present _v001 is appended as suffix.
+
+    Returns:
+        str: filepath with increased version number
+
+    """
+
+    dirname = os.path.dirname(filepath)
+    basename, ext = os.path.splitext(os.path.basename(filepath))
+
+    regex = "[._]v\d+"
+    matches = re.findall(regex, str(basename), re.IGNORECASE)
+    if not matches:
+        log.info("Creating version...")
+        new_label = "_v{version:03d}".format(version=1)
+        new_basename = "{}{}".format(basename, new_label)
+    else:
+        label = matches[-1]
+        version = re.search("\d+", label).group()
+        padding = len(version)
+
+        new_version = int(version) + 1
+        new_version = '{version:0{padding}d}'.format(version=new_version,
+                                                     padding=padding)
+        new_label = label.replace(version, new_version, 1)
+        new_basename = _rreplace(basename, label, new_label)
+
+    new_filename = "{}{}".format(new_basename, ext)
+    new_filename = os.path.join(dirname, new_filename)
+    new_filename = os.path.normpath(new_filename)
+
+    if new_filename == filepath:
+        raise RuntimeError("Created path is the same as current file,"
+                           "this is a bug")
+
+    if os.path.exists(new_filename):
+        log.info("Skipping existing version %s" % new_label)
+        return version_up(new_filename)
+
+    log.info("New version %s" % new_label)
+    return new_filename
