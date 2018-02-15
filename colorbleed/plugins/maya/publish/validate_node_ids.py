848ac357841069a67729633c5ea0629b8caf748e
aardschok <wikoreman@gmail.com>
2018-02-15 10:50:21 +0100

    reverted back to bool True

--- colorbleed/plugins/maya/publish/validate_node_ids.py
+++ colorbleed/plugins/maya/publish/validate_node_ids.py
@@ -40,8 +40,9 @@ class ValidateNodeIDs(pyblish.api.InstancePlugin):
         """Return the member nodes that are invalid"""
 
         # We do want to check the referenced nodes as it might be
-        # part of the end product
-        id_nodes = lib.get_id_required_nodes(nodes=instance[:])
+        # part of the end product.
+        id_nodes = lib.get_id_required_nodes(referenced_nodes=True,
+                                             nodes=instance[:])
         invalid = [n for n in id_nodes if not lib.get_id(n)]
 
         return invalid
