062c44b32f25d097be61a7970b13f1478e80dddf
Roy Nieterau <roy_nieterau@hotmail.com>
2018-02-20 18:08:28 +0100

    Move logic of publish image sequence for local render to the plug-in itself so it runs per instance and requires less logic

--- colorbleed/plugins/fusion/publish/extract_image_sequence.py
+++ colorbleed/plugins/fusion/publish/extract_image_sequence.py
@@ -1,14 +1,11 @@
-import json
-import os
-import re
-
 import pyblish.api
 
 
 class ExtractImageSequence(pyblish.api.Extractor):
     """Extract result of saver by starting a comp render
 
-    This will run the local render of Fusion,
+    This will run the local render of Fusion.
+
     """
 
     order = pyblish.api.ExtractorOrder
@@ -22,7 +19,6 @@ class ExtractImageSequence(pyblish.api.Extractor):
         start_frame = current_comp.GetAttrs("COMPN_RenderStart")
         end_frame = current_comp.GetAttrs("COMPN_RenderEnd")
 
-        # Build Fusion Render Job
         self.log.info("Starting render")
         self.log.info("Start frame: {}".format(start_frame))
         self.log.info("End frame: {}".format(end_frame))
@@ -30,32 +26,3 @@ class ExtractImageSequence(pyblish.api.Extractor):
         result = current_comp.Render()
         if not result:
             raise RuntimeError("Comp render failed")
-
-        # Write metadata json file per Saver instance
-        instances = [i for i in context[:] if i[0].ID == "Saver"]
-        for instance in instances:
-            subset = instance.data["subset"]
-            ext = instance.data["ext"]
-
-            # Regex to match resulting renders
-            regex = "^{subset}.*[0-9]+.{ext}+$".format(subset=re.escape(subset)
-                                                       , ext=re.escape(ext))
-
-            # The instance has most of the information already stored
-            metadata = {
-                "regex": regex,
-                "startFrame": context.data["startFrame"],
-                "endFrame": context.data["endFrame"],
-                "families": ["colorbleed.imagesequence"],
-            }
-
-            # Write metadata and store the path in the instance
-            output_directory = instance.data["outputDir"]
-            path = os.path.join(output_directory,
-                                "{}_metadata.json".format(subset))
-
-            with open(path, "w") as f:
-                json.dump(metadata, f)
-
-            # Store json path in instance
-            instance.data["jsonpath"] = path
--- colorbleed/plugins/fusion/publish/publish_image_sequences.py
+++ colorbleed/plugins/fusion/publish/publish_image_sequences.py
@@ -1,4 +1,6 @@
+import re
 import os
+import json
 import subprocess
 
 import pyblish.api
@@ -11,7 +13,7 @@ def _get_script():
 
     try:
         from colorbleed.scripts import publish_filesequence
-    except Exception as e:
+    except Exception:
         raise RuntimeError("Expected module 'publish_imagesequence'"
                            "to be available")
 
@@ -23,22 +25,38 @@ def _get_script():
 
 
 class PublishImageSequence(pyblish.api.Extractor):
-    """Extract result of saver by starting a comp render
+    """Publish the generated local image sequences."""
 
-    This will run the local render of Fusion,
-    """
-
-    order = pyblish.api.ExtractorOrder
+    order = pyblish.api.ExtractorOrder + 0.1
     label = "Publish Rendered Image Sequence(s)"
     hosts = ["fusion"]
     targets = ["renderlocal"]
+    families = ["fusion.local"]
 
     def process(self, instance):
 
-        context = instance.context
+        subset = instance.data["subset"]
+        ext = instance.data["ext"]
 
-        # Get metadata file, check if path is to an existing file
-        path = instance.data.get("jsonpath", "")
+        # Regex to match resulting renders
+        regex = "^{subset}.*[0-9]+.{ext}+$".format(subset=re.escape(subset),
+                                                   ext=re.escape(ext))
+
+        # The instance has most of the information already stored
+        metadata = {
+            "regex": regex,
+            "startFrame": instance.context.data["startFrame"],
+            "endFrame": instance.context.data["endFrame"],
+            "families": ["colorbleed.imagesequence"],
+        }
+
+        # Write metadata and store the path in the instance
+        output_directory = instance.data["outputDir"]
+        path = os.path.join(output_directory,
+                            "{}_metadata.json".format(subset))
+        with open(path, "w") as f:
+            json.dump(metadata, f)
+
         assert os.path.isfile(path), ("Stored path is not a file for %s"
                                       % instance.data["name"])
 
