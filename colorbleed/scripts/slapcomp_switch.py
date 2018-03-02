a5be74475e5f0b5c6aa4c0acaf96269d504485e2
wijnand <wikoreman@gmail.com>
2018-03-02 18:10:21 +0100

    Improved logic, arg only needs shot name

--- colorbleed/scripts/slapcomp_switch.py
+++ colorbleed/scripts/slapcomp_switch.py
@@ -9,47 +9,55 @@ import avalon.fusion
 log = logging.getLogger("UpdateSlapComp")
 
 
-def switch(data, targets=None):
+def switch(shot):
     """Update comp loaders through the containers
 
     Args:
         data (dict): collection if {asset: [subset, subset]}
-        targets (list): to be implemented
     """
-
-    representations = []
-
-    for asset_name, subsets in data.iteritems():
-
-        asset = io.find_one({"type": "asset", "name": asset_name})
-        assert asset, ("Could not find asset in the database with the name "
-                       "'%s'" % asset_name)
-
-        subsets = io.find({"type": "subset",
-                           "name": {"$in": subsets},
-                           "parent": asset["_id"]})
-
-        for subset in subsets:
-            version = io.find_one({"type": "version",
-                                   "parent": subset["_id"]},
-                                  sort=[('name', -1)])
-
-            if not version:
-                log.error("Could not find a verison for {}.{}".format(
-                    asset["name"], subset["name"]
-                ))
-                continue
-
-            representation = io.find_one({"type": "representation",
-                                          "parent": version["_id"]})
-
-            representations.append(representation)
 
     host = api.registered_host()
     containers = list(host.ls())
+
+    targets = [c["name"] for c in containers]
+
+    representations = {}
+
+    asset = io.find_one({"type": "asset", "name": shot})
+    assert asset, ("Could not find asset in the database with the name "
+                   "'%s'" % shot)
+
+    subsets = io.find({"type": "subset",
+                       "name": {"$in": targets},
+                       "parent": asset["_id"]})
+
+    if len(subsets) != len(targets):
+        log.warning("Could not find the same amount of subsets in '%s'" % shot)
+
+    for subset in subsets:
+        version = io.find_one({"type": "version",
+                               "parent": subset["_id"]},
+                              sort=[('name', -1)])
+
+        if not version:
+            log.error("Could not find a verison for {}.{}".format(
+                asset["name"], subset["name"]
+            ))
+            continue
+
+        representation = io.find_one({"type": "representation",
+                                      "parent": version["_id"]})
+
+        representations[subset["name"]] = representation
+
     count = 0
     for i, container in enumerate(containers):
-        representation = representations[i]
+        subsetname = container["name"]
+        if subsetname not in representations:
+            log.error("Subset '%s' not found in comp, skipping" % subsetname)
+            continue
+
+        representation = representations[container["name"]]
         api.switch(container, representation)
         count += 1
 
@@ -60,21 +68,20 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--data",
-                        help="Dictionary with asset name as key and related "
-                             "subsets (list) as value")
+    parser.add_argument("--shot",
+                        help="Shotname to update the current comp to")
 
     parser.add_argument("--targets",
                         optional=True,
                         help="A list of target to update with given data")
 
     args = parser.parse_known_args()
-    if not args.data:
+    if not args.shot:
         raise RuntimeError("No data given, cannot update slap comp")
 
-    if not isinstance(args.data, dict):
+    if not isinstance(args.shot, dict):
         raise ValueError("Expecting data in the form of a dict, got "
-                         "%s" % type(args.data))
+                         "%s" % type(args.shot))
 
     api.install(avalon.fusion)
-    switch(data=args.data)
+    switch(shot=args.shot)
