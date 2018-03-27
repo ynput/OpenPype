2fdd3c8c64b2c294ad1c28adb1b1318435cfae19
wijnand <wikoreman@gmail.com>
2018-03-27 16:23:24 +0200

    Added register_launcher_action

--- colorbleed/__init__.py
+++ colorbleed/__init__.py
@@ -1,5 +1,6 @@
 import os
 from pyblish import api as pyblish
+from avalon import api, pipeline
 
 PACKAGE_DIR = os.path.dirname(__file__)
 PLUGINS_DIR = os.path.join(PACKAGE_DIR, "plugins")
@@ -15,4 +16,12 @@ def install():
 
 def uninstall():
     pyblish.deregister_plugin_path(PUBLISH_PATH)
+
+
+def register_launcher_actions():
+    """Register specific actions which should be accessible in the launcher"""
+
+    # Register fusion actions
+    from .fusion import rendernode
+    pipeline.register_plugin(api.Action, rendernode.FusionRenderNode)
 
