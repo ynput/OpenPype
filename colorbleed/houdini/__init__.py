901f0d688d00e20cd823c150f0a0e0744ab81726
wijnand <wikoreman@gmail.com>
2018-07-17 09:46:54 +0200

    added install logic for houdini

--- colorbleed/houdini/__init__.py
+++ colorbleed/houdini/__init__.py
@@ -1,9 +1,19 @@
 import os
-
 import logging
 
-from avalon import api as avalon  # pipeline, houdini
+import hou
+
 from pyblish import api as pyblish
+
+from avalon import api as avalon
+from avalon.houdini import pipeline as houdini
+
+from colorbleed.houdini import lib
+
+from colorbleed.lib import (
+    any_outdated,
+    update_task_from_path
+)
 
 
 PARENT_DIR = os.path.dirname(__file__)
@@ -18,6 +28,8 @@ log = logging.getLogger("colorbleed.houdini")
 
 
 def install():
+
+    # Set
 
     pyblish.register_plugin_path(PUBLISH_PATH)
     avalon.register_plugin_path(avalon.Loader, LOAD_PATH)
@@ -34,16 +46,52 @@ def install():
     avalon.data["familiesStateToggled"] = ["colorbleed.imagesequence"]
 
 
-def on_init():
-    pass
+def on_init(_):
+    houdini.on_houdini_initialize()
 
 
-def on_save():
-    pass
+def on_save(_):
+
+    avalon.logger.info("Running callback on save..")
+
+    update_task_from_path(hou.hipFile.path())
+
+    nodes = lib.get_id_required_nodes()
+    for node, new_id in lib.generate_ids(nodes):
+        lib.set_id(node, new_id, overwrite=False)
 
 
 def on_open():
-    pass
+
+    update_task_from_path(hou.hipFile.path())
+
+    if any_outdated():
+        from avalon.vendor.Qt import QtWidgets
+        from ..widgets import popup
+
+        log.warning("Scene has outdated content.")
+
+        # Find maya main window
+        top_level_widgets = {w.objectName(): w for w in
+                             QtWidgets.QApplication.topLevelWidgets()}
+        parent = top_level_widgets.get("MayaWindow", None)
+
+        if parent is None:
+            log.info("Skipping outdated content pop-up "
+                     "because Maya window can't be found.")
+        else:
+
+            # Show outdated pop-up
+            def _on_show_inventory():
+                import avalon.tools.cbsceneinventory as tool
+                tool.show(parent=parent)
+
+            dialog = popup.Popup(parent=parent)
+            dialog.setWindowTitle("Maya scene has outdated content")
+            dialog.setMessage("There are outdated containers in "
+                              "your Maya scene.")
+            dialog.on_show.connect(_on_show_inventory)
+            dialog.show()
 
 
 def on_task_changed(*args):
