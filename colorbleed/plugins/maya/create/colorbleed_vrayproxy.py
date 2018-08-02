aaaf1e6ef58a5376117117e175280a7257a3e1d3
wikoreman <wikoreman@gmail.com>
2018-08-02 11:46:04 +0200

    added vrayproxy creator

---
+++ colorbleed/plugins/maya/create/colorbleed_vrayproxy.py
@@ -0,0 +1,10 @@
+import avalon.maya
+
+
+class CreatePointCache(avalon.maya.Creator):
+    """Alembic pointcache for animated data"""
+
+    name = "vrayproxy"
+    label = "VRay Proxy"
+    family = "colorbleed.vrayproxy"
+    icon = "gears"
