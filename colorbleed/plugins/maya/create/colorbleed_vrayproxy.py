2273a61717db658ba74dd59f6a93ed15b03a2407
wikoreman <wikoreman@gmail.com>
2018-08-02 17:33:36 +0200

    added options

--- colorbleed/plugins/maya/create/colorbleed_vrayproxy.py
+++ colorbleed/plugins/maya/create/colorbleed_vrayproxy.py
@@ -1,10 +1,23 @@
+from collections import OrderedDict
+
 import avalon.maya
 
 
-class CreatePointCache(avalon.maya.Creator):
+class CreateVrayProxy(avalon.maya.Creator):
     """Alembic pointcache for animated data"""
 
     name = "vrayproxy"
     label = "VRay Proxy"
     family = "colorbleed.vrayproxy"
     icon = "gears"
+
+    def __init__(self, *args, **kwargs):
+        super(CreateVrayProxy, self).__init__(*args, **kwargs)
+
+        data = OrderedDict(**self.data)
+
+        data["animation"] = False
+        data["startFrame"] = 1
+        data["endFrame"] = 1
+
+        self.data.update(data)
