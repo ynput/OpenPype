28067766d5f15f2c131aea124267bb0563ce5f32
wikoreman <wikoreman@gmail.com>
2018-09-12 15:26:14 +0200

    Updated extract plugin

--- colorbleed/plugins/houdini/publish/extract_alembic.py
+++ colorbleed/plugins/houdini/publish/extract_alembic.py
@@ -1,7 +1,5 @@
 import os
 
-import hou
-
 import pyblish.api
 import colorbleed.api
 from colorbleed.houdini import lib
@@ -21,15 +19,17 @@ class ExtractAlembic(colorbleed.api.Extractor):
         file_name = "{}.abc".format(instance.data["subset"])
         tmp_filepath = os.path.join(staging_dir, file_name)
 
+        start_frame = float(instance.data["startFrame"])
+        end_frame = float(instance.data["endFrame"])
+
         ropnode = instance[0]
-        attributes = {"trange": 1,
-                      "f1": instance.data["startFrame"],
-                      "f2": instance.data["endFrame"]}
+        attributes = {"filename": tmp_filepath,
+                      "trange": 2}
 
         with lib.attribute_values(ropnode, attributes):
-            ropnode.execute()
+            ropnode.render(frame_range=(start_frame, end_frame, 1))
 
         if "files" not in instance.data:
             instance.data["files"] = []
 
-        instance.data["files"].append(tmp_filepath)
+        instance.data["files"].append(file_name)
