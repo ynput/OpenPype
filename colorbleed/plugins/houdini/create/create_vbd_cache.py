e9ec64b7ac9f5746ef5cdad8992744ff5ed0a2cb
wikoreman <wikoreman@gmail.com>
2018-09-13 16:05:00 +0200

    Added instnace for vdb

---
+++ colorbleed/plugins/houdini/create/create_vbd_cache.py
@@ -0,0 +1,23 @@
+from collections import OrderedDict
+
+from avalon import houdini
+
+
+class CreateVDBCache(houdini.Creator):
+    """Alembic pointcache for animated data"""
+
+    name = "vbdcache"
+    label = "VDB Cache"
+    family = "colorbleed.vbdcache"
+    icon = "cloud"
+
+    def __init__(self, *args, **kwargs):
+        super(CreateVDBCache, self).__init__(*args, **kwargs)
+
+        # create an ordered dict with the existing data first
+        data = OrderedDict(**self.data)
+
+        # Set node type to create for output
+        data["node_type"] = "geometry"
+
+        self.data = data
