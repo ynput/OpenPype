0bdc5d6e71f747ba0e9e827a12a26f7bae32a9aa
Jakub Jezek <jezschaj@gmail.com>
2018-10-10 18:34:04 +0200

    ftrack-api integration initialization

--- pype/nuke/__init__.py
+++ pype/nuke/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from avalon import api as avalon
 from pyblish import api as pyblish
-# from avalon.tools import workfiles
+from ..vendor import ftrack_api
 
 PARENT_DIR = os.path.dirname(__file__)
 PACKAGE_DIR = os.path.dirname(PARENT_DIR)
