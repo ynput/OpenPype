992262e09c53b86e5a75564cd7ff915d734067c3
wikoreman <wikoreman@gmail.com>
2018-10-11 12:12:41 +0200

    added validator for pre render callbacks scripts

---
+++ colorbleed/plugins/maya/publish/validate_renderscript_callbacks.py
@@ -0,0 +1,96 @@
+from maya import cmds
+
+import pyblish.api
+import colorbleed.api
+
+
+class ValidateRenderScriptCallbacks(pyblish.api.InstancePlugin):
+    """Check if the render script callbacks will be used during the rendering
+
+    In order to ensure the render tasks are executed properly we need to check
+    if the pre and post render callbacks are actually used.
+
+    For example:
+        Yeti is not loaded but its callback scripts are still set in the
+        render settings. This will cause an error because Maya tries to find
+        and execute the callbacks.
+
+    Developer note:
+         The pre and post render callbacks cannot be overridden
+
+    """
+
+    order = colorbleed.api.ValidateContentsOrder
+    label = "Render Script Callbacks"
+    hosts = ["maya"]
+    families = ["colorbleed.renderlayer"]
+
+    def process(self, instance):
+
+        invalid = self.get_invalid(instance)
+        if invalid:
+            raise ValueError("Invalid render callbacks found for '%s'!"
+                             % instance.name)
+
+    @classmethod
+    def get_invalid(cls, instance):
+
+        invalid = False
+
+        # lookup per render
+        render_scripts = {"vray":
+                              {"pre":  "catch(`pgYetiVRayPreRender`)",
+                               "post": "catch(`pgYetiVRayPostRender`)"},
+                          "arnold":
+                              {"pre": "pgYetiPreRender"}
+                          }
+
+        yeti_loaded = cmds.pluginInfo("pgYetiMaya", query=True, loaded=True)
+
+        renderer = instance.data["renderer"]
+        if renderer == "redshift":
+            cls.log.info("Redshift ignores any pre and post render callbacks")
+            return False
+
+        callback_lookup = render_scripts.get(renderer, {})
+        if not callback_lookup:
+            cls.log.error(
+                "Renderer '%s' is not supported in this plugin"  % renderer
+            )
+
+        pre_render_callback = cmds.getAttr("defaultRenderGlobals.preMel")
+        post_render_callback = cmds.getAttr("defaultRenderGlobals.postMel")
+
+        pre_script = callback_lookup.get("pre", "")
+        post_script = callback_lookup.get("post", "")
+
+        # If not loaded
+        if not yeti_loaded:
+            if pre_script and pre_script in pre_render_callback:
+                cls.log.error(
+                    "Found pre render callback which is not uses!"
+                )
+                invalid = True
+
+            if post_script and post_script in post_render_callback:
+                cls.log.error(
+                    "Found post render callback which is not used!"
+                )
+                invalid = True
+        else:
+            if pre_script:
+                pre_callbacks = pre_render_callback.split(";")
+                if pre_script not in pre_callbacks:
+                    cls.log.error(
+                        "Could not find required pre render callback "
+                        "`%s`" % pre_script)
+                    invalid = True
+
+            if post_script:
+                post_callbacks = post_render_callback.split(";")
+                if post_script not in post_callbacks:
+                    cls.log.error("Could not find required post render callback"
+                                  " `%s`" % post_script)
+                    invalid = True
+
+        return invalid
