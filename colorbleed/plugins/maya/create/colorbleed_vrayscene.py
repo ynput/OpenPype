dd3b17fc9d3657b9672ede82af60a603689cf36e
wikoreman <wikoreman@gmail.com>
2018-10-18 10:55:18 +0200

    Removed get camera logic

--- colorbleed/plugins/maya/create/colorbleed_vrayscene.py
+++ colorbleed/plugins/maya/create/colorbleed_vrayscene.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict
-
 import avalon.maya
 
 
@@ -12,27 +10,16 @@ class CreateVRayScene(avalon.maya.Creator):
     def __init__(self, *args, **kwargs):
         super(CreateVRayScene, self).__init__(*args, **kwargs)
 
-        # We won't be publishing this one
-        self.data["id"] = "avalon.vrayscene"
-
         # We don't need subset or asset attributes
         self.data.pop("subset", None)
         self.data.pop("asset", None)
 
-        data = OrderedDict(**self.data)
-
-        data["camera"] = self._get_cameras()
-        data["suspendRenderJob"] = False
-        data["suspendPublishJob"] = False
-        data["extendFrames"] = False
-        data["pools"] = ""
-
-        self.data = data
+        self.data.update({
+            "id": "avalon.vrayscene",  # We won't be publishing this one
+            "suspendRenderJob": False,
+            "suspendPublishJob": False,
+            "extendFrames": False,
+            "pools": ""
+        })
 
         self.options = {"useSelection": False}  # Force no content
-
-    def _get_cameras(self):
-        from maya import cmds
-
-        return [c for c in cmds.ls(type="camera")
-                if cmds.getAttr("%s.renderable" % c)]
