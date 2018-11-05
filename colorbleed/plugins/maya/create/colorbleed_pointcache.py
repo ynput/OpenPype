03f267b716c10140114191e2dc9869da6fa9a70a
Roy Nieterau <roy_nieterau@hotmail.com>
2018-11-05 21:37:58 +0100

    Fix PLN-196: Pointcache creator attributes in correct order

--- colorbleed/plugins/maya/create/colorbleed_pointcache.py
+++ colorbleed/plugins/maya/create/colorbleed_pointcache.py
@@ -13,14 +13,13 @@ class CreatePointCache(avalon.maya.Creator):
     def __init__(self, *args, **kwargs):
         super(CreatePointCache, self).__init__(*args, **kwargs)
 
-        data = {"writeColorSets": False,  # Vertex colors with the geometry.
-                "renderableOnly": False,  # Only renderable visible shapes
-                "visibleOnly": False,  # only nodes that are visible
-                "attr": "",  # Add options for custom attributes
-                "attrPrefix": ""}
+        # Add animation data
+        self.data.update(lib.collect_animation_data())
 
-        # get basic animation data : start / end / handles / steps
-        for key, value in lib.collect_animation_data().items():
-            data[key] = value
+        self.data["writeColorSets"] = False  # Vertex colors with the geometry.
+        self.data["renderableOnly"] = False  # Only renderable visible shapes
+        self.data["visibleOnly"] = False     # only nodes that are visible
 
-        self.data.update(data)
+        # Add options for custom attributes
+        self.data["attr"] = ""
+        self.data["attrPrefix"] = ""
