f7c0c1d88553466ac29872c5d73d36824fd8bad4
Roy Nieterau <roy_nieterau@hotmail.com>
2018-11-23 17:36:26 +0100

    Fix render single camera validation (actually process something)
    - Also improve logged messages

--- colorbleed/plugins/maya/publish/validate_render_single_camera.py
+++ colorbleed/plugins/maya/publish/validate_render_single_camera.py
@@ -17,17 +17,28 @@ class ValidateRenderSingleCamera(pyblish.api.InstancePlugin):
     order = colorbleed.api.ValidateContentsOrder
     label = "Render Single Camera"
     hosts = ['maya']
-    families = ['colorbleed.renderlayer',
+    families = ["colorbleed.renderlayer",
                 "colorbleed.vrayscene"]
+    actions = [colorbleed.maya.action.SelectInvalidAction]
 
     def process(self, instance):
         """Process all the cameras in the instance"""
+        invalid = self.get_invalid(instance)
+        if invalid:
+            raise RuntimeError("Invalid cameras for render.")
 
     @classmethod
     def get_invalid(cls, instance):
-        cameras = instance.data.get("cameras", [])
-        if len(cameras) != 1:
-            cls.log.error("Multiple renderable cameras" "found: %s " %
-                          instance.data["setMembers"])
 
+        cameras = instance.data.get("cameras", [])
+
+        if len(cameras) > 1:
+            cls.log.error("Multiple renderable cameras found for %s: %s " %
+                          (instance.data["setMembers"], cameras))
+            return [instance.data["setMembers"]] + cameras
+
+        elif len(cameras) < 1:
+            cls.log.error("No renderable cameras found for %s " %
+                          instance.data["setMembers"])
             return [instance.data["setMembers"]]
+
