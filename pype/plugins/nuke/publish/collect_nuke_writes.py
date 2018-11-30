e8c185b58826151ae0af1819a7eb11118c180aac
Milan Kolar <milan@pype.club>
2018-11-30 12:14:32 +0100

    change collect write to collecting all instances

--- pype/plugins/nuke/publish/collect_nuke_writes.py
+++ pype/plugins/nuke/publish/collect_nuke_writes.py
@@ -6,18 +6,17 @@ import clique
 
 
 @pyblish.api.log
-class CollectNukeWrites(pyblish.api.ContextPlugin):
+class CollectNukeInstances(pyblish.api.ContextPlugin):
     """Collect all write nodes."""
 
     order = pyblish.api.CollectorOrder
-    label = "Writes"
+    label = "Collect Instances"
     hosts = ["nuke", "nukeassist"]
 
     # targets = ["default", "process"]
 
     def process(self, context):
 
-        instances = []
         # creating instances per write node
         for node in nuke.allNodes():
             if node.Class() != "Write":
@@ -44,7 +43,7 @@ class CollectNukeWrites(pyblish.api.ContextPlugin):
             # Add collection
             collection = None
             path = nuke.filename(node)
-            path += " [{0}-{1}]".format(first_frame, last_frame)
+            path += " [{0}-{1}]".format(str(first_frame), str(last_frame))
             collection = clique.parse(path)
 
             subset = node.name()
@@ -63,13 +62,6 @@ class CollectNukeWrites(pyblish.api.ContextPlugin):
                 knob.setValue(False)
                 node.addKnob(knob)
 
-            # Compare against selection
-            selection = instance.context.data.get("selection", [])
-            if selection:
-                if list(set(instance) and set(selection)):
-                    value = True
-                else:
-                    value = False
 
             instance.data.update({
                 "asset": os.environ["AVALON_ASSET"],  # todo: not a constant
@@ -80,87 +72,18 @@ class CollectNukeWrites(pyblish.api.ContextPlugin):
                 "label": label,
                 "families": ["render.local"],
                 "family": "write",
-                "publish": value,
+                "publish": node.knob("publish"),
                 "collection": collection,
                 "first_frame": first_frame,
                 "last_frame": last_frame,
                 "output_type": output_type
             })
-            instances.append(instance)
-            self.log.info("writeNode collected: {}".format(subset))
 
-        context.data["write_instances"] = instances
+        # Sort/grouped by family (preserving local index)
+        context[:] = sorted(context, key=self.sort_by_family)
 
-        context.data["instances"] = (
-            context.data.get("instances", []) + instances)
+        return context
 
-
-class CollectNukeWritesPublish(pyblish.api.ContextPlugin):
-    """Collect all write instances for publishing."""
-
-    order = CollectNukeWrites.order + 0.01
-    label = "Writes Publish"
-    hosts = ["nuke", "nukeassist"]
-
-    # targets = ["default"]
-
-    def process(self, context):
-
-        for item in context.data["write_instances"]:
-
-            # If the collection was not generated.
-            if not item.data["collection"]:
-                continue
-
-            missing_files = []
-            for f in item.data["collection"]:
-                # print f
-                if not os.path.exists(f):
-                    missing_files.append(f)
-
-            for f in missing_files:
-                item.data["collection"].remove(f)
-
-            if not list(item.data["collection"]):
-                continue
-
-            instance = context.create_instance(item.data["name"])
-
-            for key, value in item.data.iteritems():
-                # print key, value
-                instance.data[key] = value
-
-            instance.data["families"] = ["publish"]
-            instance.data["label"] += (
-                " - " + os.path.basename(instance.data["collection"].format()))
-
-            for node in item:
-                instance.add(node)
-
-            # Adding/Checking publish attribute
-            if "publish" not in node.knobs():
-                knob = nuke.Boolean_Knob("publish", "Publish")
-                knob.setValue(False)
-                node.addKnob(knob)
-
-            value = bool(node["publish"].getValue())
-
-            # Compare against selection
-            selection = instance.context.data.get("selection", [])
-            if selection:
-                if list(set(instance) & set(selection)):
-                    value = True
-                else:
-                    value = False
-
-            instance.data["publish"] = value
-
-            def instanceToggled(instance, value):
-                # Removing and adding the knob to support NukeAssist, where
-                # you can't modify the knob value directly.
-                instance[0].removeKnob(instance[0]["publish"])
-                knob = nuke.Boolean_Knob("publish", "Publish")
-                knob.setValue(value)
-                instance[0].addKnob(knob)
-
-            instance.data["instanceToggled"] = instanceToggled
+    def sort_by_family(self, instance):
+        """Sort by family"""
+        return instance.data.get("families", instance.data.get("family"))
