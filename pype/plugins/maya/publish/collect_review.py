6394c7866a875a3e7e209d7e4f19731081d64da1
Milan Kolar <milan@pype.club>
2018-12-10 18:24:20 +0100

    only add review to subset if they are properly collected

--- pype/plugins/maya/publish/collect_review.py
+++ pype/plugins/maya/publish/collect_review.py
@@ -37,16 +37,17 @@ class CollectReviewData(pyblish.api.InstancePlugin):
 
         reviewable_subset = None
         reviewable_subset = list(set(members) & set(objectset))
-        assert len(reviewable_subset) == 1, "Multiple subsets for review"
-        self.log.debug('subset for review: {}'.format(reviewable_subset))
+        if reviewable_subset:
+            assert len(reviewable_subset) <= 1, "Multiple subsets for review"
+            self.log.debug('subset for review: {}'.format(reviewable_subset))
 
-        for inst in context:
-            self.log.debug('instance: {}'.format(instance))
-            if inst.name == reviewable_subset[0]:
-                inst.data['families'].append('review')
-                inst.data['review_camera'] = camera
-                self.log.info('adding review family to {}'.format(reviewable_subset))
-                cmds.setAttr(str(instance) + '.active', 0)
-
-        instance.data['subset'] = task + 'Review'
-        instance.data['review_camera'] = camera
+            for inst in context:
+                self.log.debug('instance: {}'.format(instance))
+                if inst.name == reviewable_subset[0]:
+                    inst.data['families'].append('review')
+                    inst.data['review_camera'] = camera
+                    self.log.info('adding review family to {}'.format(reviewable_subset))
+                    cmds.setAttr(str(instance) + '.active', 0)
+        else:
+            instance.data['subset'] = task + 'Review'
+            instance.data['review_camera'] = camera
