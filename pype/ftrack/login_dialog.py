5ae016af8a090919985b422b8e00482243630446
Jakub Trllo <jakub.trllo@gmail.com>
2018-12-11 14:18:34 +0100

    - changed hide() for close() - ftrack_run: added thread for logout from ftrack/ was freezing whole application

--- pype/ftrack/login_dialog.py
+++ pype/ftrack/login_dialog.py
@@ -301,9 +301,5 @@ class Login_Dialog_ui(QtWidgets.QWidget):
                 self.parent.loginChange()
             self._close_widget()
 
-    def closeEvent(self, event):
-        event.ignore()
-        self._close_widget()
-
     def _close_widget(self):
-        self.hide()
+        self.close()
