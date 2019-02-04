e4e96a98f42153b3e64cf328374770919b8a9d88
Jakub Trllo <jakub.trllo@gmail.com>
2019-02-04 19:03:10 +0100

    copied copyrights cleanup

--- pype/ftrack/actions/action_create_cust_attrs.py
+++ pype/ftrack/actions/action_create_cust_attrs.py
@@ -1,5 +1,3 @@
-# :coding: utf-8
-# :copyright: Copyright (c) 2017 ftrack
 import os
 import sys
 import argparse
--- pype/ftrack/actions/action_test.py
+++ pype/ftrack/actions/action_test.py
@@ -1,5 +1,3 @@
-# :coding: utf-8
-# :copyright: Copyright (c) 2017 ftrack
 import sys
 import argparse
 import logging
