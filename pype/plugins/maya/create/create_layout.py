a2d143af2ebbdde9b50a0a5ecb343cc1232abd75
Milan Kolar <milan@pype.club>
2019-02-20 17:11:52 +0100

    add layout and assembly creator

---
+++ pype/plugins/maya/create/create_layout.py
@@ -0,0 +1,11 @@
+import avalon.maya
+
+
+class CreateSetDress(avalon.maya.Creator):
+    """A grouped package of loaded content"""
+
+    name = "layout"
+    label = "Layout"
+    family = "layout"
+    icon = "map-marked"
+    defaults = ["Main"]
