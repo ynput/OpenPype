c40fa2fc2bb193698f74e63121b479e2dc599b09
Jakub Trllo <jakub.trllo@gmail.com>
2019-02-27 12:17:51 +0100

    jpeg exctractor separated

--- pype/plugins/global/publish/extract_jpeg.py
+++ pype/plugins/global/publish/extract_jpeg.py
@@ -4,7 +4,7 @@ import subprocess
 from pype.vendor import clique
 
 
-class ExtractQuicktimeEXR(pyblish.api.InstancePlugin):
+class ExtractJpegEXR(pyblish.api.InstancePlugin):
     """Resolve any dependency issies
 
     This plug-in resolves any paths which, if not updated might break
@@ -15,52 +15,37 @@ class ExtractQuicktimeEXR(pyblish.api.InstancePlugin):
     publish the shading network. Same goes for file dependent assets.
     """
 
-    label = "Extract Quicktime EXR"
+    label = "Extract Jpeg EXR"
     order = pyblish.api.ExtractorOrder
     families = ["imagesequence", "render", "write", "source"]
     host = ["shell"]
 
     def process(self, instance):
-        fps = instance.data.get("fps")
         start = instance.data.get("startFrame")
         stagingdir = os.path.normpath(instance.data.get("stagingDir"))
 
         collected_frames = os.listdir(stagingdir)
         collections, remainder = clique.assemble(collected_frames)
 
-        full_input_path = os.path.join(
-            stagingdir, collections[0].format('{head}{padding}{tail}')
+        input_file = (
+            collections[0].format('{head}{padding}{tail}') % start
         )
+        full_input_path = os.path.join(stagingdir, input_file)
         self.log.info("input {}".format(full_input_path))
 
         filename = collections[0].format('{head}')
         if not filename.endswith('.'):
             filename += "."
-        movFile = filename + "mov"
-        full_output_path = os.path.join(stagingdir, movFile)
+        jpegFile = filename + "jpg"
+        full_output_path = os.path.join(stagingdir, jpegFile)
 
         self.log.info("output {}".format(full_output_path))
 
-        input_args = [
-            "-y -gamma 2.2",
-            "-i {}".format(full_input_path),
-            "-framerate {}".format(fps),
-            "-start_number {}".format(start)
-        ]
-        output_args = [
-            "-c:v libx264",
-            "-vf colormatrix=bt601:bt709",
-            full_output_path
-        ]
-
-        mov_args = [
-            "ffmpeg",
-            " ".join(input_args),
-            " ".join(output_args)
-        ]
-        subprocess_mov = " ".join(mov_args)
-        subprocess.Popen(subprocess_mov)
+        subprocess_jpeg = "ffmpeg -y -gamma 2.2 -i {} {}".format(
+            full_input_path, full_output_path
+        )
+        subprocess.Popen(subprocess_jpeg)
 
         if "files" not in instance.data:
             instance.data["files"] = list()
-        instance.data["files"].append(movFile)
+        instance.data["files"].append(jpegFile)
