f275583de40c4e0454d286f57060b105de5923ae
Jakub Trllo <jakub.trllo@gmail.com>
2019-03-01 18:46:18 +0100

    credentials module and ftrack login count with event credentials

--- pype/ftrack/credentials.py
+++ pype/ftrack/credentials.py
@@ -5,49 +5,61 @@ import appdirs
 
 
 config_path = os.path.normpath(appdirs.user_data_dir('pype-app', 'pype'))
-config_name = 'ftrack_cred.json'
-fpath = os.path.join(config_path, config_name)
-folder = os.path.dirname(fpath)
+action_file_name = 'ftrack_cred.json'
+event_file_name = 'ftrack_event_cred.json'
+action_fpath = os.path.join(config_path, action_file_name)
+event_fpath = os.path.join(config_path, event_file_name)
+folders = set([os.path.dirname(action_fpath), os.path.dirname(event_fpath)])
 
-if not os.path.isdir(folder):
-    os.makedirs(folder)
-
-
-def _get_credentials():
-
-    folder = os.path.dirname(fpath)
-
+for folder in folders:
     if not os.path.isdir(folder):
         os.makedirs(folder)
 
+
+def _get_credentials(event=False):
+    if event:
+        fpath = event_fpath
+    else:
+        fpath = action_fpath
+
+    credentials = {}
     try:
         file = open(fpath, 'r')
+        credentials = json.load(file)
     except Exception:
-        filecreate = open(fpath, 'w')
-        filecreate.close()
-        file = open(fpath, 'r')
+        file = open(fpath, 'w')
 
-    credentials = json.load(file)
     file.close()
 
     return credentials
 
 
-def _save_credentials(username, apiKey):
-    file = open(fpath, 'w')
-
+def _save_credentials(username, apiKey, event=False, auto_connect=None):
     data = {
         'username': username,
         'apiKey': apiKey
     }
 
-    credentials = json.dumps(data)
-    file.write(credentials)
+    if event:
+        fpath = event_fpath
+        if auto_connect is None:
+            cred = _get_credentials(True)
+            auto_connect = cred.get('auto_connect', False)
+        data['auto_connect'] = auto_connect
+    else:
+        fpath = action_fpath
+
+    file = open(fpath, 'w')
+    file.write(json.dumps(data))
     file.close()
 
 
-def _clear_credentials():
-    file = open(fpath, 'w').close()
+def _clear_credentials(event=False):
+    if event:
+        fpath = event_fpath
+    else:
+        fpath = action_fpath
+    open(fpath, 'w').close()
     _set_env(None, None)
 
 
