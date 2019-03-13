c01f5946acc7fd8c1369e4992bbd19441dd4c77e
Jakub Trllo <jakub.trllo@gmail.com>
2019-03-13 11:15:03 +0100

    added tray_init to ftrack module

--- pype/ftrack/__init__.py
+++ pype/ftrack/__init__.py
@@ -1,2 +1,9 @@
 from .lib import *
 from .ftrack_server import *
+from .ftrack_run import FtrackRunner
+
+
+def tray_init(tray_widget, main_widget):
+    ftrack = FtrackRunner(main_widget, tray_widget)
+    main_widget.menu.addMenu(ftrack.trayMenu(tray_widget.menu))
+    ftrack.validate()
