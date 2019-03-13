3128a8aad8ac0887112efc9f6ab881d1cff4db1a
Jakub Trllo <jakub.trllo@gmail.com>
2019-03-13 11:14:49 +0100

    avalon_apps added to pype just to separate them from pype-setup

---
+++ pype/avalon_apps/__init__.py
@@ -0,0 +1,7 @@
+from .avalon_app import AvalonApps
+
+
+def tray_init(tray_widget, main_widget):
+    av_apps = AvalonApps(main_widget, tray_widget)
+    av_apps.tray_menu(tray_widget.menu)
+    return av_apps
