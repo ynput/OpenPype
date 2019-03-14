bc8efd319d7b12542235a3aebc4b5bea154c349f
Jakub Trllo <jakub.trllo@gmail.com>
2019-03-14 18:31:38 +0100

    only actual project is synced

--- pype/ftrack/actions/action_clockify_sync.py
+++ pype/ftrack/actions/action_clockify_sync.py
@@ -37,53 +37,7 @@ class SyncClocify(BaseAction):
         ''' Validation '''
         return True
 
-    def interface(self, session, entities, event):
-        if not event['data'].get('values', {}):
-            title = 'Select projects to sync'
-
-            projects = session.query('Project').all()
-
-            items = []
-            all_projects_label = {
-                'type': 'label',
-                'value': 'All projects'
-            }
-            all_projects_value = {
-                'name': '__all__',
-                'type': 'boolean',
-                'value': False
-            }
-            line = {
-                'type': 'label',
-                'value': '___'
-            }
-            items.append(all_projects_label)
-            items.append(all_projects_value)
-            items.append(line)
-            for project in projects:
-                label = project['full_name']
-                item_label = {
-                    'type': 'label',
-                    'value': label
-                }
-                item_value = {
-                    'name': project['id'],
-                    'type': 'boolean',
-                    'value': False
-                }
-                items.append(item_label)
-                items.append(item_value)
-
-            return {
-                'items': items,
-                'title': title
-            }
-
     def launch(self, session, entities, event):
-        values = event['data'].get('values', {})
-        if not values:
-            return
-
         # JOB SETTINGS
         userId = event['source']['user']['id']
         user = session.query('User where id is ' + userId).one()
@@ -97,50 +51,47 @@ class SyncClocify(BaseAction):
         })
         session.commit()
         try:
-            if values.get('__all__', False) is True:
-                projects_to_sync = session.query('Project').all()
-            else:
-                projects_to_sync = []
-                project_query = 'Project where id is "{}"'
-                for project_id, sync in values.items():
-                    if sync is True:
-                        projects_to_sync.append(session.query(
-                            project_query.format(project_id)
-                        ).one())
+            entity = entities[0]
 
-            projects_info = {}
-            for project in projects_to_sync:
-                task_types = []
-                for task_type in project['project_schema']['_task_type_schema'][
-                    'types'
-                ]:
-                    task_types.append(task_type['name'])
-                projects_info[project['full_name']] = task_types
+            if entity.entity_type.lower() == 'project':
+                project = entity
+            else:
+                project = entity['project']
+            project_name = project['full_name']
+
+            task_types = []
+            for task_type in project['project_schema']['_task_type_schema'][
+                'types'
+            ]:
+                task_types.append(task_type['name'])
 
             clockify_projects = self.clockapi.get_projects()
-            for project_name, task_types in projects_info.items():
-                if project_name not in clockify_projects:
-                    response = self.clockapi.add_project(project_name)
+
+            if project_name not in clockify_projects:
+                response = self.clockapi.add_project(project_name)
+                if 'id' not in response:
+                    self.log.error('Project {} can\'t be created'.format(
+                        project_name
+                    ))
+                    return {
+                        'success': False,
+                        'message': 'Can\'t create project, unexpected error'
+                    }
+                project_id = response['id']
+            else:
+                project_id = clockify_projects[project_name]
+
+            clockify_project_tasks = self.clockapi.get_tasks(project_id)
+            for task_type in task_types:
+                if task_type not in clockify_project_tasks:
+                    response = self.clockapi.add_task(
+                        task_type, project_id
+                    )
                     if 'id' not in response:
-                        self.log.error('Project {} can\'t be created'.format(
-                            project_name
+                        self.log.error('Task {} can\'t be created'.format(
+                            task_type
                         ))
                         continue
-                    project_id = response['id']
-                else:
-                    project_id = clockify_projects[project_name]
-
-                clockify_project_tasks = self.clockapi.get_tasks(project_id)
-                for task_type in task_types:
-                    if task_type not in clockify_project_tasks:
-                        response = self.clockapi.add_task(
-                            task_type, project_id
-                        )
-                        if 'id' not in response:
-                            self.log.error('Task {} can\'t be created'.format(
-                                task_type
-                            ))
-                            continue
         except Exception:
             job['status'] = 'failed'
             session.commit()
