d1b6defe18a9706a4abf56d8405fcbffb4061044
Jakub Jezek <jezschaj@gmail.com>
2019-05-25 16:38:29 +0200

    feat(nuke): adding set_frame_range with handles and irregular handles

--- pype/nuke/menu.py
+++ pype/nuke/menu.py
@@ -17,5 +17,17 @@ def install():
     menu.removeItem(rm_item[1].name())
     menu.addCommand(rm_item[1].name(), lib.reset_resolution, index=rm_item[0])
 
+    # replace reset resolution from avalon core to pype's
+    name = "Reset Frame Range"
+    rm_item = [(i, item)
+               for i, item in enumerate(menu.items())
+               if name in item.name()][0]
+    menu.removeItem(rm_item[1].name())
+    menu.addCommand(
+        rm_item[1].name(),
+        lib.reset_frame_range_handles,
+        index=rm_item[0])
+
     # add colorspace menu item
-    menu.addCommand("Set colorspace...", lib.set_colorspace, index=rm_item[0]+1)
+    menu.addCommand("Set colorspace...", lib.set_colorspace,
+                    index=rm_item[0] + 1)
