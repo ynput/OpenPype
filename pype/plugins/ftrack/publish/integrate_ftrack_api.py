56bd87c2531c019f2b2060ccba4d7509416b7736
Jakub Jezek <jezschaj@gmail.com>
2019-05-30 18:25:03 +0200

    fix(ftrack): wasnt integrating properly plates into correct hierarchy

--- pype/plugins/ftrack/publish/integrate_ftrack_api.py
+++ pype/plugins/ftrack/publish/integrate_ftrack_api.py
@@ -56,7 +56,22 @@ class IntegrateFtrackApi(pyblish.api.InstancePlugin):
     def process(self, instance):
 
         session = instance.context.data["ftrackSession"]
-        task = instance.context.data["ftrackTask"]
+        if instance.data.get("ftrackTask"):
+            task = instance.data["ftrackTask"]
+            name = task
+            parent = task["parent"]
+        elif instance.data.get("ftrackEntity"):
+            task = None
+            name = instance.data.get("ftrackEntity")['name']
+            parent = instance.data.get("ftrackEntity")
+        elif instance.context.data.get("ftrackTask"):
+            task = instance.context.data["ftrackTask"]
+            name = task
+            parent = task["parent"]
+        elif instance.context.data.get("ftrackEntity"):
+            task = None
+            name = instance.context.data.get("ftrackEntity")['name']
+            parent = instance.context.data.get("ftrackEntity")
 
         info_msg = "Created new {entity_type} with data: {data}"
         info_msg += ", metadata: {metadata}."
@@ -68,6 +83,7 @@ class IntegrateFtrackApi(pyblish.api.InstancePlugin):
             # Get existing entity.
             assettype_data = {"short": "upload"}
             assettype_data.update(data.get("assettype_data", {}))
+            self.log.debug("data: {}".format(data))
 
             assettype_entity = session.query(
                 self.query("AssetType", assettype_data)
@@ -83,9 +99,9 @@ class IntegrateFtrackApi(pyblish.api.InstancePlugin):
             # Asset
             # Get existing entity.
             asset_data = {
-                "name": task["name"],
+                "name": name,
                 "type": assettype_entity,
-                "parent": task["parent"],
+                "parent": parent,
             }
             asset_data.update(data.get("asset_data", {}))
 
@@ -120,8 +136,10 @@ class IntegrateFtrackApi(pyblish.api.InstancePlugin):
             assetversion_data = {
                 "version": 0,
                 "asset": asset_entity,
-                "task": task
             }
+            if task:
+                assetversion_data['task'] = task
+                
             assetversion_data.update(data.get("assetversion_data", {}))
 
             assetversion_entity = session.query(
