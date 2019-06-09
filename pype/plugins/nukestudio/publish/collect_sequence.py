f637d766008aecee4a3f2787b49fc819ad585ccf
Jakub Jezek <jezschaj@gmail.com>
2019-06-09 17:36:29 +0200

    fix(nukestudio): plugin order was not correct

--- pype/plugins/nukestudio/publish/collect_sequence.py
+++ pype/plugins/nukestudio/publish/collect_sequence.py
@@ -5,7 +5,7 @@ import hiero
 class CollectSequence(api.ContextPlugin):
     """Collect all Track items selection."""
 
-    order = api.CollectorOrder
+    order = api.CollectorOrder - 0.01
     label = "Collect Sequence"
     hosts = ["nukestudio"]
 
