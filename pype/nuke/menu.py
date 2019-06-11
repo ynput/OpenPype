b9f61904830d0e955a1761ddfc2cc95776fd32e9
Jakub Trllo <jakub.trllo@gmail.com>
2019-06-11 18:08:19 +0200

    changed setting items in nuke menu, can be start separately and all

--- pype/nuke/menu.py
+++ pype/nuke/menu.py
@@ -9,21 +9,33 @@ def install():
     menubar = nuke.menu("Nuke")
     menu = menubar.findItem(Session["AVALON_LABEL"])
 
-    # replace reset resolution and reset frame range from avalon with single
-    # command that 'Reset resolution', 'Reset Frame Range' and 'Set colorspace'
+    # replace reset resolution from avalon core to pype's
     name = "Reset Resolution"
+    new_name = "Set Resolution"
     rm_item = [
         (i, item) for i, item in enumerate(menu.items()) if name in item.name()
     ][0]
     menu.removeItem(rm_item[1].name())
+    menu.addCommand(new_name, lib.reset_resolution, index=rm_item[0])
 
+    # replace reset frame range from avalon core to pype's
     name = "Reset Frame Range"
+    new_name = "Set Frame Range"
     rm_item = [
         (i, item) for i, item in enumerate(menu.items()) if name in item.name()
     ][0]
     menu.removeItem(rm_item[1].name())
+    menu.addCommand(new_name, lib.reset_frame_range_handles, index=rm_item[0])
 
-    name = "Set context settings"
+    # add colorspace menu item
+    name = "Set colorspace"
     menu.addCommand(
-        name, lib.set_context_settings, index=rm_item[0]
+        name, lib.set_colorspace,
+        index=(rm_item[0]+2)
+    )
+
+    # add item that applies all setting above
+    name = "Apply all settings"
+    menu.addCommand(
+        name, lib.set_context_settings, index=(rm_item[0]+3)
     )
