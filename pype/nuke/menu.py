9590af1f53c58279f9c55bf898675aff98406ebc
Jakub Trllo <jakub.trllo@gmail.com>
2019-06-11 17:28:47 +0200

    set context action added to avalon menu

--- pype/nuke/menu.py
+++ pype/nuke/menu.py
@@ -9,25 +9,21 @@ def install():
     menubar = nuke.menu("Nuke")
     menu = menubar.findItem(Session["AVALON_LABEL"])
 
-    # replace reset resolution from avalon core to pype's
+    # replace reset resolution and reset frame range from avalon with single
+    # command that 'Reset resolution', 'Reset Frame Range' and 'Set colorspace'
     name = "Reset Resolution"
-    rm_item = [(i, item)
-               for i, item in enumerate(menu.items())
-               if name in item.name()][0]
+    rm_item = [
+        (i, item) for i, item in enumerate(menu.items()) if name in item.name()
+    ][0]
     menu.removeItem(rm_item[1].name())
-    menu.addCommand(rm_item[1].name(), lib.reset_resolution, index=rm_item[0])
 
-    # replace reset resolution from avalon core to pype's
     name = "Reset Frame Range"
-    rm_item = [(i, item)
-               for i, item in enumerate(menu.items())
-               if name in item.name()][0]
+    rm_item = [
+        (i, item) for i, item in enumerate(menu.items()) if name in item.name()
+    ][0]
     menu.removeItem(rm_item[1].name())
-    menu.addCommand(
-        rm_item[1].name(),
-        lib.reset_frame_range_handles,
-        index=rm_item[0])
 
-    # add colorspace menu item
-    menu.addCommand("Set colorspace...", lib.set_colorspace,
-                    index=rm_item[0] + 1)
+    name = "Set context settings"
+    menu.addCommand(
+        name, lib.set_context_settings, index=rm_item[0]
+    )
