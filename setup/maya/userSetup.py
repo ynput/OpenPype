7fd9500ec84ead5c63fb0a3907a1e89941418b06
Milan Kolar <milan@pype.club>
2019-07-10 19:30:02 +0200

    only build project shelf if shelf config exists in presets

--- setup/maya/userSetup.py
+++ setup/maya/userSetup.py
@@ -2,14 +2,14 @@ import os
 import sys
 from pypeapp import config
 from pype.maya import lib
-reload(lib)
 
 presets = config.get_presets()
-shelf_preset = presets['maya']['project_shelf']
-project = os.environ["AVALON_PROJECT"]
+shelf_preset = presets['maya'].get('project_shelf')
+if shelf_preset:
+    project = os.environ["AVALON_PROJECT"]
 
-modules = {}
-for k, v in shelf_preset['imports'].items():
-    sys.modules[k] = __import__(v, fromlist=[project])
+    modules = {}
+    for k, v in shelf_preset['imports'].items():
+        sys.modules[k] = __import__(v, fromlist=[project])
 
-projectShelf = lib.shelf(name=shelf_preset['name'], preset=shelf_preset)
+    projectShelf = lib.shelf(name=shelf_preset['name'], preset=shelf_preset)
