6999c2a00ac8ca728ea0776b1720da96d68fce66
Toke Jepsen <tokejepsen@gmail.com>
2019-07-26 08:12:17 +0100

    Remove redundant code and code cosmetics.

--- pype/nuke/__init__.py
+++ pype/nuke/__init__.py
@@ -1,28 +1,14 @@
 import os
 import sys
+import logging
+
+import nuke
 
 from avalon import api as avalon
 from avalon.tools import workfiles
 from pyblish import api as pyblish
-
-from .. import api
-
 from pype.nuke import menu
-import logging
-
-from .lib import (
-    create_write_node
-)
-
-import nuke
-
 from pypeapp import Logger
-
-# #removing logger handler created in avalon_core
-# for name, handler in [(handler.get_name(), handler)
-#                       for handler in Logger.logging.root.handlers[:]]:
-#     if "pype" not in str(name).lower():
-#         Logger.logging.root.removeHandler(handler)
 from . import lib
 
 
