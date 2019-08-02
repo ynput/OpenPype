cf1ad307bb7e76be3a366e9cbcd60974acef7972
Jakub Jezek <jakub@orbi.tools>
2019-08-02 18:45:48 +0200

    fix(nuke): moving creating linked attributes to Create plugin

--- pype/plugins/nuke/create/create_write.py
+++ pype/plugins/nuke/create/create_write.py
@@ -125,6 +125,24 @@ class CreateWritePrerender(avalon.nuke.Creator):
                 write_data.update({
                     "fpath_template": "{work}/prerenders/{subset}/{subset}.{frame}.{ext}"})
 
-            create_write_node(self.data["subset"], write_data)
+            # get group node
+            group_node = create_write_node(self.data["subset"], write_data)
+
+            # open group node
+            group_node.begin()
+            for n in nuke.allNodes():
+                # get write node
+                if n.Class() in "Write":
+                    write_node = n
+            group_node.end()
+
+            # linking knobs to group property panel
+            linking_knobs = ["first", "last", "use_limit"]
+            for k in linking_knobs:
+                lnk = nuke.Link_Knob(k)
+                lnk.makeLink(write_node.name(), k)
+                lnk.setName(k.replace('_', ' ').capitalize())
+                lnk.clearFlag(nuke.STARTLINE)
+                group_node.addKnob(lnk)
 
         return
