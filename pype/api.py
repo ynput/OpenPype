7a8a94b924def680be448120a49d4dae230d5491
Jakub Jezek <jakub@orbi.tools>
2019-08-14 17:06:51 +0200

    feat(pype): adding get subsets to api

--- pype/api.py
+++ pype/api.py
@@ -23,6 +23,7 @@ from .lib import (
     get_asset,
     get_project,
     get_hierarchy,
+    get_subsets,
     get_version_from_path,
     modified_environ,
     add_tool_to_environment
@@ -53,6 +54,7 @@ __all__ = [
     "get_project",
     "get_hierarchy",
     "get_asset",
+    "get_subsets",
     "get_version_from_path",
     "modified_environ",
     "add_tool_to_environment",
