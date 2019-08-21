46c06ce20d8324aa70ec88ed8f898046a7d48b53
Toke Jepsen <tokejepsen@gmail.com>
2019-08-21 16:09:31 +0100

    Publish comments from NukeStudio.

--- pype/plugins/nukestudio/publish/collect_shots.py
+++ pype/plugins/nukestudio/publish/collect_shots.py
@@ -30,6 +30,24 @@ class CollectShots(api.ContextPlugin):
             for key, value in instance.data.iteritems():
                 data[key] = value
 
+            # Collect comments.
+            data["comments"] = []
+
+            # Exclude non-tagged instances.
+            for tag in instance.data["tags"]:
+                if tag["name"].lower() == "comment":
+                    data["comments"].append(
+                        tag.metadata().dict()["tag.note"]
+                    )
+
+            # Find tags on the source clip.
+            tags = instance.data["item"].source().tags()
+            for tag in tags:
+                if tag.name().lower() == "comment":
+                    data["comments"].append(
+                        tag.metadata().dict()["tag.note"]
+                    )
+
             data["family"] = "shot"
             data["families"] = []
 
@@ -37,8 +55,14 @@ class CollectShots(api.ContextPlugin):
 
             data["name"] = data["subset"] + "_" + data["asset"]
 
-            data["label"] = data["asset"] + " - " + data["subset"] + " - tasks: {} - assetbuilds: {}".format(
-                data["tasks"], [x["name"] for x in data.get("assetbuilds", [])]
+            data["label"] = (
+                "{} - {} - tasks:{} - assetbuilds:{} - comments:{}".format(
+                    data["asset"],
+                    data["subset"],
+                    data["tasks"],
+                    [x["name"] for x in data.get("assetbuilds", [])],
+                    len(data["comments"])
+                )
             )
 
             # Create instance.
