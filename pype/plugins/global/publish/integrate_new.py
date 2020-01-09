5ace134b646dfb3a756859984236807a9ddd47aa
Milan Kolar <milan@pype.club>
2020-01-09 15:24:51 +0100

    add pathlib path resolve

--- pype/plugins/global/publish/integrate_new.py
+++ pype/plugins/global/publish/integrate_new.py
@@ -7,6 +7,7 @@ import errno
 import pyblish.api
 from avalon import api, io
 from avalon.vendor import filelink
+from pathlib import Path
 # this is needed until speedcopy for linux is fixed
 if sys.platform == "win32":
     from speedcopy import copyfile
@@ -468,8 +469,8 @@ class IntegrateAssetNew(pyblish.api.InstancePlugin):
         Returns:
             None
         """
-        src = os.path.normpath(src)
-        dst = os.path.normpath(dst)
+        src = Path(src).resolve()
+        dst = Path(dst).resolve()
 
         self.log.debug("Copying file .. {} -> {}".format(src, dst))
         dirname = os.path.dirname(dst)
@@ -490,6 +491,8 @@ class IntegrateAssetNew(pyblish.api.InstancePlugin):
 
     def hardlink_file(self, src, dst):
         dirname = os.path.dirname(dst)
+        src = Path(src).resolve()
+        dst = Path(dst).resolve()
         try:
             os.makedirs(dirname)
         except OSError as e:
