c55a182ee89a8940d9ba879e38e40de1f8e83f33
Jakub Jezek <jakub@pype.club>
2020-01-14 15:42:03 +0100

    fix(global): fix integrate_new p27 nukestudio pathlib error

--- pype/plugins/global/publish/integrate_new.py
+++ pype/plugins/global/publish/integrate_new.py
@@ -473,8 +473,7 @@ class IntegrateAssetNew(pyblish.api.InstancePlugin):
         src = str(Path(src).resolve())
         drive, _path = os.path.splitdrive(dst)
         unc = Path(drive).resolve()
-        dst = str(unc / _path)
-        src = str(src)
+        dst = str(unc) + _path
 
         self.log.debug("Copying file .. {} -> {}".format(src, dst))
         dirname = os.path.dirname(dst)
@@ -495,12 +494,11 @@ class IntegrateAssetNew(pyblish.api.InstancePlugin):
 
     def hardlink_file(self, src, dst):
         dirname = os.path.dirname(dst)
-        
+
         src = Path(src).resolve()
         drive, _path = os.path.splitdrive(dst)
         unc = Path(drive).resolve()
-        dst = str(unc / _path)
-        src = str(src)
+        dst = str(unc) + _path
 
         try:
             os.makedirs(dirname)
