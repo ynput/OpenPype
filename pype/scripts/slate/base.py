b872ee29919dd568e9ae935870e6ea6f708028b8
iLLiCiTiT <jakub.trllo@gmail.com>
2020-01-15 12:30:54 +0100

    added argument when processing field style

--- pype/scripts/slate/base.py
+++ pype/scripts/slate/base.py
@@ -211,7 +211,9 @@ class BaseObj:
 
                 result = re.search(field_regex, key)
                 if result:
-                    col_idx, row_idx = get_indexes_from_regex_match(result)
+                    col_idx, row_idx = get_indexes_from_regex_match(
+                        result, True
+                    )
                     if self.row_idx == col_idx and self.row_idx == row_idx:
                         obj_specific.update(value)
 
