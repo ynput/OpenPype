1f6d63d6a540409d57326bf09df7ed4ecef7c2d2
iLLiCiTiT <jakub.trllo@gmail.com>
2020-02-03 18:07:08 +0100

    added collect datetime plugin

---
+++ pype/plugins/global/publish/collect_datetime_data.py
@@ -0,0 +1,18 @@
+"""These data *must* be collected only once during publishing process.
+
+Provides:
+    context -> datetimeData
+"""
+
+import pyblish.api
+from pypeapp import config
+
+
+class CollectDateTimeData(pyblish.api.ContextPlugin):
+    order = pyblish.api.CollectorOrder
+    label = "Collect DateTime data"
+
+    def process(self, context):
+        key = "datetimeData"
+        if key not in context.data:
+            context.data[key] = config.get_datetime_data()
