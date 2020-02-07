05929f2b02929b9652411e4f0b53d324f3a67b76
iLLiCiTiT <jakub.trllo@gmail.com>
2020-02-07 18:31:24 +0100

    status get suprocess data ony if they are missing (why to collect same data for each action launch)

--- pype/ftrack/ftrack_server/sub_event_info.py
+++ pype/ftrack/ftrack_server/sub_event_info.py
@@ -1,137 +1,189 @@
 import os
 import sys
-import copy
+import json
 import signal
 import socket
-import uuid
-from datetime import datetime
+import datetime
 
 import ftrack_api
 from ftrack_server import FtrackServer
 from pype.ftrack.ftrack_server.lib import (
-    SocketSession, SocketBaseEventHub,
+    SocketSession, StatusEventHub,
     TOPIC_STATUS_SERVER, TOPIC_STATUS_SERVER_RESULT
 )
 from pypeapp import Logger
 
 log = Logger().get_logger("Event storer")
-log.info(os.environ.get("FTRACK_EVENT_SUB_ID"))
-
-
-class ObjectFactory:
-    session = None
-    sock = None
-    subprocess_id = os.environ["FTRACK_EVENT_SUB_ID"]
-    status_factory = None
-
-
-def trigger_status_info(status_id=None, status=None):
-    if not status and not status_id:
-        log.warning(
-            "`status_id` or `status` must be specified to trigger action."
-        )
-        return
-
-    if not status:
-        status = ObjectFactory.status_factory[status_id]
-
-    if not status:
-        return
-
-    new_event_data = copy.deepcopy(action_data)
-    new_event_data.update({
-        "selection": []
-    })
-    new_event_data["subprocess_id"] = ObjectFactory.subprocess_id
-    new_event_data["status_id"] = status.id
-
-    new_event = ftrack_api.event.base.Event(
-        topic="ftrack.action.launch",
-        data=new_event_data,
-        source=status.source
-    )
-    ObjectFactory.session.event_hub.publish(new_event)
-
-
 action_identifier = (
-    "event.server.status" + ObjectFactory.subprocess_id
+    "event.server.status" + os.environ["FTRACK_EVENT_SUB_ID"]
 )
-
-# TODO add IP adress to label
-# TODO add icon
 action_data = {
     "label": "Pype Admin",
-    "variant": "Event server Status",
+    "variant": "- Event server Status",
     "description": "Get Infromation about event server",
     "actionIdentifier": action_identifier,
     "icon": None
 }
 
 
+class ObjectFactory:
+    session = None
+    status_factory = None
+
+
 class Status:
     default_item = {
         "type": "label",
-        "value": "Information not allowed."
+        "value": "Process info is not available at this moment."
     }
+
+    def __init__(self, name, label, parent):
+        self.name = name
+        self.label = label or name
+        self.parent = parent
+
+        self.info = None
+        self.last_update = None
+
+    def update(self, info):
+        self.last_update = datetime.datetime.now()
+        self.info = info
+
+    def get_delta_string(self, delta):
+        days, hours, minutes = (
+            delta.days, delta.seconds // 3600, delta.seconds // 60 % 60
+        )
+        delta_items = [
+            "{}d".format(days),
+            "{}h".format(hours),
+            "{}m".format(minutes)
+        ]
+        if not days:
+            delta_items.pop(0)
+            if not hours:
+                delta_items.pop(0)
+                delta_items.append("{}s".format(delta.seconds % 60))
+                if not minutes:
+                    delta_items.pop(0)
+
+        return " ".join(delta_items)
+
+    def get_items(self):
+        items = []
+        last_update = "N/A"
+        if self.last_update:
+            delta = datetime.datetime.now() - self.last_update
+            last_update = "{} ago".format(
+                self.get_delta_string(delta)
+            )
+
+        last_update = "Updated: {}".format(last_update)
+        items.append({
+            "type": "label",
+            "value": "#{}".format(self.label)
+        })
+        items.append({
+            "type": "label",
+            "value": "##{}".format(last_update)
+        })
+
+        if not self.info:
+            if self.info is None:
+                trigger_info_get()
+            items.append(self.default_item)
+            return items
+
+        info = {}
+        for key, value in self.info.items():
+            if key not in ["created_at:", "created_at"]:
+                info[key] = value
+                continue
+
+            datetime_value = datetime.datetime.strptime(
+                value, "%Y.%m.%d %H:%M:%S"
+            )
+            delta = datetime.datetime.now() - datetime_value
+
+            running_for = self.get_delta_string(delta)
+            info["Started at"] = "{} [running: {}]".format(value, running_for)
+
+        for key, value in info.items():
+            items.append({
+                "type": "label",
+                "value": "<b>{}:</b> {}".format(key, value)
+            })
+
+        return items
+
+
+class StatusFactory:
+
     note_item = {
         "type": "label",
-        "value": "Hit `submit` to refresh data."
+        "value": (
+            "<i>NOTE: Hit `submit` and uncheck all"
+            " checkers to refresh data.</i>"
+        )
     }
     splitter_item = {
         "type": "label",
         "value": "---"
     }
 
-    def __init__(self, source_info, parent):
-        self.id = str(uuid.uuid1())
-        self.created = datetime.now()
-        self.parent = parent
+    def __init__(self, statuses={}):
+        self.statuses = []
+        for status in statuses.items():
+            self.create_status(*status)
 
-        self.source = source_info
+    def __getitem__(self, key):
+        return self.get(key)
 
-        self.main_process = None
-        self.storer = None
-        self.processor = None
+    def get(self, key, default=None):
+        for status in self.statuses:
+            if status.name == key:
+                return status
+        return default
 
-    def add_result(self, source, data):
-        if source.lower() == "storer":
-            self.storer = data
-
-        elif source.lower() == "processor":
-            self.processor = data
-
-        else:
-            self.main_process = data
-
-    def filled(self):
-        # WARNING DEBUG PART!!!!
+    def is_filled(self):
+        for status in self.statuses:
+            if status.info is None:
+                return False
         return True
-        return (
-            self.main_process is not None and
-            self.storer is not None and
-            self.processor is not None
-        )
 
-    def get_items_from_dict(self, in_dict):
-        items = []
-        for key, value in in_dict.items():
-            items.append({
-                "type": "label",
-                "value": "##{}".format(key)
-            })
-            items.append({
-                "type": "label",
-                "value": value
-            })
-        return items
+    def create_status(self, name, label):
+        new_status = Status(name, label, self)
+        self.statuses.append(new_status)
+
+    def process_event_result(self, event):
+        subprocess_id = event["data"].get("subprocess_id")
+        if subprocess_id != os.environ["FTRACK_EVENT_SUB_ID"]:
+            return
+
+        source = event["data"]["source"]
+        data = event["data"]["status_info"]
+        for status in self.statuses:
+            if status.name == source:
+                status.update(data)
+                break
 
     def bool_items(self):
         items = []
-        name_labels = {
-            "shutdown_main": "Shutdown main process",
-            "reset_storer": "Reset storer",
-            "reset_processor": "Reset processor"
-        }
+        items.append({
+            "type": "label",
+            "value": "#Restart process"
+        })
+        items.append({
+            "type": "label",
+            "value": (
+                "<i><b>WARNING:</b> Main process may not restart"
+                " if does not run as a service!</i>"
+            )
+        })
+
+        name_labels = {}
+        for status in self.statuses:
+            name_labels[status.name] = status.label
+
         for name, label in name_labels.items():
             items.append({
                 "type": "boolean",
@@ -144,74 +196,13 @@ class Status:
     def items(self):
         items = []
         items.append(self.note_item)
-
-        items.append({"type": "label", "value": "Main process"})
-        if not self.main_process:
-            items.append(self.default_item)
-        else:
-            items.extend(
-                self.get_items_from_dict(self.main_process)
-            )
-
-        items.append(self.splitter_item)
-        items.append({"type": "label", "value": "Storer process"})
-        if not self.storer:
-            items.append(self.default_item)
-        else:
-            items.extend(
-                self.get_items_from_dict(self.storer)
-            )
-
-        items.append(self.splitter_item)
-        items.append({"type": "label", "value": "Processor process"})
-        if not self.processor:
-            items.append(self.default_item)
-        else:
-            items.extend(
-                self.get_items_from_dict(self.processor)
-            )
-
-        items.append(self.splitter_item)
         items.extend(self.bool_items())
 
+        for status in self.statuses:
+            items.append(self.splitter_item)
+            items.extend(status.get_items())
+
         return items
-
-    @property
-    def is_overtime(self):
-        time_delta = (datetime.now() - self.created).total_seconds()
-        return time_delta >= self.parent.max_delta_seconds
-
-
-class StatusFactory:
-    max_delta_seconds = 30
-
-    def __init__(self):
-        self.statuses = {}
-
-    def __getitem__(self, key):
-        return self.statuses.get(key)
-
-    def create_status(self, source_info):
-        new_status = Status(source_info, self)
-        self.statuses[new_status.id] = new_status
-        return new_status
-
-    def process_result(self, event):
-        subprocess_id = event["data"].get("subprocess_id")
-        if subprocess_id != ObjectFactory.subprocess_id:
-            return
-
-        status_id = event["data"].get("status_id")
-        status = self.statuses[status_id]
-        if not status:
-            return
-
-        source = event["data"]["source"]
-        data = event["data"]["status_info"]
-
-        status.add_result(source, data)
-        if status.filled():
-            trigger_status_info(status=status)
 
 
 def server_activity_validate_user(event):
@@ -247,38 +238,6 @@ def server_activity_discover(event):
     return {"items": [action_data]}
 
 
-def handle_filled_event(event):
-    subprocess_id = event["data"].get("subprocess_id")
-    if subprocess_id != ObjectFactory.subprocess_id:
-        return None
-
-    status_id = event["data"].get("status_id")
-    status = ObjectFactory.status_factory[status_id]
-    if not status:
-        return None
-
-    values = event.get("values")
-    if values:
-        log.info(values)
-
-    title = "Event server - Status"
-
-    event_data = copy.deepcopy(event["data"])
-    event_data.update({
-        "type": "widget",
-        "items": status.items(),
-        "title": title
-    })
-
-    ObjectFactory.session.event_hub.publish(
-        ftrack_api.event.base.Event(
-            topic="ftrack.action.trigger-user-interface",
-            data=event_data
-        ),
-        on_error='ignore'
-    )
-
-
 def server_activity(event):
     session = ObjectFactory.session
     if session is None:
@@ -289,35 +248,47 @@ def server_activity(event):
             "message": msg
         }
 
-    valid = server_activity_validate_user(event)
-    if not valid:
+    if not server_activity_validate_user(event):
         return {
             "success": False,
             "message": "You don't have permissions to see Event server status!"
         }
 
-    subprocess_id = event["data"].get("subprocess_id")
-    if subprocess_id is not None:
-        return handle_filled_event(event)
+    values = event["data"].get("values") or {}
+    is_checked = False
+    for value in values.values():
+        if value:
+            is_checked = True
+            break
 
-    status = ObjectFactory.status_factory.create_status(event["source"])
+    if not is_checked:
+        return {
+            "items": ObjectFactory.status_factory.items(),
+            "title": "Server current status"
+        }
 
-    event_data = {
-        "status_id": status.id,
-        "subprocess_id": ObjectFactory.subprocess_id
-    }
+
+def trigger_info_get():
+    session = ObjectFactory.session
     session.event_hub.publish(
         ftrack_api.event.base.Event(
             topic=TOPIC_STATUS_SERVER,
-            data=event_data
+            data={"subprocess_id": os.environ["FTRACK_EVENT_SUB_ID"]}
         ),
         on_error="ignore"
     )
 
-    return {
-        "success": True,
-        "message": "Collecting information (this may take > 20s)"
-    }
+
+def on_start(event):
+    session = ObjectFactory.session
+    source_id = event.get("source", {}).get("id")
+    if not source_id or source_id != session.event_hub.id:
+        return
+
+    if session is None:
+        log.warning("Session is not set. Can't trigger Sync to avalon action.")
+        return True
+    trigger_info_get()
 
 
 def register(session):
@@ -326,6 +297,7 @@ def register(session):
         "topic=ftrack.action.discover",
         server_activity_discover
     )
+    session.event_hub.subscribe("topic=pype.status.started", on_start)
 
     status_launch_subscription = (
         "topic=ftrack.action.launch and data.actionIdentifier={}"
@@ -338,34 +310,51 @@ def register(session):
 
     session.event_hub.subscribe(
         "topic={}".format(TOPIC_STATUS_SERVER_RESULT),
-        ObjectFactory.status_factory.process_result
+        ObjectFactory.status_factory.process_event_result
     )
+
+
+def heartbeat():
+    if ObjectFactory.status_factory.is_filled():
+        return
+
+    trigger_info_get()
 
 
 def main(args):
     port = int(args[-1])
+    server_info = json.loads(args[-2])
 
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect the socket to the port where the server is listening
     server_address = ("localhost", port)
-    log.debug("Storer connected to {} port {}".format(*server_address))
+    log.debug("Statuser connected to {} port {}".format(*server_address))
     sock.connect(server_address)
     sock.sendall(b"CreatedStatus")
     # store socket connection object
     ObjectFactory.sock = sock
-    ObjectFactory.status_factory = StatusFactory()
 
+    statuse_names = {
+        "main": "Main process",
+        "storer": "Storer",
+        "processor": "Processor"
+    }
+
+    ObjectFactory.status_factory = StatusFactory(statuse_names)
+    ObjectFactory.status_factory["main"].update(server_info)
     _returncode = 0
     try:
         session = SocketSession(
-            auto_connect_event_hub=True, sock=sock, Eventhub=SocketBaseEventHub
+            auto_connect_event_hub=True, sock=sock, Eventhub=StatusEventHub
         )
         ObjectFactory.session = session
+        session.event_hub.heartbeat_callbacks.append(heartbeat)
         register(session)
         server = FtrackServer("event")
-        log.debug("Launched Ftrack Event storer")
+        log.debug("Launched Ftrack Event statuser")
+
         server.run_server(session, load_files=False)
 
     except Exception:
@@ -388,24 +377,3 @@ if __name__ == "__main__":
     signal.signal(signal.SIGTERM, signal_handler)
 
     sys.exit(main(sys.argv))
-
-
-example_action_event = {
-    'data': {
-        'selection': [],
-        'description': 'Test action2',
-        'variant': None,
-        'label': 'Test action2',
-        'actionIdentifier': 'test.action2.3ceffe5e9acf40f8aa80603adebd0d06',
-        'values': {},
-        'icon': None,
-    },
-    'topic': 'ftrack.action.launch',
-    'sent': None,
-    'source': {
-        'id': 'eb67d186301c4cbbab73c1aee9b7c55d',
-        'user': {'username': 'jakub.trllo', 'id': '2a8ae090-cbd3-11e8-a87a-0a580aa00121'}
-    },
-    'target': '',
-    'in_reply_to_event': None
-}
