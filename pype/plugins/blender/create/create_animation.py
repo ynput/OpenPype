2b6e90ffe00a44a7f9e972389a50c3c8b20fb972
Simone Barbieri <simone.barbieri@thudmedia.com>
2020-02-10 14:55:04 +0000

    Creation, loading, and management of animations

---
+++ pype/plugins/blender/create/create_animation.py
@@ -0,0 +1,30 @@
+import bpy
+
+from avalon import api
+from avalon.blender import Creator, lib
+
+
+class CreateAnimation(Creator):
+    """Animation output for character rigs"""
+
+    name = "animationMain"
+    label = "Animation"
+    family = "animation"
+    icon = "male"
+
+    def process(self):
+        import pype.blender
+
+        asset = self.data["asset"]
+        subset = self.data["subset"]
+        name = pype.blender.plugin.asset_name(asset, subset)
+        collection = bpy.data.collections.new(name=name)
+        bpy.context.scene.collection.children.link(collection)
+        self.data['task'] = api.Session.get('AVALON_TASK')
+        lib.imprint(collection, self.data)
+
+        if (self.options or {}).get("useSelection"):
+            for obj in lib.get_selection():
+                collection.objects.link(obj)
+
+        return collection
