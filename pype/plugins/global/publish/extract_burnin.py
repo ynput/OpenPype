86d3f4d7d2e3d0d95e28b06b1523d0073bc8d7e3
Milan Kolar <milan@pype.club>
2020-02-22 00:19:50 +0100

    prioritize instance version in burnins

--- pype/plugins/global/publish/extract_burnin.py
+++ pype/plugins/global/publish/extract_burnin.py
@@ -26,8 +26,8 @@ class ExtractBurnin(pype.api.Extractor):
         if "representations" not in instance.data:
             raise RuntimeError("Burnin needs already created mov to work on.")
 
-        version = instance.context.data.get(
-            'version', instance.data.get('version'))
+        version = instance.data.get(
+            'version', instance.context.data.get('version'))
         frame_start = int(instance.data.get("frameStart") or 0)
         frame_end = int(instance.data.get("frameEnd") or 1)
         duration = frame_end - frame_start + 1
