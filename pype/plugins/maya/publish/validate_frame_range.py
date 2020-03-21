e5fd3d3b5df0f2717d5a566264940ddabcaf8ad9
Ondrej Samohel <ondrej@samohel.cz>
2020-03-21 19:13:53 +0100

    frame range validator remake

--- pype/plugins/maya/publish/validate_frame_range.py
+++ pype/plugins/maya/publish/validate_frame_range.py
@@ -1,18 +1,18 @@
 import pyblish.api
 import pype.api
 
+from maya import cmds
+
 
 class ValidateFrameRange(pyblish.api.InstancePlugin):
     """Valides the frame ranges.
 
-    Checks the `startFrame`, `endFrame` and `handles` data.
-    This does NOT ensure there's actual data present.
+    This is optional validator checking if the frame range matches the one of
+    asset.
 
-    This validates:
-        - `startFrame` is lower than or equal to the `endFrame`.
-        - must have both the `startFrame` and `endFrame` data.
-        - The `handles` value is not lower than zero.
+    Repair action will change everything to match asset.
 
+    This can be turned off by artist to allow custom ranges.
     """
 
     label = "Validate Frame Range"
@@ -20,26 +20,138 @@ class ValidateFrameRange(pyblish.api.InstancePlugin):
     families = ["animation",
                 "pointcache",
                 "camera",
-                "renderlayer",
-                "colorbleed.vrayproxy"]
+                "render",
+                "review",
+                "yeticache"]
+    optional = True
+    actions = [pype.api.RepairAction]
 
     def process(self, instance):
+        context = instance.context
 
-        start = instance.data.get("frameStart", None)
-        end = instance.data.get("frameEnd", None)
-        handles = instance.data.get("handles", None)
+        frame_start_handle = int(context.data.get("frameStartHandle"))
+        frame_end_handle = int(context.data.get("frameEndHandle"))
+        handles = int(context.data.get("handles"))
+        handle_start = int(context.data.get("handleStart"))
+        handle_end = int(context.data.get("handleEnd"))
+        frame_start = int(context.data.get("frameStart"))
+        frame_end = int(context.data.get("frameEnd"))
 
-        # Check if any of the values are present
-        if any(value is None for value in [start, end]):
-            raise ValueError("No time values for this instance. "
-                             "(Missing `startFrame` or `endFrame`)")
+        inst_start = int(instance.data.get("frameStartHandle"))
+        inst_end = int(instance.data.get("frameEndHandle"))
 
-        self.log.info("Comparing start (%s) and end (%s)" % (start, end))
-        if start > end:
-            raise RuntimeError("The start frame is a higher value "
-                               "than the end frame: "
-                               "{0}>{1}".format(start, end))
+        # basic sanity checks
+        assert frame_start_handle <= frame_end_handle, (
+            "start frame is lower then end frame")
 
-        if handles is not None:
-            if handles < 0.0:
-                raise RuntimeError("Handles are set to a negative value")
+        assert handles >= 0, ("handles cannot have negative values")
+
+        # compare with data on instance
+        errors = []
+
+        if(inst_start != frame_start_handle):
+            errors.append("Instance start frame [ {} ] doesn't "
+                          "match the one set on instance [ {} ]: "
+                          "{}/{}/{}/{} (handle/start/end/handle)".format(
+                              inst_start,
+                              frame_start_handle,
+                              handle_start, frame_start, frame_end, handle_end
+                          ))
+
+        if(inst_end != frame_end_handle):
+            errors.append("Instance end frame [ {} ] doesn't "
+                          "match the one set on instance [ {} ]: "
+                          "{}/{}/{}/{} (handle/start/end/handle)".format(
+                              inst_end,
+                              frame_end_handle,
+                              handle_start, frame_start, frame_end, handle_end
+                          ))
+
+        minTime = int(cmds.playbackOptions(minTime=True, query=True))
+        maxTime = int(cmds.playbackOptions(maxTime=True, query=True))
+        animStartTime = int(cmds.playbackOptions(animationStartTime=True,
+                                                 query=True))
+        animEndTime = int(cmds.playbackOptions(animationEndTime=True,
+                                               query=True))
+
+        if int(minTime) != inst_start:
+            errors.append("Start of Maya timeline is set to frame [ {} ] "
+                          " and doesn't match the one set on asset [ {} ]: "
+                          "{}/{}/{}/{} (handle/start/end/handle)".format(
+                              minTime,
+                              frame_start_handle,
+                              handle_start, frame_start, frame_end, handle_end
+                          ))
+
+        if int(maxTime) != inst_end:
+            errors.append("End of Maya timeline is set to frame [ {} ] "
+                          " and doesn't match the one set on asset [ {} ]: "
+                          "{}/{}/{}/{} (handle/start/end/handle)".format(
+                              maxTime,
+                              frame_end_handle,
+                              handle_start, frame_start, frame_end, handle_end
+                          ))
+
+        if int(animStartTime) != inst_start:
+            errors.append("Animation start in Maya is set to frame [ {} ] "
+                          " and doesn't match the one set on asset [ {} ]: "
+                          "{}/{}/{}/{} (handle/start/end/handle)".format(
+                              animStartTime,
+                              frame_start_handle,
+                              handle_start, frame_start, frame_end, handle_end
+                          ))
+
+        if int(animEndTime) != inst_end:
+            errors.append("Animation start in Maya is set to frame [ {} ] "
+                          " and doesn't match the one set on asset [ {} ]: "
+                          "{}/{}/{}/{} (handle/start/end/handle)".format(
+                              animEndTime,
+                              frame_end_handle,
+                              handle_start, frame_start, frame_end, handle_end
+                          ))
+
+        render_start = int(cmds.getAttr("defaultRenderGlobals.startFrame"))
+        render_end = int(cmds.getAttr("defaultRenderGlobals.endFrame"))
+
+        if int(render_start) != inst_start:
+            errors.append("Render settings start frame is set to [ {} ] "
+                          " and doesn't match the one set on asset [ {} ]: "
+                          "{}/{}/{}/{} (handle/start/end/handle)".format(
+                              int(render_start),
+                              frame_start_handle,
+                              handle_start, frame_start, frame_end, handle_end
+                          ))
+
+        if int(render_end) != inst_end:
+            errors.append("Render settings end frame is set to [ {} ] "
+                          " and doesn't match the one set on asset [ {} ]: "
+                          "{}/{}/{}/{} (handle/start/end/handle)".format(
+                              int(render_end),
+                              frame_end_handle,
+                              handle_start, frame_start, frame_end, handle_end
+                          ))
+
+        for e in errors:
+            self.log.error(e)
+
+        assert len(errors) == 0, ("Frame range settings are incorrect")
+
+    @classmethod
+    def repair(cls, instance):
+        """
+        Repair by calling avalon reset frame range function. This will set
+        timeline frame range, render settings range and frame information
+        on instance container to match asset data.
+        """
+        import avalon.maya.interactive
+        avalon.maya.interactive.reset_frame_range()
+        cls.log.debug("-" * 80)
+        cls.log.debug("{}.frameStart".format(instance.data["name"]))
+        cmds.setAttr(
+            "{}.frameStart".format(instance.data["name"]),
+            instance.context.data.get("frameStartHandle"))
+
+        cmds.setAttr(
+            "{}.frameEnd".format(instance.data["name"]),
+            instance.context.data.get("frameEndHandle"))
+        cls.log.debug("-" * 80)
