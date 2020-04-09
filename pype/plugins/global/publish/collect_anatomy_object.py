2e431b32e1f894a398a3865b4bb2ea4e8ecd06b4
iLLiCiTiT <jakub.trllo@gmail.com>
2020-04-09 14:27:02 +0200

    collect anatomy was split into 2 plugins to be able to collect anatomy obj before collect rendered files

---
+++ pype/plugins/global/publish/collect_anatomy_object.py
@@ -0,0 +1,34 @@
+"""Collect Anatomy object.
+
+Requires:
+    os.environ -> AVALON_PROJECT
+
+Provides:
+    context -> anatomy (pypeapp.Anatomy)
+"""
+
+from avalon import io
+from pypeapp import Anatomy
+import pyblish.api
+
+
+class CollectAnatomyObject(pyblish.api.ContextPlugin):
+    """Collect Anatomy object into Context"""
+
+    order = pyblish.api.CollectorOrder - 0.11
+    label = "Collect Anatomy Object"
+
+    def process(self, context):
+        io.install()
+        project_name = io.Session.get("AVALON_PROJECT")
+        if project_name is None:
+            raise AssertionError(
+                "Environment `AVALON_PROJECT` is not set."
+                "Could not initialize project's Anatomy."
+            )
+
+        context.data["anatomy"] = Anatomy(project_name)
+
+        self.log.info(
+            "Anatomy object collected for project \"{}\".".format(project_name)
+        )
