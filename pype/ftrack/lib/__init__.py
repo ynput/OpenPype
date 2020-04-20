fef534e01489e6ca407c1ff85fe3d2e47a756fe3
iLLiCiTiT <jakub.trllo@gmail.com>
2020-04-20 20:10:49 +0200

    removed lib.py from ftrack lib because one method is duplicated and second is used only in one action

--- pype/ftrack/lib/__init__.py
+++ pype/ftrack/lib/__init__.py
@@ -4,8 +4,3 @@ from .ftrack_base_handler import BaseHandler
 from .ftrack_event_handler import BaseEvent
 from .ftrack_action_handler import BaseAction
 from .ftrack_app_handler import AppAction
-
-from .lib import (
-    get_project_from_entity,
-    get_avalon_entities_for_assetversion
-)
