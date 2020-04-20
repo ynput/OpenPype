3854831e757608a7300dece1bfd1b7626608c262
iLLiCiTiT <jakub.trllo@gmail.com>
2020-04-20 19:02:11 +0200

    import in lib are specified

--- pype/ftrack/lib/__init__.py
+++ pype/ftrack/lib/__init__.py
@@ -1,9 +1,9 @@
 from . import avalon_sync
 from . import credentials
-from .ftrack_app_handler import *
-from .ftrack_event_handler import *
-from .ftrack_action_handler import *
-from .ftrack_base_handler import *
+from .ftrack_base_handler import BaseHandler
+from .ftrack_event_handler import BaseEvent
+from .ftrack_action_handler import BaseAction
+from .ftrack_app_handler import AppAction
 
 from .lib import (
     get_project_from_entity,
