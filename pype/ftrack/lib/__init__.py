aa17248649d5591cce551043c833a90924011673
iLLiCiTiT <jakub.trllo@gmail.com>
2020-04-20 20:11:39 +0200

    ftrack/lib/__init__ has __all__ now

--- pype/ftrack/lib/__init__.py
+++ pype/ftrack/lib/__init__.py
@@ -4,3 +4,12 @@ from .ftrack_base_handler import BaseHandler
 from .ftrack_event_handler import BaseEvent
 from .ftrack_action_handler import BaseAction
 from .ftrack_app_handler import AppAction
+
+__all__ = [
+    "avalon_sync",
+    "credentials",
+    "BaseHandler",
+    "BaseEvent",
+    "BaseAction",
+    "AppAction"
+]
