47c8e26220b78517c9483812d7bd5f23db46c298
Jakub Jezek <jakub@pype.club>
2020-04-28 18:11:53 +0200

    fix(celaction): _winreg to winreg and additional prints

--- pype/hooks/celaction/prelaunch.py
+++ pype/hooks/celaction/prelaunch.py
@@ -1,6 +1,6 @@
 import logging
 import os
-import _winreg
+import winreg
 from pype.lib import PypeHook
 from pypeapp import Logger
 
@@ -30,59 +30,64 @@ class CelactionPrelaunchHook(PypeHook):
         task = env["AVALON_TASK"]
         workdir = env["AVALON_WORKDIR"]
         project_name = f"{asset}_{task}"
+        version = "v001"
 
         self.log.info(f"{self.signature}")
 
         os.makedirs(workdir, exist_ok=True)
+        self.log.info(f"Work dir is: `{workdir}`")
 
-        project_file = os.path.join(workdir, f"{project_name}.scn")
+        project_file = os.path.join(workdir, f"{project_name}_{version}.scn")
         env["PYPE_CELACTION_PROJECT_FILE"] = project_file
+
+        self.log.info(f"Workfile is: `{project_file}`")
 
         ##########################
         # setting output parameters
         path = r"Software\CelAction\CelAction2D\User Settings"
-        _winreg.CreateKey(_winreg.HKEY_CURRENT_USER, path)
-        hKey = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,
-                               r"Software\CelAction\CelAction2D\User Settings",
-                               0, _winreg.KEY_ALL_ACCESS)
+        winreg.CreateKey(winreg.HKEY_CURRENT_USER, path)
+        hKey = winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER,
+            "Software\\CelAction\\CelAction2D\\User Settings", 0,
+            winreg.KEY_ALL_ACCESS)
 
         # TODO: change to root path and pyblish standalone to premiere way
         root_path = os.getenv("PIPELINE_ROOT", os.path.dirname(__file__))
         path = os.path.join(root_path, "launchers", "pyblish_standalone.bat")
 
-        _winreg.SetValueEx(hKey, "SubmitAppTitle", 0, _winreg.REG_SZ, path)
+        winreg.SetValueEx(hKey, "SubmitAppTitle", 0, winreg.REG_SZ, path)
 
         parameters = " --path \"*SCENE*\" -d chunk *CHUNK* -d start *START*"
         parameters += " -d end *END* -d x *X* -d y *Y* -rh celaction"
         parameters += " -8 -d progpath \"*PROGPATH*\""
-        _winreg.SetValueEx(hKey, "SubmitParametersTitle", 0, _winreg.REG_SZ,
-                           parameters)
+        winreg.SetValueEx(hKey, "SubmitParametersTitle", 0, winreg.REG_SZ,
+                          parameters)
 
         # setting resolution parameters
         path = r"Software\CelAction\CelAction2D\User Settings\Dialogs"
         path += r"\SubmitOutput"
-        _winreg.CreateKey(_winreg.HKEY_CURRENT_USER, path)
-        hKey = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, path, 0,
-                               _winreg.KEY_ALL_ACCESS)
-        _winreg.SetValueEx(hKey, "SaveScene", 0, _winreg.REG_DWORD, 1)
-        _winreg.SetValueEx(hKey, "CustomX", 0, _winreg.REG_DWORD, 1920)
-        _winreg.SetValueEx(hKey, "CustomY", 0, _winreg.REG_DWORD, 1080)
+        winreg.CreateKey(winreg.HKEY_CURRENT_USER, path)
+        hKey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path, 0,
+                              winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(hKey, "SaveScene", 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(hKey, "CustomX", 0, winreg.REG_DWORD, 1920)
+        winreg.SetValueEx(hKey, "CustomY", 0, winreg.REG_DWORD, 1080)
 
         # making sure message dialogs don't appear when overwriting
         path = r"Software\CelAction\CelAction2D\User Settings\Messages"
         path += r"\OverwriteScene"
-        _winreg.CreateKey(_winreg.HKEY_CURRENT_USER, path)
-        hKey = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, path, 0,
-                               _winreg.KEY_ALL_ACCESS)
-        _winreg.SetValueEx(hKey, "Result", 0, _winreg.REG_DWORD, 6)
-        _winreg.SetValueEx(hKey, "Valid", 0, _winreg.REG_DWORD, 1)
+        winreg.CreateKey(winreg.HKEY_CURRENT_USER, path)
+        hKey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path, 0,
+                              winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(hKey, "Result", 0, winreg.REG_DWORD, 6)
+        winreg.SetValueEx(hKey, "Valid", 0, winreg.REG_DWORD, 1)
 
         path = r"Software\CelAction\CelAction2D\User Settings\Messages"
         path += r"\SceneSaved"
-        _winreg.CreateKey(_winreg.HKEY_CURRENT_USER, path)
-        hKey = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, path, 0,
-                               _winreg.KEY_ALL_ACCESS)
-        _winreg.SetValueEx(hKey, "Result", 0, _winreg.REG_DWORD, 1)
-        _winreg.SetValueEx(hKey, "Valid", 0, _winreg.REG_DWORD, 1)
+        winreg.CreateKey(winreg.HKEY_CURRENT_USER, path)
+        hKey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path, 0,
+                              winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(hKey, "Result", 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(hKey, "Valid", 0, winreg.REG_DWORD, 1)
 
         return True
