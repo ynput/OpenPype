f345da371520b96ba6cc021fdaea224510d66776
iLLiCiTiT <jakub.trllo@gmail.com>
2020-05-04 19:47:19 +0200

    removed standalone action support

--- pype/ftrack/actions/action_create_cust_attrs.py
+++ pype/ftrack/actions/action_create_cust_attrs.py
@@ -1,9 +1,6 @@
 import os
-import sys
-import argparse
 import json
 import arrow
-import logging
 import ftrack_api
 from pype.ftrack import BaseAction
 from pype.ftrack.lib.avalon_sync import CustAttrIdKey
@@ -652,42 +649,3 @@ def register(session, plugins_presets={}):
     '''Register plugin. Called when used as an plugin.'''
 
     CustomAttributes(session, plugins_presets).register()
-
-
-def main(arguments=None):
-    '''Set up logging and register action.'''
-    if arguments is None:
-        arguments = []
-
-    parser = argparse.ArgumentParser()
-    # Allow setting of logging level from arguments.
-    loggingLevels = {}
-    for level in (
-        logging.NOTSET, logging.DEBUG, logging.INFO, logging.WARNING,
-        logging.ERROR, logging.CRITICAL
-    ):
-        loggingLevels[logging.getLevelName(level).lower()] = level
-
-    parser.add_argument(
-        '-v', '--verbosity',
-        help='Set the logging output verbosity.',
-        choices=loggingLevels.keys(),
-        default='info'
-    )
-    namespace = parser.parse_args(arguments)
-
-    # Set up basic logging
-    logging.basicConfig(level=loggingLevels[namespace.verbosity])
-
-    session = ftrack_api.Session()
-    register(session)
-
-    # Wait for events
-    logging.info(
-        'Registered actions and listening for events. Use Ctrl-C to abort.'
-    )
-    session.event_hub.wait()
-
-
-if __name__ == '__main__':
-    raise SystemExit(main(sys.argv[1:]))
