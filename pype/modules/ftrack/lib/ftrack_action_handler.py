7a1b4ef35875992dc9ed6bfd576b554acd93c766
iLLiCiTiT <jakub.trllo@gmail.com>
2020-06-04 18:34:32 +0200

    added statics_icon function for easier getting icon path for actions

--- pype/modules/ftrack/lib/ftrack_action_handler.py
+++ pype/modules/ftrack/lib/ftrack_action_handler.py
@@ -1,4 +1,12 @@
+import os
 from .ftrack_base_handler import BaseHandler
+
+
+def statics_icon(*icon_statics_file_parts):
+    statics_server = os.environ.get("PYPE_STATICS_SERVER")
+    if not statics_server:
+        return None
+    return "/".join((statics_server, *icon_statics_file_parts))
 
 
 class BaseAction(BaseHandler):
