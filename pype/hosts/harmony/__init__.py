9a7412d964c571be47443ac6b44c622482e4b42d
Toke Stuart Jepsen <tokejepsen@gmail.com>
2020-06-11 23:37:14 +0100

    Ensure scene settings on application launch.

--- pype/hosts/harmony/__init__.py
+++ pype/hosts/harmony/__init__.py
@@ -1,7 +1,46 @@
 import os
+import time
 
 from avalon import api, harmony
 import pyblish.api
+from pype import lib
+
+
+def ensure_scene_settings():
+    fps = lib.get_asset()["data"]["fps"]
+    frame_start = lib.get_asset()["data"]["frameStart"]
+    frame_end = lib.get_asset()["data"]["frameEnd"]
+
+    settings = {
+        "setFrameRate": fps,
+        "setStartFrame": frame_start,
+        "setStopFrame": frame_end
+    }
+    func = """function func(arg)
+    {{
+        scene.{method}(arg);
+    }}
+    func
+    """
+    for key, value in settings.items():
+        if value is None:
+            continue
+
+        # Need to wait to not spam Harmony with multiple requests at the same
+        # time.
+        time.sleep(1)
+
+        harmony.send({"function": func.format(method=key), "args": [value]})
+
+    time.sleep(1)
+
+    func = """function func(arg)
+    {
+        frame.remove(arg, frame.numberOf() - arg);
+    }
+    func
+    """
+    harmony.send({"function": func, "args": [frame_end]})
 
 
 def install():
@@ -23,9 +62,12 @@ def install():
         api.Creator, os.path.join(plugins_directory, "create")
     )
 
+    # Register callbacks.
     pyblish.api.register_callback(
         "instanceToggled", on_pyblish_instance_toggled
     )
+
+    api.on("application.launched", ensure_scene_settings)
 
 
 def on_pyblish_instance_toggled(instance, old_value, new_value):
