d81b0221470d2052cb60202b458ada5f0c89939e
iLLiCiTiT <jakub.trllo@gmail.com>
2020-06-24 10:16:13 +0200

    module specifications are loaded from json in pype

--- pype/tools/tray/pype_tray.py
+++ pype/tools/tray/pype_tray.py
@@ -31,7 +31,17 @@ class TrayManager:
 
         self.errors = []
 
-        self.services_thread = None
+        CURRENT_DIR = os.path.dirname(__file__)
+        self.modules_imports = config.load_json(
+            os.path.join(CURRENT_DIR, "modules_imports.json")
+        )
+        presets = config.get_presets(first_run=True)
+        try:
+            self.modules_usage = presets["tray"]["menu_items"]["item_usage"]
+        except Exception:
+            self.modules_usage = {}
+            self.log.critical("Couldn't find modules usage data.")
+
         self.icon_run = QtGui.QIcon(get_resource("circle_green.png"))
         self.icon_stay = QtGui.QIcon(get_resource("circle_orange.png"))
         self.icon_failed = QtGui.QIcon(get_resource("circle_red.png"))
@@ -62,27 +72,23 @@ class TrayManager:
             }
         In this case `Statics Server` won't be used.
         """
-        # Backwards compatible presets loading
-        if isinstance(self.items, list):
-            items = self.items
-        else:
-            items = []
-            # Get booleans is module should be used
-            usages = self.items.get("item_usage") or {}
-            for item in self.items.get("item_import", []):
-                import_path = item.get("import_path")
-                title = item.get("title")
 
-                item_usage = usages.get(title)
-                if item_usage is None:
-                    item_usage = usages.get(import_path, True)
+        items = []
+        # Get booleans is module should be used
+        for item in self.modules_imports:
+            import_path = item.get("import_path")
+            title = item.get("title")
 
-                if item_usage:
-                    items.append(item)
-                else:
-                    if not title:
-                        title = import_path
-                    self.log.debug("{} - Module ignored".format(title))
+            item_usage = self.modules_usage.get(title)
+            if item_usage is None:
+                item_usage = self.modules_usage.get(import_path, True)
+
+            if item_usage:
+                items.append(item)
+            else:
+                if not title:
+                    title = import_path
+                self.log.info("{} - Module ignored".format(title))
 
         if items:
             self.process_items(items, self.tray_widget.menu)
