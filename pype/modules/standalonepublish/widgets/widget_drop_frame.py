c558f46dd3dc165577d02943e8988926cfa18b58
iLLiCiTiT <jakub.trllo@gmail.com>
2020-06-24 17:27:05 +0200

    extensions moved from presets to standalone publisher widgets

--- pype/modules/standalonepublish/widgets/widget_drop_frame.py
+++ pype/modules/standalonepublish/widgets/widget_drop_frame.py
@@ -10,6 +10,34 @@ from . import DropEmpty, ComponentsList, ComponentItem
 
 
 class DropDataFrame(QtWidgets.QFrame):
+    image_extensions = [
+        ".ani", ".anim", ".apng", ".art", ".bmp", ".bpg", ".bsave", ".cal",
+        ".cin", ".cpc", ".cpt", ".dds", ".dpx", ".ecw", ".exr", ".fits",
+        ".flic", ".flif", ".fpx", ".gif", ".hdri", ".hevc", ".icer",
+        ".icns", ".ico", ".cur", ".ics", ".ilbm", ".jbig", ".jbig2",
+        ".jng", ".jpeg", ".jpeg-ls", ".jpeg", ".2000", ".jpg", ".xr",
+        ".jpeg", ".xt", ".jpeg-hdr", ".kra", ".mng", ".miff", ".nrrd",
+        ".ora", ".pam", ".pbm", ".pgm", ".ppm", ".pnm", ".pcx", ".pgf",
+        ".pictor", ".png", ".psd", ".psb", ".psp", ".qtvr", ".ras",
+        ".rgbe", ".logluv", ".tiff", ".sgi", ".tga", ".tiff", ".tiff/ep",
+        ".tiff/it", ".ufo", ".ufp", ".wbmp", ".webp", ".xbm", ".xcf",
+        ".xpm", ".xwd"
+    ]
+    video_extensions = [
+        ".3g2", ".3gp", ".amv", ".asf", ".avi", ".drc", ".f4a", ".f4b",
+        ".f4p", ".f4v", ".flv", ".gif", ".gifv", ".m2v", ".m4p", ".m4v",
+        ".mkv", ".mng", ".mov", ".mp2", ".mp4", ".mpe", ".mpeg", ".mpg",
+        ".mpv", ".mxf", ".nsv", ".ogg", ".ogv", ".qt", ".rm", ".rmvb",
+        ".roq", ".svi", ".vob", ".webm", ".wmv", ".yuv"
+    ]
+    extensions = {
+        "nuke": [".nk"],
+        "maya": [".ma", ".mb"],
+        "houdini": [".hip"],
+        "image_file": image_extensions,
+        "video_file": video_extensions
+    }
+
     def __init__(self, parent):
         super().__init__()
         self.parent_widget = parent
