2afe643284cd6755987844ca9dcb1fb09e87f067
iLLiCiTiT <jakub.trllo@gmail.com>
2020-06-26 16:00:25 +0200

    rest api and timers manager got CLASS_DEFINITION variable in module definition

--- pype/modules/rest_api/__init__.py
+++ pype/modules/rest_api/__init__.py
@@ -2,6 +2,8 @@ from .rest_api import RestApiServer
 from .base_class import RestApi, abort, route, register_statics
 from .lib import RestMethods, CallbackResult
 
+CLASS_DEFINIION = RestApiServer
+
 
 def tray_init(tray_widget, main_widget):
     return RestApiServer()
