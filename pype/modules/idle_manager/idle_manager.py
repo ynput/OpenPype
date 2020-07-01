b28c7f36fb03011a757f06aac91c44d92d9011c9
iLLiCiTiT <jakub.trllo@gmail.com>
2020-07-01 17:42:23 +0200

    there are not registered Qt signals ut callbacks

--- pype/modules/idle_manager/idle_manager.py
+++ pype/modules/idle_manager/idle_manager.py
@@ -11,14 +11,13 @@ class IdleManager(threading.Thread):
     Idle time resets on keyboard/mouse input.
     Is able to emit signals at specific time idle.
     """
-    time_signals = collections.defaultdict(list)
+    time_callbacks = collections.defaultdict(list)
     idle_time = 0
     signal_reset_timer = QtCore.Signal()
 
     def __init__(self):
         super(IdleManager, self).__init__()
         self.log = Logger().get_logger(self.__class__.__name__)
-        self.signal_reset_timer.connect(self._reset_time)
         self.qaction = None
         self.failed_icon = None
         self._is_running = False
@@ -33,18 +32,18 @@ class IdleManager(threading.Thread):
     def tray_exit(self):
         self.stop()
         try:
-            self.time_signals = {}
+            self.time_callbacks = {}
         except Exception:
             pass
 
-    def add_time_signal(self, emit_time, signal):
-        """ If any module want to use IdleManager, need to use add_time_signal
-        :param emit_time: time when signal will be emitted
-        :type emit_time: int
-        :param signal: signal that will be emitted (without objects)
-        :type signal: QtCore.Signal
+    def add_time_callback(self, emit_time, callback):
+        """If any module want to use IdleManager, need to use this method.
+
+        Args:
+            emit_time(int): Time when callback will be triggered.
+            callback(func): Callback that will be triggered.
         """
-        self.time_signals[emit_time].append(signal)
+        self.time_callbacks[emit_time].append(callback)
 
     @property
     def is_running(self):
