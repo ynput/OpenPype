22ff14b93d7bfb62808f1b784d7c0f85bc722bd0
iLLiCiTiT <jakub.trllo@gmail.com>
2020-07-13 11:18:28 +0200

    set CLASS_DEFINIION in clockify to be able set workspace attribute

--- pype/modules/clockify/__init__.py
+++ pype/modules/clockify/__init__.py
@@ -3,12 +3,8 @@ from .widget_settings import ClockifySettings
 from .widget_message import MessageWidget
 from .clockify import ClockifyModule
 
-__all__ = [
-    "ClockifyAPI",
-    "ClockifySettings",
-    "ClockifyModule",
-    "MessageWidget"
-]
-    
+CLASS_DEFINIION = ClockifyModule
+
+
 def tray_init(tray_widget, main_widget):
     return ClockifyModule(main_widget, tray_widget)
