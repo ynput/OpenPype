2fdf6a4ec1e2253370b1bc4a052e233c8b7929b2
iLLiCiTiT <jakub.trllo@gmail.com>
2020-07-14 11:28:39 +0200

    undo changes in ftrack.lib.__init__

--- pype/modules/ftrack/lib/__init__.py
+++ pype/modules/ftrack/lib/__init__.py
@@ -1,6 +1,6 @@
 from . import avalon_sync
 from . import credentials
-from .ftrack_base_handler import BaseHandler, MissingPermision
+from .ftrack_base_handler import BaseHandler
 from .ftrack_event_handler import BaseEvent
 from .ftrack_action_handler import BaseAction, statics_icon
 from .ftrack_app_handler import AppAction
@@ -9,7 +9,6 @@ __all__ = [
     "avalon_sync",
     "credentials",
     "BaseHandler",
-    "MissingPermision",
     "BaseEvent",
     "BaseAction",
     "statics_icon",
