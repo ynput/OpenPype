0cdf9ac479d2ce0bf5d5258010502ac29251cec5
iLLiCiTiT <jakub.trllo@gmail.com>
2020-07-15 15:02:20 +0200

    set estimate for new project

--- pype/modules/clockify/clockify_api.py
+++ pype/modules/clockify/clockify_api.py
@@ -363,7 +363,7 @@ class ClockifyAPI(metaclass=Singleton):
             "clientId": "",
             "isPublic": "false",
             "estimate": {
-                # "estimate": "3600",
+                "estimate": "0",
                 "type": "AUTO"
             },
             "color": "#f44336",
