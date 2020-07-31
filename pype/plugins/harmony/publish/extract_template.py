a6a35e5ed0424bfaf53434e6a4110f4d2fdd9c1b
Aleks Berland <aleksb@yowzaanimation.com>
2020-07-30 22:17:40 -0400

    Add ftrack family to extract_template.py

--- pype/plugins/harmony/publish/extract_template.py
+++ pype/plugins/harmony/publish/extract_template.py
@@ -11,7 +11,7 @@ class ExtractTemplate(pype.api.Extractor):
 
     label = "Extract Template"
     hosts = ["harmony"]
-    families = ["scene"]
+    families = ["scene", "ftrack"]
 
     def process(self, instance):
         staging_dir = self.staging_dir(instance)
