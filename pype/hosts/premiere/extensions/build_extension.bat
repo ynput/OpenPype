6ec98ea426834ea86ebbb373747344ad2597b4fd
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-04 18:40:38 -0400

    Fix build of premiere zxp and added built zxp

--- pype/hosts/premiere/extensions/build_extension.bat
+++ pype/hosts/premiere/extensions/build_extension.bat
@@ -8,13 +8,13 @@ rem !!! make sure you run windows power shell as admin
 set pwd="12PPROext581"
 
 echo ">>> creating certificate ..."
-.\ZXPSignCmd -selfSignedCert CZ Prague OrbiTools "Signing robot" %pwd% certificate.p12
+ZXPSignCmd -selfSignedCert CZ Prague OrbiTools "Signing robot" %pwd% certificate.p12
 echo ">>> building com.pype"
-.\ZXPSignCmd -sign com.pype/ pype.zxp certificate.p12 %pwd%
+ZXPSignCmd -sign com.pype/ pype.zxp certificate.p12 %pwd%
 echo ">>> building com.pype.rename"
-.\ZXPSignCmd -sign com.pype.rename/ pype_rename.zxp certificate.p12 %pwd%
+ZXPSignCmd -sign com.pype.rename/ pype_rename.zxp certificate.p12 %pwd%
 
 echo ">>> installing com.pype"
-.\ExManCmd.exe /install .\pype.zxp
+ExManCmd.exe /install pype.zxp
 echo ">>> installing com.pype.rename"
-.\ExManCmd.exe /install .\pype_rename.zxp
+ExManCmd.exe /install pype_rename.zxp
