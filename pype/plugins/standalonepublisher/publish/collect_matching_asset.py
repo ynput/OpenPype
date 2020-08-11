c2696e4d4fae61a6022c34f78513b83f87c82383
iLLiCiTiT <jakub.trllo@gmail.com>
2020-08-11 18:24:23 +0200

    moved CollectProjectAssets to CollectMatchingAssetToInstance as is not used anywhere else yet

--- pype/plugins/standalonepublisher/publish/collect_matching_asset.py
+++ pype/plugins/standalonepublisher/publish/collect_matching_asset.py
@@ -1,5 +1,6 @@
 import os
 import pyblish.api
+from avalon import io
 from pprint import pformat
 
 
@@ -15,12 +16,31 @@ class CollectMatchingAssetToInstance(pyblish.api.InstancePlugin):
     family = ["image"]
 
     def process(self, instance):
-        project_assets = instance.context.data["projectAssets"]
         source_file = os.path.basename(instance.data["source"])
-        asset = next((project_assets[name] for name in project_assets
-                      if name in source_file), None)
+        self.log.info("Looking for asset document for file \"{}\"".format(
+            instance.data["source"]
+        ))
+        project_assets = {
+            asset_doc["name"]: asset_doc
+            for asset_doc in io.find({"type": "asset"})
+        }
 
-        if asset:
-            instance.data["asset"] = asset["name"]
-            instance.data["assetEntity"] = asset
-            self.log.info(f"Matching asset assigned: {pformat(asset)}")
+        matching_asset_doc = project_assets.get(source_file)
+        if matching_asset_doc is None:
+            for asset_doc in project_assets.values():
+                if asset_doc["name"] in source_file:
+                    matching_asset_doc = asset_doc
+                    break
+
+        if not matching_asset_doc:
+            # TODO better error message
+            raise AssertionError((
+                "Filename does not contain any name of"
+                " asset documents in database."
+            ))
+
+        instance.data["asset"] = matching_asset_doc["name"]
+        instance.data["assetEntity"] = matching_asset_doc
+        self.log.info(
+            f"Matching asset found: {pformat(matching_asset_doc)}"
+        )
