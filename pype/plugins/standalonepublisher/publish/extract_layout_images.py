9e3419424d5884298c977b326e353f9c78648f24
iLLiCiTiT <jakub.trllo@gmail.com>
2020-08-11 19:04:31 +0200

    version is updated and added few logs

--- pype/plugins/standalonepublisher/publish/extract_layout_images.py
+++ pype/plugins/standalonepublisher/publish/extract_layout_images.py
@@ -2,6 +2,7 @@ import os
 import copy
 import pype.api
 import pyblish.api
+from avalon import io
 
 PSDImage = None
 
@@ -14,6 +15,7 @@ class ExtractImageForLayout(pype.api.Extractor):
     hosts = ["standalonepublisher"]
 
     new_instance_family = "image"
+    ignored_instance_data_keys = ("name", "label", "stagingDir", "version")
 
     # Presetable
     allowed_group_names = ["OL", "BG", "MG", "FG", "UL", "SKY", "Field Guide"]
@@ -51,6 +53,7 @@ class ExtractImageForLayout(pype.api.Extractor):
         instance.context.remove(instance)
 
     def create_new_instances(self, instance, psd_object):
+        asset_doc = instance.data["assetEntity"]
         for layer in psd_object:
             if (
                 not layer.is_visible()
@@ -59,21 +62,37 @@ class ExtractImageForLayout(pype.api.Extractor):
                 continue
 
             layer_name = layer.name.replace(" ", "_")
-            instance_name = f"image_{layer_name}"
+            instance_name = subset_name = f"image{layer_name}"
+            self.log.info(
+                f"Creating new instance with name \"{instance_name}\""
+            )
             new_instance = instance.context.create_instance(instance_name)
             for key, value in instance.data.items():
-                if key not in ("name", "label", "stagingDir"):
+                if key not in self.ignored_instance_data_keys:
                     new_instance.data[key] = copy.deepcopy(value)
 
             new_instance.data["label"] = " ".join(
                 (new_instance.data["asset"], instance_name)
             )
 
-            new_instance.data["subset"] = instance_name
-            new_instance.data["anatomyData"]["subset"] = instance_name
+            # Find latest version
+            version_number = self.find_latest_version(subset_name, asset_doc)
+            self.log.info(
+                "Next version of instance \"{}\" will be {}".format(
+                    instance_name, version_number
+                )
+            )
 
-            # Set `family`
+            # Set family and subset
             new_instance.data["family"] = self.new_instance_family
+            new_instance.data["subset"] = subset_name
+            new_instance.data["version"] = version_number
+
+            new_instance.data["anatomyData"].update({
+                "subset": subset_name,
+                "family": self.new_instance_family,
+                "version": version_number
+            })
 
             # Copy `families` and check if `family` is not in current families
             families = new_instance.data.get("families") or list()
@@ -100,3 +119,24 @@ class ExtractImageForLayout(pype.api.Extractor):
             }
             new_instance.data["representations"] = [new_repre]
             self.log.debug(new_instance.data["name"])
+
+    def find_latest_version(self, subset_name, asset_doc):
+        subset_doc = io.find_one({
+            "type": "subset",
+            "name": subset_name,
+            "parent": asset_doc["_id"]
+        })
+
+        if subset_doc is None:
+            self.log.debug("Subset entity does not exist yet.")
+        else:
+            version_doc = io.find_one(
+                {
+                    "type": "version",
+                    "parent": subset_doc["_id"]
+                },
+                sort=[("name", -1)]
+            )
+            if version_doc:
+                return int(version_doc["name"]) + 1
+        return 1
