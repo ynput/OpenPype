91bc270495e5833386e6f4dd902cf1e7603ed6e0
Jakub Jezek <jakub@orbi.tools>
2020-08-12 10:54:03 +0100

    celaction fixes

--- pype/lib.py
+++ pype/lib.py
@@ -110,7 +110,9 @@ def _subprocess(*args, **kwargs):
             log.error(line)
 
     if proc.returncode != 0:
-        raise ValueError("\"{}\" was not successful: {}".format(args, output))
+        raise ValueError(
+            "\"{}\" was not successful:\nOutput: {}\nError: {}".format(
+                args, output, error))
     return output
 
 
