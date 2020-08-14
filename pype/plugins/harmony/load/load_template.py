880cbc44404dc0d1961fc0918f485f43e1c75131
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-14 02:50:19 -0400

    WIP Harmony Loader

--- pype/plugins/harmony/load/load_template.py
+++ pype/plugins/harmony/load/load_template.py
@@ -8,7 +8,49 @@ import uuid
 from avalon import api, harmony
 import pype.lib
 
-replace_node = """include("OpenHarmony.js");;
+load_template = """include("openHarmony.js");
+function load_template(args)
+{
+    var doc = $.scn;
+    var template_path = args[0];
+    var asset_name = args[1];
+    var subset = args[2];
+    var group_id = args[3];
+
+    node_view = "";
+    for (i = 0; i < 200; i++) {
+        node_view = "View" + i;
+        if (view.type(node_view) == "Node View") {
+            break;
+        }
+    }
+    MessageLog.trace(node_view);
+    MessageLog.trace("-------------------------------------");
+    MessageLog.trace(view.group(node_view));
+    const current_group = doc.$node(view.group(node_view));
+        
+    // Get a unique iterative name for the container group
+    var num = 0;
+    var container_group_name = "";
+    do {
+        container_group_name = asset_name + "_" + (num++) + "_" + subset;
+    } while (current_group.getNodeByName(container_group_name) != null);
+    
+    // import the template
+    var tpl_nodes = current_group.importTemplate(template_path);
+    MessageLog.trace(tpl_nodes)
+    // Create the container group and import the template
+    var group_node = current_group.addGroup(container_group_name, false, false, tpl_nodes);
+    
+    // Add uuid to attribute of the container group
+    node.createDynamicAttr(group_node, "STRING", "uuid", "uuid", false)
+    node.setTextAttr(group_node, "uuid", 1.0, group_id)
+    
+    return group_node;
+}
+load_template
+"""
+replace_node = """include("OpenHarmony.js");
 function replace_node(dst_node_path, src_node_path, rename_src, clone_src, link_columns) {
 
     var doc = $.scn
@@ -92,49 +134,7 @@ class LoadTemplateLoader(api.Loader):
         with zipfile.ZipFile(zip_file, "r") as zip_ref:
             zip_ref.extractall(template_path)
 
-        func = """function func(args)
-        {
-            include("openHarmony.js");
-            
-            var doc = $.scn;
-            var template_path = args[0];
-            var asset_name = args[1];
-            var subset = args[2];
-            var group_id = args[3];
-        
-            node_view = "";
-            for (i = 0; i < 200; i++) {
-                node_view = "View" + i;
-                if (view.type(node_view) == "Node View") {
-                    break;
-                }
-            }
-            MessageLog.trace(node_view);
-            MessageLog.trace("-------------------------------------");
-            MessageLog.trace(view.group(node_view));
-            const current_group = doc.$node(view.group(node_view));
-                
-            // Get a unique iterative name for the container group
-            var num = 0;
-            var container_group_name = "";
-            do {
-                container_group_name = asset_name + "_" + (num++) + "_" + subset;
-            } while (current_group.getNodeByName(container_group_name) != null);
-            
-            // import the template
-            var tpl_nodes = current_group.importTemplate(template_path);
-            MessageLog.trace(tpl_nodes)
-            // Create the container group and import the template
-            var group_node = current_group.addGroup(container_group_name, false, false, tpl_nodes);
-            
-            // Add uuid to attribute of the container group
-            node.createDynamicAttr(group_node, "STRING", "uuid", "uuid", false)
-            node.setTextAttr(group_node, "uuid", 1.0, group_id)
-            
-            return group_node;
-        }
-        func
-        """
+        func = load_template
 
         group_id = "{}".format(uuid.uuid4())
 
