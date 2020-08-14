0ff02afe84f1a8c34e5ae39404fb4195041d1b3a
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-14 19:01:29 -0400

    WIP

--- pype/plugins/harmony/load/load_template.py
+++ pype/plugins/harmony/load/load_template.py
@@ -92,7 +92,7 @@ class LoadTemplateLoader(api.Loader):
         with zipfile.ZipFile(zip_file, "r") as zip_ref:
             zip_ref.extractall(template_path)
 
-        func = """include("OpenHarmony.js");
+        func = """include(System.getenv("LIB_OPENHARMONY_PATH") + "/OpenHarmony.js");
         function func(args)
         {
             var doc = $.scn;
