22f08892d39f0a0af5425f334dbbab74402d889b
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-16 02:00:33 -0400

    WIP

--- pype/plugins/harmony/load/load_template.py
+++ pype/plugins/harmony/load/load_template.py
@@ -92,9 +92,7 @@ class LoadTemplateLoader(api.Loader):
         with zipfile.ZipFile(zip_file, "r") as zip_ref:
             zip_ref.extractall(template_path)
 
-        func = """
-        
-        function func(args)
+        func = """function func(args)
         {
             require("OpenHarmony.js");
             var doc = $.scn;
@@ -131,7 +129,7 @@ class LoadTemplateLoader(api.Loader):
             
             return String(group_node);
         }
-        
+        func
         """
 
         group_id = "{}".format(uuid.uuid4())
