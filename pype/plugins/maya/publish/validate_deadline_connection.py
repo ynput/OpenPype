8f9a184337854f2242501d7e247d5fe9ecb71aba
Jakub Jezek <jakub@orbi.tools>
2020-08-18 12:56:49 +0200

    refactory(maya): deadline validate rest url in new way

--- pype/plugins/maya/publish/validate_deadline_connection.py
+++ pype/plugins/maya/publish/validate_deadline_connection.py
@@ -1,6 +1,6 @@
 import pyblish.api
 
-from avalon.vendor import requests
+from pype.api import get_deadline_url
 from pype.plugin import contextplugin_should_run
 import os
 
@@ -16,34 +16,12 @@ class ValidateDeadlineConnection(pyblish.api.ContextPlugin):
         active = False
 
     def process(self, context):
-
         # Workaround bug pyblish-base#250
         if not contextplugin_should_run(self, context):
             return
 
-        try:
-            DEADLINE_REST_URL = os.environ["DEADLINE_REST_URL"]
-        except KeyError:
-            self.log.error("Deadline REST API url not found.")
-            raise ValueError("Deadline REST API url not found.")
+        url = context.data.get("deadlienRestUrl")
+        assert url, "Deadline Rest Url is missing"
 
-        # Check response
-        response = self._requests_get(DEADLINE_REST_URL)
-        assert response.ok, "Response must be ok"
-        assert response.text.startswith("Deadline Web Service "), (
-            "Web service did not respond with 'Deadline Web Service'"
-        )
-
-    def _requests_get(self, *args, **kwargs):
-        """ Wrapper for requests, disabling SSL certificate validation if
-            DONT_VERIFY_SSL environment variable is found. This is useful when
-            Deadline or Muster server are running with self-signed certificates
-            and their certificate is not added to trusted certificates on
-            client machines.
-
-            WARNING: disabling SSL certificate validation is defeating one line
-            of defense SSL is providing and it is not recommended.
-        """
-        if 'verify' not in kwargs:
-            kwargs['verify'] = False if os.getenv("PYPE_DONT_VERIFY_SSL", True) else True  # noqa
-        return requests.get(*args, **kwargs)
+        # test url. Asserts are part of the function
+        get_deadline_url(url)
