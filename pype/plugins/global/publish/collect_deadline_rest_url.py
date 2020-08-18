d6e6d157f361cf24efedca7562bff991ccacb498
Jakub Jezek <jakub@orbi.tools>
2020-08-18 15:13:35 +0200

    todo(global): adding todo annotation
    https://github.com/pypeclub/pype/issues/449

--- pype/plugins/global/publish/collect_deadline_rest_url.py
+++ pype/plugins/global/publish/collect_deadline_rest_url.py
@@ -15,6 +15,9 @@ class CollectDeadlineRestUrl(pyblish.api.ContextPlugin):
     label = "Collect Deadline Rest Url"
     order = pyblish.api.CollectorOrder
 
+    # QUESTION: what if we dont wont to have deadline in pipeline?
+    # TODO: https://github.com/pypeclub/pype/issues/449
+
     def process(self, context):
         dl_rest_url = get_deadline_url()
 
