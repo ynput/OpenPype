c471a5131fb1952c3f2a49053c8d62696fbbe7ca
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-19 18:17:51 -0400

    Added palette thumbnail

--- pype/plugins/harmony/publish/extract_palette.py
+++ pype/plugins/harmony/publish/extract_palette.py
@@ -1,7 +1,9 @@
+import csv
 import os
+from PIL import Image, ImageDraw, ImageFont
 
 from avalon import harmony
-import pype.api
+
 import pype.hosts.harmony
 
 
@@ -17,13 +19,16 @@ class ExtractPalette(pype.api.Extractor):
         {
             var palette_list = PaletteObjectManager.getScenePaletteList();
             var palette = palette_list.getPaletteById(args[0]);
-            return (palette.getPath() + "/" + palette.getName() + ".plt");
+            return [palette, (palette.getPath() + "/" + palette.getName() + ".plt")];
         }
         func
         """
-        palette_file = harmony.send(
+
+        result = harmony.send(
             {"function": func, "args": [instance.data["id"]]}
         )["result"]
+        palette_name = result[0]
+        palette_file = result[1]
 
         representation = {
             "name": "plt",
@@ -31,4 +36,91 @@ class ExtractPalette(pype.api.Extractor):
             "files": os.path.basename(palette_file),
             "stagingDir": os.path.dirname(palette_file)
         }
-        instance.data["representations"] = [representation]
+
+        tmp_thumb_path = ""
+        palette_version = "001"
+
+        thumbnail_path = self.create_palette_thumbnail(palette_name,
+                                                       palette_file,
+                                                       palette_version,
+                                                       tmp_thumb_path)
+        thumbnail = {
+            "name": "thumbnail",
+            "ext": "png",
+            "files": os.path.basename(thumbnail_path),
+            "stagingDir": os.dirname(thumbnail_path),
+            "tags": ["thumbnail"]
+        }
+
+        instance.data["representations"] = [representation, thumbnail]
+
+    def create_palette_thumbnail(self,
+                                 palette_name,
+                                 palette_version,
+                                 palette_path,
+                                 dst_path):
+        colors = {}
+
+        with open(palette_path, newline='') as plt:
+            plt_parser = csv.reader(plt, delimiter=" ")
+            for i, line in enumerate(plt_parser):
+                if i == 0: continue
+                while ("" in line): line.remove("")
+                print(line)
+                color_name = line[1].strip('"')
+                colors[color_name] = {"type": line[0],
+                                      "uuid": line[2],
+                                      "rgb": (int(line[3]),
+                                              int(line[4]),
+                                              int(line[5]),
+                                              int(line[6])),
+                                      }
+            plt.close()
+
+        img_pad_top = 80
+        label_pad_name = 30
+        label_pad_rgb = 580
+        swatch_pad_left = 300
+        swatch_pad_top = 10
+        swatch_x = 120
+        swatch_y = 50
+        image_x = 800
+        image_y = (img_pad_top +
+                   (len(colors.keys()) *
+                    swatch_y) +
+                   (swatch_pad_top *
+                    len(colors.keys()))
+                   )
+
+        img = Image.new("RGBA", (image_x, image_y), "white")
+
+        draw = ImageDraw.Draw(img)
+        title_font = ImageFont.truetype("arial.ttf", 28)
+        label_font = ImageFont.truetype("arial.ttf", 20)
+
+        draw.text((label_pad_name, 20),
+                  "{} (v{})".format(palette_name, palette_version),
+                  "black",
+                  font=title_font)
+
+        for i, name in enumerate(colors):
+            draw.rectangle((
+                swatch_pad_left,  # upper left x
+                img_pad_top + swatch_pad_top + (i * swatch_y),  # upper left y
+                swatch_pad_left + (swatch_x * 2),  # lower right x
+                img_pad_top + swatch_y + (i * swatch_y)),  # lower right y
+                fill=colors[name]["rgb"], outline=(0, 0, 0), width=2)
+
+            draw.text((label_pad_name, img_pad_top + (i * swatch_y) + swatch_pad_top + (swatch_y / 4)),
+                      name,
+                      "black",
+                      font=label_font)
+            draw.text((label_pad_rgb, img_pad_top + (i * swatch_y) + swatch_pad_top + (swatch_y / 4)),
+                      str(colors[name]["rgb"]),
+                      "black",
+                      font=label_font)
+
+        draw = ImageDraw.Draw(img)
+
+        img.save(dst_path)
+        return dst_path
