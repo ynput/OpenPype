580e84b9270094ce90b6f36e6e74b0468459be29
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-18 22:40:38 -0400

    WIP

--- pype/plugins/harmony/publish/extract_template.py
+++ pype/plugins/harmony/publish/extract_template.py
@@ -11,7 +11,7 @@ class ExtractTemplate(pype.api.Extractor):
 
     label = "Extract Template"
     hosts = ["harmony"]
-    families = ["scene", "ftrack"]
+    families = ["scene"]
 
     def process(self, instance):
         staging_dir = self.staging_dir(instance)
