ec463466bec006ff3d4120835ba79ce8b8197049
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-20 22:22:13 -0400

    WIP

--- pype/tools/launcher/models.py
+++ pype/tools/launcher/models.py
@@ -276,7 +276,7 @@ class ProjectModel(QtGui.QStandardItemModel):
         self.dbcon = dbcon
 
         self.hide_invisible = False
-        self.project_icon = qtawesome.icon("fas.hamsa", color="white")
+        self.project_icon = qtawesome.icon("fa.sun", color="white")
 
     def refresh(self):
         self.clear()
