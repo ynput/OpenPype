04ea72f0a6daff2ebb471abeac412d227532f200
Jakub Jezek <jakub@orbi.tools>
2020-08-21 16:29:39 +0200

    Revert "Merge branch 'draft/pyblish_pype_keep_gui_responsive' into feature/451-Fusion_basic_integration"
    This reverts commit bcb7201ffb7a1d71daa47226b484fa9a71f4c28b.

--- pype/tools/pyblish_pype/control.py
+++ pype/tools/pyblish_pype/control.py
@@ -9,10 +9,8 @@ import os
 import sys
 import traceback
 import inspect
-import threading
 
-import six
-from Qt import QtCore, QtWidgets
+from Qt import QtCore
 
 import pyblish.api
 import pyblish.util
@@ -28,27 +26,6 @@ from pype.api import config
 
 class IterationBreak(Exception):
     pass
-
-
-class ProcessThread(threading.Thread):
-    def __init__(self, plugin, context, instance):
-        super(ProcessThread, self).__init__()
-
-        self.result = None
-        self.exception = None
-
-        self.plugin = plugin
-        self.context = context
-        self.instance = instance
-
-    def run(self):
-        try:
-            result = pyblish.plugin.process(
-                self.plugin, self.context, self.instance
-            )
-            self.result = result
-        except Exception:
-            self.exception = sys.exc_info()
 
 
 class Controller(QtCore.QObject):
@@ -254,16 +231,7 @@ class Controller(QtCore.QObject):
         self.processing["nextOrder"] = plugin.order
 
         try:
-            thread = ProcessThread(plugin, self.context, instance)
-            thread.start()
-            while thread.isAlive():
-                QtWidgets.QApplication.processEvents()
-
-            thread.join()
-            if thread.exception:
-                six.reraise(*thread.exception)
-
-            result = thread.result
+            result = pyblish.plugin.process(plugin, self.context, instance)
             # Make note of the order at which the
             # potential error error occured.
             if result["error"] is not None:
