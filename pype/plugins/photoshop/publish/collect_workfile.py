cad499682c5dc10a5d5b3e5141445a756e80f0bf
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-21 16:29:34 -0400

    WIP

--- pype/plugins/photoshop/publish/collect_workfile.py
+++ pype/plugins/photoshop/publish/collect_workfile.py
@@ -1,6 +1,8 @@
-import pyblish.api
 import os
-
+import pyblish.api
+import pype.api
+import pype.lib
+from avalon import photoshop
 
 class CollectWorkfile(pyblish.api.ContextPlugin):
     """Collect current script for publish."""
@@ -31,42 +33,111 @@ class CollectWorkfile(pyblish.api.ContextPlugin):
         })
 
         # creating representation
-        psd = {
+        instance.data["representations"].append({
             "name": "psd",
             "ext": "psd",
             "files": base_name,
             "stagingDir": staging_dir,
-        }
-
-        representations = [psd]
-
-        image_context_instances = instance.context.data.get(
-            "image_instances") or []
-        for image_context_instance in image_context_instances:
-            if image_context_instance.data.get("representations"):
-                image_context_instance.data["representations"].extend(
-                    representations)
-            else:
-                image_context_instance.data["representations"] = \
-                    representations
-            # Required for extract_review plugin (L222 onwards).
-            image_context_instance.data["frameStart"] = 1
-            image_context_instance.data["frameEnd"] = 1
-            image_context_instance.data["fps"] = 24
-
-            self.log.info(f"Extracted {instance} to {staging_dir}")
+        })
 
         instance.data["version_name"] = "{}_{}". \
             format(instance.data["subset"],
                    os.environ["AVALON_TASK"])
 
-        if image_context_instances:
-            instance.data["families"].append("paired_media")
-        else:
-            if instance.data.get("representations"):
-                instance.data["representations"].extend(representations)
-            else:
-                instance.data["representations"] = representations
+        self.log.info(f"Extracted {instance} to {staging_dir}")
+        self.create_review()
 
 
+    def create_review(self, instance):
+
+        staging_dir = self.staging_dir(instance)
+        self.log.info("Outputting image to {}".format(staging_dir))
+
+        layers = []
+        for image_instance in instance.context:
+            if image_instance.data["family"] != "image":
+                continue
+            layers.append(image_instance[0])
+
+        # Perform extraction
+        output_image = "{}.jpg".format(
+            os.path.splitext(photoshop.app().ActiveDocument.Name)[0]
+        )
+        output_image_path = os.path.join(staging_dir, output_image)
+        with photoshop.maintained_visibility():
+            # Hide all other layers.
+            extract_ids = [
+                x.id for x in photoshop.get_layers_in_layers(layers)
+            ]
+            for layer in photoshop.get_layers_in_document():
+                if layer.id in extract_ids:
+                    layer.Visible = True
+                else:
+                    layer.Visible = False
+
+            photoshop.app().ActiveDocument.SaveAs(
+                output_image_path,
+                photoshop.com_objects.JPEGSaveOptions(),
+                True
+            )
+
+        ffmpeg_path = pype.lib.get_ffmpeg_tool_path("ffmpeg")
+
+        instance.data["representations"].append({
+            "name": "jpg",
+            "ext": "jpg",
+            "files": output_image,
+            "stagingDir": staging_dir
+        })
+        instance.data["stagingDir"] = staging_dir
+
+        # Generate thumbnail.
+        thumbnail_path = os.path.join(staging_dir, "thumbnail.jpg")
+        args = [
+            ffmpeg_path, "-y",
+            "-i", output_image_path,
+            "-vf", "scale=300:-1",
+            "-vframes", "1",
+            thumbnail_path
+        ]
+        output = pype.lib._subprocess(args)
+
+        self.log.debug(output)
+
+        instance.data["representations"].append({
+            "name": "thumbnail",
+            "ext": "jpg",
+            "files": os.path.basename(thumbnail_path),
+            "stagingDir": staging_dir,
+            "tags": ["thumbnail"]
+        })
+
+        # Generate mov.
+        mov_path = os.path.join(staging_dir, "review.mov")
+        args = [
+            ffmpeg_path, "-y",
+            "-i", output_image_path,
+            "-vf", "pad=ceil(iw/2)*2:ceil(ih/2)*2",
+            "-vframes", "1",
+            mov_path
+        ]
+        output = pype.lib._subprocess(args)
+
+        self.log.debug(output)
+
+        instance.data["representations"].append({
+            "name": "mov",
+            "ext": "mov",
+            "files": os.path.basename(mov_path),
+            "stagingDir": staging_dir,
+            "frameStart": 1,
+            "frameEnd": 1,
+            "fps": 24,
+            "preview": True,
+            "tags": ["review", "ftrackreview"]
+        })
+
+        instance.data["frameStart"] = 1
+        instance.data["frameEnd"] = 1
+        instance.data["fps"] = 24
 
--- pype/plugins/photoshop/publish/extract_image.py
+++ pype/plugins/photoshop/publish/extract_image.py
@@ -1,7 +1,9 @@
 import os
 
-import pype.api
 from avalon import photoshop
+
+import pype.api
+import pype.lib
 
 
 class ExtractImage(pype.api.Extractor):
@@ -13,7 +15,7 @@ class ExtractImage(pype.api.Extractor):
     label = "Extract Image"
     hosts = ["photoshop"]
     families = ["image"]
-    formats = ["png", "jpg"]
+    formats = ["png"]
 
     def process(self, instance):
 
@@ -63,3 +65,98 @@ class ExtractImage(pype.api.Extractor):
         instance.data["stagingDir"] = staging_dir
 
         self.log.info(f"Extracted {instance} to {staging_dir}")
+
+        self.create_review()
+
+    def create_review(self, instance):
+
+        staging_dir = self.staging_dir(instance)
+        self.log.info("Outputting image to {}".format(staging_dir))
+
+        layers = []
+        for image_instance in instance.context:
+            if image_instance.data["family"] != "image":
+                continue
+            layers.append(image_instance[0])
+
+        # Perform extraction
+        output_image = "{}.jpg".format(
+            os.path.splitext(photoshop.app().ActiveDocument.Name)[0]
+        )
+        output_image_path = os.path.join(staging_dir, output_image)
+        with photoshop.maintained_visibility():
+            # Hide all other layers.
+            extract_ids = [
+                x.id for x in photoshop.get_layers_in_layers(layers)
+            ]
+            for layer in photoshop.get_layers_in_document():
+                if layer.id in extract_ids:
+                    layer.Visible = True
+                else:
+                    layer.Visible = False
+
+            photoshop.app().ActiveDocument.SaveAs(
+                output_image_path,
+                photoshop.com_objects.JPEGSaveOptions(),
+                True
+            )
+
+        ffmpeg_path = pype.lib.get_ffmpeg_tool_path("ffmpeg")
+
+        instance.data["representations"].append({
+            "name": "jpg",
+            "ext": "jpg",
+            "files": output_image,
+            "stagingDir": staging_dir
+        })
+        instance.data["stagingDir"] = staging_dir
+
+        # Generate thumbnail.
+        thumbnail_path = os.path.join(staging_dir, "thumbnail.jpg")
+        args = [
+            ffmpeg_path, "-y",
+            "-i", output_image_path,
+            "-vf", "scale=300:-1",
+            "-vframes", "1",
+            thumbnail_path
+        ]
+        output = pype.lib._subprocess(args)
+
+        self.log.debug(output)
+
+        instance.data["representations"].append({
+            "name": "thumbnail",
+            "ext": "jpg",
+            "files": os.path.basename(thumbnail_path),
+            "stagingDir": staging_dir,
+            "tags": ["thumbnail"]
+        })
+
+        # Generate mov.
+        mov_path = os.path.join(staging_dir, "review.mov")
+        args = [
+            ffmpeg_path, "-y",
+            "-i", output_image_path,
+            "-vf", "pad=ceil(iw/2)*2:ceil(ih/2)*2",
+            "-vframes", "1",
+            mov_path
+        ]
+        output = pype.lib._subprocess(args)
+
+        self.log.debug(output)
+
+        instance.data["representations"].append({
+            "name": "mov",
+            "ext": "mov",
+            "files": os.path.basename(mov_path),
+            "stagingDir": staging_dir,
+            "frameStart": 1,
+            "frameEnd": 1,
+            "fps": 24,
+            "preview": True,
+            "tags": ["review", "ftrackreview"]
+        })
+
+        instance.data["frameStart"] = 1
+        instance.data["frameEnd"] = 1
+        instance.data["fps"] = 24
