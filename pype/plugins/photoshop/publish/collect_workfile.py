855f097a7fc001e09203325770ebc1eee9359c50
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-21 16:10:03 -0400

    restore default ps pyblish

--- pype/plugins/photoshop/publish/collect_workfile.py
+++ pype/plugins/photoshop/publish/collect_workfile.py
@@ -31,9 +31,42 @@ class CollectWorkfile(pyblish.api.ContextPlugin):
         })
 
         # creating representation
-        instance.data["representations"].append({
+        psd = {
             "name": "psd",
             "ext": "psd",
             "files": base_name,
             "stagingDir": staging_dir,
-        })
+        }
+
+        representations = [psd]
+
+        image_context_instances = instance.context.data.get(
+            "image_instances") or []
+        for image_context_instance in image_context_instances:
+            if image_context_instance.data.get("representations"):
+                image_context_instance.data["representations"].extend(
+                    representations)
+            else:
+                image_context_instance.data["representations"] = \
+                    representations
+            # Required for extract_review plugin (L222 onwards).
+            image_context_instance.data["frameStart"] = 1
+            image_context_instance.data["frameEnd"] = 1
+            image_context_instance.data["fps"] = 24
+
+            self.log.info(f"Extracted {instance} to {staging_dir}")
+
+        instance.data["version_name"] = "{}_{}". \
+            format(instance.data["subset"],
+                   os.environ["AVALON_TASK"])
+
+        if image_context_instances:
+            instance.data["families"].append("paired_media")
+        else:
+            if instance.data.get("representations"):
+                instance.data["representations"].extend(representations)
+            else:
+                instance.data["representations"] = representations
+
+
+
