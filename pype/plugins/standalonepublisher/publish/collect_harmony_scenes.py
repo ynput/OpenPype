9c20da569f04272b95a3523af72e166e95e61fa2
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-22 20:03:45 -0400

    WIP

--- pype/plugins/standalonepublisher/publish/collect_harmony_scenes.py
+++ pype/plugins/standalonepublisher/publish/collect_harmony_scenes.py
@@ -1,9 +1,7 @@
 import copy
-import os
-import shutil
+from pprint import pformat
 
 import pyblish.api
-from avalon import io
 
 
 class CollectHarmonyScenes(pyblish.api.InstancePlugin):
@@ -23,104 +21,49 @@ class CollectHarmonyScenes(pyblish.api.InstancePlugin):
         }
     }
 
-    new_instance_family = "scene"
-    ignored_instance_data_keys = ("name", "label", "stagingDir", "version")
+    unchecked_by_default = []
 
     def process(self, instance):
         context = instance.context
-        asset_doc = instance.context.data["assetEntity"]
+        asset_data = instance.data["assetEntity"]
         asset_name = instance.data["asset"]
+        anatomy_data = instance.data["anatomyData"]
 
         for subset_name, subset_data in self.subsets.items():
             instance_name = f"{asset_name}_{subset_name}"
-            task = subset_data.get("task", "background")
+            task = subset_data.get("task", "ingest")
 
-            instance_name = subset_name = "sceneMain"
-
+            # create new instance
             new_instance = context.create_instance(instance_name)
+
+            # add original instance data except name key
             for key, value in instance.data.items():
-                if key not in self.ignored_instance_data_keys:
+                if key not in ["name"]:
+                    # Make sure value is copy since value may be object which
+                    # can be shared across all new created objects
                     new_instance.data[key] = copy.deepcopy(value)
 
-            new_instance.data["label"] = " ".join(
-                (new_instance.data["asset"], instance_name)
-            )
+            # add subset data from preset
+            new_instance.data.update(subset_data)
 
-            # Find latest version
-            latest_version = self.find_last_version(subset_name, asset_doc)
-            version_number = 1
-            if latest_version is not None:
-                version_number += latest_version
-
-            self.log.info(
-                "Next version of instance \"{}\" will be {}".format(
-                    instance_name, version_number
-                )
-            )
-
-            # Set family and subset
-            new_instance.data["family"] = self.new_instance_family
+            new_instance.data["label"] = f"{instance_name}"
             new_instance.data["subset"] = subset_name
-            new_instance.data["version"] = version_number
-            new_instance.data["latestVersion"] = latest_version
 
-            new_instance.data["anatomyData"].update({
-                "subset": subset_name,
-                "family": self.new_instance_family,
-                "version": version_number
+            # fix anatomy data
+            anatomy_data_new = copy.deepcopy(anatomy_data)
+            # updating hierarchy data
+            anatomy_data_new.update({
+                "asset": asset_data["name"],
+                "task": task,
+                "subset": subset_name
             })
+            new_instance.data["anatomyData"] = anatomy_data_new
 
-            # Copy `families` and check if `family` is not in current families
-            families = new_instance.data.get("families") or list()
-            if families:
-                families = list(set(families))
+            if subset_name in self.unchecked_by_default:
+                new_instance.data["publish"] = False
 
-            if self.new_instance_family in families:
-                families.remove(self.new_instance_family)
-            new_instance.data["families"] = families
-
-            # Prepare staging dir for new instance
-            staging_dir = self.staging_dir(new_instance)
-
-            repres = instance.data.get("representations")
-            source = os.path.join(repres[0]["stagingDir"], repres[0]["files"])
-
-            os.chdir(os.path.dirname(source))
-            zip_file = shutil.make_archive(
-                os.path.basename(source), "zip", staging_dir)
-            output_filename = os.path.basename(zip_file)
-
-            new_repre = {
-                "name": "zip",
-                "ext": "zip",
-                "files": output_filename,
-                "stagingDir": staging_dir
-            }
-            self.log.debug(
-                "Creating new representation: {}".format(new_repre)
-            )
-            new_instance.data["representations"] = [new_repre]
+            self.log.info(f"Created new instance: {instance_name}")
+            self.log.debug(f"_ inst_data: {pformat(new_instance.data)}")
 
         # delete original instance
         context.remove(instance)
-
-    def find_last_version(self, subset_name, asset_doc):
-        subset_doc = io.find_one({
-            "type": "subset",
-            "name": subset_name,
-            "parent": asset_doc["_id"]
-        })
-
-        if subset_doc is None:
-            self.log.debug("Subset entity does not exist yet.")
-        else:
-            version_doc = io.find_one(
-                {
-                    "type": "version",
-                    "parent": subset_doc["_id"]
-                },
-                sort=[("name", -1)]
-            )
-            if version_doc:
-                return int(version_doc["name"])
-        return None
---
+++ pype/plugins/standalonepublisher/publish/extract_harmony_zip_from_xstage.py
@@ -0,0 +1,104 @@
+import os
+import pype.api
+import copy
+import os
+import tempfile
+import shutil
+
+import pyblish.api
+from avalon import io
+from pprint import pformat
+
+
+class ExtractHarmonyZipFromXstage(pype.api.Extractor):
+    """Extract Harmony zip"""
+
+    label = "Extract Shot Data"
+    hosts = ["standalonepublisher"]
+    families = ["review", "audio"]
+
+    # presets
+
+    def process(self, instance):
+        context = instance.context
+        asset_doc = instance.context.data["assetEntity"]
+        asset_name = instance.data["asset"]
+        subset_name = instance.data["subset"]
+        instance_name = instance.data["name"]
+        family = instance.data["family"]
+        # Find latest version
+        latest_version = self.find_last_version(subset_name, asset_doc)
+        version_number = 1
+        if latest_version is not None:
+            version_number += latest_version
+        
+        self.log.info(
+            "Next version of instance \"{}\" will be {}".format(
+                instance_name, version_number
+            )
+        )
+
+        # Set family and subset
+        instance.data["family"] = family
+        instance.data["subset"] = subset_name
+        instance.data["version"] = version_number
+        instance.data["latestVersion"] = latest_version
+
+        instance.data["anatomyData"].update({
+            "subset": subset_name,
+            "family": family ,
+            "version": version_number
+        })
+
+        # Copy `families` and check if `family` is not in current families
+        families = instance.data.get("families") or list()
+        if families:
+            families = list(set(families))
+
+        if self.instance_family in families:
+            families.remove(self.instance_family)
+        instance.data["families"] = families
+
+        # Prepare staging dir for new instance
+        staging_dir = self.staging_dir(instance)
+
+        repres = instance.data.get("representations")
+        source = os.path.join(repres[0]["stagingDir"], repres[0]["files"])
+
+        os.chdir(os.path.dirname(source))
+        zip_file = shutil.make_archive(
+            os.path.basename(source), "zip", staging_dir)
+        output_filename = os.path.basename(zip_file)
+
+        new_repre = {
+            "name": "zip",
+            "ext": "zip",
+            "files": output_filename,
+            "stagingDir": staging_dir
+        }
+        self.log.debug(
+            "Creating new representation: {}".format(new_repre)
+        )
+        instance.data["representations"] = [new_repre]
+ 
+
+    def find_last_version(self, subset_name, asset_doc):
+        subset_doc = io.find_one({
+            "type": "subset",
+            "name": subset_name,
+            "parent": asset_doc["_id"]
+        })
+
+        if subset_doc is None:
+            self.log.debug("Subset entity does not exist yet.")
+        else:
+            version_doc = io.find_one(
+                {
+                    "type": "version",
+                    "parent": subset_doc["_id"]
+                },
+                sort=[("name", -1)]
+            )
+            if version_doc:
+                return int(version_doc["name"])
+        return None
