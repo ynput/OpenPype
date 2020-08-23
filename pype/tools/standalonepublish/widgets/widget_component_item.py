ded86e87a84443b98d5c8ba1d0cfc9c37300b525
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-23 15:35:45 -0400

    WIP

--- pype/tools/standalonepublish/widgets/widget_component_item.py
+++ pype/tools/standalonepublish/widgets/widget_component_item.py
@@ -1,21 +1,8 @@
 import os
 from Qt import QtCore, QtGui, QtWidgets
-
+from pype.resources import get_resource
 from avalon import style
 
-def get_resource(*args):
-    """ Serves to simple resources access
-
-    :param *args: should contain *subfolder* names and *filename* of
-                  resource from resources folder
-    :type *args: list
-    """
-    return os.path.normpath(
-        os.path.join(
-            os.path.dirname(__file__),
-            *args
-        )
-    )
 
 class ComponentItem(QtWidgets.QFrame):
 
