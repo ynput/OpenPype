c03c0f69bcb25c5229ed6824e6a045c4c744bcb0
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-22 23:11:10 -0400

    WIP

--- pype/plugins/standalonepublisher/publish/collect_harmony_scenes.py
+++ pype/plugins/standalonepublisher/publish/collect_harmony_scenes.py
@@ -7,7 +7,7 @@ import pyblish.api
 class CollectHarmonyScenes(pyblish.api.InstancePlugin):
     """Collect Harmony xstage files"""
 
-    order = pyblish.api.CollectorOrder + 0.01
+    order = pyblish.api.CollectorOrder + 0.06
     label = "Collect Harmony Scene"
     hosts = ["standalonepublisher"]
     families = ["scene"]
