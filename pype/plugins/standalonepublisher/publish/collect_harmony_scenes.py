2360e2dc2f548512970bbc0e6d9d9e7fc3e6e2d5
Aleks Berland <aleksb@yowzaanimation.com>
2020-08-24 00:17:27 -0400

    WIP

--- pype/plugins/standalonepublisher/publish/collect_harmony_scenes.py
+++ pype/plugins/standalonepublisher/publish/collect_harmony_scenes.py
@@ -1,6 +1,9 @@
 import copy
+import glob
+import os
 import six
 import sys
+
 from pprint import pformat
 
 import pyblish.api
@@ -21,7 +24,8 @@ class CollectHarmonyScenes(pyblish.api.InstancePlugin):
             "family": "scene",
             "families": ["ftrack"],
             "extension": ".xstage"
-        }
+        },
+
     }
 
     ignored_instance_data_keys = ("name", "label", "stagingDir", "version")
@@ -31,7 +35,10 @@ class CollectHarmonyScenes(pyblish.api.InstancePlugin):
         asset_data = instance.context.data["assetEntity"]
         asset_name = instance.data["asset"]
         anatomy_data = instance.context.data["anatomyData"]
+        staging_dir = instance.data["stagingDir"]
+        files = instance.data["files"]
 
+        # A folder was dropped
         for subset_name, subset_data in self.subsets.items():
             instance_name = f"{asset_name}_{subset_name}"
             task = instance.data.get("task", "harmonyIngest")
@@ -64,6 +71,18 @@ class CollectHarmonyScenes(pyblish.api.InstancePlugin):
 
             new_instance.data["anatomyData"] = anatomy_data_new
             new_instance.data["publish"] = True
+
+            # When a project folder was dropped vs. just an xstage file, find
+            # the latest file version and update the instance
+            if not files.endswith(".xstage"):
+
+                latest_file = max(glob.iglob(staging_dir + "/*.xstage"),
+                                   key=os.path.getctime).replace("\\", "/")
+
+                new_instance.data["stagingDir"] = os.path.join(staging_dir,
+                                                               files)
+                new_instance.data["files"] = os.path.basename(latest_file)
+
             self.log.info(f"Created new instance: {instance_name}")
             self.log.debug(f"_ inst_data: {pformat(new_instance.data)}")
 
