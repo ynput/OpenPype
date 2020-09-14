2f2eb3689bf726cf5ecc4613ad6eafe0e1309d9c
iLLiCiTiT <jakub.trllo@gmail.com>
2020-09-14 18:13:43 +0200

    extracted config.py to __init__.py

---
+++ pype/configurations/__init__.py
@@ -0,0 +1,9 @@
+from .lib import (
+    system_configurations,
+    project_configurations
+)
+
+__all__ = (
+    "system_configurations",
+    "project_configurations"
+)
--- pype/configurations/lib.py
+++ pype/configurations/lib.py
@@ -239,3 +239,20 @@ def apply_overrides(global_presets, project_overrides):
     if not project_overrides:
         return global_presets
     return merge_overrides(global_presets, project_overrides)
+
+
+def system_configurations():
+    default_values = default_configuration()["system_configurations"]
+    studio_values = studio_system_configurations()
+    return apply_overrides(default_values, studio_values)
+
+
+def project_configurations(project_name):
+    default_values = default_configuration()["project_configurations"]
+    studio_values = studio_project_configurations()
+
+    studio_overrides = apply_overrides(default_values, studio_values)
+
+    project_overrides = project_configurations_overrides(project_name)
+
+    return apply_overrides(studio_overrides, project_overrides)
