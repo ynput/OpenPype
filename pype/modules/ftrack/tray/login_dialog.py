bb77c72b98d204b39d7d4bd953df289eeab5b18e
iLLiCiTiT <jakub.trllo@gmail.com>
2020-09-30 11:30:21 +0200

    fix thread stopping in ftrack login

--- pype/modules/ftrack/tray/login_dialog.py
+++ pype/modules/ftrack/tray/login_dialog.py
@@ -238,6 +238,8 @@ class CredentialsDialog(QtWidgets.QDialog):
 
         # If there is an existing server thread running we need to stop it.
         if self._login_server_thread:
+            if self._login_server_thread.isAlive():
+                self._login_server_thread.stop()
             self._login_server_thread.join()
             self._login_server_thread = None
 
