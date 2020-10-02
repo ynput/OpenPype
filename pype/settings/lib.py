cdacc265721d43ed098dabc19f0b3895c06068cf
iLLiCiTiT <jakub.trllo@gmail.com>
2020-10-03 01:19:44 +0200

    STUDIO_OVERRIDES_PATH is safer

--- pype/settings/lib.py
+++ pype/settings/lib.py
@@ -13,7 +13,7 @@ M_ENVIRONMENT_KEY = "__environment_keys__"
 M_POP_KEY = "__pop_key__"
 
 # Folder where studio overrides are stored
-STUDIO_OVERRIDES_PATH = os.environ["PYPE_PROJECT_CONFIGS"]
+STUDIO_OVERRIDES_PATH = os.getenv("PYPE_PROJECT_CONFIGS")
 
 # File where studio's system overrides are stored
 SYSTEM_SETTINGS_KEY = "system_settings"
@@ -88,7 +88,6 @@ def load_json_file(fpath):
             "File has invalid json format \"{}\"".format(fpath),
             exc_info=True
         )
-
     return {}
 
 
