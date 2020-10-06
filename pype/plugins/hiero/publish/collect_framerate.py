0eaf09101ec02eb7d5b906c0f849c62c43d0208c
Jakub Jezek <jakub@orbi.tools>
2020-10-06 17:38:19 +0200

    feat(hiero): renaming host name references

---
+++ pype/plugins/hiero/publish/collect_framerate.py
@@ -0,0 +1,22 @@
+from pyblish import api
+
+
+class CollectFramerate(api.ContextPlugin):
+    """Collect framerate from selected sequence."""
+
+    order = api.CollectorOrder + 0.01
+    label = "Collect Framerate"
+    hosts = ["hiero"]
+
+    def process(self, context):
+        sequence = context.data["activeSequence"]
+        context.data["fps"] = self.get_rate(sequence)
+
+    def get_rate(self, sequence):
+        num, den = sequence.framerate().toRational()
+        rate = float(num) / float(den)
+
+        if rate.is_integer():
+            return rate
+
+        return round(rate, 3)
