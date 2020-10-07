3fb4ee1d6f87eab6b0d90eaa378edfe37e402ce3
Simone Barbieri <simonebarbieri90@gmail.com>
2020-10-07 13:00:34 +0100

    Fixed problem with rig loader
    When reopening the Unreal project, the skeleton was not connected to the skeletal mesh anymore. Some materials were not assigned to the mesh either.

--- pype/plugins/unreal/load/load_rig.py
+++ pype/plugins/unreal/load/load_rig.py
@@ -1,5 +1,6 @@
-from avalon import api
-from avalon import unreal as avalon_unreal
+from avalon import api, pipeline
+from avalon.unreal import lib
+from avalon.unreal import pipeline as unreal_pipeline
 import unreal
 
 
@@ -35,53 +36,84 @@ class SkeletalMeshFBXLoader(api.Loader):
             list(str): list of container content
         """
 
-        tools = unreal.AssetToolsHelpers().get_asset_tools()
-        temp_dir, temp_name = tools.create_unique_asset_name(
-            "/Game/{}".format(name), "_TMP"
-        )
-
-        unreal.EditorAssetLibrary.make_directory(temp_dir)
-
+        # Create directory for asset and avalon container
+        root = "/Game"
         asset = context.get('asset')
         asset_name = asset.get('name')
+        if asset_name:
+            container_name = "{}_{}".format(asset_name, name)
+        else:
+            container_name = "{}".format(name)
 
-        destination_name = "{}_{}".format(asset_name, name)
+        tools = unreal.AssetToolsHelpers().get_asset_tools()
+        asset_dir, avalon_asset_name = tools.create_unique_asset_name(
+            "{}/{}".format(root, container_name), "_CON"
+        )
+
+        unreal.EditorAssetLibrary.make_directory(asset_dir)
 
         task = unreal.AssetImportTask()
 
         task.set_editor_property('filename', self.fname)
-        task.set_editor_property('destination_path', temp_dir)
-        task.set_editor_property('destination_name', destination_name)
+        task.set_editor_property('destination_path', asset_dir)
+        task.set_editor_property('destination_name', container_name)
         task.set_editor_property('replace_existing', False)
         task.set_editor_property('automated', True)
-        task.set_editor_property('save', True)
+        task.set_editor_property('save', False)
 
         # set import options here
-        task.options = unreal.FbxImportUI()
-        task.options.set_editor_property('create_physics_asset', True)
-        task.options.set_editor_property('import_as_skeletal', True)
-        task.options.set_editor_property('import_animations', False)
+        options = unreal.FbxImportUI()
+        options.set_editor_property('import_as_skeletal', True)
+        options.set_editor_property('import_animations', False)
+        options.set_editor_property('import_mesh', True)
+        options.set_editor_property('import_materials', True)
+        options.set_editor_property('import_textures', True)
+        options.set_editor_property('skeleton', None)
+        options.set_editor_property('create_physics_asset', False)
 
+        options.set_editor_property('mesh_type_to_import',
+            unreal.FBXImportType.FBXIT_SKELETAL_MESH)
+
+        options.skeletal_mesh_import_data.set_editor_property(
+            'import_content_type', 
+            unreal.FBXImportContentType.FBXICT_ALL
+        )
         # set to import normals, otherwise Unreal will compute them
         # and it will take a long time, depending on the size of the mesh
-        task.options.skeletal_mesh_import_data.set_editor_property(
+        options.skeletal_mesh_import_data.set_editor_property(
             'normal_import_method', 
             unreal.FBXNormalImportMethod.FBXNIM_IMPORT_NORMALS
         )
 
+        task.options = options
         unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([task])  # noqa: E501
 
-        imported_assets = unreal.EditorAssetLibrary.list_assets(
-            temp_dir, recursive=True, include_folder=True
-        )
-        new_dir = avalon_unreal.containerise(
-            name, namespace, imported_assets, context, self.__class__.__name__)
+        # Create Asset Container
+        lib.create_avalon_container(
+            container=avalon_asset_name, path=asset_dir)
+
+        namespace = asset_dir
+
+        data = {
+            "schema": "avalon-core:container-2.0",
+            "id": pipeline.AVALON_CONTAINER_ID,
+            "name": avalon_asset_name,
+            "namespace": namespace,
+            "asset_name": asset_name,
+            "loader": str(self.__class__.__name__),
+            "representation": context["representation"]["_id"],
+            "parent": context["representation"]["parent"],
+            "family": context["representation"]["context"]["family"]
+        }
+        unreal_pipeline.imprint(
+            "{}/{}".format(asset_dir, avalon_asset_name), data)
 
         asset_content = unreal.EditorAssetLibrary.list_assets(
-            new_dir, recursive=True, include_folder=True
+            asset_dir, recursive=True, include_folder=True
         )
 
-        unreal.EditorAssetLibrary.delete_directory(temp_dir)
+        for a in asset_content:
+            unreal.EditorAssetLibrary.save_asset(a)
 
         return asset_content
 
@@ -115,7 +147,7 @@ class SkeletalMeshFBXLoader(api.Loader):
         container_path = "{}/{}".format(container["namespace"],
                                         container["objectName"])
         # update metadata
-        avalon_unreal.imprint(
+        unreal_pipeline.imprint(
             container_path, {"_id": str(representation["_id"])})
 
     def remove(self, container):
