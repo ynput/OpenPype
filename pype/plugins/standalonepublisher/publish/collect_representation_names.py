866d18e472c4412fd14a8d04fb7d83de08e8539a
Milan Kolar <milan@pype.club>
2020-10-15 14:11:44 +0200

    remove useless logs

--- pype/plugins/standalonepublisher/publish/collect_representation_names.py
+++ pype/plugins/standalonepublisher/publish/collect_representation_names.py
@@ -1,17 +1,9 @@
-"""
-Requires:
-    Nothing
-
-Provides:
-    Instance
-"""
-
-import pyblish.api
-from pprint import pformat
 import re
 import os
+import pyblish.api
 
-class CollecRepresentationNames(pyblish.api.InstancePlugin):
+
+class CollectRepresentationNames(pyblish.api.InstancePlugin):
     """
     Sets the representation names for given families based on RegEx filter
     """
@@ -23,18 +15,17 @@ class CollecRepresentationNames(pyblish.api.InstancePlugin):
     name_filter = ""
 
     def process(self, instance):
-        self.log.debug(f"instance.data: {pformat(instance.data['representations'])}")
         for repre in instance.data['representations']:
-            self.log.debug(repre['files'])
+            new_repre_name = None
             if isinstance(repre['files'], list):
                 shortened_name = os.path.splitext(repre['files'][0])[0]
-                new_repre_name = re.search(self.name_filter, shortened_name)
+                new_repre_name = re.search(self.name_filter,
+                                           shortened_name).group()
             else:
-                new_repre_name = re.search(self.name_filter, repre['files'])
+                new_repre_name = re.search(self.name_filter,
+                                           repre['files']).group()
 
+            if new_repre_name:
+                repre['name'] = new_repre_name
 
-            self.log.debug(new_repre_name.group())
-            repre['name'] = new_repre_name.group()
-            repre['outputName'] = new_repre_name.group()
-
-        self.log.debug(f"instance.data: {pformat(instance.data['representations'])}")
+            repre['outputName'] = repre['name']
