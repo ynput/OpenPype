270d94f29d4124dfe142ca6bd086e00b17176cf3
iLLiCiTiT <jakub.trllo@gmail.com>
2020-10-27 20:58:15 +0100

    added pype's localization file

--- pype/hosts/tvpaint/__init__.py
+++ pype/hosts/tvpaint/__init__.py
@@ -1,1 +1,8 @@
-kwargs = None
+import os
+from avalon.tvpaint.communication_server import register_localization_file
+
+
+def install():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    localization_file = os.path.join(current_dir, "avalon.loc")
+    register_localization_file(localization_file)
