946828f232c6226dc1ed339a89c753ff5c145ac0
iLLiCiTiT <jakub.trllo@gmail.com>
2020-10-28 19:15:07 +0100

    formatting modifications

--- pype/tools/pyblish_pype/util.py
+++ pype/tools/pyblish_pype/util.py
@@ -1,5 +1,9 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals
+)
 
 import os
 import sys
