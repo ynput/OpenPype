4483e61e8f3d63fac35d6694b6a4225c8c959301
Jakub Jezek <jakub@orbi.tools>
2020-11-03 17:04:27 +0100

    fix(hiero): plugin qt imports

--- setup/hiero/hiero_plugin_path/Python/Startup/new_sequence_from_selection.py
+++ setup/hiero/hiero_plugin_path/Python/Startup/new_sequence_from_selection.py
@@ -2,8 +2,15 @@
 
 from hiero.core import *
 from hiero.ui import *
-from PySide.QtGui import *
-from PySide.QtCore import *
+
+try:
+    from PySide.QtGui import *
+    from PySide.QtCore import *
+except ImportError:
+    from PySide2.QtGui import *
+    from PySide2.QtWidgets import *
+    from PySide2.QtCore import *
+
 import re
 import os
 
