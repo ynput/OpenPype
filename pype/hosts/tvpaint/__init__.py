9839e540252d77f51d243d3e5da94d7c2202495b
iLLiCiTiT <jakub.trllo@gmail.com>
2020-11-05 18:26:49 +0100

    added toggle callback to activate/deactivate instances

--- pype/hosts/tvpaint/__init__.py
+++ pype/hosts/tvpaint/__init__.py
@@ -2,6 +2,7 @@ import os
 import logging
 
 from avalon.tvpaint.communication_server import register_localization_file
+from avalon.tvpaint import pipeline
 import avalon.api
 import pyblish.api
 from pype import PLUGINS_DIR
@@ -13,6 +14,23 @@ LOAD_PATH = os.path.join(PLUGINS_DIR, "tvpaint", "load")
 CREATE_PATH = os.path.join(PLUGINS_DIR, "tvpaint", "create")
 
 
+def on_instance_toggle(instance, old_value, new_value):
+    instance_id = instance.data["id"]
+    found = False
+    current_instances = pipeline.list_instances()
+    for idx, workfile_instance in enumerate(current_instances):
+        if workfile_instance["id"] == instance_id:
+            found = True
+            break
+
+    if not found:
+        return
+
+    if "active" in current_instances[idx]:
+        current_instances[idx]["active"] = new_value
+        pipeline._write_instances(current_instances)
+
+
 def install():
     log.info("Pype - Installing TVPaint integration")
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -22,6 +40,12 @@ def install():
     pyblish.api.register_plugin_path(PUBLISH_PATH)
     avalon.api.register_plugin_path(avalon.api.Loader, LOAD_PATH)
     avalon.api.register_plugin_path(avalon.api.Creator, CREATE_PATH)
+
+    registered_callbacks = (
+        pyblish.api.registered_callbacks().get("instanceToggled") or []
+    )
+    if on_instance_toggle not in registered_callbacks:
+        pyblish.api.register_callback("instanceToggled", on_instance_toggle)
 
 
 def uninstall():
