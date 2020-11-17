b5b62f8a6a45d2d2baa02dd19f0c8afc6933327e
Jakub Jezek <jakub@orbi.tools>
2020-11-17 17:25:47 +0100

    feat(hiero): apply colorspace from presets
    also collect colorspace to workfile instance

--- pype/plugins/hiero/publish/collect_workfile.py
+++ pype/plugins/hiero/publish/collect_workfile.py
@@ -8,22 +8,41 @@ class CollectWorkfile(pyblish.api.ContextPlugin):
     """Inject the current working file into context"""
 
     label = "Collect Workfile"
-    order = pyblish.api.CollectorOrder - 0.5
+    order = pyblish.api.CollectorOrder - 0.501
 
     def process(self, context):
+        asset = avalon.Session["AVALON_ASSET"]
+        subset = "workfile"
+
         project = phiero.get_current_project()
         active_sequence = phiero.get_current_sequence()
         current_file = project.path()
         staging_dir = os.path.dirname(current_file)
         base_name = os.path.basename(current_file)
 
-        # get main project attributes to context
+        # get workfile's colorspace properties
+        _clrs = {}
+        _clrs["useOCIOEnvironmentOverride"] =  project.useOCIOEnvironmentOverride()  # noqa
+        _clrs["lutSetting16Bit"] = project.lutSetting16Bit()
+        _clrs["lutSetting8Bit"] = project.lutSetting8Bit()
+        _clrs["lutSettingFloat"] = project.lutSettingFloat()
+        _clrs["lutSettingLog"] = project.lutSettingLog()
+        _clrs["lutSettingViewer"] = project.lutSettingViewer()
+        _clrs["lutSettingWorkingSpace"] = project.lutSettingWorkingSpace()
+        _clrs["lutUseOCIOForExport"] = project.lutUseOCIOForExport()
+        _clrs["ocioConfigName"] = project.ocioConfigName()
+        _clrs["ocioConfigPath"] = project.ocioConfigPath()
+
+        from pprint import pformat
+        print pformat(_clrs)
+
+        # set main project attributes to context
         context.data["activeProject"] = project
         context.data["activeSequence"] = active_sequence
         context.data["currentFile"] = current_file
+        context.data["colorspace"] = _clrs
+
         self.log.info("currentFile: {}".format(current_file))
-        asset = avalon.Session["AVALON_ASSET"]
-        subset = "workfile"
 
         # creating workfile representation
         representation = {
@@ -39,6 +58,11 @@ class CollectWorkfile(pyblish.api.ContextPlugin):
             "subset": "{}{}".format(asset, subset.capitalize()),
             "item": project,
             "family": "workfile",
+
+            # version data
+            "versionData": {
+                "colorspace": _clrs
+            },
 
             # source attribute
             "sourcePath": current_file,
