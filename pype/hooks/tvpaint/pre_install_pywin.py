8abd04959ad7b7e3bc9f8140c2073ae84c9c6f61
iLLiCiTiT <jakub.trllo@gmail.com>
2020-11-24 18:25:26 +0100

    added prelaunch hook for tvpaint for installation of pywin32

---
+++ pype/hooks/tvpaint/pre_install_pywin.py
@@ -0,0 +1,34 @@
+from pype.lib import (
+    PreLaunchHook,
+    ApplicationLaunchFailed,
+    _subprocess
+)
+
+
+class PreInstallPyWin(PreLaunchHook):
+    """Hook makes sure there is installed python module pywin32 on windows."""
+    # WARNING This hook will probably be deprecated in Pype 3 - kept for test
+    order = 10
+    hosts = ["tvpaint"]
+    platforms = ["windows"]
+
+    def execute(self):
+        installed = False
+        try:
+            from win32com.shell import shell
+            self.log.debug("Python module `pywin32` already installed.")
+            installed = True
+        except Exception:
+            pass
+
+        if installed:
+            return
+
+        try:
+            output = _subprocess(
+                ["pip", "install", "pywin32==227"], logger=self.log
+            )
+        except RuntimeError:
+            msg = "Installation of python module `pywin32` crashed."
+            self.log.warning(msg, exc_info=True)
+            raise ApplicationLaunchFailed(msg)
