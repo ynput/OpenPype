529bca3f9c08779573066ca0dff2790b7cfc16b7
iLLiCiTiT <jakub.trllo@gmail.com>
2020-12-01 17:09:41 +0100

    fixed defaults save in projects

--- pype/tools/settings/settings/widgets/base.py
+++ pype/tools/settings/settings/widgets/base.py
@@ -239,14 +239,6 @@ class SystemWidget(QtWidgets.QWidget):
         if not self.items_are_valid():
             return
 
-        output = {}
-        for item in self.input_fields:
-            output.update(item.config_value())
-
-        for key in reversed(self.keys):
-            _output = {key: output}
-            output = _output
-
         all_values = {}
         for item in self.input_fields:
             all_values.update(item.config_value())
@@ -625,6 +617,8 @@ class ProjectWidget(QtWidgets.QWidget):
         all_values = {}
         for item in self.input_fields:
             all_values.update(item.config_value())
+
+        all_values = lib.convert_gui_data_with_metadata(all_values)
 
         for key in reversed(self.keys):
             _all_values = {key: all_values}
