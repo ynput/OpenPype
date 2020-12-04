45a6988fd80d075b74a6cdf9f7c257ab3abd6e7f
iLLiCiTiT <jakub.trllo@gmail.com>
2020-12-04 19:14:24 +0100

    added default envent handler paths to ftrack lib

--- pype/modules/ftrack/lib/__init__.py
+++ pype/modules/ftrack/lib/__init__.py
@@ -1,8 +1,19 @@
+import os
 from . import avalon_sync
 from . import credentials
 from .ftrack_base_handler import BaseHandler
 from .ftrack_event_handler import BaseEvent
 from .ftrack_action_handler import BaseAction, ServerAction, statics_icon
+
+FTRACK_MODULE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SERVER_HANDLERS_DIR = os.path.join(
+    FTRACK_MODULE_DIR,
+    "events"
+)
+USER_HANDLERS_DIR = os.path.join(
+    FTRACK_MODULE_DIR,
+    "actions"
+)
 
 __all__ = (
     "avalon_sync",
@@ -11,5 +22,8 @@ __all__ = (
     "BaseEvent",
     "BaseAction",
     "ServerAction",
-    "statics_icon"
+    "statics_icon",
+    "FTRACK_MODULE_DIR",
+    "SERVER_HANDLERS_DIR",
+    "USER_HANDLERS_DIR"
 )
