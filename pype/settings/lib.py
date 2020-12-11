ab5fadccbb18e6c53687fc1e51b8feb9867a391b
iLLiCiTiT <jakub.trllo@gmail.com>
2020-12-11 16:05:21 +0100

    extracted constants to constants.py

--- pype/settings/lib.py
+++ pype/settings/lib.py
@@ -2,52 +2,26 @@ import os
 import json
 import logging
 import copy
+from .constants import (
+    M_OVERRIDEN_KEY,
+    M_ENVIRONMENT_KEY,
+    M_DYNAMIC_KEY_LABEL,
+    M_POP_KEY,
+
+    METADATA_KEYS,
+
+    SYSTEM_SETTINGS_KEY,
+    ENVIRONMENTS_KEY,
+    PROJECT_SETTINGS_KEY,
+    PROJECT_ANATOMY_KEY
+)
+)
 
 log = logging.getLogger(__name__)
 
 # Py2 + Py3 json decode exception
 JSON_EXC = getattr(json.decoder, "JSONDecodeError", ValueError)
 
-# Metadata keys for work with studio and project overrides
-M_OVERRIDEN_KEY = "__overriden_keys__"
-# Metadata key for storing information about environments
-M_ENVIRONMENT_KEY = "__environment_keys__"
-# Metadata key for storing dynamic created labels
-M_DYNAMIC_KEY_LABEL = "__dynamic_keys_labels__"
-# NOTE key popping not implemented yet
-M_POP_KEY = "__pop_key__"
-
-METADATA_KEYS = (
-    M_OVERRIDEN_KEY,
-    M_ENVIRONMENT_KEY,
-    M_DYNAMIC_KEY_LABEL,
-    M_POP_KEY
-)
-
-# Folder where studio overrides are stored
-STUDIO_OVERRIDES_PATH = os.getenv("PYPE_PROJECT_CONFIGS") or ""
-
-# File where studio's system overrides are stored
-SYSTEM_SETTINGS_KEY = "system_settings"
-SYSTEM_SETTINGS_PATH = os.path.join(
-    STUDIO_OVERRIDES_PATH, SYSTEM_SETTINGS_KEY + ".json"
-)
-
-# File where studio's environment overrides are stored
-ENVIRONMENTS_KEY = "environments"
-
-# File where studio's default project overrides are stored
-PROJECT_SETTINGS_KEY = "project_settings"
-PROJECT_SETTINGS_FILENAME = PROJECT_SETTINGS_KEY + ".json"
-PROJECT_SETTINGS_PATH = os.path.join(
-    STUDIO_OVERRIDES_PATH, PROJECT_SETTINGS_FILENAME
-)
-
-PROJECT_ANATOMY_KEY = "project_anatomy"
-PROJECT_ANATOMY_FILENAME = PROJECT_ANATOMY_KEY + ".json"
-PROJECT_ANATOMY_PATH = os.path.join(
-    STUDIO_OVERRIDES_PATH, PROJECT_ANATOMY_FILENAME
-)
 
 # Path to default settings
 DEFAULTS_DIR = os.path.join(os.path.dirname(__file__), "defaults")
