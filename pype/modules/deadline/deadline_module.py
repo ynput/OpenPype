6232b59a9a82a89682da64a4178396f0affdf96f
iLLiCiTiT <jakub.trllo@gmail.com>
2020-12-11 11:59:41 +0100

    implemented base of deadline module

---
+++ pype/modules/deadline/deadline_module.py
@@ -0,0 +1,21 @@
+import os
+from .. import PypeModule
+
+
+class DeadlineModule(PypeModule):
+    name = "deadline"
+
+    def initialize(self, modules_settings):
+        # This module is always enabled
+        deadline_settings = modules_settings[self.name]
+        self.enabled = deadline_settings["enabled"]
+        self.deadline_url = deadline_settings["DEADLINE_REST_URL"]
+
+    def get_global_environments(self):
+        """Deadline global environments for pype implementation."""
+        return {
+            "DEADLINE_REST_URL": self.deadline_url
+        }
+
+    def connect_with_modules(self, *_a, **_kw):
+        return
