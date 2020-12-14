8c70ebb78dabb2e4625271b7bf7a258e20d32631
Ondřej Samohel <ondrej.samohel@pype.club>
2020-12-14 18:01:09 +0100

    switch to blessed

--- pype/lib/terminal_splash.py
+++ pype/lib/terminal_splash.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """Pype terminal animation :)"""
 import os
+import blessed
 from pathlib import Path
 from time import sleep
 import sys
 
+term = blessed.Terminal()
 
 def play_animation():
+    print(term.home + term.clear)
     frame_size = 7
     splash_file = Path(__file__).parent / "splash.txt"
     with splash_file.open("r") as sf:
@@ -15,11 +18,17 @@ def play_animation():
     animation_length = int(len(animation) / frame_size)
     current_frame = 0
     for _ in range(animation_length):
-        if sys.platform.startswith('win'):
-            os.system('cls')
-        else:
-            os.system('clear')
+        frame = ""
+        y = 0
         for scanline in animation[current_frame:current_frame + frame_size]:
-            print(scanline.rstrip())
-        sleep(0.005)
+            frame += scanline
+            y += 1
+
+        with term.location(0, 0):
+            # term.aquamarine3_bold(frame)
+            print(f"{term.bold}{term.aquamarine3}{frame}{term.normal}")
+
+
+        sleep(0.05)
         current_frame += frame_size
+    print(term.move_y(7))
