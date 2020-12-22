86419f80c65f855bd4ea01c97651990cf3173634
iLLiCiTiT <jakub.trllo@gmail.com>
2020-12-22 13:14:17 +0100

    get_project_entity_from_event store only project name not whole entity

--- pype/modules/ftrack/lib/ftrack_event_handler.py
+++ pype/modules/ftrack/lib/ftrack_event_handler.py
@@ -65,14 +65,15 @@ class BaseEvent(BaseHandler):
                 )
             )
         # Try to get project entity from event
-        project_entities = event["data"].get("project_entities")
-        if not project_entities:
-            project_entities = {}
-            event["data"]["project_entities"] = project_entities
+        project_data = event["data"].get("project_data")
+        if not project_data:
+            project_data = {}
+            event["data"]["project_data"] = project_data
 
-        project_entity = project_entities.get(project_id)
-        if not project_entity:
+        project_name = project_data.get(project_id)
+        if not project_name:
             # Get project entity from task and store to event
             project_entity = session.get("Project", project_id)
-            event["data"]["project_entities"][project_id] = project_entity
-        return project_entity
+            project_name = project_entity["full_name"]
+            event["data"]["project_data"][project_id] = project_name
+        return project_name
