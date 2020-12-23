367b0649f0203c6ca1483fd1538d81d7b1ca3f0c
Milan Kolar <milan@pype.club>
2020-12-23 12:47:12 +0100

    move nuke to nuke.api and temp disable onscript callbacks

--- setup/nuke/nuke_path/menu.py
+++ setup/nuke/nuke_path/menu.py
@@ -2,7 +2,7 @@ import os
 import sys
 import KnobScripter
 
-from pype.hosts.nuke.lib import (
+from pype.hosts.nuke.api.lib import (
     writes_version_sync,
     on_script_load,
     check_inventory_versions
@@ -14,10 +14,10 @@ from pype.api import Logger
 log = Logger().get_logger(__name__, "nuke")
 
 
-# nuke.addOnScriptSave(writes_version_sync)
-nuke.addOnScriptSave(on_script_load)
-nuke.addOnScriptLoad(check_inventory_versions)
-nuke.addOnScriptSave(check_inventory_versions)
-# nuke.addOnScriptSave(writes_version_sync)
+# nuke.addOnScriptSave(lib.writes_version_sync)
+# nuke.addOnScriptSave(lib.on_script_load)
+# nuke.addOnScriptLoad(lib.check_inventory_versions)
+# nuke.addOnScriptSave(lib.check_inventory_versions)
+# nuke.addOnScriptSave(lib.writes_version_sync)
 
 log.info('Automatic syncing of write file knob to script version')
