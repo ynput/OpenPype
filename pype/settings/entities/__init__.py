ab68ccfdfa6106a5027b48f7f5de856e67a1b52f
iLLiCiTiT <jakub.trllo@gmail.com>
2021-01-22 15:27:04 +0100

    adde init py to entities

---
+++ pype/settings/entities/__init__.py
@@ -0,0 +1,16 @@
+from . import (
+    constants,
+    base_entity,
+    item_entities,
+    input_entities,
+    lib
+)
+
+
+__all__ = (
+    "constants",
+    "base_entity",
+    "item_entities",
+    "input_entities",
+    "lib"
+)
