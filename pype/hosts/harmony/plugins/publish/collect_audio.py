a44b735f8acef888ac1a8f8f297512e2422c7daf
Petr Kalis <petr.kalis@gmail.com>
2021-01-27 16:24:00 +0100

    Harmony fix - no audio file won't fail

--- pype/hosts/harmony/plugins/publish/collect_audio.py
+++ pype/hosts/harmony/plugins/publish/collect_audio.py
@@ -1,6 +1,7 @@
 import os
 import pyblish.api
 
+import pyblish.api
 
 class CollectAudio(pyblish.api.InstancePlugin):
     """
@@ -18,6 +19,7 @@ class CollectAudio(pyblish.api.InstancePlugin):
     families = ["renderlayer"]
 
     def process(self, instance):
+        full_file_name = None
         audio_dir = os.path.join(
             os.path.dirname(instance.context.data.get("currentFile")), 'audio')
         if os.path.isdir(audio_dir):
@@ -27,7 +29,9 @@ class CollectAudio(pyblish.api.InstancePlugin):
                 if file_ext not in ['.wav', '.mp3', '.aiff']:
                     self.log.error("Unsupported file {}.{}".format(file_name,
                                                                    file_ext))
+                    full_file_name = None
 
-        audio_file_path = os.path.join('audio', full_file_name)
-        self.log.debug("audio_file_path {}".format(audio_file_path))
-        instance.data["audioFile"] = audio_file_path
+        if full_file_name:
+            audio_file_path = os.path.join('audio', full_file_name)
+            self.log.debug("audio_file_path {}".format(audio_file_path))
+            instance.data["audioFile"] = audio_file_path
