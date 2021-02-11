6bdf19b639f8b001ddf31c4fe3b4be08fbc537ba
Milan Kolar <milan@pype.club>
2021-02-11 10:07:31 +0100

    nuke fixes - waiting for confirmation
    should be included in hotfix once it's ready

--- pype/plugins/nuke/publish/collect_instances.py
+++ pype/plugins/nuke/publish/collect_instances.py
@@ -51,7 +51,7 @@ class CollectNukeInstances(pyblish.api.ContextPlugin):
 
             # establish families
             family = avalon_knob_data["family"]
-            families_ak = avalon_knob_data.get("families")
+            families_ak = avalon_knob_data.get("families", [])
             families = list()
 
             if families_ak:
