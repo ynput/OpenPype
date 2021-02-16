00ce7e38abd63169cfbdbc403751fc520d23f1f6
iLLiCiTiT <jakub.trllo@gmail.com>
2021-02-16 15:18:15 +0100

    removed item_types and anatomy_types

--- pype/tools/settings/settings/widgets/__init__.py
+++ pype/tools/settings/settings/widgets/__init__.py
@@ -1,11 +1,8 @@
 from .window import MainWidget
 from .widgets import ProjectListWidget
-from . import item_types
-from . import anatomy_types
+
 
 __all__ = [
     "MainWidget",
-    "ProjectListWidget",
-    "item_types",
-    "anatomy_types"
+    "ProjectListWidget"
 ]
