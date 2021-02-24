6faface6f3a5a34ae08a2a0c15812a55bbf265d5
iLLiCiTiT <jakub.trllo@gmail.com>
2021-02-24 13:08:46 +0100

    inputs have unified way how to use label wrap

--- pype/tools/settings/settings/widgets/base.py
+++ pype/tools/settings/settings/widgets/base.py
@@ -1,4 +1,6 @@
 from Qt import QtWidgets, QtGui, QtCore
+from .lib import CHILD_OFFSET
+from .widgets import ExpandingWidget
 
 
 class BaseWidget(QtWidgets.QWidget):
@@ -161,26 +163,91 @@ class BaseWidget(QtWidgets.QWidget):
 
 
 class InputWidget(BaseWidget):
+    def create_ui(self):
+        if self.entity.use_label_wrap:
+            label = None
+            self._create_label_wrap_ui()
+        else:
+            label = self.entity.label
+            self.label_widget = None
+            self.body_widget = None
+            self.content_widget = self
+            self.content_layout = self._create_layout(self)
+
+        self._add_inputs_to_layout()
+
+        self.entity_widget.add_widget_to_layout(self, label)
+
+    def _create_label_wrap_ui(self):
+        content_widget = QtWidgets.QWidget(self)
+        content_widget.setObjectName("ContentWidget")
+
+        content_widget.setProperty("content_state", "")
+        content_layout_margins = (CHILD_OFFSET, 5, 0, 0)
+
+        body_widget = ExpandingWidget(self.entity.label, self)
+        label_widget = body_widget.label_widget
+        body_widget.set_content_widget(content_widget)
+
+        content_layout = self._create_layout(content_widget)
+        content_layout.setContentsMargins(*content_layout_margins)
+
+        main_layout = QtWidgets.QHBoxLayout(self)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(0)
+        main_layout.addWidget(body_widget)
+
+        self.label_widget = label_widget
+        self.body_widget = body_widget
+        self.content_widget = content_widget
+        self.content_layout = content_layout
+
+    def _create_layout(self, parent_widget):
+        layout = QtWidgets.QHBoxLayout(parent_widget)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(5)
+        return layout
+
+    def _add_inputs_to_layout(self):
+        raise NotImplementedError(
+            "Method `_add_inputs_to_layout` not implemented {}".format(
+                self.__class__.__name__
+            )
+        )
+
     def update_style(self):
         has_unsaved_changes = self.entity.has_unsaved_changes
         if not has_unsaved_changes and self.entity.group_item:
             has_unsaved_changes = self.entity.group_item.has_unsaved_changes
-        state = self.get_style_state(
+        style_state = self.get_style_state(
             self.is_invalid,
             has_unsaved_changes,
             self.entity.has_project_override,
             self.entity.has_studio_override
         )
-        if self._style_state == state:
+        if self._style_state == style_state:
             return
 
-        self._style_state = state
+        self._style_state = style_state
 
-        self.input_field.setProperty("input-state", state)
+        self.input_field.setProperty("input-state", style_state)
         self.input_field.style().polish(self.input_field)
         if self.label_widget:
-            self.label_widget.setProperty("state", state)
+            self.label_widget.setProperty("state", style_state)
             self.label_widget.style().polish(self.label_widget)
+
+        if self.body_widget:
+            if style_state:
+                child_style_state = "child-{}".format(style_state)
+            else:
+                child_style_state = ""
+
+            self.body_widget.side_line_widget.setProperty(
+                "state", child_style_state
+            )
+            self.body_widget.side_line_widget.style().polish(
+                self.body_widget.side_line_widget
+            )
 
     @property
     def child_invalid(self):
