73916c5bb83ac2b64bf97115afea594ffccc9684
Toke Stuart Jepsen <tokejepsen@gmail.com>
2021-02-27 12:45:16 +0000

    Reinstate Nuke asset dependency.

--- pype/plugins/nuke/publish/submit_nuke_deadline.py
+++ pype/plugins/nuke/publish/submit_nuke_deadline.py
@@ -151,7 +151,7 @@ class NukeSubmitDeadline(pyblish.api.InstancePlugin):
                 "BatchName": script_name,
 
                 # Asset dependency to wait for at least the scene file to sync.
-                # "AssetDependency0": script_path,
+                "AssetDependency0": script_path,
 
                 # Job name, as seen in Monitor
                 "Name": jobname,
