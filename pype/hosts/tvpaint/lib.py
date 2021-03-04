50a59f227411ad7d7c82243d5b142c72c03121af
iLLiCiTiT <jakub.trllo@gmail.com>
2021-03-04 20:43:29 +0100

    extraction is a little bit faster

--- pype/hosts/tvpaint/lib.py
+++ pype/hosts/tvpaint/lib.py
@@ -1,9 +1,15 @@
 from PIL import Image
 
 
-def composite_images(
-    input_image_paths, output_filepath, scene_width, scene_height
-):
+def composite_images(input_image_paths, output_filepath):
+    """Composite images in order from passed list.
+
+    Raises:
+        ValueError: When entered list is empty.
+    """
+    if not input_image_paths:
+        raise ValueError("Nothing to composite.")
+
     img_obj = None
     for image_filepath in input_image_paths:
         _img_obj = Image.open(image_filepath)
@@ -11,7 +17,4 @@ def composite_images(
             img_obj = _img_obj
         else:
             img_obj.alpha_composite(_img_obj)
-
-    if img_obj is None:
-        img_obj = Image.new("RGBA", (scene_width, scene_height), (0, 0, 0, 0))
     img_obj.save(output_filepath)
