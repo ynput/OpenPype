bc4f46cf0d8496774bc7dd5b5dbe2356d6775a54
iLLiCiTiT <jakub.trllo@gmail.com>
2021-03-05 19:11:57 +0100

    added environments view

--- pype/tools/tray/pype_info_widget.py
+++ pype/tools/tray/pype_info_widget.py
@@ -1,10 +1,59 @@
 import os
 import sys
+import json
+import collections
 
 from avalon import style
 from Qt import QtCore, QtGui, QtWidgets
-from pype.api import Logger, resources
+from pype.api import resources
 import pype.version
+
+
+class EnvironmentsView(QtWidgets.QTreeView):
+    def __init__(self, model, parent=None):
+        super(EnvironmentsView, self).__init__(parent)
+        self.setModel(model)
+        self.setIndentation(0)
+        self.header().setSectionResizeMode(
+            0, QtWidgets.QHeaderView.ResizeToContents
+        )
+        self.setSelectionMode(QtWidgets.QTreeView.MultiSelection)
+
+    def get_all_as_dict(self):
+        pass
+
+    def get_selection_as_dict(self):
+        indexes = self.selectionModel().selectedIndexes()
+        mapping = collections.defaultdict(dict)
+        for index in indexes:
+            row = index.row()
+            value = index.data(QtCore.Qt.DisplayRole)
+            if index.column() == 0:
+                key = "key"
+            else:
+                key = "value"
+            mapping[row][key] = value
+        result = {}
+        for item in mapping.values():
+            result[item["key"]] = item["value"]
+        return result
+
+    def keyPressEvent(self, event):
+        if (
+            event.type() == QtGui.QKeyEvent.KeyPress
+            and event.matches(QtGui.QKeySequence.Copy)
+        ):
+            selected_dict = self.get_selection_as_dict()
+            selected_str = json.dumps(selected_dict, indent=4)
+
+            mime_data = QtCore.QMimeData()
+            mime_data.setText(selected_str)
+            QtWidgets.QApplication.instance().clipboard().setMimeData(
+                mime_data
+            )
+            event.accept()
+        else:
+            return super(EnvironmentsView, self).keyPressEvent(event)
 
 
 class PypeInfoWidget(QtWidgets.QWidget):
@@ -21,6 +70,34 @@ class PypeInfoWidget(QtWidgets.QWidget):
 
         main_layout = QtWidgets.QVBoxLayout(self)
         main_layout.addWidget(self._create_pype_info_widget())
+        main_layout.addWidget(self._create_environ_widget())
+
+    def _create_environ_widget(self):
+        env_widget = QtWidgets.QWidget(self)
+
+        env_label_widget = QtWidgets.QLabel("Environments", env_widget)
+        env_label_widget.setStyleSheet("font-weight: bold;")
+
+        env_model = QtGui.QStandardItemModel()
+
+        env = os.environ.copy()
+        keys = []
+        values = []
+        for key in sorted(env.keys()):
+            keys.append(QtGui.QStandardItem(key))
+            values.append(QtGui.QStandardItem(env[key]))
+
+        env_model.appendColumn(keys)
+        env_model.appendColumn(values)
+        env_model.setHorizontalHeaderLabels(["Key", "Value"])
+
+        env_view = EnvironmentsView(env_model, env_widget)
+
+        env_layout = QtWidgets.QVBoxLayout(env_widget)
+        env_layout.addWidget(env_label_widget)
+        env_layout.addWidget(env_view)
+
+        return env_widget
 
     def _create_pype_info_widget(self):
         """Create widget with information about pype application."""
@@ -47,6 +124,9 @@ class PypeInfoWidget(QtWidgets.QWidget):
 
         info_widget = QtWidgets.QWidget(self)
         info_layout = QtWidgets.QGridLayout(info_widget)
+        # Add spacer to 3rd column
+        info_layout.addWidget(QtWidgets.QWidget(info_widget), 0, 2)
+        info_layout.setColumnStretch(2, 1)
 
         title_label = QtWidgets.QLabel(info_widget)
         title_label.setText("Application information")
@@ -59,6 +139,9 @@ class PypeInfoWidget(QtWidgets.QWidget):
                 QtWidgets.QLabel(label), row, 0, 1, 1
             )
             value_label = QtWidgets.QLabel(value or self.not_allowed)
+            value_label.setTextInteractionFlags(
+                QtCore.Qt.TextSelectableByMouse
+            )
             info_layout.addWidget(
                 value_label, row, 1, 1, 1
             )
