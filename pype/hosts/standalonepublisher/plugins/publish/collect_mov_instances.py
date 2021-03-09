406c665d1d36a4deb129b09397c7854ed1c0e230
iLLiCiTiT <jakub.trllo@gmail.com>
2021-03-09 14:46:12 +0100

    created copy of psb bulk plugin but for render mov batch
    # Conflicts:
    #	pype/hosts/standalonepublisher/plugins/publish/collect_mov_instances.py

--- pype/hosts/standalonepublisher/plugins/publish/collect_mov_instances.py
+++ pype/hosts/standalonepublisher/plugins/publish/collect_mov_instances.py
@@ -3,40 +3,29 @@ import pyblish.api
 from pprint import pformat
 
 
-class CollectPsdInstances(pyblish.api.InstancePlugin):
-    """
-    Collect all available instances from psd batch.
-    """
+class CollectMovInstances(pyblish.api.InstancePlugin):
+    """Collect all available instances from render mov batch."""
 
-    label = "Collect Psd Instances"
+    label = "Collect Mov Instances"
     order = pyblish.api.CollectorOrder + 0.489
     hosts = ["standalonepublisher"]
-    families = ["background_batch"]
+    families = ["render_mov_batch"]
 
     # presets
     subsets = {
-        "backgroundLayout": {
-            "task": "background",
-            "family": "backgroundLayout"
-        },
-        "backgroundComp": {
-            "task": "background",
-            "family": "backgroundComp"
-        },
-        "workfileBackground": {
-            "task": "background",
-            "family": "workfile"
+        "render": {
+            "task": "compositing",
+            "family": "render"
         }
     }
     unchecked_by_default = []
 
     def process(self, instance):
         context = instance.context
-        asset_data = instance.data["assetEntity"]
         asset_name = instance.data["asset"]
         for subset_name, subset_data in self.subsets.items():
             instance_name = f"{asset_name}_{subset_name}"
-            task = subset_data.get("task", "background")
+            task_name = subset_data["task"]
 
             # create new instance
             new_instance = context.create_instance(instance_name)
@@ -51,15 +40,15 @@ class CollectPsdInstances(pyblish.api.InstancePlugin):
             # add subset data from preset
             new_instance.data.update(subset_data)
 
-            new_instance.data["label"] = f"{instance_name}"
+            new_instance.data["label"] = instance_name
             new_instance.data["subset"] = subset_name
-            new_instance.data["task"] = task
+            new_instance.data["task"] = task_name
 
             if subset_name in self.unchecked_by_default:
                 new_instance.data["publish"] = False
 
             self.log.info(f"Created new instance: {instance_name}")
-            self.log.debug(f"_ inst_data: {pformat(new_instance.data)}")
+            self.log.debug(f"New instance data: {pformat(new_instance.data)}")
 
         # delete original instance
         context.remove(instance)
