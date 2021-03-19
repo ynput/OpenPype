728b6ab5004f6ed06e581623168597ebfbaa6200
Petr Kalis <petr.kalis@gmail.com>
2021-03-19 15:20:44 +0100

    Basic working AddSyncSite

--- pype/plugins/load/add_site.py
+++ pype/plugins/load/add_site.py
@@ -1,6 +1,5 @@
-import os
-
 from avalon import api
+from pype.modules import ModulesManager
 
 
 class AddSyncSite(api.Loader):
@@ -14,9 +13,17 @@ class AddSyncSite(api.Loader):
     color = "#999999"
 
     def load(self, context, name=None, namespace=None, data=None):
-        self.log.info("Added site to representation: {0}".format(self.fname))
-        self.add_site_to_representation(self.fname)
+        self.log.info("Adding site to representation: {0}".format(self.fname))
+        self.add_site_to_representation(data["project_name"],
+                                        data["_id"],
+                                        data["site_name"])
 
     @staticmethod
-    def add_site_to_representation(path):
-        pass
+    def add_site_to_representation(project_name, representation_id, site_name):
+        manager = ModulesManager()
+        sync_server = manager.modules_by_name["sync_server"]
+        sync_server.add_site(project_name, representation_id, site_name)
+
+    def filepath_from_context(self, context):
+        """No real file loading"""
+        return ""
