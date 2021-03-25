71c9fb431a072d1dd5327589a988b325ce3189de
iLLiCiTiT <jakub.trllo@gmail.com>
2021-03-25 17:28:47 +0100

    implemented function to apply project specific environments

--- pype/lib/applications.py
+++ pype/lib/applications.py
@@ -940,6 +940,19 @@ def prepare_host_environments(data):
     data["env"].update(final_env)
 
 
+def apply_project_environments_value(project_name, env, project_settings=None):
+    import acre
+
+    if project_settings is None:
+        project_settings = get_project_settings(project_name)
+
+    env_value = project_settings["global"]["project_environments"]
+    if not env_value:
+        return env
+    parsed = acre.parse(env_value)
+    return _merge_env(parsed, env)
+
+
 def prepare_context_environments(data):
     """Modify launch environemnts with context data for launched host.
 
@@ -963,6 +976,12 @@ def prepare_context_environments(data):
             " Launch context does not contain required data."
         )
         return
+
+    # Load project specific environments
+    project_name = project_doc["name"]
+    data["env"] = apply_project_environments_value(
+        project_name, data["env"]
+    )
 
     app = data["app"]
     workdir_data = get_workdir_data(
