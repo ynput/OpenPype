6f68a7c38a8cca4fd6e81ab2e34a85e9f98c97f2
iLLiCiTiT <jakub.trllo@gmail.com>
2021-03-29 14:25:21 +0200

    removed usage of settings for ftrack mongo names

--- pype/modules/ftrack/lib/settings.py
+++ pype/modules/ftrack/lib/settings.py
@@ -1,5 +1,7 @@
 from pype.api import get_system_settings
 
+PYPE_DATABASE_NAME = "pype"
+
 
 def get_ftrack_settings():
     return get_system_settings()["modules"]["ftrack"]
@@ -11,6 +13,6 @@ def get_ftrack_url_from_settings():
 
 def get_ftrack_event_mongo_info():
     ftrack_settings = get_ftrack_settings()
-    database_name = ftrack_settings["mongo_database_name"]
-    collection_name = ftrack_settings["mongo_collection_name"]
+    database_name = PYPE_DATABASE_NAME
+    collection_name = "ftrack_events"
     return database_name, collection_name
