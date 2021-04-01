bb92dae0f27a3ae5e6b909e1e6acc81cc2618086
Petr Kalis <petr.kalis@gmail.com>
2021-04-01 17:29:24 +0200

    DL - backward compatible GlobalJobPreload

--- vendor/deadline/custom/plugins/GlobalJobPreLoad.py
+++ vendor/deadline/custom/plugins/GlobalJobPreLoad.py
@@ -4,28 +4,13 @@ import tempfile
 import time
 import subprocess
 import json
+import platform
 from Deadline.Scripting import RepositoryUtils, FileUtils
 
 
 def inject_openpype_environment(deadlinePlugin):
     job = deadlinePlugin.GetJob()
     job = RepositoryUtils.GetJob(job.JobId, True)  # invalidates cache
-
-    openpype_render_job = \
-        job.GetJobEnvironmentKeyValue('OPENPYPE_RENDER_JOB') or '0'
-    openpype_publish_job = \
-        job.GetJobEnvironmentKeyValue('OPENPYPE_PUBLISH_JOB') or '0'
-
-    if openpype_publish_job == '1' and openpype_render_job == '1':
-        raise RuntimeError("Misconfiguration. Job couldn't be both " +
-                           "render and publish.")
-
-    if openpype_publish_job == '1':
-        print("Publish job, skipping inject.")
-        return
-    elif openpype_render_job == '0':
-        # not openpype triggered job
-        return
 
     print("inject_openpype_environment start")
     try:
@@ -88,5 +73,93 @@ def inject_openpype_environment(deadlinePlugin):
         raise
 
 
+def pype_command_line(executable, arguments, workingDirectory):
+    """Remap paths in comand line argument string.
+
+    Using Deadline rempper it will remap all path found in command-line.
+
+    Args:
+        executable (str): path to executable
+        arguments (str): arguments passed to executable
+        workingDirectory (str): working directory path
+
+    Returns:
+        Tuple(executable, arguments, workingDirectory)
+
+    """
+    print("-" * 40)
+    print("executable: {}".format(executable))
+    print("arguments: {}".format(arguments))
+    print("workingDirectory: {}".format(workingDirectory))
+    print("-" * 40)
+    print("Remapping arguments ...")
+    arguments = RepositoryUtils.CheckPathMapping(arguments)
+    print("* {}".format(arguments))
+    print("-" * 40)
+    return executable, arguments, workingDirectory
+
+
+def pype(deadlinePlugin):
+    """Remaps `PYPE_METADATA_FILE` and `PYPE_PYTHON_EXE` environment vars.
+
+    `PYPE_METADATA_FILE` is used on farm to point to rendered data. This path
+    originates on platform from which this job was published. To be able to
+    publish on different platform, this path needs to be remapped.
+
+    `PYPE_PYTHON_EXE` can be used to specify custom location of python
+    interpreter to use for Pype. This is remappeda also if present even
+    though it probably doesn't make much sense.
+
+    Arguments:
+        deadlinePlugin: Deadline job plugin passed by Deadline
+
+    """
+    job = deadlinePlugin.GetJob()
+    # PYPE should be here, not OPENPYPE - backward compatibility!!
+    pype_metadata = job.GetJobEnvironmentKeyValue("PYPE_METADATA_FILE")
+    pype_python = job.GetJobEnvironmentKeyValue("PYPE_PYTHON_EXE")
+    # test if it is pype publish job.
+    if pype_metadata:
+        pype_metadata = RepositoryUtils.CheckPathMapping(pype_metadata)
+        if platform.system().lower() == "linux":
+            pype_metadata = pype_metadata.replace("\\", "/")
+
+        print("- remapping PYPE_METADATA_FILE: {}".format(pype_metadata))
+        job.SetJobEnvironmentKeyValue("PYPE_METADATA_FILE", pype_metadata)
+        deadlinePlugin.SetProcessEnvironmentVariable(
+            "PYPE_METADATA_FILE", pype_metadata)
+
+    if pype_python:
+        pype_python = RepositoryUtils.CheckPathMapping(pype_python)
+        if platform.system().lower() == "linux":
+            pype_python = pype_python.replace("\\", "/")
+
+        print("- remapping PYPE_PYTHON_EXE: {}".format(pype_python))
+        job.SetJobEnvironmentKeyValue("PYPE_PYTHON_EXE", pype_python)
+        deadlinePlugin.SetProcessEnvironmentVariable(
+            "PYPE_PYTHON_EXE", pype_python)
+
+    deadlinePlugin.ModifyCommandLineCallback += pype_command_line
+
+
 def __main__(deadlinePlugin):
-    inject_openpype_environment(deadlinePlugin)
+    job = deadlinePlugin.GetJob()
+    job = RepositoryUtils.GetJob(job.JobId, True)  # invalidates cache
+
+    openpype_render_job = \
+        job.GetJobEnvironmentKeyValue('OPENPYPE_RENDER_JOB') or '0'
+    openpype_publish_job = \
+        job.GetJobEnvironmentKeyValue('OPENPYPE_PUBLISH_JOB') or '0'
+
+    if openpype_publish_job == '1' and openpype_render_job == '1':
+        raise RuntimeError("Misconfiguration. Job couldn't be both " +
+                           "render and publish.")
+
+    if openpype_publish_job == '1':
+        print("Publish job, skipping inject.")
+        return
+    elif openpype_render_job == '1':
+        inject_openpype_environment(deadlinePlugin)
+    else:
+        pype(deadlinePlugin)  # backward compatibility with Pype2
+
