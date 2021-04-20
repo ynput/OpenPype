0ec066af7ed1a34f40d162a6e40ad9f6aa0f9d88
iLLiCiTiT <jakub.trllo@gmail.com>
2021-04-20 18:12:47 +0200

    defined constants in ftrack lib

--- openpype/modules/ftrack/lib/__init__.py
+++ openpype/modules/ftrack/lib/__init__.py
@@ -1,3 +1,10 @@
+from .constants import (
+    CUST_ATTR_ID_KEY,
+    CUST_ATTR_AUTO_SYNC,
+    CUST_ATTR_GROUP,
+    CUST_ATTR_TOOLS,
+    CUST_ATTR_APPLICATIONS
+)
 from . settings import (
     get_ftrack_url_from_settings,
     get_ftrack_event_mongo_info
@@ -10,6 +17,12 @@ from .ftrack_action_handler import BaseAction, ServerAction, statics_icon
 
 
 __all__ = (
+    "CUST_ATTR_ID_KEY",
+    "CUST_ATTR_AUTO_SYNC",
+    "CUST_ATTR_GROUP",
+    "CUST_ATTR_TOOLS",
+    "CUST_ATTR_APPLICATIONS",
+
     "get_ftrack_url_from_settings",
     "get_ftrack_event_mongo_info",
 
