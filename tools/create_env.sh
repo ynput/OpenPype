bb427ac15d2d852528dd52e78a44dc84029156f1
Milan Kolar <milan@pype.club>
2021-04-20 21:49:26 +0200

    remove openpype from self dependency in poetry

--- tools/create_env.sh
+++ tools/create_env.sh
@@ -160,7 +160,7 @@ main () {
     echo -e "${BIGreen}>>>${RST} Installing dependencies ..."
   fi
 
-  poetry install $poetry_verbosity || { echo -e "${BIRed}!!!${RST} Poetry environment installation failed"; return; }
+  poetry install --no-root $poetry_verbosity || { echo -e "${BIRed}!!!${RST} Poetry environment installation failed"; return; }
 
   echo -e "${BIGreen}>>>${RST} Cleaning cache files ..."
   clean_pyc
