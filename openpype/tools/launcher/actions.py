db0f6dcefe6d9091b280f5fd422487f541a706ae
iLLiCiTiT <jakub.trllo@gmail.com>
2021-04-21 16:16:33 +0200

    removed "default actions" and it's registration function

--- openpype/tools/launcher/actions.py
+++ openpype/tools/launcher/actions.py
@@ -1,7 +1,6 @@
 import os
-import importlib
 
-from avalon import api, lib, style
+from avalon import api, style
 from openpype import PLUGINS_DIR
 from openpype.api import Logger, resources
 from openpype.lib import (
@@ -9,66 +8,6 @@ from openpype.lib import (
     ApplicationLaunchFailed
 )
 from Qt import QtWidgets, QtGui
-
-
-class ProjectManagerAction(api.Action):
-    name = "projectmanager"
-    label = "Project Manager"
-    icon = "gear"
-    order = 999     # at the end
-
-    def is_compatible(self, session):
-        return "AVALON_PROJECT" in session
-
-    def process(self, session, **kwargs):
-        return lib.launch(
-            executable="python",
-            args=[
-                "-u", "-m", "avalon.tools.projectmanager",
-                session['AVALON_PROJECT']
-            ]
-        )
-
-
-class LoaderAction(api.Action):
-    name = "loader"
-    label = "Loader"
-    icon = "cloud-download"
-    order = 998
-
-    def is_compatible(self, session):
-        return "AVALON_PROJECT" in session
-
-    def process(self, session, **kwargs):
-        return lib.launch(
-            executable="python",
-            args=[
-                "-u", "-m", "avalon.tools.loader", session['AVALON_PROJECT']
-            ]
-        )
-
-
-class LoaderLibrary(api.Action):
-    name = "loader_os"
-    label = "Library Loader"
-    icon = "book"
-    order = 997     # at the end
-
-    def is_compatible(self, session):
-        return True
-
-    def process(self, session, **kwargs):
-        return lib.launch(
-            executable="python",
-            args=["-u", "-m", "avalon.tools.libraryloader"]
-        )
-
-
-def register_default_actions():
-    """Register default actions for Launcher"""
-    api.register_plugin(api.Action, ProjectManagerAction)
-    api.register_plugin(api.Action, LoaderAction)
-    api.register_plugin(api.Action, LoaderLibrary)
 
 
 def register_actions_from_paths(paths):
