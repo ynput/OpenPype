00b1f35526da3784f691cc75a09b41239321636b
iLLiCiTiT <jakub.trllo@gmail.com>
2021-04-30 11:11:08 +0200

    renmamed variables in window

--- openpype/tools/project_manager/project_manager/window.py
+++ openpype/tools/project_manager/project_manager/window.py
@@ -15,14 +15,15 @@ class Window(QtWidgets.QWidget):
 
         dbcon = AvalonMongoDB()
 
-        model = HierarchyModel(dbcon)
-        view = HierarchyView(model, self)
-        view.setModel(model)
-        _selection_model = HierarchySelectionModel()
-        _selection_model.setModel(view.model())
-        view.setSelectionModel(_selection_model)
+        hierarchy_model = HierarchyModel(dbcon)
 
-        header = view.header()
+        hierarchy_view = HierarchyView(hierarchy_model, self)
+        hierarchy_view.setModel(hierarchy_model)
+        _selection_model = HierarchySelectionModel()
+        _selection_model.setModel(hierarchy_view.model())
+        hierarchy_view.setSelectionModel(_selection_model)
+
+        header = hierarchy_view.header()
         header.setStretchLastSection(False)
         header.setSectionResizeMode(
             header.logicalIndex(0), QtWidgets.QHeaderView.Stretch
@@ -31,27 +32,26 @@ class Window(QtWidgets.QWidget):
         # btn = QtWidgets.QPushButton("Refresh")
 
         main_layout = QtWidgets.QVBoxLayout(self)
-        main_layout.addWidget(view)
+        main_layout.addWidget(hierarchy_view)
         main_layout.addWidget(checkbox)
         # main_layout.addWidget(btn)
         # btn.clicked.connect(self._on_refresh)
 
         checkbox.toggled.connect(self._on_checkbox)
 
-        self.view = view
-        self.model = model
         # self.btn = btn
+        self.hierarchy_view = hierarchy_view
+        self.hierarchy_model = hierarchy_model
         self.checkbox = checkbox
 
         self.change_edit_mode()
 
         self.resize(1200, 600)
-        model.set_project({"name": "test"})
 
     def change_edit_mode(self, value=None):
         if value is None:
             value = self.checkbox.isChecked()
-        self.model.change_edit_mode(value)
+        self.hierarchy_model.change_edit_mode(value)
 
     def _on_checkbox(self, value):
         self.change_edit_mode(value)
