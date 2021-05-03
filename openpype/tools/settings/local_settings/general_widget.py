e7d1651cff162eada9a32aacde02daef21653f28
iLLiCiTiT <jakub.trllo@gmail.com>
2021-05-03 12:47:51 +0200

    added is_admin input to local settings

--- openpype/tools/settings/local_settings/general_widget.py
+++ openpype/tools/settings/local_settings/general_widget.py
@@ -10,18 +10,33 @@ class LocalGeneralWidgets(QtWidgets.QWidget):
         username_input = QtWidgets.QLineEdit(self)
         username_input.setPlaceholderText(getpass.getuser())
 
+        is_admin_input = QtWidgets.QCheckBox(self)
+
         layout = QtWidgets.QFormLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
 
         layout.addRow("OpenPype Username", username_input)
+        layout.addRow("Admin permissions", is_admin_input)
+
+        is_admin_input.stateChanged.connect(self._on_admin_check_change)
 
         self.username_input = username_input
+        self.is_admin_input = is_admin_input
 
     def update_local_settings(self, value):
         username = ""
+        is_admin = False
         if value:
             username = value.get("username", username)
+            is_admin = value.get("is_admin", is_admin)
+
         self.username_input.setText(username)
+
+        if self.is_admin_input.isChecked() != is_admin:
+            self.is_admin_input.setChecked(is_admin)
+
+    def _on_admin_check_change(self):
+        self.is_admin_input.setChecked(False)
 
     def settings_value(self):
         # Add changed
