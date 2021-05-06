26c1acf250ce6789a8e1b8dbba69c3532c1bfeec
Jakub Jezek <jakub@orbi.tools>
2021-05-06 17:18:16 +0200

    resolve: reviewTrack was duplicated to all tags

--- openpype/hosts/resolve/api/plugin.py
+++ openpype/hosts/resolve/api/plugin.py
@@ -783,6 +783,8 @@ class PublishClip:
         # add review track only to hero track
         if hero_track and self.review_layer:
             self.tag_data.update({"reviewTrack": self.review_layer})
+        else:
+            self.tag_data.update({"reviewTrack": None})
 
 
     def _solve_tag_hierarchy_data(self, hierarchy_formating_data):
@@ -819,7 +821,7 @@ class PublishClip:
 
     def _create_parents(self):
         """ Create parents and return it in list. """
-        self.parents = list()
+        self.parents = []
 
         patern = re.compile(self.parents_search_patern)
         par_split = [patern.findall(t).pop()
