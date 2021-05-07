70ed7abeb5d4201965570bbc88ee53e68587c4d2
iLLiCiTiT <jakub.trllo@gmail.com>
2021-05-07 15:13:50 +0200

    added projectmanager to openpype cli commands

---
+++ tools/run_project_manager.ps1
@@ -0,0 +1,18 @@
+<#
+.SYNOPSIS
+  Helper script OpenPype Tray.
+
+.DESCRIPTION
+
+
+.EXAMPLE
+
+PS> .\run_tray.ps1
+
+#>
+$current_dir = Get-Location
+$script_dir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$openpype_root = (Get-Item $script_dir).parent.FullName
+Set-Location -Path $openpype_root
+& poetry run python "$($openpype_root)\start.py" projectmanager
+Set-Location -Path $current_dir
