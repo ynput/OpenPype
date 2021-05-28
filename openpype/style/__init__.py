dabce348931cc87ff600ebe437b1ad2f2616d107
iLLiCiTiT <jakub.trllo@gmail.com>
2021-05-28 21:46:44 +0200

    added Poppins font into style

--- openpype/style/__init__.py
+++ openpype/style/__init__.py
@@ -5,6 +5,9 @@ from openpype import resources
 
 
 _STYLESHEET_CACHE = None
+_FONT_IDS = None
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def _load_stylesheet():
@@ -12,7 +15,6 @@ def _load_stylesheet():
 
     qrc_resources.qInitResources()
 
-    current_dir = os.path.dirname(os.path.abspath(__file__))
     style_path = os.path.join(current_dir, "style.css")
     with open(style_path, "r") as style_file:
         stylesheet = style_file.read()
@@ -41,10 +43,48 @@ def _load_stylesheet():
     return stylesheet
 
 
+def _load_font():
+    from Qt import QtGui
+
+    global _FONT_IDS
+
+    # Check if font ids are still loaded
+    if _FONT_IDS is not None:
+        for font_id in tuple(_FONT_IDS):
+            font_families = QtGui.QFontDatabase.applicationFontFamilies(
+                font_id
+            )
+            # Reset font if font id is not available
+            if not font_families:
+                _FONT_IDS = None
+                break
+
+    if _FONT_IDS is None:
+        _FONT_IDS = []
+        fonts_dirpath = os.path.join(current_dir, "fonts")
+        font_dirs = []
+        font_dirs.append(os.path.join(fonts_dirpath, "Poppins"))
+
+        loaded_fonts = []
+        for font_dir in font_dirs:
+            for filename in os.listdir(font_dir):
+                if os.path.splitext(filename)[1] != ".ttf":
+                    continue
+                full_path = os.path.join(font_dir, filename)
+                font_id = QtGui.QFontDatabase.addApplicationFont(full_path)
+                _FONT_IDS.append(font_id)
+                font_families = QtGui.QFontDatabase.applicationFontFamilies(
+                    font_id
+                )
+                loaded_fonts.extend(font_families)
+        print("Registered font families: {}".format(", ".join(loaded_fonts)))
+
+
 def load_stylesheet():
     global _STYLESHEET_CACHE
     if _STYLESHEET_CACHE is None:
         _STYLESHEET_CACHE = _load_stylesheet()
+    _load_font()
     return _STYLESHEET_CACHE
 
 
