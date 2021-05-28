148412f0484b7d84e9cba971e36f1b31caed3c1c
iLLiCiTiT <jakub.trllo@gmail.com>
2021-05-28 13:24:54 +0200

    replaced style f older in project manager with single file

--- openpype/tools/project_manager/project_manager/style.py
+++ openpype/tools/project_manager/project_manager/style.py
@@ -1,11 +1,9 @@
-import os
-from openpype import resources
 from avalon.vendor import qtawesome
 
 
 class ResourceCache:
     colors = {
-        "standard": "#333333",
+        "standard": "#bfccd6",
         "new": "#2d9a4c",
         "warning": "#c83232"
     }
@@ -68,31 +66,3 @@ class ResourceCache:
     @classmethod
     def get_color(cls, color_name):
         return cls.colors[color_name]
-
-    @classmethod
-    def style_fill_data(cls):
-        output = {}
-        for color_name, color_value in cls.colors.items():
-            key = "color:{}".format(color_name)
-            output[key] = color_value
-        return output
-
-
-def load_stylesheet():
-    from . import qrc_resources
-
-    qrc_resources.qInitResources()
-
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    style_path = os.path.join(current_dir, "style.css")
-    with open(style_path, "r") as style_file:
-        stylesheet = style_file.read()
-
-    for key, value in ResourceCache.style_fill_data().items():
-        replacement_key = "{" + key + "}"
-        stylesheet = stylesheet.replace(replacement_key, value)
-    return stylesheet
-
-
-def app_icon_path():
-    return resources.pype_icon_filepath()
