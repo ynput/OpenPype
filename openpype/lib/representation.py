b2589592e816c07ef8dd9486f53bd9a6daef820d
Ondrej Samohel <ondrej.samohel@orbi.tools>
2021-06-03 16:00:36 +0200

    wip

--- openpype/lib/representation.py
+++ openpype/lib/representation.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
 """Class for handling representation data."""
 import attr
+import sys
+if sys.version_info >= (3, 6):
+    from enum import Enum, unique, auto
+else:
+    enum = None
+
+
+if enum:
+    @unique
+    class Tags(Enum):
+        CLEAN_NAME = auto()
+        DELETE = auto()
+        SLATE_FRAME = auto()
+        NO_HANDLES = auto()
+        BURNIN = auto()
+        FTRACK_REVIEW = auto()
+        REVIEW = auto()
+        THUMBNAIL = auto()
+        TO_SCANLINE = auto()
 
 
 @attr.s
@@ -13,10 +32,11 @@ class Representation(object):
     frameStart = attr.ib()
     frameEnd = attr.ib()
     stagingDir = attr.ib()
-    fps = attr.ib()
-    tags = attr.ib()
+    fps = attr.ib(default=None)
+    tags = attr.ib(default=attr.Factory(set))
 
     _id = attr.ib(default=None)
+
 
     def __init__(self):
         ...
