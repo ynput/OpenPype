008dc3b7d05454c647b8a1d12cb95e3e0a8822eb
iLLiCiTiT <jakub.trllo@gmail.com>
2021-06-10 18:28:25 +0200

    fixed on->from

--- openpype/tools/project_manager/project_manager/model.py
+++ openpype/tools/project_manager/project_manager/model.py
@@ -589,9 +589,9 @@ class HierarchyModel(QtCore.QAbstractItemModel):
         return None
 
     def remove_delete_flag(self, item_ids, with_children=True):
-        """Remove deletion flag on items with matching ids.
+        """Remove deletion flag from items with matching ids.
 
-        The flag is also removed on all parents of passed children as it
+        The flag is also removed from all parents of passed children as it
         wouldn't make sense to not to do so.
 
         Children of passed item ids are by default also unset for deletion.
