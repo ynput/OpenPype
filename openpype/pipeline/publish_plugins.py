814889203b0692e2db0d2697cf6a658ca61d190f
iLLiCiTiT <jakub.trllo@gmail.com>
2021-06-15 15:15:46 +0200

    added base of set state implementation

--- openpype/pipeline/publish_plugins.py
+++ openpype/pipeline/publish_plugins.py
@@ -10,6 +10,20 @@ log = Logger.get_logger(__name__)
 class OpenPypePyblishPluginMixin:
     executable_in_thread = False
 
+    state_message = None
+    state_percent = None
+    _state_change_callbacks = []
+
     @classmethod
     def get_family_attribute_defs(cls, family):
         return None
+
+    def set_state(self, percent=None, message=None):
+        if percent is not None:
+            self.state_percent = percent
+
+        if message:
+            self.state_message = message
+
+        for callback in self._state_change_callbacks:
+            callback(self)
