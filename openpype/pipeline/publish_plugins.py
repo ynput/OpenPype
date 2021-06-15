b00a9fa9009ce9ce15ee1a690849d9dbec6220f8
iLLiCiTiT <jakub.trllo@gmail.com>
2021-06-15 15:13:54 +0200

    Initial commit of pyblish plugin mixin

--- openpype/pipeline/publish_plugins.py
+++ openpype/pipeline/publish_plugins.py
@@ -5,3 +5,10 @@ from openpype.api import (
 import pyblish.api
 
 log = Logger.get_logger(__name__)
+
+
+class OpenPypePyblishPluginMixin:
+
+    @classmethod
+    def get_family_attribute_defs(cls, family):
+        return None
