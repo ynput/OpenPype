3c2253810a5dbb98bbea1b1f3e54effa9dae680e
Petr Kalis <petr.kalis@gmail.com>
2021-06-15 11:29:37 +0200

    #680 - added warning if no profiles configured, changed if order

--- pype/plugins/ftrack/publish/collect_ftrack_family.py
+++ pype/plugins/ftrack/publish/collect_ftrack_family.py
@@ -31,27 +31,30 @@ class CollectFtrackFamily(pyblish.api.InstancePlugin):
     }
 
     def process(self, instance):
-        if self.profiles:
-            anatomy_data = instance.context.data["anatomyData"]
-            task_name = instance.data("task",
-                                      instance.context.data["task"])
-            host_name = anatomy_data.get("app",
-                                         os.environ["AVALON_APP"])
-            family = instance.data["family"]
+        if not self.profiles:
+            self.log.warning("No profiles present for adding Ftrack family")
+            return
 
-            filtering_criteria = {
-                "hosts": host_name,
-                "families": family,
-                "tasks": task_name
-            }
-            profile = filter_profiles(self.profiles, filtering_criteria)
+        anatomy_data = instance.context.data["anatomyData"]
+        task_name = instance.data("task",
+                                  instance.context.data["task"])
+        host_name = anatomy_data.get("app",
+                                     os.environ["AVALON_APP"])
+        family = instance.data["family"]
 
-            if profile and profile["add_ftrack_family"]:
-                self.log.debug("Adding ftrack family")
-                families = instance.data.get("families")
-                if families and "ftrack" not in families:
-                    instance.data["families"].append("ftrack")
-                else:
-                    instance.data["families"] = ["ftrack"]
+        filtering_criteria = {
+            "hosts": host_name,
+            "families": family,
+            "tasks": task_name
+        }
+        profile = filter_profiles(self.profiles, filtering_criteria)
+
+        if profile and profile["add_ftrack_family"]:
+            self.log.debug("Adding ftrack family")
+            families = instance.data.get("families")
+            if families and "ftrack" not in families:
+                instance.data["families"].append("ftrack")
+            else:
+                instance.data["families"] = ["ftrack"]
 
         self.log.debug("instance.data:: {}".format(instance.data))
