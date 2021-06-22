b2c5f4c10473cdea9d69fbcf7e103e81cbe6fdae
Ondrej Samohel <ondrej.samohel@orbi.tools>
2021-06-22 17:53:09 +0200

    remove PyQt5 and add PySide2

--- tools/fetch_thirdparty_libs.ps1
+++ tools/fetch_thirdparty_libs.ps1
@@ -33,6 +33,9 @@ if (-not (Test-Path -PathType Container -Path "$($env:POETRY_HOME)\bin")) {
 } else {
     Write-Host "OK" -ForegroundColor Green
 }
+Write-Host ">>> " -NoNewline -ForegroundColor Green
+Write-Host "Installing PySide2 ... "
+& "$($env:POETRY_HOME)\bin\poetry.bat" run python -m pip install PySide2 -t "$($openpype_root)\vendor\python\PySide2"
 
 & "$($env:POETRY_HOME)\bin\poetry" run python "$($openpype_root)\tools\fetch_thirdparty_libs.py"
 Set-Location -Path $current_dir
