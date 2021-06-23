578ed02a08314844168d63f906b7858d6f247f7d
iLLiCiTiT <jakub.trllo@gmail.com>
2021-06-23 12:11:17 +0200

    added comment

--- openpype/modules/log_viewer/tray/widgets.py
+++ openpype/modules/log_viewer/tray/widgets.py
@@ -76,6 +76,9 @@ class CustomCombo(QtWidgets.QWidget):
 
         toolbutton.setMenu(toolmenu)
         toolbutton.setPopupMode(QtWidgets.QToolButton.MenuButtonPopup)
+
+        # Fake popupMenu property as PySide2 does not store it's value as
+        #   integer but as enum object
         toolbutton.setProperty("popup_mode", "1")
 
         layout = QtWidgets.QHBoxLayout(self)
