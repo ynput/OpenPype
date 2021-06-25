041dab589931e5fe09120d833a0bc2f065627f6d
Petr Kalis <petr.kalis@gmail.com>
2021-06-25 18:18:55 +0200

    client#115 - added extractor to store published workfile path
    Pre calculates where workfile will be published and stores this to all texture instances for linking them to workfile.

---
+++ pype/plugins/standalonepublisher/publish/extract_workfile_location.py
@@ -0,0 +1,41 @@
+import os
+import pyblish.api
+
+
+class ExtractWorkfileUrl(pyblish.api.ContextPlugin):
+    """
+        Modifies 'workfile' field to contain link to published workfile.
+
+        Expects that batch contains only single workfile and matching
+        (multiple) textures.
+    """
+
+    label = "Extract Workfile Url SP"
+    hosts = ["standalonepublisher"]
+    order = pyblish.api.ExtractorOrder
+
+    families = ["textures"]
+
+    def process(self, context):
+        filepath = None
+
+        # first loop for workfile
+        for instance in context:
+            if instance.data["family"] == 'workfile':
+                anatomy = context.data['anatomy']
+                template_data = instance.data.get("anatomyData")
+                rep_name = instance.data.get("representations")[0].get("name")
+                template_data["representation"] = rep_name
+                anatomy_filled = anatomy.format(template_data)
+                template_filled = anatomy_filled["publish"]["path"]
+                filepath = os.path.normpath(template_filled)
+                self.log.info("Using published scene for render {}".format(
+                    filepath))
+
+        if not filepath:
+            raise ValueError("Texture batch doesn't contain workfile.")
+
+        # then apply to all textures
+        for instance in context:
+            if instance.data["family"] == 'textures':
+                instance.data["versionData"]["workfile"] = filepath
