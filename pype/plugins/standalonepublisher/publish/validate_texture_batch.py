da91205e995697255a651d2ce088a0e6be08d27a
Petr Kalis <petr.kalis@gmail.com>
2021-06-28 14:41:02 +0200

    client#115 - added shader support

--- pype/plugins/standalonepublisher/publish/validate_texture_batch.py
+++ pype/plugins/standalonepublisher/publish/validate_texture_batch.py
@@ -9,6 +9,7 @@ class ValidateTextureBatch(pyblish.api.ContextPlugin):
             some textures are present
             workfile has resource files (optional)
             texture version matches to workfile version
+            if texture name was parsed correctly
     """
 
     label = "Validate Texture Batch"
@@ -30,6 +31,10 @@ class ValidateTextureBatch(pyblish.api.ContextPlugin):
                     self.log.warning(msg)
 
             if instance.data["family"] == "textures":
+                file_name = instance.data["representations"][0]["files"][0]
+                self._check_proper_collected(instance.data["versionData"],
+                                             file_name)
+
                 wfile = instance.data["versionData"]["workfile"]
                 workfiles_in_textures.append(wfile)
 
@@ -45,3 +50,24 @@ class ValidateTextureBatch(pyblish.api.ContextPlugin):
               "\nCheck that both workfile and textures are present"
         keys = set(workfiles) == set(workfiles_in_textures)
         assert keys, msg
+
+    def _check_proper_collected(self, versionData, file_name):
+        """
+            Loop through collected versionData to check if name parsing was OK.
+        Args:
+            versionData: (dict)
+
+        Returns:
+            raises AssertionException
+        """
+        missing_key_values = []
+        for key, value in versionData.items():
+            if not value:
+                missing_key_values.append(key)
+
+        msg = "Collected data {} doesn't contain values for {}".format(
+            versionData, missing_key_values) + "\n" + \
+            "Name of the texture file doesn't match expected pattern.\n" + \
+            "Please rename file(s) {}".format(file_name)
+
+        assert not missing_key_values, msg
