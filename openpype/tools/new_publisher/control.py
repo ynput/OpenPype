4dfc0b7fa7d5609e8cba973e764c855f8694a4f9
iLLiCiTiT <jakub.trllo@gmail.com>
2021-06-30 20:27:26 +0200

    store creators by family

--- openpype/tools/new_publisher/control.py
+++ openpype/tools/new_publisher/control.py
@@ -10,7 +10,7 @@ class PublisherController:
         self.log = logging.getLogger("PublisherController")
         self.host = avalon.api.registered_host()
 
-        self.creators = []
+        self.creators = {}
         self.publish_plugins = []
         self.instances = []
 
@@ -26,17 +26,17 @@ class PublisherController:
 
     def _reset(self):
         """Reset to initial state."""
-        creators = []
         publish_plugins = pyblish.api.discover()
         self.publish_plugins = publish_plugins
 
+        creators = {}
         for creator in avalon.api.discover(BaseCreator):
             if inspect.isabstract(creator):
                 self.log.info(
                     "Skipping abstract Creator {}".format(str(creator))
                 )
                 continue
-            creators.append(creator)
+            creators[creator.family] = creator
 
         self.creators = creators
         self.publish_plugins = []
