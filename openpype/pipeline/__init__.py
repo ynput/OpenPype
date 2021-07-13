4e6e6e2cbc7670e56c50451c7b8b22f3245b179e
iLLiCiTiT <jakub.trllo@gmail.com>
2021-07-13 12:56:10 +0200

    added few import to init

--- openpype/pipeline/__init__.py
+++ openpype/pipeline/__init__.py
@@ -1,15 +1,22 @@
+from .lib import attribute_definitions
+
 from .creator_plugins import (
     BaseCreator,
     Creator,
-    AutoCreator
+    AutoCreator,
+    AvalonInstance
 )
 
 from .publish_plugins import OpenPypePyblishPluginMixin
 
+
 __all__ = (
+    "attribute_definitions",
+
     "BaseCreator",
     "Creator",
     "AutoCreator",
+    "AvalonInstance",
 
     "OpenPypePyblishPluginMixin"
 )
