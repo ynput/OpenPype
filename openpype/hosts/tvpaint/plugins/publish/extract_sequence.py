bdd065a840418410132c8f12a71a17466946260a
iLLiCiTiT <jakub.trllo@gmail.com>
2021-07-21 17:53:14 +0200

    use scene start frame as an offset

--- openpype/hosts/tvpaint/plugins/publish/extract_sequence.py
+++ openpype/hosts/tvpaint/plugins/publish/extract_sequence.py
@@ -49,6 +49,14 @@ class ExtractSequence(pyblish.api.Extractor):
         family_lowered = instance.data["family"].lower()
         mark_in = instance.context.data["sceneMarkIn"]
         mark_out = instance.context.data["sceneMarkOut"]
+
+        # Scene start frame offsets the output files, so we need to offset the
+        # marks.
+        scene_start_frame = instance.context.data["sceneStartFrame"]
+        difference = scene_start_frame - mark_in
+        mark_in += difference
+        mark_out += difference
+
         # Frame start/end may be stored as float
         frame_start = int(instance.data["frameStart"])
         frame_end = int(instance.data["frameEnd"])
