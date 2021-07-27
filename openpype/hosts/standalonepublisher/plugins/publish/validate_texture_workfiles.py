c2ffeb89538dc0fe845a98e78ec7ad34858ff7ce
Petr Kalis <petr.kalis@gmail.com>
2021-07-27 11:30:12 +0200

    Textures publishing - tweaked validator
    Look for resources (secondary workfiles) only for main workfile.

--- openpype/hosts/standalonepublisher/plugins/publish/validate_texture_workfiles.py
+++ openpype/hosts/standalonepublisher/plugins/publish/validate_texture_workfiles.py
@@ -14,8 +14,16 @@ class ValidateTextureBatchWorkfiles(pyblish.api.InstancePlugin):
     families = ["workfile"]
     optional = True
 
+    # from presets
+    main_workfile_extensions = ['mra']
+
     def process(self, instance):
         if instance.data["family"] == "workfile":
-            msg = "No resources for workfile {}".\
+            ext = instance.data["representations"][0]["ext"]
+            if ext not in self.main_workfile_extensions:
+                self.log.warning("Only secondary workfile present!")
+                return
+
+            msg = "No secondary workfiles present for workfile {}".\
                 format(instance.data["name"])
             assert instance.data.get("resources"), msg
