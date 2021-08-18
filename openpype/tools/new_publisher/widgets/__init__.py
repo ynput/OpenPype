107b8841b53c9f0fa6f8cf2203305904a0c397b8
iLLiCiTiT <jakub.trllo@gmail.com>
2021-08-18 10:54:13 +0200

    added widgets to subfolder

---
+++ openpype/tools/new_publisher/widgets/__init__.py
@@ -0,0 +1,16 @@
+from .widgets import (
+    PublishOverlayFrame,
+    SubsetAttributesWidget,
+    InstanceCardView,
+    InstanceListView,
+    CreateDialog
+)
+
+
+__all__ = (
+    "PublishOverlayFrame",
+    "SubsetAttributesWidget",
+    "InstanceCardView",
+    "InstanceListView",
+    "CreateDialog"
+)
