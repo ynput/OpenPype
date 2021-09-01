19c058eafd3f6dbda94cef6230612ddb3489cf7c
Toke Jepsen <tokejepsen@gmail.com>
2021-09-01 09:06:51 +0200

    Respond with error message
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- openpype/modules/default_modules/timers_manager/rest_api.py
+++ openpype/modules/default_modules/timers_manager/rest_api.py
@@ -63,7 +63,9 @@ class TimersManagerModuleRestApi:
             asset_name = data['asset_name']
             task_name = data['task_name']
         except KeyError:
-            log.error("Payload must contain fields 'project_name, " +
+            message = "Payload must contain fields 'project_name, 'asset_name', 'task_name'"
+            log.warning(message)
+            return Response(text=message, status=404)
                       "'asset_name', 'task_name'")
             return Response(status=400)
 
