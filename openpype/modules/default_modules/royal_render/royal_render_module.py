d96ccac6198c58c076c98cafe266ad2a4fe3b96a
Ondrej Samohel <ondrej.samohel@orbi.tools>
2021-09-08 18:55:25 +0200

    initial wip

---
+++ openpype/modules/default_modules/royal_render/royal_render_module.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Module providing support for Royal Render."""
+import os
+from openpype.modules import OpenPypeModule
+from openpype_interfaces import IPluginPaths
+
+
+class RoyalRenderModule(OpenPypeModule, IPluginPaths):
+    """Class providing basic Royal Render implementation logic."""
+    name = "royalrender"
+
+    def __init__(self, manager, settings):
+        self.rr_paths = {}
+        super(RoyalRenderModule, self).__init__(manager, settings)
+
+    def initialize(self, module_settings):
+        rr_settings = module_settings[self.name]
+        self.enabled = rr_settings["enabled"]
+        self.rr_paths = rr_settings.get("rr_paths")
+
+    @staticmethod
+    def get_plugin_paths(self):
+        """Deadline plugin paths."""
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        return {
+            "publish": [os.path.join(current_dir, "plugins", "publish")]
+        }
