dc653ce73d900d4025176c2ce481183b6ba90d31
iLLiCiTiT <jakub.trllo@gmail.com>
2021-09-09 19:29:47 +0200

    initial card view modification

--- openpype/tools/new_publisher/widgets/card_view_widgets.py
+++ openpype/tools/new_publisher/widgets/card_view_widgets.py
@@ -4,7 +4,8 @@ from openpype.widgets.nice_checkbox import NiceCheckbox
 
 from .widgets import (
     AbstractInstanceView,
-    ContextWarningLabel
+    ContextWarningLabel,
+    ClickableFrame
 )
 from ..constants import (
     INSTANCE_ID_ROLE,
@@ -13,39 +14,88 @@ from ..constants import (
 )
 
 
-class ContextCardWidget(QtWidgets.QWidget):
-    def __init__(self, item, parent):
-        super(ContextCardWidget, self).__init__(parent)
+class FamilyLabel(QtWidgets.QWidget):
+    def __init__(self, family, parent):
+        super(FamilyLabel, self).__init__(parent)
 
-        self.item = item
+        label_widget = QtWidgets.QLabel(family, self)
 
-        subset_name_label = QtWidgets.QLabel(CONTEXT_LABEL, self)
+        line_widget = QtWidgets.QWidget(self)
+        line_widget.setObjectName("Separator")
+        line_widget.setMinimumHeight(1)
+        line_widget.setMaximumHeight(1)
 
         layout = QtWidgets.QHBoxLayout(self)
-        layout.setContentsMargins(2, 2, 2, 2)
-        layout.addWidget(subset_name_label)
-        layout.addStretch(1)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(label_widget, 0)
+        layout.addWidget(line_widget, 1)
 
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        subset_name_label.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-
-        self.subset_name_label = subset_name_label
-
-    def showEvent(self, event):
-        super(ContextCardWidget, self).showEvent(event)
-        self.item.setSizeHint(self.sizeHint())
+        self._label_widget = label_widget
 
 
-class InstanceCardWidget(QtWidgets.QWidget):
+class CardWidget(ClickableFrame):
+    selected = QtCore.Signal(str)
+
+    def __init__(self, parent):
+        super(CardWidget, self).__init__(parent)
+        self.setObjectName("CardViewWidget")
+
+        self._selected = False
+        self._id = None
+
+    def set_selected(self, selected):
+        if selected == self._selected:
+            return
+        self._selected = selected
+        state = "selected" if selected else ""
+        self.setProperty("state", state)
+        self.style().polish(self)
+
+    def _mouse_release_callback(self):
+        self.selected.emit(self._id)
+
+
+class ContextCardWidget(CardWidget):
+    def __init__(self, parent):
+        super(ContextCardWidget, self).__init__(parent)
+
+        self._id = CONTEXT_ID
+
+        icon_widget = QtWidgets.QLabel(self)
+
+        label_widget = QtWidgets.QLabel(CONTEXT_LABEL, self)
+
+        icon_layout = QtWidgets.QHBoxLayout()
+        icon_layout.setContentsMargins(5, 5, 5, 5)
+        icon_layout.addWidget(icon_widget)
+
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.setContentsMargins(0, 5, 10, 5)
+        layout.addLayout(icon_layout, 0)
+        layout.addWidget(label_widget, 1)
+
+        self.icon_widget = icon_widget
+        self.label_widget = label_widget
+
+
+class InstanceCardWidget(CardWidget):
     active_changed = QtCore.Signal(str, bool)
 
-    def __init__(self, instance, item, parent):
+    def __init__(self, instance, parent):
         super(InstanceCardWidget, self).__init__(parent)
 
-        self.instance = instance
-        self.item = item
+        self._id = instance.data["uuid"]
 
-        subset_name_label = QtWidgets.QLabel(instance.data["subset"], self)
+        self.instance = instance
+
+        icon_widget = QtWidgets.QLabel(self)
+
+        icon_layout = QtWidgets.QHBoxLayout()
+        icon_layout.setContentsMargins(5, 5, 5, 5)
+        icon_layout.addWidget(icon_widget)
+
+        label_widget = QtWidgets.QLabel(instance.data["subset"], self)
+
         active_checkbox = NiceCheckbox(parent=self)
         active_checkbox.setChecked(instance.data["active"])
 
@@ -54,6 +104,8 @@ class InstanceCardWidget(QtWidgets.QWidget):
             context_warning.setVisible(False)
 
         expand_btn = QtWidgets.QToolButton(self)
+        # Not yet implemented
+        expand_btn.setVisible(False)
         expand_btn.setObjectName("ArrowBtn")
         expand_btn.setArrowType(QtCore.Qt.DownArrow)
         expand_btn.setMaximumWidth(14)
@@ -64,26 +116,25 @@ class InstanceCardWidget(QtWidgets.QWidget):
         self.detail_widget = detail_widget
 
         top_layout = QtWidgets.QHBoxLayout()
-        top_layout.addWidget(subset_name_label)
-        top_layout.addStretch(1)
-        top_layout.addWidget(context_warning)
-        top_layout.addWidget(active_checkbox)
-        top_layout.addWidget(expand_btn)
+        top_layout.addLayout(icon_layout, 0)
+        top_layout.addWidget(label_widget, 1)
+        top_layout.addWidget(context_warning, 0)
+        top_layout.addWidget(active_checkbox, 0)
+        top_layout.addWidget(expand_btn, 0)
 
-        layout = QtWidgets.QVBoxLayout(self)
-        layout.setContentsMargins(2, 2, 2, 2)
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.setContentsMargins(0, 5, 10, 5)
         layout.addLayout(top_layout)
         layout.addWidget(detail_widget)
 
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        subset_name_label.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         active_checkbox.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         expand_btn.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
         active_checkbox.stateChanged.connect(self._on_active_change)
         expand_btn.clicked.connect(self._on_expend_clicked)
 
-        self.subset_name_label = subset_name_label
+        self.icon_widget = icon_widget
+        self.label_widget = label_widget
         self.context_warning = context_warning
         self.active_checkbox = active_checkbox
         self.expand_btn = expand_btn
@@ -112,11 +163,6 @@ class InstanceCardWidget(QtWidgets.QWidget):
         if expanded is None:
             expanded = not self.detail_widget.isVisible()
         self.detail_widget.setVisible(expanded)
-        self.item.setSizeHint(self.sizeHint())
-
-    def showEvent(self, event):
-        super(InstanceCardWidget, self).showEvent(event)
-        self.item.setSizeHint(self.sizeHint())
 
     def _on_active_change(self):
         new_value = self.active_checkbox.isChecked()
@@ -137,27 +183,17 @@ class InstanceCardView(AbstractInstanceView):
 
         self.controller = controller
 
-        list_widget = QtWidgets.QListWidget(self)
-        list_widget.setSelectionMode(
-            QtWidgets.QAbstractItemView.ExtendedSelection
-        )
-        list_widget.setResizeMode(QtWidgets.QListView.Adjust)
-
-        layout = QtWidgets.QHBoxLayout(self)
+        layout = QtWidgets.QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(list_widget, 1)
+        layout.addStretch(1)
 
-        list_widget.selectionModel().selectionChanged.connect(
-            self._on_selection_change
-        )
+        self._content_layout = layout
 
-        self._items_by_id = {}
         self._widgets_by_id = {}
-
+        self._family_widgets_by_name = {}
         self._context_widget = None
-        self._context_item = None
 
-        self.list_widget = list_widget
+        self._selected_widget = None
 
     def refresh(self):
         instances_by_id = {}
@@ -165,107 +201,79 @@ class InstanceCardView(AbstractInstanceView):
             instance_id = instance.data["uuid"]
             instances_by_id[instance_id] = instance
 
-        if not self._context_item:
-            item = QtWidgets.QListWidgetItem(self.list_widget)
-            item.setData(INSTANCE_ID_ROLE, CONTEXT_ID)
-
-            widget = ContextCardWidget(item, self)
-            self.list_widget.addItem(item)
-            self.list_widget.setItemWidget(item, widget)
-
-            self._context_item = item
+        if not self._context_widget:
+            widget = ContextCardWidget(self)
+            widget.selected.connect(self._on_widget_selection)
+            widget.set_selected(True)
+            self.selection_changed.emit()
+            self._content_layout.insertWidget(0, widget)
             self._context_widget = widget
+            self._selected_widget = widget
 
-        for instance_id in tuple(self._items_by_id.keys()):
+        for instance_id in tuple(self._widgets_by_id.keys()):
             if instance_id not in instances_by_id:
-                item = self._items_by_id.pop(instance_id)
-                self.list_widget.removeItemWidget(item)
                 widget = self._widgets_by_id.pop(instance_id)
+                widget.setVisible(False)
+                self._content_widget.removeWidget(widget)
                 widget.deleteLater()
-                row = self.list_widget.row(item)
-                self.list_widget.takeItem(row)
 
         for instance_id, instance in instances_by_id.items():
-            if instance_id in self._items_by_id:
+            if instance_id in self._widgets_by_id:
                 widget = self._widgets_by_id[instance_id]
                 widget.update_instance(instance)
+                continue
 
-            else:
-                item = QtWidgets.QListWidgetItem(self.list_widget)
-                widget = InstanceCardWidget(instance, item, self)
-                widget.active_changed.connect(self._on_active_changed)
-                item.setData(INSTANCE_ID_ROLE, instance_id)
-                self.list_widget.addItem(item)
-                self.list_widget.setItemWidget(item, widget)
-                self._items_by_id[instance_id] = item
-                self._widgets_by_id[instance_id] = widget
+            family = instance.data["family"]
+            if family not in self._family_widgets_by_name:
+                widget = FamilyLabel(family, self)
+                self._family_widgets_by_name[family] = widget
+                pos = self._content_layout.count() - 1
+                self._content_layout.insertWidget(pos, widget)
+
+            widget = InstanceCardWidget(instance, self)
+            widget.selected.connect(self._on_widget_selection)
+            widget.active_changed.connect(self._on_active_changed)
+            self._widgets_by_id[instance_id] = widget
+            pos = self._content_layout.count() - 1
+            self._content_layout.insertWidget(pos, widget)
 
     def refresh_instance_states(self):
         for widget in self._widgets_by_id.values():
             widget.update_instance_values()
 
     def _on_active_changed(self, changed_instance_id, new_value):
-        selected_ids = set()
-        found = False
-        for item in self.list_widget.selectedItems():
-            instance_id = item.data(INSTANCE_ID_ROLE)
-            selected_ids.add(instance_id)
-            if not found and instance_id == changed_instance_id:
-                found = True
-
-        if not found:
-            selected_ids = set()
-            selected_ids.add(changed_instance_id)
-
         changed_ids = set()
-        for instance_id in selected_ids:
-            widget = self._widgets_by_id.get(instance_id)
-            if widget:
-                changed_ids.add(instance_id)
-                widget.set_active(new_value)
-
         if changed_ids:
             self.active_changed.emit(changed_ids)
 
-    def _on_selection_change(self, *_args):
+    def _on_widget_selection(self, widget_id):
+        if widget_id == CONTEXT_ID:
+            new_widget = self._context_widget
+        else:
+            new_widget = self._widgets_by_id.get(widget_id)
+
+        if new_widget is self._selected_widget:
+            return
+
+        if self._selected_widget is not None:
+            self._selected_widget.set_selected(False)
+
+        self._selected_widget = new_widget
+        if new_widget is not None:
+            new_widget.set_selected(True)
+
         self.selection_changed.emit()
 
     def get_selected_items(self):
         instances = []
         context_selected = False
-        for item in self.list_widget.selectedItems():
-            instance_id = item.data(INSTANCE_ID_ROLE)
-            if instance_id == CONTEXT_ID:
-                context_selected = True
-            else:
-                widget = self._widgets_by_id.get(instance_id)
-                if widget:
-                    instances.append(widget.instance)
+        if self._selected_widget is self._context_widget:
+            context_selected = True
+
+        elif self._selected_widget is not None:
+            instances.append(self._selected_widget.instance)
 
         return instances, context_selected
 
     def set_selected_items(self, instances, context_selected):
-        indexes = []
-        model = self.list_widget.model()
-        if context_selected and self._context_item is not None:
-            row = self.list_widget.row(self._context_item)
-            index = model.index(row, 0)
-            indexes.append(index)
-
-        for instance in instances:
-            instance_id = instance.data["uuid"]
-            item = self._items_by_id.get(instance_id)
-            if item:
-                row = self.list_widget.row(item)
-                index = model.index(row, 0)
-                indexes.append(index)
-
-        selection_model = self.list_widget.selectionModel()
-        first_item = True
-        for index in indexes:
-            if first_item:
-                first_item = False
-                select_type = QtCore.QItemSelectionModel.ClearAndSelect
-            else:
-                select_type = QtCore.QItemSelectionModel.Select
-            selection_model.select(index, select_type)
+        pass
