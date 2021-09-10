65ac5bee7ec061fdcb0f285774ff0a29d9868521
iLLiCiTiT <jakub.trllo@gmail.com>
2021-09-10 18:15:12 +0200

    enhanced testhost creators

--- openpype/hosts/testhost/plugins/create/test_creator_1.py
+++ openpype/hosts/testhost/plugins/create/test_creator_1.py
@@ -1,3 +1,4 @@
+from openpype import resources
 from openpype.hosts.testhost import api
 from openpype.pipeline import (
     Creator,
@@ -8,6 +9,10 @@ from openpype.pipeline import (
 
 class TestCreatorOne(Creator):
     family = "test_one"
+    description = "Testing creator of testhost"
+
+    def get_icon(self):
+        return resources.get_openpype_splash_filepath()
 
     def create(self, subset_name, data, options=None):
         avalon_instance = CreatedInstance(self.family, subset_name, data, self)
@@ -27,3 +32,20 @@ class TestCreatorOne(Creator):
             lib.NumberDef("number_key", label="Number")
         ]
         return output
+
+    def get_detail_description(self):
+        return """# Relictus funes est Nyseides currusque nunc oblita
+
+## Causa sed
+
+Lorem markdownum posito consumptis, *plebe Amorque*, abstitimus rogatus fictaque
+gladium Circe, nos? Bos aeternum quae. Utque me, si aliquem cladis, et vestigia
+arbor, sic mea ferre lacrimae agantur prospiciens hactenus. Amanti dentes pete,
+vos quid laudemque rastrorumque terras in gratantibus **radix** erat cedemus?
+
+Pudor tu ponderibus verbaque illa; ire ergo iam Venus patris certe longae
+cruentum lecta, et quaeque. Sit doce nox. Anteit ad tempora magni plenaque et
+videres mersit sibique auctor in tendunt mittit cunctos ventisque gravitate
+volucris quemquam Aeneaden. Pectore Mensis somnus; pectora
+[ferunt](http://www.mox.org/oculosbracchia)? Fertilitatis bella dulce et suum?
+        """
--- openpype/hosts/testhost/plugins/create/test_creator_2.py
+++ openpype/hosts/testhost/plugins/create/test_creator_2.py
@@ -8,6 +8,10 @@ from openpype.pipeline import (
 
 class TestCreatorTwo(Creator):
     family = "test_two"
+    description = "A second testing creator"
+
+    def get_icon(self):
+        return "cube"
 
     def create(self, subset_name, data, options=None):
         avalon_instance = CreatedInstance(self.family, subset_name, data, self)
@@ -21,3 +25,31 @@ class TestCreatorTwo(Creator):
             lib.TextDef("text_key")
         ]
         return output
+
+    def get_detail_description(self):
+        return """# Lorem ipsum, dolor sit amet. [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)
+
+> A curated list of awesome lorem ipsum generators.
+
+Inspired by the [awesome](https://github.com/sindresorhus/awesome) list thing.
+
+
+## Table of Contents
+
+- [Legend](#legend)
+- [Practical](#briefcase-practical)
+- [Whimsical](#roller_coaster-whimsical)
+    - [Animals](#rabbit-animals)
+    - [Eras](#tophat-eras)
+    - [Famous Individuals](#sunglasses-famous-individuals)
+    - [Music](#microphone-music)
+    - [Food and Drink](#pizza-food-and-drink)
+    - [Geographic and Dialects](#earth_africa-geographic-and-dialects)
+    - [Literature](#books-literature)
+    - [Miscellaneous](#cyclone-miscellaneous)
+    - [Sports and Fitness](#bicyclist-sports-and-fitness)
+    - [TV and Film](#movie_camera-tv-and-film)
+- [Tools, Apps, and Extensions](#wrench-tools-apps-and-extensions)
+- [Contribute](#contribute)
+- [TODO](#todo)
+"""
