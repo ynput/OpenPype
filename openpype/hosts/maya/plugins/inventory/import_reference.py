0adcf3334ad5a119341132c777540a22a5f51f30
Petr Kalis <petr.kalis@gmail.com>
2021-09-13 13:35:24 +0200

    Added new InventoryAction to import (localize) reference in Maya
    PYPE-1399

---
+++ openpype/hosts/maya/plugins/inventory/import_reference.py
@@ -0,0 +1,27 @@
+from maya import cmds
+
+from avalon import api
+
+
+class ImportReference(api.InventoryAction):
+    """Imports selected reference inside the file."""
+
+    label = "Import Reference"
+    icon = "mouse-pointer"
+    color = "#d8d8d8"
+
+    def process(self, containers):
+        references = cmds.ls(type="reference")
+
+        for container in containers:
+            if container["loader"] != "ReferenceLoader":
+                print("Not a reference, skipping")
+                continue
+
+            reference_name = container["namespace"] + "RN"
+            if reference_name in references:
+                print("Importing {}".format(reference_name))
+
+                ref_file = cmds.referenceQuery(reference_name, f=True)
+
+                cmds.file(ref_file, importReference=True)
