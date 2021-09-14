9aaceef4110253d1032726702df048739b99781b
Ondrej Samohel <ondrej.samohel@orbi.tools>
2021-09-15 00:03:21 +0200

    wip on api

--- openpype/modules/default_modules/royal_render/api.py
+++ openpype/modules/default_modules/royal_render/api.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
+"""Wrapper around Royal Render API."""
 from .rr_job import RRJob, SubmitFile
 
 
 class Api:
 
-    def create_submission(self, jobs, submitter_attributes):
-        """"""
+    def create_submission(self, jobs, submitter_attributes, file_name=None):
+        # type: (list, list, str) -> SubmitFile
+        """Create jobs submission file.
+
+        Args:
+            jobs (list): List of :class:`RRJob`
+            submitter_attributes (list): List of submitter attributes
+                :class:`SubmitterParameter` for whole submission batch.
+            file_name (str), optional): File path to write data to.
+
+        Returns:
+            str: XML data of job submission files.
+
+        """
         raise NotImplementedError
 
-    def add_job(self, job):
+    def send_job_file(self, submit_file):
+        # type: (str) -> None
         ...
