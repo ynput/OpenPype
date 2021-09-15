2647b3fb1fcf351d7e748889cd4618c8edf763b1
iLLiCiTiT <jakub.trllo@gmail.com>
2021-09-15 11:32:34 +0200

    added example of connector to timers manager

--- openpype/modules/default_modules/timers_manager/timers_manager.py
+++ openpype/modules/default_modules/timers_manager/timers_manager.py
@@ -10,14 +10,78 @@ from openpype_interfaces import (
 from avalon.api import AvalonMongoDB
 
 
+class ExampleTimersManagerConnector:
+    """Timers manager can handle timers of multiple modules/addons.
+
+    Module must have object under `timers_manager_connector` attribute with
+    few methods. This is example class of the object that could be stored under
+    module.
+
+    Required methods are 'stop_timer' and 'start_timer'.
+
+    # TODO pass asset document instead of `hierarchy`
+    Example of `data` that are passed during changing timer:
+    ```
+    data = {
+        "project_name": project_name,
+        "task_name": task_name,
+        "task_type": task_type,
+        "hierarchy": hierarchy
+    }
+    ```
+    """
+    # Not needed at all
+    def __init__(self, module):
+        # Store timer manager module to be able call it's methods when needed
+        self._timers_manager_module = None
+
+        # Store module which want to use timers manager to have access
+        self._module = module
+
+    # Required
+    def stop_timer(self):
+        """Called by timers manager when module should stop timer."""
+        self._module.stop_timer()
+
+    # Required
+    def start_timer(self, data):
+        """Method called by timers manager when should start timer."""
+        self._module.start_timer(data)
+
+    # Optional
+    def register_timers_manager(self, timer_manager_module):
+        """Method called by timers manager where it's object is passed.
+
+        This is moment when timers manager module can be store to be able
+        call it's callbacks (e.g. timer started).
+        """
+        self._timers_manager_module = timer_manager_module
+
+    # Custom implementation
+    def timer_started(self, data):
+        """This is example of possibility to trigger callbacks on manager."""
+        if self._timers_manager_module is not None:
+            self._timers_manager_module.timer_started(self._module.id, data)
+
+    # Custom implementation
+    def timer_stopped(self):
+        if self._timers_manager_module is not None:
+            self._timers_manager_module.timer_stopped(self._module.id)
+
+
 class TimersManager(
     OpenPypeModule, ITrayService, IIdleManager, IWebServerRoutes
 ):
     """ Handles about Timers.
 
     Should be able to start/stop all timers at once.
-    If IdleManager is imported then is able to handle about stop timers
-        when user idles for a long time (set in presets).
+
+    To be able use this advantage module has to have attribute with name
+    `timers_manager_connector` which has two methods 'stop_timer'
+    and 'start_timer'. Optionally may have `register_timers_manager` where
+    object of TimersManager module is passed to be able call it's callbacks.
+
+    See `ExampleTimersManagerConnector`.
     """
     name = "timers_manager"
     label = "Timers Service"
