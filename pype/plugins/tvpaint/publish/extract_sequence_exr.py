1f1410479696f44fd2f0b6d187f7c7d19d986aa8
Toke Stuart Jepsen <tokejepsen@gmail.com>
2021-09-20 17:28:50 +0100

    TVPaint EXR extractor.

---
+++ pype/plugins/tvpaint/publish/extract_sequence_exr.py
@@ -0,0 +1,65 @@
+import os
+import json
+
+import pyblish.api
+import pype.api
+
+
+class ExtractSequenceEXR(pyblish.api.InstancePlugin):
+
+    # Offset to get after ExtractSequence plugin.
+    order = pyblish.api.ExtractorOrder + 0.1
+    label = "Extract Sequence EXR"
+    hosts = ["tvpaint"]
+    families = ["renderPass", "renderLayer"]
+    active = False
+
+    def process(self, instance):
+        ignore_names = ["thumbnail"]
+        ignore_extensions = ["mp4"]
+        old_representations = instance.data["representations"]
+        new_representations = []
+        for count, representation in enumerate(old_representations):
+            if representation["name"] in ignore_names:
+                continue
+
+            if representation["ext"] in ignore_extensions:
+                continue
+
+            self.log.info(
+                "Processeing representation: {}".format(
+                    json.dumps(representation, sort_keys=True, indent=4)
+                )
+            )
+
+            files = []
+            for f in representation["files"]:
+                f = os.path.join(representation["stagingDir"], f)
+                path, ext = os.path.splitext(f)
+                output_path = path + ".exr"
+                args = [
+                    "oiiotool", f,
+                    "--compression", "DWAA",
+                    "-o", output_path
+                ]
+                pype.api.subprocess(args)
+                files.append(output_path)
+
+            new_representations.append(
+                {
+                    "name": "exr",
+                    "ext": "exr",
+                    "files": files,
+                    "stagingDir": representation["stagingDir"],
+                    "tags": representation["tags"]
+                }
+            )
+
+            instance.data["representations"].remove(representation)
+
+        instance.data["representations"].extend(new_representations)
+        self.log.info(
+            json.dumps(
+                instance.data["representations"], sort_keys=True, indent=4
+            )
+        )
