8cd3821ee4e000863bc7337dda5e486bea6103cd
Jakub Jezek <jakub@orbi.tools>
2021-09-21 18:26:45 +0200

    hound: suggestion

--- openpype/hosts/hiero/plugins/publish/precollect_workfile.py
+++ openpype/hosts/hiero/plugins/publish/precollect_workfile.py
@@ -7,7 +7,6 @@ from pprint import pformat
 from openpype.hosts.hiero.otio import hiero_export
 from Qt.QtGui import QPixmap
 import tempfile
-reload(hiero_export)
 
 
 class PrecollectWorkfile(pyblish.api.ContextPlugin):
