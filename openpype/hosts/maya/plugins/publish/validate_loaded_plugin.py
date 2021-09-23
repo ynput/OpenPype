d4827bcc7aecadbce4c8430cf379456e28f01afc
clement.hector <c.hector@ellipsanime.fr>
2021-09-23 11:10:47 +0200

    add validator

---
+++ openpype/hosts/maya/plugins/publish/validate_loaded_plugin.py
@@ -0,0 +1,39 @@
+import pyblish.api
+import maya.cmds as cmds
+import openpype.api
+from openpype import lib
+
+
+class ValidateLoadedPlugin(pyblish.api.ContextPlugin):
+    """Ensure there are no unauthorized loaded plugins"""
+
+    label = "Loaded Plugin"
+    order = pyblish.api.ValidatorOrder
+    host = ["maya"]
+    actions = [openpype.api.RepairContextAction]
+
+    @classmethod
+    def get_invalid(cls, context):
+
+        invalid = []
+
+        for plugin in context.data.get("loadedPlugins"):
+            if plugin not in cls.authorized_plugins:
+                invalid.append(plugin)
+
+        return invalid
+
+    def process(self, context):
+
+        invalid = self.get_invalid(context)
+        if invalid:
+            raise RuntimeError(
+                "Found forbidden plugin name: {}".format(", ".join(invalid))
+            )
+
+    @classmethod
+    def repair(cls, context):
+        """Unload forbidden plugins"""
+
+        for plugin in cls.get_invalid(context):
+            cmds.unloadPlugin(plugin, force=True)
