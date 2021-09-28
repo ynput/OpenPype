2cd450fb10fcaf7478a6d2fc5e37ad8544d74681
iLLiCiTiT <jakub.trllo@gmail.com>
2021-09-28 13:10:41 +0200

    removed unused methods

--- openpype/modules/default_modules/timers_manager/timers_manager.py
+++ openpype/modules/default_modules/timers_manager/timers_manager.py
@@ -253,65 +253,6 @@ class TimersManager(OpenPypeModule, ITrayService, IIdleManager):
                         " for connector of module \"{}\"."
                     ).format(module.name))
 
-    def callbacks_by_idle_time(self):
-        """Implementation of IIdleManager interface."""
-        # Time when message is shown
-        if not self.auto_stop:
-            return {}
-
-        callbacks = collections.defaultdict(list)
-        callbacks[self.time_show_message].append(lambda: self.time_callback(0))
-
-        # Times when idle is between show widget and stop timers
-        show_to_stop_range = range(
-            self.time_show_message - 1, self.time_stop_timer
-        )
-        for num in show_to_stop_range:
-            callbacks[num].append(lambda: self.time_callback(1))
-
-        # Times when widget is already shown and user restart idle
-        shown_and_moved_range = range(
-            self.time_stop_timer - self.time_show_message
-        )
-        for num in shown_and_moved_range:
-            callbacks[num].append(lambda: self.time_callback(1))
-
-        # Time when timers are stopped
-        callbacks[self.time_stop_timer].append(lambda: self.time_callback(2))
-
-        return callbacks
-
-    def time_callback(self, int_def):
-        if not self.signal_handler:
-            return
-
-        if int_def == 0:
-            self.signal_handler.signal_show_message.emit()
-        elif int_def == 1:
-            self.signal_handler.signal_change_label.emit()
-        elif int_def == 2:
-            self.signal_handler.signal_stop_timers.emit()
-
-    def change_label(self):
-        if self.is_running is False:
-            return
-
-        if (
-            not self.idle_manager
-            or self.widget_user_idle.bool_is_showed is False
-        ):
-            return
-
-        if self.idle_manager.idle_time > self.time_show_message:
-            value = self.time_stop_timer - self.idle_manager.idle_time
-        else:
-            value = 1 + (
-                self.time_stop_timer -
-                self.time_show_message -
-                self.idle_manager.idle_time
-            )
-        self.widget_user_idle.change_count_widget(value)
-
     def show_message(self):
         if self.is_running is False:
             return
