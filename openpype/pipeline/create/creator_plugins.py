8eac18e4effb513df9efab9a7580068c9a8ddae8
iLLiCiTiT <jakub.trllo@gmail.com>
2021-10-05 15:56:55 +0200

    removed unused method convert_family_attribute_values

--- openpype/pipeline/create/creator_plugins.py
+++ openpype/pipeline/create/creator_plugins.py
@@ -171,31 +171,6 @@ class BaseCreator:
         """
         return []
 
-    def convert_family_attribute_values(self, attribute_values):
-        """Convert values loaded from workfile metadata.
-
-        If passed values match current creator version just return the value
-        back. Update of changes in workfile must not happen in this method.
-
-        Default implementation only convert passed values to right types. But
-        implementation can be changed to do more stuff (update instance
-        to newer version etc.).
-
-        Args:
-            attribute_values(dict): Values from instance metadata.
-
-        Returns:
-            dict: Converted values.
-        """
-        attr_defs = self.get_attribute_defs()
-        for attr_def in attr_defs:
-            key = attr_def.key
-            if key in attribute_values:
-                attribute_values[key] = attr_def.convert_value(
-                    attribute_values[key]
-                )
-        return attribute_values
-
 
 class Creator(BaseCreator):
     """Creator that has more information for artist to show in UI.
