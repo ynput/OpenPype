253f7f97c4839293d5f60cdf55297c558e472578
iLLiCiTiT <jakub.trllo@gmail.com>
2021-10-05 10:19:56 +0200

    raise not found custom attributes only for attributes that can be set on ftrack

--- openpype/modules/default_modules/ftrack/ftrack_module.py
+++ openpype/modules/default_modules/ftrack/ftrack_module.py
@@ -230,7 +230,13 @@ class FtrackModule(
             return
 
         import ftrack_api
-        from openpype_modules.ftrack.lib import get_openpype_attr
+        from openpype_modules.ftrack.lib import (
+            get_openpype_attr,
+            default_custom_attributes_definition,
+            CUST_ATTR_TOOLS,
+            CUST_ATTR_APPLICATIONS,
+            CUST_ATTR_INTENT
+        )
 
         try:
             session = self.create_ftrack_session()
@@ -255,6 +261,15 @@ class FtrackModule(
 
         project_id = project_entity["id"]
 
+        ca_defs = default_custom_attributes_definition()
+        hierarchical_attrs = ca_defs.get("is_hierarchical") or {}
+        project_attrs = ca_defs.get("show") or {}
+        ca_keys = (
+            set(hierarchical_attrs.keys())
+            + set(project_attrs.keys())
+            + {CUST_ATTR_TOOLS, CUST_ATTR_APPLICATIONS, CUST_ATTR_INTENT}
+        )
+
         cust_attr, hier_attr = get_openpype_attr(session)
         cust_attr_by_key = {attr["key"]: attr for attr in cust_attr}
         hier_attrs_by_key = {attr["key"]: attr for attr in hier_attr}
@@ -266,10 +281,11 @@ class FtrackModule(
             if not configuration:
                 configuration = cust_attr_by_key.get(key)
             if not configuration:
-                self.log.warning(
-                    "Custom attribute \"{}\" was not found.".format(key)
-                )
-                missing[key] = value
+                if key in ca_keys:
+                    self.log.warning(
+                        "Custom attribute \"{}\" was not found.".format(key)
+                    )
+                    missing[key] = value
                 continue
 
             # TODO add add permissions check
