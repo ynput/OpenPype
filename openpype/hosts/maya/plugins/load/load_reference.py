0d2d878e8a50d9f5787d2cc8e6aef892e74b4b13
karimmozilla <82811760+karimmozilla@users.noreply.github.com>
2021-10-05 11:59:55 +0200

    delete white space

--- openpype/hosts/maya/plugins/load/load_reference.py
+++ openpype/hosts/maya/plugins/load/load_reference.py
@@ -72,7 +72,7 @@ class ReferenceLoader(openpype.hosts.maya.api.plugin.ReferenceLoader):
                 except:  # noqa: E722
                     pass
 
-            if family not in ["layout", "setdress", "mayaAscii" , "mayaScene"]:
+            if family not in ["layout", "setdress", "mayaAscii", "mayaScene"]:
                 for root in roots:
                     root.setParent(world=True)
 
