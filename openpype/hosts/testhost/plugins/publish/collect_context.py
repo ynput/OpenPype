9e69af258e7eb5cbc967d8ce165c07ddc85107f6
iLLiCiTiT <jakub.trllo@gmail.com>
2021-10-06 12:11:16 +0200

    modified testhost collector

--- openpype/hosts/testhost/plugins/publish/collect_context.py
+++ openpype/hosts/testhost/plugins/publish/collect_context.py
@@ -1,7 +1,6 @@
 import pyblish.api
 from avalon import io
 
-from openpype.hosts.testhost import api
 from openpype.pipeline import (
     OpenPypePyblishPluginMixin,
     attribute_definitions
@@ -16,8 +15,8 @@ class CollectContextDataTestHost(
     and path for returning json data back to hostself.
     """
 
-    label = "Collect Context - Test Host"
-    order = pyblish.api.CollectorOrder - 0.5
+    label = "Collect Source - Test Host"
+    order = pyblish.api.CollectorOrder - 0.4
     hosts = ["testhost"]
 
     @classmethod
@@ -32,32 +31,5 @@ class CollectContextDataTestHost(
 
     def process(self, context):
         # get json paths from os and load them
-        io.install()
-
-        for instance_data in api.list_instances():
-            if instance_data.get("active", True):
-                # create instance
-                self.create_instance(context, instance_data)
-
-    def create_instance(self, context, in_data):
-        subset = in_data["subset"]
-        # If instance data already contain families then use it
-        instance_families = in_data.get("families") or []
-
-        instance = context.create_instance(subset)
-        instance.data.update({
-            "subset": subset,
-            "asset": in_data["asset"],
-            "label": subset,
-            "name": subset,
-            "family": in_data["family"],
-            "families": instance_families
-        })
-        for key, value in in_data.items():
-            if key not in instance.data:
-                instance.data[key] = value
-        self.log.info("collected instance: {}".format(instance.data))
-        self.log.info("parsing data: {}".format(in_data))
-
-        instance.data["representations"] = list()
-        instance.data["source"] = "testhost"
+        for instance in context:
+            instance.data["source"] = "testhost"
