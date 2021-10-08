93766c1d0b9515764b1e57f80406948d843f62eb
Petr Kalis <petr.kalis@gmail.com>
2021-10-08 17:27:15 +0200

    PYPE-1901 - Added plugin to close PS after remote publishing

---
+++ openpype/hosts/photoshop/plugins/publish/closePS.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Close PS after publish. For Webpublishing only."""
+import os
+
+import pyblish.api
+
+from avalon import photoshop
+
+class ClosePS(pyblish.api.InstancePlugin):
+    """Close PS after publish. For Webpublishing only.
+    """
+
+    order = pyblish.api.IntegratorOrder + 14
+    label = "Close PS"
+    optional = True
+    active = True
+
+    hosts = ["photoshop"]
+
+    def process(self, instance):
+        self.log.info("ClosePS")
+        if not os.environ.get("IS_HEADLESS"):
+            return
+
+        stub = photoshop.stub()
+        self.log.info("Shutting down PS")
+        stub.save()
+        stub.close()
