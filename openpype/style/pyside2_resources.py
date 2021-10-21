426c996b71e673ee39edbc84c86170241b9f38b3
iLLiCiTiT <jakub.trllo@gmail.com>
2021-10-21 14:50:06 +0200

    hound fixes in pyside2 resources

--- openpype/style/pyside2_resources.py
+++ openpype/style/pyside2_resources.py
@@ -811,10 +811,14 @@ qt_resource_struct = b"\
 \x00\x00\x00\xde\x00\x00\x00\x00\x00\x01\x00\x00\x02\xa6\
 "
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(
+        0x01, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
-
-qInitResources()
+    QtCore.qUnregisterResourceData(
+        0x01, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
