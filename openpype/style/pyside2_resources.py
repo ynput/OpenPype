c39c3ecb045629160d809fd3e25bdb3bad173ea1
iLLiCiTiT <jakub.trllo@gmail.com>
2021-10-22 12:40:38 +0200

    do not call pyside initialization on import

--- openpype/style/pyside2_resources.py
+++ openpype/style/pyside2_resources.py
@@ -811,14 +811,14 @@ qt_resource_struct = b"\
 \x00\x00\x02X\x00\x00\x00\x00\x00\x01\x00\x00\x16D\
 "
 
+
 def qInitResources():
     QtCore.qRegisterResourceData(
         0x01, qt_resource_struct, qt_resource_name, qt_resource_data
     )
 
+
 def qCleanupResources():
     QtCore.qUnregisterResourceData(
         0x01, qt_resource_struct, qt_resource_name, qt_resource_data
     )
-
-qInitResources()
