91ee0777a340491dc99a1221fd973f2c1da899c9
iLLiCiTiT <jakub.trllo@gmail.com>
2021-10-22 18:35:14 +0200

    fix parenting of experimental tools

--- openpype/tools/experimental_tools/dialog.py
+++ openpype/tools/experimental_tools/dialog.py
@@ -81,7 +81,9 @@ class ExperimentalToolsDialog(QtWidgets.QDialog):
         tool_btns_layout.addWidget(separator_widget, 0)
         tool_btns_layout.addWidget(tool_btns_label, 0)
 
-        experimental_tools = ExperimentalTools()
+        experimental_tools = ExperimentalTools(
+            parent=parent, filter_hosts=True
+        )
 
         # Main layout
         layout = QtWidgets.QVBoxLayout(self)
