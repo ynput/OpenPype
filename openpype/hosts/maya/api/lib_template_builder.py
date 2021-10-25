087794f0505a97c1df4a4bffe618f173de78ee32
clement.hector <clement.hector@gmail.com>
2021-10-25 17:24:35 +0200

    Unify place holder creation

--- openpype/hosts/maya/api/lib_template_builder.py
+++ openpype/hosts/maya/api/lib_template_builder.py
@@ -1,20 +1,18 @@
 import maya.cmds as cmds
 
 
-def create_linked_asset_place_holder(place_holder_name="linked_asset_builder"):
+def create_place_holder():
 
-    return create_place_holder(place_holder_name, "linked_asset_builder")
+    place_holder_name = cmds.spaceLocator(name="place_holder")
 
-def create_context_place_holder(place_holder_name="context_place_holder"):
-
-    return create_place_holder(place_holder_name, "linked_asset_builder")
-
-def create_place_holder(place_holder_name, attribute_name):
-
-    place_holder_name = cmds.spaceLocator(name=place_holder_name)
-
-    cmds.addAttr(place_holder_name, longName=attribute_name, attributeType='bool')
-    cmds.addAttr(place_holder_name, longName="representation", dataType="string")
+    cmds.addAttr(
+        place_holder_name,
+        enumName="context_asset=1:linked_asset=2",
+        longName="builder_type",
+        attributeType='enum')
+    cmds.addAttr(
+        place_holder_name, longName="representation", dataType="string"
+    )
     cmds.addAttr(place_holder_name, longName="families", dataType='string')
     cmds.addAttr(place_holder_name, longName="repre_name", dataType='string')
     cmds.addAttr(place_holder_name, longName="asset", dataType='string')
