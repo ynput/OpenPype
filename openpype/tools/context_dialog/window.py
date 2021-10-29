b61a7a9364c57c857acfa8af39bfa1ab8d6f8c7b
iLLiCiTiT <jakub.trllo@gmail.com>
2021-10-29 19:23:05 +0200

    added few docstrings

--- openpype/tools/context_dialog/window.py
+++ openpype/tools/context_dialog/window.py
@@ -48,16 +48,20 @@ class ContextDialog(QtWidgets.QDialog):
         # UI initialization
         main_splitter = QtWidgets.QSplitter(self)
 
+        # Left side widget containt project combobox and asset widget
         left_side_widget = QtWidgets.QWidget(main_splitter)
 
         project_combobox = QtWidgets.QComboBox(left_side_widget)
+        # Styled delegate to propagate stylessheet
         project_delegate = QtWidgets.QStyledItemDelegate(project_combobox)
         project_combobox.setItemDelegate(project_delegate)
+        # Project model with only active projects without default item
         project_model = ProjectModel(
             dbcon,
             only_active=True,
             add_default_project=False
         )
+        # Sorting proxy model
         project_proxy = ProjectSortFilterProxy()
         project_proxy.setSourceModel(project_model)
         project_combobox.setModel(project_proxy)
@@ -72,17 +76,22 @@ class ContextDialog(QtWidgets.QDialog):
         left_side_layout.addWidget(project_combobox)
         left_side_layout.addWidget(assets_widget)
 
+        # Right side of window contains only tasks
         task_view = QtWidgets.QListView(main_splitter)
         task_model = TasksModel(dbcon)
         task_proxy = TasksProxyModel()
         task_proxy.setSourceModel(task_model)
         task_view.setModel(task_proxy)
 
+        # Add widgets to main splitter
         main_splitter.addWidget(left_side_widget)
         main_splitter.addWidget(task_view)
+
+        # Set stretch of both sides
         main_splitter.setStretchFactor(0, 7)
         main_splitter.setStretchFactor(1, 3)
 
+        # Add confimation button to bottom right
         ok_btn = QtWidgets.QPushButton("OK", self)
 
         buttons_layout = QtWidgets.QHBoxLayout()
@@ -94,6 +103,10 @@ class ContextDialog(QtWidgets.QDialog):
         main_layout.addWidget(main_splitter, 1)
         main_layout.addLayout(buttons_layout, 0)
 
+        # Timer which will trigger asset refresh
+        # - this is needed because asset widget triggers
+        #   finished refresh before hides spin box so we need to trigger
+        #   refreshing in small offset if we want re-refresh asset widget
         assets_timer = QtCore.QTimer()
         assets_timer.setInterval(50)
         assets_timer.setSingleShot(True)
@@ -137,9 +150,11 @@ class ContextDialog(QtWidgets.QDialog):
         self._rerefresh_assets = True
         self._assets_refreshing = False
 
+        # Set stylehseet and resize window on first show
+        self._first_show = True
+
         # Helper attributes for handling of refresh
         self._ignore_value_changes = False
-        self._first_show = True
         self._refresh_on_next_show = True
 
         # Output of dialog
@@ -150,6 +165,7 @@ class ContextDialog(QtWidgets.QDialog):
         }
 
     def closeEvent(self, event):
+        """Ignore close event if is in strict state and context is not done."""
         if self._strict and not self._ok_btn.isEnabled():
             event.ignore()
             return
@@ -159,24 +175,32 @@ class ContextDialog(QtWidgets.QDialog):
         super(ContextDialog, self).closeEvent(event)
 
     def set_strict(self, strict):
+        """Change strictness of dialog."""
         self._strict = strict
         self._validate_strict()
 
     def _set_refresh_on_next_show(self):
-        if self._refresh_on_next_show:
-            return
+        """Refresh will be called on next showEvent.
 
+        If window is already visible then just execute refresh.
+        """
         self._refresh_on_next_show = True
         if self.isVisible():
             self.refresh()
 
     def _refresh_assets(self):
+        """Trigger refreshing of asset widget.
+
+        This will set mart to rerefresh asset when current refreshing is done
+        or do it immidietely if asset widget is not refreshing at the time.
+        """
         if self._assets_refreshing:
             self._rerefresh_assets = True
         else:
             self._on_asset_refresh_timer()
 
     def showEvent(self, event):
+        """Override show event to do some callbacks."""
         super(ContextDialog, self).showEvent(event)
         if self._first_show:
             self._first_show = False
@@ -189,19 +213,30 @@ class ContextDialog(QtWidgets.QDialog):
             self.refresh()
 
     def refresh(self):
-        """Load assets from database"""
+        """Refresh all widget one by one.
+
+        When asset refresh is triggered we have to wait when is done so
+        this method continues with `_on_asset_widget_refresh_finished`.
+        """
+        # Change state of refreshing (no matter how refresh was called)
         self._refresh_on_next_show = False
 
+        # Ignore changes of combobox and asset widget
         self._ignore_value_changes = True
 
+        # Get current project name to be able set it afterwards
         select_project_name = self._dbcon.Session.get("AVALON_PROJECT")
+        # Trigger project refresh
         self._project_model.refresh()
+        # Sort projects
         self._project_proxy.sort(0)
 
+        # Disable combobox if project was passed to `set_context`
         if self._set_context_project:
             select_project_name = self._set_context_project
             self._project_combobox.setEnabled(False)
         else:
+            # Find new project to select
             self._project_combobox.setEnabled(True)
             if (
                 select_project_name is None
@@ -219,12 +254,20 @@ class ContextDialog(QtWidgets.QDialog):
             self._project_combobox.currentText()
         )
 
+        # Trigger asset refresh
         self._refresh_assets()
 
     def _on_asset_refresh_timer(self):
+        """This is only way how to trigger refresh asset widget.
+
+        Use `_refresh_assets` method to refresh asset widget.
+        """
         self._assets_widget.refresh()
 
     def _on_asset_widget_refresh_finished(self):
+        """Catch when asset widget finished refreshing."""
+        # If should refresh again then skip all other callbacks and trigger
+        #   assets timer directly.
         self._assets_refreshing = False
         if self._rerefresh_assets:
             self._rerefresh_assets = False
@@ -241,7 +284,9 @@ class ContextDialog(QtWidgets.QDialog):
             self._assets_widget.setEnabled(True)
             self._assets_widget.set_current_asset_btn_visibility(False)
 
+        # Refresh tasks
         self._task_model.refresh()
+        # Sort tasks
         self._task_proxy.sort(0, QtCore.Qt.AscendingOrder)
 
         self._ignore_value_changes = False
@@ -282,20 +327,26 @@ class ContextDialog(QtWidgets.QDialog):
         if asset_ids:
             asset_id = asset_ids[0]
         self._task_model.set_asset_id(asset_id)
+        self._task_proxy.sort(0, QtCore.Qt.AscendingOrder)
 
     def _confirm_values(self):
+        """Store values to output."""
         self._context_to_store["project"] = self.get_selected_project()
         self._context_to_store["asset"] = self.get_selected_asset()
         self._context_to_store["task"] = self.get_selected_task()
 
     def _on_ok_click(self):
+        # Store values to output
         self._confirm_values()
+        # Close dialog
         self.accept()
 
     def get_selected_project(self):
+        """Get selected project."""
         return self._project_combobox.currentText()
 
     def get_selected_asset(self):
+        """Currently selected asset in asset widget."""
         asset_name = None
         for asset_doc in self._assets_widget.get_selected_assets():
             asset_name = asset_doc["name"]
@@ -303,6 +354,7 @@ class ContextDialog(QtWidgets.QDialog):
         return asset_name
 
     def get_selected_task(self):
+        """Currently selected task."""
         task_name = None
         index = self._task_view.selectionModel().currentIndex()
         if index.isValid():
@@ -325,6 +377,7 @@ class ContextDialog(QtWidgets.QDialog):
         self._ok_btn.setEnabled(enabled)
 
     def set_context(self, project_name=None, asset_name=None):
+        """Set context which will be used and locked in dialog."""
         if project_name is None:
             asset_name = None
 
@@ -337,6 +390,7 @@ class ContextDialog(QtWidgets.QDialog):
         self._set_refresh_on_next_show()
 
     def get_context(self):
+        """Result of dialog."""
         return self._context_to_store
 
 
@@ -346,18 +400,24 @@ def main(
     asset_name=None,
     strict=True
 ):
-    app = QtWidgets.QApplication([])
+    # Run Qt application
+    app = QtWidgets.QApplication.instance()
+    if app is None:
+        app = QtWidgets.QApplication([])
     window = ContextDialog()
     window.set_strict(strict)
     window.set_context(project_name, asset_name)
     window.show()
     app.exec_()
 
+    # Get result from window
     data = window.get_context()
 
+    # Make sure json filepath directory exists
     file_dir = os.path.dirname(path_to_store)
     if not os.path.exists(file_dir):
         os.makedirs(file_dir)
 
+    # Store result into json file
     with open(path_to_store, "w") as stream:
         json.dump(data, stream)
