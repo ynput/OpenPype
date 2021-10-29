52801768d8a5427716ce8b027fb17ee14e9d4eef
iLLiCiTiT <jakub.trllo@gmail.com>
2021-10-29 18:32:39 +0200

    moved task model into tools/utils/models.py

---
+++ openpype/tools/utils/constants.py
@@ -0,0 +1,6 @@
+from Qt import QtCore
+
+
+TASK_NAME_ROLE = QtCore.Qt.UserRole + 301
+TASK_TYPE_ROLE = QtCore.Qt.UserRole + 302
+TASK_ORDER_ROLE = QtCore.Qt.UserRole + 403
--- openpype/tools/workfiles/app.py
+++ openpype/tools/workfiles/app.py
@@ -14,13 +14,15 @@ from avalon.tools import lib as tools_lib
 from avalon.tools.widgets import AssetWidget
 from avalon.tools.delegates import PrettyTimeDelegate
 
-from .model import (
+from openpype.tools.utils.constants import (
     TASK_NAME_ROLE,
-    TASK_TYPE_ROLE,
-    FilesModel,
+    TASK_TYPE_ROLE
+)
+from openpype.tools.utils.model import (
     TasksModel,
     TasksProxyModel
 )
+from .model import FilesModel
 from .view import FilesView
 
 from openpype.lib import (
