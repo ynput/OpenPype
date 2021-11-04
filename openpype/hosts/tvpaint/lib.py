6093f2d26a0a2612b99dbccac75abd04e778b447
iLLiCiTiT <jakub.trllo@gmail.com>
2021-11-04 19:12:29 +0100

    created copy of composite_images from tvpaint api

--- openpype/hosts/tvpaint/lib.py
+++ openpype/hosts/tvpaint/lib.py
@@ -469,3 +469,22 @@ def copy_render_file(src_path, dst_path):
         os.link(src_path, dst_path)
     else:
         shutil.copy(src_path, dst_path)
+
+
+def composite_images(input_image_paths, output_filepath):
+    """Composite images in order from passed list.
+
+    Raises:
+        ValueError: When entered list is empty.
+    """
+    if not input_image_paths:
+        raise ValueError("Nothing to composite.")
+
+    img_obj = None
+    for image_filepath in input_image_paths:
+        _img_obj = Image.open(image_filepath)
+        if img_obj is None:
+            img_obj = _img_obj
+        else:
+            img_obj.alpha_composite(_img_obj)
+    img_obj.save(output_filepath)
