f68d23b3f8db40dcb6b5f1c4da3cd7e980f0618d
Jakub Jezek <jakub@orbi.tools>
2021-11-05 11:00:07 +0100

    breaking into multimodule

--- openpype/hosts/flame/utility_scripts/openpype_flame_to_ftrack/openpype_flame_to_ftrack.py
+++ openpype/hosts/flame/utility_scripts/openpype_flame_to_ftrack/openpype_flame_to_ftrack.py
@@ -21,10 +21,12 @@ TEMP_DIR_DATA_PATH = None
 F_PROJ_ENTITY = None
 
 SCRIPT_DIR = os.path.dirname(__file__)
+PACKAGE_DIR = os.pah.join(SCRIPT_DIR, "flame_to_ftrack_modules")
 EXPORT_PRESETS_DIR = os.path.join(SCRIPT_DIR, "export_preset")
 CONFIG_DIR = os.path.join(os.path.expanduser(
     "~/.openpype"), "openpype_flame_to_ftrack")
 
+sys.path.append(PACKAGE_DIR)
 
 def import_ftrack_api():
     try:
@@ -252,197 +254,6 @@ def timecode_to_frames(timecode, framerate):
     frames = int(round(tc_to_frames(timecode, start='00:00:00:00')))
 
     return frames
-
-
-class FlameLabel(QtWidgets.QLabel):
-    """
-    Custom Qt Flame Label Widget
-
-    For different label looks set label_type as: 'normal', 'background', or 'outline'
-
-    To use:
-
-    label = FlameLabel('Label Name', 'normal', window)
-    """
-
-    def __init__(self, label_name, label_type, parent_window, *args, **kwargs):
-        super(FlameLabel, self).__init__(*args, **kwargs)
-
-        self.setText(label_name)
-        self.setParent(parent_window)
-        self.setMinimumSize(130, 28)
-        self.setMaximumHeight(28)
-        self.setFocusPolicy(QtCore.Qt.NoFocus)
-
-        # Set label stylesheet based on label_type
-
-        if label_type == 'normal':
-            self.setStyleSheet('QLabel {color: #9a9a9a; border-bottom: 1px inset #282828; font: 14px "Discreet"}'
-                               'QLabel:disabled {color: #6a6a6a}')
-        elif label_type == 'background':
-            self.setAlignment(QtCore.Qt.AlignCenter)
-            self.setStyleSheet(
-                'color: #9a9a9a; background-color: #393939; font: 14px "Discreet"')
-        elif label_type == 'outline':
-            self.setAlignment(QtCore.Qt.AlignCenter)
-            self.setStyleSheet(
-                'color: #9a9a9a; background-color: #212121; border: 1px solid #404040; font: 14px "Discreet"')
-
-
-class FlameLineEdit(QtWidgets.QLineEdit):
-    """
-    Custom Qt Flame Line Edit Widget
-
-    Main window should include this: window.setFocusPolicy(QtCore.Qt.StrongFocus)
-
-    To use:
-
-    line_edit = FlameLineEdit('Some text here', window)
-    """
-
-    def __init__(self, text, parent_window, *args, **kwargs):
-        super(FlameLineEdit, self).__init__(*args, **kwargs)
-
-        self.setText(text)
-        self.setParent(parent_window)
-        self.setMinimumHeight(28)
-        self.setMinimumWidth(110)
-        self.setStyleSheet('QLineEdit {color: #9a9a9a; background-color: #373e47; selection-color: #262626; selection-background-color: #b8b1a7; font: 14px "Discreet"}'
-                           'QLineEdit:focus {background-color: #474e58}'
-                           'QLineEdit:disabled {color: #6a6a6a; background-color: #373737}')
-
-
-class FlameTreeWidget(QtWidgets.QTreeWidget):
-    """
-    Custom Qt Flame Tree Widget
-
-    To use:
-
-    tree_headers = ['Header1', 'Header2', 'Header3', 'Header4']
-    tree = FlameTreeWidget(tree_headers, window)
-    """
-
-    def __init__(self, tree_headers, parent_window, *args, **kwargs):
-        super(FlameTreeWidget, self).__init__(*args, **kwargs)
-
-        self.setMinimumWidth(1000)
-        self.setMinimumHeight(300)
-        self.setSortingEnabled(True)
-        self.sortByColumn(0, QtCore.Qt.AscendingOrder)
-        self.setAlternatingRowColors(True)
-        self.setFocusPolicy(QtCore.Qt.NoFocus)
-        self.setStyleSheet(
-            'QTreeWidget {color: #9a9a9a; background-color: #2a2a2a; alternate-background-color: #2d2d2d; font: 14px "Discreet"}'
-            'QTreeWidget::item:selected {color: #d9d9d9; background-color: #474747; border: 1px solid #111111}'
-            'QHeaderView {color: #9a9a9a; background-color: #393939; font: 14px "Discreet"}'
-            'QTreeWidget::item:selected {selection-background-color: #111111}'
-            'QMenu {color: #9a9a9a; background-color: #24303d; font: 14px "Discreet"}'
-            'QMenu::item:selected {color: #d9d9d9; background-color: #3a4551}'
-        )
-        self.verticalScrollBar().setStyleSheet('color: #818181')
-        self.horizontalScrollBar().setStyleSheet('color: #818181')
-        self.setHeaderLabels(tree_headers)
-
-
-class FlameButton(QtWidgets.QPushButton):
-    """
-    Custom Qt Flame Button Widget
-
-    To use:
-
-    button = FlameButton('Button Name', do_this_when_pressed, window)
-    """
-
-    def __init__(self, button_name, do_when_pressed, parent_window, *args, **kwargs):
-        super(FlameButton, self).__init__(*args, **kwargs)
-
-        self.setText(button_name)
-        self.setParent(parent_window)
-        self.setMinimumSize(QtCore.QSize(110, 28))
-        self.setMaximumSize(QtCore.QSize(110, 28))
-        self.setFocusPolicy(QtCore.Qt.NoFocus)
-        self.clicked.connect(do_when_pressed)
-        self.setStyleSheet('QPushButton {color: #9a9a9a; background-color: #424142; border-top: 1px inset #555555; border-bottom: 1px inset black; font: 14px "Discreet"}'
-                           'QPushButton:pressed {color: #d9d9d9; background-color: #4f4f4f; border-top: 1px inset #666666; font: italic}'
-                           'QPushButton:disabled {color: #747474; background-color: #353535; border-top: 1px solid #444444; border-bottom: 1px solid #242424}')
-
-
-class FlamePushButton(QtWidgets.QPushButton):
-    """
-    Custom Qt Flame Push Button Widget
-
-    To use:
-
-    pushbutton = FlamePushButton(' Button Name', True_or_False, window)
-    """
-
-    def __init__(self, button_name, button_checked, parent_window, *args, **kwargs):
-        super(FlamePushButton, self).__init__(*args, **kwargs)
-
-        self.setText(button_name)
-        self.setParent(parent_window)
-        self.setCheckable(True)
-        self.setChecked(button_checked)
-        self.setMinimumSize(155, 28)
-        self.setMaximumSize(155, 28)
-        self.setFocusPolicy(QtCore.Qt.NoFocus)
-        self.setStyleSheet('QPushButton {color: #9a9a9a; background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: .93 #424142, stop: .94 #2e3b48); text-align: left; border-top: 1px inset #555555; border-bottom: 1px inset black; font: 14px "Discreet"}'
-                           'QPushButton:checked {color: #d9d9d9; background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: .93 #4f4f4f, stop: .94 #5a7fb4); font: italic; border: 1px inset black; border-bottom: 1px inset #404040; border-right: 1px inset #404040}'
-                           'QPushButton:disabled {color: #6a6a6a; background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: .93 #383838, stop: .94 #353535); font: light; border-top: 1px solid #575757; border-bottom: 1px solid #242424; border-right: 1px solid #353535; border-left: 1px solid #353535}'
-                           'QToolTip {color: black; background-color: #ffffde; border: black solid 1px}')
-
-
-class FlamePushButtonMenu(QtWidgets.QPushButton):
-    """
-    Custom Qt Flame Menu Push Button Widget
-
-    To use:
-
-    push_button_menu_options = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
-    menu_push_button = FlamePushButtonMenu('push_button_name', push_button_menu_options, window)
-
-    or
-
-    push_button_menu_options = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
-    menu_push_button = FlamePushButtonMenu(push_button_menu_options[0], push_button_menu_options, window)
-    """
-    selection_changed = QtCore.Signal(str)
-
-    def __init__(self, button_name, menu_options, parent_window, *args, **kwargs):
-        super(FlamePushButtonMenu, self).__init__(*args, **kwargs)
-
-        self.setParent(parent_window)
-        self.setMinimumHeight(28)
-        self.setMinimumWidth(110)
-        self.setFocusPolicy(QtCore.Qt.NoFocus)
-        self.setStyleSheet('QPushButton {color: #9a9a9a; background-color: #24303d; font: 14px "Discreet"}'
-                           'QPushButton:disabled {color: #747474; background-color: #353535; border-top: 1px solid #444444; border-bottom: 1px solid #242424}')
-
-        pushbutton_menu = QtWidgets.QMenu(parent_window)
-        pushbutton_menu.setFocusPolicy(QtCore.Qt.NoFocus)
-        pushbutton_menu.setStyleSheet('QMenu {color: #9a9a9a; background-color:#24303d; font: 14px "Discreet"}'
-                                      'QMenu::item:selected {color: #d9d9d9; background-color: #3a4551}')
-
-        self._pushbutton_menu = pushbutton_menu
-        self.setMenu(pushbutton_menu)
-        self.set_menu_options(menu_options, button_name)
-
-    def set_menu_options(self, menu_options, current_option=None):
-        self._pushbutton_menu.clear()
-        current_option = current_option or menu_options[0]
-
-        for option in menu_options:
-            action = self._pushbutton_menu.addAction(option)
-            action.triggered.connect(self._on_action_trigger)
-
-        if current_option is not None:
-            self.setText(current_option)
-
-    def _on_action_trigger(self):
-        action = self.sender()
-        self.setText(action.text())
-        self.selection_changed.emit(action.text())
-
 
 class FtrackComponentCreator:
     default_location = "ftrack.server"
@@ -676,6 +487,8 @@ class FtrackComponentCreator:
 
 def main_window(selection):
     import flame
+    import uiwidgets
+
     global TEMP_DIR_DATA_PATH
     global F_PROJ_ENTITY
 
@@ -1043,7 +856,7 @@ def main_window(selection):
 
     print(ordered_column_labels)
 
-    tree = FlameTreeWidget(ordered_column_labels, window)
+    tree = uiwidgets.FlameTreeWidget(ordered_column_labels, window)
 
     # Allow multiple items in tree to be selected
     tree.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
@@ -1067,40 +880,40 @@ def main_window(selection):
             break
 
         # input fields
-        shot_name_label = FlameLabel(
+        shot_name_label = uiwidgets.FlameLabel(
             'Shot name template', 'normal', window)
-        shot_name_template_input = FlameLineEdit(
+        shot_name_template_input = uiwidgets.FlameLineEdit(
             cfg_d["shot_name_template"], window)
 
-        hierarchy_label = FlameLabel(
+        hierarchy_label = uiwidgets.FlameLabel(
             'Parents template', 'normal', window)
-        hierarchy_template_input = FlameLineEdit(
+        hierarchy_template_input = uiwidgets.FlameLineEdit(
             cfg_d["hierarchy_template"], window)
 
-        start_frame_label = FlameLabel(
+        start_frame_label = uiwidgets.FlameLabel(
             'Workfile start frame', 'normal', window)
-        start_frame_input = FlameLineEdit(
+        start_frame_input = uiwidgets.FlameLineEdit(
             cfg_d["workfile_start_frame"], window)
 
-        handles_label = FlameLabel(
+        handles_label = uiwidgets.FlameLabel(
             'Shot handles', 'normal', window)
-        handles_input = FlameLineEdit(cfg_d["shot_handles"], window)
+        handles_input = uiwidgets.FlameLineEdit(cfg_d["shot_handles"], window)
 
-        width_label = FlameLabel(
+        width_label = uiwidgets.FlameLabel(
             'Sequence width', 'normal', window)
-        width_input = FlameLineEdit(str(seq_width), window)
+        width_input = uiwidgets.FlameLineEdit(str(seq_width), window)
 
-        height_label = FlameLabel(
+        height_label = uiwidgets.FlameLabel(
             'Sequence height', 'normal', window)
-        height_input = FlameLineEdit(str(seq_height), window)
+        height_input = uiwidgets.FlameLineEdit(str(seq_height), window)
 
-        pixel_aspect_label = FlameLabel(
+        pixel_aspect_label = uiwidgets.FlameLabel(
             'Pixel aspect ratio', 'normal', window)
-        pixel_aspect_input = FlameLineEdit(str(1.00), window)
+        pixel_aspect_input = uiwidgets.FlameLineEdit(str(1.00), window)
 
-        fps_label = FlameLabel(
+        fps_label = uiwidgets.FlameLabel(
             'Frame rate', 'normal', window)
-        fps_input = FlameLineEdit(str(fps), window)
+        fps_input = uiwidgets.FlameLineEdit(str(fps), window)
 
         # get project name from flame current project
         project_name = flame.project.current_project.name
@@ -1121,25 +934,25 @@ def main_window(selection):
             project_names = [p["full_name"] for p in all_projects]
             TASK_TYPES_ALL = {p["full_name"]: get_all_task_types(
                 p).keys() for p in all_projects}
-            project_select_label = FlameLabel(
+            project_select_label = uiwidgets.FlameLabel(
                 'Select Ftrack project', 'normal', window)
-            project_select_input = FlamePushButtonMenu(
+            project_select_input = uiwidgets.FlamePushButtonMenu(
                 F_PROJ_ENTITY["full_name"], project_names, window)
             project_select_input.selection_changed.connect(_on_project_changed)
 
         F_PROJ_TASK_TYPES = get_all_task_types(F_PROJ_ENTITY)
 
-        task_type_label = FlameLabel(
+        task_type_label = uiwidgets.FlameLabel(
             'Create Task (type)', 'normal', window)
-        task_type_input = FlamePushButtonMenu(
+        task_type_input = uiwidgets.FlamePushButtonMenu(
             cfg_d["create_task_type"], F_PROJ_TASK_TYPES.keys(), window)
 
         # Button
-        select_all_btn = FlameButton('Select All', select_all, window)
-        remove_temp_data_btn = FlameButton(
+        select_all_btn = uiwidgets.FlameButton('Select All', select_all, window)
+        remove_temp_data_btn = uiwidgets.FlameButton(
             'Remove temp data', remove_temp_data, window)
 
-        ftrack_send_btn = FlameButton('Send to Ftrack', send_to_ftrack, window)
+        ftrack_send_btn = uiwidgets.FlameButton('Send to Ftrack', send_to_ftrack, window)
 
         # left props
         v_shift = 0
