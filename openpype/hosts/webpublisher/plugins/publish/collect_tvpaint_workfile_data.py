06a165116ea8adff5d99debc5702b03454698ece
iLLiCiTiT <jakub.trllo@gmail.com>
2021-11-08 11:43:53 +0100

    modified webpublisher collector

--- openpype/hosts/webpublisher/plugins/publish/collect_tvpaint_workfile_data.py
+++ openpype/hosts/webpublisher/plugins/publish/collect_tvpaint_workfile_data.py
@@ -6,8 +6,6 @@ Requires:
 Provides:
     Instance
 """
-import json
-import time
 import pyblish.api
 from openpype.hosts.tvpaint.worker import (
     SenderTVPaintCommands,
@@ -37,5 +35,36 @@ class CollectTVPaintWorkfileData(pyblish.api.InstancePlugin):
         # Send job and wait for answer
         commands.send_job_and_wait()
 
-        # Store result
-        instance.data["sceneData"] = collect_scene_data_command.result()
+        collected_data = collect_scene_data_command.result()
+        layers_data = collected_data["layers_data"]
+        groups_data = collected_data["groups_data"]
+        scene_data = collected_data["scene_data"]
+        exposure_frames_by_layer_id = (
+            collected_data["exposure_frames_by_layer_id"]
+        )
+        pre_post_beh_by_layer_id = (
+            collected_data["pre_post_beh_by_layer_id"]
+        )
+
+        # Store results
+        # scene data store the same way as TVPaint collector
+        instance.data["sceneData"] = {
+            "sceneWidth": scene_data["width"],
+            "sceneHeight": scene_data["height"],
+            "scenePixelAspect": scene_data["pixel_aspect"],
+            "sceneFps": scene_data["fps"],
+            "sceneFieldOrder": scene_data["field_order"],
+            "sceneMarkIn": scene_data["mark_in"],
+            # scene_data["mark_in_state"],
+            "sceneMarkInState": scene_data["mark_in_set"],
+            "sceneMarkOut": scene_data["mark_out"],
+            # scene_data["mark_out_state"],
+            "sceneMarkOutState": scene_data["mark_out_set"],
+            "sceneStartFrame": scene_data["start_frame"],
+            "sceneBgColor": scene_data["bg_color"]
+        }
+        # Store only raw data
+        instance.data["groupsData"] = groups_data
+        instance.data["layersData"] = layers_data
+        instance.data["layersExposureFrames"] = exposure_frames_by_layer_id
+        instance.data["layersPrePostBehavior"] = pre_post_beh_by_layer_id
