1a7bf639ace45d4f6b7284f43b18fc132afe0183
BenoitConnan <benoitconnan.pro@gmail.com>
2021-11-09 15:22:29 +0100

    Refactoring build_workfile_by_template
    Extract placeholder logic +
    Filter by asset and hierarchy

--- openpype/lib/abstract_load_template.py
+++ openpype/lib/abstract_load_template.py
@@ -1,4 +1,5 @@
 import os
+import re
 import avalon
 from openpype.settings import get_project_settings
 from openpype.lib import Anatomy
@@ -116,8 +117,6 @@ def get_loader_by_name():
         loaders_by_name[loader_name] = loader
     return loaders_by_name
 
-##############################
-
 
 class AbstractTemplateLoader(object):
     """
@@ -151,8 +150,10 @@ class AbstractTemplateLoader(object):
             str: Solved template path
 
         Raises:
-            ValueError: No profile found from settings for current avalon session
-            KeyError: Could not solve path because a key does not exists in avalon context
+            ValueError: No profile found from settings for current avalon
+                session
+            KeyError: Could not solve path because a key does not exists
+                in avalon context
             IOError: Solved path does not exists on current filesystem
         """
         project = avalon.io.Session["AVALON_PROJECT"]
@@ -160,19 +161,21 @@ class AbstractTemplateLoader(object):
         project_settings = get_project_settings(project)
         current_dcc = avalon.io.Session["AVALON_APP"]
         current_task = avalon.io.Session["AVALON_TASK"]
+        profiles = project_settings[current_dcc]['workfile_build']['profiles']
 
-        for profile in project_settings[current_dcc]['workfile_build'][
-                'profiles']:
+        for profile in profiles:
             if current_task in profile['task_types']:
                 path = profile['path']
                 break
         else:
             raise ValueError(
                 "No matching profile found for task '{}' in DCC '{}'".format(
-                    current_task, current_dcc))
-        try:
-            solved_path = os.path.normpath(anatomy.path_remapper(path))
+                    current_task, current_dcc)
+            )
 
+        try:
+            # while solved_path != new_solved_path
+            solved_path = os.path.normpath(anatomy.path_remapper(path))
         except KeyError as missing_key:
             raise KeyError(
                 "Could not solve key '{}' in template path '{}'".format(
@@ -185,102 +188,53 @@ class AbstractTemplateLoader(object):
                     current_task, current_dcc, solved_path))
         return solved_path
 
-    def __init__(self):
+    def __init__(self, placeholder_class):
 
         self.import_template(self.template_path)
 
         loaders_by_name = get_loader_by_name()
-        # Skip if there are any loaders
+
+        # Skip if there is no loader
         if not loaders_by_name:
             self.log.warning("There are no registered loaders.")
             return
 
-        # get_assets
+        current_asset = avalon.io.Session["AVALON_ASSET"]
         current_asset_entity = avalon.io.find_one({
             "type": "asset",
-            "name": avalon.io.Session["AVALON_ASSET"]
+            "name": current_asset
         })
 
-        current_entities = _collect_last_version_repres([current_asset_entity])
-        assets = get_linked_assets(current_asset_entity)
-        linked_entities = _collect_last_version_repres(assets)
-        current_representations = self.get_representations(current_entities)
-        linked_representations = self.get_representations(linked_entities)
+        linked_asset_entities = get_linked_assets(current_asset_entity)
+        version_repres = _collect_last_version_repres([current_asset_entity] + linked_asset_entities)
 
-        placeholders = [
-            self._get_node_data(node) for node
-            in self.get_template_nodes()
-            if self.is_valid_placeholder(node)
-        ]
+        linked_representations_by_id = {representation['_id'] : representation
+            for asset in version_repres.values()
+            for subset in asset['subsets'].values()
+            for representation in subset['version']['repres']}
+        context_representations_by_id = dict()
 
-        placeholders.sort(key=lambda x: x['order'])
+        for k in linked_representations_by_id.keys():
+            if linked_representations_by_id[k]['context']['asset'] == current_asset:
+                context_representations_by_id[k] = linked_representations_by_id.pop(k)
+
+        placeholders = map(placeholder_class, self.get_template_nodes())
+        placeholders = filter(lambda ph : ph.is_valid, placeholders)
+        placeholders = sorted(placeholders, key=lambda ph : ph.order)
 
         for placeholder in placeholders:
-            loader_name = self._get_placeholder_loader_name(placeholder)
-            representations = current_representations \
-                if self.is_placeholder_context(placeholder) \
-                else linked_representations
-            for representation in \
-                self.get_valid_representations_id_for_placeholder(
-                    representations, placeholder):
+            if placeholder.data['builder_type'] == 'context_asset':
+                representations_by_id = context_representations_by_id
+            else:
+                representations_by_id = linked_representations_by_id
+            for representation_id, representation in representations_by_id.items():
+                if not placeholder.is_repres_valid(representation):
+                    continue
                 container = avalon.api.load(
-                    loaders_by_name[loader_name],
-                    representation
-                )
-                self.switch(container, placeholder)
-            self.clean_placeholder(placeholder)
-
-    def get_representations(self, entities):
-        """Parse avalon entities to get representations"""
-        return [subset['version']['repres'] for entity in entities.values()
-                for subset in entity['subsets'].values()
-                if entity['asset_entity']['_id']]
-
-    @staticmethod
-    def get_valid_representations_id_for_placeholder(self, representation):
-        """
-        filter representations
-
-        Arguments :
-            representation [dict()]:
-
-        Returns:
-            representations [dict()] :
-        """
-        raise NotImplementedError
-
-    @staticmethod
-    def validate_representation(representation, placeholder):
-        """
-        Property returning template path. Avoiding getter.
-        Getting template path from open pype settings
-        bassing on current avalon session
-        and solving the path variables if needed.
-
-        Returns:
-            str: Solved template path
-
-        Raises:
-            ValueError: No profile found from settings for current avalon
-                session
-            KeyError: Could not solve path because a key does not exists
-                in avalon context
-            IOError: Solved path does not exists on current filesystem
-        """
-        raise NotImplementedError
-
-    @staticmethod
-    def _get_node_data(self, node):
-        """
-        extract data from node
-
-        Arguments:
-            NodeClass: Your class
-
-        Returns:
-            str: Solved template path
-        """
-        raise NotImplementedError
+                    loaders_by_name[placeholder.loader],
+                    representation_id)
+                placeholder.parent(container)
+            placeholder.clean()
 
     def import_template(self, template_path):
         """
@@ -289,19 +243,6 @@ class AbstractTemplateLoader(object):
         Args:
             template_path (str): fullpath to current task and dcc's template
                 file
-        """
-        raise NotImplementedError
-
-    def switch(self, containers, placeholder):
-        """
-        Load containers for placeholder
-
-        Args:
-            containers ():
-            placeholder ():
-
-        Returns :
-            None
         """
         raise NotImplementedError
 
@@ -316,3 +257,47 @@ class AbstractTemplateLoader(object):
             str: Solved template path
         """
         raise NotImplementedError
+class AbstractPlaceholder:
+
+    attributes = {'builder_type', 'family', 'representation', 'order', 'loader'}
+    optional_attributes = {}
+
+    def __init__(self, node):
+        self.get_data(node)
+
+    def get_data(self, node):
+        raise NotImplementedError
+
+    @property
+    def order(self):
+        return self.data.get('order')
+
+    @property
+    def loader(self):
+        return self.data.get('loader')
+
+    @property
+    def is_context(self):
+        return self.data.get('builder_type') == 'context_asset'
+
+    @property
+    def is_valid(self):
+        return set(self.attributes).issubset(self.data.keys())
+
+    def parent(self, containers):
+        raise NotImplementedError
+
+    def clean(self):
+        raise NotImplementedError
+
+    def is_repres_valid(self, representation):
+        representation_context = representation['context']
+
+        is_valid = bool(re.match(
+            self.data.get('asset', ''), representation_context['asset']))
+        is_valid &= bool(re.match(
+            self.data.get('hierarchy', ''), representation_context['hierarchy']))
+        is_valid &= self.data['representation'] == representation_context['representation']
+        is_valid &= self.data['family'] == representation_context['family']
+
+        return is_valid
