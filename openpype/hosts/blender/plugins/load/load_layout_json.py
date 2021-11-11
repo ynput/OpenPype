d2ae004b494eafafa6f95df86989406493f45c66
Simone Barbieri <simone.barbieri@hey.com>
2021-11-11 10:33:11 +0000

    Disabled camera creation in Blender when loading layout from JSON

--- openpype/hosts/blender/plugins/load/load_layout_json.py
+++ openpype/hosts/blender/plugins/load/load_layout_json.py
@@ -108,20 +108,22 @@ class JsonLayoutLoader(plugin.AssetLoader):
                 options=options
             )
 
-        # Create the camera asset and the camera instance
-        creator_plugin = lib.get_creator_by_name("CreateCamera")
-        if not creator_plugin:
-            raise ValueError("Creator plugin \"CreateCamera\" was "
-                             "not found.")
+        # Camera creation when loading a layout is not necessary for now,
+        # but the code is worth keeping in case we need it in the future.
+        # # Create the camera asset and the camera instance
+        # creator_plugin = lib.get_creator_by_name("CreateCamera")
+        # if not creator_plugin:
+        #     raise ValueError("Creator plugin \"CreateCamera\" was "
+        #                      "not found.")
 
-        api.create(
-            creator_plugin,
-            name="camera",
-            # name=f"{unique_number}_{subset}_animation",
-            asset=asset,
-            options={"useSelection": False}
-            # data={"dependencies": str(context["representation"]["_id"])}
-        )
+        # api.create(
+        #     creator_plugin,
+        #     name="camera",
+        #     # name=f"{unique_number}_{subset}_animation",
+        #     asset=asset,
+        #     options={"useSelection": False}
+        #     # data={"dependencies": str(context["representation"]["_id"])}
+        # )
 
     def process_asset(self,
                       context: dict,
