e30e8ca25e1858bc23ffcc25f493037e47498ee6
BenoitConnan <benoitconnan.pro@gmail.com>
2021-11-15 13:50:20 +0100

    refact + hound fix

--- openpype/lib/build_template.py
+++ openpype/lib/build_template.py
@@ -7,7 +7,6 @@ concrete_loaders_modules = {
 
 
 class BuildWorkfileTemplate:
-    # log = logging.getLogger("BuildWorkfile")
 
     def process(self):
         dcc = avalon.io.Session['AVALON_APP']
@@ -19,10 +18,11 @@ class BuildWorkfileTemplate:
         module = importlib.import_module(module_path)
         if not hasattr(module, 'TemplateLoader'):
             raise ValueError(
-                "Linked module '{}' does not implement a template loader".format(module_path))
+                "Linked module '{}' does not "
+                "implement a template loader".format(module_path))
         if not hasattr(module, 'Placeholder'):
             raise ValueError(
-                "Linked module '{}' does not implement a placeholder template".format(module_path))
-        reload(module)
+                "Linked module '{}' does not "
+                "implement a placeholder template".format(module_path))
         concrete_loader = module.TemplateLoader
         concrete_loader(module.Placeholder)
