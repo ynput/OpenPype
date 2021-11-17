f7baeffe24b0afc38077dc741a57a4b30b33a3cf
iLLiCiTiT <jakub.trllo@gmail.com>
2021-11-17 21:49:16 +0100

    added simple asset view

---
+++ openpype/tools/utils/assets_widget.py
@@ -0,0 +1,42 @@
+from Qt import QtWidgets, QtCore
+
+from .views import (
+    TreeViewSpinner,
+    DeselectableTreeView
+)
+
+
+class AssetsView(TreeViewSpinner, DeselectableTreeView):
+    """Item view.
+    This implements a context menu.
+    """
+
+    def __init__(self, parent=None):
+        super(AssetsView, self).__init__(parent)
+        self.setIndentation(15)
+        self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.setHeaderHidden(True)
+
+    def mousePressEvent(self, event):
+        index = self.indexAt(event.pos())
+        if not index.isValid():
+            modifiers = QtWidgets.QApplication.keyboardModifiers()
+            if modifiers == QtCore.Qt.ShiftModifier:
+                return
+            elif modifiers == QtCore.Qt.ControlModifier:
+                return
+
+        super(AssetsView, self).mousePressEvent(event)
+
+    def set_loading_state(self, loading, empty):
+        if self.is_loading != loading:
+            if loading:
+                self.spinner.repaintNeeded.connect(
+                    self.viewport().update
+                )
+            else:
+                self.spinner.repaintNeeded.disconnect()
+                self.viewport().update()
+
+        self.is_loading = loading
+        self.is_empty = empty
