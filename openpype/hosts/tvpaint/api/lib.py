baba65d5e7b2a36e8363bc3ba0a269bb28f690fb
iLLiCiTiT <jakub.trllo@gmail.com>
2021-11-26 15:39:06 +0100

    use composite_images from 'tvpaint.lib'

--- openpype/hosts/tvpaint/api/lib.py
+++ openpype/hosts/tvpaint/api/lib.py
@@ -4,25 +4,6 @@ import avalon.io
 from avalon.tvpaint.lib import execute_george
 
 
-def composite_images(input_image_paths, output_filepath):
-    """Composite images in order from passed list.
-
-    Raises:
-        ValueError: When entered list is empty.
-    """
-    if not input_image_paths:
-        raise ValueError("Nothing to composite.")
-
-    img_obj = None
-    for image_filepath in input_image_paths:
-        _img_obj = Image.open(image_filepath)
-        if img_obj is None:
-            img_obj = _img_obj
-        else:
-            img_obj.alpha_composite(_img_obj)
-    img_obj.save(output_filepath)
-
-
 def set_context_settings(asset_doc=None):
     """Set workfile settings by asset document data.
 
