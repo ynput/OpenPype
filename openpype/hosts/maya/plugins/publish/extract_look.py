209dbea1a5b83aa61047b072923565fe238c56ff
Petr Kalis <petr.kalis@gmail.com>
2021-12-14 13:44:21 +0100

    OP-2151 - added logging and exception if oiio utilities not found

--- openpype/hosts/maya/plugins/publish/extract_look.py
+++ openpype/hosts/maya/plugins/publish/extract_look.py
@@ -58,6 +58,11 @@ def maketx(source, destination, *args):
     from openpype.lib import get_oiio_tools_path
 
     maketx_path = get_oiio_tools_path("maketx")
+    if not os.path.exists(maketx_path):
+        print(
+            "OIIO tool not found in {}".format(maketx_path))
+        raise AssertionError("OIIO tool not found")
+
     cmd = [
         maketx_path,
         "-v",  # verbose
