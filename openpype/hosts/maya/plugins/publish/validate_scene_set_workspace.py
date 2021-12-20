e4521558b9c4c988fc4e786e6da2dda59dc4659d
Jérôme LORRAIN <jerome.lorrain@protonmail.com>
2021-12-20 11:58:49 +0100

    Add option to publish from published workfile

--- openpype/hosts/maya/plugins/publish/validate_scene_set_workspace.py
+++ openpype/hosts/maya/plugins/publish/validate_scene_set_workspace.py
@@ -4,6 +4,7 @@ import maya.cmds as cmds
 
 import pyblish.api
 import openpype.api
+from openpype.lib.profiles_filtering import filter_profiles
 
 
 def is_subdir(path, root_dir):
@@ -25,7 +26,7 @@ def is_subdir(path, root_dir):
         return True
 
 
-class ValidateSceneSetWorkspace(pyblish.api.ContextPlugin):
+class ValidateSceneSetWorkspace(pyblish.api.InstancePlugin):
     """Validate the scene is inside the currently set Maya workspace"""
 
     order = openpype.api.ValidatePipelineOrder
@@ -34,8 +35,11 @@ class ValidateSceneSetWorkspace(pyblish.api.ContextPlugin):
     version = (0, 1, 0)
     label = 'Maya Workspace Set'
 
-    def process(self, context):
+    publish_from_published_workfiles = True
 
+    def process(self, instance):
+
+        context = instance.context
         scene_name = cmds.file(query=True, sceneName=True)
         if not scene_name:
             raise RuntimeError("Scene hasn't been saved. Workspace can't be "
@@ -44,4 +48,31 @@ class ValidateSceneSetWorkspace(pyblish.api.ContextPlugin):
         root_dir = cmds.workspace(query=True, rootDirectory=True)
 
         if not is_subdir(scene_name, root_dir):
-            raise RuntimeError("Maya workspace is not set correctly.")
+
+            if not self.publish_from_published_workfiles:
+                raise RuntimeError("Maya workspace is not set correctly.")
+            else:
+                settings = context.data.get('project_settings')
+                template_name_profiles = settings.get('global') \
+                    .get('publish') \
+                    .get('IntegrateAssetNew') \
+                    .get('template_name_profiles')
+                task_name = context.data["anatomyData"]["task"]["name"]
+                task_type = context.data["anatomyData"]["task"]["type"]
+                key_values = {
+                    "families": "workfile",
+                    "tasks": task_name,
+                    "hosts": context.data["hostName"],
+                    "task_types": task_type
+                }
+                profile = filter_profiles(
+                    template_name_profiles,
+                    key_values,
+                    logger=self.log
+                )
+                anatomy = context.data.get('anatomy')
+                anatomy_filled = anatomy.format(instance.data.get('anatomyData'))
+                pub_workfile_path = anatomy_filled[profile["template_name"]]["folder"]
+
+                if not is_subdir(scene_name, pub_workfile_path):
+                    raise RuntimeError("Maya workspace is not set correctly.")
