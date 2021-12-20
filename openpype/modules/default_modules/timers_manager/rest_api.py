ddcdff611e55ed6dadfccf598ca882a70c56cdf2
iLLiCiTiT <jakub.trllo@gmail.com>
2021-12-20 13:14:18 +0100

    return error message in response

--- openpype/modules/default_modules/timers_manager/rest_api.py
+++ openpype/modules/default_modules/timers_manager/rest_api.py
@@ -43,14 +43,19 @@ class TimersManagerModuleRestApi:
             asset_name = data["asset_name"]
             task_name = data["task_name"]
         except KeyError:
-            log.error((
+            msg = (
                 "Payload must contain fields 'project_name,"
                 " 'asset_name' and 'task_name'"
-            ))
-            return Response(status=400)
+            )
+            log.error(msg)
+            return Response(status=400, message=msg)
 
         self.module.stop_timers()
-        self.module.start_timer(project_name, asset_name, task_name)
+        try:
+            self.module.start_timer(project_name, asset_name, task_name)
+        except Exception as exc:
+            return Response(status=404, message=str(exc))
+
         return Response(status=200)
 
     async def stop_timer(self, request):
