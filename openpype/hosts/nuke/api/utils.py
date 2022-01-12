9980aa90fa196eb07e57ea7155b7ce98469d81e9
iLLiCiTiT <jakub.trllo@gmail.com>
2022-01-12 12:32:21 +0100

    fix default value of function argument

--- openpype/hosts/nuke/api/utils.py
+++ openpype/hosts/nuke/api/utils.py
@@ -49,12 +49,14 @@ def gizmo_is_nuke_default(gizmo):
     return gizmo.filename().startswith(plug_dir)
 
 
-def bake_gizmos_recursively(in_group=nuke.Root()):
+def bake_gizmos_recursively(in_group=None):
     """Converting a gizmo to group
 
     Argumets:
         is_group (nuke.Node)[optonal]: group node or all nodes
     """
+    if in_group is None:
+        in_group = nuke.Root()
     # preserve selection after all is done
     with maintained_selection():
         # jump to the group
