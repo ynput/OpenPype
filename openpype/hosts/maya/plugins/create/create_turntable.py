914a7cb1589116edd2704a99c8cb239c687ec2f6
karimmozlia <karimmozilla@gmail.com>
2022-02-11 16:54:58 +0200

    configure anim

--- openpype/hosts/maya/plugins/create/create_turntable.py
+++ openpype/hosts/maya/plugins/create/create_turntable.py
@@ -1,5 +1,8 @@
 from openpype.hosts.maya.api import plugin
 from openpype.api import get_project_settings
+from openpype.lib import get_creator_by_name
+from avalon.api import Session, create
+
 import os
 import platform
 import maya.cmds as cmds
@@ -25,19 +28,34 @@ class CreateTurnTable(plugin.Creator):
         frames = ExtractTurntable_setting ['frames']
         modelTransform = ExtractTurntable_setting["modelTransform"]
         lightTransform = ExtractTurntable_setting["lightTransform"]
+        cameraShape = ExtractTurntable_setting["cameraShape"]
 
-
+        namespace="turntable"
         # reference template file
         cmds.file(path,
                 reference=True,
                 groupReference=True,
-                namespace="",
+                namespace=namespace,
                 groupName="_GRP",
         )
-        # append selected model while creation and parent it to model grp
-        cmds.parent(instance, modelTransform)
-
 
         # # get animation time and set key frames
-        # cmds.setKeyframe(modelTransform, 0, frames/2, 0, 360, linear)
-        # cmds.setKeyframe(lightTransform, frames/2, frames, 0, 360, linear)
+        cmds.setKeyframe(instance, t=1001, v=0.0, at="ry", itt="linear", ott="linear")
+        cmds.setKeyframe(instance, t=int(int(frames)/2)+1001, v=360.0 , at="ry", itt="linear", ott="linear")
+        cmds.setKeyframe(namespace +":"+ modelTransform, t=1001, v=0.0, at="ry", itt="linear", ott="linear")
+        cmds.setKeyframe(namespace +":"+ modelTransform, t=int(int(frames)/2)+1001, v=360.0 , at="ry", itt="linear", ott="linear")
+
+        ## second turn
+        cmds.setKeyframe(namespace +":"+ lightTransform, t=int(int(frames)/2)+1001, v=0.0, at="ry", itt="linear", ott="linear")
+        cmds.setKeyframe(namespace +":"+ lightTransform, t=int(frames)+1001, v=360.0 , at="ry", itt="linear", ott="linear")
+
+        ## fit camera to object
+        fit_factor=0.5
+        cmds.viewFit( namespace +":"+ cameraShape, f=fit_factor)
+
+        cmds.delete(instance)
+        Creator = get_creator_by_name("CreateRender")
+        asset = Session["AVALON_ASSET"]
+        container = create(Creator,
+                    name="renderingTurnTableMain",
+                    asset=asset)
