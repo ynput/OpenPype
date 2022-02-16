a2ff92660ccafb9d05f4460d91874ad4b7479122
Jakub Jezek <jakub@orbi.tools>
2022-02-16 21:30:28 +0100

    flame: validator fix, adding exception

--- openpype/hosts/flame/plugins/publish/validate_source_clip.py
+++ openpype/hosts/flame/plugins/publish/validate_source_clip.py
@@ -1,5 +1,6 @@
 import pyblish
 
+
 @pyblish.api.log
 class ValidateSourceClip(pyblish.api.InstancePlugin):
     """Validate instance is not having empty `flameSourceClip`"""
@@ -12,10 +13,12 @@ class ValidateSourceClip(pyblish.api.InstancePlugin):
     def process(self, instance):
         flame_source_clip = instance.data["flameSourceClip"]
 
+        self.log.debug("_ flame_source_clip: {}".format(flame_source_clip))
+
         if flame_source_clip is None:
-            raise (
+            raise AttributeError((
                 "Timeline segment `{}` is not having "
                 "relative clip in reels. Please make sure "
                 "you push `Save Sources` button in Conform Tab").format(
-                    instance.data["name"]
-                )
+                    instance.data["asset"]
+            ))
