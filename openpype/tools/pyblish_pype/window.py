f6e9d688c61765b098d09a15633700cbf3b25e1d
Jakub Trllo <jakub.trllo@gmail.com>
2022-02-22 15:42:02 +0100

    fix visibility of placeholder after reset

--- openpype/tools/pyblish_pype/window.py
+++ openpype/tools/pyblish_pype/window.py
@@ -1138,8 +1138,8 @@ class Window(QtWidgets.QDialog):
         if self.intent_model.has_items:
             self.intent_box.setCurrentIndex(self.intent_model.default_index)
 
-        if self.comment_box.text():
-            self.comment_box.placeholder.setVisible(False)
+        self.comment_box.placeholder.setVisible(False)
+        if not self.comment_box.text():
             self.comment_box.placeholder.setVisible(True)
         # Launch controller reset
         self.controller.reset()
