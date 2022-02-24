e0670d34eb9a6779f959f5d908247928806afeea
Jakub Jezek <jakub@orbi.tools>
2022-02-24 11:42:07 +0100

    flame: fixing getting already created entity

--- openpype/hosts/flame/startup/openpype_babypublisher/modules/ftrack_lib.py
+++ openpype/hosts/flame/startup/openpype_babypublisher/modules/ftrack_lib.py
@@ -389,13 +389,17 @@ class FtrackEntityOperator:
         return entity
 
     def get_ftrack_entity(self, session, type, name, parent):
-        query = '{} where name is "{}" and project_id is "{}"'.format(
+        query_no_parent = '{} where name is "{}" and project_id is "{}"'.format(
             type, name, self.project_entity["id"])
+        query_with_parent = (
+            '{} where name is "{}" and project_id is "{}" '
+            'and parent_id is {}').format(
+                type, name, self.project_entity["id"], parent["id"])
 
-        try:
-            entity = session.query(query).one()
-        except Exception:
-            entity = None
+        entity = (
+            session.query(query_no_parent).first() or
+            session.query(query_with_parent).first()
+        )
 
         # if entity doesnt exist then create one
         if not entity:
