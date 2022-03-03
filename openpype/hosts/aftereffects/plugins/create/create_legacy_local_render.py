a27119bee40d29725eea5493e1b2004d1813669d
Petr Kalis <petr.kalis@gmail.com>
2022-03-03 19:26:04 +0100

    OP-2765 - renamed old creators

--- openpype/hosts/aftereffects/plugins/create/create_legacy_local_render.py
+++ openpype/hosts/aftereffects/plugins/create/create_legacy_local_render.py
@@ -1,7 +1,7 @@
-from openpype.hosts.aftereffects.plugins.create import create_render
+from openpype.hosts.aftereffects.plugins.create import create_legacy_render
 
 
-class CreateLocalRender(create_render.CreateRender):
+class CreateLocalRender(create_legacy_render.CreateRender):
     """ Creator to render locally.
 
         Created only after default render on farm. So family 'render.local' is
