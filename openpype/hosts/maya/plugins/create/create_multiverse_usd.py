d155f6024903070ebbcfd942e89dff49541a59e7
Bo Zhou <Bo.Schwarzstein@gmail.com>
2022-03-08 12:11:40 +0900

    fill attributes to creator CreateMultiverseUsd

--- openpype/hosts/maya/plugins/create/create_multiverse_usd.py
+++ openpype/hosts/maya/plugins/create/create_multiverse_usd.py
@@ -1,4 +1,4 @@
-from openpype.hosts.maya.api import plugin
+from openpype.hosts.maya.api import plugin, lib
 
 
 class CreateMultiverseUsd(plugin.Creator):
@@ -8,3 +8,49 @@ class CreateMultiverseUsd(plugin.Creator):
     label = "Multiverse USD"
     family = "usd"
     icon = "cubes"
+
+    def __init__(self, *args, **kwargs):
+        super(CreateMultiverseUsd, self).__init__(*args, **kwargs)
+
+        self.data["stripNamespaces"] = False
+        self.data["mergeTransformAndShape"] = False
+        self.data["writeAncestors"] = True
+        self.data["flattenParentXforms"] = False
+        self.data["writeSparseOverrides"] = False
+        self.data["useMetaPrimPath"] = False
+        self.data["customRootPath"] = ''
+        self.data["customAttributes"] = ''
+        self.data["nodeTypesToIgnore"] = ''
+        self.data["writeMeshes"] = True
+        self.data["writeCurves"] = True
+        self.data["writeParticles"] = True
+        self.data["writeCameras"] = False
+        self.data["writeLights"] = False
+        self.data["writeJoints"] = False
+        self.data["writeCollections"] = False
+        self.data["writePositions"] = True
+        self.data["writeNormals"] = True
+        self.data["writeUVs"] = True
+        self.data["writeColorSets"] = False
+        self.data["writeTangents"] = False
+        self.data["writeRefPositions"] = False
+        self.data["writeBlendShapes"] = False
+        self.data["writeDisplayColor"] = False
+        self.data["writeSkinWeights"] = False
+        self.data["writeMaterialAssignment"] = False
+        self.data["writeHardwareShader"] = False
+        self.data["writeShadingNetworks"] = False
+        self.data["writeTransformMatrix"] = True
+        self.data["writeUsdAttributes"] = False
+        self.data["timeVaryingTopology"] = False
+        self.data["customMaterialNamespace"] = ''
+
+        animation_data = lib.collect_animation_data()
+
+        self.data["writeTimeRange"] = False
+        self.data["timeRangeStart"] = animation_data["frameStart"]
+        self.data["timeRangeEnd"] = animation_data["frameEnd"]
+        self.data["timeRangeIncrement"] = animation_data["step"]
+        self.data["timeRangeNumTimeSamples"] = 0
+        self.data["timeRangeSamplesSpan"] = 0.0
+        self.data["timeRangeFramesPerSecond"] = 24.0
