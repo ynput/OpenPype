fba9399bd3cef9ea5043f312903684bc88b0cab1
Bo Zhou <Bo.Schwarzstein@gmail.com>
2022-03-22 12:31:09 +0900

    use values from processed options from multiverse usd extractor

--- openpype/hosts/maya/plugins/publish/extract_multiverse_usd.py
+++ openpype/hosts/maya/plugins/publish/extract_multiverse_usd.py
@@ -183,10 +183,10 @@ class ExtractMultiverseUsd(openpype.api.Extractor):
                 'step',
                 'fps'
             ]
-            for key, _value in options_items():
+            for key, value in options_items():
                 if key in options_discard_keys:
                     continue
-                setattr(asset_write_opts, key, instance.data[key])
+                setattr(asset_write_opts, key, value)
 
             multiverse.WriteAsset(file_path, members, asset_write_opts)
 
