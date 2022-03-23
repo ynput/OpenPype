85b49da44e14ec82a93e43bd4f8f1571b403627a
Pype Club <license@pype.club>
2022-03-23 12:11:48 +0100

    OP-2766 - skip non active instances

--- openpype/hosts/photoshop/plugins/publish/collect_instances.py
+++ openpype/hosts/photoshop/plugins/publish/collect_instances.py
@@ -35,27 +35,30 @@ class CollectInstances(pyblish.api.ContextPlugin):
         layers_meta = stub.get_layers_metadata()
         instance_names = []
         for layer_item in layer_items:
-            layer_instance_data = stub.read(layer_item, layers_meta)
+            layer_meta_data = stub.read(layer_item, layers_meta)
 
             # Skip layers without metadata.
-            if layer_instance_data is None:
+            if layer_meta_data is None:
                 continue
 
             # Skip containers.
-            if "container" in layer_instance_data["id"]:
+            if "container" in layer_meta_data["id"]:
+                continue
+
+            if not layer_meta_data.get("active", True):  # active might not be in legacy meta
                 continue
 
             instance = instance_by_layer_id.get(str(layer_item.id))
             if instance is None:
-                instance = context.create_instance(layer_instance_data["subset"])
+                instance = context.create_instance(layer_meta_data["subset"])
 
             instance.data["layer"] = layer_item
-            instance.data.update(layer_instance_data)
+            instance.data.update(layer_meta_data)
             instance.data["families"] = self.families_mapping[
-                layer_instance_data["family"]
+                layer_meta_data["family"]
             ]
             instance.data["publish"] = layer_item.visible
-            instance_names.append(layer_instance_data["subset"])
+            instance_names.append(layer_meta_data["subset"])
 
             # Produce diagnostic message for any graphical
             # user interface interested in visualising it.
