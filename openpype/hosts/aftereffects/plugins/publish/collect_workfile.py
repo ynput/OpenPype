349827b3a20a718130c214057081f0fdcaa9e41f
Petr Kalis <petr.kalis@gmail.com>
2022-03-25 12:00:37 +0100

    OP-2765 - cleaned up workfile collector

--- openpype/hosts/aftereffects/plugins/publish/collect_workfile.py
+++ openpype/hosts/aftereffects/plugins/publish/collect_workfile.py
@@ -17,16 +17,37 @@ class CollectWorkfile(pyblish.api.ContextPlugin):
                 existing_instance = instance
                 break
 
-        task = api.Session["AVALON_TASK"]
         current_file = context.data["currentFile"]
         staging_dir = os.path.dirname(current_file)
         scene_file = os.path.basename(current_file)
+        if existing_instance is None:  # old publish
+            instance = self._get_new_instance(context, scene_file)
+        else:
+            instance = existing_instance
+
+        # creating representation
+        representation = {
+            'name': 'aep',
+            'ext': 'aep',
+            'files': scene_file,
+            "stagingDir": staging_dir,
+        }
+
+        instance.data["representations"].append(representation)
+
+    def _get_new_instance(self, context, scene_file):
+        task = api.Session["AVALON_TASK"]
         version = context.data["version"]
         asset_entity = context.data["assetEntity"]
         project_entity = context.data["projectEntity"]
 
-        shared_instance_data = {
+        # workfile instance
+        family = "workfile"
+        subset = family + task.capitalize()   # TOOD use method
+
+        instance_data = {
             "asset": asset_entity["name"],
+            "task": task,
             "frameStart": asset_entity["data"]["frameStart"],
             "frameEnd": asset_entity["data"]["frameEnd"],
             "handleStart": asset_entity["data"]["handleStart"],
@@ -40,37 +61,16 @@ class CollectWorkfile(pyblish.api.ContextPlugin):
                 project_entity["data"]["resolutionHeight"]),
             "pixelAspect": 1,
             "step": 1,
-            "version": version
+            "version": version,
+            "subset": subset,
+            "label": scene_file,
+            "family": family,
+            "families": [family],
+            "representations": list()
         }
 
-        # workfile instance
-        family = "workfile"
-        subset = family + task.capitalize()
-        if existing_instance is None:  # old publish
-            # Create instance
-            instance = context.create_instance(subset)
+        # Create instance
+        instance = context.create_instance(subset)
+        instance.data.update(instance_data)
 
-            # creating instance data
-            instance.data.update({
-                "subset": subset,
-                "label": scene_file,
-                "family": family,
-                "families": [family],
-                "representations": list()
-            })
-
-            # adding basic script data
-            instance.data.update(shared_instance_data)
-        else:
-            instance = existing_instance
-            instance.data["publish"] = True  # for DL
-
-        # creating representation
-        representation = {
-            'name': 'aep',
-            'ext': 'aep',
-            'files': scene_file,
-            "stagingDir": staging_dir,
-        }
-
-        instance.data["representations"].append(representation)
+        return instance
