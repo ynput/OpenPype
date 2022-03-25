f175d77d006d145b1d2a88cdb71e78b26882c9af
Allan Ihsan <allan.ihsan@gmail.com>
2022-03-25 14:57:50 +0300

    remove unused import

--- openpype/pipeline/farm/patterning.py
+++ openpype/pipeline/farm/patterning.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 import re
 
 def match_aov_pattern(self, app, render_file_name):
@@ -9,7 +8,8 @@ def match_aov_pattern(self, app, render_file_name):
     that we are grabbing the render filename string
     from the collection that we have grabbed from exp_files.    
     """
-    
+
+
     if app in self.aov_filter.keys():
         for aov_pattern in self.aov_filter[app]:
             if re.match(aov_pattern, render_file_name):
