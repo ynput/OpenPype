911cfb2a94f1b330531b2905782500c5e8ecc5e2
Allan Ihsan <allan.ihsan@gmail.com>
2022-03-25 15:04:03 +0300

    adds empty line

--- openpype/pipeline/farm/patterning.py
+++ openpype/pipeline/farm/patterning.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import re
 
+
 def match_aov_pattern(self, app, render_file_name):
     """Matching against a AOV pattern in the render files
     In order to match the AOV name
     we must compare against the render filename string 
     that we are grabbing the render filename string
-    from the collection that we have grabbed from exp_files.    
+    from the collection that we have grabbed from exp_files.
     """
     if app in self.aov_filter.keys():
         for aov_pattern in self.aov_filter[app]:
