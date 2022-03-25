4dcf12ee4c7c77af12c1620c756f4453b31c40c6
Petr Kalis <petr.kalis@gmail.com>
2022-03-25 15:30:28 +0100

    OP-2764 - scene should be always saved

--- openpype/hosts/aftereffects/plugins/publish/extract_save_scene.py
+++ openpype/hosts/aftereffects/plugins/publish/extract_save_scene.py
@@ -1,15 +1,16 @@
+import pyblish.api
+
 import openpype.api
 from openpype.hosts.aftereffects.api import get_stub
 
 
-class ExtractSaveScene(openpype.api.Extractor):
+class ExtractSaveScene(pyblish.api.ContextPlugin):
     """Save scene before extraction."""
 
     order = openpype.api.Extractor.order - 0.48
     label = "Extract Save Scene"
     hosts = ["aftereffects"]
-    families = ["workfile"]
 
-    def process(self, instance):
+    def process(self, context):
         stub = get_stub()
         stub.save()
