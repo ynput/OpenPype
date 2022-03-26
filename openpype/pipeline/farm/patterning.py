6cd0423e5f8f0d3cacd9f24d86508c2280d7e90e
Allan Ihsan <allan.ihsan@gmail.com>
2022-03-26 21:31:58 +0300

    remove whitespace

--- openpype/pipeline/farm/patterning.py
+++ openpype/pipeline/farm/patterning.py
@@ -4,17 +4,17 @@ import re
 
 def match_aov_pattern(app, aov_patterns, render_file_name):
     """Matching against a `AOV` pattern in the render files.
-    
+
     In order to match the AOV name we must compare
     against the render filename string that we are
     grabbing the render filename string  from the collection
     that we have grabbed from `exp_files`.
-    
+
     Args:
         app (str): Host name.
         aov_patterns (list): List of AOV patterns from AOV filters.
         render_file_name (str): Incoming file name to match against.
-    
+
     Returns:
         bool: Review state for rendered file (render_file_name).
     """
