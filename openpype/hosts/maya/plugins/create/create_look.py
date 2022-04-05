0bcf353c82df569dc4bab517f3f2713f05f29b71
Allan Ihsan <allan.ihsan@gmail.com>
2022-04-05 20:26:47 +0300

    add redshift texture create option to look creator

--- openpype/hosts/maya/plugins/create/create_look.py
+++ openpype/hosts/maya/plugins/create/create_look.py
@@ -12,6 +12,7 @@ class CreateLook(plugin.Creator):
     family = "look"
     icon = "paint-brush"
     make_tx = True
+    rs_tex = False
 
     def __init__(self, *args, **kwargs):
         super(CreateLook, self).__init__(*args, **kwargs)
@@ -20,6 +21,7 @@ class CreateLook(plugin.Creator):
 
         # Whether to automatically convert the textures to .tx upon publish.
         self.data["maketx"] = self.make_tx
-
+        # Whether to automatically convert the textures to .rstex upon publish.
+        self.data["rstex"] = self.rs_tex
         # Enable users to force a copy.
         self.data["forceCopy"] = False
