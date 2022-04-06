586a429beabec5b7c71ee44ba6896f383ae4a2e6
Petr Kalis <petr.kalis@gmail.com>
2022-04-06 14:15:56 +0200

    Added basic test for alternate site method

---
+++ tests/unit/openpype/modules/sync_server/test_module_api.py
@@ -0,0 +1,59 @@
+"""Test file for Sync Server, tests API methods, currently for integrate_new
+
+    File:
+        creates temporary directory and downloads .zip file from GDrive
+        unzips .zip file
+        uses content of .zip file (MongoDB's dumps) to import to new databases
+        with use of 'monkeypatch_session' modifies required env vars
+            temporarily
+        runs battery of tests checking that site operation for Sync Server
+            module are working
+        removes temporary folder
+        removes temporary databases (?)
+"""
+import pytest
+
+import sys, os
+
+os.environ["OPENPYPE_MONGO"] = "mongodb://localhost:27017"
+os.environ["AVALON_MONGO"] = "mongodb://localhost:27017"
+os.environ["OPENPYPE_DATABASE_NAME"] = "openpype"
+os.environ["AVALON_TIMEOUT"] = '3000'
+os.environ["OPENPYPE_DEBUG"] = "3"
+
+os.environ["AVALON_PROJECT"] = "petr_test"
+os.environ["AVALON_DB"] = "avalon"
+os.environ["QT_PREFERRED_BINDING"] = "PySide2"
+os.environ["QT_VERBOSE"] = "true"
+
+from tests.lib.testing_classes import ModuleUnitTest
+
+
+class TestModuleApi(ModuleUnitTest):
+
+    REPRESENTATION_ID = "60e578d0c987036c6a7b741d"
+
+    TEST_FILES = [("1eCwPljuJeOI8A3aisfOIBKKjcmIycTEt",
+                   "test_site_operations.zip", '')]
+
+    @pytest.fixture(scope="module")
+    def setup_sync_server_module(self, dbcon):
+        """Get sync_server_module from ModulesManager"""
+        from openpype.modules import ModulesManager
+
+        manager = ModulesManager()
+        sync_server = manager.modules_by_name["sync_server"]
+        yield sync_server
+
+    def test_get_alt_site_pairs(self, setup_sync_server_module):
+        conf_sites = {'SFTP': {"alternative_sites": ["studio"]},
+                      "studio2": {"alternative_sites": ["studio"]}}
+
+        ret = setup_sync_server_module._get_alt_site_pairs(conf_sites)
+        expected = {"SFTP": ["studio", "studio2"],
+                    "studio": ["SFTP", "studio2"],
+                    "studio2": ["studio", "SFTP"]}
+        assert ret == expected, "Not matching result"
+
+
+test_case = TestModuleApi()
