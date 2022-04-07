81b2be514f970e19ccc292bee0afba80f893cb83
Jakub Trllo <jakub.trllo@gmail.com>
2022-04-07 18:47:37 +0200

    changed order of ftrack collectors

--- openpype/modules/ftrack/plugins/publish/collect_ftrack_api.py
+++ openpype/modules/ftrack/plugins/publish/collect_ftrack_api.py
@@ -6,7 +6,7 @@ import avalon.api
 class CollectFtrackApi(pyblish.api.ContextPlugin):
     """ Collects an ftrack session and the current task id. """
 
-    order = pyblish.api.CollectorOrder + 0.4999
+    order = pyblish.api.CollectorOrder + 0.4991
     label = "Collect Ftrack Api"
 
     def process(self, context):
