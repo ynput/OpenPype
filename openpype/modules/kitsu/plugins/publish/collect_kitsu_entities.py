196c81ab78dffdafcca28c00657d11c799a9d7f4
clement.hector <clement.hector@fixstudio.com>
2022-04-08 12:03:43 +0200

    collect all kitsu entities in context

--- openpype/modules/kitsu/plugins/publish/collect_kitsu_entities.py
+++ openpype/modules/kitsu/plugins/publish/collect_kitsu_entities.py
@@ -13,52 +13,31 @@ class CollectKitsuEntities(pyblish.api.ContextPlugin):
 
     def process(self, context):
 
-        os.environ["AVALON_PROJECT"],
-        os.environ["AVALON_ASSET"],
-        os.environ["AVALON_TASK"],
-        os.environ["AVALON_APP_NAME"]
-
         asset_data = context.data["assetEntity"]["data"]
         zoo_asset_data = asset_data.get("zou")
         if not zoo_asset_data:
-            raise
+            raise AssertionError("Zoo asset data not found in OpenPype!")
+        self.log.debug("Collected zoo asset data: {}".format(zoo_asset_data))
+
+        zoo_task_data = asset_data["tasks"][os.environ["AVALON_TASK"]].get("zou")
+        if not zoo_task_data:
+            raise AssertionError("Zoo task data not found in OpenPype!")
+        self.log.debug("Collected zoo task data: {}".format(zoo_task_data))
 
         kitsu_project = gazu.project.get_project(zoo_asset_data["project_id"])
         if not kitsu_project:
-            raise
+            raise AssertionError("Project not not found in kitsu!")
         context.data["kitsu_project"] = kitsu_project
+        self.log.debug("Collect kitsu project: {}".format(kitsu_project))
 
-        kitsu_asset = gazu.asset.get_asset(zoo_asset_data["entity_type_id"])
+        kitsu_asset = gazu.asset.get_asset(zoo_asset_data["id"])
         if not kitsu_asset:
-            raise
+            raise AssertionError("Asset not not found in kitsu!")
         context.data["kitsu_asset"] = kitsu_asset
+        self.log.debug("Collect kitsu asset: {}".format(kitsu_asset))
 
-        # kitsu_task_type = gazu.task.get_task_type_by_name(instance.data["task"])
-        # if not kitsu_task_type:
-        #     raise
-        # context.data["kitsu_task_type"] = kitsu_task_type
-
-        zoo_task_data = asset_data["tasks"][os.environ["AVALON_TASK"]].get("zou")
-        kitsu_task = gazu.task.get_task(
-            asset_data["zou"],
-            kitsu_task_type
-        )
+        kitsu_task = gazu.task.get_task(zoo_task_data["id"]) 
         if not kitsu_task:
-            raise
+            raise AssertionError("Task not not found in kitsu!")
         context.data["kitsu_task"] = kitsu_task
-
-        wip = gazu.task.get_task_status_by_short_name("wip")
-
-        task = gazu.task.get_task_by_name(asset, modeling)
-        comment = gazu.task.add_comment(task, wip, "Change status to work in progress")
-
-        person = gazu.person.get_person_by_desktop_login("john.doe")
-
-        # task_type = gazu.task.get_task_type_by_name(instance.data["task"])
-
-        # entity_task = gazu.task.get_task_by_entity(
-        #     asset_data["zou"],
-        #     task_type
-        # )
-
-        raise
+        self.log.debug("Collect kitsu task: {}".format(kitsu_task))
