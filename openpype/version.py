bb222642fa295e46357f1a6cc364ac34b5f6bce1
Jakub Jezek <jakub@orbi.tools>
2022-04-08 19:06:32 +0200

    reversing version rename

--- openpype/version.py
+++ openpype/version.py
@@ -1,10 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring Pype version."""
-__version__ = "3.9.3-nightly.1-upp220408"
-
-
-'''
-includes:
-    - Flame: integrate batch groups:
-        https://github.com/pypeclub/OpenPype/pull/2928
-'''
+__version__ = "3.9.3"
