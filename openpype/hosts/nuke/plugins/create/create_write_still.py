094729c3f86db58dd6f28cff75173933f7603f07
Petr Kalis <petr.kalis@gmail.com>
2022-04-19 17:28:08 +0200

    OP-3021 - refactored still image creator
    Not tested yet as it is not working in regular develop either.

--- openpype/hosts/nuke/plugins/create/create_write_still.py
+++ openpype/hosts/nuke/plugins/create/create_write_still.py
@@ -1,12 +1,10 @@
-from collections import OrderedDict
-
 import nuke
 
-from openpype.hosts.nuke.api import plugin
 from openpype.hosts.nuke.api.lib import create_write_node
+from openpype.hosts.nuke.plugins.create import create_write_render
 
 
-class CreateWriteStill(plugin.OpenPypeCreator):
+class CreateWriteStill(create_write_render.CreateWriteRender):
     # change this to template preset
     name = "WriteStillFrame"
     label = "Create Write Still Image"
@@ -23,77 +21,8 @@ class CreateWriteStill(plugin.OpenPypeCreator):
     def __init__(self, *args, **kwargs):
         super(CreateWriteStill, self).__init__(*args, **kwargs)
 
-        data = OrderedDict()
-
-        data["family"] = self.family
-        data["families"] = self.n_class
-
-        for k, v in self.data.items():
-            if k not in data.keys():
-                data.update({k: v})
-
-        self.data = data
-        self.nodes = nuke.selectedNodes()
-        self.log.debug("_ self.data: '{}'".format(self.data))
-
-    def process(self):
-
-        inputs = []
-        outputs = []
-        instance = nuke.toNode(self.data["subset"])
-        selected_node = None
-
-        # use selection
-        if (self.options or {}).get("useSelection"):
-            nodes = self.nodes
-
-            if not (len(nodes) < 2):
-                msg = ("Select only one node. "
-                       "The node you want to connect to, "
-                       "or tick off `Use selection`")
-                self.log.error(msg)
-                nuke.message(msg)
-                return
-
-            if len(nodes) == 0:
-                msg = (
-                    "No nodes selected. Please select a single node to connect"
-                    " to or tick off `Use selection`"
-                )
-                self.log.error(msg)
-                nuke.message(msg)
-                return
-
-            selected_node = nodes[0]
-            inputs = [selected_node]
-            outputs = selected_node.dependent()
-
-            if instance:
-                if (instance.name() in selected_node.name()):
-                    selected_node = instance.dependencies()[0]
-
-        # if node already exist
-        if instance:
-            # collect input / outputs
-            inputs = instance.dependencies()
-            outputs = instance.dependent()
-            selected_node = inputs[0]
-            # remove old one
-            nuke.delete(instance)
-
-        # recreate new
-        write_data = {
-            "nodeclass": self.n_class,
-            "families": [self.family],
-            "avalon": self.data
-        }
-
-        # add creator data
-        creator_data = {"creator": self.__class__.__name__}
-        self.data.update(creator_data)
-        write_data.update(creator_data)
-
-        self.log.info("Adding template path from plugin")
+    def _create_write_node(self, selected_node, inputs, outputs, write_data):
+        # explicitly reset template to 'renders', not same as other 2 writes
         write_data.update({
             "fpath_template": (
                 "{work}/renders/nuke/{subset}/{subset}.{ext}")})
@@ -118,16 +47,9 @@ class CreateWriteStill(plugin.OpenPypeCreator):
             farm=False,
             linked_knobs=["channels", "___", "first", "last", "use_limit"])
 
-        # relinking to collected connections
-        for i, input in enumerate(inputs):
-            write_node.setInput(i, input)
+        return write_node
 
-        write_node.autoplace()
-
-        for output in outputs:
-            output.setInput(0, write_node)
-
-        # link frame hold to group node
+    def _modify_write_node(self, write_node):
         write_node.begin()
         for n in nuke.allNodes():
             # get write node
