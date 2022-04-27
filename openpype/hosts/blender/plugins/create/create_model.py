4570e5088209556a68b15a84b3f41695e2bc618f
Kaa Maurice <kaalium@gmail.com>
2022-04-27 18:15:34 +0200

    create model override library for specific tasks

--- openpype/hosts/blender/plugins/create/create_model.py
+++ openpype/hosts/blender/plugins/create/create_model.py
@@ -1,5 +1,7 @@
 """Create a model asset."""
 
+import bpy
+
 from openpype.pipeline import legacy_io
 from openpype.hosts.blender.api import plugin, lib, ops
 
@@ -24,6 +26,11 @@ class CreateModel(plugin.Creator):
         subset = self.data["subset"]
         name = plugin.asset_name(asset, subset)
 
+        # fectch only child collection from scene root collection
+        only_child_collection = None
+        if len(bpy.context.scene.collection.children) == 1:
+            only_child_collection = bpy.context.scene.collection.children[0]
+
         # Create the container
         container = plugin.create_container(name)
         if container is None:
@@ -33,10 +40,19 @@ class CreateModel(plugin.Creator):
         self.data["task"] = legacy_io.Session.get("AVALON_TASK")
         lib.imprint(container, self.data)
 
-        # Add selected objects to container
+        # Add selected objects to container if useSelection is True
         if (self.options or {}).get("useSelection"):
             # if has no selected object ask for adding all scene content
             selected = lib.get_selection()
             plugin.link_to_collection(selected, container)
+
+        # If only child collection, add this content and remove it.
+        elif only_child_collection:
+            plugin.link_to_collection(
+                list(only_child_collection.objects) +
+                list(only_child_collection.children),
+                container,
+            )
+            bpy.data.collections.remove(only_child_collection)
 
         return container
