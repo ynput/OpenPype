f3fa32f362041428cad3e76d9ba04f91bddc10e2
Kaa Maurice <kaalium@gmail.com>
2022-04-28 16:36:03 +0200

    refactor and optimize Create plugins

--- openpype/hosts/blender/plugins/create/create_model.py
+++ openpype/hosts/blender/plugins/create/create_model.py
@@ -1,9 +1,6 @@
 """Create a model asset."""
 
-import bpy
-
-from openpype.pipeline import legacy_io
-from openpype.hosts.blender.api import plugin, lib, ops
+from openpype.hosts.blender.api import plugin
 
 
 class CreateModel(plugin.Creator):
@@ -13,46 +10,3 @@ class CreateModel(plugin.Creator):
     label = "Model"
     family = "model"
     icon = "cube"
-
-    def process(self):
-        """Run the creator on Blender main thread"""
-        mti = ops.MainThreadItem(self._process)
-        ops.execute_in_main_thread(mti)
-
-    def _process(self):
-
-        # Get info from data and create name value
-        asset = self.data["asset"]
-        subset = self.data["subset"]
-        name = plugin.asset_name(asset, subset)
-
-        # fectch only child collection from scene root collection
-        only_child_collection = None
-        if len(bpy.context.scene.collection.children) == 1:
-            only_child_collection = bpy.context.scene.collection.children[0]
-
-        # Create the container
-        container = plugin.create_container(name)
-        if container is None:
-            raise RuntimeError(f"This instance already exists: {name}")
-
-        # Add custom property on the instance container with the data
-        self.data["task"] = legacy_io.Session.get("AVALON_TASK")
-        lib.imprint(container, self.data)
-
-        # Add selected objects to container if useSelection is True
-        if (self.options or {}).get("useSelection"):
-            # if has no selected object ask for adding all scene content
-            selected = lib.get_selection()
-            plugin.link_to_collection(selected, container)
-
-        # If only child collection, add this content and remove it.
-        elif only_child_collection:
-            plugin.link_to_collection(
-                list(only_child_collection.objects) +
-                list(only_child_collection.children),
-                container,
-            )
-            bpy.data.collections.remove(only_child_collection)
-
-        return container
