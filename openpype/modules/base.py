4fc8617bd1037b1b77b34cb903c85ddbb651a1d4
Roy Nieterau <roy_nieterau@hotmail.com>
2022-05-02 22:32:23 +0200

    Fix typo in comment

--- openpype/modules/base.py
+++ openpype/modules/base.py
@@ -353,7 +353,7 @@ def _load_modules():
             basename, ext = os.path.splitext(filename)
 
             if os.path.isdir(fullpath):
-                # Check existence of init fil
+                # Check existence of init file
                 init_path = os.path.join(fullpath, "__init__.py")
                 if not os.path.exists(init_path):
                     log.debug((
