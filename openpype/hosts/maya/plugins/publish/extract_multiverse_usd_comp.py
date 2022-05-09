b796c2563decaf953f130e93df75d79d8dae54d2
DMO <dmo@j-cube.jp>
2022-05-09 16:52:51 +0900

    PEP Formatting and typo fixing.

--- openpype/hosts/maya/plugins/publish/extract_multiverse_usd_comp.py
+++ openpype/hosts/maya/plugins/publish/extract_multiverse_usd_comp.py
@@ -113,10 +113,10 @@ class ExtractMultiverseUsdComposition(openpype.api.Extractor):
                 time_opts.framePerSecond = fps
 
             comp_write_opts = multiverse.CompositionWriteOptions()
-            if not hasattr(comp_write_opts,"forceAbsolutePaths"):
+            if not hasattr(comp_write_opts, "forceAbsolutePaths"):
                 self.log.warning("multiverse.CompositionWriteOptions is " +
-                    "missing forceAbsolutePaths', extract will yield " +
-                    "inccorect reference paths.")
+                                 "missing 'forceAbsolutePaths', extract " +
+                                 "will yield incorrect reference paths.")
             else:
                 comp_write_opts.forceAbsolutePaths = True
             options_discard_keys = {
