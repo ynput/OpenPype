b2e1c4badec2c833e8d2a75f6a5c5b114faf88cd
Petr Kalis <petr.kalis@gmail.com>
2022-05-18 09:53:59 +0200

    Fix - skip collector in PS when automatic testing

--- openpype/hosts/photoshop/plugins/publish/collect_batch_data.py
+++ openpype/hosts/photoshop/plugins/publish/collect_batch_data.py
@@ -39,8 +39,7 @@ class CollectBatchData(pyblish.api.ContextPlugin):
     def process(self, context):
         self.log.info("CollectBatchData")
         batch_dir = os.environ.get("OPENPYPE_PUBLISH_DATA")
-        if (os.environ.get("IS_TEST") and
-                (not batch_dir or not os.path.exists(batch_dir))):
+        if os.environ.get("IS_TEST"):
             self.log.debug("Automatic testing, no batch data, skipping")
             return
 
