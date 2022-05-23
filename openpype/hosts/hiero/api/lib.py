2e5acf6221e5063517870c93046979e79b243e49
Jakub Jezek <jakubjezek001@gmail.com>
2022-05-23 17:50:09 +0200

    hound

--- openpype/hosts/hiero/api/lib.py
+++ openpype/hosts/hiero/api/lib.py
@@ -2,7 +2,6 @@
 Host specific functions where host api is connected
 """
 
-import contextlib
 from copy import deepcopy
 import os
 import re
@@ -986,7 +985,8 @@ def get_sequence_pattern_and_padding(file):
         return None, None
     found = sorted(list(set(foundall[0])))[-1]
 
-    padding = int(re.findall(r"\d+", found)[-1]) if "%" in found else len(found)
+    padding = int(
+        re.findall(r"\d+", found)[-1]) if "%" in found else len(found)
     return found, padding
 
 
