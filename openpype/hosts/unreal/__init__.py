15822591793cb94ea730941358abb0a06dac211c
Ondrej Samohel <ondrej.samohel@orbi.tools>
2022-05-26 14:07:30 +0200

    :truck: split versions of integration plugin

--- openpype/hosts/unreal/__init__.py
+++ openpype/hosts/unreal/__init__.py
@@ -1,13 +1,19 @@
 import os
 import openpype.hosts
+from openpype.lib.applications import Application
 
 
-def add_implementation_envs(env, _app):
+def add_implementation_envs(env: dict, _app: Application) -> None:
     """Modify environments to contain all required for implementation."""
     # Set OPENPYPE_UNREAL_PLUGIN required for Unreal implementation
+
+    engine_version = _app.name.split("/")[-1].replace("-", ".")
+    major_version = int(engine_version.split(".")[0])
+
+    ue_plugin = "UE_4.7" if major_version == 4 else "UE_5.0"
     unreal_plugin_path = os.path.join(
         os.path.dirname(os.path.abspath(openpype.hosts.__file__)),
-        "unreal", "integration"
+        "unreal", "integration", ue_plugin
     )
     if not env.get("OPENPYPE_UNREAL_PLUGIN"):
         env["OPENPYPE_UNREAL_PLUGIN"] = unreal_plugin_path
