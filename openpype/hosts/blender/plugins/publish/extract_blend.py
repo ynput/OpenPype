11abe0519938155b7dd00072b261a8b706b45161
Kaa Maurice <kaalium@gmail.com>
2022-06-14 17:27:24 +0200

    Fix pack images ans add settings to manage it

--- openpype/hosts/blender/plugins/publish/extract_blend.py
+++ openpype/hosts/blender/plugins/publish/extract_blend.py
@@ -4,7 +4,11 @@ import bpy
 from bson.objectid import ObjectId
 
 import openpype.api
-from openpype.pipeline import discover_loader_plugins, AVALON_CONTAINER_ID
+from openpype.pipeline import (
+    legacy_io,
+    discover_loader_plugins,
+    AVALON_CONTAINER_ID,
+)
 from openpype.pipeline.load.utils import loaders_from_repre_context
 from openpype.hosts.blender.api import plugin
 from openpype.hosts.blender.api.pipeline import (
@@ -48,18 +52,19 @@ class ExtractBlend(openpype.api.Extractor):
                     and mtl_slot.material.node_tree.type == "SHADER"
                 ):
                     materials.add(mtl_slot.material)
-        # Get ShaderNodeTexImage from material node_tree.
-        shader_texture_nodes = set()
+        # Get ShaderNodeTexImage images from material node_tree.
+        images = set()
         for material in materials:
             for node in material.node_tree.nodes:
                 if (
                     isinstance(node, bpy.types.ShaderNodeTexImage)
                     and node.image
                 ):
-                    shader_texture_nodes.add(node)
-        # Pack ShaderNodeTexImage images.
-        for node in shader_texture_nodes:
-            node.image.pack()
+                    images.add(node.image)
+        # Pack images.
+        for image in images:
+            if not image.packed_file:
+                image.pack()
 
     def process(self, instance):
         # Define extract output file path
@@ -67,6 +72,18 @@ class ExtractBlend(openpype.api.Extractor):
         stagingdir = self.staging_dir(instance)
         filename = f"{instance.name}.blend"
         filepath = os.path.join(stagingdir, filename)
+
+        # Get project settings
+        project_settings = openpype.api.get_project_settings(
+            legacy_io.Session["AVALON_PROJECT"]
+        )
+        pack_images = (
+            project_settings
+            ["blender"]
+            ["publish"]
+            ["ExtractBlend"]
+            ["pack_images"]
+        )
 
         # Perform extraction
         self.log.info("Performing extraction..")
@@ -116,7 +133,8 @@ class ExtractBlend(openpype.api.Extractor):
         )
 
         # Pack used images in the blend files.
-        self._pack_images_from_objects(objects)
+        if pack_images:
+            self._pack_images_from_objects(objects)
 
         bpy.ops.file.make_paths_absolute()
         bpy.data.libraries.write(filepath, data_blocks)
