c0eb8c145bbc48134d1ff1d497c0a3d3b177661b
maxpareschi <max.pareschi@gmail.com>
2022-06-14 02:46:16 +0200

    dear hound...

--- openpype/lib/path_tools.py
+++ openpype/lib/path_tools.py
@@ -188,8 +188,8 @@ def compute_paths(basic_paths_items, project_root):
     return output
 
 
-def create_project_folders(basic_paths, project_name, root_paths=[]):
-    if len(root_paths) == 0:
+def create_project_folders(basic_paths, project_name, root_paths=None):
+    if root_paths == None:
         anatomy = Anatomy(project_name)
         if isinstance(anatomy.roots, dict):
             for root in anatomy.roots.values():
