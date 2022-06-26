05592c7bff6bb815f2d81c5f09532f36a5e9a64b
maxpareschi <19264472+maxpareschi@users.noreply.github.com>
2022-06-26 15:50:06 +0200

    rescan frames and collections after slate render

---
+++ openpype/hosts/nuke/plugins/publish/extract_rescan_after_slate.py
@@ -0,0 +1,49 @@
+import os
+import clique
+import pyblish.api
+from openpype.hosts.nuke.api.lib import maintained_selection
+
+
+class RescanAfterSlate(pyblish.api.InstancePlugin):
+    """Rescans extracted representations after slate.
+    Helps integration consistency if files were added
+    during the slate extraction step, since the 
+    Representation file list and collection gets
+    collected only once before it.
+    """
+    label = 'Rescan Repre After Slate'
+    order = pyblish.api.ExtractorOrder + 0.005
+    families = ["slate"]
+    hosts = ['nuke']
+
+    def process(self, instance):
+
+        for repre in instance.data["representations"]:
+
+            if isinstance(repre["files"], str):
+                return
+
+            self.log.debug("Current representation: {0}".format(repre))
+            self.log.debug("Current collection: {0}".format(
+                instance.data["collection"]
+            ))
+
+            staged_files = os.listdir(repre["stagingDir"])
+
+            collected_frames = [x for x in staged_files if repre["ext"] in x]
+
+            if len(repre["files"]) < len(collected_frames):
+                self.log.debug(
+                    "Found more files in staging than in representation," + \
+                    " updating with new files (slate frame)..."
+                )
+                repre["files"] = collected_frames
+                collections, remainder = clique.assemble(collected_frames)
+                if collections:
+                    instance.data["collection"] = collections[0]
+                self.log.debug("Rescanned representation: {0}".format(repre))
+                self.log.debug("Rescanned collection: {0}".format(
+                    instance.data["collection"]
+                ))
+            else:
+                self.log.debug("No range issue found, moving on...")
