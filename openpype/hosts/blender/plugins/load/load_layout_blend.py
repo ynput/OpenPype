94327280741e3cb31df4f561ede3cbe635f46e7d
Kaa Maurice <kaalium@gmail.com>
2022-07-01 15:58:22 +0200

    bugfix load_animation update_layout and namespace issues

--- openpype/hosts/blender/plugins/load/load_layout_blend.py
+++ openpype/hosts/blender/plugins/load/load_layout_blend.py
@@ -1,11 +1,80 @@
 """Load a layout in Blender."""
+from contextlib import contextmanager
+from typing import Dict
 
 import bpy
 
 from openpype import lib
-from openpype.pipeline import legacy_io, legacy_create
+from openpype.pipeline import legacy_io, legacy_create, AVALON_INSTANCE_ID
 from openpype.pipeline.create import get_legacy_creator_by_name
 from openpype.hosts.blender.api import plugin
+from openpype.hosts.blender.api.pipeline import AVALON_PROPERTY
+
+
+class LayoutMaintainer(plugin.ContainerMaintainer):
+    """Overloaded ContainerMaintainer to maintain only needed properties
+    for layout container."""
+
+    maintained_parameters = [
+        "parent",
+        "transforms",
+        "modifiers",
+        "constraints",
+        "targets",
+        "drivers",
+        "actions",
+        "animation_instances",
+    ]
+
+    @contextmanager
+    def maintained_animation_instances(self, objects):
+        """Maintain animation container content during context."""
+        # Store animation instance collections content from scene collection.
+        animation_instances = {
+            collection.name: {
+                "objects": [
+                    obj.name for obj in collection.objects if obj in objects
+                ],
+                "childrens": [
+                    children.name for children in collection.children
+                ],
+            }
+            for collection in plugin.get_children_recursive(
+                bpy.context.scene.collection
+            )
+            if (
+                collection.get(AVALON_PROPERTY)
+                and collection[AVALON_PROPERTY]["id"] == AVALON_INSTANCE_ID
+                and collection[AVALON_PROPERTY]["family"] == "animation"
+            )
+        }
+        import pprint
+        pprint.pprint(animation_instances)
+        try:
+            yield
+        finally:
+            # Restor animation instance collections content.
+            scene_collections = set(
+                plugin.get_children_recursive(bpy.context.scene.collection)
+            )
+            pprint.pprint(scene_collections)
+
+            for instance_name, content in animation_instances.items():
+                # Ensure animation instance still linked to the scene.
+                for collection in scene_collections:
+                    if collection.name == instance_name:
+                        anim_instance = collection
+                        scene_collections.remove(collection)
+                        break
+                else:
+                    continue
+                # Restor content if animation_instance still valid.
+                for collection in scene_collections:
+                    if collection.name in content["childrens"]:
+                        plugin.link_to_collection(collection, anim_instance)
+                for obj in bpy.context.scene.objects:
+                    if obj.name in content["objects"]:
+                        plugin.link_to_collection(obj, anim_instance)
 
 
 class BlendLayoutLoader(plugin.AssetLoader):
@@ -18,6 +87,15 @@ class BlendLayoutLoader(plugin.AssetLoader):
     icon = "code-fork"
     color = "orange"
     color_tag = "COLOR_02"
+
+    update_mainterner = LayoutMaintainer
+
+    def _get_rig_assets(self, asset_group):
+        return [
+            child
+            for child in plugin.get_children_recursive(asset_group)
+            if plugin.is_container(child, family="rig")
+        ]
 
     def _make_local_actions(self, asset_group):
         """Make local for all actions from objects."""
@@ -51,6 +129,18 @@ class BlendLayoutLoader(plugin.AssetLoader):
             # Assign local action.
             obj.animation_data.action = local_action
 
+    def _get_animation_collection(self):
+        for collection in plugin.get_children_recursive(
+            bpy.context.scene.collection
+        ):
+            if (
+                collection.get(AVALON_PROPERTY)
+                and collection[AVALON_PROPERTY]["id"] == AVALON_INSTANCE_ID
+                and collection[AVALON_PROPERTY]["family"] == "animation"
+                and collection[AVALON_PROPERTY]["subset"] == "animationMain"
+            ):
+                return collection
+
     def _create_animation_collection(self, asset_groups, context):
         creator_plugin = get_legacy_creator_by_name("CreateAnimation")
         if not creator_plugin:
@@ -79,13 +169,20 @@ class BlendLayoutLoader(plugin.AssetLoader):
 
         # Create animation collection subset for loaded rig asset groups.
         if legacy_io.Session.get("AVALON_TASK") == "Animation":
-            rig_assets = [
-                child
-                for child in plugin.get_children_recursive(asset_group)
-                if plugin.is_container(child, family="rig")
-            ]
+            rig_assets = self._get_rig_assets(asset_group)
             self._create_animation_collection(rig_assets, context)
 
         plugin.orphans_purge()
 
         return asset_group
+
+    def exec_update(self, container: Dict, representation: Dict):
+        """Update the loaded asset"""
+        asset_group = self._update_process(container, representation)
+
+        # Add new loaded rig asset groups to animationMain collection.
+        if legacy_io.Session.get("AVALON_TASK") == "Animation":
+            rig_assets = self._get_rig_assets(asset_group)
+            animation_collection = self._get_animation_collection()
+            if rig_assets and animation_collection:
+                plugin.link_to_collection(rig_assets, animation_collection)
