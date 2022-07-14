64b4bdaf880a8b639e921045c4c14a2d6b0cd95b
Jakub Trllo <jakub.trllo@gmail.com>
2022-07-14 15:19:25 +0200

    handle review representations using reviewable file input

--- openpype/hosts/traypublisher/plugins/publish/collect_simple_instances.py
+++ openpype/hosts/traypublisher/plugins/publish/collect_simple_instances.py
@@ -1,5 +1,4 @@
 import os
-import json
 import tempfile
 
 import clique
@@ -28,28 +27,6 @@ class CollectSettingsSimpleInstances(pyblish.api.InstancePlugin):
 
     hosts = ["traypublisher"]
 
-    _image_extensions = [
-        ".ani", ".anim", ".apng", ".art", ".bmp", ".bpg", ".bsave", ".cal",
-        ".cin", ".cpc", ".cpt", ".dds", ".dpx", ".ecw", ".exr", ".fits",
-        ".flic", ".flif", ".fpx", ".gif", ".hdri", ".hevc", ".icer",
-        ".icns", ".ico", ".cur", ".ics", ".ilbm", ".jbig", ".jbig2",
-        ".jng", ".jpeg", ".jpeg-ls", ".jpeg", ".2000", ".jpg", ".xr",
-        ".jpeg", ".xt", ".jpeg-hdr", ".kra", ".mng", ".miff", ".nrrd",
-        ".ora", ".pam", ".pbm", ".pgm", ".ppm", ".pnm", ".pcx", ".pgf",
-        ".pictor", ".png", ".psb", ".psp", ".qtvr", ".ras",
-        ".rgbe", ".logluv", ".tiff", ".sgi", ".tga", ".tiff", ".tiff/ep",
-        ".tiff/it", ".ufo", ".ufp", ".wbmp", ".webp", ".xbm", ".xcf",
-        ".xpm", ".xwd"
-    ]
-    _video_extensions = [
-        ".3g2", ".3gp", ".amv", ".asf", ".avi", ".drc", ".f4a", ".f4b",
-        ".f4p", ".f4v", ".flv", ".gif", ".gifv", ".m2v", ".m4p", ".m4v",
-        ".mkv", ".mng", ".mov", ".mp2", ".mp4", ".mpe", ".mpeg", ".mpg",
-        ".mpv", ".mxf", ".nsv", ".ogg", ".ogv", ".qt", ".rm", ".rmvb",
-        ".roq", ".svi", ".vob", ".webm", ".wmv", ".yuv"
-    ]
-    _review_extensions = _image_extensions + _video_extensions
-
     def process(self, instance):
         if not instance.data.get("settings_creator"):
             return
@@ -64,96 +41,132 @@ class CollectSettingsSimpleInstances(pyblish.api.InstancePlugin):
             "Created temp staging directory for instance {}. {}"
         ).format(instance_label, tmp_folder))
 
-        repres = instance.data["representations"]
+        # Store filepaths for validation of their existence
+        source_filepaths = []
+        # Make sure there are no representations with same name
+        repre_names_counter = {}
+        # Store created names for logging
+        repre_names = []
+        # Store set of filepaths per each representation
+        representation_files_mapping = []
+        source = self._create_main_representations(
+            instance,
+            source_filepaths,
+            repre_names_counter,
+            repre_names,
+            representation_files_mapping
+        )
 
+        self._create_review_representation(
+            instance,
+            source_filepaths,
+            repre_names_counter,
+            repre_names,
+            representation_files_mapping
+        )
+
+        instance.data["source"] = source
+        instance.data["sourceFilepaths"] = list(set(source_filepaths))
+
+        self.log.debug(
+            (
+                "Created Simple Settings instance \"{}\""
+                " with {} representations: {}"
+            ).format(
+                instance_label,
+                len(instance.data["representations"]),
+                ", ".join(repre_names)
+            )
+        )
+
+    def _create_main_representations(
+        self,
+        instance,
+        source_filepaths,
+        repre_names_counter,
+        repre_names,
+        representation_files_mapping
+    ):
         creator_attributes = instance.data["creator_attributes"]
-        self.log.info(json.dumps(creator_attributes))
         filepath_items = creator_attributes["filepath"]
         if not isinstance(filepath_items, list):
             filepath_items = [filepath_items]
 
-        # Last found representation is used as source for instance
         source = None
-        # Check if review is enabled and should be created
-        reviewable = creator_attributes.get("reviewable")
-        # Store review representation - first found that can be used for
-        #   review is stored
-        review_representation = None
-        review_path = None
-
-        # Make sure there are no representations with same name
-        repre_names_counter = {}
-        # Store created names for logging
-        _repre_names = []
-        # Store filepaths for validation of their existence
-        source_filepaths = []
-
-        # Create representations
         for filepath_item in filepath_items:
-            filepaths = [
+            # Skip if filepath item does not have filenames
+            if not filepath_item["filenames"]:
+                continue
+
+            filepaths = {
                 os.path.join(filepath_item["directory"], filename)
                 for filename in filepath_item["filenames"]
-            ]
+            }
             source_filepaths.extend(filepaths)
 
             source = self._calculate_source(filepaths)
-            filenames = filepath_item["filenames"]
-            _, ext = os.path.splitext(filenames[0])
-            if len(filenames) == 1:
-                filenames = filenames[0]
-
-            repre_name = repre_ext = ext[1:]
-            if repre_name not in repre_names_counter:
-                repre_names_counter[repre_name] = 2
-            else:
-                counter = repre_names_counter[repre_name]
-                repre_names_counter[repre_name] += 1
-                repre_name = "{}_{}".format(repre_name, counter)
-
-            _repre_names.append('"{}"'.format(repre_name))
-            representation = {
-                "ext": repre_ext,
-                "name": repre_name,
-                "stagingDir": filepath_item["directory"],
-                "files": filenames,
-                "tags": []
-            }
-            repres.append(representation)
-
-            if (
-                reviewable
-                and review_representation is None
-                and ext in self._review_extensions
-            ):
-                review_representation = representation
-                review_path = source
-
-        instance.data["source"] = source
-        instance.data["sourceFilepaths"] = source_filepaths
-
-        if reviewable:
-            self._prepare_review(instance, review_representation, review_path)
-
-        self.log.debug((
-            "Created Simple Settings instance \"{}\""
-            " with {} representations: {}"
-        ).format(instance_label, len(repres), ", ".join(_repre_names)))
-
-    def _calculate_source(self, filepaths):
-        cols, rems = clique.assemble(filepaths)
-        if cols:
-            source = cols[0].format("{head}{padding}{tail}")
-        elif rems:
-            source = rems[0]
+            representation = self._create_representation_data(
+                filepath_item, repre_names_counter, repre_names
+            )
+            instance.data["representations"].append(representation)
+            representation_files_mapping.append(
+                (filepaths, representation, source)
+            )
         return source
 
-    def _prepare_review(self, instance, review_representation, review_path):
-        if not review_representation:
+    def _create_review_representation(
+        self,
+        instance,
+        source_filepaths,
+        repre_names_counter,
+        repre_names,
+        representation_files_mapping
+    ):
+        # Skip review representation creation if there are no representations
+        #   created for "main" part
+        #   - review representation must not be created in that case so
+        #       validation can care about it
+        if not representation_files_mapping:
             self.log.warning((
-                "Didn't find any representation"
-                " that could be used as source for review"
+                "There are missing source representations."
+                " Creation of review representation was skipped."
             ))
             return
+
+        creator_attributes = instance.data["creator_attributes"]
+        review_file_item = creator_attributes["reviewable"]
+        filenames = review_file_item.get("filenames")
+        if not filenames:
+            self.log.debug((
+                "Filepath for review is not defined."
+                " Skipping review representation creation."
+            ))
+            return
+
+        filepaths = {
+            os.path.join(review_file_item["directory"], filename)
+            for filename in filenames
+        }
+        source_filepaths.extend(filepaths)
+        # First try to find out representation with same filepaths
+        #   so it's not needed to create new representation just for review
+        review_representation = None
+        # Review path (only for logging)
+        review_path = None
+        for item in representation_files_mapping:
+            _filepaths, representation, repre_path = item
+            if _filepaths == filepaths:
+                review_representation = representation
+                review_path = repre_path
+                break
+
+        if review_representation is None:
+            self.log.debug("Creating new review representation")
+            review_path = self._calculate_source(filepaths)
+            review_representation = self._create_representation_data(
+                review_file_item, repre_names_counter, repre_names
+            )
+            instance.data["representations"].append(review_representation)
 
         if "review" not in instance.data["families"]:
             instance.data["families"].append("review")
@@ -162,3 +175,49 @@ class CollectSettingsSimpleInstances(pyblish.api.InstancePlugin):
         self.log.debug("Representation {} was marked for review. {}".format(
             review_representation["name"], review_path
         ))
+
+    def _create_representation_data(
+        self, filepath_item, repre_names_counter, repre_names
+    ):
+        """Create new representation data based on file item.
+
+        Args:
+            filepath_item (Dict[str, Any]): Item with information about
+                representation paths.
+            repre_names_counter (Dict[str, int]): Store count of representation
+                names.
+            repre_names (List[str]): All used representation names. For
+                logging purposes.
+
+        Returns:
+            Dict: Prepared base representation data.
+        """
+
+        filenames = filepath_item["filenames"]
+        _, ext = os.path.splitext(filenames[0])
+        if len(filenames) == 1:
+            filenames = filenames[0]
+
+        repre_name = repre_ext = ext[1:]
+        if repre_name not in repre_names_counter:
+            repre_names_counter[repre_name] = 2
+        else:
+            counter = repre_names_counter[repre_name]
+            repre_names_counter[repre_name] += 1
+            repre_name = "{}_{}".format(repre_name, counter)
+        repre_names.append(repre_names)
+        return {
+            "ext": repre_ext,
+            "name": repre_name,
+            "stagingDir": filepath_item["directory"],
+            "files": filenames,
+            "tags": []
+        }
+
+    def _calculate_source(self, filepaths):
+        cols, rems = clique.assemble(filepaths)
+        if cols:
+            source = cols[0].format("{head}{padding}{tail}")
+        elif rems:
+            source = rems[0]
+        return source
