0ebd4b7c9afffa3174ae876f94ca22fa52f83627
Jakub Trllo <jakub.trllo@gmail.com>
2022-07-19 09:57:00 +0200

    added remaining hosts to integrator hosts

--- openpype/plugins/publish/integrate.py
+++ openpype/plugins/publish/integrate.py
@@ -105,7 +105,10 @@ class IntegrateAsset(pyblish.api.InstancePlugin):
 
     label = "Integrate Asset"
     order = pyblish.api.IntegratorOrder
-    hosts = ["maya"]
+    hosts = ["aftereffects", "blender", "celaction", "flame", "harmony",
+             "hiero", "houdini", "nuke", "photoshop", "resolve",
+             "standalonepublisher", "traypublisher", "tvpaint", "unreal",
+             "webpublisher"]
     families = ["workfile",
                 "pointcache",
                 "camera",
