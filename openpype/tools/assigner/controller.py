5c53fc1721f12a75ebb59dc9b2ebe8ad6eff7528
Jakub Trllo <jakub.trllo@gmail.com>
2022-07-21 15:40:16 +0200

    controller has event system

--- openpype/tools/assigner/controller.py
+++ openpype/tools/assigner/controller.py
@@ -1,3 +1,4 @@
+from openpype.lib.events import EventSystem
 from openpype.pipeline import legacy_io
 from .model import AssignerToolModel
 
@@ -5,13 +6,26 @@ from .model import AssignerToolModel
 class AssignerController(object):
     def __init__(self, host):
         self._host = host
+
+        self._event_system = EventSystem()
+        self._register_callbacks()
+
         self._model = AssignerToolModel(self)
+
+    def _register_callbacks(self):
+        self._event_system.add_callback(
+            "container.selection.changed", self.container_selection_changed
+        )
 
     @property
     def host(self):
         """Quick access to related host."""
 
         return self._host
+
+    @property
+    def event_system(self):
+        return self._event_system
 
     @property
     def project_name(self):
