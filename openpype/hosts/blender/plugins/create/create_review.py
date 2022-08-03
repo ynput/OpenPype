0e8e4644c306d5a646804b5fe8bc2d0783f2b59b
Kaa Maurice <kaalium@gmail.com>
2022-08-03 18:08:23 +0200

    auto generate camera for review collection

--- openpype/hosts/blender/plugins/create/create_review.py
+++ openpype/hosts/blender/plugins/create/create_review.py
@@ -1,5 +1,7 @@
 """Create review."""
 
+import bpy
+
 from openpype.hosts.blender.api import plugin
 from openpype.hosts.blender.api.lib import get_selection
 
@@ -16,3 +18,21 @@ class CreateReview(plugin.Creator):
     def _use_selection(self, container):
         selected_objects = set(get_selection())
         plugin.link_to_collection(selected_objects, container)
+
+    def _process(self):
+        # Get Instance Container
+        container = super()._process()
+
+        # Create camera if not get with use_selection.
+        for obj in container.all_objects:
+            if obj.type == "CAMERA":
+                break
+        else:
+            asset = self.data["asset"]
+            subset = self.data["subset"]
+            name = plugin.asset_name(asset, subset)
+            camera = bpy.data.cameras.new(name)
+            camera_obj = bpy.data.objects.new(name, camera)
+            container.objects.link(camera_obj)
+
+        return container
