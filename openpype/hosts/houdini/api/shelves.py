a302caf6bd431b98136ce5b41c56cb0c60e49b4f
Thomas Fricard <tfricard@ws114.prs.vfx.int>
2022-08-10 13:05:37 +0200

    setting shelf set filepath if any in right OS

--- openpype/hosts/houdini/api/shelves.py
+++ openpype/hosts/houdini/api/shelves.py
@@ -1,26 +1,38 @@
 import os
 import logging
+import platform
 
 from openpype.settings import get_project_settings
 
-log = logging.getLogger(__name__)
+import hou
+
+log = logging.getLogger("openpype.hosts.houdini")
 
 
 def generate_shelves():
+    current_os = platform.system().lower()
     # load configuration of custom menu
     project_settings = get_project_settings(os.getenv("AVALON_PROJECT"))
     shelves_set_config = project_settings["houdini"]["shelves"]
 
     if not shelves_set_config:
-        log.warning("No custom shelves found.")
+        log.warning(
+            "SHELF ERROR: No custom shelves found in project settings."
+        )
         return
 
     # run the shelf generator for Houdini
-    for shelf_set in shelves_set_config:
-        pass
-        # if shelf_set_source_path is not None we load the source path and return
+    for shelf_set_config in shelves_set_config:
+        shelf_set_filepath = shelf_set_config.get('shelf_set_source_path')
+        # if shelf_set_source_path is not None we load the source path and continue
+        if shelf_set_filepath[current_os]:
+            hou.shelves.newShelfSet(file_path=shelf_set_filepath[current_os])
+            # hou.ShelfSet.setFilePath(file_path=shelf_set_filepath[operating_system])
+            continue
 
         # if the shelf set name already exists, do nothing, else, create a new one
+        shelf_set_name = shelf_set_config.get('shelf_set_name')
+        shelf_set = get_or_create_shelf_set(shelf_set_name)
 
         # go through each shelf
         # if shelf_file_path exists, load the shelf and return
@@ -35,8 +47,9 @@ def generate_shelves():
         # add the shelf to the shelf set with the shelfs already in it
 
 
-def get_or_create_shelf_set():
-    pass
+def get_or_create_shelf_set(shelf_set_name):
+    log.warning("IN GET OR CREATE SHELF SET: {}".format(shelf_set_name))
+    hou.shelves.shelves()
 
 
 def get_or_create_shelf():
