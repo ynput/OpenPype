ca424baf73db7d1df54d3faabacc032a9362b2c5
Roy Nieterau <roy_nieterau@hotmail.com>
2022-08-12 16:39:20 +0200

    Scene Inventory: Maya add actions to select from or to scene

---
+++ openpype/hosts/maya/plugins/inventory/select_containers.py
@@ -0,0 +1,46 @@
+from maya import cmds
+
+from openpype.pipeline import InventoryAction, registered_host
+from openpype.hosts.maya.api.lib import get_container_members
+
+
+class SelectInScene(InventoryAction):
+    """Select nodes in the scene from selected containers in scene inventory"""
+
+    label = "Select In Scene"
+    icon = "search"
+    color = "#888888"
+    order = 99
+
+    def process(self, containers):
+
+        all_members = []
+        for container in containers:
+            members = get_container_members(container)
+            all_members.extend(members)
+        cmds.select(all_members, replace=True, noExpand=True)
+
+
+class SelectFromScene(InventoryAction):
+    """Select containers in scene inventory from the current scene selection"""
+
+    label = "Select From Scene"
+    icon = "search"
+    color = "#888888"
+    order = 100
+
+    def process(self, containers):
+
+        selection = set(cmds.ls(selection=True, long=True, objectsOnly=True))
+        host = registered_host()
+
+        to_select = []
+        for container in host.ls():
+            members = get_container_members(container)
+            if any(member in selection for member in members):
+                to_select.append(container["objectName"])
+
+        return {
+            "objectNames": to_select,
+            "options": {"clear": True}
+        }
