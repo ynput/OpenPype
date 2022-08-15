e6584a9b940782bb6927e807b6a19412a1fd2fe4
Jakub Trllo <jakub.trllo@gmail.com>
2022-08-15 12:08:20 +0200

    removed pype 2 compatibility

--- openpype/modules/deadline/repository/custom/plugins/GlobalJobPreLoad.py
+++ openpype/modules/deadline/repository/custom/plugins/GlobalJobPreLoad.py
@@ -260,52 +260,6 @@ def pype_command_line(executable, arguments, workingDirectory):
     return executable, arguments, workingDirectory
 
 
-def pype(deadlinePlugin):
-    """Remaps `PYPE_METADATA_FILE` and `PYPE_PYTHON_EXE` environment vars.
-
-    `PYPE_METADATA_FILE` is used on farm to point to rendered data. This path
-    originates on platform from which this job was published. To be able to
-    publish on different platform, this path needs to be remapped.
-
-    `PYPE_PYTHON_EXE` can be used to specify custom location of python
-    interpreter to use for Pype. This is remappeda also if present even
-    though it probably doesn't make much sense.
-
-    Arguments:
-        deadlinePlugin: Deadline job plugin passed by Deadline
-
-    """
-    print(">>> Getting job ...")
-    job = deadlinePlugin.GetJob()
-    # PYPE should be here, not OPENPYPE - backward compatibility!!
-    pype_metadata = job.GetJobEnvironmentKeyValue("PYPE_METADATA_FILE")
-    pype_python = job.GetJobEnvironmentKeyValue("PYPE_PYTHON_EXE")
-    print(">>> Having backward compatible env vars {}/{}".format(pype_metadata,
-                                                                 pype_python))
-    # test if it is pype publish job.
-    if pype_metadata:
-        pype_metadata = RepositoryUtils.CheckPathMapping(pype_metadata)
-        if platform.system().lower() == "linux":
-            pype_metadata = pype_metadata.replace("\\", "/")
-
-        print("- remapping PYPE_METADATA_FILE: {}".format(pype_metadata))
-        job.SetJobEnvironmentKeyValue("PYPE_METADATA_FILE", pype_metadata)
-        deadlinePlugin.SetProcessEnvironmentVariable(
-            "PYPE_METADATA_FILE", pype_metadata)
-
-    if pype_python:
-        pype_python = RepositoryUtils.CheckPathMapping(pype_python)
-        if platform.system().lower() == "linux":
-            pype_python = pype_python.replace("\\", "/")
-
-        print("- remapping PYPE_PYTHON_EXE: {}".format(pype_python))
-        job.SetJobEnvironmentKeyValue("PYPE_PYTHON_EXE", pype_python)
-        deadlinePlugin.SetProcessEnvironmentVariable(
-            "PYPE_PYTHON_EXE", pype_python)
-
-    deadlinePlugin.ModifyCommandLineCallback += pype_command_line
-
-
 def __main__(deadlinePlugin):
     print("*** GlobalJobPreload start ...")
     print(">>> Getting job ...")
@@ -329,5 +283,3 @@ def __main__(deadlinePlugin):
         inject_render_job_id(deadlinePlugin)
     elif openpype_render_job == '1' or openpype_remote_job == '1':
         inject_openpype_environment(deadlinePlugin)
-    else:
-        pype(deadlinePlugin)  # backward compatibility with Pype2
