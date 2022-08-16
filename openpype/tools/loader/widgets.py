48706101137e544dc0ad66eb3f36e153a77ed699
Roy Nieterau <roy_nieterau@hotmail.com>
2022-08-16 10:17:11 +0200

    Report subsets without representation in one go

--- openpype/tools/loader/widgets.py
+++ openpype/tools/loader/widgets.py
@@ -567,12 +567,12 @@ class SubsetWidget(QtWidgets.QWidget):
 
             # Trigger
             project_name = self.dbcon.active_project()
-            subset_names_by_version_id = collections.defaultdict(set)
+            subset_name_by_version_id = dict()
             for item in items:
                 version_id = item["version_document"]["_id"]
-                subset_names_by_version_id[version_id].add(item["subset"])
+                subset_name_by_version_id[version_id] = item["subset"]
 
-            version_ids = set(subset_names_by_version_id.keys())
+            version_ids = set(subset_name_by_version_id.keys())
             repre_docs = get_representations(
                 project_name,
                 representation_names=[representation_name],
@@ -588,10 +588,11 @@ class SubsetWidget(QtWidgets.QWidget):
                 version_id = repre_doc["parent"]
                 version_ids.remove(version_id)
 
-            for version_id in version_ids:
+            if version_ids:
+                # report versions that didn't have valid representation
                 joined_subset_names = ", ".join([
-                    '"{}"'.format(subset)
-                    for subset in subset_names_by_version_id[version_id]
+                    '"{}"'.format(subset_name_by_version_id[version_id])
+                    for version_id in version_ids
                 ])
                 self.echo("Subsets {} don't have representation '{}'".format(
                     joined_subset_names, representation_name
