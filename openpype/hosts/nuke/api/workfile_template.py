367144ecbc5a7c2275285b3975b51d13603379c6
jrsndlr <jrsndlr@gmail.com>
2022-08-17 14:22:07 +0200

    style

--- openpype/hosts/nuke/api/workfile_template.py
+++ openpype/hosts/nuke/api/workfile_template.py
@@ -70,50 +70,52 @@ class WorkfileTemplate():
         the input index that is connected to 'node'.
         '''
 
-        inputNodes = [(i, node.input(i)) for i in range(node.inputs())]
-        outputNodes = []
+        input_nodes = [(i, node.input(i)) for i in range(node.inputs())]
+        output_nodes = []
         deps = nuke.dependentNodes(nuke.INPUTS | nuke.HIDDEN_INPUTS, node)
-        for depNode in deps:
-            for i in range(depNode.inputs()):
-                if depNode.input(i) == node:
-                    outputNodes.append((i, depNode))
-        return (inputNodes, outputNodes)
+        for dep_node in deps:
+            for i in range(dep_node.inputs()):
+                if dep_node.input(i) == node:
+                    output_nodes.append((i, dep_node))
+        return (input_nodes, output_nodes)
 
-    def _swap_nodes(self, targetNode, newNode):
+    def _swap_nodes(self, target_node, new_node):
         '''
         Mimics the Ctrl + Shift + drag-and-drop node functionality in Nuke.
 
-        'targetNode': The node (or node name) to be replaced.
-        'newNode': The node (or node name) that will replace it.
+        'target_node': The node (or node name) to be replaced.
+        'new_node': The node (or node name) that will replace it.
         '''
-        if isinstance(targetNode, str):
-            targetNode = nuke.toNode(targetNode)
-        if isinstance(newNode, str):
-            newNode = nuke.toNode(newNode)
-        if not (isinstance(targetNode, nuke.Node) and
-                isinstance(newNode, nuke.Node)):
+
+        # Convert node names to nodes
+        if isinstance(target_node, str):
+            target_node = nuke.toNode(target_node)
+        if isinstance(new_node, str):
+            new_node = nuke.toNode(new_node)
+        if not (isinstance(target_node, nuke.Node) and
+                isinstance(new_node, nuke.Node)):
             return
 
-        source_node_width = int(newNode.screenWidth() / 2)
-        source_node_height = int(newNode.screenHeight() / 2)
-        target_node_width = int(targetNode.screenWidth() / 2)
-        target_node_height = int(targetNode.screenHeight() / 2)
+        source_node_width = int(new_node.screenWidth() / 2)
+        source_node_height = int(new_node.screenHeight() / 2)
+        target_node_width = int(target_node.screenWidth() / 2)
+        target_node_height = int(target_node.screenHeight() / 2)
         width_offset = target_node_width - source_node_width
         height_offset = target_node_height - source_node_height
-        sourcePos = (newNode.xpos(), newNode.ypos())
-        targetPos = (targetNode.xpos() + width_offset,
-                     targetNode.ypos() + height_offset)
-        inputNodes, outputNodes = self._get_connected_nodes(targetNode)
+        source_pos = (new_node.xpos(), new_node.ypos())
+        target_pos = (target_node.xpos() + width_offset,
+                     target_node.ypos() + height_offset)
+        input_nodes, output_nodes = self._get_connected_nodes(target_node)
         nukescripts.clear_selection_recursive()
-        targetNode.setSelected(True)
+        target_node.setSelected(True)
         nuke.extractSelected()
-        targetNode.setSelected(False)
-        newNode.setXYpos(*targetPos)
-        targetNode.setXYpos(*sourcePos)
-        for inNode in inputNodes:
-            newNode.setInput(*inNode)
-        for index, node in outputNodes:
-            node.setInput(index, newNode)
+        target_node.setSelected(False)
+        new_node.setXYpos(*target_pos)
+        target_node.setXYpos(*source_pos)
+        for inNode in input_nodes:
+            new_node.setInput(*inNode)
+        for index, node in output_nodes:
+            node.setInput(index, new_node)
 
     def _make_temps_from_op_nodes(self, op_nodes):
         '''
@@ -143,7 +145,7 @@ class WorkfileTemplate():
                 'deadlineConcurrentTasks', 'suspend_publish'
                 'channels', 'first', 'last', 'use_limit'],
             'LoadClip':
-                ['avalon:version', 'avalon:frameStart', 'avalon:frameEnd'],
+                [],
             'LoadEffectsInputProcess':
                 ['name']
         }
@@ -165,7 +167,7 @@ class WorkfileTemplate():
             knob_is_template.setVisible(False)
             node_name = str(node['name'].value())
 
-            # Loader or Creator?
+            # Make Template node - NoOp
             if str(node['avalon:id'].value()) == 'pyblish.avalon.container':
                 # Node type is pyblish.avalon.container - Loader
                 template_node = nuke.nodes.NoOp(tile_color='0xff7f00ff')
@@ -190,10 +192,12 @@ class WorkfileTemplate():
                 knob_subset.setText(str(node['avalon:subset'].value()))
                 template_label = str(node['avalon:subset'].value())
 
+            # Name the Template node nicely
             template_node['name'].setValue('Template{}'.format(node_count))
             template_node['label'].setValue(template_label)
 
             # Store handpicked knobs and their values - from knobs_to_store
+            # Uses knob type number for later type conversion
             to_store = {}
             if knobs_to_store.get(load_create):
                 for knob_to_store in knobs_to_store[load_create]:
@@ -210,8 +214,8 @@ class WorkfileTemplate():
                             # Some knobs to be stored might not be present
                             log.debug('Node {}: knob to be stored not found'
                                       .format(node_name))
-
             knob_other.setText(str(to_store))
+
             # Add knobs to Template node
             template_node.addKnob(knob_tab)
             template_node.addKnob(knob_load_create)
@@ -220,7 +224,9 @@ class WorkfileTemplate():
             template_node.addKnob(knob_other)
             template_node.addKnob(knob_is_template)
 
+            # Swap (and reconect) just created template node with it's source
             self._swap_nodes(node, template_node)
+            # Delete the source node, we have template instead
             nuke.delete(node)
 
     def _make_op_creator_nodes_from_temps(self):
@@ -257,10 +263,14 @@ class WorkfileTemplate():
         op_nodes = self._get_op_nodes(selected_only=False)
         template_nodes = self._get_op_template_nodes()
 
+        # For all the template nodes, find corresponding template nodes
         for one_template in template_nodes:
+            # Template detail
             load_create = str(one_template['loaderCreator'].value())
             subset = str(one_template['subset'].value())
             is_op_creator = bool(one_template['is_op_creator'].value())
+
+            # Decode templated knobs and values
             knob_dict = {}
             try:
                 knob_dict = ast.literal_eval(
@@ -269,6 +279,7 @@ class WorkfileTemplate():
                 log.warning('Temp node {} knobs failed to parse.'.format(
                     one_template['name'].value()))
 
+            # Find corresponding OP node (target)
             target_node = None
             if is_op_creator:
                 # it is a creator, compare subsets
@@ -292,7 +303,9 @@ class WorkfileTemplate():
                         pass
 
             if target_node:
+                # We have a template and target OP node, swap it
                 self._swap_nodes(one_template, target_node)
+                # Try to set the OP node knobs by the stored template knobs
                 for knob_name, knob in knob_dict.items():
                     try:
                         knob_value = str(knob['value'])
@@ -311,8 +324,6 @@ class WorkfileTemplate():
                         log.debug("knob {} type conversion failed"
                                   .format(knob_name))
                 nuke.delete(one_template)
-
-        return True
 
     def create_template(self):
 
