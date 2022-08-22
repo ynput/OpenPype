8c849670872d1d0be4ad5611e7c21c4d77fff8e1
Jakub Trllo <jakub.trllo@gmail.com>
2022-08-22 18:23:20 +0200

    modified standalone publish action to work also as host module

--- openpype/hosts/standalonepublisher/standalonepublish_module.py
+++ openpype/hosts/standalonepublisher/standalonepublish_module.py
@@ -1,26 +1,26 @@
 import os
 import platform
 import subprocess
+
+import click
+
 from openpype.lib import get_openpype_execute_args
+from openpype.lib.execute import run_detached_process
 from openpype.modules import OpenPypeModule
-from openpype_interfaces import ITrayAction
+from openpype.modules.interfaces import ITrayAction, IHostModule
+
+STANDALONEPUBLISH_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-class StandAlonePublishAction(OpenPypeModule, ITrayAction):
+class StandAlonePublishModule(OpenPypeModule, ITrayAction, IHostModule):
     label = "Publish"
     name = "standalonepublish_tool"
+    host_name = "standalonepublisher"
 
     def initialize(self, modules_settings):
-        import openpype
         self.enabled = modules_settings[self.name]["enabled"]
         self.publish_paths = [
-            os.path.join(
-                openpype.PACKAGE_DIR,
-                "hosts",
-                "standalonepublisher",
-                "plugins",
-                "publish"
-            )
+            os.path.join(STANDALONEPUBLISH_ROOT_DIR, "plugins", "publish")
         ]
 
     def tray_init(self):
@@ -31,19 +31,10 @@ class StandAlonePublishAction(OpenPypeModule, ITrayAction):
 
     def connect_with_modules(self, enabled_modules):
         """Collect publish paths from other modules."""
+
         publish_paths = self.manager.collect_plugin_paths()["publish"]
         self.publish_paths.extend(publish_paths)
 
     def run_standalone_publisher(self):
-        args = get_openpype_execute_args("standalonepublisher")
-        kwargs = {}
-        if platform.system().lower() == "darwin":
-            new_args = ["open", "-na", args.pop(0), "--args"]
-            new_args.extend(args)
-            args = new_args
-
-        detached_process = getattr(subprocess, "DETACHED_PROCESS", None)
-        if detached_process is not None:
-            kwargs["creationflags"] = detached_process
-
-        subprocess.Popen(args, **kwargs)
+        args = get_openpype_execute_args("module", self.name, "launch")
+        run_detached_process(args)
