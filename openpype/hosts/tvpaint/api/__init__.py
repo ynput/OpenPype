dbd983f8c8c35c7d747c247e9593d451b42f6d5d
Jakub Trllo <jakub.trllo@gmail.com>
2022-08-23 10:55:23 +0200

    cleanup imports in api init file

--- openpype/hosts/tvpaint/api/__init__.py
+++ openpype/hosts/tvpaint/api/__init__.py
@@ -1,9 +1,4 @@
 from .communication_server import CommunicationWrapper
-from . import lib
-from . import launch_script
-from . import workio
-from . import pipeline
-from . import plugin
 from .pipeline import (
     TVPaintHost,
 )
@@ -11,12 +6,6 @@ from .pipeline import (
 
 __all__ = (
     "CommunicationWrapper",
-
-    "lib",
-    "launch_script",
-    "workio",
-    "pipeline",
-    "plugin",
 
     "TVPaintHost",
 )
