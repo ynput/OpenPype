a42f7278c2ec1123b2f19b103933182f2e3cd92d
Jakub Trllo <jakub.trllo@gmail.com>
2022-08-24 17:59:46 +0200

    define resolve as module

---
+++ openpype/hosts/resolve/__init__.py
@@ -0,0 +1,6 @@
+from .module import ResolveModule
+
+
+__all__ = (
+    "ResolveModule",
+)
---
+++ openpype/hosts/resolve/module.py
@@ -0,0 +1,24 @@
+import os
+
+from openpype.modules import OpenPypeModule
+from openpype.modules.interfaces import IHostModule
+
+from .utils import RESOLVE_ROOT_DIR
+
+
+class ResolveModule(OpenPypeModule, IHostModule):
+    name = "resolve"
+    host_name = "resolve"
+
+    def initialize(self, module_settings):
+        self.enabled = True
+
+    def get_launch_hook_paths(self, app):
+        if app.host_name != self.host_name:
+            return []
+        return [
+            os.path.join(RESOLVE_ROOT_DIR, "hooks")
+        ]
+
+    def get_workfile_extensions(self):
+        return [".drp"]
