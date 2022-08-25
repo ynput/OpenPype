d8c750777c53a6c18f3dccbe29bab3fc7d251a4f
Kaa Maurice <kaalium@gmail.com>
2022-08-25 11:34:05 +0200

    Fix build workfile

--- openpype/hosts/blender/scripts/build_workfile.py
+++ openpype/hosts/blender/scripts/build_workfile.py
@@ -11,20 +11,15 @@ from openpype.modules import ModulesManager
 from openpype.pipeline import (
     legacy_io,
     legacy_create,
-
     discover_loader_plugins,
     load_container,
     loaders_from_representation,
 )
 
 
-ALL_LOADERS = discover_loader_plugins()
-PROJET_NAME = legacy_io.Session["AVALON_PROJECT"]
-
-
-def load_subset(asset_name, subset_name, loader_type=None):
+def load_subset(project_name, asset_name, subset_name, loader_type=None):
     last_version = get_last_version_by_subset_name(
-        PROJET_NAME,
+        project_name,
         subset_name,
         asset_name=asset_name,
         fields=["_id"],
@@ -34,7 +29,7 @@ def load_subset(asset_name, subset_name, loader_type=None):
 
     repre_blend = next(
         get_representations(
-            PROJET_NAME,
+            project_name,
             version_ids=[last_version["_id"]],
             extensions=["blend"],
         ),
@@ -43,22 +38,24 @@ def load_subset(asset_name, subset_name, loader_type=None):
     if not repre_blend:
         return
 
-    loaders = loaders_from_representation(ALL_LOADERS, repre_blend)
+    all_loaders = discover_loader_plugins(project_name=project_name)
+    loaders = loaders_from_representation(all_loaders, repre_blend)
     for loader in loaders:
         if loader_type and loader_type not in loader.__name__:
             continue
         return load_container(loader, repre_blend)
 
 
-def create_instance(creator_name, instance_name):
+def create_instance(creator_name, instance_name, **options):
     legacy_create(
         get_creator_by_name(creator_name),
         name=instance_name,
         asset=legacy_io.Session.get("AVALON_ASSET"),
+        options=options,
     )
 
 
-def load_casting(shot_name):
+def load_casting(project_name, shot_name):
 
     modules_manager = ModulesManager()
     kitsu_module = modules_manager.modules_by_name.get("kitsu")
@@ -70,53 +67,63 @@ def load_casting(shot_name):
     gazu.client.set_host(os.environ["KITSU_SERVER"])
     gazu.log_in(os.environ["KITSU_LOGIN"], os.environ["KITSU_PWD"])
 
-    project = gazu.project.get_project_by_name(PROJET_NAME)
-    episode = gazu.shot.get_episode_by_name(project, "E01")
-    sequence = gazu.shot.get_sequence_by_name(project, "SQ01", episode=episode)
+    episode_name, sequence_name, shot_name = shot_name.split("_")
+
+    project = gazu.project.get_project_by_name(project_name)
+    episode = gazu.shot.get_episode_by_name(project, episode_name)
+    sequence = gazu.shot.get_sequence_by_name(project, sequence_name, episode=episode)  # noqa E501
     shot = gazu.shot.get_shot_by_name(sequence, shot_name)
     casting = gazu.casting.get_shot_casting(shot)
 
     for actor in casting:
         for i in range(actor["nb_occurences"]):
-            load_subset(actor["asset_name"], "rigMain", loader_type="Link")
+            load_subset(actor["asset_name"], "rigMain", "Link")
 
     gazu.log_out()
 
 
 def build_workfile():
-    task_name = legacy_io.Session.get("AVALON_TASK").lower()
-    asset_name = legacy_io.Session.get("AVALON_ASSET")
+    project = legacy_io.Session["AVALON_PROJECT"]
+    task = legacy_io.Session.get("AVALON_TASK").lower()
+    asset = legacy_io.Session.get("AVALON_ASSET")
 
-    if task_name in ("model", "modeling"):
-        create_instance("CreateModel", "modelMain")
+    if task in ("model", "modeling"):
+        bpy.ops.mesh.primitive_cube_add()
+        bpy.context.object.name = f"{asset}_model"
+        bpy.context.object.data.name = f"{asset}_model"
+        create_instance("CreateModel", "modelMain", useSelection=True)
 
-    elif task_name in (
-        "texture", "texturing", "look", "lookdev", "shader", "shadering"
-    ):
+    elif task in ("texture", "texturing", "look", "lookdev", "shader", "shadering"):  # noqa E501
         create_instance("CreateLook", "lookMain")
-        load_subset(asset_name, "modelMain", loader_type="Append")
+        load_subset(project, asset, "modelMain", "Append")
 
-    elif task_name in ("rig", "rigging"):
-        create_instance("CreateRig", "rigMain")
-        load_subset(asset_name, "modelMain", loader_type="Append")
+    elif task in ("rig", "rigging"):
+        bpy.ops.object.armature_add()
+        bpy.context.object.name = f"{asset}_armature"
+        bpy.context.object.data.name = f"{asset}_armature"
+        create_instance("CreateRig", "rigMain", useSelection=True)
+        load_subset(project, asset, "modelMain", "Append")
 
-    elif task_name == "layout":
+    elif task == "layout":
         create_instance("CreateLayout", "layoutMain")
         create_instance("CreateCamera", "cameraMain")
         # create_instance("CreateReview", "reviewMain")
-        # load_casting(asset_name)
+        try:
+            load_casting(project, asset)
+        except RuntimeError:
+            pass
 
-    elif task_name in ("anim", "animation"):
-        load_subset(asset_name, "layoutMain", loader_type="Append")
-        load_subset(asset_name, "cameraMain", loader_type="Link")
+    elif task in ("anim", "animation"):
+        load_subset(project, asset, "layoutMain", "Append")
+        load_subset(project, asset, "cameraMain", "Link")
         # create_instance("CreateReview", "reviewMain")
 
-    elif task_name in ("lighting", "light", "render", "rendering"):
-        if not load_subset(asset_name, "layoutFromAnim", loader_type="Link"):
-            load_subset(asset_name, "layoutMain", loader_type="Append")
-        if not load_subset(asset_name, "cameraFromAnim", loader_type="Link"):
-            load_subset(asset_name, "cameraMain", loader_type="Link")
-        load_subset(asset_name, "animationMain", loader_type="Link")
+    elif task in ("lighting", "light", "render", "rendering"):
+        if not load_subset(project, asset, "layoutFromAnim", "Link"):
+            load_subset(project, asset, "layoutMain", "Append")
+        if not load_subset(project, asset, "cameraFromAnim", "Link"):
+            load_subset(project, asset, "cameraMain", "Link")
+        load_subset(project, asset, "animationMain", "Link")
     else:
         return False
 
