38932ba3012668e8ca2a239ffa2b68b002d979d6
Jakub Trllo <jakub.trllo@gmail.com>
2022-08-27 10:38:01 +0200

    fixed interface name

--- openpype/hosts/fusion/addon.py
+++ openpype/hosts/fusion/addon.py
@@ -1,11 +1,11 @@
 import os
 from openpype.modules import OpenPypeModule
-from openpype.modules.interfaces import IHostModule
+from openpype.modules.interfaces import IHostAddon
 
 FUSION_HOST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-class FusionAddon(OpenPypeModule, IHostModule):
+class FusionAddon(OpenPypeModule, IHostAddon):
     name = "fusion"
     host_name = "fusion"
 
