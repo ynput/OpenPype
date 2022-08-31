f920e64cc1bc3a009c5cdb6dc22f77202500b4c6
Kayla Man <moonyuet2008@hotmail.cocm>
2022-08-31 20:50:16 +0800

    add lock task workfiles when the user is working on the scene

--- openpype/hosts/maya/api/pipeline.py
+++ openpype/hosts/maya/api/pipeline.py
@@ -490,9 +490,7 @@ def after_file_open():
         comment = " %s is working the same workfile!" % username
         cmds.text(comment, align='center')
         cmds.text(vis=False)
-        cmds.rowColumnLayout(numberOfColumns=3,
-                             columnWidth=[(1, 300), (2, 100)],
-                             columnSpacing=[(2, 10)])
+        cmds.rowColumnLayout(numberOfColumns=3, columnWidth=[(1, 300), (2, 100)], columnSpacing=[(2, 10)])
         cmds.separator(vis=False)
         quit_command = "cmds.quit(force=True);cmds.deleteUI('%s')" % reminder
         cmds.button(label='Ok',command=quit_command)
