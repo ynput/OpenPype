442037e599187959b3fbe5a1bddd3104c0d0b1fd
Jakub Trllo <jakub.trllo@gmail.com>
2022-09-02 16:39:07 +0200

    added dummy functions for op4 client

---
+++ openpype/client/server/entities.py
@@ -0,0 +1,132 @@
+def get_projects(*args, **kwargs):
+    raise NotImplementedError("'get_projects' not implemented")
+
+
+def get_project(*args, **kwargs):
+    raise NotImplementedError("'get_project' not implemented")
+
+
+def get_whole_project(*args, **kwargs):
+    raise NotImplementedError("'get_whole_project' not implemented")
+
+
+def get_asset_by_id(*args, **kwargs):
+    raise NotImplementedError("'get_asset_by_id' not implemented")
+
+
+def get_asset_by_name(*args, **kwargs):
+    raise NotImplementedError("'get_asset_by_name' not implemented")
+
+
+def get_assets(*args, **kwargs):
+    raise NotImplementedError("'get_assets' not implemented")
+
+
+def get_archived_assets(*args, **kwargs):
+    raise NotImplementedError("'get_archived_assets' not implemented")
+
+
+def get_asset_ids_with_subsets(*args, **kwargs):
+    raise NotImplementedError("'get_asset_ids_with_subsets' not implemented")
+
+
+def get_subset_by_id(*args, **kwargs):
+    raise NotImplementedError("'get_subset_by_id' not implemented")
+
+
+def get_subset_by_name(*args, **kwargs):
+    raise NotImplementedError("'get_subset_by_name' not implemented")
+
+
+def get_subsets(*args, **kwargs):
+    raise NotImplementedError("'get_subsets' not implemented")
+
+
+def get_subset_families(*args, **kwargs):
+    raise NotImplementedError("'get_subset_families' not implemented")
+
+
+def get_version_by_id(*args, **kwargs):
+    raise NotImplementedError("'get_version_by_id' not implemented")
+
+
+def get_version_by_name(*args, **kwargs):
+    raise NotImplementedError("'get_version_by_name' not implemented")
+
+
+def get_versions(*args, **kwargs):
+    raise NotImplementedError("'get_versions' not implemented")
+
+
+def get_hero_version_by_id(*args, **kwargs):
+    raise NotImplementedError("'get_hero_version_by_id' not implemented")
+
+
+def get_hero_version_by_subset_id(*args, **kwargs):
+    raise NotImplementedError("'get_hero_version_by_subset_id' not implemented")
+
+
+def get_hero_versions(*args, **kwargs):
+    raise NotImplementedError("'get_hero_versions' not implemented")
+
+
+def get_last_versions(*args, **kwargs):
+    raise NotImplementedError("'get_last_versions' not implemented")
+
+
+def get_last_version_by_subset_id(*args, **kwargs):
+    raise NotImplementedError("'get_last_version_by_subset_id' not implemented")
+
+
+def get_last_version_by_subset_name(*args, **kwargs):
+    raise NotImplementedError(
+        "'get_last_version_by_subset_name' not implemented"
+    )
+
+
+def get_output_link_versions(*args, **kwargs):
+    raise NotImplementedError("'get_output_link_versions' not implemented")
+
+
+def version_is_latest(*args, **kwargs):
+    raise NotImplementedError("'version_is_latest' not implemented")
+
+
+def get_representation_by_id(*args, **kwargs):
+    raise NotImplementedError("'get_representation_by_id' not implemented")
+
+
+def get_representation_by_name(*args, **kwargs):
+    raise NotImplementedError("'get_representation_by_name' not implemented")
+
+
+def get_representations(*args, **kwargs):
+    raise NotImplementedError("'get_representations' not implemented")
+
+
+def get_representation_parents(*args, **kwargs):
+    raise NotImplementedError("'get_representation_parents' not implemented")
+
+
+def get_representations_parents(*args, **kwargs):
+    raise NotImplementedError("'get_representations_parents' not implemented")
+
+
+def get_archived_representations(*args, **kwargs):
+    raise NotImplementedError("'get_archived_representations' not implemented")
+
+
+def get_thumbnail(*args, **kwargs):
+    raise NotImplementedError("'get_thumbnail' not implemented")
+
+
+def get_thumbnails(*args, **kwargs):
+    raise NotImplementedError("'get_thumbnails' not implemented")
+
+
+def get_thumbnail_id_from_source(*args, **kwargs):
+    raise NotImplementedError("'get_thumbnail_id_from_source' not implemented")
+
+
+def get_workfile_info(*args, **kwargs):
+    raise NotImplementedError("'get_workfile_info' not implemented")
