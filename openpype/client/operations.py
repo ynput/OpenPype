0b3c22ccfa1bacf26a10638f487edfdc30288990
Jakub Trllo <jakub.trllo@gmail.com>
2022-09-02 16:38:20 +0200

    fix few imports

--- openpype/client/operations.py
+++ openpype/client/operations.py
@@ -1,5 +1,6 @@
 from openpype import OP4_TEST_ENABLED
 
+from .operations_base import REMOVED_VALUE
 if not OP4_TEST_ENABLED:
     from .mongo.operations import *
     OperationsSession = MongoOperationsSession
