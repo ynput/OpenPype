b88def9aea1fc1a682209ea78edcf5ae87a652e3
Kayla Man <moonyuet2008@hotmail.cocm>
2022-09-05 19:40:30 +0800

    adding and loading maya mel workspace through openpype project setting

--- openpype/hosts/maya/lib.py
+++ openpype/hosts/maya/lib.py
@@ -1,32 +1,7 @@
 import os
 import shutil
 
-import json
 from openpype.settings import get_current_project_settings
-
-def copy_workspace_mel(workdir):
-    # Check that source mel exists
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    src_filepath = os.path.join(current_dir, "resources", "workspace.mel")
-    if not os.path.exists(src_filepath):
-        print("Source mel file does not exist. {}".format(src_filepath))
-        return
-
-    # Skip if workspace.mel already exists
-    dst_filepath = os.path.join(workdir, "workspace.mel")
-    if os.path.exists(dst_filepath):
-        return
-
-    # Create workdir if does not exists yet
-    if not os.path.exists(workdir):
-        os.makedirs(workdir)
-
-    # Copy file
-    print("Copying workspace mel \"{}\" -> \"{}\"".format(
-        src_filepath, dst_filepath
-    ))
-    shutil.copy(src_filepath, dst_filepath)
-
 
 def load_workspace_mel(workdir):
     dst_filepath = os.path.join(workdir, "workspace.mel")
@@ -38,7 +13,7 @@ def load_workspace_mel(workdir):
 
     with open(dst_filepath, "w") as mel_file:
         setting = get_current_project_settings()
-        mel_script = setting["maya"]["mel-workspace"]["scripts"]
+        mel_script = setting["maya"]["mel-workspace"]["definition"]
         for mel in mel_script:
             mel_file.write(mel)
             mel_file.write("\n")
