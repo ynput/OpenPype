797d1ea59da616f84083e21b9b790c056f8f8c29
Jakub Trllo <jakub.trllo@gmail.com>
2022-09-07 13:47:02 +0200

    copied attribute defs widgets into 'openpype.tools'

---
+++ openpype/tools/attribute_defs/__init__.py
@@ -0,0 +1,10 @@
+from .widgets import (
+    create_widget_for_attr_def,
+    AttributeDefinitionsWidget,
+)
+
+
+__all__ = (
+    "create_widget_for_attr_def",
+    "AttributeDefinitionsWidget",
+)
