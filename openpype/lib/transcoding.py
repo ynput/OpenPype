8d9c3f2b5e217fda216f3af3901736ee1ec81e40
Petr Kalis <petr.kalis@gmail.com>
2022-09-22 14:51:29 +0200

    OP-3938 - extracted image an video extensions to lib.transcoding
    Extracted to lib for better reusability.

--- openpype/lib/transcoding.py
+++ openpype/lib/transcoding.py
@@ -42,6 +42,28 @@ XML_CHAR_REF_REGEX_HEX = re.compile(r"&#x?[0-9a-fA-F]+;")
 # Regex to parse array attributes
 ARRAY_TYPE_REGEX = re.compile(r"^(int|float|string)\[\d+\]$")
 
+IMAGE_EXTENSIONS = [
+    ".ani", ".anim", ".apng", ".art", ".bmp", ".bpg", ".bsave", ".cal",
+    ".cin", ".cpc", ".cpt", ".dds", ".dpx", ".ecw", ".exr", ".fits",
+    ".flic", ".flif", ".fpx", ".gif", ".hdri", ".hevc", ".icer",
+    ".icns", ".ico", ".cur", ".ics", ".ilbm", ".jbig", ".jbig2",
+    ".jng", ".jpeg", ".jpeg-ls", ".jpeg", ".2000", ".jpg", ".xr",
+    ".jpeg", ".xt", ".jpeg-hdr", ".kra", ".mng", ".miff", ".nrrd",
+    ".ora", ".pam", ".pbm", ".pgm", ".ppm", ".pnm", ".pcx", ".pgf",
+    ".pictor", ".png", ".psb", ".psp", ".qtvr", ".ras",
+    ".rgbe", ".logluv", ".tiff", ".sgi", ".tga", ".tiff", ".tiff/ep",
+    ".tiff/it", ".ufo", ".ufp", ".wbmp", ".webp", ".xbm", ".xcf",
+    ".xpm", ".xwd"
+]
+
+VIDEO_EXTENSIONS = [
+    ".3g2", ".3gp", ".amv", ".asf", ".avi", ".drc", ".f4a", ".f4b",
+    ".f4p", ".f4v", ".flv", ".gif", ".gifv", ".m2v", ".m4p", ".m4v",
+    ".mkv", ".mng", ".mov", ".mp2", ".mp4", ".mpe", ".mpeg", ".mpg",
+    ".mpv", ".mxf", ".nsv", ".ogg", ".ogv", ".qt", ".rm", ".rmvb",
+    ".roq", ".svi", ".vob", ".webm", ".wmv", ".yuv"
+]
+
 
 def get_transcode_temp_directory():
     """Creates temporary folder for transcoding.
