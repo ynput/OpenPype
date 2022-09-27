10b76fbb0b47d0abbcda25f212eb434f6de62895
Jakub Trllo <jakub.trllo@gmail.com>
2022-09-27 14:13:20 +0200

    added few docstrings and enhancements

--- common/openpype_common/connection/server.py
+++ common/openpype_common/connection/server.py
@@ -5,10 +5,11 @@ from .credentials import load_token, get_last_server
 
 
 class UrlError(Exception):
-    def __init__(self, message, hints=None):
+    def __init__(self, message, title, hints=None):
         if hints is None:
             hints = []
 
+        self.title = title
         self.hints = hints
         super().__init__(message)
 
@@ -20,10 +21,53 @@ def _try_parse_url(url):
         return None
 
 
+def _try_connect_to_server(url):
+    try:
+        # TODO add validation if the url lead to OpenPype server
+        #   - thiw won't validate if the url lead to 'google.com'
+        requests.get(url)
+
+    except BaseException:
+        return False
+    return True
+
+
 def validate_url(url):
+    """Validate url if is valid and server is available.
+
+    Validation checks if can be parsed as url and contains scheme.
+
+    Function will try to autofix url thus will return modified url when
+    connection to server works.
+
+    ```python
+    my_url = "my.server.url"
+    try:
+        # Store new url
+        validated_url = validate_url(my_url)
+
+    except UrlError:
+        # Handle invalid url
+        ...
+    ```
+
+    Args:
+        url (str): Server url.
+
+    Returns:
+        Url which was used to connect to server.
+
+    Raises:
+        UrlError: Error with short description and hints for user.
+    """
+
     stripperd_url = url.strip()
     if not stripperd_url:
-        raise UrlError("Invalid url format", hints=["url seems to be empty"])
+        raise UrlError(
+            "Invalid url format. Url is empty.",
+            title="Invalid url format",
+            hints=["url seems to be empty"]
+        )
 
     # Not sure if this is good idea?
     modified_url = stripperd_url.rstrip("/")
@@ -33,37 +77,51 @@ def validate_url(url):
     ]
     if parsed_url is None:
         raise UrlError(
-            "Invalid url format",
+            "Invalid url format. Url cannot be parsed as url \"{}\".".format(
+                modified_url
+            ),
+            title="Invalid url format",
             hints=universal_hints
         )
 
+    # Try add 'https://' scheme if is missing
+    # - this will trigger UrlError if both will crash
     if not parsed_url.scheme:
         new_url = "https://" + modified_url
-        raise UrlError(
-            "Invalid url format",
-            hints=["did you mean \"{}\"?".format(new_url)]
-        )
+        if _try_connect_to_server(new_url):
+            return new_url
 
-    try:
-        # TODO add validation if the url lead to OpenPype server
-        #   - thiw won't validate if the url lead to 'google.com'
-        response = requests.get(modified_url)
+    if _try_connect_to_server(modified_url):
         return modified_url
-    except BaseException:
-        pass
 
     hints = []
-    if "/" in parsed_url.path:
+    if "/" in parsed_url.path or not parsed_url.scheme:
         new_path = parsed_url.path.split("/")[0]
-        hints.append("did you mean \"{}\"?".format(parsed_url.scheme + new_path))
+        if not parsed_url.scheme:
+            new_path = "https://" + new_path
+
+        hints.append(
+            "did you mean \"{}\"?".format(parsed_url.scheme + new_path)
+        )
 
     raise UrlError(
-        "Couldn't connect to server",
+        "Couldn't connect to server on \"{}\"".format(),
+        title="Couldn't connect to server",
         hints=hints + universal_hints
     )
 
 
 def is_token_valid(url, token):
+    """Check if token is valid.
+
+    Args:
+        url (str): Server url.
+        token (str): User's token.
+
+    Returns:
+        bool: True if token is valid.
+    """
+
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer {}".format(token)
@@ -76,6 +134,18 @@ def is_token_valid(url, token):
 
 
 def login(url, username, password):
+    """Use login to the server to receive token.
+
+    Args:
+        url (str): Server url.
+        username (str): User's username.
+        password (str): User's password.
+
+    Returns:
+        Union[str, None]: User's token if login was successfull.
+            Otherwise 'None'.
+    """
+
     headers = {"Content-Type": "application/json"}
     response = requests.post(
         "{}/api/auth/login".format(url),
@@ -95,7 +165,21 @@ def login(url, username, password):
     return token
 
 
-def set_environments():
+def load_environments():
+    """Load environments on startup.
+
+    Handle environments needed for connection with server. Environments are
+    'OPENPYPE_SERVER_URL' and 'OPENPYPE_TOKEN'.
+
+    Server is looked up from environment. Already set environent is not
+    changed. If environemnt is not filled then last server stored in appdirs
+    is used.
+
+    Token is skipped if url is not available. Otherwise is also checked from
+    env and if is not available then uses 'load_token' to try get token based
+    on server url.
+    """
+
     server_url = os.environ.get("OPENPYPE_SERVER_URL")
     if not server_url:
         server_url = get_last_server()
@@ -110,7 +194,28 @@ def set_environments():
             os.environ["OPENPYPE_TOKEN"] = token
 
 
-def need_login():
+def set_environments(url, token):
+    """Change url and token environemnts in currently running process.
+
+    Args:
+        url (str): New server url.
+        token (str): User's token.
+    """
+
+    os.environ["OPENPYPE_SERVER_URL"] = url or ""
+    os.environ["OPENPYPE_TOKEN"] = token or ""
+
+
+def need_server_or_login():
+    """Check if server url or login to the server are needed.
+
+    It is recommended to call 'load_environments' on startup before this check.
+    But in some cases this function could be called after startup.
+
+    Returns:
+        bool: 'True' if server and token are available. Otherwise 'False'.
+    """
+
     server_url = os.environ.get("OPENPYPE_SERVER_URL")
     if not server_url:
         return True
