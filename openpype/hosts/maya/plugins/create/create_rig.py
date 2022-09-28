affd54bd1ae6603fb0e59fffc3c39c2b08c85e41
Kayla Man <moonyuet2008@hotmail.cocm>
2022-09-28 18:45:47 +0800

    write color sets

--- openpype/hosts/maya/plugins/create/create_rig.py
+++ openpype/hosts/maya/plugins/create/create_rig.py
@@ -13,18 +13,12 @@ class CreateRig(plugin.Creator):
     label = "Rig"
     family = "rig"
     icon = "wheelchair"
-    write_color_sets = False
-    write_face_sets = False
-
-    def __init__(self, *args, **kwargs):
-        super(CreateRig, self).__init__(*args, **kwargs)
-        self.data["writeColorSets"] = self.write_color_sets
-        self.data["writeFaceSets"] = self.write_face_sets
 
     def process(self):
 
         with lib.undo_chunk():
             instance = super(CreateRig, self).process()
+
             self.log.info("Creating Rig instance set up ...")
             controls = cmds.sets(name="controls_SET", empty=True)
             pointcache = cmds.sets(name="out_SET", empty=True)
