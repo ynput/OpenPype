541b9d41f2460e4da3a24f4188ec3309546a992a
Jakub Jezek <jakubjezek001@gmail.com>
2022-09-29 22:25:02 +0200

    nuke: workfile creator cleaning

--- openpype/hosts/nuke/plugins/create/workfile_creator.py
+++ openpype/hosts/nuke/plugins/create/workfile_creator.py
@@ -48,6 +48,7 @@ class WorkfileCreator(AutoCreator):
         instance = CreatedInstance(
             self.family, subset_name, instance_data, self
         )
+        instance.transient_data["node"] = root_node
         self._add_instance_to_context(instance)
 
     def update_instances(self, update_list):
@@ -55,49 +56,6 @@ class WorkfileCreator(AutoCreator):
         pass
 
     def create(self, options=None):
-        existing_instance = None
-        for instance in self.create_context.instances:
-            if instance.family == self.family:
-                existing_instance = instance
-                break
-
-        project_name = legacy_io.Session["AVALON_PROJECT"]
-        asset_name = legacy_io.Session["AVALON_ASSET"]
-        task_name = legacy_io.Session["AVALON_TASK"]
-        host_name = legacy_io.Session["AVALON_APP"]
-
-        self.log.debug("__ existing_instance: {}".format(existing_instance))
-
-        if existing_instance is None:
-            asset_doc = get_asset_by_name(project_name, asset_name)
-            subset_name = self.get_subset_name(
-                self.default_variant, task_name, asset_doc,
-                project_name, host_name
-            )
-            data = {
-                "asset": asset_name,
-                "task": task_name,
-                "variant": self.default_variant
-            }
-            data.update(self.get_dynamic_data(
-                self.default_variant, task_name, asset_doc,
-                project_name, host_name
-            ))
-
-            new_instance = CreatedInstance(
-                self.family, subset_name, data, self
-            )
-            self._add_instance_to_context(new_instance)
-
-        elif (
-            existing_instance["asset"] != asset_name
-            or existing_instance["task"] != task_name
-        ):
-            asset_doc = get_asset_by_name(project_name, asset_name)
-            subset_name = self.get_subset_name(
-                self.default_variant, task_name, asset_doc,
-                project_name, host_name
-            )
-            existing_instance["asset"] = asset_name
-            existing_instance["task"] = task_name
-            existing_instance["subset"] = subset_name
+        # no need to create if it is created
+        # in `collect_instances`
+        pass
