5bafa21ea84da086f078999e4ee67a9f4d181a20
Jakub Jezek <jakubjezek001@gmail.com>
2022-10-05 17:20:19 +0200

    nuke: refactor create write render to new publisher

--- openpype/hosts/nuke/plugins/create/create_write_render.py
+++ openpype/hosts/nuke/plugins/create/create_write_render.py
@@ -1,86 +1,89 @@
+from pprint import pformat
 import nuke
 
+from openpype.lib import (
+    BoolDef,
+    NumberDef,
+    UISeparatorDef,
+    UILabelDef
+)
 from openpype.hosts.nuke.api import plugin
 from openpype.hosts.nuke.api.lib import (
-    create_write_node, create_write_node_legacy)
+    create_write_node)
 
 
-class CreateWriteRender(plugin.AbstractWriteRender):
-    # change this to template preset
-    name = "WriteRender"
+class CreateWriteRender(plugin.NukeWriteCreator):
+    identifier = "create_write_render"
     label = "Create Write Render"
-    hosts = ["nuke"]
-    n_class = "Write"
     family = "render"
     icon = "sign-out"
 
-    # settings
-    fpath_template = "{work}/render/nuke/{subset}/{subset}.{frame}.{ext}"
-    defaults = ["Main", "Mask"]
-    prenodes = {
-        "Reformat01": {
-            "nodeclass": "Reformat",
-            "dependent": None,
-            "knobs": [
-                {
-                    "type": "text",
-                    "name": "resize",
-                    "value": "none"
-                },
-                {
-                    "type": "bool",
-                    "name": "black_outside",
-                    "value": True
-                }
-            ]
-        }
-    }
+    def get_pre_create_attr_defs(self):
+        attr_defs = [
+            BoolDef("use_selection", label="Use selection"),
+            self._get_render_target_enum()
+        ]
+        return attr_defs
 
-    def __init__(self, *args, **kwargs):
-        super(CreateWriteRender, self).__init__(*args, **kwargs)
+    def get_instance_attr_defs(self):
+        attr_defs = [
+            self._get_render_target_enum(),
+            self._get_reviewable_bool()
+        ]
+        if "farm_rendering" in self.instance_attributes:
+            attr_defs.extend([
+                UISeparatorDef(),
+                UILabelDef("Farm rendering attributes"),
+                BoolDef("suspended_publish", label="Suspended publishing"),
+                NumberDef(
+                    "farm_priority",
+                    label="Priority",
+                    minimum=1,
+                    maximum=99,
+                    default=50
+                ),
+                NumberDef(
+                    "farm_chunk",
+                    label="Chunk size",
+                    minimum=1,
+                    maximum=99,
+                    default=10
+                ),
+                NumberDef(
+                    "farm_concurency",
+                    label="Concurent tasks",
+                    minimum=1,
+                    maximum=10,
+                    default=1
+                )
+            ])
+        return attr_defs
 
-    def _create_write_node(self, selected_node, inputs, outputs, write_data):
+    def create_instance_node(self, subset_name, instance_data):
         # add fpath_template
-        write_data["fpath_template"] = self.fpath_template
+        write_data = {
+            "creator": self.__class__.__name__,
+            "subset": subset_name,
+            "fpath_template": self.temp_rendering_path_template
+        }
 
-        # add reformat node to cut off all outside of format bounding box
+        write_data.update(instance_data)
+
         # get width and height
-        try:
-            width, height = (selected_node.width(), selected_node.height())
-        except AttributeError:
+        if self.selected_node:
+            width, height = (
+                self.selected_node.width(), self.selected_node.height())
+        else:
             actual_format = nuke.root().knob('format').value()
             width, height = (actual_format.width(), actual_format.height())
 
-        if not self.is_legacy():
-            return create_write_node(
-                self.data["subset"],
-                write_data,
-                input=selected_node,
-                prenodes=self.prenodes,
-                **{
-                    "width": width,
-                    "height": height
-                }
-            )
-        else:
-            _prenodes = [
-                {
-                    "name": "Reformat01",
-                    "class": "Reformat",
-                    "knobs": [
-                        ("resize", 0),
-                        ("black_outside", 1),
-                    ],
-                    "dependent": None
-                }
-            ]
-
-            return create_write_node_legacy(
-                self.data["subset"],
-                write_data,
-                input=selected_node,
-                prenodes=_prenodes
-            )
-
-    def _modify_write_node(self, write_node):
-        return write_node
+        return create_write_node(
+            subset_name,
+            write_data,
+            input=self.selected_node,
+            prenodes=self.prenodes,
+            **{
+                "width": width,
+                "height": height
+            }
+        )
