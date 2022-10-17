56f59c11ec4b0120ffa33b9fe5d0a06027f062e2
Félix David <felixg.david@gmail.com>
2022-10-17 10:36:33 +0200

    Moved loader matching to handler

--- openpype/hosts/blender/blender_addon/startup/init.py
+++ openpype/hosts/blender/blender_addon/startup/init.py
@@ -1,4 +1,5 @@
 import re
+from typing import List
 
 import bpy
 from bpy.app.handlers import persistent
@@ -6,15 +7,41 @@ from bpy.app.handlers import persistent
 from openpype.hosts.blender.api.pipeline import AVALON_PROPERTY
 from openpype.pipeline import install_host
 from openpype.hosts.blender import api
+from openpype.pipeline.constants import AVALON_CONTAINER_ID
+from openpype.pipeline.load.plugins import (
+    LoaderPlugin,
+    discover_loader_plugins,
+)
+from openpype.pipeline.load.utils import loaders_from_repre_context
 
 camel_case_first = re.compile(r"^[A-Z][a-z]*")
+
+all_loaders = discover_loader_plugins()
+
+
+def get_loader_name(loaders: List[LoaderPlugin], load_type: str) -> str:
+    """Get loader name from list by requested load type.
+
+    Args:
+        loaders (List[LoaderPlugin]): List of available loaders
+        load_type (str): Load type to get loader of
+
+    Returns:
+        str: Loader name
+    """
+    return next(
+        [l.__name__ for l in loaders if l.__name__.startswith(load_type)],
+        None,
+    )
 
 
 @persistent
 def load_handler(scene):
     # Get all types
     all_types = [
-        getattr(bpy.data, datablock) for datablock in dir(bpy.data) if not datablock.startswith("__")
+        getattr(bpy.data, datablock)
+        for datablock in dir(bpy.data)
+        if not datablock.startswith("__")
     ]
     all_instanced_collections = {
         obj.instance_collection
@@ -30,24 +57,32 @@ def load_handler(scene):
                 if not avalon_data or not loader_name:
                     continue
 
+                # Get available loaders
+                all_loaders = discover_loader_plugins()
+                context = {
+                    "subset": {"schema": AVALON_CONTAINER_ID},
+                    "version": {"data": {"families": [avalon_data["family"]]}},
+                    "representation": {"name": "blend"},
+                }
+                loaders = loaders_from_repre_context(all_loaders, context)
+
                 # Instance loader, an instance in OP is necessarily a link
-                current_loading_type = camel_case_first.findall(loader_name)[0]
                 if (
                     type(datablock) is bpy.types.Collection
                     and datablock in all_instanced_collections
                 ):
-                    datablock[AVALON_PROPERTY]["loader"] = loader_name.replace(
-                        current_loading_type, "Instance"
+                    datablock[AVALON_PROPERTY]["loader"] = get_loader_name(
+                        loaders, "Instance"
                     )
                 else:
                     if datablock.library:  # Link loader
-                        datablock[AVALON_PROPERTY][
-                            "loader"
-                        ] = loader_name.replace(current_loading_type, "Link")
+                        datablock[AVALON_PROPERTY]["loader"] = get_loader_name(
+                            loaders, "Link"
+                        )
                     else:  # Append loader
-                        datablock[AVALON_PROPERTY][
-                            "loader"
-                        ] = loader_name.replace(current_loading_type, "Append")
+                        datablock[AVALON_PROPERTY]["loader"] = get_loader_name(
+                            loaders, "Append"
+                        )
 
 
 def register():
