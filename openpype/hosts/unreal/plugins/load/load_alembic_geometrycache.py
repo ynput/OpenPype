148de1b7ba67668de30f5b3cad04c974204311c5
Simone Barbieri <simone.barbieri@hey.com>
2022-10-26 16:12:16 +0100

    Load alembic geometry cache through websocket request

--- openpype/hosts/unreal/plugins/load/load_alembic_geometrycache.py
+++ openpype/hosts/unreal/plugins/load/load_alembic_geometrycache.py
@@ -7,9 +7,7 @@ from openpype.pipeline import (
     AVALON_CONTAINER_ID
 )
 from openpype.hosts.unreal.api import plugin
-from openpype.hosts.unreal.api import pipeline as unreal_pipeline
-
-import unreal  # noqa
+from openpype.hosts.unreal.api import pipeline as up
 
 
 class PointCacheAlembicLoader(plugin.Loader):
@@ -21,47 +19,7 @@ class PointCacheAlembicLoader(plugin.Loader):
     icon = "cube"
     color = "orange"
 
-    def get_task(
-        self, filename, asset_dir, asset_name, replace, frame_start, frame_end
-    ):
-        task = unreal.AssetImportTask()
-        options = unreal.AbcImportSettings()
-        gc_settings = unreal.AbcGeometryCacheSettings()
-        conversion_settings = unreal.AbcConversionSettings()
-        sampling_settings = unreal.AbcSamplingSettings()
-
-        task.set_editor_property('filename', filename)
-        task.set_editor_property('destination_path', asset_dir)
-        task.set_editor_property('destination_name', asset_name)
-        task.set_editor_property('replace_existing', replace)
-        task.set_editor_property('automated', True)
-        task.set_editor_property('save', True)
-
-        # set import options here
-        # Unreal 4.24 ignores the settings. It works with Unreal 4.26
-        options.set_editor_property(
-            'import_type', unreal.AlembicImportType.GEOMETRY_CACHE)
-
-        gc_settings.set_editor_property('flatten_tracks', False)
-
-        conversion_settings.set_editor_property('flip_u', False)
-        conversion_settings.set_editor_property('flip_v', True)
-        conversion_settings.set_editor_property(
-            'scale', unreal.Vector(x=100.0, y=100.0, z=100.0))
-        conversion_settings.set_editor_property(
-            'rotation', unreal.Vector(x=-90.0, y=0.0, z=180.0))
-
-        sampling_settings.set_editor_property('frame_start', frame_start)
-        sampling_settings.set_editor_property('frame_end', frame_end)
-
-        options.geometry_cache_settings = gc_settings
-        options.conversion_settings = conversion_settings
-        options.sampling_settings = sampling_settings
-        task.options = options
-
-        return task
-
-    def load(self, context, name, namespace, data):
+    def load(self, context, name, namespace, options):
         """Load and containerise representation into Content Browser.
 
         This is two step process. First, import FBX to temporary path and
@@ -91,31 +49,68 @@ class PointCacheAlembicLoader(plugin.Loader):
             asset_name = "{}_{}".format(asset, name)
         else:
             asset_name = "{}".format(name)
+        version = context.get('version').get('name')
 
-        tools = unreal.AssetToolsHelpers().get_asset_tools()
-        asset_dir, container_name = tools.create_unique_asset_name(
-            "{}/{}/{}".format(root, asset, name), suffix="")
+        default_conversion = False
+        if options.get("default_conversion"):
+            default_conversion = options.get("default_conversion")
+
+        asset_dir, container_name = up.send_request_literal(
+            "create_unique_asset_name", params=[root, asset, name, version])
 
         container_name += suffix
 
-        unreal.EditorAssetLibrary.make_directory(asset_dir)
+        if not up.send_request_literal(
+                "does_directory_exist", params=[asset_dir]):
+            up.send_request("make_directory", params=[asset_dir])
 
-        frame_start = context.get('asset').get('data').get('frameStart')
-        frame_end = context.get('asset').get('data').get('frameEnd')
+            frame_start = context.get('asset').get('data').get('frameStart')
+            frame_end = context.get('asset').get('data').get('frameEnd')
 
-        # If frame start and end are the same, we increase the end frame by
-        # one, otherwise Unreal will not import it
-        if frame_start == frame_end:
-            frame_end += 1
+            # If frame start and end are the same, we increase the end frame by
+            # one, otherwise Unreal will not import it
+            if frame_start == frame_end:
+                frame_end += 1
 
-        task = self.get_task(
-            self.fname, asset_dir, asset_name, False, frame_start, frame_end)
+            task_properties = [
+                ("filename", up.format_string(self.fname)),
+                ("destination_path", up.format_string(asset_dir)),
+                ("destination_name", up.format_string(asset_name)),
+                ("replace_existing", "False"),
+                ("automated", "True"),
+                ("save", "True")
+            ]
 
-        unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([task])  # noqa: E501
+            options_properties = [
+                ("import_type", "unreal.AlembicImportType.GEOMETRY_CACHE")
+            ]
 
-        # Create Asset Container
-        unreal_pipeline.create_container(
-            container=container_name, path=asset_dir)
+            options_extra_properties = [
+                ("geometry_cache_settings", "flatten_tracks", "False"),
+                ("sampling_settings", "frame_start", str(frame_start)),
+                ("sampling_settings", "frame_end", str(frame_end))
+            ]
+
+            if not default_conversion:
+                options_extra_properties.extend([
+                    ("conversion_settings", "preset", "unreal.AbcConversionPreset.CUSTOM"),
+                    ("conversion_settings", "flip_u", "False"),
+                    ("conversion_settings", "flip_v", "True"),
+                    ("conversion_settings", "rotation", "[0.0, 0.0, 0.0]"),
+                    ("conversion_settings", "scale", "[1.0, 1.0, 1.0]")
+                ])
+
+            up.send_request(
+                "import_abc_task",
+                params=[
+                    str(task_properties),
+                    str(options_properties),
+                    str(options_extra_properties)
+                ])
+
+            # Create Asset Container
+            up.send_request(
+                "create_container", params=[container_name, asset_dir])
 
         data = {
             "schema": "openpype:container-2.0",
@@ -125,58 +120,57 @@ class PointCacheAlembicLoader(plugin.Loader):
             "container_name": container_name,
             "asset_name": asset_name,
             "loader": str(self.__class__.__name__),
-            "representation": context["representation"]["_id"],
-            "parent": context["representation"]["parent"],
+            "representation": str(context["representation"]["_id"]),
+            "parent": str(context["representation"]["parent"]),
             "family": context["representation"]["context"]["family"]
         }
-        unreal_pipeline.imprint(
-            "{}/{}".format(asset_dir, container_name), data)
+        up.send_request(
+            "imprint", params=[f"{asset_dir}/{container_name}", str(data)])
 
-        asset_content = unreal.EditorAssetLibrary.list_assets(
-            asset_dir, recursive=True, include_folder=True
-        )
+        asset_content = up.send_request_literal(
+            "list_assets", params=[asset_dir, "True", "True"])
 
-        for a in asset_content:
-            unreal.EditorAssetLibrary.save_asset(a)
+        up.send_request(
+            "save_listed_assets", params=[str(asset_content)])
 
         return asset_content
 
-    def update(self, container, representation):
-        name = container["asset_name"]
-        source_path = get_representation_path(representation)
-        destination_path = container["namespace"]
+    # def update(self, container, representation):
+    #     name = container["asset_name"]
+    #     source_path = get_representation_path(representation)
+    #     destination_path = container["namespace"]
 
-        task = self.get_task(source_path, destination_path, name, True)
+    #     task = self.get_task(source_path, destination_path, name, True)
 
-        # do import fbx and replace existing data
-        unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([task])
+    #     # do import fbx and replace existing data
+    #     unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([task])
 
-        container_path = "{}/{}".format(container["namespace"],
-                                        container["objectName"])
-        # update metadata
-        unreal_pipeline.imprint(
-            container_path,
-            {
-                "representation": str(representation["_id"]),
-                "parent": str(representation["parent"])
-            })
+    #     container_path = "{}/{}".format(container["namespace"],
+    #                                     container["objectName"])
+    #     # update metadata
+    #     up.imprint(
+    #         container_path,
+    #         {
+    #             "representation": str(representation["_id"]),
+    #             "parent": str(representation["parent"])
+    #         })
 
-        asset_content = unreal.EditorAssetLibrary.list_assets(
-            destination_path, recursive=True, include_folder=True
-        )
+    #     asset_content = unreal.EditorAssetLibrary.list_assets(
+    #         destination_path, recursive=True, include_folder=True
+    #     )
 
-        for a in asset_content:
-            unreal.EditorAssetLibrary.save_asset(a)
+    #     for a in asset_content:
+    #         unreal.EditorAssetLibrary.save_asset(a)
 
-    def remove(self, container):
-        path = container["namespace"]
-        parent_path = os.path.dirname(path)
+    # def remove(self, container):
+    #     path = container["namespace"]
+    #     parent_path = os.path.dirname(path)
 
-        unreal.EditorAssetLibrary.delete_directory(path)
+    #     unreal.EditorAssetLibrary.delete_directory(path)
 
-        asset_content = unreal.EditorAssetLibrary.list_assets(
-            parent_path, recursive=False
-        )
+    #     asset_content = unreal.EditorAssetLibrary.list_assets(
+    #         parent_path, recursive=False
+    #     )
 
-        if len(asset_content) == 0:
-            unreal.EditorAssetLibrary.delete_directory(parent_path)
+    #     if len(asset_content) == 0:
+    #         unreal.EditorAssetLibrary.delete_directory(parent_path)
