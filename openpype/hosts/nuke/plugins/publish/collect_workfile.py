8a2c10e3d1ed558054e204672b219d28c327e0da
Jakub Jezek <jakubjezek001@gmail.com>
2022-10-27 11:20:21 +0200

    Nuke: rewrite collect workfile so we are able to disable its publishing

--- openpype/hosts/nuke/plugins/publish/collect_workfile.py
+++ openpype/hosts/nuke/plugins/publish/collect_workfile.py
@@ -1,69 +1,24 @@
 import os
-
 import nuke
-
 import pyblish.api
-import openpype.api as api
-from openpype.pipeline import KnownPublishError
 
 
 class CollectWorkfile(pyblish.api.InstancePlugin):
     """Collect current script for publish."""
 
-    order = pyblish.api.CollectorOrder - 0.499
+    order = pyblish.api.CollectorOrder - 0.498
     label = "Collect Workfile"
     hosts = ['nuke']
     families = ["workfile"]
 
     def process(self, instance):  # sourcery skip: avoid-builtin-shadow
-        root = nuke.root()
 
+        script_data = instance.context.data["scriptData"]
         current_file = os.path.normpath(nuke.root().name())
-
-        if current_file.lower() == "root":
-            raise KnownPublishError(
-                "Workfile is not correct file name. \n"
-                "Use workfile tool to manage the name correctly."
-            )
 
         # creating instances per write node
         staging_dir = os.path.dirname(current_file)
         base_name = os.path.basename(current_file)
-
-        # Get frame range
-        first_frame = int(root["first_frame"].getValue())
-        last_frame = int(root["last_frame"].getValue())
-
-        handle_start = instance.data["handleStart"]
-        handle_end = instance.data["handleEnd"]
-
-        # Get format
-        format = root['format'].value()
-        resolution_width = format.width()
-        resolution_height = format.height()
-        pixel_aspect = format.pixelAspect()
-
-        script_data = {
-            "frameStart": first_frame + handle_start,
-            "frameEnd": last_frame - handle_end,
-            "resolutionWidth": resolution_width,
-            "resolutionHeight": resolution_height,
-            "pixelAspect": pixel_aspect,
-
-            # backward compatibility handles
-            "handles": handle_start,
-            "handleStart": handle_start,
-            "handleEnd": handle_end,
-            "step": 1,
-            "fps": root['fps'].value(),
-
-            "currentFile": current_file,
-            "version": int(api.get_version_from_path(current_file)),
-
-            "host": pyblish.api.current_host(),
-            "hostVersion": nuke.NUKE_VERSION_STRING
-        }
-        instance.context.data.update(script_data)
 
         # creating representation
         representation = {
@@ -82,4 +37,4 @@ class CollectWorkfile(pyblish.api.InstancePlugin):
         # adding basic script data
         instance.data.update(script_data)
 
-        self.log.info('Publishing script version')
+        self.log.info("Collect script version")
