f23000b1bab9106f0380db4e67eedff3b5de3982
Felix David <felixg.david@gmail.com>
2022-11-01 18:06:21 +0100

    Feature: Pre-Launch convert blend file paths to absolute before opening

--- openpype/hosts/blender/utility_scripts/make_paths_absolute.py
+++ openpype/hosts/blender/utility_scripts/make_paths_absolute.py
@@ -1,4 +1,4 @@
-"""Make all paths relative."""
+"""Make all paths absolute."""
 
 import bpy
 
@@ -7,8 +7,8 @@ from openpype.lib.log import Logger
 if __name__ == "__main__":
     log = Logger().get_logger()
     log.debug(
-        f"Blend file | All paths converted to relative: {bpy.data.filepath}"
+        f"Blend file | All paths converted to absolute: {bpy.data.filepath}"
     )
 
-    bpy.ops.file.make_paths_relative()
+    bpy.ops.file.make_paths_absolute()
     bpy.ops.wm.save_mainfile()
