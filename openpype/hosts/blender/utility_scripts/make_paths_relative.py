1a9419ebeeca53c57c260aac9a8def9769f1fc47
Kaa Maurice <kaalium@gmail.com>
2022-11-01 12:34:11 +0100

    fix make paths relative

--- openpype/hosts/blender/utility_scripts/make_paths_relative.py
+++ openpype/hosts/blender/utility_scripts/make_paths_relative.py
@@ -4,17 +4,5 @@ import bpy
 
 
 if __name__ == "__main__":
-    data_blocks = set(bpy.data.collections)
-
-    for obj in bpy.data.objects:
-        data_blocks.add(obj)
-        # Get reference from override library.
-        if obj.override_library and obj.override_library.reference:
-            data_blocks.add(obj.override_library.reference)
-
-    bpy.data.libraries.write(
-        bpy.data.filepath,
-        data_blocks,
-        path_remap="RELATIVE_ALL",
-        compress=bpy.context.preferences.filepaths.use_file_compression,
-    )
+    bpy.ops.file.make_paths_relative()
+    bpy.ops.wm.save_mainfile()
