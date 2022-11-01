697dcbf68380b2ee6220ac8cabcdadeff4482234
Kaa Maurice <kaalium@gmail.com>
2022-11-01 16:11:01 +0100

    feat: load image as background

--- openpype/hosts/blender/plugins/load/load_image.py
+++ openpype/hosts/blender/plugins/load/load_image.py
@@ -1,13 +1,9 @@
-"""Load audio in Blender."""
-
-from pathlib import Path
-from typing import Dict, Optional
+"""Load image in Blender."""
 
 import bpy
 
 from openpype.hosts.blender.api import plugin
-from openpype.hosts.blender.api.pipeline import AVALON_PROPERTY
-from openpype.pipeline.load.utils import get_representation_path
+from openpype.hosts.blender.api.lib import get_selection
 
 
 class ImageLoader(plugin.AssetLoader):
@@ -38,10 +34,11 @@ class ReferenceLoader(ImageLoader):
     """Load image as Reference."""
 
     label = "Load Reference"
-    icon = "image"
     order = 1
 
-    def process_asset(self, context: dict, *args, **kwargs) -> bpy.types.Object:
+    def process_asset(
+        self, context: dict, *args, **kwargs
+    ) -> bpy.types.Object:
         img = super().process_asset(context, *args, **kwargs)
 
         libpath = self.fname
@@ -50,7 +47,7 @@ class ReferenceLoader(ImageLoader):
 
         group_name = plugin.asset_name(asset, subset)
         asset_group = bpy.data.objects.new(group_name, object_data=None)
-        
+
         asset_group.empty_display_type = "IMAGE"
         asset_group.empty_display_size = 10
         asset_group.empty_image_depth = 'DEFAULT'
@@ -64,3 +61,38 @@ class ReferenceLoader(ImageLoader):
         self._update_metadata(asset_group, context, group_name, libpath)
 
         return asset_group
+
+
+class BackgroundLoader(ImageLoader):
+    """Load image as Background."""
+
+    label = "Load Background"
+    order = 2
+
+    def process_asset(self, *args, **kwargs) -> bpy.types.Image:
+        img = super().process_asset(*args, **kwargs)
+
+        camera = None
+        if bpy.context.object and bpy.context.object.type == "CAMERA":
+            camera = bpy.context.object
+        if not camera:
+            camera = next(
+                (obj for obj in get_selection() if obj.type == "CAMERA"),
+                None,
+            )
+        if not camera:
+            camera = next(
+                (
+                    obj for obj in bpy.context.scene.objects
+                    if obj.type == "CAMERA"
+                ),
+                None,
+            )
+
+        if camera:
+            camera.data.show_background_images = True
+            bkg_img = camera.data.background_images.new()
+            bkg_img.image = img
+            bkg_img.frame_method = "FIT"
+
+        return camera
