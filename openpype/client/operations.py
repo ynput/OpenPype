8628afa10841eb60e2948073626ca1ed221fd951
Jakub Trllo <jakub.trllo@gmail.com>
2022-11-01 19:04:00 +0100

    import some constants from mongo

--- openpype/client/operations.py
+++ openpype/client/operations.py
@@ -7,3 +7,14 @@ if not OP4_TEST_ENABLED:
 
 else:
     from .server.operations import *
+    from .mongo.operations import (
+        CURRENT_PROJECT_SCHEMA,
+        CURRENT_PROJECT_CONFIG_SCHEMA,
+        CURRENT_ASSET_DOC_SCHEMA,
+        CURRENT_SUBSET_SCHEMA,
+        CURRENT_VERSION_SCHEMA,
+        CURRENT_HERO_VERSION_SCHEMA,
+        CURRENT_REPRESENTATION_SCHEMA,
+        CURRENT_WORKFILE_INFO_SCHEMA,
+        CURRENT_THUMBNAIL_SCHEMA
+    )
