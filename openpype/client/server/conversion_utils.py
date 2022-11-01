f6a4f0289ddf51ba5533880b5bef86fb5c9c858b
Jakub Trllo <jakub.trllo@gmail.com>
2022-11-01 19:03:39 +0100

    move conversion sripts to separated file

---
+++ openpype/client/server/conversion_utils.py
@@ -0,0 +1,529 @@
+import datetime
+
+from .constants import (
+    FOLDER_ATTRIBS,
+    DEFAULT_V3_FOLDER_FIELDS,
+    DEFAULT_FOLDER_FIELDS,
+
+    SUBSET_ATTRIBS,
+    DEFAULT_SUBSET_FIELDS,
+
+    VERSION_ATTRIBS_FIELDS,
+    DEFAULT_VERSION_FIELDS,
+
+    REPRESENTATION_ATTRIBS_FIELDS,
+    REPRESENTATION_FILES_FIELDS,
+    DEFAULT_REPRESENTATION_FIELDS,
+)
+
+# --- Project entity ---
+PROJECT_FIELDS_MAPPING_V3_V4 = {
+    "_id": {"name"},
+    "name": {"name"},
+    "data": {"data", "attrib", "code"},
+    "data.library_project": {"library"},
+    "data.code": {"code"},
+    "data.active": {"active"},
+}
+
+# TODO this should not be hardcoded but received from server!!!
+# --- Folder entity ---
+FOLDER_FIELDS_MAPPING_V3_V4 = {
+    "_id": {"id"},
+    "name": {"name"},
+    "label": {"name"},
+    "data": {
+        "parentId", "parents", "active", "tasks", "thumbnailId"
+    } | FOLDER_ATTRIBS_FIELDS,
+    "data.visualParent": {"parentId"},
+    "data.parents": {"parents"},
+    "data.active": {"active"},
+    "data.thumbnail_id": {"thumbnailId"}
+}
+
+# --- Subset entity ---
+SUBSET_FIELDS_MAPPING_V3_V4 = {
+    "_id": {"id"},
+    "name": {"name"},
+    "data.active": {"active"},
+    "parent": {"folderId"}
+}
+
+# --- Version entity ---
+VERSION_FIELDS_MAPPING_V3_V4 = {
+    "_id": {"id"},
+    "name": {"version"},
+    "parent": {"subsetId"}
+}
+
+# --- Representation entity ---
+REPRESENTATION_FIELDS_MAPPING_V3_V4 = {
+    "_id": {"id"},
+    "name": {"name"},
+    "parent": {"versionId"},
+    "context": {"context"},
+    "files": {"files"},
+}
+
+
+def project_fields_v3_to_v4(fields):
+    """Convert project fields from v3 to v4 structure.
+
+    Args:
+        fields (Union[Iterable(str), None]): fields to be converted.
+
+    Returns:
+        Union[Set(str), None]: Converted fields to v4 fields.
+    """
+
+    # TODO config fields
+    # - config.apps
+    # - config.groups
+    if not fields:
+        return None
+
+    output = set()
+    for field in fields:
+        # If config is needed the rest api call must be used
+        if field.startswith("config"):
+            return None
+
+        if field in PROJECT_FIELDS_MAPPING_V3_V4:
+            output |= PROJECT_FIELDS_MAPPING_V3_V4[field]
+
+        elif field.startswith("data"):
+            new_field = "attrib" + field[4:]
+            output.add(new_field)
+
+        else:
+            raise ValueError("Unknown field mapping for {}".format(field))
+
+    if "name" not in output:
+        output.add("name")
+    return output
+
+
+def convert_v4_project_to_v3(project):
+    """Convert Project entity data from v4 structure to v3 structure.
+
+    Args:
+        project (Dict[str, Any]): Project entity queried from v4 server.
+
+    Returns:
+        Dict[str, Any]: Project converted to v3 structure.
+    """
+
+    if not project:
+        return project
+
+    project_name = project["name"]
+    output = {
+        "_id": project_name,
+        "name": project_name,
+        "schema": "openpype:project-3.0"
+    }
+    if "config" in project:
+        output["config"] = project["config"]
+        output["config"]["tasks"] = project.get("taskTypes")
+        output["config"]["apps"] = []
+
+    data = project.get("data") or {}
+
+    for data_key, key in (
+        ("library_project", "library"),
+        ("code", "code"),
+        ("active", "active")
+    ):
+        if key in project:
+            data[data_key] = project[key]
+
+    if "attrib" in project:
+        for key, value in project["attrib"].items():
+            data[key] = value
+
+    if data:
+        output["data"] = data
+    return output
+
+
+def folder_fields_v3_to_v4(fields):
+    """Convert folder fields from v3 to v4 structure.
+
+    Args:
+        fields (Union[Iterable(str), None]): fields to be converted.
+
+    Returns:
+        Union[Set(str), None]: Converted fields to v4 fields.
+    """
+
+    if not fields:
+        return set(DEFAULT_V3_FOLDER_FIELDS)
+
+    output = set()
+    for field in fields:
+        if field in ("schema", "type", "parent"):
+            continue
+
+        if field in FOLDER_FIELDS_MAPPING_V3_V4:
+            output |= FOLDER_FIELDS_MAPPING_V3_V4[field]
+
+        elif field.startswith("data"):
+            field_parts = field.split(".")
+            field_parts.pop(0)
+            data_key = ".".join(field_parts)
+            if data_key == "label":
+                output.add("name")
+
+            elif data_key in ("icon", "color"):
+                continue
+
+            elif data_key.startswith("tasks"):
+                output.add("tasks")
+            elif data_key in FOLDER_ATTRIBS:
+                new_field = "attrib" + field[4:]
+                output.add(new_field)
+            else:
+                print(data_key)
+                raise ValueError("Can't query data for field {}".format(field))
+
+        else:
+            raise ValueError("Unknown field mapping for {}".format(field))
+
+    if "id" not in output:
+        output.add("id")
+    return output
+
+
+def convert_v4_tasks_to_v3(tasks):
+    """Convert v4 task item to v3 task.
+
+    Args:
+        tasks (List[Dict[str, Any]]): Task entites.
+
+    Returns:
+        Dict[str, Dict[str, Any]]: Tasks in v3 variant ready for v3 asset.
+    """
+
+    output = {}
+    for task in tasks:
+        task_name = task["name"]
+        new_task = {
+            "type": task["taskType"]
+        }
+        output[task_name] = new_task
+    return output
+
+
+def convert_v4_folder_to_v3(folder, project_name):
+    """Convert v4 folder to v3 asset.
+
+    Args:
+        folder (Dict[str, Any]): Folder entity data.
+        project_name (str): Project name from which folder was queried.
+
+    Returns:
+        Dict[str, Any]: Converted v4 folder to v3 asset.
+    """
+
+    output = {
+        "_id": folder["id"],
+        "parent": project_name,
+        "type": "asset",
+        "schema": "openpype:asset-3.0"
+    }
+
+    output_data = folder.get("data") or {}
+
+    if "name" in folder:
+        output["name"] = folder["name"]
+        output_data["label"] = folder["name"]
+
+    for data_key, key in (
+        ("visualParent", "parentId"),
+        ("active", "active"),
+        ("thumbnail_id", "thumbnailId")
+    ):
+        if key not in folder:
+            continue
+
+        output_data[data_key] = folder[key]
+
+    if "attrib" in folder:
+        output_data.update(folder["attrib"])
+
+    if "tasks" in folder:
+        if output_data is None:
+            output_data = {}
+        output_data["tasks"] = convert_v4_tasks_to_v3(folder["tasks"])
+
+    output["data"] = output_data
+
+    return output
+
+
+def subset_fields_v3_to_v4(fields):
+    """Convert subset fields from v3 to v4 structure.
+
+    Args:
+        fields (Union[Iterable(str), None]): fields to be converted.
+
+    Returns:
+        Union[Set(str), None]: Converted fields to v4 fields.
+    """
+
+    if not fields:
+        return None
+
+    output = set()
+    for field in fields:
+        if field in ("schema", "type"):
+            continue
+
+        if field in SUBSET_FIELDS_MAPPING_V3_V4:
+            output |= SUBSET_FIELDS_MAPPING_V3_V4[field]
+
+        elif field == "data":
+            output.add("family")
+            output |= SUBSET_ATTRIBS
+
+        elif field.startswith("data"):
+            field_parts = field.split(".")
+            field_parts.pop(0)
+            data_key = ".".join(field_parts)
+            if data_key == "subsetGroup":
+                output.add("attrib.subsetGroup")
+
+            elif data_key in ("family", "families"):
+                output.add("family")
+
+            else:
+                print(data_key)
+                raise ValueError("Can't query data for field {}".format(field))
+
+        else:
+            raise ValueError("Unknown field mapping for {}".format(field))
+
+    if "id" not in output:
+        output.add("id")
+    return output
+
+
+def convert_v4_subset_to_v3(subset):
+    output = {
+        "_id": subset["id"],
+        "type": "subset",
+        "schema": "openpype:subset-3.0"
+    }
+    if "folderId" in subset:
+        output["parent"] = subset["folderId"]
+
+    output_data = subset.get("data") or {}
+
+    if "name" in subset:
+        output["name"] = subset["name"]
+
+    if "active" in subset:
+        output_data["active"] = subset["active"]
+
+    if "attrib" in subset:
+        attrib = subset["attrib"]
+        output_data.update(attrib)
+
+    family = subset.get("family")
+    if family:
+        output_data["family"] = family
+        output_data["families"] = [family]
+
+    output["data"] = output_data
+
+    return output
+
+
+def version_fields_v3_to_v4(fields):
+    """Convert version fields from v3 to v4 structure.
+
+    Args:
+        fields (Union[Iterable(str), None]): fields to be converted.
+
+    Returns:
+        Union[Set(str), None]: Converted fields to v4 fields.
+    """
+
+    if not fields:
+        return None
+
+    output = set()
+    for field in fields:
+        if field in ("type", "schema", "version_id"):
+            continue
+
+        if field in VERSION_FIELDS_MAPPING_V3_V4:
+            output |= VERSION_FIELDS_MAPPING_V3_V4[field]
+
+        elif field == "data":
+            output |= VERSION_ATTRIBS_FIELDS
+            output |= {
+                "author",
+                "createdAt",
+                "thumbnailId",
+            }
+
+        elif field.startswith("data"):
+            field_parts = field.split(".")
+            field_parts.pop(0)
+            data_key = ".".join(field_parts)
+            if data_key == "thumbnail_id":
+                output.add("thumbnailId")
+
+            elif data_key == "time":
+                output.add("createdAt")
+
+            elif data_key == "author":
+                output.add("author")
+
+            elif data_key in ("tags", ):
+                continue
+
+            else:
+                print(data_key)
+                raise ValueError("Can't query data for field {}".format(field))
+
+        else:
+            raise ValueError("Unknown field mapping for {}".format(field))
+
+    if "id" not in output:
+        output.add("id")
+    return output
+
+
+def convert_v4_version_to_v3(version):
+    """Convert v4 version entity to v4 version.
+
+    Args:
+        version (Dict[str, Any]): Queried v4 version entity.
+
+    Returns:
+        Dict[str, Any]: Conveted version entity to v3 structure.
+    """
+
+    version_num = version["version"]
+    doc_type = "version"
+    schema = "openpype:version-3.0"
+    if version_num < 0:
+        doc_type = "hero_version"
+        schema = "openpype:hero_version-1.0"
+
+    output = {
+        "_id": version["id"],
+        "type": doc_type,
+        "name": version_num,
+        "schema": schema
+    }
+    if "subsetId" in version:
+        output["parent"] = version["subsetId"]
+
+    output_data = version.get("data") or {}
+    if "attrib" in version:
+        output_data.update(version["attrib"])
+
+    for key, data_key in (
+        ("active", "active"),
+        ("thumbnailId", "thumbnail_id"),
+        ("author", "author")
+    ):
+        if key in version:
+            output_data[data_key] = version[key]
+
+    if "createdAt" in version:
+        # TODO probably will need a conversion?
+        created_at = datetime.datetime.fromtimestamp(version["createdAt"])
+        output_data["time"] = created_at.strftime("%Y%m%dT%H%M%SZ")
+
+    output["data"] = output_data
+
+    return output
+
+
+def representation_fields_v3_to_v4(fields):
+    """Convert representation fields from v3 to v4 structure.
+
+    Args:
+        fields (Union[Iterable(str), None]): fields to be converted.
+
+    Returns:
+        Union[Set(str), None]: Converted fields to v4 fields.
+    """
+
+    if not fields:
+        return None
+
+    output = set()
+    for field in fields:
+        if field in ("type", "schema"):
+            continue
+
+        if field in REPRESENTATION_FIELDS_MAPPING_V3_V4:
+            output |= REPRESENTATION_FIELDS_MAPPING_V3_V4[field]
+
+        elif field.startswith("context"):
+            output.add("context")
+
+        # TODO: 'files' can have specific attributes but the keys in v3 and v4
+        #   are not the same (content is not the same)
+        elif field.startswith("files"):
+            output |= REPRESENTATION_FILES_FIELDS
+
+        elif field.startswith("data"):
+            fields |= REPRESENTATION_ATTRIBS_FIELDS
+
+        else:
+            raise ValueError("Unknown field mapping for {}".format(field))
+
+    if "id" not in output:
+        output.add("id")
+    return output
+
+
+def convert_v4_representation_to_v3(representation):
+    """Convert v4 representation to v3 representation.
+
+    Args:
+        representation (Dict[str, Any]): Queried representation from v4 server.
+
+    Returns:
+        Dict[str, Any]: Converted representation to v3 structure.
+    """
+
+    output = {
+        "_id": representation["id"],
+        "type": "representation",
+        "schema": "openpype:representation-2.0",
+    }
+    for v3_key, v4_key in (
+        ("name", "name"),
+        ("files", "files"),
+        ("context", "context"),
+        ("parent", "versionId")
+    ):
+        if v4_key in representation:
+            output[v3_key] = representation[v4_key]
+
+    if "files" in output and not output["files"]:
+        # Fake studio files
+        output["files"].append({
+            "name": "studio",
+            "created_dt": datetime.datetime.now()
+        })
+    output_data = representation.get("data") or {}
+    if "attrib" in representation:
+        output_data.update(representation["attrib"])
+
+    for key, data_key in (
+        ("active", "active"),
+    ):
+        if key in representation:
+            output_data[data_key] = representation[key]
+
+    output["data"] = output_data
+
+    return output
