383c2824c991e03f7f7263f93d142d3d82b6b0b3
Kaa Maurice <kaalium@gmail.com>
2022-11-02 11:27:49 +0100

    update comment and docs

--- openpype/hosts/blender/plugins/load/load_image.py
+++ openpype/hosts/blender/plugins/load/load_image.py
@@ -7,9 +7,14 @@ from openpype.hosts.blender.api.lib import get_selection
 
 
 class ImageLoader(plugin.AssetLoader):
-    """Load image in Blender."""
+    """Load image in Blender.
 
-    families = ["image"]
+    Append an bpy.types.Image in data of the current blend file.
+    This Image entity allow image files and movie files.
+    Images append can be found in contextual lists like image texture.
+    """
+
+    families = ["image", "render2d", "source", "plate", "render", "review"]
     representations = ["png", "jpg", "mov", "mp4"]
 
     label = "Load Image"
@@ -27,6 +32,7 @@ class ImageLoader(plugin.AssetLoader):
         img = bpy.data.images.load(libpath)
         img.name = plugin.asset_name(asset, subset)
 
+        # Setup image as movie clip if source is file extension is mov or mp4.
         if libpath.endswith((".mov", "mp4")):
             img.source = 'MOVIE'
 
@@ -34,7 +40,11 @@ class ImageLoader(plugin.AssetLoader):
 
 
 class ReferenceLoader(ImageLoader):
-    """Load image as Reference."""
+    """Load image as Reference.
+
+    Append an bpy.types.Image in data of the current blend file and create
+    empty object using the Image as display.
+    """
 
     label = "Load Reference"
     order = 1
@@ -47,10 +57,10 @@ class ReferenceLoader(ImageLoader):
         libpath = self.fname
         asset = context["asset"]["name"]
         subset = context["subset"]["name"]
-
         group_name = plugin.asset_name(asset, subset)
-        asset_group = bpy.data.objects.new(group_name, object_data=None)
 
+        # generate empty object with image as display.
+        asset_group = bpy.data.objects.new(group_name, object_data=None)
         asset_group.empty_display_type = "IMAGE"
         asset_group.empty_display_size = 10
         asset_group.empty_image_depth = 'DEFAULT'
@@ -59,6 +69,8 @@ class ReferenceLoader(ImageLoader):
         asset_group.show_empty_image_perspective = True
         asset_group.data = img
 
+        # Setup image as movie clip if source is "MOVIE"
+        # and match frame start and duration with the current scene settings.
         if img.source == "MOVIE":
             asset_group.image_user.frame_start = bpy.context.scene.frame_start
             asset_group.image_user.frame_duration = (
@@ -73,7 +85,11 @@ class ReferenceLoader(ImageLoader):
 
 
 class BackgroundLoader(ImageLoader):
-    """Load image as Background linked to a camera."""
+    """Load image as Background linked to a camera.
+
+    Append an bpy.types.Image in data of the current blend file and bind it
+    to the selected camera as background image.
+    """
 
     label = "Load Background for camera"
     order = 2
@@ -82,7 +98,7 @@ class BackgroundLoader(ImageLoader):
         img = super().process_asset(*args, **kwargs)
 
         camera = None
-        
+
         # First try, catching camera with current active object.
         if bpy.context.object and bpy.context.object.type == "CAMERA":
             camera = bpy.context.object
@@ -104,10 +120,14 @@ class BackgroundLoader(ImageLoader):
 
         assert camera, "No camera found!"
 
+        # Link image as background image.
         camera.data.show_background_images = True
         bkg_img = camera.data.background_images.new()
         bkg_img.image = img
         bkg_img.frame_method = "FIT"
+
+        # Setup image as movie clip if source is "MOVIE"
+        # and match frame start and duration with the current scene settings.
         if img.source == "MOVIE":
             bkg_img.image_user.frame_start = bpy.context.scene.frame_start
             bkg_img.image_user.frame_duration = (
