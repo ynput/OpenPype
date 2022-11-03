ed96f1d5b33649e8f5f21e5598e4ee56436f63df
Felix David <felixg.david@gmail.com>
2022-11-03 11:41:59 +0100

    requested cosmetic changes

--- openpype/hooks/pre_copy_last_published_workfile.py
+++ openpype/hooks/pre_copy_last_published_workfile.py
@@ -35,6 +35,17 @@ class CopyLastPublishedWorkfile(PreLaunchHook):
         Returns:
             None: This is a void method.
         """
+        # Check there is no workfile available
+        last_workfile = self.data.get("last_workfile_path")
+        if os.path.exists(last_workfile):
+            self.log.debug(
+                "Last workfile exists. Skipping {} process.".format(
+                    self.__class__.__name__
+                )
+            )
+            return
+
+        # Get data
         project_name = self.data["project_name"]
         task_name = self.data["task_name"]
         task_type = self.data["task_type"]
@@ -73,97 +84,94 @@ class CopyLastPublishedWorkfile(PreLaunchHook):
 
         self.log.info("Trying to fetch last published workfile...")
 
-        # Check there is no workfile available
-        last_workfile = self.data.get("last_workfile_path")
-        if os.path.exists(last_workfile):
-            self.log.debug(
-                "Last workfile exists. Skipping {} process.".format(
-                    self.__class__.__name__
-                )
-            )
-            return
-
         project_doc = self.data.get("project_doc")
         asset_doc = self.data.get("asset_doc")
         anatomy = self.data.get("anatomy")
-        if project_doc and asset_doc:
-            # Get subset id
-            subset_id = next(
-                (
-                    subset["_id"]
-                    for subset in get_subsets(
-                        project_name,
-                        asset_ids=[asset_doc["_id"]],
-                        fields=["_id", "data.family"],
-                    )
-                    if subset["data"]["family"] == "workfile"
-                ),
-                None,
-            )
-            if not subset_id:
-                self.log.debug('No any workfile for asset "{}".').format(
-                    asset_doc["name"]
-                )
-                return
 
-            # Get workfile representation
-            workfile_representation = next(
-                (
-                    representation
-                    for representation in get_representations(
-                        project_name,
-                        version_ids=[
+        # Check it can proceed
+        if not project_doc and not asset_doc:
+            return
+
+        # Get subset id
+        subset_id = next(
+            (
+                subset["_id"]
+                for subset in get_subsets(
+                    project_name,
+                    asset_ids=[asset_doc["_id"]],
+                    fields=["_id", "data.family"],
+                )
+                if subset["data"]["family"] == "workfile"
+            ),
+            None,
+        )
+        if not subset_id:
+            self.log.debug('No any workfile for asset "{}".').format(
+                asset_doc["name"]
+            )
+            return
+
+        # Get workfile representation
+        workfile_representation = next(
+            (
+                representation
+                for representation in get_representations(
+                    project_name,
+                    version_ids=[
+                        (
                             get_last_version_by_subset_id(
                                 project_name, subset_id, fields=["_id"]
-                            )["_id"]
-                        ],
-                    )
-                    if representation["context"]["task"]["name"] == task_name
-                ),
-                None,
-            )
+                            )
+                            or {}
+                        ).get("_id")
+                    ],
+                )
+                if representation["context"]["task"]["name"] == task_name
+            ),
+            None,
+        )
 
-            if not workfile_representation:
-                self.log.debug(
-                    'No published workfile for task "{}" and host "{}".'
-                ).format(task_name, host_name)
-                return
+        if not workfile_representation:
+            self.log.debug(
+                'No published workfile for task "{}" and host "{}".'
+            ).format(task_name, host_name)
+            return
 
-            # Get sync server from Tray,
-            # which handles the asynchronous thread instance
-            sync_server = next(
-                (
-                    t["sync_server"]
-                    for t in [
-                        obj
-                        for obj in gc.get_objects()
-                        if isinstance(obj, ModulesManager)
-                    ]
-                    if t["sync_server"].sync_server_thread
-                ),
-                None,
-            )
+        # Get sync server from Tray,
+        # which handles the asynchronous thread instance
+        sync_server = next(
+            (
+                t["sync_server"]
+                for t in [
+                    obj
+                    for obj in gc.get_objects()
+                    if isinstance(obj, ModulesManager)
+                ]
+                if t["sync_server"].sync_server_thread
+            ),
+            None,
+        )
 
-            # Add site and reset timer
-            active_site = sync_server.get_active_site(project_name)
-            sync_server.add_site(
-                project_name,
-                workfile_representation["_id"],
-                active_site,
-                force=True,
-            )
-            sync_server.reset_timer()
+        # Add site and reset timer
+        active_site = sync_server.get_active_site(project_name)
+        sync_server.add_site(
+            project_name,
+            workfile_representation["_id"],
+            active_site,
+            force=True,
+        )
+        sync_server.reset_timer()
 
-            # Wait for the download loop to end
-            sync_server.sync_server_thread.files_processed.wait()
+        # Wait for the download loop to end
+        sync_server.sync_server_thread.files_processed.wait()
 
-            # Get paths
-            published_workfile_path = get_representation_path(
-                workfile_representation, root=anatomy.roots
-            )
-            local_workfile_dir = os.path.dirname(last_workfile)
+        # Get paths
+        published_workfile_path = get_representation_path(
+            workfile_representation, root=anatomy.roots
+        )
+        local_workfile_dir = os.path.dirname(last_workfile)
 
-            # Copy file and substitute path
-            self.data["last_workfile_path"] = shutil.copy(
-                published_workfile_path, local_workfile_dir
-            )
+        # Copy file and substitute path
+        self.data["last_workfile_path"] = shutil.copy(
+            published_workfile_path, local_workfile_dir
+        )
