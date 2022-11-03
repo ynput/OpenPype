0e9c0a5eb3671503a00406acd14a1434d1795687
Petr Kalis <petr.kalis@gmail.com>
2022-11-03 12:51:42 +0100

    OP-3908 - fix - use values, filtering is later

--- openpype/hosts/photoshop/plugins/create/workfile_creator.py
+++ openpype/hosts/photoshop/plugins/create/workfile_creator.py
@@ -21,7 +21,7 @@ class PSWorkfileCreator(AutoCreator):
         instances = cache_and_get_instances(self,
                                             "openpype.photoshop.instances",
                                             self.host.list_instances)
-        for instance_data in instances.get(self.family, []):
+        for instance_data in instances.values():
             creator_id = instance_data.get("creator_identifier")
             if creator_id == self.identifier:
                 subset_name = instance_data["subset"]
