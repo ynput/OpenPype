3cd1918f04ef5c13ab10e003064699b1659f8fb0
Jakub Trllo <jakub.trllo@gmail.com>
2022-11-10 11:23:32 +0100

    shorter animation

--- openpype/tools/publisher/widgets/widgets.py
+++ openpype/tools/publisher/widgets/widgets.py
@@ -1726,7 +1726,7 @@ class CreateNextPageOverlay(QtWidgets.QWidget):
         change_anim = QtCore.QVariantAnimation()
         change_anim.setStartValue(0.0)
         change_anim.setEndValue(1.0)
-        change_anim.setDuration(400)
+        change_anim.setDuration(200)
         change_anim.setEasingCurve(QtCore.QEasingCurve.OutCubic)
 
         change_anim.valueChanged.connect(self._on_anim)
