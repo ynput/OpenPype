cd27df0e8d35ccb94a4cfde09f399cbb6319a1c4
Kayla Man <moonyuet2008@hotmail.cocm>
2022-11-10 19:16:10 +0800

    Alembic Loader as Arnold Standin

--- openpype/hosts/maya/plugins/load/load_abc_to_standin.py
+++ openpype/hosts/maya/plugins/load/load_abc_to_standin.py
@@ -50,10 +50,10 @@ class AlembicStandinLoader(load.LoaderPlugin):
         if c is not None:
             cmds.setAttr(root + ".useOutlinerColor", 1)
             cmds.setAttr(root + ".outlinerColor",
-                        (float(c[0])/255),
-                        (float(c[1])/255),
-                        (float(c[2])/255)
-            )
+                         (float(c[0])/255),
+                         (float(c[1])/255),
+                         (float(c[2])/255)
+                        )
 
         transform_name = label + "_ABC"
 
