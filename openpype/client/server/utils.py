e84925bab67694a25192ebbc2ba8016d320b9109
Jakub Trllo <jakub.trllo@gmail.com>
2022-11-11 11:12:33 +0100

    prepared conversion helper functions for v4 entities

--- openpype/client/server/utils.py
+++ openpype/client/server/utils.py
@@ -1,5 +1,54 @@
 import uuid
 
+from openpype.client.operations_base import REMOVED_VALUE
+
 
 def create_entity_id():
     return uuid.uuid1().hex
+
+
+def prepare_attribute_changes(old_entity, new_entity, replace=False):
+    attrib_changes = {}
+    new_attrib = new_entity.get("attrib")
+    old_attrib = old_entity.get("attrib")
+    if new_attrib is None:
+        if not replace:
+            return attrib_changes
+        new_attrib = {}
+
+    if old_attrib is None:
+        return new_attrib
+
+    for attr, new_attr_value in new_attrib.items():
+        old_attr_value = old_attrib.get(attr)
+        if old_attr_value != new_attr_value:
+            attrib_changes[attr] = new_attr_value
+
+    if replace:
+        for attr in old_attrib:
+            if attr not in new_attrib:
+                attrib_changes[attr] = REMOVED_VALUE
+
+    return attrib_changes
+
+
+def prepare_entity_changes(old_entity, new_entity, replace=False):
+    """Prepare changes of v4 entities."""
+
+    changes = {}
+    for key, new_value in new_entity.items():
+        if key == "attrib":
+            continue
+
+        old_value = old_entity.get(key)
+        if old_value != new_value:
+            changes[key] = new_value
+
+    if replace:
+        for key in old_entity:
+            if key not in new_entity:
+                changes[key] = REMOVED_VALUE
+
+    attr_changes = prepare_attribute_changes(old_entity, new_entity, replace)
+    if attr_changes:
+        changes["attrib"] = attr_changes
