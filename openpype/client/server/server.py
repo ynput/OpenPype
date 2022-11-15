784649a23aeef24119d862c58838082a5cdfef1b
Jakub Trllo <jakub.trllo@gmail.com>
2022-11-15 15:28:14 +0100

    move create and delete project directly to server api

--- openpype/client/server/server.py
+++ openpype/client/server/server.py
@@ -1,4 +1,5 @@
 import os
+import re
 import json
 import logging
 import collections
@@ -33,6 +34,11 @@ from .graphql_queries import (
 )
 
 JSONDecodeError = getattr(json, "JSONDecodeError", ValueError)
+# This should be collected from server schema
+PROJECT_NAME_ALLOWED_SYMBOLS = "a-zA-Z0-9_"
+PROJECT_NAME_REGEX = re.compile(
+    "^[{}]+$".format(PROJECT_NAME_ALLOWED_SYMBOLS)
+)
 
 
 class ServerError(Exception):
@@ -1861,6 +1867,92 @@ class ServerAPIBase(object):
         return self.get_thumbnail(
             project_name, "workfile", workfile_id, thumbnail_id
         )
+
+    def create_project(
+        self,
+        project_name,
+        project_code,
+        library_project=False,
+        preset_name=None
+    ):
+        """Create project using OpenPype settings.
+
+        This project creation function is not validating project document on
+        creation. It is because project document is created blindly with only
+        minimum required information about project which is it's name, code, type
+        and schema.
+
+        Entered project name must be unique and project must not exist yet.
+
+        Note:
+            This function is here to be OP v4 ready but in v3 has more logic
+                to do. That's why inner imports are in the body.
+
+        Args:
+            project_name (str): New project name. Should be unique.
+            project_code (str): Project's code should be unique too.
+            library_project (bool): Project is library project.
+            preset_name (str): Name of anatomy preset. Default is used if not
+                passed.
+            con (ServerAPI): Connection to server with logged user.
+
+        Raises:
+            ValueError: When project name already exists in MongoDB.
+
+        Returns:
+            dict: Created project document.
+        """
+
+        if self.get_project(project_name, fields=["name"]):
+            raise ValueError("Project with name \"{}\" already exists".format(
+                project_name
+            ))
+
+        if not PROJECT_NAME_REGEX.match(project_name):
+            raise ValueError((
+                "Project name \"{}\" contain invalid characters"
+            ).format(project_name))
+
+        preset = self.get_project_anatomy_preset(preset_name)
+
+        result = self.post(
+            "projects",
+            name=project_name,
+            code=project_code,
+            anatomy=preset,
+            library=library_project
+        )
+        if result.status != 201:
+            details = "Unknown details ({})".format(result.status)
+            if result.data:
+                details = result.data.get("detail") or details
+            raise ValueError("Failed to create project \"{}\": {}".format(
+                project_name, details
+            ))
+
+        return self.get_project(project_name)
+
+    def delete_project(self, project_name):
+        """Delete project from server.
+
+        This will completely remove project from server without any step back.
+
+        Args:
+            project_name (str): Project name that will be removed.
+        """
+
+        if not self.get_project(project_name, fields=["name"]):
+            raise ValueError("Project with name \"{}\" was not found".format(
+                project_name
+            ))
+
+        result = self.delete("projects/{}".format(project_name))
+        if result.status_code != 204:
+            raise ValueError(
+                "Failed to delete project \"{}\". {}".format(
+                    project_name, result.data["detail"]
+                )
+            )
 
     def create_thumbnail(self, project_name, src_filepath):
         """Create new thumbnail on server from passed path.
