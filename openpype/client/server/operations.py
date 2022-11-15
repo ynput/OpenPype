a88c48c54a8ddf8faa6010d0ff5ebd8a58eab12d
Jakub Trllo <jakub.trllo@gmail.com>
2022-11-15 15:28:14 +0100

    move create and delete project directly to server api

--- openpype/client/server/operations.py
+++ openpype/client/server/operations.py
@@ -1,4 +1,3 @@
-import re
 import copy
 import json
 import collections
@@ -43,12 +42,6 @@ from .conversion_utils import (
     convert_update_workfile_info_to_v4,
 )
 from .utils import create_entity_id
-
-
-PROJECT_NAME_ALLOWED_SYMBOLS = "a-zA-Z0-9_"
-PROJECT_NAME_REGEX = re.compile(
-    "^[{}]+$".format(PROJECT_NAME_ALLOWED_SYMBOLS)
-)
 
 
 def _create_or_convert_to_id(entity_id=None):
@@ -848,52 +841,19 @@ def create_project(
     if con is None:
         con = get_server_api_connection()
 
-    if con.get_project(project_name, fields=["name"]):
-        raise ValueError("Project with name \"{}\" already exists".format(
-            project_name
-        ))
-
-    if not PROJECT_NAME_REGEX.match(project_name):
-        raise ValueError((
-            "Project name \"{}\" contain invalid characters"
-        ).format(project_name))
-
-    preset = con.get_project_anatomy_preset(preset_name)
-
-    result = con.post(
-        "projects",
-        name=project_name,
-        code=project_code,
-        anatomy=preset,
-        library=library_project
+    return con.create_project(
+        project_name,
+        project_code,
+        library_project,
+        preset_name
     )
-    if result.status != 201:
-        details = "Unknown details ({})".format(result.status)
-        if result.data:
-            details = result.data.get("detail") or details
-        raise ValueError("Failed to create project \"{}\": {}".format(
-            project_name, details
-        ))
-
-    return con.get_project(project_name)
 
 
 def delete_project(project_name, con=None):
     if con is None:
         con = get_server_api_connection()
 
-    if not con.get_project(project_name, fields=["name"]):
-        raise ValueError("Project with name \"{}\" was not found".format(
-            project_name
-        ))
-
-    result = con.delete("projects/{}".format(project_name))
-    if result.status_code != 204:
-        raise ValueError(
-            "Failed to delete project \"{}\". {}".format(
-                project_name, result.data["detail"]
-            )
-        )
+    return con.delete_project(project_name)
 
 
 def create_thumbnail(project_name, src_filepath, con=None):
