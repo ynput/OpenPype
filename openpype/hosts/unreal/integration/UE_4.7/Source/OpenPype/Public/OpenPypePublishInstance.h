110cd58fd168861b91cbdee366324edbe21c8917
Joseff <vzotov42@gmail.com>
2022-11-22 18:40:42 +0100

    Update the UOpenPypePublishInstance to use UDataAsset

--- openpype/hosts/unreal/integration/UE_4.7/Source/OpenPype/Public/OpenPypePublishInstance.h
+++ openpype/hosts/unreal/integration/UE_4.7/Source/OpenPype/Public/OpenPypePublishInstance.h
@@ -5,17 +5,51 @@
 
 
 UCLASS(Blueprintable)
-class OPENPYPE_API UOpenPypePublishInstance : public UObject
+class OPENPYPE_API UOpenPypePublishInstance : public UPrimaryDataAsset
 {
-	GENERATED_BODY()
-
+	GENERATED_UCLASS_BODY()
+	
 public:
-	UOpenPypePublishInstance(const FObjectInitializer& ObjectInitalizer);
-
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	TSet<UObject*> AssetDataInternal;
+	
+	/**
+	 * This property allows exposing the array to include other assets from any other directory than what it's currently
+	 * monitoring. NOTE: that these assets have to be added manually! They are not automatically registered or added!
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-		TArray<FString> assets;
+	bool bAddExternalAssets = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(EditCondition="bAddExternalAssets"))
+	TSet<UObject*> AssetDataExternal;
+
+	/**
+	 * Function for returning all the assets in the container.
+	 * 
+	 * @return Returns all the internal and externally added assets into one set (TSet).
+	 */
+	UFUNCTION(BlueprintCallable, Category = Python)
+	TSet<UObject*> GetAllAssets() const
+	{
+		return AssetDataInternal.Union(AssetDataExternal);
+	};
+
+
 private:
-	void OnAssetAdded(const FAssetData& AssetData);
-	void OnAssetRemoved(const FAssetData& AssetData);
-	void OnAssetRenamed(const FAssetData& AssetData, const FString& str);
+
+	void OnAssetCreated(const FAssetData& InAssetData);
+	void OnAssetRemoved(const FAssetData& InAssetData);
+	void OnAssetUpdated(const FAssetData& InAssetData);
+
+	bool IsUnderSameDir(const UObject* InAsset) const;
+
+#ifdef WITH_EDITOR
+	
+	void SendNotification(const FString& Text) const;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+#endif
+	
 };
+
--- openpype/hosts/unreal/integration/UE_5.0/Source/OpenPype/Public/OpenPypePublishInstance.h
+++ openpype/hosts/unreal/integration/UE_5.0/Source/OpenPype/Public/OpenPypePublishInstance.h
@@ -1,21 +1,62 @@
 #pragma once
 
+#include "EditorTutorial.h"
 #include "Engine.h"
 #include "OpenPypePublishInstance.generated.h"
 
 
 UCLASS(Blueprintable)
-class OPENPYPE_API UOpenPypePublishInstance : public UObject
+class OPENPYPE_API UOpenPypePublishInstance : public UPrimaryDataAsset
 {
-	GENERATED_BODY()
-
+	GENERATED_UCLASS_BODY()
+	
 public:
-	UOpenPypePublishInstance(const FObjectInitializer& ObjectInitalizer);
-
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	TSet<TObjectPtr<UObject>> AssetDataInternal;
+	
+	/**
+	 * This property allows exposing the array to include other assets from any other directory than what it's currently
+	 * monitoring. NOTE: that these assets have to be added manually! They are not automatically registered or added!
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-		TArray<FString> assets;
+	bool bAddExternalAssets = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(EditCondition="bAddExternalAssets"))
+	TSet<TObjectPtr<UObject>> AssetDataExternal;
+
+	/**
+	 * Function for returning all the assets in the container.
+	 * 
+	 * @return Returns all the internal and externally added assets into one set (TSet).
+	 */
+	UFUNCTION(BlueprintCallable, Category = Python)
+	TSet<UObject*> GetAllAssets() const
+	{
+		TSet<TObjectPtr<UObject>> Unionized = AssetDataInternal.Union(AssetDataExternal);
+
+		TSet<UObject*> ResultSet;
+		
+		for (auto& Asset : Unionized)
+			ResultSet.Add(Asset.Get());
+
+		return ResultSet;
+	}
+
 private:
-	void OnAssetAdded(const FAssetData& AssetData);
-	void OnAssetRemoved(const FAssetData& AssetData);
-	void OnAssetRenamed(const FAssetData& AssetData, const FString& str);
+
+	void OnAssetCreated(const FAssetData& InAssetData);
+	void OnAssetRemoved(const FAssetData& InAssetData);
+	void OnAssetUpdated(const FAssetData& InAssetData);
+
+	bool IsUnderSameDir(const TObjectPtr<UObject>& InAsset) const;
+
+#ifdef WITH_EDITOR
+	
+	void SendNotification(const FString& Text) const;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+#endif
+	
 };
+
