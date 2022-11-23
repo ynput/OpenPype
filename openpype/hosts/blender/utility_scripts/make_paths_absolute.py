ff80be2346e1af95aed6ff38c71a48eaa9c04321
Felix David <felixg.david@gmail.com>
2022-11-23 19:05:47 +0100

    Fix: relative paths conformation

--- openpype/hosts/blender/utility_scripts/make_paths_absolute.py
+++ openpype/hosts/blender/utility_scripts/make_paths_absolute.py
@@ -1,5 +1,9 @@
 """Make all paths absolute."""
 
+import argparse
+from pathlib import Path
+import sys
+
 import bpy
 
 from openpype.lib.log import Logger
@@ -10,5 +14,27 @@ if __name__ == "__main__":
         f"Blend file | All paths converted to absolute: {bpy.data.filepath}"
     )
 
-    bpy.ops.file.make_paths_absolute()
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description="Update AVALON metadata with given representation ID."
+    )
+    parser.add_argument(
+        "--source_filepath",
+        type=Path,
+        nargs="?",
+        help="source filepath",
+    )
+    args = parser.parse_args(sys.argv[sys.argv.index("--") + 1 :])
+
+    current_filepath = Path(bpy.data.filepath)
+    if args.source_filepath:
+        for lib in bpy.data.libraries:
+            lib.filepath = str(Path(bpy.path.abspath(lib.filepath, start=args.source_filepath.parent)).resolve())
+            try:
+                lib.reload()
+            except RuntimeError as e:
+                log.error(e)
+    else:
+        bpy.ops.file.make_paths_absolute()
+
     bpy.ops.wm.save_mainfile()
