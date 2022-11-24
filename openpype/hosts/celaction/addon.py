1b882cb7caddf5921b5e12a921f0b7c6182346b4
Jakub Jezek <jakubjezek001@gmail.com>
2022-11-24 14:47:44 +0100

    celaction: recognize hook

--- openpype/hosts/celaction/addon.py
+++ openpype/hosts/celaction/addon.py
@@ -11,6 +11,13 @@ class CelactionAddon(OpenPypeModule, IHostAddon):
     def initialize(self, module_settings):
         self.enabled = True
 
+    def get_launch_hook_paths(self, app):
+        if app.host_name != self.host_name:
+            return []
+        return [
+            os.path.join(CELACTION_ROOT_DIR, "hooks")
+        ]
+
     def add_implementation_envs(self, env, _app):
         # Set default values if are not already set via settings
         defaults = {
