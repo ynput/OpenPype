30550c26b01bbe502abaf74562408bd0f1308475
Jakub Jezek <jakubjezek001@gmail.com>
2022-11-25 17:04:19 +0100

    celaction: render from published workfile fix

--- openpype/modules/deadline/plugins/publish/submit_celaction_deadline.py
+++ openpype/modules/deadline/plugins/publish/submit_celaction_deadline.py
@@ -82,7 +82,7 @@ class CelactionSubmitDeadline(pyblish.api.InstancePlugin):
         script_name = os.path.basename(script_path)
 
         for item in instance.context:
-            if "workfile" in item.data["families"]:
+            if "workfile" in item.data["family"]:
                 msg = "Workfile (scene) must be published along"
                 assert item.data["publish"] is True, msg
 
