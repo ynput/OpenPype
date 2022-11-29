381fb9ebd23d47b8ae17853253efca27b269176e
Kaa Maurice <kaalium@gmail.com>
2022-11-29 10:34:02 +0100

    fix: build wotkfile with casting

--- openpype/hosts/blender/scripts/build_workfile.py
+++ openpype/hosts/blender/scripts/build_workfile.py
@@ -1,9 +1,10 @@
 import os
 import bpy
 
-
 from openpype.client import (
-    get_last_version_by_subset_name,
+    get_asset_by_name,
+    get_subset_by_name,
+    get_last_version_by_subset_id,
     get_representations,
 )
 from openpype.lib import get_creator_by_name
@@ -17,33 +18,52 @@ from openpype.pipeline import (
 )
 
 
-def load_subset(project_name, asset_name, subset_name, loader_type=None):
-    last_version = get_last_version_by_subset_name(
+def load_subset(
+    project_name, asset_name, subset_name, loader_type=None, ext="blend"
+):
+
+    asset = get_asset_by_name(
+        project_name,
+        asset_name,
+        fields=["_id"]
+    )
+    if not asset:
+        return
+
+    subset = get_subset_by_name(
         project_name,
         subset_name,
-        asset_name=asset_name,
+        asset["_id"],
+        fields=["_id"],
+    )
+    if not subset:
+        return
+
+    last_version = get_last_version_by_subset_id(
+        project_name,
+        subset["_id"],
         fields=["_id"],
     )
     if not last_version:
         return
 
-    repre_blend = next(
+    representation = next(
         get_representations(
             project_name,
             version_ids=[last_version["_id"]],
-            context_filters={"ext": ["blend"]},
+            context_filters={"ext": [ext]},
         ),
         None
     )
-    if not repre_blend:
+    if not representation:
         return
 
     all_loaders = discover_loader_plugins(project_name=project_name)
-    loaders = loaders_from_representation(all_loaders, repre_blend)
+    loaders = loaders_from_representation(all_loaders, representation)
     for loader in loaders:
         if loader_type and loader_type not in loader.__name__:
             continue
-        return load_container(loader, repre_blend)
+        return load_container(loader, representation)
 
 
 def create_instance(creator_name, instance_name, **options):
@@ -67,17 +87,14 @@ def load_casting(project_name, shot_name):
     gazu.client.set_host(os.environ["KITSU_SERVER"])
     gazu.log_in(os.environ["KITSU_LOGIN"], os.environ["KITSU_PWD"])
 
-    episode_name, sequence_name, shot_name = shot_name.split("_")
+    shot_data = get_asset_by_name(project_name, shot_name, fields=["data"])
 
-    project = gazu.project.get_project_by_name(project_name)
-    episode = gazu.shot.get_episode_by_name(project, episode_name)
-    sequence = gazu.shot.get_sequence_by_name(project, sequence_name, episode=episode)  # noqa E501
-    shot = gazu.shot.get_shot_by_name(sequence, shot_name)
+    shot = gazu.shot.get_shot(shot_data["data"]["zou"]["id"])
     casting = gazu.casting.get_shot_casting(shot)
 
     for actor in casting:
-        for i in range(actor["nb_occurences"]):
-            load_subset(actor["asset_name"], "rigMain", "Link")
+        for _ in range(actor["nb_occurences"]):
+            load_subset(project_name, actor["asset_name"], "rigMain", "Link")
 
     gazu.log_out()
 
@@ -93,7 +110,7 @@ def build_workfile():
         bpy.context.object.data.name = f"{asset}_model"
         create_instance("CreateModel", "modelMain", useSelection=True)
 
-    elif task in ("texture", "texturing", "look", "lookdev", "shader", "shadering", "shading"):  # noqa E501
+    elif task in ("texture", "look", "lookdev", "shader"):
         create_instance("CreateLook", "lookMain")
         load_subset(project, asset, "modelMain", "Append")
 
@@ -107,7 +124,8 @@ def build_workfile():
     elif task == "layout":
         create_instance("CreateLayout", "layoutMain")
         create_instance("CreateCamera", "cameraMain")
-        # create_instance("CreateReview", "reviewMain")
+        create_instance("CreateReview", "reviewMain")
+        load_subset(project, asset, "BoardReview", "Background", "mov")
         try:
             load_casting(project, asset)
         except RuntimeError:
@@ -116,7 +134,7 @@ def build_workfile():
     elif task in ("anim", "animation"):
         load_subset(project, asset, "layoutMain", "Append")
         load_subset(project, asset, "cameraMain", "Link")
-        # create_instance("CreateReview", "reviewMain")
+        create_instance("CreateReview", "reviewMain")
 
     elif task in ("lighting", "light", "render", "rendering"):
         if not load_subset(project, asset, "layoutFromAnim", "Link"):
