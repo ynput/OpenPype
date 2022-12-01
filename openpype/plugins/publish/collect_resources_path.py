1106f8cf5ad6d23051643c83fd894846127ec600
Petr Kalis <petr.kalis@gmail.com>
2022-12-01 18:38:24 +0100

    OP-4504 - check for None

--- openpype/plugins/publish/collect_resources_path.py
+++ openpype/plugins/publish/collect_resources_path.py
@@ -111,7 +111,7 @@ class CollectResourcesPath(pyblish.api.InstancePlugin):
         current_file = instance.data.get("currentFile")
         source = instance.data.get("source")
         source_file = current_file or source
-        if os.path.exists(source_file):
+        if source_file and os.path.exists(source_file):
             self.log.debug("Parsing paths for {}".format(source_file))
             if not instance.data.get("originalBasename"):
                 instance.data["originalBasename"] = \
