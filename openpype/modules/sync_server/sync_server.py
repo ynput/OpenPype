72aef0812e68ecc67a61af40141d62d4a06c6790
Sharkitty <81646000+Sharkitty@users.noreply.github.com>
2022-12-01 11:02:42 +0100

    Enhancement:  copy last workfile as reusable methods (#6)
    * Enhancement: copy last published workfile as reusable methods (WiP)
    
    * Added get_host_extensions method, added subset_id and las_version_doc access, added optional arguments to get_last_published_workfile
    
    * Plugged in the new methods + minor changes
    
    * Added docstrings, last workfile optional argument, and removed unused code
    
    * Using new implementation to get local workfile path. Warning: It adds an extra dot to the extension which I need to fix
    
    * Refactoring and fixed double dots
    
    * Added match subset_id and get representation method, plus clan up
    
    * Removed unused vars
    
    * Fixed some rebasing errors
    
    * delinted unchanged code and renamed get_representation into get_representation_with_task
    
    * This time it's really delinted, I hope...
    
    * Update openpype/modules/sync_server/sync_server.py
    
    reprenation isn't the right spelling (:
    
    Co-authored-by: Félix David <felixg.david@gmail.com>
    
    * Changes based on reviews
    
    * Fixed non imperative docstring and missing space
    
    * Fixed another non imperative docstring
    
    * Update openpype/modules/sync_server/sync_server.py
    
    Fixed typo
    
    Co-authored-by: Félix David <felixg.david@gmail.com>
    
    Co-authored-by: Hayley GUILLOT <hayleyguillot@outlook.com>
    Co-authored-by: Félix David <felixg.david@gmail.com>

--- openpype/modules/sync_server/sync_server.py
+++ openpype/modules/sync_server/sync_server.py
@@ -1,12 +1,23 @@
 """Python 3 only implementation."""
 import os
+import shutil
 import asyncio
 import threading
 import concurrent.futures
+from time import sleep
 from concurrent.futures._base import CancelledError
 
 from .providers import lib
+from openpype.client.entity_links import get_linked_representation_id
 from openpype.lib import Logger
+from openpype.lib.local_settings import get_local_site_id
+from openpype.modules.base import ModulesManager
+from openpype.pipeline import Anatomy
+from openpype.pipeline.template_data import get_template_data_with_names
+from openpype.pipeline.load.utils import get_representation_path_with_anatomy
+from openpype.pipeline.workfile.path_resolving import get_workfile_template_key
+from openpype.settings.lib import get_project_settings
+from openpype.client.entities import get_asset_by_name
 
 from .utils import SyncStatus, ResumableError
 
@@ -227,6 +238,173 @@ def _get_configured_sites_from_setting(module, project_name, project_setting):
             configured_sites[site_name] = True
 
     return configured_sites
+
+
+def get_last_published_workfile_path(
+    host_name: str,
+    project_name: str,
+    task_name: str,
+    workfile_representation: dict,
+    anatomy: Anatomy = None,
+) -> str:
+    """Get last published workfile path.
+
+    Args:
+        host_name (str): Host name.
+        project_name (str): Project name.
+        task_name (str): Task name.
+        workfile_representation (dict): Workfile representation.
+        anatomy (Anatomy, optional): Anatomy (Used for optimization).
+            Defaults to None.
+
+    Returns:
+        str: Last published workfile path.
+    """
+    if not anatomy:
+        anatomy = Anatomy(project_name)
+
+    if not workfile_representation:
+        print(
+            "No published workfile for task '{}' and host '{}'.".format(
+                task_name, host_name
+            )
+        )
+        return
+
+    return get_representation_path_with_anatomy(
+        workfile_representation, anatomy
+    )
+
+
+def download_last_published_workfile(
+    host_name: str,
+    project_name: str,
+    asset_name: str,
+    task_name: str,
+    last_published_workfile_path: str,
+    workfile_representation: dict,
+    subset_id: str,
+    last_version_doc: dict,
+    anatomy: Anatomy = None,
+    asset_doc: dict = None,
+) -> str:
+    """Download the last published workfile, and return its path.
+
+    Args:
+        host_name (str): Host name.
+        project_name (str): Project name.
+        asset_name (str): Asset name.
+        task_name (str): Task name.
+        last_published_workfile_path (str): Last published workfile
+            path.
+        workfile_representation (dict): Workfile representation.
+        subset_id (str): Subset ID.
+        last_version_doc (dict): Last version doc.
+        anatomy (Anatomy, optional): Anatomy (Used for optimization).
+            Defaults to None.
+        asset_doc (dict, optional): Asset doc (Used for optimization).
+            Defaults to None.
+
+    Returns:
+        str: New local workfile path.
+    """
+
+    if not anatomy:
+        anatomy = Anatomy(project_name)
+
+    if not asset_doc:
+        asset_doc = get_asset_by_name(project_name, asset_name)
+
+    # Get sync server module
+    sync_server = ModulesManager().modules_by_name.get("sync_server")
+    if not sync_server or not sync_server.enabled:
+        print("Sync server module is disabled or unavailable.")
+        return
+
+    if subset_id is None:
+        print(
+            "Not any matched subset for task '{}' of '{}'.".format(
+                task_name, asset_name
+            )
+        )
+        return
+
+    if not workfile_representation:
+        print(
+            "Not published workfile for task '{}' and host '{}'.".format(
+                task_name, host_name
+            )
+        )
+        return
+
+    # If representation isn't available on remote site, then return.
+    if not sync_server.is_representation_on_site(
+        project_name,
+        workfile_representation["_id"],
+        sync_server.get_remote_site(project_name),
+    ):
+        print(
+            "Representation for task '{}' and host '{}'".format(
+                task_name, host_name
+            )
+        )
+        return
+
+    # Get local site
+    local_site_id = get_local_site_id()
+
+    # Add workfile representation to local site
+    representation_ids = {workfile_representation["_id"]}
+    representation_ids.update(
+        get_linked_representation_id(
+            project_name, repre_id=workfile_representation["_id"]
+        )
+    )
+    for repre_id in representation_ids:
+        sync_server.add_site(
+            project_name,
+            repre_id,
+            local_site_id,
+            force=True,
+            priority=99,
+            reset_timer=True,
+        )
+
+    # While representation unavailable locally, wait.
+    while not sync_server.is_representation_on_site(
+        project_name, workfile_representation["_id"], local_site_id
+    ):
+        sleep(5)
+
+    if not last_published_workfile_path:
+        last_published_workfile_path = get_last_published_workfile_path(
+            host_name,
+            project_name,
+            task_name,
+            workfile_representation,
+            anatomy=anatomy,
+        )
+
+    # Get workfile data
+    workfile_data = get_template_data_with_names(
+        project_name, asset_name, task_name, host_name
+    )
+    workfile_data["version"] = last_version_doc["name"] + 1
+    workfile_data["ext"] = last_published_workfile_path.split(".")[-1]
+
+    template_key = get_workfile_template_key(
+        task_name, host_name, project_name, get_project_settings(project_name)
+    )
+    anatomy_result = anatomy.format(workfile_data)
+    local_workfile_path = anatomy_result[template_key]["path"]
+
+    # Copy last published workfile to local workfile directory
+    shutil.copy(
+        last_published_workfile_path,
+        local_workfile_path,
+    )
+
+    return local_workfile_path
 
 
 class SyncServerThread(threading.Thread):
