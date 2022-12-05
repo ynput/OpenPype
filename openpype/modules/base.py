3df87cccb0ffa9a9f85a706045e7e69d6b8367db
Petr Kalis <petr.kalis@gmail.com>
2022-12-05 11:26:27 +0100

    Re-implemented method to get addon info
    Previous implementation wouldn't work in Python2 hosts.
    Will be refactored in the future.

--- openpype/modules/base.py
+++ openpype/modules/base.py
@@ -44,10 +44,6 @@ from .interfaces import (
     ITrayService
 )
 
-from common.openpype_common.distribution.addon_distribution import (
-    get_addons_info
-)
-
 # Files that will be always ignored on modules import
 IGNORED_FILENAMES = (
     "__pycache__",
@@ -306,6 +302,19 @@ def load_modules(force=False):
             time.sleep(0.1)
 
 
+def _get_v4_addons_information():
+    """Receive information about addons to use from server.
+    Todos:
+        Actually ask server for the information.
+        Allow project name as optional argument to be able to query information
+            about used addons for specific project.
+    Returns:
+        List[Dict[str, Any]]: List of addon information to use.
+    """
+    from openpype.client.server.server_api import get_addons_info
+    return get_addons_info()
+
+
 def _load_v4_addons(openpype_modules, modules_key, log):
     """Load v4 addons based on information from server.
 
@@ -325,7 +334,7 @@ def _load_v4_addons(openpype_modules, modules_key, log):
 
     v3_addons_to_skip = []
 
-    addons_info = get_addons_info()
+    addons_info = _get_v4_addons_information()
     if not addons_info:
         return v3_addons_to_skip
     addons_dir = os.path.join(
@@ -339,8 +348,11 @@ def _load_v4_addons(openpype_modules, modules_key, log):
         return v3_addons_to_skip
 
     for addon_info in addons_info:
-        addon_name = addon_info.name
-        addon_version = addon_info.version
+        addon_name = addon_info["name"]
+        addon_version = addon_info.get("productionVersion")
+        if not addon_version:
+            continue
+
         folder_name = "{}_{}".format(addon_name, addon_version)
         addon_dir = os.path.join(addons_dir, folder_name)
         if not os.path.exists(addon_dir):
