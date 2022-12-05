b0d75734de35208ff33fd0fb9ceaf9d28929d755
Hayley GUILLOT <hayleyguillot@outlook.com>
2022-12-05 14:30:11 +0100

    Feature: Handling workfile out of date in blender

--- openpype/hosts/blender/api/pipeline.py
+++ openpype/hosts/blender/api/pipeline.py
@@ -11,6 +11,9 @@ from . import ops
 import pyblish.api
 
 from openpype.client import get_asset_by_name
+from openpype.hosts.blender.utility_scripts.is_workfile_out_of_date import (
+    is_work_file_out_of_date,
+)
 from openpype.pipeline import (
     schema,
     legacy_io,
@@ -124,10 +127,17 @@ def set_start_end_frames():
 
 def on_new():
     set_start_end_frames()
+    bpy.types.WindowManager.is_workfile_out_of_date = bpy.props.BoolProperty(
+        name="Is Workfile Out Of Date",
+    )
 
 
 def on_open():
     set_start_end_frames()
+    if is_work_file_out_of_date():
+        bpy.context.window_manager.is_workfile_out_of_date = True
+        bpy.ops.wm.workfile_out_of_date("INVOKE_DEFAULT")
+
 
 
 @bpy.app.handlers.persistent
