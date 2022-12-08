834edafa472d97f0944b40a87ac44015a9d27007
Ondrej Samohel <ondrej.samohel@orbi.tools>
2022-12-08 22:20:14 +0100

    :art: support for unreal engine 5.1

--- openpype/hosts/unreal/api/pipeline.py
+++ openpype/hosts/unreal/api/pipeline.py
@@ -2,6 +2,7 @@
 import os
 import logging
 from typing import List
+import semver
 
 import pyblish.api
 
@@ -21,6 +22,8 @@ import unreal  # noqa
 
 logger = logging.getLogger("openpype.hosts.unreal")
 OPENPYPE_CONTAINERS = "OpenPypeContainers"
+UNREAL_VERSION = semver.VersionInfo(
+            *os.getenv("OPENPYPE_UNREAL_VERSION").split("."))
 
 HOST_DIR = os.path.dirname(os.path.abspath(openpype.hosts.unreal.__file__))
 PLUGINS_DIR = os.path.join(HOST_DIR, "plugins")
@@ -40,6 +43,7 @@ class UnrealHost(HostBase, ILoadHost):
     name = "unreal"
 
     def install(self):
+        version = UNREAL_VERSION
         install()
 
     def get_containers(self):
@@ -111,7 +115,9 @@ def ls():
 
     """
     ar = unreal.AssetRegistryHelpers.get_asset_registry()
-    openpype_containers = ar.get_assets_by_class("AssetContainer", True)
+    # UE 5.1 changed how class name is specified
+    class_name = ["/Script", "AssetContainer"] if UNREAL_VERSION.major == 5 and UNREAL_VERSION.minor > 0 else "AssetContainer"  # noqa
+    openpype_containers = ar.get_assets_by_class(class_name, True)
 
     # get_asset_by_class returns AssetData. To get all metadata we need to
     # load asset. get_tag_values() work only on metadata registered in
