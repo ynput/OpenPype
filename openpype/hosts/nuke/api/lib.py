a7a2731f05a00a8436ea9153e1ce03c560346148
Jakub Jezek <jakubjezek001@gmail.com>
2022-12-09 12:35:41 +0100

    nuke: subset with mixed letter sizes

--- openpype/hosts/nuke/api/lib.py
+++ openpype/hosts/nuke/api/lib.py
@@ -611,7 +611,7 @@ def get_created_node_imageio_setting_legacy(nodeclass, creator, subset):
 
         if (
             onode["subsets"]
-            and not any(re.search(s, subset.lower()) for s in onode["subsets"])
+            and not any(re.search(s.lower(), subset.lower()) for s in onode["subsets"])
         ):
             continue
 
@@ -704,7 +704,7 @@ def get_imageio_node_override_setting(
 
         if (
             onode["subsets"]
-            and not any(re.search(s, subset.lower()) for s in onode["subsets"])
+            and not any(re.search(s.lower(), subset.lower()) for s in onode["subsets"])
         ):
             continue
 
