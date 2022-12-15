0c481f83510438c5fb6243baa3f5c83adba6dbf6
Petr Kalis <petr.kalis@gmail.com>
2022-12-15 11:55:14 +0100

    OP-4512 - safer resolution of enabled
    If new site is added in System setting, but project settings are not saved, 'enabled' key is missing. This should be safer in this cases.

--- openpype/modules/sync_server/providers/sftp.py
+++ openpype/modules/sync_server/providers/sftp.py
@@ -72,7 +72,7 @@ class SFTPHandler(AbstractProvider):
         Returns:
             (boolean)
         """
-        return self.presets["enabled"] and self.conn is not None
+        return self.self.presets.get("enabled") and self.conn is not None
 
     @classmethod
     def get_system_settings_schema(cls):
