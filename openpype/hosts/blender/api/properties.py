6bd49dc945a5e3efa42daccbecbceee68aa855f0
Félix David <felixg.david@gmail.com>
2022-12-16 10:30:51 +0100

    Container setter and getter for name

--- openpype/hosts/blender/api/properties.py
+++ openpype/hosts/blender/api/properties.py
@@ -5,20 +5,45 @@ from bpy.types import PropertyGroup
 from bpy.utils import register_classes_factory
 
 
-class OpenpypeInstanceDatablockRef(PropertyGroup):
-    name: bpy.props.StringProperty(name="OpenPype Instance name")
-    datapath: bpy.props.StringProperty(name="OpenPype Instance name")
+def get_datablock_name(self) -> str:
+    """Get name, ensure to be identical to the referenced datablock's name.
 
-    # = Custom properties =
-    # "outliner_entity" Union[bpy.types.Collection, bpy.types.Object]:
-    #       Entity in outliner if outliner datablock
+    Returns:
+        str: Name
+    """
+    if self.datablock and self.datablock.name != self["name"]:
+        self["name"] = self.datablock.name
+
+    return self["name"]
+
+
+def set_datablock_name(self, value: str):
+    """Set name, ensure the referenced datablock to have the same.
+
+    Args:
+        value (str): Name
+    """
+    if self.datablock and self.datablock.name != self["name"]:
+        self.datablock.name = value
+    self["name"] = value
+
+
+class OpenpypeDatablockRef(PropertyGroup):
+    name: bpy.props.StringProperty(
+        name="OpenPype Instance name",
+        get=get_datablock_name,
+        set=set_datablock_name,
+    )
+    datablock: bpy.props.PointerProperty(
+        name="Datablock reference", type=bpy.types.ID
+    )
 
 
 class OpenpypeInstance(PropertyGroup):
     name: bpy.props.StringProperty(name="OpenPype Instance name")
     datablocks: bpy.props.CollectionProperty(
         name="OpenPype Instance Datablocks references",
-        type=OpenpypeInstanceDatablockRef,
+        type=OpenpypeDatablockRef,
     )
     datablock_active_index: bpy.props.IntProperty(
         name="Datablock Active Index"
@@ -28,19 +53,50 @@ class OpenpypeInstance(PropertyGroup):
     # "icons" (List): List of the icons names for the authorized types
 
 
+def get_container_name(self) -> str:
+    """Get name, apply it to the referenced outliner entity's name.
+
+    Returns:
+        str: Name
+    """
+    if self.outliner_entity and self.outliner_entity.name != self["name"]:
+        self["name"] = self.outliner_entity.name
+
+    return self["name"]
+
+
+def set_container_name(self, value: str):
+    """Set name, ensure the referenced outliner entity to have the same.
+
+    Args:
+        value (str): Name
+    """
+    if self.outliner_entity and self.outliner_entity.name != self["name"]:
+        self.outliner_entity.name = value
+
+    self["name"] = value
+
+
 class OpenpypeContainer(PropertyGroup):
-    name: bpy.props.StringProperty(name="OpenPype Container name")
+    name: bpy.props.StringProperty(
+        name="OpenPype Container name",
+        get=get_container_name,
+        set=set_container_name,
+    )
     datablocks: bpy.props.CollectionProperty(
         name="OpenPype Container Datablocks references",
-        type=OpenpypeInstanceDatablockRef,
+        type=OpenpypeDatablockRef,
     )
     library: bpy.props.PointerProperty(
         name="OpenPype Container source library", type=bpy.types.Library
     )
+    outliner_entity: bpy.props.PointerProperty(
+        name="Outliner entity reference", type=bpy.types.ID
+    )
 
 
 classes = [
-    OpenpypeInstanceDatablockRef,
+    OpenpypeDatablockRef,
     OpenpypeInstance,
     OpenpypeContainer,
 ]
