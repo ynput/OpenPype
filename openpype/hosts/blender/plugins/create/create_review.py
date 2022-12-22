9654937a63ea871cf7d98810a6505a5398de3884
Félix David <felixg.david@gmail.com>
2022-12-22 15:18:20 +0100

    Refactor: Review workflow seamless

--- openpype/hosts/blender/plugins/create/create_review.py
+++ openpype/hosts/blender/plugins/create/create_review.py
@@ -1,38 +1,12 @@
 """Create review."""
 
-import bpy
-
-from openpype.hosts.blender.api import plugin
-from openpype.hosts.blender.api.lib import get_selection
+from openpype.hosts.blender.plugins.create.create_camera import CreateCamera
 
 
-class CreateReview(plugin.Creator):
-    """Single baked camera"""
+class CreateReview(CreateCamera):
+    """Review is basically a camera with different integration."""
 
     name = "reviewDefault"
     label = "Review"
     family = "review"
     icon = "video-camera"
-    color_tag = "COLOR_07"
-
-    def _use_selection(self, container):
-        selected_objects = set(get_selection())
-        plugin.link_to_collection(selected_objects, container)
-
-    def _process(self):
-        # Get Instance Container
-        container = super()._process()
-
-        # Create camera if not get with use_selection.
-        for obj in container.all_objects:
-            if obj.type == "CAMERA":
-                break
-        else:
-            asset = self.data["asset"]
-            subset = self.data["subset"]
-            name = plugin.asset_name(asset, subset)
-            camera = bpy.data.cameras.new(name)
-            camera_obj = bpy.data.objects.new(name, camera)
-            container.objects.link(camera_obj)
-
-        return container
