90cde747f4242139558ba9733f4bea4c05deb55e
Kayla Man <moonyuet2008@hotmail.com>
2022-12-30 18:45:49 +0800

    adding path validator for non-maya nodes

--- openpype/hosts/maya/plugins/publish/validate_path_for_plugin.py
+++ openpype/hosts/maya/plugins/publish/validate_path_for_plugin.py
@@ -31,35 +31,35 @@ class ValidatePathForPlugin(pyblish.api.InstancePlugin):
         rman_archive = cmds.ls(type="RenderManArchive")
         if rman_archive:
             for rib in rman_archive:
-                rib_fname = cmds.getAttr(rib+".filename")
+                rib_fname = cmds.getAttr(rib + ".filename")
                 if rman_archive and not os.path.exists(rib_fname):
                     invalid.append(rib)
         # check Yeti node
         yeti_node = cmds.ls(type="pgYetiMaya")
         if yeti_node:
             for yeti in yeti_node:
-                yeti_fname = cmds.getAttr(yeti+".cacheFileName")
+                yeti_fname = cmds.getAttr(yeti + ".cacheFileName")
                 if yeti_fname and not os.path.exists(yeti_fname):
                     invalid.append(yeti)
         # check arnold_standin node
         arnold_standin = cmds.ls(type="aiStandIn")
         if arnold_standin:
             for standin in arnold_standin:
-                standin_fname = cmds.getAttr(standin+".dso")
+                standin_fname = cmds.getAttr(standin + ".dso")
                 if standin_fname and not os.path.exists(standin_fname):
                     invalid.append(standin)
         # check vray proxy node
         vray_proxy = cmds.ls(type="VRayProxy")
         if vray_proxy:
             for vray in vray_proxy:
-                vray_fname = cmds.getAttr(vray+".fileName")
+                vray_fname = cmds.getAttr(vray + ".fileName")
                 if vray_fname and not os.path.exists(vray_fname):
                     invalid.append(vray)
         # check redshift proxy node
         redshift_proxy = cmds.ls(type="RedshiftProxyMesh")
         if redshift_proxy:
             for redshift in redshift_proxy:
-                red_fname = cmds.getAttr(redshift+".fileName")
+                red_fname = cmds.getAttr(redshift + ".fileName")
                 if red_fname and not os.path.exists(red_fname):
                     invalid.append(redshift)
 
