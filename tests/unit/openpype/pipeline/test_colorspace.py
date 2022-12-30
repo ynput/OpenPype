72cb5ae754a04125241b5a1bd0c3bd7a2f2bc305
Jakub Jezek <jakubjezek001@gmail.com>
2022-12-30 20:48:43 +0100

    testing: pipeline colorspace get views

--- tests/unit/openpype/pipeline/test_colorspace.py
+++ tests/unit/openpype/pipeline/test_colorspace.py
@@ -24,7 +24,7 @@ log = logging.getLogger("test_colorspace")
 
 class TestPipelineColorspace(ModuleUnitTest):
 
-    # TEST_DATA_FOLDER = "C:\\CODE\\__PYPE\\__unit_testing_data\\test_pipeline_colorspace"
+    TEST_DATA_FOLDER = "C:\\CODE\\__PYPE\\__unit_testing_data\\test_pipeline_colorspace"
     TEST_FILES = [
         (
             "1JSrzYoglUzAGbJEfAOa91AeyB6fvGBOK",
@@ -65,6 +65,54 @@ class TestPipelineColorspace(ModuleUnitTest):
         ret = colorspace.parse_colorspace_from_filepath(
             path, "nuke", "test_project", config_data=config_data
         )
+
+        assert ret == expected, f"Not matching colorspace {expected}"
+
+    def test_get_ocio_config_views(self, config_path):
+        expected = {
+            "AYON/test_sRGB": {
+                "display": "AYON", "view": "test_sRGB"},
+            "ACES/sRGB": {
+                "display": "ACES", "view": "sRGB"},
+            "ACES/DCDM": {
+                "display": "ACES", "view": "DCDM"},
+            "ACES/DCDM P3D60 Limited": {
+                "display": "ACES", "view": "DCDM P3D60 Limited"},
+            "ACES/DCDM P3D65 Limited": {
+                "display": "ACES", "view": "DCDM P3D65 Limited"},
+            "ACES/P3-D60": {
+                "display": "ACES", "view": "P3-D60"},
+            "ACES/P3-DCI D60 simulation": {
+                "display": "ACES", "view": "P3-DCI D60 simulation"},
+            "ACES/P3-DCI D65 simulation": {
+                "display": "ACES", "view": "P3-DCI D65 simulation"},
+            "ACES/P3D65": {
+                "display": "ACES", "view": "P3D65"},
+            "ACES/P3D65 D60 simulation": {
+                "display": "ACES", "view": "P3D65 D60 simulation"},
+            "ACES/P3D65 Rec.709 Limited": {
+                "display": "ACES", "view": "P3D65 Rec.709 Limited"},
+            "ACES/Rec.2020": {
+                "display": "ACES", "view": "Rec.2020"},
+            "ACES/Rec.2020 P3D65 Limited": {
+                "display": "ACES", "view": "Rec.2020 P3D65 Limited"},
+            "ACES/Rec.2020 Rec.709 Limited": {
+                "display": "ACES", "view": "Rec.2020 Rec.709 Limited"},
+            "ACES/Rec.2020 HLG 1000 nits": {
+                "display": "ACES", "view": "Rec.2020 HLG 1000 nits"},
+            "ACES/Rec.709": {
+                "display": "ACES", "view": "Rec.709"},
+            "ACES/Rec.709 D60 sim.": {
+                "display": "ACES", "view": "Rec.709 D60 sim."},
+            "ACES/sRGB D60 sim.": {
+                "display": "ACES", "view": "sRGB D60 sim."},
+            "ACES/Raw": {
+                "display": "ACES", "view": "Raw"},
+            "ACES/Log": {
+                "display": "ACES", "view": "Log"}
+        }
+
+        ret = colorspace.get_ocio_config_views(config_path)
 
         assert ret == expected, f"Not matching colorspace {expected}"
 
