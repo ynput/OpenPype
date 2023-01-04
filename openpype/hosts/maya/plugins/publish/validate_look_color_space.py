1ba5223801fa9fa6695fa62a59dd53eaca7be27c
Kayla Man <moonyuet2008@hotmail.com>
2023-01-04 23:49:07 +0800

    adding optional validator for maya color space

---
+++ openpype/hosts/maya/plugins/publish/validate_look_color_space.py
@@ -0,0 +1,28 @@
+from maya import cmds
+
+import pyblish.api
+import openpype.hosts.maya.api.action
+from openpype.pipeline.publish import ValidateContentsOrder
+
+
+class ValidateMayaColorSpace(pyblish.api.InstancePlugin):
+    """
+        Check if the OCIO Color Management and maketx options
+        enabled at the same time
+
+    """
+
+    order = ValidateContentsOrder
+    families = ['look']
+    hosts = ['maya']
+    label = 'Maya Color Space'
+    optional = True
+
+    def process(self, instance):
+        ocio_maya = cmds.colorManagementPrefs(q=True,
+                                              cmConfigFileEnabled=True,
+                                              cmEnabled=True)
+        maketx = instance.data["maketx"]
+
+        if ocio_maya and maketx:
+            raise Exception("Maya is color managed and maketx option is on. OpenPype doesn't support this combination yet.") # noqa
