3711584b208692cbe86ea3f563d04d574c3193de
Felix David <felixg.david@gmail.com>
2023-01-09 13:03:17 +0100

    loader auto assigned for auto created containers

--- openpype/hosts/blender/api/pipeline.py
+++ openpype/hosts/blender/api/pipeline.py
@@ -4,6 +4,7 @@ import traceback
 from typing import Callable, Dict, List, Optional
 
 import bpy
+from openpype.hosts.blender.api import utils
 
 from . import lib
 from . import ops, properties, ui
@@ -41,7 +42,9 @@ SCRIPTS_PATH = os.path.join(HOST_DIR, "scripts")
 
 ORIGINAL_EXCEPTHOOK = sys.excepthook
 
-AVALON_PROPERTY = 'avalon'
+# TODO refactor all imports to avoid this duplicate
+AVALON_PROPERTY = utils.AVALON_PROPERTY
+
 IS_HEADLESS = bpy.app.background
 
 log = Logger.get_logger(__name__)
