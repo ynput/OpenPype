f854de46295da4d96c007335421647f78b2e6b36
Petr Kalis <petr.kalis@gmail.com>
2023-01-11 15:06:03 +0100

    OP-4679  - revert back fallback
    Availability of comment on instance has been resolved by bumping up order of CollectComment.

--- openpype/modules/ftrack/plugins/publish/integrate_ftrack_note.py
+++ openpype/modules/ftrack/plugins/publish/integrate_ftrack_note.py
@@ -45,8 +45,7 @@ class IntegrateFtrackNote(pyblish.api.InstancePlugin):
         host_name = context.data["hostName"]
         app_name = context.data["appName"]
         app_label = context.data["appLabel"]
-        # context comment is fallback until old Pyblish is removed
-        comment = instance.data["comment"] or context.data.get("comment")
+        comment = instance.data["comment"]
         if not comment:
             self.log.info("Comment is not set.")
         else:
