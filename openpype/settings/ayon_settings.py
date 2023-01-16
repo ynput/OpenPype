8112a9eea3370d1d7f340900826ab9a0ef5ca597
Jakub Trllo <jakub.trllo@gmail.com>
2023-01-16 13:47:45 +0100

    added loading of settings from ayon server

--- openpype/settings/ayon_settings.py
+++ openpype/settings/ayon_settings.py
@@ -1,8 +1,10 @@
-import copy
 import os
 import json
+import copy
+import time
 
 import six
+import ayon_api
 
 
 def _convert_color(color_value):
@@ -510,10 +512,10 @@ def _convert_shotgrid_project_settings(ayon_settings, output):
 
     ayon_shotgrid = ayon_settings["shotgrid"]
     for key in {
-        'leecher_backend_url',
-        'filter_projects_by_login',
-        'shotgrid_settings',
-        'leecher_manager_url'
+        "leecher_backend_url",
+        "filter_projects_by_login",
+        "shotgrid_settings",
+        "leecher_manager_url"
     }:
         ayon_shotgrid.pop(key)
 
@@ -671,3 +673,51 @@ def convert_project_settings(ayon_settings, default_settings):
     }
 
     return output
+
+
+class CacheItem:
+    lifetime = 10
+
+    def __init__(self, value):
+        self._value = value
+        self._outdate_time = time.time() + self.lifetime
+
+    def get_value(self):
+        return copy.deepcopy(self._value)
+
+    def update_value(self, value):
+        self._value = value
+
+    @property
+    def is_outdated(self):
+        return time.time() > self._outdate_time
+
+
+class AyonSettingsCahe:
+    _cache_by_project_name = {}
+
+    @classmethod
+    def get_value_by_project(cls, project_name):
+        cache_item = cls._cache_by_project_name.get(project_name)
+        if cache_item is None or cache_item.is_outdated:
+            if project_name is None:
+                value = ayon_api.get_production_settings()
+            else:
+                value = ayon_api.get_project_settings(project_name)
+
+            if cache_item is None:
+                cache_item = CacheItem(value)
+                cls._cache_by_project_name[project_name] = cache_item
+            else:
+                cache_item.update_value(value)
+        return cache_item.get_value()
+
+
+def get_ayon_project_settings(default_values, project_name):
+    ayon_settings = AyonSettingsCahe.get_value_by_project(project_name)
+    return convert_project_settings(ayon_settings, default_values)
+
+
+def get_ayon_system_settings(default_values):
+    ayon_settings = AyonSettingsCahe.get_value_by_project(None)
+    return convert_system_settings(ayon_settings, default_values)
