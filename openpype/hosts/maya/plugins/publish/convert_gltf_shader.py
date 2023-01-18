be2dcee1b9cb8c932b2b6d8894bc4e735fbc2e34
Kayla Man <64118225+moonyuet@users.noreply.github.com>
2023-01-18 21:17:10 +0800

    Update the correct version of the script

--- openpype/hosts/maya/plugins/publish/convert_gltf_shader.py
+++ openpype/hosts/maya/plugins/publish/convert_gltf_shader.py
@@ -21,7 +21,7 @@ class ConvertGLSLShader(publish.Extractor):
         meshes = cmds.ls(instance, type="mesh", long=True)
         self.log.info("meshes: {}".format(meshes))
         # load the glsl shader plugin
-        cmds.loadPlugin("glslShader.mll", quiet=True)
+        cmds.loadPlugin("glslShader", quiet=True)
 
         for mesh in meshes:
 
@@ -29,55 +29,64 @@ class ConvertGLSLShader(publish.Extractor):
             glsl = cmds.createNode('GLSLShader')
             glsl_shadingGrp = cmds.sets(name=glsl + "SG", empty=True,
                                         renderable=True, noSurfaceShader=True)
-            cmds.connectAttr(glsl + ".outColor", glsl_shadingGrp + ".surfaceShader")
+            cmds.connectAttr(glsl + ".outColor",
+                             glsl_shadingGrp + ".surfaceShader")
 
             # load the maya2gltf shader
-            maya_dir = os.getenv("MAYA_APP_DIR")
-            ogsfx = maya_dir + "/maya2glTF/PBR/shaders/glTF_PBR.ogsfx"
+            maya_publish = instance.context.data["project_settings"]["maya"]["publish"]
+
+            ogsfx_path = maya_publish["ConvertGLSLShader"]["ogsfx_path"]
+            if not ogsfx_path:
+                maya_dir = os.getenv("MAYA_APP_DIR")
+                if not maya_dir:
+                    raise RuntimeError("MAYA_APP_DIR not found")
+                ogsfx_path = maya_dir + "/maya2glTF/PBR/shaders/"
+            if not os.path.exists(ogsfx_path):
+                raise RuntimeError("the ogsfx file not found")
+
+            ogsfx = ogsfx_path + "glTF_PBR.ogsfx"
             cmds.setAttr(glsl + ".shader", ogsfx, typ="string")
 
             # list the materials used for the assets
-            shading_grp = cmds.listConnections(
-                          mesh, destination=True,
-                          type="shadingEngine")
+            shading_grp = cmds.listConnections(mesh,
+                                               destination=True,
+                                               type="shadingEngine")
 
             # get the materials related to the selected assets
             for material in shading_grp:
-                main_shader = cmds.listConnections(
-                              material, destination=True,
-                              type="StingrayPBS")
+                main_shader = cmds.listConnections(material,
+                                                   destination=True,
+                                                   type="StingrayPBS")
                 for shader in main_shader:
                     # get the file textures related to the PBS Shader
-                    albedo = cmds.listConnections(shader + ".TEX_color_map")[0]
-                    dif_output = albedo + ".outColor"
+                    albedo = cmds.listConnections(shader + ".TEX_color_map")
+                    if albedo:
+                        dif_output = albedo[0] + ".outColor"
+                        # get the glsl_shader input
+                        # reconnect the file nodes to maya2gltf shader
+                        glsl_dif = glsl + ".u_BaseColorTexture"
+                        cmds.connectAttr(dif_output, glsl_dif)
 
-                    orm_packed = cmds.listConnections(shader + ".TEX_ao_mapX")[0]
-                    ao_output = orm_packed + ".outColorR"
-                    rough_output = orm_packed + ".outColorG"
-                    metallic_output = orm_packed + ".outColorB"
+                    # connect orm map if there is one
+                    orm_packed = cmds.listConnections(shader +
+                                                      ".TEX_ao_map")
+                    if orm_packed:
+                        orm_output = orm_packed[0] + ".outColor"
 
-                    nrm = cmds.listConnections(shader + ".TEX_normal_map")[0]
-                    nrm_output = nrm + ".outColor"
+                        mtl = glsl + ".u_MetallicTexture"
+                        ao = glsl + ".u_OcclusionTexture"
+                        rough = glsl + ".u_RoughnessTexture"
 
-                    # get the glsl_shader input
-                    # reconnect the file nodes to maya2gltf shader
-                    glsl_dif = glsl + ".u_BaseColorTexture"
-                    glsl_nrm = glsl + ".u_NormalTexture"
-                    cmds.connectAttr(dif_output, glsl_dif)
-                    cmds.connectAttr(nrm_output, glsl_nrm)
+                        cmds.connectAttr(orm_output, mtl)
+                        cmds.connectAttr(orm_output, ao)
+                        cmds.connectAttr(orm_output, rough)
 
-                    rgb_list = ["R", "G", "B"]
-                    for ch in rgb_list:
-                        mtl = ".u_MetallicTexture.u_MetallicTexture{}".format(ch) # noqa
-                        mtl = glsl + mtl
-                        ao = ".u_OcclusionTexture.u_OcclusionTexture{}".format(ch) # noqa
-                        ao = glsl + ao
-                        rough = ".u_RoughnessTexture.u_RoughnessTexture{}".format(ch) # noqa
-                        rough = glsl + rough
-
-                        cmds.connectAttr(metallic_output, mtl)
-                        cmds.connectAttr(ao_output, ao)
-                        cmds.connectAttr(rough_output, rough)
+                    # connect nrm map if there is one
+                    nrm = cmds.listConnections(shader + ".TEX_normal_map")
+                    if nrm:
+                        nrm_output = nrm[0] + ".outColor"
+                        glsl_nrm = glsl + ".u_NormalTexture"
+                        cmds.connectAttr(nrm_output, glsl_nrm)
 
             # assign the shader to the asset
             cmds.sets(mesh, forceElement=str(glsl_shadingGrp))
