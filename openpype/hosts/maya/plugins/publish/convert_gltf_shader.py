bcde0c61c7a4aaa49c12e8da8e98a0d97586cf5c
Kayla Man <64118225+moonyuet@users.noreply.github.com>
2023-01-18 21:13:38 +0800

    Update convert_gltf_shader.py

--- openpype/hosts/maya/plugins/publish/convert_gltf_shader.py
+++ openpype/hosts/maya/plugins/publish/convert_gltf_shader.py
@@ -1,139 +1,83 @@
+import os
 from maya import cmds
-
 import pyblish.api
-from openpype.pipeline.publish import ValidateContentsOrder
+
+from openpype.pipeline import publish
 
 
-class ValidateGLTFTexturesNames(pyblish.api.InstancePlugin):
+class ConvertGLSLShader(publish.Extractor):
     """
-    Validate if the asset uses StingrayPBS material before conversion
-    of the GLSL Shader
-    Validate if the names of GLTF Textures follow
-    the packed ORM/ORMS standard.
-
-    The texture naming conventions follows the UE5-style-guides:
-    https://github.com/Allar/ue5-style-guide#anc-textures-packing
-
-    ORM: Occlusion Roughness Metallic
-    ORMS: Occlusion Roughness Metallic Specular
-
-    Texture Naming Style:
-
-    Albedo/Diffuse: {Name}_D.{imageExtension} or
-                    {Name}_D.<UDIM>.{imageExtension}
-
-    Normal: {Name}_N.{imageExtension} or
-            {Name}_N.<UDIM>.{imageExtension}
-    ORM: {Name}_ORM.{imageExtension} or
-         {Name}_ORM.<UDIM>.{imageExtension}
+    Converting StingrayPBS material to GLSL Shaders
+    specially for the glb export through Maya2GLTF plugin
 
     """
-
-    order = ValidateContentsOrder
-    families = ['gltf']
-    hosts = ['maya']
-    label = 'GLTF Textures Name'
+    order = pyblish.api.ExtractorOrder - 0.1
+    hosts = ["maya"]
+    label = "Convert StingrayPBS to GLTF"
+    families = ["gltf"]
+    optional = True
 
     def process(self, instance):
-        """Process all the nodes in the instance"""
-        pbs_shader = cmds.ls(type="StingrayPBS")
-        if not pbs_shader:
-            raise RuntimeError("No PBS Shader in the scene")
-        shading_grp = self.get_material_from_shapes(instance)
-        if not shading_grp:
-            raise RuntimeError("No shading group found")
-        invalid = self.get_texture_shader_invalid(instance)
-        if invalid:
-            raise RuntimeError("Non PBS material found: "
-                               "{0}".format(invalid))
-        invalid = self.get_texture_node_invalid(instance)
-        if invalid:
-            raise RuntimeError("At least an Albedo texture file in "
-                               "{0}".format(invalid))
-        invalid = self.get_texture_name_invalid(instance)
-        if invalid:
-            raise RuntimeError("Invalid texture name(s) found: "
-                               "{0}".format(invalid))
+        meshes = cmds.ls(instance, type="mesh", long=True)
+        self.log.info("meshes: {}".format(meshes))
+        # load the glsl shader plugin
+        cmds.loadPlugin("glslShader.mll", quiet=True)
 
-    def get_texture_name_invalid(self, instance):
+        for mesh in meshes:
 
-        invalid = set()
-        shading_grp = self.get_material_from_shapes(instance)
+            # create glsl shader
+            glsl = cmds.createNode('GLSLShader')
+            glsl_shadingGrp = cmds.sets(name=glsl + "SG", empty=True,
+                                        renderable=True, noSurfaceShader=True)
+            cmds.connectAttr(glsl + ".outColor", glsl_shadingGrp + ".surfaceShader")
 
-        # get the materials related to the selected assets
-        # get the file textures related to the PBS Shader
-        # validate the names of the textures
-        for shading_group in shading_grp:
-            main_shader = cmds.listConnections(shading_group,
-                                            destination=True,
-                                            type="StingrayPBS")
-            for shader in main_shader:
-                albedo = cmds.listConnections(shader + ".TEX_color_map")[0]
-                dif_path = cmds.getAttr(albedo + ".fileTextureName")
-                dif = dif_path.split(".")[0]
-                # "_D"
-                if not dif.endswith("_D"):
-                    invalid.add(dif_path)
-                orm_packed = cmds.listConnections(shader + ".TEX_ao_map")
-                if orm_packed:
-                    orm_path = cmds.getAttr(orm_packed[0] + ".fileTextureName")
-                    orm = orm_path.split(".")[0]
-                    # "_ORM"
-                    if not orm.endswith("_ORM"):
-                        invalid.add(orm_path)
-                nrm = cmds.listConnections(shader + ".TEX_normal_map")
-                if nrm:
-                    nrm_path = cmds.getAttr(nrm[0] + ".fileTextureName")
-                    nrm_map = nrm_path.split(".")[0]
-                    # "_N"
-                    if not nrm_map.endswith("_N"):
-                        invalid.add(nrm_path)
+            # load the maya2gltf shader
+            maya_dir = os.getenv("MAYA_APP_DIR")
+            ogsfx = maya_dir + "/maya2glTF/PBR/shaders/glTF_PBR.ogsfx"
+            cmds.setAttr(glsl + ".shader", ogsfx, typ="string")
 
-        return list(invalid)
+            # list the materials used for the assets
+            shading_grp = cmds.listConnections(
+                          mesh, destination=True,
+                          type="shadingEngine")
 
-    def get_texture_node_invalid(self, instance):
-        invalid = set()
-        shading_grp = self.get_material_from_shapes(instance)
-        for shading_group in shading_grp:
-            main_shader = cmds.listConnections(shading_group,
-                                            destination=True,
-                                            type="StingrayPBS")
-            for shader in main_shader:
-                # diffuse texture file node
-                albedo = cmds.listConnections(shader + ".TEX_color_map")
-                if not albedo:
-                    # add the material name
-                    invalid.add(shader)
-                orm_packed = cmds.listConnections(shader + ".TEX_ao_map")
-                if not orm_packed:
-                    continue
-                nrm = cmds.listConnections(shader + ".TEX_normal_map")
-                if not nrm:
-                    continue
-        return list(invalid)
+            # get the materials related to the selected assets
+            for material in shading_grp:
+                main_shader = cmds.listConnections(
+                              material, destination=True,
+                              type="StingrayPBS")
+                for shader in main_shader:
+                    # get the file textures related to the PBS Shader
+                    albedo = cmds.listConnections(shader + ".TEX_color_map")[0]
+                    dif_output = albedo + ".outColor"
 
-    def get_texture_shader_invalid(self, instance):
+                    orm_packed = cmds.listConnections(shader + ".TEX_ao_mapX")[0]
+                    ao_output = orm_packed + ".outColorR"
+                    rough_output = orm_packed + ".outColorG"
+                    metallic_output = orm_packed + ".outColorB"
 
-        invalid = set()
-        shading_grp = self.get_material_from_shapes(instance)
-        for shading_group in shading_grp:
-            material_name = "{}.surfaceShader".format(shading_group)
-            material = cmds.listConnections(material_name,
-                                            source=True,
-                                            destination=False,
-                                            type="StingrayPBS")
+                    nrm = cmds.listConnections(shader + ".TEX_normal_map")[0]
+                    nrm_output = nrm + ".outColor"
 
-            if not material:
-                # add material name
-                material = cmds.listConnections(material_name)[0]
-                invalid.add(material)
-        return list(invalid)
+                    # get the glsl_shader input
+                    # reconnect the file nodes to maya2gltf shader
+                    glsl_dif = glsl + ".u_BaseColorTexture"
+                    glsl_nrm = glsl + ".u_NormalTexture"
+                    cmds.connectAttr(dif_output, glsl_dif)
+                    cmds.connectAttr(nrm_output, glsl_nrm)
 
-    def get_material_from_shapes(self, instance):
-        shapes = cmds.ls(instance, type="mesh", long=True)
-        for shape in shapes:
-            shading_grp = cmds.listConnections(shape,
-                                               destination=True,
-                                               type="shadingEngine")
+                    rgb_list = ["R", "G", "B"]
+                    for ch in rgb_list:
+                        mtl = ".u_MetallicTexture.u_MetallicTexture{}".format(ch) # noqa
+                        mtl = glsl + mtl
+                        ao = ".u_OcclusionTexture.u_OcclusionTexture{}".format(ch) # noqa
+                        ao = glsl + ao
+                        rough = ".u_RoughnessTexture.u_RoughnessTexture{}".format(ch) # noqa
+                        rough = glsl + rough
 
-            return shading_grp or []
+                        cmds.connectAttr(metallic_output, mtl)
+                        cmds.connectAttr(ao_output, ao)
+                        cmds.connectAttr(rough_output, rough)
+
+            # assign the shader to the asset
+            cmds.sets(mesh, forceElement=str(glsl_shadingGrp))
