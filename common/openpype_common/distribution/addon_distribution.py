d5f3e35e306d0de358a244ce10f000e0a4769d5d
Jakub Trllo <jakub.trllo@gmail.com>
2023-01-20 19:21:23 +0100

    add docstrings to local dir functions

--- common/openpype_common/distribution/addon_distribution.py
+++ common/openpype_common/distribution/addon_distribution.py
@@ -30,6 +30,15 @@ ADDON_ENDPOINT = "addons?details=1"
 
 
 def get_local_dir(*subdirs):
+    """Get product directory in user's home directory.
+
+    Each user on machine have own local directory where are downloaded updates,
+    addons etc.
+
+    Returns:
+        str: Path to product local directory.
+    """
+
     if not subdirs:
         raise RuntimeError("Must fill dir_name if nothing else provided!")
 
@@ -47,6 +56,18 @@ def get_local_dir(*subdirs):
 
 
 def get_addons_dir():
+    """Directory where addon packages are stored.
+
+    Path to addons is defined using python module 'appdirs' which
+
+    The path is stored into environment variable 'AYON_ADDONS_DIR'.
+    Value of environment variable can be overriden, but we highly recommended
+    to use that option only for development purposes.
+
+    Returns:
+        str: Path to directory where addons should be downloaded.
+    """
+
     addons_dir = os.environ.get("AYON_ADDONS_DIR")
     if not addons_dir:
         addons_dir = get_local_dir("addons")
@@ -55,6 +76,18 @@ def get_addons_dir():
 
 
 def get_dependencies_dir():
+    """Directory where dependency packages are stored.
+
+    Path to addons is defined using python module 'appdirs' which
+
+    The path is stored into environment variable 'AYON_DEPENDENCIES_DIR'.
+    Value of environment variable can be overriden, but we highly recommended
+    to use that option only for development purposes.
+
+    Returns:
+        str: Path to directory where dependency packages should be downloaded.
+    """
+
     dependencies_dir = os.environ.get("AYON_DEPENDENCIES_DIR")
     if not dependencies_dir:
         dependencies_dir = get_local_dir("dependency_packages")
