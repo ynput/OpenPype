8ca483adaf2135ad66f2a126228fc1b71164c16e
Libor Batek <lbatek@gmail.com>
2023-01-25 11:36:07 +0100

    first rough concept for 3dsmax user doc

--- website/sidebars.js
+++ website/sidebars.js
@@ -49,6 +49,7 @@ module.exports = {
                     ],
                 },
                 "artist_hosts_blender",
+                "artist_hosts_3dsmax",
                 "artist_hosts_harmony",
                 "artist_hosts_houdini",
                 "artist_hosts_aftereffects",
