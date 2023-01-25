4ffef19e28d1d8f22d36eb6416228545721eff4e
Roy Nieterau <roy_nieterau@hotmail.com>
2023-01-25 13:59:19 +0100

    Refactor to use new style creator

--- openpype/hosts/houdini/plugins/create/create_arnold_rop.py
+++ openpype/hosts/houdini/plugins/create/create_arnold_rop.py
@@ -1,7 +1,7 @@
 from openpype.hosts.houdini.api import plugin
 
 
-class CreateArnoldRop(plugin.Creator):
+class CreateArnoldRop(plugin.HoudiniCreator):
     """Arnold ROP"""
 
     label = "Arnold ROP"
@@ -9,41 +9,47 @@ class CreateArnoldRop(plugin.Creator):
     icon = "magic"
     defaults = ["master"]
 
-    def __init__(self, *args, **kwargs):
-        super(CreateArnoldRop, self).__init__(*args, **kwargs)
+    # Default extension
+    ext = ".exr"
 
-        # Clear the family prefix from the subset
-        subset = self.data["subset"]
-        subset_no_prefix = subset[len(self.family):]
-        subset_no_prefix = subset_no_prefix[0].lower() + subset_no_prefix[1:]
-        self.data["subset"] = subset_no_prefix
-
-        # Add chunk size attribute
-        self.data["chunkSize"] = 1
+    def create(self, subset_name, instance_data, pre_create_data):
+        import hou
 
         # Remove the active, we are checking the bypass flag of the nodes
-        self.data.pop("active", None)
+        instance_data.pop("active", None)
+        instance_data.update({"node_type": "arnold"})
 
-        self.data.update({"node_type": "arnold"})
+        # Add chunk size attribute
+        instance_data["chunkSize"] = 1
 
-    def _process(self, instance):
+        instance = super(CreateArnoldRop, self).create(
+            subset_name,
+            instance_data,
+            pre_create_data)  # type: plugin.CreatedInstance
 
-        basename = instance.name()
-        instance.setName(basename + "_ROP", unique_name=True)
+        instance_node = hou.node(instance.get("instance_node"))
 
-        prefix = '${HIP}/render/${HIPNAME}/`chs("subset")`.$F4.exr'
+        # Hide Properties Tab on Arnold ROP since that's used
+        # for rendering instead of .ass Archive Export
+        parm_template_group = instance_node.parmTemplateGroup()
+        parm_template_group.hideFolder("Properties", True)
+        instance_node.setParmTemplateGroup(parm_template_group)
+
+        filepath = "{}{}".format(
+            hou.text.expandString("$HIP/pyblish/"),
+            "{}.$F4{}".format(subset_name, self.ext)
+        )
         parms = {
             # Render frame range
             "trange": 1,
 
             # Arnold ROP settings
-            "ar_picture": prefix,
+            "ar_picture": filepath,
             "ar_exr_half_precision": 1           # half precision
         }
-        instance.setParms(parms)
 
-        # Lock some Avalon attributes
+        instance_node.setParms(parms)
+
+        # Lock any parameters in this list
         to_lock = ["family", "id"]
-        for name in to_lock:
-            parm = instance.parm(name)
-            parm.lock(True)
+        self.lock_parameters(instance_node, to_lock)
