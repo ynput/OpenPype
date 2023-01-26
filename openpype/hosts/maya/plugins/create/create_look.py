783bd8622e47f66a3252bcbb78ab09eba114e3d3
Roy Nieterau <roy_nieterau@hotmail.com>
2023-01-27 00:19:54 +0100

    Maya: Refactor Create Look to new publisher

--- openpype/hosts/maya/plugins/create/create_look.py
+++ openpype/hosts/maya/plugins/create/create_look.py
@@ -1,27 +1,48 @@
 from openpype.hosts.maya.api import (
-    lib,
-    plugin
+    plugin,
+    lib
+)
+from openpype.lib import (
+    BoolDef,
+    TextDef
 )
 
 
-class CreateLook(plugin.Creator):
+class CreateLook(plugin.MayaCreator):
     """Shader connections defining shape look"""
 
+    identifier = "io.openpype.creators.maya.look"
     name = "look"
     label = "Look"
     family = "look"
     icon = "paint-brush"
+
     make_tx = True
 
-    def __init__(self, *args, **kwargs):
-        super(CreateLook, self).__init__(*args, **kwargs)
+    def get_instance_attr_defs(self):
 
-        self.data["renderlayer"] = lib.get_current_renderlayer()
+        return [
+            # TODO: This value should actually get set on create!
+            TextDef("renderLayer",
+                    # TODO: Bug: Hidden attribute's label is still shown in UI?
+                    hidden=True,
+                    default=lib.get_current_renderlayer(),
+                    label="Renderlayer",
+                    tooltip="Renderlayer to extract the look from"),
+            BoolDef("maketx",
+                    label="MakeTX",
+                    tooltip="Whether to generate .tx files for your textures",
+                    default=self.make_tx),
+            BoolDef("forceCopy",
+                    label="Force Copy",
+                    tooltip="Enable users to force a copy instead of hardlink."
+                            "\nNote: On Windows copy is always forced due to "
+                            "bugs in windows' implementation of hardlinks.",
+                    default=False)
+        ]
 
-        # Whether to automatically convert the textures to .tx upon publish.
-        self.data["maketx"] = self.make_tx
-
-        # Enable users to force a copy.
-        # - on Windows is "forceCopy" always changed to `True` because of
-        #   windows implementation of hardlinks
-        self.data["forceCopy"] = False
+    def get_pre_create_attr_defs(self):
+        # Show same attributes on create but include use selection
+        defs = super(CreateLook, self).get_pre_create_attr_defs()
+        defs.extend(self.get_instance_attr_defs())
+        return defs
