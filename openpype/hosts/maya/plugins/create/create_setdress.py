ccb4e956aef1adffeb42b5f4714a1192a049d975
Roy Nieterau <roy_nieterau@hotmail.com>
2023-01-27 00:48:43 +0100

    Maya: Refactor Create Setdress to new publisher

--- openpype/hosts/maya/plugins/create/create_setdress.py
+++ openpype/hosts/maya/plugins/create/create_setdress.py
@@ -1,16 +1,19 @@
 from openpype.hosts.maya.api import plugin
+from openpype.lib import BoolDef
 
 
-class CreateSetDress(plugin.Creator):
+class CreateSetDress(plugin.MayaCreator):
     """A grouped package of loaded content"""
 
-    name = "setdressMain"
+    identifier = "io.openpype.creators.maya.setdress"
     label = "Set Dress"
     family = "setdress"
     icon = "cubes"
     defaults = ["Main", "Anim"]
 
-    def __init__(self, *args, **kwargs):
-        super(CreateSetDress, self).__init__(*args, **kwargs)
-
-        self.data["exactSetMembersOnly"] = True
+    def get_instance_attr_defs(self):
+        return [
+            BoolDef("exactSetMembersOnly",
+                    label="Exact Set Members Only",
+                    default=True)
+        ]
