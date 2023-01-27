7153ba6eb90af83dcf5b58bc6db447e1f1ef9a06
Roy Nieterau <roy_nieterau@hotmail.com>
2023-01-27 01:28:26 +0100

    Fix code style

--- openpype/hosts/maya/plugins/publish/validate_plugin_path_attributes.py
+++ openpype/hosts/maya/plugins/publish/validate_plugin_path_attributes.py
@@ -39,7 +39,7 @@ class ValidatePluginPathAttributes(pyblish.api.InstancePlugin):
                 filepath = cmds.getAttr(file_attr)
 
                 if filepath and not os.path.exists(filepath):
-                    self.log.error("File {0} not exists".format(filepath)) # noqa
+                    self.log.error("File {0} not exists".format(filepath))  # noqa
                     invalid.append(target)
 
         return invalid
