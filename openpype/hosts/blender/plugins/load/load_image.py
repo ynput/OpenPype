4994e788bd957ba211b77fa72678d18728f9d5ce
Félix David <felixg.david@gmail.com>
2023-01-27 12:53:11 +0100

    refactor load image

--- openpype/hosts/blender/plugins/load/load_image.py
+++ openpype/hosts/blender/plugins/load/load_image.py
@@ -1,9 +1,12 @@
 """Load image in Blender."""
 
+from pathlib import Path
+from typing import Set, Tuple
 import bpy
 
 from openpype.hosts.blender.api import plugin
 from openpype.hosts.blender.api.lib import get_selection
+from openpype.hosts.blender.api.properties import OpenpypeContainer
 
 
 class ImageLoader(plugin.AssetLoader):
@@ -21,22 +24,40 @@ class ImageLoader(plugin.AssetLoader):
     icon = "image"
     order = 0
     color = "orange"
-    color_tag = "COLOR_02"
 
-    def process_asset(self, context: dict, *args, **kwargs) -> bpy.types.Image:
-        """Process load image in Blender"""
-        libpath = self.fname
-        asset = context["asset"]["name"]
-        subset = context["subset"]["name"]
+    load_type = "LINK"
 
-        img = bpy.data.images.load(libpath)
-        img.name = plugin.asset_name(asset, subset)
+    def _load_library_datablocks(
+        self,
+        libpath: Path,
+        container_name: str,
+        container: OpenpypeContainer = None,
+        **_kwargs
+    ) -> Tuple[OpenpypeContainer, Set[bpy.types.ID]]:
+        """OVERRIDE Load datablocks from blend file library.
+
+        Args:
+            libpath (Path): Path of library.
+            container_name (str): Name of container to be loaded.
+            container (OpenpypeContainer): Load into existing container.
+                Defaults to None.
+
+        Returns:
+            Tuple[OpenpypeContainer, Set[bpy.types.ID]]:
+                (Created scene container, Loaded datablocks)
+        """
+        img = bpy.data.images.load(libpath.as_posix())
 
         # Setup image as movie clip if inpute file extension is mov or mp4.
-        if libpath.endswith((".mov", "mp4")):
-            img.source = 'MOVIE'
+        if libpath.suffix in (".mov", "mp4"):
+            img.source = "MOVIE"
 
-        return img
+        # Put into a container
+        datablocks = [img]
+        container = self._containerize_datablocks(
+            container_name, datablocks, container
+        )
+        return container, datablocks
 
 
 class ReferenceLoader(ImageLoader):
@@ -49,39 +70,32 @@ class ReferenceLoader(ImageLoader):
     label = "Load Reference"
     order = 1
 
-    def process_asset(
-        self, context: dict, *args, **kwargs
-    ) -> bpy.types.Object:
-        img = super().process_asset(context, *args, **kwargs)
-
-        libpath = self.fname
-        asset = context["asset"]["name"]
-        subset = context["subset"]["name"]
-        group_name = plugin.asset_name(asset, subset)
+    def load(self, *args, **kwargs):
+        """OVERRIDE"""
+        container, datablocks = super().load(*args, **kwargs)
+        img = datablocks[0]
 
         # generate empty object with image as display.
-        asset_group = bpy.data.objects.new(group_name, object_data=None)
-        asset_group.empty_display_type = "IMAGE"
-        asset_group.empty_display_size = 10
-        asset_group.empty_image_depth = 'DEFAULT'
-        asset_group.empty_image_side = 'DOUBLE_SIDED'
-        asset_group.show_empty_image_orthographic = True
-        asset_group.show_empty_image_perspective = True
-        asset_group.data = img
+        empty_image = bpy.data.objects.new(container.name, object_data=None)
+        empty_image.empty_display_type = "IMAGE"
+        empty_image.empty_display_size = 10
+        empty_image.empty_image_depth = "DEFAULT"
+        empty_image.empty_image_side = "DOUBLE_SIDED"
+        empty_image.show_empty_image_orthographic = True
+        empty_image.show_empty_image_perspective = True
+        empty_image.data = img
 
         # Setup image as movie clip if source is "MOVIE"
         # and match frame start and duration with the current scene settings.
         if img.source == "MOVIE":
-            asset_group.image_user.frame_start = bpy.context.scene.frame_start
-            asset_group.image_user.frame_duration = (
+            empty_image.image_user.frame_start = bpy.context.scene.frame_start
+            empty_image.image_user.frame_duration = (
                 bpy.context.scene.frame_end - bpy.context.scene.frame_start
             )
 
-        plugin.get_main_collection().objects.link(asset_group)
+        bpy.context.scene.collection.objects.link(empty_image)
 
-        self._update_metadata(asset_group, context, group_name, libpath)
-
-        return asset_group
+        return container, datablocks
 
 
 class BackgroundLoader(ImageLoader):
@@ -94,8 +108,10 @@ class BackgroundLoader(ImageLoader):
     label = "Load Background for camera"
     order = 2
 
-    def process_asset(self, *args, **kwargs) -> bpy.types.Image:
-        img = super().process_asset(*args, **kwargs)
+    def load(self, *args, **kwargs):
+        """OVERRIDE"""
+        container, datablocks = super().load(*args, **kwargs)
+        img = datablocks[0]
 
         camera = None
 
@@ -112,7 +128,8 @@ class BackgroundLoader(ImageLoader):
         if not camera:
             camera = next(
                 (
-                    obj for obj in bpy.context.scene.objects
+                    obj
+                    for obj in bpy.context.scene.objects
                     if obj.type == "CAMERA"
                 ),
                 None,
@@ -134,4 +151,4 @@ class BackgroundLoader(ImageLoader):
                 bpy.context.scene.frame_end - bpy.context.scene.frame_start
             )
 
-        return img
+        return container, datablocks
