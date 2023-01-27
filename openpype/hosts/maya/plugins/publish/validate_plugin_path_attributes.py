5e02a1859d87a76fdfdb9a52d4f373fa86d5ac7c
Roy Nieterau <roy_nieterau@hotmail.com>
2023-01-27 01:28:15 +0100

    Fix setting key

--- openpype/hosts/maya/plugins/publish/validate_plugin_path_attributes.py
+++ openpype/hosts/maya/plugins/publish/validate_plugin_path_attributes.py
@@ -24,7 +24,7 @@ class ValidatePluginPathAttributes(pyblish.api.InstancePlugin):
         validate_path = (
             instance.context.data["project_settings"]["maya"]["publish"]
         )
-        file_attr = validate_path["ValidatePathForPlugin"]["attribute"]
+        file_attr = validate_path["ValidatePluginPathAttributes"]["attribute"]
         if not file_attr:
             return invalid
 
