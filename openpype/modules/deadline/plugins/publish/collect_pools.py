8fd44f646eb13048bfb5db28a8c8155d28fa0df9
Roy Nieterau <roy_nieterau@hotmail.com>
2023-01-29 22:57:44 +0100

    Allow to customize deadline pool in new publisher

--- openpype/modules/deadline/plugins/publish/collect_pools.py
+++ openpype/modules/deadline/plugins/publish/collect_pools.py
@@ -4,8 +4,12 @@
 """
 import pyblish.api
 
+from openpype.lib import TextDef
+from openpype.pipeline.publish import OpenPypePyblishPluginMixin
 
-class CollectDeadlinePools(pyblish.api.InstancePlugin):
+
+class CollectDeadlinePools(pyblish.api.InstancePlugin,
+                           OpenPypePyblishPluginMixin):
     """Collect pools from instance if present, from Setting otherwise."""
 
     order = pyblish.api.CollectorOrder + 0.420
@@ -15,9 +19,35 @@ class CollectDeadlinePools(pyblish.api.InstancePlugin):
     primary_pool = None
     secondary_pool = None
 
+    @classmethod
+    def apply_settings(cls, project_settings, system_settings):
+        # deadline.publish.CollectDeadlinePools
+        settings = project_settings["deadline"]["publish"]["CollectDeadlinePools"]  # noqa
+        cls.primary_pool = settings.get("primary_pool", None)
+        cls.secondary_pool = settings.get("secondary_pool", None)
+
     def process(self, instance):
         if not instance.data.get("primaryPool"):
             instance.data["primaryPool"] = self.primary_pool or "none"
 
         if not instance.data.get("secondaryPool"):
             instance.data["secondaryPool"] = self.secondary_pool or "none"
+
+    def get_attribute_defs(self):
+
+        # TODO: Preferably this would be an enum for the user
+        #       but the Deadline server URL can be dynamic and
+        #       can be set per render instance. Since get_attribute_defs
+        #       can't be dynamic unfortunately EnumDef isn't possible (yet?)
+        # pool_names = self.deadline_module.get_deadline_pools(deadline_url,
+        #                                                      self.log)
+        # secondary_pool_names = ["-"] + pool_names
+
+        return [
+            TextDef("primaryPool",
+                    label="Primary Pool",
+                    default=self.primary_pool),
+            TextDef("secondaryPool",
+                    label="Secondary Pool",
+                    default=self.secondary_pool)
+        ]
