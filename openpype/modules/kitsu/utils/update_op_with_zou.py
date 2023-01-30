bec59c2d98c354206c80f37c8a46f77d9821ba04
jeremy.oblet <jeremy.oblet@fixstudio.com>
2023-01-30 11:59:28 +0100

    update openpype projects status from kitsu settings

--- openpype/modules/kitsu/utils/update_op_with_zou.py
+++ openpype/modules/kitsu/utils/update_op_with_zou.py
@@ -267,12 +267,22 @@ def write_project_to_op(project: dict, dbcon: AvalonMongoDB) -> UpdateOne:
         # Update Zou
         gazu.project.update_project(project)
 
+    all_kitsu_projects_names = [project['name'] for project in gazu.project.all_projects()]
+    all_kitsu_open_projects_names = [project['name'] for project in gazu.project.all_open_projects()]
+    all_kitsu_closed_projects_names = [project for project in all_kitsu_projects_names if project not in all_kitsu_open_projects_names]
+
+    if project_name in all_kitsu_closed_projects_names:
+        project_status = False
+    else:
+        project_status = True
+
     # Update data
     project_data.update(
         {
             "code": project_code,
             "fps": float(project["fps"]),
             "zou_id": project["id"],
+            "active": project_status,
         }
     )
 
