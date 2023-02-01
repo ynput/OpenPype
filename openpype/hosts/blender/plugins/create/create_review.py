0d63aa3943138f4620033b3b0e95a0b13fe20e89
Félix David <felixg.david@gmail.com>
2023-02-01 13:41:29 +0100

    override review creation like camera but without renaming

--- openpype/hosts/blender/plugins/create/create_review.py
+++ openpype/hosts/blender/plugins/create/create_review.py
@@ -1,4 +1,9 @@
 """Create review."""
+from typing import List
+
+import bpy
+from openpype import plugin
+from openpype.hosts.blender.api.properties import OpenpypeInstance
 
 from openpype.hosts.blender.plugins.create.create_camera import CreateCamera
 
@@ -10,3 +15,26 @@ class CreateReview(CreateCamera):
     label = "Review"
     family = "review"
     icon = "video-camera"
+
+    def process(
+        self, datablocks: List[bpy.types.ID] = None, **kwargs
+    ) -> OpenpypeInstance:
+        """OVERRIDE from CreateCamera to not rename target camera.
+        
+        Still create one if None.
+        """
+        # Create instance object
+        asset = self.data["asset"]
+        subset = self.data["subset"]
+        instance_name = plugin.build_op_basename(asset, subset)
+
+        # Create camera if doesn't exist
+        datablocks = datablocks or []
+        camera = self._get_camera_from_datablocks(datablocks)
+        if not camera:
+            camera = bpy.data.cameras.new(instance_name)
+            camera_obj = bpy.data.objects.new(instance_name, camera)
+            datablocks.append(camera_obj)
+
+        # Create Instance
+        return super().process(datablocks, **kwargs)
