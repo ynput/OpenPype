660fa18119c1d02c0dc53a8f3f492628ba03164b
Félix David <felixg.david@gmail.com>
2023-02-02 15:26:27 +0100

    Fix: camera create by matching camera from datablocks and all children (#53)
    * Fix: camera create by matching camera from datablocks and all children
    
    * refactor with _get_camera_from_datablocks as shared function

--- openpype/hosts/blender/plugins/create/create_camera.py
+++ openpype/hosts/blender/plugins/create/create_camera.py
@@ -1,11 +1,36 @@
 """Create a camera asset."""
 
-from typing import List
+from itertools import chain
+from typing import List, Union
 import bpy
 
 from openpype.hosts.blender.api import plugin
 from openpype.hosts.blender.api.properties import OpenpypeInstance
+from openpype.hosts.blender.api.utils import get_all_outliner_children
 
+def _get_camera_from_datablocks(datablocks: List[bpy.types.ID])->Union[bpy.types.Object, None]:
+    """Get first camera found in given datablocks list.
+
+    Args:
+        datablocks (List[bpy.types.ID]):
+            List of datablocks to get camera from
+
+    Returns:
+        Union[bpy.types.Object, None]: Found camera if any.
+    """
+    outliner_children = set(
+        chain.from_iterable(
+            get_all_outliner_children(d) for d in datablocks
+        )
+    )
+    return next(
+        (
+            c
+            for c in outliner_children | set(datablocks)
+            if isinstance(c, bpy.types.Object) and c.type == "CAMERA"
+        ),
+        None,
+    )
 
 class CreateCamera(plugin.Creator):
     """Single baked camera"""
@@ -25,11 +50,10 @@ class CreateCamera(plugin.Creator):
 
         # Rename existing camera or create one
         datablocks = datablocks or []
-        for obj in datablocks:
-            if obj and obj.type == "CAMERA":
-                obj.name = instance_name
-                obj.data.name = instance_name
-                break
+        camera = _get_camera_from_datablocks(datablocks)
+        if camera:
+            camera.name = instance_name
+            camera.data.name = instance_name
         else:
             camera = bpy.data.cameras.new(instance_name)
             camera_obj = bpy.data.objects.new(instance_name, camera)
