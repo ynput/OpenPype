5f27606208acc2bec234b1eb2e9154e8031471fc
Jacob Danell <jacob@emberlight.se>
2023-02-03 00:21:37 +0100

    Fixed hound-bots comments

--- openpype/hosts/fusion/plugins/publish/render_local.py
+++ openpype/hosts/fusion/plugins/publish/render_local.py
@@ -1,6 +1,4 @@
 import os
-import copy
-
 import pyblish.api
 from openpype.hosts.fusion.api import comp_lock_and_undo_chunk
 
@@ -19,7 +17,7 @@ class Fusionlocal(pyblish.api.InstancePlugin):
     families = ["render.local"]
 
     def process(self, instance):
-        
+
         # This plug-in runs only once and thus assumes all instances
         # currently will render the same frame range
         context = instance.context
@@ -33,12 +31,12 @@ class Fusionlocal(pyblish.api.InstancePlugin):
         basename = os.path.basename(path)
         head, ext = os.path.splitext(basename)
         files = [
-            f"{head}{str(frame).zfill(4)}{ext}" for frame in range(frame_start, frame_end+1)
+            f"{head}{str(frame).zfill(4)}{ext}" for frame in range(frame_start, frame_end + 1)
         ]
         repre = {
             'name': ext[1:],
             'ext': ext[1:],
-            'frameStart': "%0{}d".format(len(str(frame_end))) % frame_start,
+            'frameStart': f"%0{len(str(frame_end))}d" % frame_start,
             'files': files,
             "stagingDir": output_dir,
         }
@@ -56,19 +54,19 @@ class Fusionlocal(pyblish.api.InstancePlugin):
     def render_once(self, context):
         """Render context comp only once, even with more render instances"""
 
-        key = "__hasRun{}".format(self.__class__.__name__)
+        key = f'__hasRun{self.__class__.__name__}'
         if context.data.get(key, False):
             return
-        else:
-            context.data[key] = True
+
+        context.data[key] = True
 
         current_comp = context.data["currentComp"]
         frame_start = context.data["frameStartHandle"]
         frame_end = context.data["frameEndHandle"]
 
         self.log.info("Starting render")
-        self.log.info("Start frame: {}".format(frame_start))
-        self.log.info("End frame: {}".format(frame_end))
+        self.log.info(f"Start frame: {frame_start}")
+        self.log.info(f"End frame: {frame_end}")
 
         with comp_lock_and_undo_chunk(current_comp):
             result = current_comp.Render({
