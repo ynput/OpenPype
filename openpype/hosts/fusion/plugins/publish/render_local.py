ed668e52eee942a182b9ad818c1bfe0168039c04
Jacob Danell <jacob@emberlight.se>
2023-02-03 12:14:45 +0100

    Made line 40 shorter

--- openpype/hosts/fusion/plugins/publish/render_local.py
+++ openpype/hosts/fusion/plugins/publish/render_local.py
@@ -37,7 +37,8 @@ class Fusionlocal(pyblish.api.InstancePlugin):
         basename = os.path.basename(path)
         head, ext = os.path.splitext(basename)
         files = [
-            f"{head}{str(frame).zfill(4)}{ext}" for frame in range(frame_start, frame_end + 1)
+            f"{head}{str(frame).zfill(4)}{ext}"
+            for frame in range(frame_start, frame_end + 1)
         ]
         repre = {
             'name': ext[1:],
