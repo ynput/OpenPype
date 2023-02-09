0a7d7e45638987a53ac35c2dd9c69d38cfe9855e
Kayla Man <moonyuet2008@hotmail.com>
2023-02-09 19:04:28 +0800

    correct separator issue in naming convention

--- openpype/hosts/max/api/lib_rendersettings.py
+++ openpype/hosts/max/api/lib_rendersettings.py
@@ -121,8 +121,7 @@ class RenderSettings(object):
         for i in range(render_elem_num):
             renderlayer_name = render_elem.GetRenderElement(i)
             target, renderpass = str(renderlayer_name).split(":")
-            render_element = os.path.join(dir, renderpass)
-            aov_name = "{0}..{1}".format(render_element, ext)
+            aov_name = "{0}_{1}..{2}".format(dir, renderpass, ext)
             try:
                 aov_separator = self._aov_chars[(
                     self._project_settings["maya"]
