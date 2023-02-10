88bda4e1f6bfbe628d80edc76789393dcc9a68a4
Jakub Trllo <jakub.trllo@gmail.com>
2023-02-10 14:24:28 +0100

    TVPaint host inherit from IPublishHost

--- openpype/hosts/tvpaint/api/pipeline.py
+++ openpype/hosts/tvpaint/api/pipeline.py
@@ -8,7 +8,7 @@ import requests
 import pyblish.api
 
 from openpype.client import get_project, get_asset_by_name
-from openpype.host import HostBase, IWorkfileHost, ILoadHost
+from openpype.host import HostBase, IWorkfileHost, ILoadHost, IPublishHost
 from openpype.hosts.tvpaint import TVPAINT_ROOT_DIR
 from openpype.settings import get_current_project_settings
 from openpype.lib import register_event_callback
@@ -58,7 +58,7 @@ instances=2
 """
 
 
-class TVPaintHost(HostBase, IWorkfileHost, ILoadHost):
+class TVPaintHost(HostBase, IWorkfileHost, ILoadHost, IPublishHost):
     name = "tvpaint"
 
     def install(self):
