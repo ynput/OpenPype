6236922e81b40a7ca0a7ed4d325aa97a0de9361e
Kayla Man <moonyuet2008@hotmail.com>
2023-02-13 17:45:10 +0800

    not include py script from the unrelated host

--- openpype/hosts/maya/plugins/publish/collect_render.py
+++ openpype/hosts/maya/plugins/publish/collect_render.py
@@ -185,6 +185,7 @@ class CollectMayaRender(pyblish.api.ContextPlugin):
                 multipart))
             assert exp_files, "no file names were generated, this is bug"
             self.log.info(exp_files)
+
             # if we want to attach render to subset, check if we have AOV's
             # in expectedFiles. If so, raise error as we cannot attach AOV
             # (considered to be subset on its own) to another subset
