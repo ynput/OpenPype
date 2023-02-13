3eeed20e876c2ead3bea16666de2e7bb072d1d6b
maxpareschi <max.pareschi@gmail.com>
2023-02-13 15:13:30 +0100

    workfile tools can set 0

--- openpype/tools/workfiles/save_as_dialog.py
+++ openpype/tools/workfiles/save_as_dialog.py
@@ -218,7 +218,7 @@ class SaveAsDialog(QtWidgets.QDialog):
 
         # Version number input
         version_input = QtWidgets.QSpinBox(version_widget)
-        version_input.setMinimum(1)
+        version_input.setMinimum(0)
         version_input.setMaximum(9999)
 
         # Last version checkbox
