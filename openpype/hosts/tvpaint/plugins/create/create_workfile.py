173b404d0e2af1712f9e7cf4d9f60ac5ea030570
Jakub Trllo <jakub.trllo@gmail.com>
2023-02-17 13:06:52 +0100

    swap also usage of settings in review and workfile creators

--- openpype/hosts/tvpaint/plugins/create/create_workfile.py
+++ openpype/hosts/tvpaint/plugins/create/create_workfile.py
@@ -43,8 +43,6 @@ class TVPaintWorkfileCreator(TVPaintAutoCreator):
                 "task": task_name,
                 "variant": self.default_variant
             }
-            if not self.active_on_create:
-                data["active"] = False
 
             new_instance = CreatedInstance(
                 self.family, subset_name, data, self
