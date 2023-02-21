3c84b4195d6077ffc4abf51933359f2c69cec7a8
Jakub Jezek <jakubjezek001@gmail.com>
2023-02-21 13:39:02 +0100

    adding nukeassist hook

---
+++ openpype/hosts/nuke/hooks/pre_nukeassist_setup.py
@@ -0,0 +1,10 @@
+from openpype.lib import PreLaunchHook
+
+class PrelaunchNukeAssistHook(PreLaunchHook):
+    """
+    Adding flag when nukeassist
+    """
+    app_groups = ["nukeassist"]
+
+    def execute(self):
+        self.launch_context.env["NUKEASSIST"] = True
