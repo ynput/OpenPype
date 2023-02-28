6f074d44342bd1cd1ff5d71ce320d2f434f2b217
Jakub Trllo <jakub.trllo@gmail.com>
2023-02-28 18:03:30 +0100

    added docstring

--- openpype/lib/local_settings.py
+++ openpype/lib/local_settings.py
@@ -519,10 +519,20 @@ def _create_local_site_id(registry=None):
 
 
 def get_ayon_appdirs(*args):
+    """Local app data directory of AYON client.
+
+    Args:
+        *args (Iterable[str]): Subdirectories/files in local app data dir.
+
+    Returns:
+        str: Path to directory/file in local app data dir.
+    """
+
     return os.path.join(
         appdirs.user_data_dir("ayon", "ynput"),
         *args
     )
+
 
 def _get_ayon_local_site_id():
     # used for background syncing
