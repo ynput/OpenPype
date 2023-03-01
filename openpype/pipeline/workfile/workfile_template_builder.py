e1fa9f7c3133c359475dd15145b405406aada8f5
Jakub Jezek <jakubjezek001@gmail.com>
2023-03-01 17:30:31 +0100

    adding noqa for hound

--- openpype/pipeline/workfile/workfile_template_builder.py
+++ openpype/pipeline/workfile/workfile_template_builder.py
@@ -1662,7 +1662,7 @@ class PlaceholderCreateMixin(object):
                     task_name=task_name
                 )
 
-        except:
+        except: # noqa: E722
             failed = True
             self.create_failed(placeholder, creator_data)
 
