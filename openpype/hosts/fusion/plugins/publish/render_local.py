59cea8c7d070bfe1b757d5f12be96c8f4d1e9a47
Jacob Danell <jacob@emberlight.se>
2023-03-02 12:15:37 +0100

    Catch for each instance whether the render succeeded or not

--- openpype/hosts/fusion/plugins/publish/render_local.py
+++ openpype/hosts/fusion/plugins/publish/render_local.py
@@ -59,24 +59,27 @@ class Fusionlocal(pyblish.api.InstancePlugin):
         # to speed up the rendering. The check below makes sure that we only
         # execute the rendering once and not for each instance.
         key = f"__hasRun{self.__class__.__name__}"
-        if context.data.get(key, False):
-            return
-        context.data[key] = True
+        if key not in context.data:
+            # We initialize as false to indicate it wasn't successful yet
+            # so we can keep track of whether Fusion succeeded
+            context.data[key] = False
 
-        current_comp = context.data["currentComp"]
-        frame_start = context.data["frameStartHandle"]
-        frame_end = context.data["frameEndHandle"]
+            current_comp = context.data["currentComp"]
+            frame_start = context.data["frameStartHandle"]
+            frame_end = context.data["frameEndHandle"]
+    
+            self.log.info("Starting Fusion render")
+            self.log.info(f"Start frame: {frame_start}")
+            self.log.info(f"End frame: {frame_end}")
+    
+            with comp_lock_and_undo_chunk(current_comp):
+                result = current_comp.Render({
+                    "Start": frame_start,
+                    "End": frame_end,
+                    "Wait": True
+                })
+                
+            context.data[key] = bool(result)
 
-        self.log.info("Starting render")
-        self.log.info(f"Start frame: {frame_start}")
-        self.log.info(f"End frame: {frame_end}")
-
-        with comp_lock_and_undo_chunk(current_comp):
-            result = current_comp.Render({
-                "Start": frame_start,
-                "End": frame_end,
-                "Wait": True
-            })
-
-        if not result:
+        if context.data[key] is False:
             raise RuntimeError("Comp render failed")
