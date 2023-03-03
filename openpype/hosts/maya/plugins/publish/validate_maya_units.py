bd482673310e18ab9dcd6ab9fd20e39f8bc4e597
Roy Nieterau <roy_nieterau@hotmail.com>
2023-03-03 15:56:55 +0100

    Cosmetics

--- openpype/hosts/maya/plugins/publish/validate_maya_units.py
+++ openpype/hosts/maya/plugins/publish/validate_maya_units.py
@@ -49,10 +49,10 @@ class ValidateMayaUnits(pyblish.api.ContextPlugin):
         )
 
         cls.validate_linear_units = settings.get("validate_linear_units",
-                                                  cls.validate_linear_units)
+                                                 cls.validate_linear_units)
         cls.linear_units = settings.get("linear_units", cls.linear_units)
         cls.validate_angular_units = settings.get("validate_angular_units",
-                                                   cls.validate_angular_units)
+                                                  cls.validate_angular_units)
         cls.angular_units = settings.get("angular_units", cls.angular_units)
         cls.validate_fps = settings.get("validate_fps", cls.validate_fps)
 
