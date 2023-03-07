77bfc6357a1df334647ce9d43db045d9bafe583e
jbeaulieu <jbeaulieu@alkemy-x.com>
2023-03-06 19:25:29 -0500

    Change single quotes to double quotes. Utilize run_subprocess. Switch print for log. Fix outlying frame math issue. Update spelling issue. Remove useless code

--- openpype/hosts/hiero/plugins/publish/transcode_frames.py
+++ openpype/hosts/hiero/plugins/publish/transcode_frames.py
@@ -8,22 +8,45 @@ from openpype.lib import (
 from openpype.pipeline import publish
 from openpype.lib.applications import ApplicationManager
 
-from subprocess import Popen, PIPE, call
+
 def nuke_transcode_template(output_ext, input_frame, first_frame, last_frame, read_path, write_path, src_colorspace, dst_colorspace):
-    python_template = '/home/jbeaulieu/dev/hiero/nuke_transcode.py'
-    nuke_template = '/home/jbeaulieu/dev/hiero/ingest_transcode.nk'
+    python_template = "/pipe/hiero/templates/nuke_transcode.py"
+    nuke_template = "/pipe/hiero/templates/ingest_transcode.nk"
     app_manager = ApplicationManager()
-    nuke_app = app_manager.applications.get('nuke/14-02')
+    nuke_app = app_manager.applications.get("nuke/14-02")
     nuke_args = nuke_app.find_executable().as_args()
-    cmd = nuke_args + ['-t', python_template, nuke_template, '{0}_{1}_{2}'.format(first_frame, last_frame, input_frame), output_ext, read_path, write_path, src_colorspace, dst_colorspace]
-    proc = Popen(
-            cmd,
-            stdout=PIPE,
-            stderr=PIPE,
-            env=os.environ
-        )
-    out, err =  proc.communicate()
-    return not ('error: ' in str(err).lower() or 'Traceback' in str(err))
+    cmd = nuke_args + ["-t", python_template, nuke_template, "{0}_{1}_{2}".format(first_frame, last_frame, input_frame), output_ext, read_path, write_path, src_colorspace, dst_colorspace]
+
+    # If non exist status is returned output will raise exception.
+    # No need to handle since run_subprocess already formats and handles error
+    run_subprocess(cmd)
+
+
+def openpype_publish_tag(track_item, instance_tags):
+    """
+    Get tag that was used to publish track item
+    """
+    for instance_tag in instance_tags:
+        t_metadata = dict(instance_tag.metadata())
+        t_family = t_metadata.get("tag.family", "")
+        if t_family == "plate":
+            for item_tag in track_item.tags():
+                if instance_tag.name() == item_tag.name():
+                    return dict(item_tag.metadata())
+
+    return {}
+
+
+def get_tag_handles(track_item, instance_tags):
+    tag = openpype_publish_tag(track_item, instance_tags)
+    try:
+        handle_start = int(tag.get("tag.handleStart", "0"))
+        handle_end = int(tag.get("tag.handleEnd", "0"))
+    except ValueError:
+        raise Exception("Handle field should only contain numbers")
+
+    return handle_start, handle_end
+
 
 class TranscodeFrames(publish.Extractor):
     """Transcode frames"""
@@ -32,76 +55,81 @@ class TranscodeFrames(publish.Extractor):
     label = "Transcode Frames"
     hosts = ["hiero"]
     families = ["plate"]
-    movie_extensions = ["mov", "mp4", 'mxf']
 
     def process(self, instance):
         """
-        Plate - transcodes to - exr
-        Reference - transcodes to - jpg
-
+        Plate - Transcodes to exr with color becoming linear
+        Reference - For now does not get transcoded and stays same as source
         """
         oiio_tool_path = get_oiio_tools_path()
 
         staging_dir = self.staging_dir(instance)
         output_template = os.path.join(staging_dir, instance.data["name"])
         output_dir = os.path.dirname(output_template)
-        handle_start = instance.data['handleStart']
-        handle_end = instance.data['handleEnd']
-        first_frame = instance.data['frameStart']
-        end_frame = instance.data['frameEnd']
-        track_item = instance.data['item']
+        instance_tags = instance.data["tags"]
+        track_item = instance.data["item"]
+        media_source = track_item.source().mediaSource()
 
+        # Define frame output range
+        # handleStart and handleEnd are overriden to reflect media range and not absolute handles
+        # Solution is to take the handle values directly from the tag instead of instance data
+        handle_start, handle_end = get_tag_handles(track_item, instance_tags)
+        first_frame = instance.data["frameStart"]
+        end_frame = instance.data["frameEnd"] + handle_end
 
+        # Need clip source in and original clip source media in and out to calculate matching input frame
         clip_source_in = track_item.sourceIn()
         source_start = track_item.source().sourceIn()
         source_end = track_item.source().sourceOut()
-        media_source = track_item.source().mediaSource()
+
+        # Define source path along with extension
         input_path = media_source.fileinfos()[0].filename()
         source_ext = os.path.splitext(input_path)[1][1:]
+
+        # Determine color transformation
         src_colorspace = track_item.sourceMediaColourTransform()
-        dst_colorspace = 'scene_linear'
+        dst_colorspace = "scene_linear"
 
         files = []
-        output_ext = 'exr'
-        frames = range(first_frame, end_frame+handle_start+handle_end+1)
+        movie_extensions = ["mov", "mp4", "mxf"]
+        output_ext = "exr"
+        frames = range(first_frame, end_frame + handle_start + 1)
+        self.log.info('Trancoding frame range {0} - {1}'.format(frames[0], frames[-1]))
         for frame in frames:
+            # Calculate input_frame for output by normalizing input media to first frame
             input_frame = source_start + clip_source_in - handle_start + frame - first_frame
-            if not input_frame >= 0 or input_frame >= source_end:
-                print('Frame out of range of source - Skipping frame "{0}" - Source frame "{1}"'.format(frame, input_frame))
+            if not input_frame >= 1 or input_frame >= source_end + 1:
+                self.log.warning("Frame out of range of source - Skipping frame '{0}' - Source frame '{1}'".format(frame, input_frame))
                 continue
 
             output_path = output_template
             output_path += ".{:04d}.{}".format(int(frame), output_ext)
-            # Only if input and output aren't video can OIIO be ran
-            if output_ext.lower() in ['mxf', 'mov', 'mp4'] or source_ext.lower() in ['mxf', 'mov', 'mp4']:
-                output = nuke_transcode_template(output_ext, int(input_frame), int(frame), int(frame), input_path, output_path, src_colorspace, dst_colorspace)
-                if not output:
-                    raise ValueError("Nuke transcode processing failed.")
+            # If either source or output is a video format, transcode using Nuke
+            if output_ext.lower() in movie_extensions or source_ext.lower() in movie_extensions:
+                # No need to raise error as Nuke raises an error exit value if something went wrong
+                nuke_transcode_template(output_ext, int(input_frame), int(frame), int(frame), input_path, output_path, src_colorspace, dst_colorspace)
 
-            # Use OIIO instead of Nuke for faster transcoding
+            # Else use OIIO instead of Nuke for faster transcoding
             else:
                 args = [oiio_tool_path]
 
                 # Input frame start
-                if source_ext in self.movie_extensions:
-                    args.extend(["--subimage", str(int(input_frame))])
-                else:
-                    args.extend(["--frames", str(int(input_frame))])
+                args.extend(["--frames", str(int(input_frame))])
 
                 # Input path
                 args.append(input_path)
 
                 # Add colorspace conversion
-                args.extend(['--colorconvert', src_colorspace, dst_colorspace])
+                args.extend(["--colorconvert", src_colorspace, dst_colorspace])
 
                 # Copy old metadata
-                args.append('--pastemeta')
+                args.append("--pastemeta")
 
                 # Add metadata
                 # Ingest colorspace
-                args.extend(['--sattrib', 'akemy/ingest/colorspace', src_colorspace])
-                # Input Fileline
-                args.extend(['--sattrib', 'input/filename', input_path])
+                args.extend(["--sattrib", "alkemy/ingest/colorspace", src_colorspace])
+                # Input Filename
+                args.extend(["--sattrib", "input/filename", input_path])
 
                 # Output path
                 args.extend(["-o", output_path])
@@ -126,15 +154,15 @@ class TranscodeFrames(publish.Extractor):
             )
 
         try:
-            representation_exts = [rep['ext'] for rep in instance.data["representations"]]
+            representation_exts = [rep["ext"] for rep in instance.data["representations"]]
             representation_ext_index = representation_exts.index(source_ext)
             if source_ext in representation_exts:
-                print('Removing source representation and replacing with transcoded frames')
+                self.log.info("Removing source representation and replacing with transcoded frames")
                 instance.data["representations"].pop(representation_ext_index)
             else:
-                print('No source ext to remove from representation')
-        except:
-            print('Failed to remove source ext "{0}" from representations'.format(source_ext))
+                self.log.info("No source ext to remove from representation")
+        except IndexError:
+            self.log.warning("Failed to remove source ext '{0}' from representations".format(source_ext))
 
         if len(files) == 1:
             instance.data["representations"].append(
