71332fe460284227df7623d539c3fc5b24691c3a
Jakub Trllo <jakub.trllo@gmail.com>
2023-03-07 10:50:48 +0100

    use 'setdefault' to set 'inputVersions'

--- openpype/plugins/publish/collect_input_representations_to_versions.py
+++ openpype/plugins/publish/collect_input_representations_to_versions.py
@@ -41,11 +41,9 @@ class CollectInputRepresentationsToVersions(pyblish.api.ContextPlugin):
             if not inst_repre:
                 continue
 
-            if "inputVersions" not in instance.data:
-                instance.data["inputVersions"] = []
+            instance.data.setdefault("inputVersions", [])
             input_versions = instance.data["inputVersions"]
             for repre_id in inst_repre:
                 version_id = representation_id_to_version_id.get(repre_id)
                 if version_id:
                     input_versions.append(version_id)
-            instance.data["inputVersions"] = input_versions
