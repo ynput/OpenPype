a36ee718cab2a8873c929d1c2e353267609dddde
Ondrej Samohel <ondrej.samohel@orbi.tools>
2023-03-10 10:43:20 +0100

    :rotating_light: PEP style changes

--- openpype/hosts/unreal/api/p4_integrate.py
+++ openpype/hosts/unreal/api/p4_integrate.py
@@ -6,8 +6,8 @@ from openpype.settings import get_project_settings
 
 
 class P4Integrate:
-    p4Connection = None
-    p4OpenPypeChangelist = None
+    P4_connection = None
+    P4_openpype_changelist = None
 
     def __init__(self, project_name: str):
         settings = get_project_settings(project_name)
@@ -17,66 +17,65 @@ class P4Integrate:
         self.user = self.settings["user"]
         self.project_name = project_name
 
-
-    def P4VConnect(self):
+    def p4_connect(self):
         try:
-            if self.p4Connection is None:
+            if self.P4_connection is None:
                 p4 = P4()
                 p4.client = self.client
                 p4.port = self.port
                 p4.user = self.user
                 p4.connect()
 
-                self.p4Connection = p4
-            return self.p4Connection
+                self.P4_connection = p4
+            return self.P4_connection
         except P4Exception as e:
-            raise P4Exception(e)
+            raise P4Exception(e) from e
 
-    def P4VCreateOrLoadOpenPypeChangelist(self, changeListDesc: str,
-                                          changeListIdentity: str | None,
-                                          changeListFiles: list[str] | None = None):
+    def p4_create_or_load_openpype_changelist(
+            self, changelist_desc: str, changelist_identity: str | None,
+            changelist_files: list[str] | None = None):
         try:
-            p4 = self.P4VConnect()
+            p4 = self.p4_connect()
 
-            if changeListFiles is None:
-                changeListFiles = []
+            if changelist_files is None:
+                changelist_files = []
 
-            if changeListIdentity is not None:
-                changelist = self.P4VGetChangelistByIdentity(changeListIdentity)
-                self.p4OpenPypeChangelist = changelist
+            if changelist_identity is not None:
+                changelist = self.p4_get_changelist_by_identity(changelist_identity)
+                self.P4_openpype_changelist = changelist
 
-            if self.p4OpenPypeChangelist is None:
-                existingChangelist = self.P4VGetChangelistForSourceAppAndTargetEnv(P4CustomIdentity.sourceApp,
-                                                                                   P4CustomIdentity.targetEnv)
+            if self.P4_openpype_changelist is None:
+                existing_changelist = self.p4_get_changelist_for_source_app_and_target_env(
+                    P4CustomIdentity.source_app, P4CustomIdentity.target_env)
 
-                if existingChangelist is not None:
-                    self.p4OpenPypeChangelist = existingChangelist
+                if existing_changelist is not None:
+                    self.P4_openpype_changelist = existing_changelist
                 else:
-                    currentTimestamp = round(time.time() * 1000)
-                    customIdentity = P4CustomIdentity(self.project_name, str(currentTimestamp))
+                    current_timestamp = round(time.time() * 1000)
+                    custom_identity = P4CustomIdentity(self.project_name, str(current_timestamp))
 
                     change = p4.fetch_change()
-                    change._identity = customIdentity.getIdentityAsJson()
-                    change._description = changeListDesc
-                    change._files = changeListFiles
-                    changeList = p4.save_change(change)[0].split(" ")
+                    change._identity = custom_identity.get_identity_as_json()
+                    change._description = changelist_desc
+                    change._files = changelist_files
+                    changelist = p4.save_change(change)[0].split(" ")
 
-                    self.p4OpenPypeChangelist = changeList[1]
-            return self.p4OpenPypeChangelist
+                    self.P4_openpype_changelist = changelist[1]
+            return self.P4_openpype_changelist
         except Exception as e:
-            raise Exception(e)
+            raise Exception(e) from e
 
-    def P4VDisconnect(self):
+    def p4_disconnect(self):
         try:
-            p4 = self.p4Connection
+            p4 = self.P4_connection
             if p4 is not None:
                 p4.disconnect()
         except P4Exception as e:
-            raise P4Exception(e)
+            raise P4Exception(e) from e
 
-    def P4VGetChangelistByIdentity(self, identity: str):
+    def p4_get_changelist_by_identity(self, identity: str):
         try:
-            p4 = self.P4VConnect()
+            p4 = self.p4_connect()
             for changelist in p4.run("changes", "-c", p4.client):
                 if "changeIdentity" in changelist:
                     if changelist["changeIdentity"] == identity:
@@ -85,22 +84,22 @@ class P4Integrate:
         except P4Exception as P4E:
             print(P4E)
 
-    def P4VGetChangelistForSourceAppAndTargetEnv(self, sourceAppName: str, targetEnv: str):
-        p4 = self.P4VConnect()
+    def p4_get_changelist_for_source_app_and_target_env(self, source_app_name: str, target_env: str):
+        p4 = self.p4_connect()
 
         for changelist in p4.run("changes", "-c", p4.client, "-l"):
             if changelist['status'] != P4ChangelistStatusEnum.SUBMITTED.value:
-                if self.IsJson(changelist["desc"]):
-                    parsedJson = json.loads(changelist['desc'])
-                    if "sourceApp" in parsedJson and "targetEnv" in parsedJson:
-                        if parsedJson["sourceApp"] == sourceAppName and parsedJson["targetEnv"] == targetEnv:
+                if self.is_json(changelist["desc"]):
+                    parsed_json = json.loads(changelist['desc'])
+                    if "sourceApp" in parsed_json and "targetEnv" in parsed_json:
+                        if parsed_json["sourceApp"] == source_app_name and parsed_json["targetEnv"] == target_env:
                             return changelist["change"]
         return None
 
     @staticmethod
-    def IsJson(stringToCheck: str):
+    def is_json(string_to_check: str):
         try:
-            json.loads(stringToCheck)
+            json.loads(string_to_check)
             return True
         except ValueError:
             return False
@@ -114,22 +113,22 @@ class P4ChangelistStatusEnum(Enum):
 
 
 class P4CustomIdentity:
-    sourceApp: str = "OpenPype"
-    targetEnv: str = "Unreal"
+    source_app: str = "OpenPype"
+    target_env: str = "Unreal"
 
-    def __init__(self, projectName, timestamp):
-        self.projectName = projectName
+    def __init__(self, project_name, timestamp):
+        self.project_name = project_name
         self.timestamp = timestamp
 
-    def getIdHash(self):
-        uniqueString = self.projectName + self.timestamp
-        return hash(uniqueString)
+    def get_id_hash(self):
+        unique_str = self.project_name + self.timestamp
+        return hash(unique_str)
 
-    def getIdentityAsJson(self):
-        jsonString = {
-            "id": self.getIdHash(),
-            "sourceApp": self.sourceApp,
-            "targetEnv": self.targetEnv
+    def get_identity_as_json(self):
+        json_string = {
+            "id": self.get_id_hash(),
+            "sourceApp": self.source_app,
+            "targetEnv": self.target_env
         }
 
-        return json.dumps(jsonString)
+        return json.dumps(json_string)
