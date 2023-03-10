a0f8dc291ee7e71dd450f7337bbe1373bc2aa083
Sharkitty <hayleylalicorne@gmail.com>
2023-03-10 17:19:59 +0100

    Changed integrator order to reduce risk of workfile publish time not being updated in blend file

--- openpype/hosts/blender/plugins/publish/update_current_workfile_last_publish_time.py
+++ openpype/hosts/blender/plugins/publish/update_current_workfile_last_publish_time.py
@@ -8,7 +8,7 @@ class UpdateCurrentWorkfileLastPublishTime(pyblish.api.ContextPlugin):
     Used to ensure current workfile is up to date with published workfiles.
     """
 
-    order = pyblish.api.IntegratorOrder + 0.8
+    order = pyblish.api.IntegratorOrder + 0.00002
     hosts = ["blender"]
     families = ["workfile"]
     label = "Update Current Workfile Last Publish Time"
