1e9670f54dd9121527438c25649c53016fd4ea92
Jacob Danell <jacob@emberlight.se>
2023-03-14 17:19:43 +0100

    Fixed docstring and removed unused variables

--- openpype/hosts/fusion/plugins/publish/collect_renders.py
+++ openpype/hosts/fusion/plugins/publish/collect_renders.py
@@ -3,21 +3,11 @@ from pprint import pformat
 
 
 class CollectFusionRenders(pyblish.api.InstancePlugin):
-    """Collect current comp's render Mode
+    """Collect current saver node's render Mode
 
     Options:
-        local
-        farm
-
-    Note that this value is set for each comp separately. When you save the
-    comp this information will be stored in that file. If for some reason the
-    available tool does not visualize which render mode is set for the
-    current comp, please run the following line in the console (Py2)
-
-    comp.GetData("openpype.rendermode")
-
-    This will return the name of the current render mode as seen above under
-    Options.
+        local (Render locally)
+        frames (Use existing frames)
 
     """
 
@@ -29,10 +19,8 @@ class CollectFusionRenders(pyblish.api.InstancePlugin):
     def process(self, instance):
         self.log.debug(pformat(instance.data))
 
-        saver_node = instance.data["transientData"]["tool"]
         render_target = instance.data["render_target"]
         family = instance.data["family"]
-        families = instance.data["families"]
 
         # add targeted family to families
         instance.data["families"].append(
