a78b2fc72dd8188745b2fa039aa6a637ed189454
Roy Nieterau <roy_nieterau@hotmail.com>
2023-03-14 14:48:30 +0100

    Allow instances to have the same `cbId` attribute

--- openpype/hosts/maya/plugins/publish/validate_node_ids_unique.py
+++ openpype/hosts/maya/plugins/publish/validate_node_ids_unique.py
@@ -9,7 +9,15 @@ from openpype.hosts.maya.api import lib
 class ValidateNodeIdsUnique(pyblish.api.InstancePlugin):
     """Validate the nodes in the instance have a unique Colorbleed Id
 
-    Here we ensure that what has been added to the instance is unique
+    Here we ensure that what has been added to the instance is unique.
+
+    The validator can allow instanced nodes however a warning will be logged
+    due to instanced nodes being unable to have a unique `cbId` attribute.
+    The `cbId` attribute is what is used to uniquely define a specific node
+    in the scene and is used by the look development system to store and assign
+    the shaders per node. For instances this means that they cannot have unique
+    shaders assigned, thus a warning is logged if instances are detected.
+
     """
 
     order = ValidatePipelineOrder
@@ -22,6 +30,8 @@ class ValidateNodeIdsUnique(pyblish.api.InstancePlugin):
 
     actions = [openpype.hosts.maya.api.action.SelectInvalidAction,
                openpype.hosts.maya.api.action.GenerateUUIDsOnInvalidAction]
+
+    allow_instances = True
 
     def process(self, instance):
         """Process all meshes"""
@@ -54,7 +64,47 @@ class ValidateNodeIdsUnique(pyblish.api.InstancePlugin):
         invalid = list()
         _iteritems = getattr(ids, "iteritems", ids.items)
         for _ids, members in _iteritems():
-            if len(members) > 1:
+
+            if cls.allow_instances:
+                # Filter to all unique members that are not instances of itself
+                # to only invalidate when multiple nodes are found not of the
+                # same instance.
+                unique_members = []
+                processed_instance_paths = set()
+                instance_groups = defaultdict(set)
+                for member in members:
+                    all_paths = cmds.ls(member, allPaths=True, long=True)
+
+                    # Get all instance paths for this node
+                    if member in processed_instance_paths:
+                        instance_groups[tuple(all_paths)].add(member)
+                        continue
+
+                    if len(all_paths) > 1:
+                        processed_instance_paths.update(all_paths)
+                        instance_groups[tuple(all_paths)].add(member)
+                    unique_members.append(member)
+
+                for instance_grp, instance_members in instance_groups.items():
+                    if len(instance_members) < 2:
+                        # Ignore nodes that are instances but their instances
+                        # don't appear in the export
+                        continue
+
+                    # Log to the user about the usage of instances. They are
+                    # set to be allowed but can cause issues with lookdev since
+                    # they cannot have unique shader assignments due to how
+                    # the `cbId` attribute is shared between the instances
+                    cls.log.warning("Instanced members detected. This is ok, "
+                                    "but can introduce issues due to not "
+                                    "having a unique `cbId` for each node."
+                                    "Instanced nodes: "
+                                    "{}".format(instance_members))
+
+            else:
+                unique_members = members
+
+            if len(unique_members) > 1:
                 cls.log.error("ID found on multiple nodes: '%s'" % members)
                 invalid.extend(members)
 
