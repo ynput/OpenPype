c104805830c349260b30756b19560836bd9866a6
moonyuet <moonyuet2008@hotmail.com>
2023-03-14 16:17:17 +0100

    adding creator, loader for redshift proxy in 3dsmax

---
+++ openpype/hosts/max/plugins/load/load_redshift_proxy.py
@@ -0,0 +1,59 @@
+import os
+import clique
+
+from openpype.pipeline import (
+    load,
+    get_representation_path
+)
+from openpype.hosts.max.api.pipeline import containerise
+from openpype.hosts.max.api import lib
+
+
+class RedshiftProxyLoader(load.LoaderPlugin):
+    """Redshift Proxy Loader"""
+
+    families = ["redshiftproxy"]
+    representations = ["rs"]
+    order = -9
+    icon = "code-fork"
+    color = "white"
+
+    def load(self, context, name=None, namespace=None, data=None):
+        from pymxs import runtime as rt
+
+        filepath = os.path.normpath(self.fname)
+        rs_proxy = rt.RedshiftProxy()
+        rs_proxy.file = filepath
+        files_in_folder = os.listdir(os.path.dirname(filepath))
+        collections, remainder = clique.assemble(files_in_folder)
+        if collections:
+            rs_proxy.is_sequence = True
+
+        container = rt.container()
+        container.name = f"{name}"
+        rs_proxy.Parent = container
+
+        asset = rt.getNodeByName(f"{name}")
+
+        return containerise(
+            name, [asset], context, loader=self.__class__.__name__)
+
+    def update(self, container, representation):
+        from pymxs import runtime as rt
+
+        path = get_representation_path(representation)
+        node = rt.getNodeByName(container["instance_node"])
+
+        proxy_objects = self.get_container_children(node)
+        for proxy in proxy_objects:
+            proxy.source = path
+
+        lib.imprint(container["instance_node"], {
+            "representation": str(representation["_id"])
+        })
+
+    def remove(self, container):
+        from pymxs import runtime as rt
+
+        node = rt.getNodeByName(container["instance_node"])
+        rt.delete(node)
---
+++ openpype/hosts/max/plugins/publish/extract_redshift_proxy.py
@@ -0,0 +1,78 @@
+import os
+import pyblish.api
+from openpype.pipeline import (
+    publish,
+    OptionalPyblishPluginMixin
+)
+from pymxs import runtime as rt
+from openpype.hosts.max.api import (
+    maintained_selection,
+    get_all_children
+)
+
+
+class ExtractCameraAlembic(publish.Extractor,
+                           OptionalPyblishPluginMixin):
+    """
+    Extract Camera with AlembicExport
+    """
+
+    order = pyblish.api.ExtractorOrder - 0.1
+    label = "Extract RedShift Proxy"
+    hosts = ["max"]
+    families = ["redshiftproxy"]
+
+    def process(self, instance):
+        container = instance.data["instance_node"]
+        start = int(instance.context.data.get("frameStart"))
+        end = int(instance.context.data.get("frameEnd"))
+
+        self.log.info("Extracting Redshift Proxy...")
+        stagingdir = self.staging_dir(instance)
+        rs_filename = "{name}.rs".format(**instance.data)
+
+        rs_filepath = os.path.join(stagingdir, rs_filename)
+
+        # MaxScript command for export
+        export_cmd = (
+            f"""
+fn ProxyExport fp selected:true compress:false connectivity:false startFrame: endFrame: camera:undefined warnExisting:true transformPivotToOrigin:false = (
+	if startFrame == unsupplied then (
+		startFrame = (currentTime.frame as integer)
+	)
+
+	if endFrame == unsupplied then (
+		endFrame = (currentTime.frame as integer)
+	)
+
+    ret = rsProxy fp selected compress connectivity startFrame endFrame camera warnExisting transformPivotToOrigin
+
+    ret
+)
+execute = ProxyExport fp selected:true compress:false connectivity:false startFrame:{start} endFrame:{end} warnExisting:false transformPivotToOrigin:bTransformPivotToOrigin
+
+            """)    # noqa
+
+        with maintained_selection():
+            # select and export
+            rt.select(container.Children)
+            rt.execute(export_cmd)
+
+        self.log.info("Performing Extraction ...")
+        if "representations" not in instance.data:
+            instance.data["representations"] = []
+
+        representation = {
+            'name': 'rs',
+            'ext': 'rs',
+            # need to count the files
+            'files': rs_filename,
+            "stagingDir": stagingdir,
+        }
+        instance.data["representations"].append(representation)
+        self.log.info("Extracted instance '%s' to: %s" % (instance.name,
+                                                          rs_filepath))
+
+        # TODO: set sequence
+        def get_rsfiles(self, container, startFrame, endFrame):
+            pass
