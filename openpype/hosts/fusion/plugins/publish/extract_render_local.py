191882424047cb517f0fc97171819dcca6d84093
Jacob Danell <jacob@emberlight.se>
2023-03-14 17:56:11 +0100

    Moved the review representation back to render_local.py

--- openpype/hosts/fusion/plugins/publish/extract_render_local.py
+++ openpype/hosts/fusion/plugins/publish/extract_render_local.py
@@ -29,6 +29,41 @@ class FusionRenderLocal(
             )
         )
 
+        # Generate the frame list
+        frame_start = context.data["frameStartHandle"]
+        frame_end = context.data["frameEndHandle"]
+        path = instance.data["path"]
+        output_dir = instance.data["outputDir"]
+
+        basename = os.path.basename(path)
+        head, ext = os.path.splitext(basename)
+        files = [
+            f"{head}{str(frame).zfill(4)}{ext}"
+            for frame in range(frame_start, frame_end + 1)
+        ]
+        repre = {
+            "name": ext[1:],
+            "ext": ext[1:],
+            "frameStart": f"%0{len(str(frame_end))}d" % frame_start,
+            "files": files,
+            "stagingDir": output_dir,
+        }
+
+        # Get the colorspace represenation
+        self.set_representation_colorspace(
+            representation=repre,
+            context=context,
+        )
+
+        # review representation
+        if instance.data.get("review", False):
+            repre["tags"] = ["review"]
+
+        # add the repre to the instance
+        if "representations" not in instance.data:
+            instance.data["representations"] = []
+        instance.data["representations"].append(repre)
+
     def render_once(self, context):
         """Render context comp only once, even with more render instances"""
 
