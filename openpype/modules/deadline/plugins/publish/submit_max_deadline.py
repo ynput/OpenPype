fe5d4600338956471b35931aefd565139457abac
Kayla Man <moonyuet2008@hotmail.com>
2023-03-16 16:40:43 +0800

    adding the maxrender families into the pool collectors

--- openpype/modules/deadline/plugins/publish/submit_max_deadline.py
+++ openpype/modules/deadline/plugins/publish/submit_max_deadline.py
@@ -69,10 +69,8 @@ class MaxSubmitDeadline(abstract_submit_deadline.AbstractSubmitDeadline):
         )
         job_info.Frames = frames
 
-        if self.deadline_pool:
-            job_info.Pool = self.deadline_pool
-        if self.deadline_pool_secondary:
-            job_info.SecondaryPool = self.deadline_pool_secondary
+        job_info.Pool = instance.data.get("primaryPool")
+        job_info.SecondaryPool = instance.data.get("secondaryPool")
         job_info.ChunkSize = instance.data.get("chunkSize", 1)
         job_info.Comment = context.data.get("comment")
         job_info.Priority = instance.data.get("priority", self.priority)
