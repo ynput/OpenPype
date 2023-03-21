4215bb18cf57f26f22c110d92db3f1244aa99d6d
Petr Kalis <petr.kalis@gmail.com>
2023-03-21 10:41:46 +0100

    Update openpype/plugins/publish/preintegrate_thumbnail_representation.py
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- openpype/plugins/publish/preintegrate_thumbnail_representation.py
+++ openpype/plugins/publish/preintegrate_thumbnail_representation.py
@@ -60,8 +60,7 @@ class PreIntegrateThumbnails(pyblish.api.InstancePlugin):
         if not found_profile:
             return
 
-        if not thumbnail_repre.get("tags"):
-            thumbnail_repre["tags"] = []
+        thumbnail_repre.setdefault("tags", [])
 
         if not found_profile["integrate_thumbnail"]:
             if "delete" not in thumbnail_repre["tags"]:
