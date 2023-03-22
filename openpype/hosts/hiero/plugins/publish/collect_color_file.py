8e53014f07bf1b7a605117ce0d76ef478b23b12a
jbeaulieu <jbeaulieu@alkemy-x.com>
2023-03-22 16:22:17 -0400

    Fix empty parse cdl values. Correct offset straight cdl values. Ensure CDL lives in incoming source is file

--- openpype/hosts/hiero/plugins/publish/collect_color_file.py
+++ openpype/hosts/hiero/plugins/publish/collect_color_file.py
@@ -96,38 +96,34 @@ def parse_cdl(path):
     with open(path, "r") as f:
         cdl_data = f.read().lower()
 
-    cdl = {}
-
     slope_pattern = r"<slope>(?P<sR>[-,\d,.]*)[ ]{1}(?P<sG>[-,\d,.]+)[ ]{1}(?P<sB>[-,\d,.]*)</slope>"
     offset_pattern = r"<offset>(?P<oR>[-,\d,.]*)[ ]{1}(?P<oG>[-,\d,.]+)[ ]{1}(?P<oB>[-,\d,.]*)</offset>"
     power_pattern = r"<power>(?P<pR>[-,\d,.]*)[ ]{1}(?P<pG>[-,\d,.]+)[ ]{1}(?P<pB>[-,\d,.]*)</power>"
     sat_pattern = r"<saturation\>(?P<sat>[-,\d,.]+)</saturation\>"
-    path_pattern = r"<originalpath\>(?P<path>.*)<\/originalpath\>"
+    path_pattern = r"<originalpath\>(?P<file>.*)<\/originalpath\>"
 
     slope_match = re.search(slope_pattern, cdl_data)
-    if slope_match:
-        slope = (tuple(map(float, (slope_match.group("sR"), slope_match.group("sG"), slope_match.group("sB")))))
-        cdl["slope"] = slope
+    slope = (tuple(map(float, (slope_match.group("sR"), slope_match.group("sG"), slope_match.group("sB"))))) if slope_match else None
 
     offset_match = re.search(offset_pattern, cdl_data)
-    if offset_match:
-        offset = (tuple(map(float, (offset_match.group("oR"), offset_match.group("oG"), offset_match.group("oB")))))
-        cdl["offset"] = offset
+    offset = (tuple(map(float, (offset_match.group("oR"), offset_match.group("oG"), offset_match.group("oB"))))) if offset_match else None
 
     power_match = re.search(power_pattern, cdl_data)
-    if power_match:
-        power = (tuple(map(float, (power_match.group("pR"), power_match.group("pG"), power_match.group("pB")))))
-        cdl["power"] = power
+    power = (tuple(map(float, (power_match.group("pR"), power_match.group("pG"), power_match.group("pB"))))) if power_match else None
 
     sat_match = re.search(sat_pattern, cdl_data)
-    if sat_match:
-        sat = float(sat_match.group("sat"))
-        cdl["sat"] = sat
+    sat = float(sat_match.group("sat")) if sat_match else None
 
     path_match = re.search(path_pattern, cdl_data)
-    if path_match:
-        path_value = path_match.group("path")
-        cdl["file"] = path_value
+    file = path_match.group("file") if path_match else ""
+
+    cdl = {
+    "slope": slope,
+    "offset": offset,
+    "power": power,
+    "sat": sat,
+    "file": file
+    }
 
     return cdl
 
@@ -510,7 +506,7 @@ class MissingColorFile(QtWidgets.QDialog):
     def blank_grade(self):
         cdl = {
             "slope": (1, 1, 1),
-            "offset": (1, 1, 1),
+            "offset": (0, 0, 0),
             "power": (1, 1, 1),
             "sat": 1
         }
@@ -550,7 +546,15 @@ class MissingColorFile(QtWidgets.QDialog):
         if not os.path.isfile(color_file_path):
             # Make sure that color file is a file on disk
             QtWidgets.QMessageBox.information(hiero.ui.mainWindow(), "Info",
-                                              "Please make sure the file you selected exists")  # 3DL, Cube
+                                              "Please make sure the file you selected exists")
+            return
+
+        incoming_pattern = r"\/proj\/.*\/incoming"
+        incoming_match = re.match(incoming_pattern, color_file_path)
+        if not incoming_match:
+            # Make sure that color file is a file on disk
+            QtWidgets.QMessageBox.information(hiero.ui.mainWindow(), "Info",
+                                              "Please make sure the file you selected is in show incoming")
             return
 
         if not color_file_path_ext in COLOR_FILE_EXTS:
@@ -562,7 +566,7 @@ class MissingColorFile(QtWidgets.QDialog):
         self.set_data()
         if self.data["cdl"]["slope"][0] == "NA":
             QtWidgets.QMessageBox.information(hiero.ui.mainWindow(), "Info",
-                                              "No color data found!\n\nIf this shot needs a blank grade press 'Blank Grade'")  # 3DL, Cube
+                                              "No color data found!\n\nIf this shot needs a blank grade press 'Blank Grade'")
             return
 
         self.accept()
@@ -707,12 +711,10 @@ class CollectColorFile(pyblish.api.InstancePlugin):
             print(dialog.data, "dialog_result.data")
             if dialog_result:
                 cdl = dialog.data["cdl"]
-                color_file = dialog.data["path"]
+                cdl["file"] = dialog.data["path"]
+            else:
+                self.log.critical("No color file found for plate '{0}'-'{1}'".format(item_name, source_name))
+                return
 
-        cdl["file"] = color_file
-
-        if cdl:
-            instance.data["cdl"] = cdl
-            self.log.info("Collected CDL: {0}".format(cdl))
-        else:
-           self.log.critical("No color file found for plate '{0}'-'{1}'".format(item_name, source_name))
+        instance.data["cdl"] = cdl
+        self.log.info("Collected CDL: {0}".format(cdl))
