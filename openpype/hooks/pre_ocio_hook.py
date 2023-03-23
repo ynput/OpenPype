53ad2610dabe68dc54d2453bddc9e8800eea5b9b
jbeaulieu <jbeaulieu@alkemy-x.com>
2023-03-22 20:26:02 -0400

    Fix class name

--- openpype/hooks/pre_ocio_hook.py
+++ openpype/hooks/pre_ocio_hook.py
@@ -4,7 +4,7 @@ from openpype.pipeline.colorspace import get_imageio_config
 from openpype.pipeline.template_data import get_template_data_with_names
 
 
-class FusionPreLaunchOCIO(PreLaunchHook):
+class PreLaunchOCIO(PreLaunchHook):
     """Set OCIO environment variable for Fusion"""
 
     def execute(self):
