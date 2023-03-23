6f75a8f62ddbfbc67d976c5d12b6eca7f678e073
Roy Nieterau <roy_nieterau@hotmail.com>
2023-03-23 10:19:10 +0100

    Refactor variable names

--- openpype/modules/kitsu/plugins/publish/integrate_kitsu_review.py
+++ openpype/modules/kitsu/plugins/publish/integrate_kitsu_review.py
@@ -14,15 +14,15 @@ class IntegrateKitsuReview(pyblish.api.InstancePlugin):
     def process(self, instance):
 
         # Check comment has been created
-        comment = instance.data.get("kitsu_comment", {}).get("id")
-        if not comment:
+        comment_id = instance.data.get("kitsu_comment", {}).get("id")
+        if not comment_id:
             self.log.debug(
                 "Comment not created, review not pushed to preview."
             )
             return
 
         # Add review representations as preview of comment
-        task = instance.data["kitsu_task"]["id"]
+        task_id = instance.data["kitsu_task"]["id"]
         for representation in instance.data.get("representations", []):
             # Skip if not tagged as review
             if "kitsureview" not in representation.get("tags", []):
@@ -31,6 +31,6 @@ class IntegrateKitsuReview(pyblish.api.InstancePlugin):
             self.log.debug("Found review at: {}".format(review_path))
 
             gazu.task.add_preview(
-                task, comment, review_path, normalize_movie=True
+                task_id, comment_id, review_path, normalize_movie=True
             )
             self.log.info("Review upload on comment")
