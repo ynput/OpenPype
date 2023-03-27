26a5e40dd059bd115fa7da86c4f52d23019f8e65
Kayla Man <moonyuet2008@hotmail.com>
2023-03-27 17:27:35 +0800

    WIP for loadmaxscene

--- openpype/hosts/max/plugins/load/load_max_scene.py
+++ openpype/hosts/max/plugins/load/load_max_scene.py
@@ -47,14 +47,26 @@ class MaxSceneLoader(load.LoaderPlugin):
 
         path = get_representation_path(representation)
         node = rt.getNodeByName(container["instance_node"])
-        rt.mergeMaxFile(path,
-                        rt.Name("noRedraw"),
-                        rt.Name("deleteOldDups"),
-                        rt.Name("useSceneMtlDups"))
 
         lib.imprint(container["instance_node"], {
             "representation": str(representation["_id"])
         })
+        max_objs_prev = []
+        max_objs_after = []
+        for c in rt.rootNode.Children:
+            if rt.classOf(c) == rt.Container and c.startswith("maxSceneMain"):
+                if c != container["instance_node"]:
+                    max_objs_prev.append(c)
+        rt.mergeMaxFile(path,
+                        rt.Name("noRedraw"),
+                        rt.Name("deleteOldDups"),
+                        rt.Name("useSceneMtlDups"))
+        for c in rt.rootNode.Children:
+            if rt.classOf(c) == rt.Container and c.startswith("maxSceneMain"):
+                if c != container["instance_node"]:
+                    max_objs_after.append(c)
+
+        max_obj = set(max_objs_after) - set(max_objs_prev)
 
     def switch(self, container, representation):
         self.update(container, representation)
