71adb2964b5807742622949045899c845b5ada94
Roy Nieterau <roy_nieterau@hotmail.com>
2023-03-29 17:52:54 +0200

    Fix set correct variable

--- openpype/hosts/houdini/plugins/publish/collect_current_file.py
+++ openpype/hosts/houdini/plugins/publish/collect_current_file.py
@@ -20,7 +20,7 @@ class CollectHoudiniCurrentFile(pyblish.api.InstancePlugin):
             # By default, Houdini will even point a new scene to a path.
             # However if the file is not saved at all and does not exist,
             # we assume the user never set it.
-            filepath = ""
+            current_file = ""
 
         elif os.path.basename(current_file) == "untitled.hip":
             # Due to even a new file being called 'untitled.hip' we are unable
