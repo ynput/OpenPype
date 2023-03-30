35a5d5f172a5a04fe96d4e568ecd37f301f485f4
Roy Nieterau <roy_nieterau@hotmail.com>
2023-03-30 19:43:33 +0200

    Revert for backwards compatibility of client code
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- openpype/lib/execute.py
+++ openpype/lib/execute.py
@@ -200,7 +200,7 @@ def run_openpype_process(*args, **kwargs):
     args = get_openpype_execute_args(*args)
     env = kwargs.pop("env", None)
     # Keep env untouched if are passed and not empty
-    if env is None:
+    if not env:
         # Skip envs that can affect OpenPype process
         # - fill more if you find more
         env = clean_envs_for_openpype_process(os.environ)
