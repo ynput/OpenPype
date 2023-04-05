4b399bca9e2c62eb3e51a5f46b35392296cdb39d
Jean-Francois Beaulieu <jbeaulieu@alkemy-x.com>
2023-04-05 18:41:23 +0000

    Remove lines that aren't needed

--- openpype/modules/shotgrid/plugins/publish/integrate_shotgrid_cut_info.py
+++ openpype/modules/shotgrid/plugins/publish/integrate_shotgrid_cut_info.py
@@ -1,14 +1,11 @@
 import os
 import pyblish.api
-
-
-from openpype.pipeline import publish
 from openpype.hosts.hiero import api as phiero
-from openpype.pipeline.publish import ValidateContentsOrder
+
+
 def openpype_publish_tag(track_item):
     """Get tag that was used to publish track item"""
     for item_tag in track_item.tags():
-        # print(instance_tag, 'instance_tag')
         tag_metadata = dict(item_tag.metadata())
         tag_family = tag_metadata.get("tag.family", "")
         if tag_family == "reference":
@@ -29,21 +26,9 @@ def get_tag_handles(track_item):
     return handle_start, handle_end
 
 
-
-
-                    # class ValidateShotgridUser(pyblish.api.ContextPlugin):
-                    #     """
-                    #     Check if user is valid and have access to the project.
-                    #     """
-                    #
-                    #     label = "Validate Shotgrid User"
-                    #     order = ValidateContentsOrder
 class IntegrateShotgridCutInfo(pyblish.api.InstancePlugin):
-# class IntegrateShotgridCutInfo(publish.Extractor):
 
     order = pyblish.api.IntegratorOrder + 0.4999
-    # order = ValidateContentsOrder
-    # order = pyblish.api.ExtractorOrder - .5
     label = "Integrate Shotgrid Cut Info"
     hosts = ["hiero"]
     families = ["reference"]
@@ -55,18 +40,11 @@ class IntegrateShotgridCutInfo(pyblish.api.InstancePlugin):
         self.sg = context.data.get("shotgridSession")
         shotgrid_version = instance.data.get("shotgridVersion")
 
-        # print(self.tag_data, 'self.tag_data')
         # If cut already added to shotgrid shot then don't update cut?
         if not shotgrid_version:
             self.log.warning("No Shotgrid version collect. Cut Info could not be integrated into shot")
             return
-        # self.log.info("Cut In and Cut Out already in Shotgrid")
 
-
-        # first_frame = instance.data["frameStart"]
-        # end_frame = instance.data["frameEnd"] + handle_end
-
-        # need the cut item to calculate the cut info keys
 
         track_item = instance.data["item"]
         openpype_tag = phiero.get_track_item_tags(track_item)
