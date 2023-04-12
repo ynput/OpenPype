d8987149ad599d08870aa0717151503a82c8f30c
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2023-04-12 10:34:11 +0200

    General: Extract burnin hosts filters (#4749)
    * remove hosts filter in ExtractBurnin
    
    * skip instances without representations instead of crash
    
    ---------
    
    Co-authored-by: Jakub Ježek <jakubjezek001@gmail.com>

--- openpype/plugins/publish/extract_burnin.py
+++ openpype/plugins/publish/extract_burnin.py
@@ -34,24 +34,6 @@ class ExtractBurnin(publish.Extractor):
     order = pyblish.api.ExtractorOrder + 0.03
 
     families = ["review", "burnin"]
-    hosts = [
-        "nuke",
-        "maya",
-        "shell",
-        "hiero",
-        "premiere",
-        "traypublisher",
-        "standalonepublisher",
-        "harmony",
-        "fusion",
-        "aftereffects",
-        "tvpaint",
-        "webpublisher",
-        "aftereffects",
-        "photoshop",
-        "flame"
-        # "resolve"
-    ]
 
     optional = True
 
@@ -78,9 +60,10 @@ class ExtractBurnin(publish.Extractor):
             self.log.warning("No profiles present for create burnin")
             return
 
-        # QUESTION what is this for and should we raise an exception?
-        if "representations" not in instance.data:
-            raise RuntimeError("Burnin needs already created mov to work on.")
+        if not instance.data.get("representations"):
+            self.log.info(
+                "Instance does not have filled representations. Skipping")
+            return
 
         self.main_process(instance)
 
