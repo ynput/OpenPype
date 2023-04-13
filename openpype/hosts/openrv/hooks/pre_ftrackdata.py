c3a7042a9e4620b538dca231229e80b2dc229621
Roy Nieterau <roy_nieterau@hotmail.com>
2023-04-13 13:18:53 +0200

    Cleanup code

--- openpype/hosts/openrv/hooks/pre_ftrackdata.py
+++ openpype/hosts/openrv/hooks/pre_ftrackdata.py
@@ -5,21 +5,15 @@ from openpype.lib import PreLaunchHook
 
 
 class PreFtrackData(PreLaunchHook):
-    """Pre-hook for openrv/ftrack
-    """
+    """Pre-hook for openrv/ftrack."""
     app_groups = ["openrv"]
 
     def execute(self):
-        print("----- ---- ---- ---- ---")
-        print("-------  OPENRV PreFtrackData ")
-        print("----- ---- ---- ---- ---")
-
 
         representations = self.data.get("extra", None)
         if representations:
             payload = {"representations": representations}
-            repr_file = tempfile.NamedTemporaryFile(mode="w+", delete=False)
-            json.dump(payload, repr_file)
-            repr_file.flush()
+            with tempfile.NamedTemporaryFile(mode="w+", delete=False) as file:
+                json.dump(payload, file)
 
-            self.launch_context.env["OPENPYPE_LOADER_REPRESENTATIONS"] = str(repr_file.name)
+            self.launch_context.env["OPENPYPE_LOADER_REPRESENTATIONS"] = str(file.name)  # noqa
