371ec048f0dddeda2a60a93eeb06341ddfd2e271
Roy Nieterau <roy_nieterau@hotmail.com>
2023-04-17 20:43:22 +0200

    Cleanup, move import to top + improve docstring

--- openpype/hosts/openrv/api/lib.py
+++ openpype/hosts/openrv/api/lib.py
@@ -1,5 +1,7 @@
 import contextlib
 
+import rv
+
 
 @contextlib.contextmanager
 def maintained_selection():
@@ -12,15 +14,18 @@ def command_batch(name):
 
 
 def group_member_of_type(group_node, member_type):
-    """
-    usage layout_stack = group_member_of_type(sequence_layout, "RVStack")
-    :param group_node:
-    :param member_type:
-    :return:
-    """
-    # todo: move import to top of file?
-    import rv
+    """Return first member of group that is of the given node type.
 
+    This is similar to `rv.extra_commands.nodesInGroupOfType` but only
+    returns the first entry directly if it has any match.
+
+    Args:
+        group_node (str): The group node to search in.
+        member_type (str): The node type to search for.
+
+    Returns:
+        str or None: The first member found of given type or None
+    """
     for node in rv.commands.nodesInGroup(group_node):
         if rv.commands.nodeType(node) == member_type:
             return node
