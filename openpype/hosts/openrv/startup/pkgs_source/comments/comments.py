b0970445571cfe0d21009ad2d7863c426e9ef4f5
Roy Nieterau <roy_nieterau@hotmail.com>
2023-04-17 21:53:26 +0200

    Remove redundant check, it's already checked in other if statement

--- openpype/hosts/openrv/startup/pkgs_source/comments/comments.py
+++ openpype/hosts/openrv/startup/pkgs_source/comments/comments.py
@@ -153,9 +153,6 @@ class ReviewMenu(MinorMode):
             self.dockWidget.toggleViewAction().trigger()
 
     def _is_active(self):
-        if self.dockWidget is None:
-            return rv.commands.UncheckedMenuState
-
         if self.dockWidget is not None and self.dockWidget.isVisible():
             return rv.commands.CheckedMenuState
         else:
