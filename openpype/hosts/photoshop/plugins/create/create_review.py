67c8389a02cf754ebddd5354866eba55dea8d022
Petr Kalis <petr.kalis@gmail.com>
2023-04-19 12:42:04 +0200

    OP-5656 - refactor - both auto creators should use same class
    Provided separate description.

--- openpype/hosts/photoshop/plugins/create/create_review.py
+++ openpype/hosts/photoshop/plugins/create/create_review.py
@@ -7,3 +7,13 @@ class ReviewCreator(PSAutoCreator):
     family = "review"
 
     default_variant = "Main"
+
+    def get_detail_description(self):
+        return """Auto creator for review.
+
+        Photoshop review is created from all published images or from all
+        visible layers if no `image` instances got created.
+        
+        Review might be disabled by an artist (instance shouldn't be deleted as
+        it will get recreated in next publish either way).
+        """
--- openpype/hosts/photoshop/plugins/create/create_workfile.py
+++ openpype/hosts/photoshop/plugins/create/create_workfile.py
@@ -1,79 +1,20 @@
-import openpype.hosts.photoshop.api as api
-from openpype.client import get_asset_by_name
-from openpype.pipeline import (
-    AutoCreator,
-    CreatedInstance
-)
-from openpype.hosts.photoshop.api.pipeline import cache_and_get_instances
+from openpype.hosts.photoshop.lib import PSAutoCreator
 
 
-class WorkfileCreator(AutoCreator):
+class WorkfileCreator(PSAutoCreator):
     identifier = "workfile"
     family = "workfile"
 
     default_variant = "Main"
 
-    def get_instance_attr_defs(self):
-        return []
+    def get_detail_description(self):
+        return """Auto creator for workfile.
 
-    def collect_instances(self):
-        for instance_data in cache_and_get_instances(self):
-            creator_id = instance_data.get("creator_identifier")
-            if creator_id == self.identifier:
-                subset_name = instance_data["subset"]
-                instance = CreatedInstance(
-                    self.family, subset_name, instance_data, self
-                )
-                self._add_instance_to_context(instance)
-
-    def update_instances(self, update_list):
-        # nothing to change on workfiles
-        pass
-
-    def create(self, options=None):
-        existing_instance = None
-        for instance in self.create_context.instances:
-            if instance.family == self.family:
-                existing_instance = instance
-                break
-
-        context = self.create_context
-        project_name = context.get_current_project_name()
-        asset_name = context.get_current_asset_name()
-        task_name = context.get_current_task_name()
-        host_name = context.host_name
-        if existing_instance is None:
-            asset_doc = get_asset_by_name(project_name, asset_name)
-            subset_name = self.get_subset_name(
-                self.default_variant, task_name, asset_doc,
-                project_name, host_name
-            )
-            data = {
-                "asset": asset_name,
-                "task": task_name,
-                "variant": self.default_variant
-            }
-            data.update(self.get_dynamic_data(
-                self.default_variant, task_name, asset_doc,
-                project_name, host_name, None
-            ))
-
-            new_instance = CreatedInstance(
-                self.family, subset_name, data, self
-            )
-            self._add_instance_to_context(new_instance)
-            api.stub().imprint(new_instance.get("instance_id"),
-                               new_instance.data_to_store())
-
-        elif (
-            existing_instance["asset"] != asset_name
-            or existing_instance["task"] != task_name
-        ):
-            asset_doc = get_asset_by_name(project_name, asset_name)
-            subset_name = self.get_subset_name(
-                self.default_variant, task_name, asset_doc,
-                project_name, host_name
-            )
-            existing_instance["asset"] = asset_name
-            existing_instance["task"] = task_name
-            existing_instance["subset"] = subset_name
+        It is expected that each publish will also publish its source workfile
+        for safekeeping. This creator triggers automatically without need for
+        an artist to remember and trigger it explicitly.
+        
+        Workfile instance could be disabled if it is not required to publish
+        workfile. (Instance shouldn't be deleted though as it will be recreated
+        in next publish automatically).
+        """
