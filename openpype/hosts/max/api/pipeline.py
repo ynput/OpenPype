5e012d6be254dfa68721d5f08a981cafecbc8842
Kayla Man <moonyuet2008@hotmail.com>
2023-04-21 17:29:47 +0800

    add validator for resolution setting

--- openpype/hosts/max/api/pipeline.py
+++ openpype/hosts/max/api/pipeline.py
@@ -56,9 +56,6 @@ class MaxHost(HostBase, IWorkfileHost, ILoadHost, INewPublisher):
         rt.callbacks.addScript(rt.Name('systemPostNew'),
                                context_setting)
 
-        rt.callbacks.addScript(rt.Name('filePostOpen'),
-                               context_setting)
-
     def has_unsaved_changes(self):
         # TODO: how to get it from 3dsmax?
         return True
