73b42d67e50223d5398d448d5c672b02a8909613
Jacob Danell <jacob@emberlight.se>
2023-04-21 17:49:50 +0200

    Make working cleanup_placeholder function

--- openpype/hosts/fusion/api/workfile_template_builder.py
+++ openpype/hosts/fusion/api/workfile_template_builder.py
@@ -179,7 +179,7 @@ class FusionPlaceholderLoadPlugin(
     def _before_repre_load(self, placeholder, representation):
         comp = get_current_comp()
         nodes_init = []
-        for node in comp.GetToolList(False, "Fuse.OPEmpty").values():
+        for node in comp.GetToolList().values():
             nodes_init.append(node.Name)
         placeholder.data["nodes_init"] = list(nodes_init)
         placeholder.data["last_repre_id"] = str(representation["_id"])
@@ -217,7 +217,11 @@ class FusionPlaceholderLoadPlugin(
     def cleanup_placeholder(self, placeholder, failed):
         # deselect all selected nodes
         comp = get_current_comp()
-        placeholder_node = comp.FindTool(placeholder.scene_identifier)
+
+        # Make sure no node is selected before starting to process
+        flow = comp.CurrentFrame.FlowView
+        flow.Select()
+        placeholder.data["node"] = comp.FindTool(placeholder.scene_identifier)
 
         # getting the latest nodes added
         # TODO get from shared populate data!
@@ -230,41 +234,23 @@ class FusionPlaceholderLoadPlugin(
         for name in nodes_loaded_names:
             nodes_loaded.append(comp.FindTool(name))
 
-        self.log.debug("Loaded nodes: {}".format(nodes_loaded))
+        self.log.debug("Loaded node: {}".format(nodes_loaded_names))
 
         if not nodes_loaded:
             return
 
         placeholder.data["delete"] = True
 
-        nodes_loaded = self._move_to_placeholder_group(
-            placeholder, nodes_loaded
-        )
         placeholder.data["last_loaded"] = nodes_loaded
 
-        # positioning of the loaded nodes
-        # FUSION! - I don't think this is needed at all?
-
-        # flow = comp.CurrentFrame.FlowView
-        # min_x, min_y, _, _ = get_extreme_positions(nodes_loaded)
-        # for node in nodes_loaded:
-        #    node_xPos, node_yPos = flow.GetPosTable(node).values()
-        #    placeholder_xPos, placeholder_yPos = flow.GetPosTable(
-        #        placeholder_node
-        #    ).values()
-        #    xpos = (node_xPos - min_x) + placeholder_xPos
-        #    ypos = (node_yPos - min_y) + placeholder_yPos
-        #    flow.SetPos(node, xpos, ypos)
-
-        # fix the problem of z_order for backdrops
-        self._setdata_siblings(placeholder)
+        if placeholder.data.get("keep_placeholder"):
+            self._setdata_siblings(placeholder)
 
         if placeholder.data["nb_children"] == 0:
             # save initial nodes postions and dimensions, update them
             # and set inputs and outputs of loaded nodes
+            self._update_nodes(placeholder.data["node"], nodes_loaded)
 
-            self._setdata_inits()
-            self._update_nodes(placeholder, nuke.allNodes(), nodes_loaded)
             self._set_loaded_connections(placeholder)
 
         elif placeholder.data["siblings"]:
@@ -303,42 +289,15 @@ class FusionPlaceholderLoadPlugin(
                 node.setXYpos(xpos, ypos)
 
         placeholder.data["nb_children"] += 1
-        reset_selection()
 
         # remove placeholders marked as delete
         if placeholder.data.get("delete") and not placeholder.data.get(
             "keep_placeholder"
         ):
             self.log.debug(
-                "Deleting node: {}".format(placeholder_node.name())
+                "Deleting node: {}".format(placeholder.data["node"].Name)
             )
-            nuke.delete(placeholder_node)
-
-        # go back to root group
-        nuke.root().begin()
-
-    def _move_to_placeholder_group(self, placeholder, nodes_loaded):
-        """
-        opening the placeholder's group and copying loaded nodes in it.
-
-        Returns :
-            nodes_loaded (list): the new list of pasted nodes
-        """
-
-        groups_name = placeholder.data["group_name"]
-        comp = get_current_comp()
-        flow = comp.CurrentFrame.FlowView
-        flow.Select(nodes_loaded)
-        if groups_name:
-            with node_tempfile() as filepath:
-                nuke.nodeCopy(filepath)
-                for node in nuke.selectedNodes():
-                    nuke.delete(node)
-                group = nuke.toNode(groups_name)
-                group.begin()
-                nuke.nodePaste(filepath)
-                nodes_loaded = nuke.selectedNodes()
-        return nodes_loaded
+            placeholder.data["node"].Delete()
 
     def _fix_z_order(self, placeholder):
         """Fix the problem of z_order when a backdrop is loaded."""
@@ -381,7 +340,7 @@ class FusionPlaceholderLoadPlugin(
         loaded_nodes_set = set(loaded_nodes)
 
         for node in loaded_nodes:
-            if node["builder_type"] is None:
+            if node.GetData("builder_type") is None:
                 # save the id of representation for all imported nodes
                 node.SetData(
                     "repre_id", str(placeholder.data["last_repre_id"])
@@ -410,85 +369,33 @@ class FusionPlaceholderLoadPlugin(
             refresh_node(node)
 
     def _update_nodes(
-        self, placeholder, nodes, considered_nodes, offset_y=None
+        self, placeholder_node, nodes
     ):
-        """Adjust backdrop nodes dimensions and positions.
+        """Adjust nodes positions.
 
         Considering some nodes sizes.
 
         Args:
             nodes (list): list of nodes to update
-            considered_nodes (list): list of nodes to consider while updating
-                positions and dimensions
-            offset (int): distance between copies
         """
 
-        placeholder_node = nuke.toNode(placeholder.scene_identifier)
+        comp = get_current_comp()
+        flow = comp.CurrentFrame.FlowView
 
-        min_x, min_y, max_x, max_y = get_extreme_positions(considered_nodes)
-
-        diff_x = diff_y = 0
-        contained_nodes = []  # for backdrops
-
-        if offset_y is None:
-            width_ph = placeholder_node.screenWidth()
-            height_ph = placeholder_node.screenHeight()
-            diff_y = max_y - min_y - height_ph
-            diff_x = max_x - min_x - width_ph
-            contained_nodes = [placeholder_node]
-            min_x = placeholder_node.xpos()
-            min_y = placeholder_node.ypos()
-        else:
-            siblings = get_nodes_by_names(placeholder.data["siblings"])
-            minX, _, maxX, _ = get_extreme_positions(siblings)
-            diff_y = max_y - min_y + 20
-            diff_x = abs(max_x - min_x - maxX + minX)
-            contained_nodes = considered_nodes
-
-        if diff_y <= 0 and diff_x <= 0:
-            return
-
+        x, y = flow.GetPosTable(placeholder_node).values()
+        flow.SetPos(placeholder_node, x-1, y)
+        x_offset = 0
         for node in nodes:
-            refresh_node(node)
-
-            if node == placeholder_node or node in considered_nodes:
-                continue
-
-            if not isinstance(node, nuke.BackdropNode) or (
-                isinstance(node, nuke.BackdropNode)
-                and not set(contained_nodes) <= set(node.getNodes())
-            ):
-                if offset_y is None and node.xpos() >= min_x:
-                    node.setXpos(node.xpos() + diff_x)
-
-                if node.ypos() >= min_y:
-                    node.setYpos(node.ypos() + diff_y)
-
-            else:
-                width = node.screenWidth()
-                height = node.screenHeight()
-                node.knob("bdwidth").setValue(width + diff_x)
-                node.knob("bdheight").setValue(height + diff_y)
-
-            refresh_node(node)
+            flow.SetPos(node, x+x_offset, y)
+            x_offset = x_offset + 1
 
     def _set_loaded_connections(self, placeholder):
         """
         set inputs and outputs of loaded nodes"""
 
-        placeholder_node = nuke.toNode(placeholder.scene_identifier)
-        input_node, output_node = get_group_io_nodes(
-            placeholder.data["last_loaded"]
-        )
-        for node in placeholder_node.dependent():
-            for idx in range(node.inputs()):
-                if node.input(idx) == placeholder_node and output_node:
-                    node.setInput(idx, output_node)
-
-        for node in placeholder_node.dependencies():
-            for idx in range(placeholder_node.inputs()):
-                if placeholder_node.input(idx) == node and input_node:
-                    input_node.setInput(0, node)
+        inputs = placeholder.data["node"].Output.GetConnectedInputs().values()
+        for input in inputs:
+            input.ConnectTo(placeholder.data["last_loaded"][0].Output)
 
     def _create_sib_copies(self, placeholder):
         """creating copies of the palce_holder siblings (the ones who were
@@ -635,9 +542,6 @@ class FusionPlaceholderCreatePlugin(
 
         placeholder.data["delete"] = True
 
-        nodes_created = self._move_to_placeholder_group(
-            placeholder, nodes_created
-        )
         placeholder.data["last_created"] = nodes_created
         refresh_nodes(nodes_created)
 
@@ -651,14 +555,15 @@ class FusionPlaceholderCreatePlugin(
 
         # fix the problem of z_order for backdrops
         self._fix_z_order(placeholder)
-        self._setdata_siblings(placeholder)
+        if placeholder.data.get("keep_placeholder"):
+            self._setdata_siblings(placeholder)
 
         if placeholder.data["nb_children"] == 0:
             # save initial nodes postions and dimensions, update them
             # and set inputs and outputs of created nodes
+            if placeholder.data.get("keep_placeholder"):
+                self._update_nodes(placeholder, nuke.allNodes(), nodes_loaded)
 
-            self._setdata_inits()
-            self._update_nodes(placeholder, nuke.allNodes(), nodes_created)
             self._set_created_connections(placeholder)
 
         elif placeholder.data["siblings"]:
@@ -710,27 +615,6 @@ class FusionPlaceholderCreatePlugin(
 
         # go back to root group
         nuke.root().begin()
-
-    def _move_to_placeholder_group(self, placeholder, nodes_created):
-        """
-        opening the placeholder's group and copying created nodes in it.
-
-        Returns :
-            nodes_created (list): the new list of pasted nodes
-        """
-        groups_name = placeholder.data["group_name"]
-        reset_selection()
-        select_nodes(nodes_created)
-        if groups_name:
-            with node_tempfile() as filepath:
-                nuke.nodeCopy(filepath)
-                for node in nuke.selectedNodes():
-                    nuke.delete(node)
-                group = nuke.toNode(groups_name)
-                group.begin()
-                nuke.nodePaste(filepath)
-                nodes_created = nuke.selectedNodes()
-        return nodes_created
 
     def _fix_z_order(self, placeholder):
         """Fix the problem of z_order when a backdrop is create."""
