0994ae818c18e6f1a6c615ee14cc9b8901f3854a
Petr Kalis <petr.kalis@gmail.com>
2023-04-24 01:43:04 +0200

    OP-5656 - reworked auto collectors
    This allows to use automatic test for proper testing.

---
+++ openpype/hosts/photoshop/plugins/publish/collect_auto_image.py
@@ -0,0 +1,103 @@
+import pprint
+
+import pyblish.api
+
+from openpype.settings import get_project_settings
+from openpype.hosts.photoshop import api as photoshop
+from openpype.pipeline.create import get_subset_name
+from openpype.pipeline.context_tools import get_current_project_name
+from openpype.client import get_asset_by_name
+from openpype.pipeline import get_current_asset_name, get_current_task_name
+
+
+class CollectAutoImage(pyblish.api.ContextPlugin):
+    """Creates auto image in non artist based publishes (Webpublisher)"""
+
+    label = "Collect Instances"
+    order = pyblish.api.CollectorOrder
+    hosts = ["photoshop"]
+    order = pyblish.api.CollectorOrder + 0.2
+
+    targets = ["remotepublish"]
+
+    def process(self, context):
+        family = "image"
+        for instance in context:
+            creator_identifier = instance.data.get("creator_identifier")
+            if creator_identifier and creator_identifier == "auto_image":
+                self.log.debug("Auto image instance found, won't create new")
+                return
+
+        project_name = context.data["anatomyData"]["project"]["name"]
+        proj_settings = context.data["project_settings"]
+        task_name = context.data["anatomyData"]["task"]["name"]
+        host_name = context.data["hostName"]
+        asset_doc =  context.data["assetEntity"]
+        asset_name = asset_doc["name"]
+
+        auto_creator = proj_settings.get(
+            "photoshop", {}).get(
+            "create", {}).get(
+            "AutoImageCreator", {})
+
+        if not auto_creator or not auto_creator["enabled"]:
+            self.log.debug("Auto image creator disabled, won't create new")
+
+        stub = photoshop.stub()
+        stored_items = stub.get_layers_metadata()
+        for item in stored_items:
+            if item.get("creator_identifier") == family:
+                if not item.get("active"):
+                    self.log.debug("Auto_image instance disabled")
+                    return
+
+        layer_items = stub.get_layers()
+
+        publishable_ids = [layer.id for layer in layer_items
+                           if layer.visible]
+
+        # collect stored image instances
+        instance_names = []
+        for layer_item in layer_items:
+            layer_meta_data = stub.read(layer_item, stored_items)
+
+            # Skip layers without metadata.
+            if layer_meta_data is None:
+                continue
+
+            # Skip containers.
+            if "container" in layer_meta_data["id"]:
+                continue
+
+            # active might not be in legacy meta
+            if layer_meta_data.get("active", True) and layer_item.visible:
+                instance_names.append(layer_meta_data["subset"])
+
+        if len(instance_names) == 0:
+            variants = proj_settings.get(
+                "photoshop", {}).get(
+                "create", {}).get(
+                "CreateImage", {}).get(
+                "default_variants", [''])
+            family = "image"
+
+            variant = context.data.get("variant") or variants[0]
+
+            subset_name = get_subset_name(
+                family, variant, task_name, asset_doc,
+                project_name, host_name
+            )
+
+            instance = context.create_instance(subset_name)
+            instance.data["family"] = family
+            instance.data["asset"] = asset_name
+            instance.data["subset"] = subset_name
+            instance.data["ids"] = publishable_ids
+            instance.data["publish"] = True
+            instance.data["creator_identifier"] = "auto_image"
+
+            if auto_creator["mark_for_review"]:
+                instance.data["creator_attributes"] = {"mark_for_review": True}
+                instance.data["families"] = ["review"]
+
+            self.log.info("auto image instance: {} ".format(instance.data))
--- openpype/hosts/photoshop/plugins/publish/collect_review.py
+++ openpype/hosts/photoshop/plugins/publish/collect_review.py
@@ -14,10 +14,7 @@ from openpype.pipeline.create import get_subset_name
 
 
 class CollectReview(pyblish.api.ContextPlugin):
-    """Collect or create review instance.
-
-    Review instance might be autocreated or needs to be created if non artist
-    based workflow is used (eg. Webpublisher)
+    """Adds review to families for instances marked to be reviewable.
     """
 
     label = "Collect Review"
@@ -28,43 +25,8 @@ class CollectReview(pyblish.api.ContextPlugin):
     publish = True
 
     def process(self, context):
-        family = "review"
-        has_review = False
         for instance in context:
-            if instance.data["family"] == family:
-                has_review = True
-
             creator_attributes = instance.data["creator_attributes"]
             if (creator_attributes.get("mark_for_review") and
                     "review" not in instance.data["families"]):
                 instance.data["families"].append("review")
-
-        # additional logic only for remote publishing from Webpublisher
-        if "remotepublish" not in pyblish.api.registered_targets():
-            return
-
-        if has_review:
-            self.log.debug("Review instance found, won't create new")
-            return
-
-        subset = get_subset_name(
-            family,
-            context.data.get("variant", ''),
-            context.data["anatomyData"]["task"]["name"],
-            context.data["assetEntity"],
-            context.data["anatomyData"]["project"]["name"],
-            host_name=context.data["hostName"],
-            project_settings=context.data["project_settings"]
-        )
-
-        instance = context.create_instance(subset)
-        instance.data.update({
-            "subset": subset,
-            "label": subset,
-            "name": subset,
-            "family": family,
-            "families": [],
-            "representations": [],
-            "asset": os.environ["AVALON_ASSET"],
-            "publish": self.publish
-        })
