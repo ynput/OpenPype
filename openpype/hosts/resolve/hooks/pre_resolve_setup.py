4388da15df4b6a5f5a07e3514c54652e12b58691
Alexey Bogomolov <mail@abogomolov.com>
2023-04-28 18:32:33 +0300

    black formatting

--- openpype/hosts/resolve/hooks/pre_resolve_setup.py
+++ openpype/hosts/resolve/hooks/pre_resolve_setup.py
@@ -12,6 +12,7 @@ class ResolvePrelaunch(PreLaunchHook):
     path to the project by environment variable to Premiere launcher
     shell script.
     """
+
     app_groups = ["resolve"]
 
     def execute(self):
@@ -27,10 +28,14 @@ class ResolvePrelaunch(PreLaunchHook):
                 "/Library/Application Support/Blackmagic Design"
                 "/DaVinci Resolve/Developer/Scripting"
             ),
-            "linux": "/opt/resolve/Developer/Scripting"
+            "linux": "/opt/resolve/Developer/Scripting",
         }
-        resolve_script_api = Path(resolve_script_api_locations[current_platform])
-        self.launch_context.env["RESOLVE_SCRIPT_API"] = resolve_script_api.as_posix()
+        resolve_script_api = Path(
+            resolve_script_api_locations[current_platform]
+        )
+        self.launch_context.env[
+            "RESOLVE_SCRIPT_API"
+        ] = resolve_script_api.as_posix()
 
         resolve_script_lib_dirs = {
             "windows": (
@@ -41,14 +46,18 @@ class ResolvePrelaunch(PreLaunchHook):
                 "/Applications/DaVinci Resolve/DaVinci Resolve.app"
                 "/Contents/Libraries/Fusion/fusionscript.so"
             ),
-            "linux": "/opt/resolve/libs/Fusion/fusionscript.so"
+            "linux": "/opt/resolve/libs/Fusion/fusionscript.so",
         }
         resolve_script_lib = Path(resolve_script_lib_dirs[current_platform])
-        self.launch_context.env["RESOLVE_SCRIPT_LIB"] = resolve_script_lib.as_posix()
+        self.launch_context.env[
+            "RESOLVE_SCRIPT_LIB"
+        ] = resolve_script_lib.as_posix()
 
         # TODO: add OTIO installation from `openpype/requirements.py`
         # making sure python <3.9.* is installed at provided path
-        python3_home = Path(self.launch_context.env.get("RESOLVE_PYTHON3_HOME", ""))
+        python3_home = Path(
+            self.launch_context.env.get("RESOLVE_PYTHON3_HOME", "")
+        )
 
         assert python3_home.is_dir(), (
             "Python 3 is not installed at the provided folder path. Either "
@@ -62,14 +71,18 @@ class ResolvePrelaunch(PreLaunchHook):
 
         # add to the python path to PATH
         env_path = self.launch_context.env["PATH"]
-        self.launch_context.env["PATH"] = f"{python3_home_str}{os.pathsep}{env_path}"
+        self.launch_context.env[
+            "PATH"
+        ] = f"{python3_home_str}{os.pathsep}{env_path}"
 
         self.log.debug(f"PATH: {self.launch_context.env['PATH']}")
 
         # add to the PYTHONPATH
         env_pythonpath = self.launch_context.env["PYTHONPATH"]
         modules_path = Path(resolve_script_api, "Modules").as_posix()
-        self.launch_context.env["PYTHONPATH"] = f"{modules_path}{os.pathsep}{env_pythonpath}"
+        self.launch_context.env[
+            "PYTHONPATH"
+        ] = f"{modules_path}{os.pathsep}{env_pythonpath}"
 
         self.log.debug(f"PYTHONPATH: {self.launch_context.env['PYTHONPATH']}")
 
@@ -82,11 +95,15 @@ class ResolvePrelaunch(PreLaunchHook):
                 "/Library/Application Support/Blackmagic Design"
                 "/DaVinci Resolve/Fusion/Scripts/Comp"
             ),
-            "linux": "/opt/resolve/Fusion/Scripts/Comp"
+            "linux": "/opt/resolve/Fusion/Scripts/Comp",
         }
-        resolve_utility_scripts_dir = Path(resolve_utility_scripts_dirs[current_platform])
+        resolve_utility_scripts_dir = Path(
+            resolve_utility_scripts_dirs[current_platform]
+        )
         # setting utility scripts dir for scripts syncing
-        self.launch_context.env["RESOLVE_UTILITY_SCRIPTS_DIR"] = resolve_utility_scripts_dir.as_posix()
+        self.launch_context.env[
+            "RESOLVE_UTILITY_SCRIPTS_DIR"
+        ] = resolve_utility_scripts_dir.as_posix()
 
         # remove terminal coloring tags
         self.launch_context.env["OPENPYPE_LOG_NO_COLORS"] = "True"
