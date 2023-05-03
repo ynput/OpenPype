d2b2c47de1e70454fd7f79b04f326727888f014f
Jacob Danell <jacob@emberlight.se>
2023-05-03 18:16:11 +0200

    make pyside2 installation work on Fusion 17+

--- openpype/hosts/fusion/deploy/MenuScripts/install_pyside2.py
+++ openpype/hosts/fusion/deploy/MenuScripts/install_pyside2.py
@@ -5,6 +5,18 @@ import subprocess
 import importlib
 
 
+def get_python_path():
+    fusion_py_envs = [
+        "FUSION_PYTHON36_HOME",
+        "FUSION16_PYTHON36_HOME",
+        "FUSION_PYTHON3_HOME",
+    ]
+
+    for env in fusion_py_envs:
+        if os.environ.get(env) is not None:
+            return os.environ.get(env)
+
+
 try:
     from qtpy import API_NAME
 
@@ -13,14 +25,17 @@ try:
     print(f"Qt path: {mod.__file__}")
     print("Qt library found, nothing to do..")
 
-except ImportError:
+except:
+    python_path = get_python_path()
+    if python_path == None:
+        print("Can't find any any python environment.")
+
     print("Assuming no Qt library is installed..")
-    print('Installing PySide2 for Python 3.6: '
-          f'{os.environ["FUSION16_PYTHON36_HOME"]}')
+    print(f"Installing PySide2 for Python 3.6: {python_path}")
 
     # Get full path to python executable
-    exe = "python.exe" if os.name == 'nt' else "python"
-    python = os.path.join(os.environ["FUSION16_PYTHON36_HOME"], exe)
+    exe = "python.exe" if os.name == "nt" else "python"
+    python = os.path.join(python_path, exe)
     assert os.path.exists(python), f"Python doesn't exist: {python}"
 
     # Do python -m pip install PySide2
