6f876558d2fc71994deb640e9619ccfc965961fd
Félix David <felixg.david@gmail.com>
2023-05-10 11:00:14 +0200

    Fix: Make relative paths for UNC on Windows (#34)
    * fix already relative paths
    
    * added OSError exception for make relative path
    
    * refactor for loops in one
    
    * fix not statement with filepath.startswith
    
    * refactor list concat with itertools chain
    
    ---------
    
    Co-authored-by: kaamaurice <kaalium@gmail.com>

--- openpype/hosts/blender/utility_scripts/make_paths_relative.py
+++ openpype/hosts/blender/utility_scripts/make_paths_relative.py
@@ -1,5 +1,7 @@
 """Make all paths relative."""
 
+from pathlib import Path
+from itertools import chain
 import bpy
 
 from openpype.lib.log import Logger
@@ -9,6 +11,20 @@ if __name__ == "__main__":
     log.debug(
         f"Blend file | All paths converted to relative: {bpy.data.filepath}"
     )
+    # Resolve path from source filepath with the relative filepath
+    for datablock in chain(bpy.data.libraries, bpy.data.images):
+        try:
+            if (
+                datablock
+                and not datablock.is_library_indirect
+                and not datablock.filepath.startswith("//")
+            ):
+                datablock.filepath = bpy.path.relpath(
+                    str(Path(datablock.filepath).resolve()),
+                    start=str(Path(bpy.data.filepath).parent.resolve()),
+                )
+        except (RuntimeError, ReferenceError, ValueError, OSError) as e:
+            log.error(e)
 
     bpy.ops.file.make_paths_relative()
     bpy.ops.wm.save_mainfile()
