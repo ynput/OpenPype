b47d172944aa1e0039b3fb554093dbc7508b3033
Toke Stuart Jepsen <tokejepsen@gmail.com>
2023-05-15 12:30:32 +0100

    Move family check to process

--- openpype/hosts/maya/plugins/publish/validate_instance_has_members.py
+++ openpype/hosts/maya/plugins/publish/validate_instance_has_members.py
@@ -13,12 +13,6 @@ class ValidateInstanceHasMembers(pyblish.api.InstancePlugin):
 
     @classmethod
     def get_invalid(cls, instance):
-
-        # Allow renderlayer and workfile to be empty
-        skip_families = ["workfile", "renderlayer", "rendersetup"]
-        if instance.data.get("family") in skip_families:
-            return
-
         invalid = list()
         if not instance.data["setMembers"]:
             objectset_name = instance.data['name']
@@ -27,6 +21,10 @@ class ValidateInstanceHasMembers(pyblish.api.InstancePlugin):
         return invalid
 
     def process(self, instance):
+        # Allow renderlayer and workfile to be empty
+        skip_families = ["workfile", "renderlayer", "rendersetup"]
+        if instance.data.get("family") in skip_families:
+            return
 
         invalid = self.get_invalid(instance)
         if invalid:
