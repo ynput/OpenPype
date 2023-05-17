497a97b70d1eef294e0b7d2ea14365f5add380b4
Jakub Jezek <jakubjezek001@gmail.com>
2023-05-17 14:40:47 +0200

    pr comment

--- openpype/hosts/fusion/plugins/create/create_saver.py
+++ openpype/hosts/fusion/plugins/create/create_saver.py
@@ -232,7 +232,7 @@ class CreateSaver(NewCreator):
 
     def _get_frame_range_enum(self):
         frame_range_options = {
-            "asset_db": "From asset database",
+            "asset_db": "Current asset context",
             "render_range": "From viewer render in/out",
             "comp_range": "From composition timeline"
         }
