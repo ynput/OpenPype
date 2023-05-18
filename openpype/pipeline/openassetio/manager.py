5981b8df7f5cc8aa978d71b2a129e660bdc26b4a
Ondrej Samohel <ondrej.samohel@ynput.io>
2023-05-18 11:26:58 +0200

    :art: minor style changes

--- openpype/pipeline/openassetio/manager.py
+++ openpype/pipeline/openassetio/manager.py
@@ -6,6 +6,17 @@ __all__ = ['OpenPypeManager']
 
 
 class OpenPypeManager(ManagerInterface):
+    __reference_prefix = "op:///"
+    __settings = {}
+
+
+    def __init__(self):
+        super().__init__()
+        self.__settings = {}  # TODO: initialize with Settings
+
+    def settings(self, hostSession):
+        return self.__settings.copy()
+
 
     def entityExists(self, entityRefs, context, hostSession):
         pass
@@ -16,10 +27,13 @@ class OpenPypeManager(ManagerInterface):
     def entityDisplayName(self, entityRefs, context, hostSession):
         pass
 
-    def getRelatedReferences(self, entityRefs, relationshipTraitsDatas, context, hostSession, resultTraitSet=None):
+    def getRelatedReferences(
+            self, entityRefs, relationshipTraitsDatas,
+            context, hostSession, resultTraitSet=None):
         pass
 
-    def managementPolicy(self, traitSet, Set=None, p_str=None, *args, **kwargs):
+    def managementPolicy(
+            self, traitSet, Set=None, p_str=None, *args, **kwargs):
         return [TraitsData() for _ in traitSet]
 
     def identifier(self):
@@ -29,4 +43,9 @@ class OpenPypeManager(ManagerInterface):
         return "OpenPype Manager"
 
     def info(self):
-        ...
+        return {
+            constants.kField_EntityReferencesMatchPrefix: self.__reference_prefix  # noqa: E501
+        }
+
+    def isEntityReferenceString(self, someString, hostSession):
+        return someString.startswith(self.__reference_prefix)
