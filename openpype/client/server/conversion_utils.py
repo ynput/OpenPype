0daa5580d974e0f6bc65151139351bc175c43558
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2023-05-18 17:31:00 +0200

    AYON: ISO date format conversion issues (#4981)
    * added arrow to requirements of openpype
    
    * moved 'arrow' from ftrack python 2 dependencies to global
    
    * use arrow in conversion utils for datetime conversion
    
    * skip datetime conversion

--- openpype/client/server/conversion_utils.py
+++ openpype/client/server/conversion_utils.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+import arrow
 import collections
 import json
 
@@ -566,7 +566,7 @@ def convert_v4_version_to_v3(version):
             output_data[dst_key] = version[src_key]
 
     if "createdAt" in version:
-        created_at = datetime.datetime.fromisoformat(version["createdAt"])
+        created_at = arrow.get(version["createdAt"])
         output_data["time"] = created_at.strftime("%Y%m%dT%H%M%SZ")
 
     output["data"] = output_data
