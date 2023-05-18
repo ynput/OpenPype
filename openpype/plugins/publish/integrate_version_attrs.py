e716191b207f796676089bb7d095f580d103da0d
Jakub Trllo <jakub.trllo@gmail.com>
2023-05-18 18:29:50 +0200

    implemented integrator for version attributes

---
+++ openpype/plugins/publish/integrate_version_attrs.py
@@ -0,0 +1,86 @@
+import pyblish.api
+import ayon_api
+
+from openpype import AYON_SERVER_ENABLED
+from openpype.client.operations import OperationsSession
+
+
+class IntegrateVersionAttributes(pyblish.api.ContextPlugin):
+    """Integrate version attributes from predefined key.
+
+    Any integration after 'IntegrateAsset' can fill 'versionAttributes' with
+    attribute key & value to be updated on created version.
+
+    The integration must make sure the attribute is available for the version
+    entity otherwise an error would be raised.
+    """
+
+    label = "Integrate Version Attributes"
+    order = pyblish.api.IntegratorOrder + 0.5
+
+    def process(self, context):
+        available_attributes = ayon_api.get_attributes_for_type("version")
+        skipped_attributes = set()
+        project_name = context.data["projectName"]
+        op_session = OperationsSession()
+        for instance in context:
+            label = self.get_instance_label(instance)
+            version_entity = instance.data.get("versionEntity")
+            if not version_entity:
+                continue
+            attributes = instance.data.get("versionAttributes")
+            if not attributes:
+                self.log.debug((
+                    "Skipping instance {} does not have set attributes"
+                ).format(label))
+                continue
+
+            filtered_attributes = {}
+            for attr, value in attributes.items():
+                if attr not in available_attributes:
+                    skipped_attributes.add(attr)
+                else:
+                    filtered_attributes[attr] = value
+
+            if not filtered_attributes:
+                self.log.debug((
+                    "Skipping instance {} all attributes were filtered"
+                ).format(label))
+                continue
+
+            self.log.debug("Updated attributes on version {} to {}".format(
+                version_entity["_id"], str(filtered_attributes)
+            ))
+            op_session.update_entity(
+                project_name,
+                "version",
+                version_entity["_id"],
+                {"attrib": filtered_attributes}
+            )
+
+        if skipped_attributes:
+            self.log.warning(
+                "Attributes that are not available on a server {}".format(
+                    str(skipped_attributes)
+                )
+            )
+
+        if len(op_session):
+            op_session.commit()
+            self.log.info("Updated version attributes")
+        else:
+            self.log.info("There is nothing to update")
+
+    @staticmethod
+    def get_instance_label(instance):
+        return (
+            instance.data.get("label")
+            or instance.data.get("name")
+            or instance.data.get("subset")
+            or str(instance)
+        )
+
+
+# Discover the plugin only in AYON mode
+if not AYON_SERVER_ENABLED:
+    IntegrateVersionAttributes = object
