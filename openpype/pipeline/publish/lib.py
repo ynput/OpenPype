a73d19b612c6c4d423a8784b0598e71263213c9f
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2023-05-23 18:16:05 +0200

    Publisher: Show instances in report page (#4915)
    * renamed 'validations_widget.py' to 'report_page.py'
    
    * Implemented base logic and widgets for logs
    
    * make one report page
    
    * added missing imports
    
    * added missing constants
    
    * move and rename 'VerticallScrollArea' to 'VerticalScrollArea'
    
    * Validation erro item have id
    
    * use 'ReportPageWidget' in window
    
    * change 'bg-button-hover' key to 'bg-buttons-hover' in style colors
    
    * move publish actions widgets
    
    * Refactored how validation error title is showed
    
    * remove item id from validation error item but add id to group items
    
    * remove margins from actions widget
    
    * shrink publish frame on finished publishing
    
    * fix dash line draw
    
    * add missing styles
    
    * fix dash line in thumbnail widget
    
    * added crash widget and changed layout a little
    
    * added infor overlay message
    
    * export and copy report happens in main window
    
    * fix docstrings
    
    * added per plugin filtering for validation errors
    
    * added implementation of 'FlowLayout'
    
    * actions buttons are in flow layout
    
    * fix actions order
    
    * implemented expanding text edit widget
    
    * expand button has some signals and properties
    
    * description and details are separated widgets
    
    * fix typo
    
    * added constans to '__all__'
    
    * parse icon def is a function
    
    * change layout of widgets
    
    * fix log filtering
    
    * added state icon to instances
    
    * fix pyside6 issues
    
    * implemented 'ClassicExpandBtnLabel' with arrow images
    
    * modified details separator
    
    * added some spacing to layouts
    
    * fix syle of description inputs and progress color
    
    * removed unused import
    
    * add 'is_validation_error' to errored result
    
    * validation error has different icon in logs view
    
    * added plugin name to ValueError if happens
    
    * spacer before detail inputs moved out of detals widget
    
    * fix actions visible in craash report
    
    * ignore pyblish base classes
    
    * filter base plugins in discovery
    
    * use 'is' comparison instead of '__eq__'
    
    * fix action error handling
    
    * Fix handling of 'None' values in comparison
    
    * formatting fix
    
    * Report instance card have same margins as in create mode
    
    * publish instances are grouped by family
    
    * log messages are rstripped

--- openpype/pipeline/publish/lib.py
+++ openpype/pipeline/publish/lib.py
@@ -320,6 +320,14 @@ def publish_plugins_discover(paths=None):
                 continue
 
             for plugin in pyblish.plugin.plugins_from_module(module):
+                # Ignore base plugin classes
+                # NOTE 'pyblish.api.discover' does not ignore them!
+                if (
+                    plugin is pyblish.api.Plugin
+                    or plugin is pyblish.api.ContextPlugin
+                    or plugin is pyblish.api.InstancePlugin
+                ):
+                    continue
                 if not allow_duplicates and plugin.__name__ in plugin_names:
                     result.duplicated_plugins.append(plugin)
                     log.debug("Duplicate plug-in found: %s", plugin)
