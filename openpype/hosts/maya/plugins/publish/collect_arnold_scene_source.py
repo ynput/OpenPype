791dd3ee6eabb4a4cb5ce77c4c366b61c5e92a3b
Toke Stuart Jepsen <tokejepsen@gmail.com>
2023-06-05 15:21:39 +0100

    Debug logging instead of error

--- openpype/hosts/maya/plugins/publish/collect_arnold_scene_source.py
+++ openpype/hosts/maya/plugins/publish/collect_arnold_scene_source.py
@@ -35,18 +35,16 @@ class CollectArnoldSceneSource(pyblish.api.InstancePlugin):
         # camera.
         cameras = cmds.ls(type="camera", long=True)
         renderable = [c for c in cameras if cmds.getAttr("%s.renderable" % c)]
-        if not renderable:
-            raise ValueError(
-                "No renderable cameras found, which is required for "
-                "publishing ASS."
-            )
-        camera = renderable[0]
-        for node in instance.data["contentMembers"]:
-            camera_shapes = cmds.listRelatives(
-                node, shapes=True, type="camera"
-            )
-            if camera_shapes:
-                camera = node
-        instance.data["camera"] = camera
+        if renderable:
+            camera = renderable[0]
+            for node in instance.data["contentMembers"]:
+                camera_shapes = cmds.listRelatives(
+                    node, shapes=True, type="camera"
+                )
+                if camera_shapes:
+                    camera = node
+            instance.data["camera"] = camera
+        else:
+            self.log.debug("No renderable cameraes found.")
 
         self.log.debug("data: {}".format(instance.data))
