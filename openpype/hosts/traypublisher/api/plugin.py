8f275eaf606a63dc232cfac1a8541375893f0bbf
Jakub Trllo <jakub.trllo@gmail.com>
2023-06-14 17:30:03 +0200

    modified simple creator plugin to be able handle version control

--- openpype/hosts/traypublisher/api/plugin.py
+++ openpype/hosts/traypublisher/api/plugin.py
@@ -1,4 +1,16 @@
-from openpype.lib.attribute_definitions import FileDef
+import collections
+
+from openpype.client import (
+    get_assets,
+    get_subsets,
+    get_last_versions,
+)
+from openpype.lib.attribute_definitions import (
+    FileDef,
+    BoolDef,
+    NumberDef,
+    UISeparatorDef,
+)
 from openpype.lib.transcoding import IMAGE_EXTENSIONS, VIDEO_EXTENSIONS
 from openpype.pipeline.create import (
     Creator,
@@ -94,6 +106,7 @@ class TrayPublishCreator(Creator):
 class SettingsCreator(TrayPublishCreator):
     create_allow_context_change = True
     create_allow_thumbnail = True
+    allow_version_control = False
 
     extensions = []
 
@@ -101,8 +114,19 @@ class SettingsCreator(TrayPublishCreator):
         # Pass precreate data to creator attributes
         thumbnail_path = pre_create_data.pop(PRE_CREATE_THUMBNAIL_KEY, None)
 
+        # Fill 'version_to_use' if version control is enabled
+        if self.allow_version_control:
+            asset_name = data["asset"]
+            subset_docs_by_asset_id = self._prepare_next_versions(
+                [asset_name], [subset_name])
+            version = subset_docs_by_asset_id[asset_name].get(subset_name)
+            if version is not None:
+                pre_create_data["version_to_use"] = version
+            data["_previous_last_version"] = version
+
         data["creator_attributes"] = pre_create_data
         data["settings_creator"] = True
+
         # Create new instance
         new_instance = CreatedInstance(self.family, subset_name, data, self)
 
@@ -111,7 +135,152 @@ class SettingsCreator(TrayPublishCreator):
         if thumbnail_path:
             self.set_instance_thumbnail_path(new_instance.id, thumbnail_path)
 
+    def _prepare_next_versions(self, asset_names, subset_names):
+        """Prepare next versions for given asset and subset names.
+
+        Todos:
+            Expect combination of subset names by asset name to avoid
+                unnecessary server calls for unused subsets.
+
+        Args:
+            asset_names (Iterable[str]): Asset names.
+            subset_names (Iterable[str]): Subset names.
+
+        Returns:
+            dict[str, dict[str, int]]: Last versions by asset
+                and subset names.
+        """
+
+        subset_docs_by_asset_id = collections.defaultdict(dict)
+        if not asset_names or not subset_names:
+            return subset_docs_by_asset_id
+
+        asset_docs = get_assets(
+            self.project_name,
+            asset_names=asset_names,
+            fields=["_id", "name"]
+        )
+        asset_names_by_id = {
+            asset_doc["_id"]: asset_doc["name"]
+            for asset_doc in asset_docs
+        }
+        subset_docs = list(get_subsets(
+            self.project_name,
+            asset_ids=asset_names_by_id.keys(),
+            subset_names=subset_names,
+            fields=["_id", "name", "parent"]
+        ))
+
+        subset_ids = {subset_doc["_id"] for subset_doc in subset_docs}
+        last_versions = get_last_versions(
+            self.project_name,
+            subset_ids,
+            fields=["name", "parent"])
+
+        for subset_doc in subset_docs:
+            asset_id = subset_doc["parent"]
+            asset_name = asset_names_by_id[asset_id]
+            subset_name = subset_doc["name"]
+            subset_id = subset_doc["_id"]
+            last_version = last_versions.get(subset_id)
+            version = None
+            if last_version is not None:
+                version = last_version["name"]
+            subset_docs_by_asset_id[asset_name][subset_name] = version + 1
+        return subset_docs_by_asset_id
+
+    def _fill_next_versions(self, instances_data):
+        """Fill next version for instances.
+
+        Instances have also stored previous next version to be able to
+        recognize if user did enter different version. If version was
+        not changed by user, or user set it to '0' the next version will be
+        updated by current database state.
+        """
+
+        filtered_instance_data = []
+        for instance in instances_data:
+            previous_last_version = instance.get("_previous_last_version")
+            creator_attributes = instance["creator_attributes"]
+            use_next_version = creator_attributes.get(
+                "use_next_version", True)
+            version = creator_attributes.get("version_to_use", 0)
+            if (
+                use_next_version
+                or version == 0
+                or version == previous_last_version
+            ):
+                filtered_instance_data.append(instance)
+
+        asset_names = {
+            instance["asset"]
+            for instance in filtered_instance_data}
+        subset_names = {
+            instance["subset"]
+            for instance in filtered_instance_data}
+        subset_docs_by_asset_id = self._prepare_next_versions(
+            asset_names, subset_names
+        )
+        for instance in instances_data:
+            asset_name = instance["asset"]
+            subset_name = instance["subset"]
+            version = subset_docs_by_asset_id[asset_name].get(subset_name)
+            if version is not None:
+                instance["creator_attributes"]["version_to_use"] = version
+            instance["_previous_last_version"] = version
+
+    def collect_instances(self):
+        """Collect instances from host.
+
+        Overriden to be able to manage version control attributes. If version
+        control is disabled, the attributes will be removed from instances,
+        and next versions are filled if is version control enabled.
+        """
+
+        instances_by_identifier = cache_and_get_instances(
+            self, SHARED_DATA_KEY, list_instances
+        )
+        instances = instances_by_identifier[self.identifier]
+        if not instances:
+            return
+
+        if self.allow_version_control:
+            self._fill_next_versions(instances)
+
+        for instance_data in instances:
+            # Make sure that there are not data related to version control
+            #   if plugin does not support it
+            if not self.allow_version_control:
+                instance_data.pop("_previous_last_version", None)
+                creator_attributes = instance_data["creator_attributes"]
+                creator_attributes.pop("version_to_use", None)
+                creator_attributes.pop("use_next_version", None)
+
+            instance = CreatedInstance.from_existing(instance_data, self)
+            self._add_instance_to_context(instance)
+
     def get_instance_attr_defs(self):
+        defs = self.get_pre_create_attr_defs()
+        if self.allow_version_control:
+            defs += [
+                UISeparatorDef(),
+                BoolDef(
+                    "use_next_version",
+                    default=True,
+                    label="Use next version",
+                ),
+                NumberDef(
+                    "version_to_use",
+                    default=1,
+                    minimum=0,
+                    maximum=999,
+                    label="Version to use",
+                )
+            ]
+        return defs
+
+    def get_pre_create_attr_defs(self):
+        # Use same attributes as for instance attributes
         return [
             FileDef(
                 "representation_files",
@@ -132,10 +301,6 @@ class SettingsCreator(TrayPublishCreator):
             )
         ]
 
-    def get_pre_create_attr_defs(self):
-        # Use same attributes as for instance attrobites
-        return self.get_instance_attr_defs()
-
     @classmethod
     def from_settings(cls, item_data):
         identifier = item_data["identifier"]
@@ -155,6 +320,8 @@ class SettingsCreator(TrayPublishCreator):
                 "extensions": item_data["extensions"],
                 "allow_sequences": item_data["allow_sequences"],
                 "allow_multiple_items": item_data["allow_multiple_items"],
-                "default_variants": item_data["default_variants"]
+                "allow_version_control": item_data.get(
+                    "allow_version_control", False),
+                "default_variants": item_data["default_variants"],
             }
         )
