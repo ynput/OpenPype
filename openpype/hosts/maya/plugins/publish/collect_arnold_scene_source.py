89a1d546bde5a8400f2625575e86c04c0fec1d6d
Toke Stuart Jepsen <tokejepsen@gmail.com>
2023-06-21 09:06:02 +0100

    Simplify bug fix.

--- openpype/hosts/maya/plugins/publish/collect_arnold_scene_source.py
+++ openpype/hosts/maya/plugins/publish/collect_arnold_scene_source.py
@@ -18,49 +18,24 @@ class CollectArnoldSceneSource(pyblish.api.InstancePlugin):
         for objset in objsets:
             objset = str(objset)
             members = cmds.sets(objset, query=True)
+            members = cmds.ls(members, long=True)
             if members is None:
                 self.log.warning("Skipped empty instance: \"%s\" " % objset)
                 continue
             if objset.endswith("content_SET"):
-                members = cmds.ls(members, long=True)
-                members_list = []
-                for member in members:
-                    shape = cmds.listRelatives(
-                        member, shapes=True, fullPath=True)
-                    if not shape:
-                        continue
-                    members_list = members + shape
-                    group_name = "|{}".format(member)
-                    if group_name in members_list:
-                        members_list.remove(group_name)
-
                 children = get_all_children(members)
-
-                if members_list:
-                    children.extend(members_list)
-                instance.data["contentMembers"] = children
-                self.log.debug("content members: {}".format(children))
-
+                instance.data["contentMembers"] = children + members
+                self.log.debug(
+                    "content members: {}".format(
+                        instance.data["contentMembers"]
+                    )
+                )
             elif objset.endswith("proxy_SET"):
-                proxy_members = cmds.ls(members, long=True)
-                proxy_list = []
-                for proxy in proxy_members:
-                    shape = cmds.listRelatives(
-                        proxy, shapes=True, fullPath=True)
-                    if not shape:
-                        continue
-                    proxy_list = proxy_members + shape
-                    group_name = "|{}".format(proxy)
-                    if group_name in proxy_list:
-                        proxy_list.remove(group_name)
-
-                set_members = get_all_children(proxy_members)
-                if proxy_list:
-                    set_members.extend(proxy_list)
-
-                instance.data["proxy"] = set_members
-                self.log.debug("proxy members: {}".format(set_members))
-
+                children = get_all_children(cmds.ls(members, long=True))
+                instance.data["proxy"] = children + members
+                self.log.debug(
+                    "proxy members: {}".format(instance.data["proxy"])
+                )
 
         # Use camera in object set if present else default to render globals
         # camera.
