5abbceeace24f3b183507d6d91f8aa7a4cb6bd95
Kayla Man <moonyuet2008@hotmail.com>
2023-06-24 13:55:20 +0800

    hound fix

--- openpype/hosts/max/plugins/publish/validate_animation_timeline.py
+++ openpype/hosts/max/plugins/publish/validate_animation_timeline.py
@@ -28,8 +28,8 @@ class ValidateAnimationTimeline(pyblish.api.InstancePlugin):
         frame_end_handle = frame_range["frameEnd"] + int(
             frame_range["handleEnd"]
         )
-        if rt.animationRange != rt.interval(
-            frame_start_handle, frame_end_handle):
+        if rt.animationRange != rt.interval(frame_start_handle,
+                                            frame_end_handle):
             raise PublishValidationError("Incorrect animation timeline"
                                          "set for preview animation.. "
                                          "\nYou can use repair action to "
