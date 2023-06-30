85b55944bf9b4dccb614d8da0f250e1109032e16
Jakub Trllo <jakub.trllo@gmail.com>
2023-06-30 10:19:08 +0200

    removed duplicated data

--- common/ayon_common/distribution/tests/test_addon_distributtion.py
+++ common/ayon_common/distribution/tests/test_addon_distributtion.py
@@ -75,13 +75,6 @@ def sample_addon_info():
             "1.0.0": {
                 "hasSettings": True,
                 "hasSiteSettings": False,
-                "frontendScopes": {},
-                "clientSourceInfo": [
-                    {
-                        "type": "server",
-                        "filename": "client.zip"
-                    }
-                ],
                 "clientPyproject": {
                     "tool": {
                         "poetry": {
