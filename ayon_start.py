ac113b48ac73d73c04b619fdf9c8d8a3d96f4ea0
Jakub Trllo <jakub.trllo@gmail.com>
2023-07-04 15:16:40 +0200

    modified print messages on issues with bundles

--- ayon_start.py
+++ ayon_start.py
@@ -212,6 +212,7 @@ elif os.getenv("SSL_CERT_FILE") != certifi.where():
 
 from ayon_api import get_base_url
 from ayon_api.constants import SERVER_URL_ENV_KEY, SERVER_API_ENV_KEY
+from ayon_common import is_staging_enabled
 from ayon_common.connection.credentials import (
     ask_to_login_ui,
     add_server,
@@ -327,16 +328,22 @@ def _check_and_update_from_ayon_server():
 
         elif bundle_name:
             _print((
-                f"!!! Requested bundle '{bundle_name}'"
+                f"!!! Requested release bundle '{bundle_name}'"
                 " is not available on server."
             ))
             _print(
-                f"!!! Check if server '{url}' still have bundle available.")
+                "!!! Check if selected release bundle"
+                f" is available on the server '{url}'."
+            )
 
         else:
-            _print("!!! Server does not have defined bundle to use.")
+            mode = "staging" if is_staging_enabled() else "production"
             _print(
-                f"!!! Make sure server '{url}' has set bundle."
+                f"!!! No release bundle is set as {mode} on the AYON server."
+            )
+            _print(
+                "!!! Make sure there is a release bundle set"
+                f" as \"{mode}\" on the AYON server '{url}'."
             )
         sys.exit(1)
 
@@ -395,8 +402,7 @@ def main_cli():
 
     # print info when not running scripts defined in 'silent commands'
     if not SKIP_HEADERS:
-        use_staging = os.environ.get("AYON_USE_STAGING") == "1"
-        info = get_info(use_staging)
+        info = get_info(is_staging_enabled())
         info.insert(0, f">>> Using AYON from [ {AYON_ROOT} ]")
 
         t_width = 20
