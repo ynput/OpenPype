c08bc46cdb48691a74e514bb9b2f3053d944d8da
Jakub Trllo <jakub.trllo@gmail.com>
2023-07-04 18:06:19 +0200

    prepared helper functions to find latest and next versions for instances

---
+++ openpype/pipeline/create/utils.py
@@ -0,0 +1,123 @@
+import collections
+
+from openpype.client import get_assets, get_subsets, get_last_versions
+
+
+def get_last_versions_for_instances(
+    project_name, instances, use_value_for_missing=False
+):
+    """Get last versions for instances by their asset and subset name.
+
+    Args:
+        project_name (str): Project name.
+        instances (list[CreatedInstance]): Instances to get next versions for.
+        use_value_for_missing (Optional[bool]): Missing values are replaced
+            with negative value if True. Otherwise None is used. -2 is used
+            for instances without filled asset or subset name. -1 is used
+            for missing entities.
+
+    Returns:
+        dict[str, Union[int, None]]: Last versions by instance id.
+    """
+
+    output = {
+        instance.id: -1 if use_value_for_missing else None
+        for instance in instances
+    }
+    subset_names_by_asset_name = collections.defaultdict(set)
+    instances_by_hierarchy = {}
+    for instance in instances:
+        asset_name = instance.data.get("asset")
+        subset_name = instance.subset_name
+        if not asset_name or not subset_name:
+            output[instance.id] = -2
+            continue
+
+        (
+            instances_by_hierarchy
+            .setdefault(asset_name, {})
+            .setdefault(subset_name, [])
+            .append(instance)
+        )
+        subset_names_by_asset_name[asset_name].add(subset_name)
+
+    if not subset_names_by_asset_name:
+        return output
+
+    asset_docs = get_assets(
+        project_name,
+        asset_names=subset_names_by_asset_name.keys(),
+        fields=["name", "_id"]
+    )
+    asset_names_by_id = {
+        asset_doc["_id"]: asset_doc["name"]
+        for asset_doc in asset_docs
+    }
+    asset_ids_by_name = {
+        asset_name: asset_id
+        for asset_id, asset_name in asset_names_by_id.items()
+    }
+    subset_names = set()
+    for names in subset_names_by_asset_name.values():
+        subset_names |= names
+
+    if not asset_ids_by_name or not subset_names:
+        return output
+
+    subset_docs = get_subsets(
+        project_name,
+        asset_ids=asset_ids_by_name.values(),
+        subset_names=subset_names,
+        fields=["_id", "name", "parent"]
+    )
+    subset_docs_by_asset_id = collections.defaultdict(list)
+    subset_docs_by_id = {}
+    for subset_doc in subset_docs:
+        # Filter subset docs by subset names under parent
+        asset_id = subset_doc["parent"]
+        asset_name = asset_names_by_id[asset_id]
+        subset_name = subset_doc["name"]
+        if subset_name not in subset_names_by_asset_name[asset_name]:
+            continue
+        subset_docs_by_id[subset_doc["_id"]] = subset_doc
+        subset_docs_by_asset_id[asset_id].append(subset_doc)
+
+    last_versions_by_subset_id = get_last_versions(
+        project_name,
+        subset_docs_by_id.keys(),
+        fields=["name", "parent"]
+    )
+    for subset_id, version_doc in last_versions_by_subset_id.items():
+        subset_doc = subset_docs_by_id[subset_id]
+        asset_id = subset_doc["parent"]
+        asset_name = asset_names_by_id[asset_id]
+        _instances = instances_by_hierarchy[asset_name][subset_doc["name"]]
+        for instance in _instances:
+            output[instance.id] = version_doc["name"]
+
+    return output
+
+
+def get_next_versions_for_instances(project_name, instances):
+    """Get next versions for instances by their asset and subset name.
+
+    Args:
+        project_name (str): Project name.
+        instances (list[CreatedInstance]): Instances to get next versions for.
+
+    Returns:
+        dict[str, int]: Next versions by instance id.
+    """
+
+    last_versions = get_last_versions_for_instances(
+        project_name, instances, True)
+
+    output = {}
+    for instance_id, version in last_versions.items():
+        if version == -2:
+            output[instance_id] = None
+        elif version == -1:
+            output[instance_id] = 1
+        else:
+            output[instance_id] = version + 1
+    return output
