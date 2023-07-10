bc0175c849043c2397e779530bc376711a3cec34
Laroustine <justin.blard.dev@gmail.com>
2023-07-10 10:18:56 +0200

    [REVERT] Remove adding of os at line 664

--- openpype/pipeline/create/creator_plugins.py
+++ openpype/pipeline/create/creator_plugins.py
@@ -661,7 +661,6 @@ def discover_convertor_plugins(*args, **kwargs):
 
 def discover_legacy_creator_plugins():
     from openpype.lib import Logger
-    import os
 
     log = Logger.get_logger("CreatorDiscover")
 
