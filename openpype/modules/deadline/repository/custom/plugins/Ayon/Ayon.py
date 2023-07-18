87700f72dccc836376b036b1dc4aef9191d6983a
Petr Kalis <petr.kalis@gmail.com>
2023-07-18 19:48:03 +0200

    OP-4845 - injecting required env vars for ayon_console
    Renamed OP to Ayon

--- openpype/modules/deadline/repository/custom/plugins/Ayon/Ayon.py
+++ openpype/modules/deadline/repository/custom/plugins/Ayon/Ayon.py
@@ -21,18 +21,18 @@ import platform
 # main DeadlinePlugin class.
 ######################################################################
 def GetDeadlinePlugin():
-    return OpenPypeDeadlinePlugin()
+    return AyonDeadlinePlugin()
 
 
 def CleanupDeadlinePlugin(deadlinePlugin):
     deadlinePlugin.Cleanup()
 
 
-class OpenPypeDeadlinePlugin(DeadlinePlugin):
+class AyonDeadlinePlugin(DeadlinePlugin):
     """
-        Standalone plugin for publishing from OpenPype.
+        Standalone plugin for publishing from Ayon
 
-        Calls OpenPype executable 'openpype_console' from first correctly found
+        Calls Ayonexecutable 'ayon_console' from first correctly found
         file based on plugin configuration. Uses 'publish' command and passes
         path to metadata json file, which contains all needed information
         for publish process.
@@ -61,124 +61,40 @@ class OpenPypeDeadlinePlugin(DeadlinePlugin):
         self.AddStdoutHandlerCallback(
             ".*Progress: (\d+)%.*").HandleCallback += self.HandleProgress
 
-    @staticmethod
-    def get_openpype_version_from_path(path, build=True):
-        """Get OpenPype version from provided path.
-             path (str): Path to scan.
-             build (bool, optional): Get only builds, not sources
-
-        Returns:
-            str or None: version of OpenPype if found.
-
-        """
-        # fix path for application bundle on macos
-        if platform.system().lower() == "darwin":
-            path = os.path.join(path, "MacOS")
-
-        version_file = os.path.join(path, "openpype", "version.py")
-        if not os.path.isfile(version_file):
-            return None
-
-        # skip if the version is not build
-        exe = os.path.join(path, "openpype_console.exe")
-        if platform.system().lower() in ["linux", "darwin"]:
-            exe = os.path.join(path, "openpype_console")
-
-        # if only builds are requested
-        if build and not os.path.isfile(exe):  # noqa: E501
-            print(f"   ! path is not a build: {path}")
-            return None
-
-        version = {}
-        with open(version_file, "r") as vf:
-            exec(vf.read(), version)
-
-        version_match = re.search(r"(\d+\.\d+.\d+).*", version["__version__"])
-        return version_match[1]
-
     def RenderExecutable(self):
         job = self.GetJob()
-        openpype_versions = []
-        # if the job requires specific OpenPype version,
-        # lets go over all available and find compatible build.
-        requested_version = job.GetJobEnvironmentKeyValue("OPENPYPE_VERSION")
-        if requested_version:
-            self.LogInfo((
-                "Scanning for compatible requested "
-                f"version {requested_version}"))
-            dir_list = self.GetConfigEntry("OpenPypeInstallationDirs")
 
-            # clean '\ ' for MacOS pasting
-            if platform.system().lower() == "darwin":
-                dir_list = dir_list.replace("\\ ", " ")
+        # set required env vars for Ayon
+        # cannot be in InitializeProcess as it is too soon
+        config = RepositoryUtils.GetPluginConfig("Ayon")
+        ayon_server_url = (
+                job.GetJobEnvironmentKeyValue("AYON_SERVER_URL") or
+                config.GetConfigEntryWithDefault("AyonServerUrl", "")
+        )
+        ayon_api_key = (
+                job.GetJobEnvironmentKeyValue("AYON_API_KEY") or
+                config.GetConfigEntryWithDefault("AyonApiKey", "")
+        )
+        ayon_bundle_name = job.GetJobEnvironmentKeyValue("AYON_BUNDLE_NAME")
 
-            for dir_list in dir_list.split(","):
-                install_dir = DirectoryUtils.SearchDirectoryList(dir_list)
-                if install_dir:
-                    sub_dirs = [
-                        f.path for f in os.scandir(install_dir)
-                        if f.is_dir()
-                    ]
-                    for subdir in sub_dirs:
-                        version = self.get_openpype_version_from_path(subdir)
-                        if not version:
-                            continue
-                        openpype_versions.append((version, subdir))
+        environment = {
+            "AYON_SERVER_URL": ayon_server_url,
+            "AYON_API_KEY": ayon_api_key,
+            "AYON_BUNDLE_NAME": ayon_bundle_name,
+        }
 
-        exe_list = self.GetConfigEntry("OpenPypeExecutable")
+        for env, val in environment.items():
+            self.SetProcessEnvironmentVariable(env, val)
+
+        exe_list = self.GetConfigEntry("AyonExecutable")
         # clean '\ ' for MacOS pasting
         if platform.system().lower() == "darwin":
             exe_list = exe_list.replace("\\ ", " ")
         exe = FileUtils.SearchFileList(exe_list)
-        if openpype_versions:
-            # if looking for requested compatible version,
-            # add the implicitly specified to the list too.
-            version = self.get_openpype_version_from_path(
-                os.path.dirname(exe))
-            if version:
-                openpype_versions.append((version, os.path.dirname(exe)))
-
-        if requested_version:
-            # sort detected versions
-            if openpype_versions:
-                openpype_versions.sort(
-                    key=lambda ver: [
-                        int(t) if t.isdigit() else t.lower()
-                        for t in re.split(r"(\d+)", ver[0])
-                    ])
-            requested_major, requested_minor, _ = requested_version.split(".")[:3]  # noqa: E501
-            compatible_versions = []
-            for version in openpype_versions:
-                v = version[0].split(".")[:3]
-                if v[0] == requested_major and v[1] == requested_minor:
-                    compatible_versions.append(version)
-            if not compatible_versions:
-                self.FailRender(("Cannot find compatible version available "
-                                 "for version {} requested by the job. "
-                                 "Please add it through plugin configuration "
-                                 "in Deadline or install it to configured "
-                                 "directory.").format(requested_version))
-            # sort compatible versions nad pick the last one
-            compatible_versions.sort(
-                key=lambda ver: [
-                    int(t) if t.isdigit() else t.lower()
-                    for t in re.split(r"(\d+)", ver[0])
-                ])
-            # create list of executables for different platform and let
-            # Deadline decide.
-            exe_list = [
-                os.path.join(
-                    compatible_versions[-1][1], "openpype_console.exe"),
-                os.path.join(
-                    compatible_versions[-1][1], "openpype_console"),
-                os.path.join(
-                    compatible_versions[-1][1], "MacOS", "openpype_console")
-            ]
-            exe = FileUtils.SearchFileList(";".join(exe_list))
 
         if exe == "":
             self.FailRender(
-                "OpenPype executable was not found " +
+                "Ayon executable was not found " +
                 "in the semicolon separated list " +
                 "\"" + ";".join(exe_list) + "\". " +
                 "The path to the render executable can be configured " +
