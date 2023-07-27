a9eaa68ac60e783691806d172056251305f4a961
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2023-07-27 14:42:21 +0200

    AYON: Remove AYON launch logic from OpenPype (#5348)
    * removed AYON launch logic from OpenPype
    
    * updated ayon api to 0.3.3
    
    * removed common from include files
    
    ---------
    
    Co-authored-by: 64qam <dvorak.peta@gmail.com>

--- common/ayon_common/distribution/__init__.py
+++
@@ -1,9 +0,0 @@
-from .control import AyonDistribution, BundleNotFoundError
-from .utils import show_missing_bundle_information
-
-
-__all__ = (
-    "AyonDistribution",
-    "BundleNotFoundError",
-    "show_missing_bundle_information",
-)
