5a9281823d3d18cc9857875b796f7e65d9c747f1
Mustafa-Zarkash <mustafataherzaky@outlook.com>
2023-07-28 00:06:55 +0300

    update create_fbx

--- openpype/hosts/houdini/plugins/create/create_fbx.py
+++ openpype/hosts/houdini/plugins/create/create_fbx.py
@@ -12,22 +12,60 @@ class CreateFilmboxFBX(plugin.HoudiniCreator):
     family = "filmboxfbx"
     icon = "fa5s.cubes"
 
+    # Overrides HoudiniCreator.create()
     def create(self, subset_name, instance_data, pre_create_data):
-        instance_data.pop("active", None)
+        # instance_data.pop("active", None)
+
+        # set node type
         instance_data.update({"node_type": "filmboxfbx"})
 
+        # set essential extra parameters
         instance = super(CreateFilmboxFBX, self).create(
             subset_name,
             instance_data,
             pre_create_data)
 
+        # get the created node
         instance_node = hou.node(instance.get("instance_node"))
+
+        # get output path
         output_path = hou.text.expandString(
             "$HIP/pyblish/{}.fbx".format(subset_name))
 
+        # get selection
+        selection = self.get_selection()
+
+        # parms dictionary
         parms = {
+            "startnode" : selection,
             "sopoutput": output_path
         }
+
+        # set parms
+        instance_node.setParms(parms)
+
+        # Lock any parameters in this list
+        to_lock = []
+        self.lock_parameters(instance_node, to_lock)
+
+    # Overrides HoudiniCreator.get_network_categories()
+    def get_network_categories(self):
+        return [
+            hou.ropNodeTypeCategory(),
+            hou.sopNodeTypeCategory()
+        ]
+
+    def get_selection(self):
+        """Selection Logic.
+
+        how self.selected_nodes should be processed to get
+        the desirable node from selection.
+
+        Returns:
+            str : node path
+        """
+
+        selection = ""
 
         if self.selected_nodes:
             selected_node = self.selected_nodes[0]
@@ -37,7 +75,7 @@ class CreateFilmboxFBX(plugin.HoudiniCreator):
 
             # Allow sop level paths (e.g. /obj/geo1/box1)
             if isinstance(selected_node, hou.SopNode):
-                parms["startnode"] = selected_node.path()
+                selection = selected_node.path()
                 self.log.debug(
                     "Valid SopNode selection, 'Export' in filmboxfbx"
                     " will be set to '%s'."
@@ -54,14 +92,14 @@ class CreateFilmboxFBX(plugin.HoudiniCreator):
                 sop_path = self.get_obj_output(selected_node)
 
                 if sop_path:
-                    parms["startnode"] = sop_path.path()
+                    selection = sop_path.path()
                     self.log.debug(
                         "Valid ObjNode selection, 'Export' in filmboxfbx "
                         "will be set to the child path '%s'."
                         % sop_path
                     )
 
-            if not parms.get("startnode", None):
+            if not selection:
                 self.log.debug(
                     "Selection isn't valid. 'Export' in filmboxfbx will be empty."
                 )
@@ -70,20 +108,12 @@ class CreateFilmboxFBX(plugin.HoudiniCreator):
                 "No Selection. 'Export' in filmboxfbx will be empty."
             )
 
-        instance_node.setParms(parms)
-
-        # Lock any parameters in this list
-        to_lock = []
-        self.lock_parameters(instance_node, to_lock)
-
-    def get_network_categories(self):
-        return [
-            hou.ropNodeTypeCategory(),
-            hou.sopNodeTypeCategory()
-        ]
+        return selection
 
     def get_obj_output(self, obj_node):
-        """Find output node with the smallest 'outputidx'."""
+        """Find output node with the smallest 'outputidx'
+        or return tje node with the render flag instead.
+        """
 
         outputs = obj_node.subnetOutputs()
 
