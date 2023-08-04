f357a8633ce605f0cba8db867b1b3f5d34f6c36c
Thomas Fricard <tfricard@ws114.prs.vfx.int>
2023-08-04 10:00:44 +0200

    add warning message if target task is different from current task

--- openpype/tools/workfiles/save_as_dialog.py
+++ openpype/tools/workfiles/save_as_dialog.py
@@ -9,6 +9,7 @@ from openpype.pipeline import (
     registered_host,
     legacy_io,
 )
+from openpype.pipeline.context_tools import get_current_task_name
 from openpype.pipeline.workfile import get_last_workfile_with_version
 from openpype.pipeline.template_data import get_template_data_with_names
 from openpype.tools.utils import PlaceholderLineEdit
@@ -234,6 +235,14 @@ class SaveAsDialog(QtWidgets.QDialog):
 
         # Preview widget
         preview_label = QtWidgets.QLabel("Preview filename", inputs_widget)
+        current_task_name = get_current_task_name()
+        target_task_name = self.data.get("task").get("name")
+        warning_label = QtWidgets.QLabel(
+            "<font color='red'>Warning: You are saving to a different task "
+            "than the current one. "
+            "Current task: {}. Target task: {}"
+            "</font>".format(current_task_name, target_task_name)
+        )
 
         # Subversion input
         subversion = SubversionLineEdit(inputs_widget)
@@ -281,6 +290,8 @@ class SaveAsDialog(QtWidgets.QDialog):
             subversion.setVisible(False)
         inputs_layout.addRow("Extension:", ext_combo)
         inputs_layout.addRow("Preview:", preview_label)
+        if current_task_name != target_task_name:
+            inputs_layout.addRow(warning_label)
 
         # Build layout
         main_layout = QtWidgets.QVBoxLayout(self)
@@ -315,6 +326,7 @@ class SaveAsDialog(QtWidgets.QDialog):
         self.last_version_check = last_version_check
 
         self.preview_label = preview_label
+        self.warning_label = warning_label
         self.subversion = subversion
         self.ext_combo = ext_combo
         self._ext_delegate = ext_delegate
