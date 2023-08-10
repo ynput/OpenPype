f810d42c18b2a2bd0188bfbdc52f2a0abc1b3684
Simone Barbieri <simone.barbieri@hey.com>
2023-08-10 09:59:00 +0100

    Implemented suggestions
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- openpype/plugins/actions/open_file_explorer.py
+++ openpype/plugins/actions/open_file_explorer.py
@@ -1,7 +1,6 @@
 import os
 import platform
 import subprocess
-import getpass
 
 from openpype.client import (
     get_project,
@@ -11,6 +10,7 @@ from openpype.pipeline import (
     Anatomy,
     LauncherAction,
 )
+from openpype.pipeline.template_data import get_template_data
 
 
 class OpenTaskPath(LauncherAction):
@@ -29,40 +29,30 @@ class OpenTaskPath(LauncherAction):
         project_name = session["AVALON_PROJECT"]
         asset_name = session.get("AVALON_ASSET", None)
         task_name = session.get("AVALON_TASK", None)
-        host_name = None    # never present in session
 
         project = get_project(project_name)
         asset = get_asset_by_name(project_name, asset_name)
 
-        data = self.get_workdir_data(project_doc=project,
-                                     asset_doc=asset,
-                                     task_name=task_name,
-                                     host_name=host_name)
+        data = get_template_data(project, asset, task_name)
+
         anatomy = Anatomy(project_name)
-        result = anatomy.format(data)
+        workdir = anatomy.templates_obj["work"]["folder"].format(data)
 
-        # todo: implement custom template keys instead of 'work'
-        # Get template key
-        # template_key = get_workfile_template_key_from_context(
-        #    asset_name, task_name, host_name, project_name, dbcon
-        # )
-        workdir = result["work"]["folder"]
+        valid_workdir = workdir.split("{", 1)[0]
 
-        # Keep only the part of the path that was formatted by splitting up to
-        # the first stub - we can only explore up to there
-        STUB = "<<NULL>>"
-        valid_dir = workdir.split(STUB, 1)[0]
+        # Path is not filled
+        if not valid_workdir:
+            return
 
-        # If the path endswith `/work` and the path does not exist but the
-        # parent does then we allow to go to the parent because it could for
-        # example be the hierarchical folder for `asset` in e.g. `asset/hero`
-        if not os.path.exists(valid_dir) and valid_dir.endswith("/work/"):
-            # /folder/ dirname is /folder so we rstrip the last / to be sure
-            parent_dir = os.path.dirname(valid_dir.rstrip("/"))
-            if os.path.exists(parent_dir):
-                valid_dir = parent_dir
+        # Normalize
+        valid_workdir = os.path.normpath(valid_workdir)
+        while not os.path.exists(valid_workdir):
+            prev_workdir = valid_workdir
+            valid_workdir = os.path.dirname(prev_workdir)
+            if not valid_workdir or valid_workdir == prev_workdir:
+                return None
 
-        path = os.path.normpath(valid_dir)
+        path = valid_workdir
 
         app = QtWidgets.QApplication.instance()
         ctrl_pressed = QtCore.Qt.ControlModifier & app.keyboardModifiers()
@@ -71,73 +61,6 @@ class OpenTaskPath(LauncherAction):
             self.copy_path_to_clipboard(path)
         else:
             self.open_in_explorer(path)
-
-    def get_workdir_data(self, project_doc, asset_doc, task_name, host_name):
-        """Mimic `openpype.lib.get_workdir_data` but require less data.
-
-        This allows for example to have only a valid `project_doc` passed in
-        and the rest being None. For all that is None a stub placeholder data
-        value will be returned.
-
-        Returns:
-            dict: Workdir data.
-
-        """
-
-        # Start with mostly stub placeholder data where we cannot match
-        # `openpype.lib.get_workdir_data` due to lack of input variables.
-        STUB = "<<NULL>>"
-        data = {
-            "project": {
-                "name": project_doc["name"],
-                "code": project_doc["data"].get("code")
-            },
-            "task": {
-                "name": STUB,
-                "type": STUB,
-                "short": STUB,
-            },
-            "asset": STUB,
-            "parent": STUB,
-            "hierarchy": STUB,
-            "app": STUB,
-            "user": getpass.getuser()
-        }
-
-        # Retrieve data similar to `openpype.lib.get_workdir_data` but only
-        # up to where we can. First using AVALON_ASSET in session.
-        if asset_doc:
-            asset_parents = asset_doc["data"]["parents"]
-            hierarchy = "/".join(asset_parents)
-
-            parent_name = project_doc["name"]
-            if asset_parents:
-                parent_name = asset_parents[-1]
-
-            # Insert asset data
-            data.update({
-                "asset": asset_doc["name"],
-                "parent": parent_name,
-                "hierarchy": hierarchy,
-            })
-
-        # Then insert task data when AVALON_TASK in session
-        if asset_doc and task_name is not None:
-            asset_tasks = asset_doc['data']['tasks']
-            task_type = asset_tasks.get(task_name, {}).get('type')
-            project_task_types = project_doc["config"]["tasks"]
-            task_code = project_task_types.get(task_type, {}).get(
-                "short_name"
-            )
-
-            # Insert task data
-            data["task"].update({
-                "name": task_name,
-                "type": task_type,
-                "code": task_code
-            })
-
-        return data
 
     @staticmethod
     def open_in_explorer(path):
