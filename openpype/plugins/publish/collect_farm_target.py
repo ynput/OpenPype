97ef1ed22709dfa41b20af44b4e87ea7472f6aa7
Jakub Jezek <jakubjezek001@gmail.com>
2023-08-14 10:26:24 +0200

    global farm target collector

--- openpype/plugins/publish/collect_farm_target.py
+++ openpype/plugins/publish/collect_farm_target.py
@@ -22,3 +22,20 @@ class CollectFarmTargetInstance(pyblish.api.InstancePlugin):
         except AttributeError:
             self.log.error("Cannot get OpenPype Deadline module.")
             raise AssertionError("OpenPype Deadline module not found.")
+
+        try:
+            royalrender_module = \
+                context.data.get("openPypeModules")["royalrender"]
+            instance.data["toBeRenderedOn"] = "royalrender"
+            self.log.debug("Collected render target: royalrender")
+        except AttributeError:
+            self.log.error("Cannot get OpenPype RoyalRender module.")
+            raise AssertionError("OpenPype RoyalRender module not found.")
+
+        try:
+            muster_module = context.data.get("openPypeModules")["muster"]
+            instance.data["toBeRenderedOn"] = "muster"
+            self.log.debug("Collected render target: muster")
+        except AttributeError:
+            self.log.error("Cannot get OpenPype Muster module.")
+            raise AssertionError("OpenPype Muster module not found.")
