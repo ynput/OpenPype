a66ba2a1886c6c3ce3909a7a5049959e0def67d4
Jakub Trllo <jakub.trllo@gmail.com>
2023-08-22 16:06:52 +0200

    modified abstract controller

--- openpype/tools/ayon_workfiles/abstract.py
+++ openpype/tools/ayon_workfiles/abstract.py
@@ -5,34 +5,6 @@ import six
 
 @six.add_metaclass(ABCMeta)
 class AbstractWorkfileController(object):
-    @property
-    @abstractmethod
-    def log(self):
-        """Controller's logger object.
-
-        Returns:
-            logging.Logger: Logger object that can be used for logging.
-        """
-
-        pass
-
-    @property
-    @abstractmethod
-    def event_system(self):
-        """Inner event system for publisher controller."""
-
-        pass
-
-    def emit_event(self, topic, data=None, source=None):
-        """Use implemented event system to trigger event."""
-
-        if data is None:
-            data = {}
-        self.event_system.emit(topic, data, source)
-
-    def register_event_callback(self, topic, callback):
-        self.event_system.add_callback(topic, callback)
-
     # Host information
     @abstractmethod
     def get_workfile_extensions(self):
@@ -52,25 +24,16 @@ class AbstractWorkfileController(object):
     def get_current_project_name(self):
         pass
 
-    @property
-    @abstractmethod
-    def project_anatomy(self):
-        """Anatomy object for current project.
-
-        Todos:
-            Rename to 'project_anatomy'.
-
-        Returns:
-            Anatomy: Project anatomy.
-        """
-        pass
-
     @abstractmethod
     def get_current_folder_id(self):
         pass
 
     @abstractmethod
     def get_current_task_name(self):
+        pass
+
+    @abstractmethod
+    def get_current_workfile(self):
         pass
 
     # Selection information
@@ -162,6 +125,10 @@ class AbstractWorkfileController(object):
 
     @abstractmethod
     def get_workarea_file_items(self, folder_id, task_id):
+        pass
+
+    @abstractmethod
+    def get_workarea_save_as_data(self, folder_id, task_id):
         pass
 
     @abstractmethod
