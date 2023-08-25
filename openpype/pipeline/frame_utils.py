d41f66d52f4f8a1e22dccf91e7bda4f962887e5e
Jakub Jezek <jakubjezek001@gmail.com>
2023-08-25 14:14:27 +0200

    fixing code after suggestion commit

--- openpype/pipeline/frame_utils.py
+++ openpype/pipeline/frame_utils.py
@@ -5,6 +5,7 @@ This module contains utility functions dedicated to frame range operations
 import clique
 
 from openpype.lib import Logger
+from openpype.client import get_asset_by_name
 
 log = Logger.get_logger(__name__)
 
@@ -27,15 +28,12 @@ def get_asset_frame_range(project_name, asset_name, asset_doc=None):
     """Get the current assets frame range and handles.
 
     Args:
-        asset_name Optional[str]: Name of asset used for filter.
-        asset_id Optional[Union[str, ObjectId]]: Asset document id.
-            If entered then is used as only filter.
-        fields Optional[Union[List[str], None]]: Limit returned data
-            of asset documents to specific keys.
+        project_name (str): Name of project.
+        asset_name (str): Name of asset.
+        asset_doc (Optional[dict]): Asset document.
     Returns:
         dict: with frame start, frame end, handle start, handle end.
     """
-    from openpype.pipeline.context_tools import get_current_project_asset
 
     # Set frame start/end
     if not asset_doc:
@@ -49,14 +47,15 @@ def get_asset_frame_range(project_name, asset_name, asset_doc=None):
                 "data.handleEnd"
             ]
         )
-    frame_start = asset["data"].get("frameStart")
-    frame_end = asset["data"].get("frameEnd")
+
+    frame_start = asset_doc["data"].get("frameStart")
+    frame_end = asset_doc["data"].get("frameEnd")
 
     if frame_start is None or frame_end is None:
         return
 
-    handle_start = asset["data"].get("handleStart", 0)
-    handle_end = asset["data"].get("handleEnd", 0)
+    handle_start = asset_doc["data"].get("handleStart", 0)
+    handle_end = asset_doc["data"].get("handleEnd", 0)
 
     return {
         "frameStart": frame_start,
