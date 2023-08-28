ba237487a65176f2ee58e08533c65115b5a9c19d
Jakub Jezek <jakubjezek001@gmail.com>
2023-08-28 15:53:36 +0200

    cashing OCIO config version data

--- openpype/pipeline/colorspace.py
+++ openpype/pipeline/colorspace.py
@@ -23,6 +23,7 @@ log = Logger.get_logger(__name__)
 class CashedData:
     remapping = None
     python3compatible = None
+    config_version_data = None
 
 
 class DeprecatedWarning(DeprecationWarning):
@@ -395,16 +396,17 @@ def compatibility_check_config_version(config_path, major=1, minor=None):
             "config", "get_version", config_path=config_path
         )
 
-    from openpype.scripts.ocio_wrapper import _get_version_data
+    if not CashedData.config_version_data:
+        from openpype.scripts.ocio_wrapper import _get_version_data
 
-    version_data = _get_version_data(config_path)
+        CashedData.config_version_data = _get_version_data(config_path)
 
     # check major version
-    if version_data["major"] != major:
+    if CashedData.config_version_data["major"] != major:
         return False
 
     # check minor version
-    if minor and version_data["minor"] != minor:
+    if minor and CashedData.config_version_data["minor"] != minor:
         return False
 
     # compatible
