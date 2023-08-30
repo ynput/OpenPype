42033a02946ee2e3480365613eef39ad1b7f3881
Kayla Man <moonyuet2008@hotmail.com>
2023-08-30 21:09:12 +0800

    remove unnecessary codes

--- openpype/hosts/max/plugins/create/create_render.py
+++ openpype/hosts/max/plugins/create/create_render.py
@@ -35,8 +35,6 @@ class CreateRender(plugin.MaxCreator):
         # set output paths for rendering(mandatory for deadline)
         RenderSettings().render_output(container_name)
         ocio_display = instance.data.get("ocio_display")
-        if ocio_display:
-            self.ocio_display = ocio_display
 
     def get_pre_create_attr_defs(self):
         attrs = super(CreateRender, self).get_pre_create_attr_defs()
