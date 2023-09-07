e85c818c2c9eada178764d6816d92c4c87e081db
Petr Kalis <petr.kalis@gmail.com>
2023-09-07 11:08:42 +0200

    OP-6345 - added validator if footage files exist
    Comp could contain multiple FootageItems, eg imported file(s). If file is missing render triggered by jsx fails silently.

---
+++ openpype/hosts/aftereffects/plugins/publish/validate_footage_items.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""Validate presence of footage items in composition
+Requires:
+"""
+import os
+import re
+
+import pyblish.api
+
+from openpype.pipeline import (
+    PublishXmlValidationError,
+    OptionalPyblishPluginMixin
+)
+from openpype.hosts.aftereffects.api import get_stub
+
+
+class ValidateFootageItems(OptionalPyblishPluginMixin,
+                           pyblish.api.InstancePlugin):
+    """
+        Validates if FootageItems contained in composition exist.
+
+    AE fails silently and doesn't render anything. This validator tries to
+    check existence of files.
+    It will not protect from missing frame though (as AE api doesn't provide
+    this information and it cannot be told how many frames should be there
+    easily). Missing frame is replaced by placeholder.
+    """
+
+    order = pyblish.api.ValidatorOrder
+    label = "Validate Footage Items"
+    families = ["render.farm", "render.local", "render"]
+    hosts = ["aftereffects"]
+    optional = True
+
+
+    def process(self, instance):
+        """Plugin entry point."""
+        # Skip the instance if is not active by data on the instance
+        if not self.is_active(instance.data):
+            return
+
+        comp_id = instance.data["comp_id"]
+        for footage_item in get_stub().get_items(comps=False, folders=False,
+                                                 footages=True):
+            self.log.info(footage_item)
+            if comp_id not in footage_item.containing_comps:
+                continue
+
+            path = footage_item.path
+            if path and not os.path.exists(path):
+
+                msg = f"File {path} not found."
+                formatting = {"name": "comp", "path": path}
+                raise PublishXmlValidationError(self, msg,
+                                                formatting_data=formatting)
