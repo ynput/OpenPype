636554c49e2f0f7f2717583145caad45588d3a9e
kalisp <petr.kalis@gmail.com>
2023-09-08 16:59:17 +0200

    OP-6763 - added explicit local plugin
    Automated plugin has different logic, local would need to handle if auto_image is disabled by artist

---
+++ openpype/hosts/photoshop/plugins/publish/collect_auto_image_refresh.py
@@ -0,0 +1,26 @@
+import pyblish.api
+
+from openpype.hosts.photoshop import api as photoshop
+from openpype.pipeline.create import get_subset_name
+
+
+class CollectAutoImageRefresh(pyblish.api.ContextPlugin):
+    """Refreshes auto_image instance with currently visible layers..
+    """
+
+    label = "Collect Auto Image Refresh"
+    order = pyblish.api.CollectorOrder
+    hosts = ["photoshop"]
+    order = pyblish.api.CollectorOrder + 0.2
+
+
+    def process(self, context):
+        for instance in context:
+            creator_identifier = instance.data.get("creator_identifier")
+            if creator_identifier and creator_identifier == "auto_image":
+                self.log.debug("Auto image instance found, won't create new")
+                # refresh existing auto image instance with current visible
+                publishable_ids = [layer.id for layer in photoshop.stub().get_layers()  # noqa
+                                   if layer.visible]
+                instance.data["ids"] = publishable_ids
+                return
