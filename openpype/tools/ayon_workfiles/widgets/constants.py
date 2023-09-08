076d16a50d21ceb1edfbc806b20554a971c01b40
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2023-09-08 13:45:00 +0200

    Workfiles tool: Refactor workfiles tool (for AYON) (#5550)
    * ayon workfiles tool initial commit
    
    * separated models into smaller files
    
    * workfile can be listed and opened
    
    * added browse logic
    
    * added TODO for helper functions
    
    * modified abstract controller
    
    * implemented required methods
    
    * base of save dialog
    
    * added project settings to controller
    
    * set context of side panel on init
    
    * implemented save as dialog
    
    * cleanup expected selection
    
    * unify controller variable name
    
    * base of published workfiles
    
    * working published workfile copy
    
    * added more missing features from workfiles tool
    
    * Changed size policy of buttons to fill space vertically
    
    * added overlay messages
    
    * moved objects to abstraction
    
    * moved 'window.py' to widgets
    
    * small modifications in widgets
    
    * get_workfile_info returns object
    
    * filled docstrings in abstractions
    
    * finishing touches
    
    * backwards compatible work with host
    
    * close window on successfull open
    
    * remove indentation completelly
    
    * added style for overlay label
    
    * added handling of invalid host in controller
    
    * added overlay with message if host is not valid
    
    * added missing feature of disabled save
    
    * use ayon_workfiles in ayon mode
    
    * cleanup
    
    * hound fixes
    
    * use asset doc for 'change_current_context'
    
    * added duplication action
    
    * removed unused attributes and methods
    
    * refresh workarea view on save as finished
    
    * support host integrations without 'HostBase'
    
    * fix 'filepath' fill
    
    * reset item cache on save
    
    * do not handle filepath in prepare workfile
    
    * rename '_create_workfile_doc' > '_create_workfile_info_entity'
    
    * fill comment before formatting
    
    * fix column count by not calling 'clear'
    
    * more explicit name of method
    
    * use 'setHeaderData' to define header labels
    
    * mimic changes from workarea widget in published widget

---
+++ openpype/tools/ayon_workfiles/widgets/constants.py
@@ -0,0 +1,7 @@
+from qtpy import QtCore
+
+
+ITEM_ID_ROLE = QtCore.Qt.UserRole + 1
+PARENT_ID_ROLE = QtCore.Qt.UserRole + 2
+ITEM_NAME_ROLE = QtCore.Qt.UserRole + 3
+TASK_TYPE_ROLE = QtCore.Qt.UserRole + 4
