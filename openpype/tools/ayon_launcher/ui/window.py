95aefc29cbbbc30ef1b13eafc3adca0d985230c3
Jakub Trllo <jakub.trllo@gmail.com>
2023-09-12 11:54:36 +0200

    added page slide animation

--- openpype/tools/ayon_launcher/ui/window.py
+++ openpype/tools/ayon_launcher/ui/window.py
@@ -14,6 +14,7 @@ class LauncherWindow(QtWidgets.QWidget):
     """Launcher interface"""
     message_interval = 5000
     refresh_interval = 10000
+    page_side_anim_interval = 250
 
     def __init__(self, controller=None, parent=None):
         super(LauncherWindow, self).__init__(parent)
@@ -52,10 +53,10 @@ class LauncherWindow(QtWidgets.QWidget):
         # - Second page - Hierarchy (folders & tasks)
         hierarchy_page = HierarchyPage(controller, pages_widget)
 
-        pages_layout = QtWidgets.QStackedLayout(pages_widget)
+        pages_layout = QtWidgets.QHBoxLayout(pages_widget)
         pages_layout.setContentsMargins(0, 0, 0, 0)
-        pages_layout.addWidget(projects_page)
-        pages_layout.addWidget(hierarchy_page)
+        pages_layout.addWidget(projects_page, 1)
+        pages_layout.addWidget(hierarchy_page, 1)
 
         # Actions
         actions_widget = ActionsWidget(controller, content_body)
@@ -101,8 +102,17 @@ class LauncherWindow(QtWidgets.QWidget):
         refresh_timer = QtCore.QTimer()
         refresh_timer.setInterval(self.refresh_interval)
 
+        page_slide_anim = QtCore.QVariantAnimation(self)
+        page_slide_anim.setDuration(self.page_side_anim_interval)
+        page_slide_anim.setStartValue(0.0)
+        page_slide_anim.setEndValue(1.0)
+        page_slide_anim.setEasingCurve(QtCore.QEasingCurve.OutQuad)
+
         message_timer.timeout.connect(self._on_message_timeout)
         refresh_timer.timeout.connect(self._on_refresh_timeout)
+        page_slide_anim.valueChanged.connect(
+            self._on_page_slide_value_changed)
+        page_slide_anim.finished.connect(self._on_page_slide_finished)
 
         controller.register_event_callback(
             "selection.project.changed",
@@ -114,6 +124,7 @@ class LauncherWindow(QtWidgets.QWidget):
         self._is_on_projects_page = True
         self._window_is_active = False
 
+        self._pages_widget = pages_widget
         self._pages_layout = pages_layout
         self._projects_page = projects_page
         self._hierarchy_page = hierarchy_page
@@ -124,7 +135,9 @@ class LauncherWindow(QtWidgets.QWidget):
 
         self._message_timer = message_timer
         self._refresh_timer = refresh_timer
+        self._page_slide_anim = page_slide_anim
 
+        hierarchy_page.setVisible(not self._is_on_projects_page)
         self.resize(520, 740)
 
     def showEvent(self, event):
@@ -160,14 +173,98 @@ class LauncherWindow(QtWidgets.QWidget):
         else:
             self._refresh_timer.stop()
 
-    def _on_message_timeout(self):
-        self._message_label.setText("")
-
     def _echo(self, message):
         self._message_label.setText(str(message))
         self._message_timer.start()
 
-    # def on_history_action(self, history_data):
+    def _on_message_timeout(self):
+        self._message_label.setText("")
+
+    def _on_project_selection_change(self, event):
+        project_name = event["project_name"]
+        if not project_name:
+            self._go_to_projects_page()
+
+        elif self._is_on_projects_page:
+            self._go_to_hierarchy_page(project_name)
+
+    def _is_page_slide_anim_running(self):
+        return (
+            self._page_slide_anim.state() == QtCore.QAbstractAnimation.Running
+        )
+
+    def _go_to_projects_page(self):
+        if self._is_on_projects_page:
+            return
+        self._is_on_projects_page = True
+        self._hierarchy_page.set_page_visible(False)
+
+        self._start_page_slide_animation()
+
+    def _go_to_hierarchy_page(self, project_name):
+        if not self._is_on_projects_page:
+            return
+        self._is_on_projects_page = False
+        self._hierarchy_page.set_page_visible(True, project_name)
+
+        self._start_page_slide_animation()
+
+    def _start_page_slide_animation(self):
+        if self._is_on_projects_page:
+            direction = QtCore.QAbstractAnimation.Backward
+        else:
+            direction = QtCore.QAbstractAnimation.Forward
+        self._page_slide_anim.setDirection(direction)
+        if self._is_page_slide_anim_running():
+            return
+
+        layout_spacing = self._pages_layout.spacing()
+        if self._is_on_projects_page:
+            hierarchy_geo = self._hierarchy_page.geometry()
+            projects_geo = QtCore.QRect(hierarchy_geo)
+            projects_geo.moveRight(
+                hierarchy_geo.left() - (layout_spacing + 1))
+
+            self._projects_page.setVisible(True)
+
+        else:
+            projects_geo = self._projects_page.geometry()
+            hierarchy_geo = QtCore.QRect(projects_geo)
+            hierarchy_geo.moveLeft(projects_geo.right() + layout_spacing)
+            self._hierarchy_page.setVisible(True)
+
+        while self._pages_layout.count():
+            self._pages_layout.takeAt(0)
+
+        self._projects_page.setGeometry(projects_geo)
+        self._hierarchy_page.setGeometry(hierarchy_geo)
+
+        self._page_slide_anim.start()
+
+    def _on_page_slide_value_changed(self, value):
+        layout_spacing = self._pages_layout.spacing()
+        content_width = self._pages_widget.width() - layout_spacing
+        content_height = self._pages_widget.height()
+
+        # Visible widths of other widgets
+        hierarchy_width = int(content_width * value)
+
+        hierarchy_geo = QtCore.QRect(
+            content_width - hierarchy_width, 0, content_width, content_height
+        )
+        projects_geo = QtCore.QRect(hierarchy_geo)
+        projects_geo.moveRight(hierarchy_geo.left() - (layout_spacing + 1))
+
+        self._projects_page.setGeometry(projects_geo)
+        self._hierarchy_page.setGeometry(hierarchy_geo)
+
+    def _on_page_slide_finished(self):
+        self._pages_layout.addWidget(self._projects_page, 1)
+        self._pages_layout.addWidget(self._hierarchy_page, 1)
+        self._projects_page.setVisible(self._is_on_projects_page)
+        self._hierarchy_page.setVisible(not self._is_on_projects_page)
+
+    # def _on_history_action(self, history_data):
     #     action, session = history_data
     #     app = QtWidgets.QApplication.instance()
     #     modifiers = app.keyboardModifiers()
@@ -179,25 +276,3 @@ class LauncherWindow(QtWidgets.QWidget):
     #     else:
     #         # User is holding control, rerun the action
     #         self.run_action(action, session=session)
-
-    def _go_to_projects_page(self):
-        if self._is_on_projects_page:
-            return
-        self._hierarchy_page.set_page_visible(False)
-        self._pages_layout.setCurrentWidget(self._projects_page)
-        self._is_on_projects_page = True
-
-    def _go_to_hierarchy_page(self, project_name):
-        if not self._is_on_projects_page:
-            return
-        self._hierarchy_page.set_page_visible(True, project_name)
-        self._pages_layout.setCurrentWidget(self._hierarchy_page)
-        self._is_on_projects_page = False
-
-    def _on_project_selection_change(self, event):
-        project_name = event["project_name"]
-        if not project_name:
-            self._go_to_projects_page()
-
-        elif self._is_on_projects_page:
-            self._go_to_hierarchy_page(project_name)
