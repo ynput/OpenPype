12e14815c2a6fd576be0e29d880800f0ef920bdf
kalisp <petr.kalis@gmail.com>
2023-09-14 15:45:43 +0200

    OP-6780 - move injection of PYTHONPATH much sooner
    At previous position it was too late.

--- openpype/hosts/fusion/deploy/MenuScripts/openpype_menu.py
+++ openpype/hosts/fusion/deploy/MenuScripts/openpype_menu.py
@@ -1,12 +1,30 @@
 import os
 import sys
 
+# ugly hack if using Python 3.6 which clashes with distributed libraries
+# todo remove when not necessary
+try:
+    import requests
+except ImportError:
+    FUSION_HOST_DIR = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '..', '..'
+    ))
+
+    python_path = os.environ["PYTHONPATH"]
+    python_path_parts = []
+    if python_path:
+        python_path_parts = python_path.split(os.pathsep)
+    vendor_path = os.path.join(FUSION_HOST_DIR, "vendor")
+    python_path_parts.insert(0, vendor_path)
+    os.environ["PYTHONPATH"] = os.pathsep.join(python_path_parts)
+
+    print(f"Added vendorized libraries from {vendor_path}")
+
 from openpype.lib import Logger
 from openpype.pipeline import (
     install_host,
     registered_host,
 )
-from openpype.hosts.fusion.addon import FUSION_HOST_DIR
 
 
 def main(env):
@@ -22,21 +40,6 @@ def main(env):
 
     log = Logger.get_logger(__name__)
     log.info(f"Registered host: {registered_host()}")
-
-    # ugly hack if using Python 3.6 which clashes with distributed libraries
-    # todo remove when not necessary
-    try:
-        import requests
-    except ImportError:
-        python_path = os.environ["PYTHONPATH"]
-        python_path_parts = []
-        if python_path:
-            python_path_parts = python_path.split(os.pathsep)
-        vendor_path = os.path.join(FUSION_HOST_DIR, "vendor")
-        python_path_parts.insert(0, vendor_path)
-        os.environ["PYTHONPATH"] = os.pathsep.join(python_path_parts)
-
-        log.warning(f"Added vendorized libraries from {vendor_path}")
 
     menu.launch_openpype_menu()
 
