5f3dc1b3c5532cfe906c25eba5ffbd7648fc3636
Ondrej Samohel <ondrej.samohel@ynput.io>
2023-09-15 15:06:07 +0200

    :package: add AYON addon settings

---
+++ server_addon/equalizer/server/settings/__init__.py
@@ -0,0 +1,10 @@
+from .main import (
+    EqualizerSettings,
+    DEFAULT_EQUALIZER_SETTING,
+)
+
+
+__all__ = (
+    "EqualizerSettings",
+    "DEFAULT_EQUALIZER_SETTING",
+)
---
+++ server_addon/equalizer/server/version.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""Package declaring addon version."""
+__version__ = "0.0.1"
