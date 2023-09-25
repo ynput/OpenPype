a352a6468022cf46febfeaf19c19ba93d5e0d59c
Mustafa-Zarkash <mustafataherzaky@outlook.com>
2023-09-25 20:16:56 +0300

    add JOB path houdini setting

---
+++ server_addon/houdini/server/settings/general.py
@@ -0,0 +1,22 @@
+from pydantic import Field
+from ayon_server.settings import BaseSettingsModel
+
+
+class JobPathModel(BaseSettingsModel):
+    enabled: bool = Field(title="Enabled")
+    path: str = Field(title="Path")
+
+
+class GeneralSettingsModel(BaseSettingsModel):
+    JobPath: JobPathModel = Field(
+        default_factory=JobPathModel,
+        title="JOB Path"
+    )
+
+
+DEFAULT_GENERAL_SETTINGS = {
+    "JobPath": {
+        "enabled": True,
+        "path": "{root[work]}/{project[name]}/{hierarchy}/{asset}/work/{task[name]}"
+    }
+}
