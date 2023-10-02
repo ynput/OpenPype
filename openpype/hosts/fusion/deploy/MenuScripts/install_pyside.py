1fbd3b8d30cdc8df10f0cc5bfb99b3f0cb0c824d
Jacob Danell <jacob@emberlight.se>
2023-10-02 16:45:26 +0200

    Merge install_pip_package with install_pyside

--- openpype/hosts/fusion/deploy/MenuScripts/install_pyside.py
+++ openpype/hosts/fusion/deploy/MenuScripts/install_pyside.py
@@ -2,8 +2,63 @@
 # Qt library installed
 import os
 import importlib
-from install_pip_package import pip_install
+from pathlib import Path
+import platform
+import subprocess
 from openpype.hosts.fusion import FUSION_VERSIONS_DICT
+
+
+def get_python_home() -> Path:
+    python_home = Path(os.__file__).parent.parent
+    return python_home
+
+
+def get_python_executable(python_home):
+    if platform.system() == "Windows":
+        python_executable = Path(os.path.join(python_home, "python.exe"))
+    else:
+        python_executable = Path(
+            os.path.join(python_home.parent, "bin", "python3")
+        )
+    if not python_executable.exists():
+        print(f"No Python executable found at {python_home}")
+        return
+    return python_executable.as_posix()
+
+
+def run_installation_command(python_executable, package) -> None:
+    if not python_executable:
+        return
+    command = [
+        python_executable,
+        "-m",
+        "pip",
+        "install",
+        package,
+    ]
+    print(f"running command: {command}")
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    while True:
+        output = process.stdout.readline().decode()
+        if output == "" or process.poll() is not None:
+            break
+        if output:
+            print(output.strip())
+
+
+def pip_install(package: str, fusion_python_home=None) -> None:
+    if fusion_python_home is None:
+        fusion_python_home = get_python_home()
+
+    python_executable = get_python_executable(fusion_python_home)
+    if not python_executable:
+        return
+    print(f"Installing {package}")
+    run_installation_command(python_executable, package)
+    print("Done.")
+
 
 try:
     from qtpy import API_NAME
