e364f4e74c86e3e5e6779617ca6fb5a835b18e3b
Kayla Man <moonyuet2008@hotmail.com>
2023-10-05 20:41:44 +0800

    hound

--- openpype/hosts/max/plugins/publish/validate_tyflow_data.py
+++ openpype/hosts/max/plugins/publish/validate_tyflow_data.py
@@ -19,20 +19,21 @@ class ValidateTyFlowData(pyblish.api.InstancePlugin):
             2. Validate if tyFlow operator Export Particle exists
 
         """
-        report = []
 
         invalid_object = self.get_tyflow_object(instance)
+        if invalid_object:
             self.log.error(f"Non tyFlow object found: {invalid_object}")
 
         invalid_operator = self.get_tyflow_operator(instance)
         if invalid_operator:
             self.log.error(
                 "Operator 'Export Particles' not found in tyFlow editor.")
-        if report:
+        if invalid_object or invalid_operator:
             raise PublishValidationError(
                 "issues occurred",
                 description="Container should only include tyFlow object\n "
-                "and tyflow operator Export Particle should be in the tyFlow editor")
+                "and tyflow operator 'Export Particle' should be in \n"
+                "the tyFlow editor")
 
     def get_tyflow_object(self, instance):
         """Get the nodes which are not tyFlow object(s)
