91d41c86c5310d5239ce5638dcb01c1c66b600b9
Kayla Man <moonyuet2008@hotmail.com>
2023-10-06 19:57:35 +0800

    hound

--- openpype/hosts/maya/plugins/publish/validate_resolution.py
+++ openpype/hosts/maya/plugins/publish/validate_resolution.py
@@ -30,8 +30,7 @@ class ValidateSceneResolution(pyblish.api.InstancePlugin,
                     "Wrong render resolution setting. "
                     "Please use repair button to fix it.\n"
                     "If current renderer is V-Ray, "
-                    "make sure vraySettings node has been created"
-        ))
+                    "make sure vraySettings node has been created"))
 
     def get_invalid_resolution(self, instance):
         width, height, pixelAspect = self.get_db_resolution(instance)
