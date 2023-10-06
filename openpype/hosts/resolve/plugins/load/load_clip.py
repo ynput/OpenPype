e03fe24a07126403b384f5cbae18653d55111356
Jakub Ježek <jakubjezek001@gmail.com>
2023-10-06 18:18:28 +0200

    Update openpype/hosts/resolve/plugins/load/load_clip.py
    Co-authored-by: Roy Nieterau <roy_nieterau@hotmail.com>

--- openpype/hosts/resolve/plugins/load/load_clip.py
+++ openpype/hosts/resolve/plugins/load/load_clip.py
@@ -170,6 +170,9 @@ class LoadClip(plugin.TimelineItemLoader):
         take_mp_item = timeline_item.GetMediaPoolItem()
         timeline = lib.get_current_timeline()
 
+        # DeleteClips function was added in Resolve 18.5+
+        # by checking None we can detect whether the
+        # function exists in Resolve
         if timeline.DeleteClips is not None:
             timeline.DeleteClips([timeline_item])
 
