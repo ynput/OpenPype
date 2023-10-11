0498a4016d6f0f6dd1a599f9b985daba1805d317
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2023-10-11 18:16:45 +0200

    Loader tool: Refactor loader tool (for AYON) (#5729)
    * initial commitof ayon loader
    
    * tweaks in ayon utils
    
    * implemented product type filtering
    
    * products have icons and proper style
    
    * fix refresh of products
    
    * added enable grouping checkbox
    
    * added icons and sorting of grouped items
    
    * fix version delegate
    
    * add splitter between context and product type filtering
    
    * fix products filtering by name
    
    * implemented 'filter_repre_contexts_by_loader'
    
    * implemented base of action items
    
    * implemented folder underline colors
    
    * changed version items to dictionary
    
    * use 'product_id' instead of 'subset_id'
    
    * base implementation of info widget
    
    * require less to trigger action
    
    * set selection of version ids in controller
    
    * added representation widget and related logic changes
    
    * implemented actions in representations widget
    
    * handle load error
    
    * use versions for subset loader
    
    * fix representations widget
    
    * implemente "in scene" logic properly
    
    * use ayon loader in host tools
    
    * fix used function to get tasks
    
    * show actions per representation name
    
    * center window
    
    * add window flag to loader window
    
    * added 'ThumbnailPainterWidget' to tool utils
    
    * implemented thumbnails model
    
    * implement thumbnail widget
    
    * fix FolderItem args docstring
    
    * bypass bug in ayon_api
    
    * fix sorting of folders
    
    * added refresh button
    
    * added expected selection and go to current context
    
    * added information if project item is library project
    
    * added more filtering options to projects widget
    
    * added missing information abou is library to model items
    
    * remove select project item on selection change
    
    * filter out non library projects
    
    * set current context project to project combobox
    
    * change window title
    
    * fix hero version queries
    
    * move current project to the top
    
    * fix reset
    
    * change icon for library projects
    
    * added libraries separator to project widget
    
    * show libraries separator in loader
    
    * ise single line expression
    
    * library loader tool is loader tool in AYON mode
    
    * fixes in grouping model
    
    * implemented grouping logic
    
    * use loader in tray action
    
    * better initial sizes
    
    * moved 'ActionItem' to abstract
    
    * filter loaders by tool name based on current context project
    
    * formatting fixes
    
    * separate abstract classes into frontend and backend abstractions
    
    * added docstrings to abstractions
    
    * implemented 'to_data' and 'from_data' for action item options
    
    * added more docstrings
    
    * first filter representation contexts and then create action items
    
    * implemented 'refresh' method
    
    * do not reset controller in '_on_first_show'
    
    Method '_on_show_timer' will take about the reset.
    
    * 'ThumbnailPainterWidget' have more options of bg painting
    
    * do not use checkerboard in loader thumbnail
    
    * fix condition
    
    Co-authored-by: Roy Nieterau <roy_nieterau@hotmail.com>
    
    ---------
    
    Co-authored-by: Roy Nieterau <roy_nieterau@hotmail.com>

---
+++ openpype/tools/ayon_loader/models/__init__.py
@@ -0,0 +1,10 @@
+from .selection import SelectionModel
+from .products import ProductsModel
+from .actions import LoaderActionsModel
+
+
+__all__ = (
+    "SelectionModel",
+    "ProductsModel",
+    "LoaderActionsModel",
+)
