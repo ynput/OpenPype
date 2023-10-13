08baaca5a339adc11ebfb4fc77ad1d163df759f6
Roy Nieterau <roy_nieterau@hotmail.com>
2023-10-13 14:18:03 +0200

    Refactor `SelectInvalidAction` to behave like other action for other host, create `SelectInstanceNodeAction` as dedicated action to select the instance node for a failed plugin.
    - Note: Selecting Instance Node will still select the instance node even if the user has currently 'fixed' the problem.

--- openpype/hosts/nuke/api/actions.py
+++ openpype/hosts/nuke/api/actions.py
@@ -20,31 +20,58 @@ class SelectInvalidAction(pyblish.api.Action):
 
     def process(self, context, plugin):
 
+        errored_instances = get_errored_instances_from_context(context,
+                                                               plugin=plugin)
+
+        # Get the invalid nodes for the plug-ins
+        self.log.info("Finding invalid nodes..")
+        invalid = set()
+        for instance in errored_instances:
+            invalid_nodes = plugin.get_invalid(instance)
+
+            if invalid_nodes:
+                if isinstance(invalid_nodes, (list, tuple)):
+                    invalid.update(invalid_nodes)
+                else:
+                    self.log.warning("Plug-in returned to be invalid, "
+                                     "but has no selectable nodes.")
+
+        if invalid:
+            self.log.info("Selecting invalid nodes: {}".format(invalid))
+            reset_selection()
+            select_nodes(invalid)
+        else:
+            self.log.info("No invalid nodes found.")
+
+
+class SelectInstanceNodeAction(pyblish.api.Action):
+    """Select instance node for failed plugin."""
+    label = "Select instance node"
+    on = "failed"  # This action is only available on a failed plug-in
+    icon = "mdi.cursor-default-click"
+
+    def process(self, context, plugin):
+
         # Get the errored instances for the plug-in
         errored_instances = get_errored_instances_from_context(
             context, plugin)
 
         # Get the invalid nodes for the plug-ins
-        self.log.info("Finding invalid nodes..")
-        invalid_nodes = set()
+        self.log.info("Finding instance nodes..")
+        nodes = set()
         for instance in errored_instances:
-            invalid = plugin.get_invalid(instance)
-
-            if not invalid:
-                continue
-
-            select_node = instance.data.get("transientData", {}).get("node")
-            if not select_node:
+            instance_node = instance.data.get("transientData", {}).get("node")
+            if not instance_node:
                 raise RuntimeError(
                     "No transientData['node'] found on instance: {}".format(
-                        instance)
+                        instance
+                    )
                 )
+            nodes.add(instance_node)
 
-            invalid_nodes.add(select_node)
-
-        if invalid_nodes:
-            self.log.info("Selecting invalid nodes: {}".format(invalid_nodes))
+        if nodes:
+            self.log.info("Selecting instance nodes: {}".format(nodes))
             reset_selection()
-            select_nodes(list(invalid_nodes))
+            select_nodes(nodes)
         else:
-            self.log.info("No invalid nodes found.")
+            self.log.info("No instance nodes found.")
