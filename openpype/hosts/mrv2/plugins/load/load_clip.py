f41012c022d0334aaca4cef788bbbaf117fa2117
Roy Nieterau <roy_nieterau@hotmail.com>
2023-10-18 12:17:49 +0200

    Fix `setSpeed` call (it's on `timeline` not in `cmd`) + add set colorspace logic

--- openpype/hosts/mrv2/plugins/load/load_clip.py
+++ openpype/hosts/mrv2/plugins/load/load_clip.py
@@ -1,5 +1,8 @@
+import os
+
 import openpype.pipeline.load as load
 from openpype.lib.transcoding import IMAGE_EXTENSIONS, VIDEO_EXTENSIONS
+from openpype.lib import BoolDef
 
 
 class LoadClip(load.LoaderPlugin):
@@ -23,23 +26,95 @@ class LoadClip(load.LoaderPlugin):
     icon = "code-fork"
     color = "orange"
 
-    def load(self, context, name, namespace, data):
-        path = self.filepath_from_context(context)
+    options = [
+        BoolDef(
+            "setFps",
+            label="Set FPS",
+            default=True,
+            tooltip=(
+                "Set the FPS of the representation, version, asset or project "
+                "related to the loaded file.\n"
+                "Use whichever has 'data.fps' defined first."
+            )
+        ),
+        BoolDef(
+            "setColorspace",
+            label="Set Colorspace",
+            default=True,
+            tooltip=(
+                "Set the colorspace of the representation if it has any "
+                "colorspace information."
+            )
+        )
+    ]
 
-        from mrv2 import cmd
+    def load(self, context, name, namespace, data):
+
+        from mrv2 import cmd, timeline
+
+        path = self.filepath_from_context(context)
         cmd.open(path)
 
-        # Match publish fps if mrv2 supports `setSpeed` (0.8+)
-        if hasattr(cmd, "setSpeed"):
+        # Match publish fps if mrv2 supports `setSpeed` (0.8.0+)
+        if data.get("setFps", True) and hasattr(timeline, "setSpeed"):
             fps = self.get_fps(context)
-            cmd.setSpeed(fps)
+            timeline.setSpeed(fps)
+
+        # Match publish colorspace if mrv2 supports `setOcioConfig` (0.8.2+)
+        if data.get("setColorspace", True) and hasattr(cmd, "setOcioConfig"):
+            colorspace_data = self.get_colorspace_data(context)
+            if colorspace_data:
+                self.set_colorspace(colorspace_data)
+
+    def set_colorspace(self, colorspace_data):
+
+        from mrv2 import image
+
+        # TODO: The path should actually be retrieved through template
+        config_path = colorspace_data["config"]["path"]
+        if os.path.exists(config_path):
+
+            image.setOcioConfig(config_path)
+            image.setOcioIcs(colorspace_data["colorspace"])
+            # TODO: Should we also set display `colorspace_data["display"]`?
+            image.setOcioView(colorspace_data["view"])
+        else:
+            self.log.warning(
+                "OCIO Colorspace data found for file, but OCIO config "
+                f"path not found on disk: {config_path}. "
+                "Ignoring colorspace data."
+            )
 
     def get_fps(self, context, default=25):
         for entity in ["representation", "version", "asset", "project"]:
             fps = context[entity].get("data", {}).get("fps")
             if fps is not None:
-                self.log.info(f"Using FPS from {entity}: {fps}")
+                self.log.info(f"Using FPS from '{entity}': {fps}")
                 return fps
 
         self.log.warning(f"Using fallback default FPS: {fps}")
         return default
+
+    def get_colorspace_data(self, context):
+        """Return colorspace of the file to load.
+
+        Retrieves the explicit colorspace from the publish.
+
+        Returns:
+            dict or None: The colorspace data or None if not detected.
+
+        """
+        representation = context["representation"]
+        colorspace_data = representation.get("data", {}).get("colorspaceData")
+        if colorspace_data:
+            # Example data:
+            # "colorspaceData": {
+            #     "colorspace": "ACEScg",
+            #     "config": {
+            #         "path": "/path/to/config.ocio",
+            #         "template": "{template}/to/config.ocio"
+            #     },
+            #     "display": "sRGB",
+            #     "view": "ACES 1.0 SDR-video"
+            # },
+            return colorspace_data
