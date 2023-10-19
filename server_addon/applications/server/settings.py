76aaf2aae3c4c733675c4860840443027da9d171
kalisp <petr.kalis@gmail.com>
2023-10-19 18:34:43 +0200

    Added Wrap to applications

--- server_addon/applications/server/settings.py
+++ server_addon/applications/server/settings.py
@@ -166,6 +166,8 @@ class ApplicationsSettings(BaseSettingsModel):
         default_factory=AppGroupWithPython, title="Celaction 2D")
     unreal: AppGroup = Field(
         default_factory=AppGroupWithPython, title="Unreal Editor")
+    wrap: AppGroup = Field(
+        default_factory=AppGroupWithPython, title="Wrap")
     additional_apps: list[AdditionalAppGroup] = Field(
         default_factory=list, title="Additional Applications")
 
