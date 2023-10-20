de2a6c33248b51f19faa51c74ab86fc935be6454
Simone Barbieri <simone.barbieri@hey.com>
2023-10-20 16:03:44 +0100

    Added dialog to confirm before deleting files

--- openpype/hosts/unreal/plugins/inventory/delete_unused_assets.py
+++ openpype/hosts/unreal/plugins/inventory/delete_unused_assets.py
@@ -1,8 +1,8 @@
 import unreal
 
+from openpype.hosts.unreal.api.tools_ui import qt_app_context
 from openpype.hosts.unreal.api.pipeline import delete_asset_if_unused
 from openpype.pipeline import InventoryAction
-
 
 
 class DeleteUnusedAssets(InventoryAction):
@@ -14,7 +14,9 @@ class DeleteUnusedAssets(InventoryAction):
     color = "red"
     order = 1
 
-    def process(self, containers):
+    dialog = None
+
+    def _delete_unused_assets(self, containers):
         allowed_families = ["model", "rig"]
 
         for container in containers:
@@ -29,3 +31,36 @@ class DeleteUnusedAssets(InventoryAction):
             )
 
             delete_asset_if_unused(container, asset_content)
+
+    def _show_confirmation_dialog(self, containers):
+        from qtpy import QtCore
+        from openpype.widgets import popup
+        from openpype.style import load_stylesheet
+
+        dialog = popup.Popup()
+        dialog.setWindowFlags(
+            QtCore.Qt.Window
+            | QtCore.Qt.WindowStaysOnTopHint
+        )
+        dialog.setFocusPolicy(QtCore.Qt.StrongFocus)
+        dialog.setWindowTitle("Delete all unused assets")
+        dialog.setMessage(
+            "You are about to delete all the assets in the project that \n"
+            "are not used in any level. Are you sure you want to continue?"
+        )
+        dialog.setButtonText("Delete")
+
+        dialog.on_clicked.connect(
+            lambda: self._delete_unused_assets(containers)
+        )
+
+        dialog.show()
+        dialog.raise_()
+        dialog.activateWindow()
+        dialog.setStyleSheet(load_stylesheet())
+
+        self.dialog = dialog
+
+    def process(self, containers):
+        with qt_app_context():
+            self._show_confirmation_dialog(containers)
