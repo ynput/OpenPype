ca5bbf0cac4b5de3e021f2abfc0352ddb4d92b0f
Santi Svirsky <santi@boat.media>
2023-10-23 12:39:22 -0300

    new method collect_input

--- openpype/modules/puf_addons/gobbler/module.py
+++ openpype/modules/puf_addons/gobbler/module.py
@@ -3,7 +3,7 @@ import os
 import time
 import pyblish
 import click
-
+from pathlib import Path
 # import gazu
 
 from openpype.modules import OpenPypeModule
@@ -19,6 +19,14 @@ from openpype.hosts.traypublisher.api import TrayPublisherHost
 from openpype.pipeline import install_host
 from openpype.lib import Logger
 
+# google spreadsheet
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials # this is deprecated
+# from google.auth.credentials import Credentials # but using the new one requires refactoring so not using it for now
+
+import pandas
 
 # class GobblerHost(TrayPublisherHost):
 #     name = "gobbler"
@@ -117,6 +125,89 @@ def go(project_name, directory=None):
                         expected_representations,
                         publish_data,
                         batch_name,)
+
+
+@cli_main.command()
+@click.option("-r", "--named_range",
+              required=True,
+              help="Name of range containing files to collect")
+def collect_input(named_range):
+    '''Collects input from named_range to cse/in/YYMMDD_uuid directory'''
+    import uuid
+    from datetime import datetime
+
+    # Get the current date
+    current_date = datetime.now()
+    # Format it as YYMMDD
+    formatted_date = current_date.strftime("%y%m%d")
+
+    destination = f"Y:\WORKS\_openpype\cse\in\{formatted_date}___{uuid.uuid4()}"
+    print(f"Destination: {destination}")
+    df = _load_data(named_range)
+    for row in df.iterrows():
+        _copy_files(row, destination)
+
+def _copy_files(row, destination):
+    # copies files from row to destination
+    import shutil
+    print(row)
+    shot = row[1]['shot']
+    fg = row[1]['foreground']
+    bg = row[1]['background']
+
+    # copy fg
+    fg_destination = Path(destination)/shot/"fg"
+    shutil.copytree(fg, fg_destination)
+
+    # copy bg
+    bg_destination = Path(destination)/shot/"bg"
+    shutil.copytree(bg, bg_destination)
+
+
+def _load_data(named_range):
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    SPREADSHEET_ID = '18Z-fn_GUGdWTg0-LW1CcS0Bg75Iczu0qu0omH31yO8M'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    print(">>>>>>>>>>>>", os.getcwd())
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '../openpype/modules/puf_addons/gobbler/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API and consume all 'data'
+    sheet = service.spreadsheets()
+    all_lines = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=named_range).execute()['values']
+
+
+    print(all_lines)
+    column_names = all_lines[0]
+    data = all_lines[1:]
+
+    # Create a DataFrame from the list of rows and specify column names
+    df = pandas.DataFrame(data, columns=column_names)
+    print(df.head())  # data sanity check
+
+
+    return df
+    # values = result.get('values', [])
 
 
 def _fuzz_asset(item, assets_dict):
