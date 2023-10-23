496d5e32b6f5706725aa3a019cca03e5396a111d
Roy Nieterau <roy_nieterau@hotmail.com>
2023-10-23 23:27:11 +0200

    Add tests for Create Context publish attribute saves

---
+++ tests/unit/openpype/pipeline/create/lib.py
@@ -0,0 +1,159 @@
+import copy
+import contextlib
+
+import pyblish.api
+
+from openpype.host import HostBase, IPublishHost
+from openpype.pipeline import (
+    register_host,
+    registered_host,
+    CreatedInstance,
+    Creator
+)
+from openpype.pipeline.create.creator_plugins import (
+    BaseCreator,
+    LegacyCreator,
+    SubsetConvertorPlugin,
+)
+
+
+@contextlib.contextmanager
+def no_plugins(plugins):
+    """Ensure no registered plug-ins of type `plugins` exist during the context.
+
+    Also reverts any plugins or plugin paths registered during
+    the context to be undoed after the context of those plugin types.
+
+    """
+    # TODO: It seems there is no access to the registered paths
+    #  or plugins so for now I'm just hacking into it here
+
+    from openpype.pipeline.plugin_discover import _GlobalDiscover
+
+    context = _GlobalDiscover.get_context()
+
+    original_registered_plugins = {}
+    original_registered_plugin_paths = {}
+    for plugin in plugins:
+        registered_plugins = context._registered_plugins.pop(plugin, None)
+        original_registered_plugins[plugin] = copy.deepcopy(registered_plugins)
+        registered_plugin_paths = context._registered_plugin_paths.pop(plugin,
+                                                                       None)
+        original_registered_plugin_paths[plugin] = copy.deepcopy(
+            registered_plugin_paths)
+
+    try:
+        yield
+    finally:
+        # Revert registered plugins
+        for plugin_key, registered_plugins in original_registered_plugins.items():
+            if plugin is None:
+                context._registered_plugins.pop(plugin_key, None)
+            else:
+                context._registered_plugins[plugin_key] = registered_plugins
+
+        # Revert registered plugins
+        for plugin_key, registered_plugin_paths in original_registered_plugin_paths.items():
+            if plugin is None:
+                context._registered_plugin_paths.pop(plugin_key, None)
+            else:
+                context._registered_plugin_paths[
+                    plugin_key] = registered_plugin_paths
+
+
+@contextlib.contextmanager
+def no_creator_plugins():
+    """Deregister all creator plugins during context, revert afterwards"""
+    with no_plugins([
+        BaseCreator,
+        LegacyCreator,
+        SubsetConvertorPlugin
+    ]):
+        yield
+
+
+@contextlib.contextmanager
+def no_pyblish_plugins():
+    """Deregister all Pyblish plugins during context, revert afterwards"""
+    paths = copy.deepcopy(pyblish.api.registered_paths())
+    plugins = copy.deepcopy(pyblish.api.registered_plugins())
+
+    try:
+        pyblish.api.deregister_all_paths()
+        pyblish.api.deregister_all_plugins()
+        yield
+    finally:
+        pyblish.api.deregister_all_paths()
+        pyblish.api.deregister_all_plugins()
+        for path in paths:
+            pyblish.api.register_plugin_path(path)
+        for plugin in plugins:
+            pyblish.api.register_plugin(plugin)
+
+
+class DummyPublishHost(HostBase, IPublishHost):
+    """Dummy IPublishHost that does *not* store any context data"""
+
+    name = "dummy"
+
+    def get_context_data(self):
+        return {}
+
+    def update_context_data(self, data, changes):
+        pass
+
+
+@contextlib.contextmanager
+def host_during_context(host):
+    """Set registered host during context"""
+    original_host = registered_host()
+    try:
+        register_host(host)
+        yield
+    finally:
+        register_host(original_host)
+
+
+@contextlib.contextmanager
+def test_setup():
+    """Set up dummy host without any registered plugins"""
+    with host_during_context(DummyPublishHost()):
+        with no_creator_plugins():
+            with no_pyblish_plugins():
+                yield
+
+
+class DummyCreator(Creator):
+    """Simple Creator that just caches instances into the module globally"""
+    identifier = "dummy"
+    label = "Dummy"
+    family = "dummy"
+    description = "Dummy Creator"
+
+    cache = {
+        # instance by id
+        "instances": {}
+    }
+
+    def create(self, subset_name, data, pre_create_data):
+        instance = CreatedInstance(self.family, subset_name, data, self)
+
+        self._add_instance_to_context(instance)
+
+        # Add to cache
+        DummyCreator.cache["instances"][instance.id] = instance.data_to_store()
+
+    def collect_instances(self):
+        for inst_data in DummyCreator.cache.get("instances", {}).values():
+            instance = CreatedInstance.from_existing(inst_data, creator=self)
+            self._add_instance_to_context(instance)
+
+    def update_instances(self, update_list):
+        for created_inst, _changes in update_list:
+            data = created_inst.data_to_store()
+            DummyCreator.cache[created_inst.id] = data
+
+    def remove_instances(self, instances):
+        for instance in instances:
+            DummyCreator.cache.pop(instance.id, None)
+            self._remove_instance_from_context(instance)
---
+++ tests/unit/openpype/pipeline/create/test_create_context_save.py
@@ -0,0 +1,111 @@
+import pyblish.api
+
+from openpype.pipeline import registered_host
+
+from openpype.lib import BoolDef
+
+from openpype.pipeline.publish import OpenPypePyblishPluginMixin
+from openpype.pipeline.create import (
+    register_creator_plugin,
+    deregister_creator_plugin,
+    CreateContext,
+)
+
+import pytest
+from tests.unit.openpype.pipeline.create.lib import test_setup, DummyCreator
+from tests.lib.testing_classes import ModuleUnitTest
+
+
+class TestCreateContext(ModuleUnitTest):
+    """ Testing Colorspace
+
+    Example:
+        cd to OpenPype repo root dir
+        poetry run python ./start.py runtests <openpype_root>/tests/unit/openpype/pipeline/test_create_context_save.py
+    """
+
+    @pytest.fixture
+    def setup_dummy_host(self):
+        """Register DummyHost without ANY plugins"""
+        with test_setup():
+            yield
+
+    @pytest.fixture
+    def dummy_creator(self):
+        """Register DummyCreator"""
+        register_creator_plugin(DummyCreator)
+        yield DummyCreator
+        deregister_creator_plugin(DummyCreator)
+
+    def test_save_publish_attribute_changes(self,
+                                            setup_dummy_host,
+                                            dummy_creator):
+        """Test CreateContext instance publish attribute change saves.
+
+        Test whether changes on CreatedInstance publish attributes are marked
+        as stored when `CreateContext.save_changes()` is triggered and that
+        no full `CreateContext.reset()` is required to update values and
+        that the changes actually go through `Creator.update_instances` to
+        persist the changes made as intended.
+
+        """
+
+        # Def
+        class Plugin(pyblish.api.InstancePlugin,
+                     OpenPypePyblishPluginMixin):
+            """Publish Plug-in that exposes bool attribute definition"""
+            order = pyblish.api.ValidatorOrder
+
+            @classmethod
+            def get_attribute_defs(cls):
+                return [
+                    BoolDef("state",
+                            label="State",
+                            default=False)
+                ]
+
+        pyblish.api.register_plugin(Plugin)
+
+        create_context = CreateContext(host=registered_host())
+        create_context.create(
+            creator_identifier=dummy_creator.identifier,
+            variant="main"
+        )
+        # Save the initial state of the attribute defs
+        create_context.save_changes()
+
+        instances = list(create_context.instances)
+        assert len(instances) == 1, "Must have one instance"
+        instance = instances[0]
+
+        def get_saved_state_in_host():
+            """Return 'host stored' state value for instance"""
+            return (
+                dummy_creator.cache["instances"][instance.id]
+                ["publish_attributes"]["Plugin"]["state"]
+            )
+
+        # Value must be unchanged and False
+        assert instance.publish_attributes["Plugin"]["state"] is False
+        assert get_saved_state_in_host() is False  # noqa
+        assert instance.changes().changed is False
+
+        # Set to True, value must be changed before save, and unchanged after
+        # and host value must be updated along
+        instance.publish_attributes["Plugin"]["state"] = True
+        assert instance.changes().changed is True
+        create_context.save_changes()
+        assert instance.changes().changed is False
+        assert instance.publish_attributes["Plugin"]["state"] is True
+        assert get_saved_state_in_host() is True
+
+        # Set to False, value must be changed before save, and unchanged after
+        # and host value must be updated along
+        instance.publish_attributes["Plugin"]["state"] = False
+        assert instance.changes().changed is True
+        create_context.save_changes()
+        assert instance.changes().changed is False
+        assert instance.publish_attributes["Plugin"]["state"] is False
+        assert get_saved_state_in_host() is False
+
+        pyblish.api.deregister_plugin(Plugin)
