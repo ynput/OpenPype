94ba9ca4b5e528de0337ab7d9f882573042f5bf1
Roy Nieterau <roy_nieterau@hotmail.com>
2023-10-29 01:48:41 +0200

    Add (maya) loader to explicitly use any published workfile as workfile template

---
+++ openpype/hosts/maya/plugins/load/load_as_template.py
@@ -0,0 +1,40 @@
+from openpype.lib import (
+    BoolDef
+)
+from openpype.pipeline import (
+    load,
+    registered_host
+)
+from openpype.hosts.maya.api.workfile_template_builder import (
+    MayaTemplateBuilder
+)
+
+
+class LoadAsTemplate(load.LoaderPlugin):
+    """Load workfile as a template """
+
+    families = ["workfile"]
+    label = "Load as template"
+    representations = ["ma", "mb"]
+    icon = "wrench"
+    color = "#775555"
+    order = 10
+
+    options = [
+        BoolDef("keep_placeholders",
+                label="Keep Placeholders",
+                default=False),
+        BoolDef("create_first_version",
+                label="Create First Version",
+                default=False),
+    ]
+
+    def load(self, context, name, namespace, data):
+
+        keep_placeholders = data.get("keep_placeholders", False)
+        create_first_version = data.get("create_first_version", False)
+        path = self.filepath_from_context(context)
+        builder = MayaTemplateBuilder(registered_host())
+        builder.build_template(template_path=path,
+                               keep_placeholders=keep_placeholders,
+                               create_first_version=create_first_version)
