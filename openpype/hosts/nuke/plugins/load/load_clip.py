16aad9928823a10034d6d9bfc1ba7cb25fd24a53
Jakub Jezek <jakubjezek001@gmail.com>
2023-11-01 21:36:48 +0100

    nuke: updating loaders so they are using nodes rather then objectName

--- openpype/hosts/nuke/plugins/load/load_clip.py
+++ openpype/hosts/nuke/plugins/load/load_clip.py
@@ -189,8 +189,6 @@ class LoadClip(plugin.NukeLoader):
                         value_ = value_.replace("\\", "/")
                     data_imprint[key] = value_
 
-            data_imprint["objectName"] = read_name
-
             if add_retime and version_data.get("retime", None):
                 data_imprint["addRetime"] = True
 
@@ -254,7 +252,7 @@ class LoadClip(plugin.NukeLoader):
 
         is_sequence = len(representation["files"]) > 1
 
-        read_node = nuke.toNode(container['objectName'])
+        read_node = container["node"]
 
         if is_sequence:
             representation = self._representation_with_hash_in_frame(
@@ -353,7 +351,7 @@ class LoadClip(plugin.NukeLoader):
         self.set_as_member(read_node)
 
     def remove(self, container):
-        read_node = nuke.toNode(container['objectName'])
+        read_node = container["node"]
         assert read_node.Class() == "Read", "Must be Read"
 
         with viewer_update_and_undo_stop():
