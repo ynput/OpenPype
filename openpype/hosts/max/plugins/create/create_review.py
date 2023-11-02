88116be4c63079598ec8a38b45ad3cc329155a87
Kayla Man <moonyuet2008@hotmail.com>
2023-11-02 20:44:21 +0800

    allows users to preset the settings before the creator setting

--- openpype/hosts/max/plugins/create/create_review.py
+++ openpype/hosts/max/plugins/create/create_review.py
@@ -12,6 +12,32 @@ class CreateReview(plugin.MaxCreator):
     family = "review"
     icon = "video-camera"
 
+    review_width = 1920
+    review_height = 1080
+    percentSize = 100
+    keep_images = False
+    image_format = "png"
+    visual_style = "Realistic"
+    viewport_preset = "Quality"
+    vp_texture = True
+    anti_aliasing = None
+
+
+    @classmethod
+    def apply_settings(cls, project_settings):
+        settings = project_settings["max"]["PreviewAnimation"]  # noqa
+
+        # Take some defaults from settings
+        cls.review_width = settings.get("review_width", cls.review_width)
+        cls.review_height = settings.get("review_height", cls.review_height)
+        cls.percentSize = settings.get("percentSize", cls.percentSize)
+        cls.keep_images = settings.get("keep_images", cls.keep_images)
+        cls.image_format = settings.get("image_format", cls.image_format)
+        cls.visual_style = settings.get("visual_style", cls.visual_style)
+        cls.viewport_preset = settings.get("viewport_preset", cls.viewport_preset)
+        cls.vp_texture = settings.get("vp_texture", cls.vp_texture)
+        cls.anti_aliasing = settings.get("anti_aliasing", cls.anti_aliasing)
+
     def create(self, subset_name, instance_data, pre_create_data):
         # Transfer settings from pre create to instance
         creator_attributes = instance_data.setdefault(
@@ -23,6 +49,7 @@ class CreateReview(plugin.MaxCreator):
                     "percentSize",
                     "visualStyleMode",
                     "viewportPreset",
+                    "anti_aliasing",
                     "vpTexture"]:
             if key in pre_create_data:
                 creator_attributes[key] = pre_create_data[key]
@@ -33,7 +60,7 @@ class CreateReview(plugin.MaxCreator):
             pre_create_data)
 
     def get_instance_attr_defs(self):
-        image_format_enum = ["exr", "jpg", "png"]
+        image_format_enum = ["exr", "jpg", "png", "tga"]
 
         visual_style_preset_enum = [
             "Realistic", "Shaded", "Facets",
@@ -45,41 +72,46 @@ class CreateReview(plugin.MaxCreator):
         preview_preset_enum = [
             "Quality", "Standard", "Performance",
             "DXMode", "Customize"]
+        anti_aliasing_enum = ["None", "2X", "4X", "8X"]
 
         return [
             NumberDef("review_width",
                       label="Review width",
                       decimals=0,
                       minimum=0,
-                      default=1920),
+                      default=self.review_width),
             NumberDef("review_height",
                       label="Review height",
                       decimals=0,
                       minimum=0,
-                      default=1080),
-            BoolDef("keepImages",
-                    label="Keep Image Sequences",
-                    default=False),
-            EnumDef("imageFormat",
-                    image_format_enum,
-                    default="png",
-                    label="Image Format Options"),
+                      default=self.review_height),
             NumberDef("percentSize",
                       label="Percent of Output",
                       default=100,
                       minimum=1,
                       decimals=0),
+            BoolDef("keepImages",
+                    label="Keep Image Sequences",
+                    default=self.keep_images),
+            EnumDef("imageFormat",
+                    image_format_enum,
+                    default=self.image_format,
+                    label="Image Format Options"),
             EnumDef("visualStyleMode",
                     visual_style_preset_enum,
-                    default="Realistic",
+                    default=self.visual_style,
                     label="Preference"),
             EnumDef("viewportPreset",
                     preview_preset_enum,
-                    default="Quality",
+                    default=self.viewport_preset,
                     label="Pre-View Preset"),
+            EnumDef("anti_aliasing",
+                    anti_aliasing_enum,
+                    default=self.anti_aliasing,
+                    label="Anti-aliasing Quality"),
             BoolDef("vpTexture",
                     label="Viewport Texture",
-                    default=False)
+                    default=self.vp_texture)
         ]
 
     def get_pre_create_attr_defs(self):
