3c82ccf8618a85b03a38d27494f88c191becaab8
Kayla Man <moonyuet2008@hotmail.com>
2023-11-14 21:03:59 +0800

    Substance Painter Addons for Ayon

---
+++ server_addon/substancepainter/server/settings/main.py
@@ -0,0 +1,26 @@
+from pydantic import Field
+from ayon_server.settings import BaseSettingsModel
+from .imageio import ImageIOSettings, DEFAULT_IMAGEIO_SETTINGS
+
+
+class ShelvesSettingsModel(BaseSettingsModel):
+    _layout = "compact"
+    name: str = Field(title="Name")
+    value: str = Field(title="Path")
+
+
+class SubstancePainterSettings(BaseSettingsModel):
+    imageio: ImageIOSettings = Field(
+        default_factory=ImageIOSettings,
+        title="Color Management (ImageIO)"
+    )
+    shelves: list[ShelvesSettingsModel] = Field(
+        default_factory=list,
+        title="Shelves"
+    )
+
+
+DEFAULT_SPAINTER_SETTINGS =  {
+    "imageio": DEFAULT_IMAGEIO_SETTINGS,
+    "shelves": []
+}
