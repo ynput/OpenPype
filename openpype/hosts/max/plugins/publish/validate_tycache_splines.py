37ee299e228320831b068ad1d7b81b0cb76c7470
Kayla Man <moonyuet2008@hotmail.com>
2023-11-17 22:52:04 +0800

    add validator for tycacheSplinesData

--- openpype/hosts/max/plugins/publish/validate_tycache_splines.py
+++ openpype/hosts/max/plugins/publish/validate_tycache_splines.py
@@ -3,17 +3,51 @@ from openpype.pipeline import (
     PublishValidationError,
     OptionalPyblishPluginMixin
 )
-from pymxs import runtime as rt
 
 
-class ValidateTyFlowSplinesData(pyblish.api.InstancePlugin,
-                                OptionalPyblishPluginMixin):
-    """Validate TyFlow Splines data when the export mode is Tycache(Splints)"""
+class ValidateTyCacheSplinesData(pyblish.api.InstancePlugin,
+                                 OptionalPyblishPluginMixin):
+    """Validate TyCache Splines data when the export
+    mode is Tycache(Splines)"""
 
-    order = pyblish.api.ValidatorOrder
+    order = pyblish.api.ValidatorOrder + 0.01
     families = ["tycache"]
     hosts = ["max"]
-    label = "TyFlow Splines Data"
+    label = "TyCache Splines Data"
+    optional = True
 
     def process(self, instance):
-        pass
+        if not self.is_active(instance.data):
+            return
+
+        if instance.data["exportMode"] != 6:
+            self.log.debug("The Export Mode is not tyCache(Splines). "
+                           "Skipping Validate TyFlow Splines Data")
+            return
+
+        invalid = self.get_invalid(instance)
+        if invalid:
+            bullet_point_invalid_statement = "\n".join(
+                "- {}".format(err) for err
+                in invalid
+            )
+            report = (
+                "Required TyCache Splines Data has invalid value.\n\n"
+                f"{bullet_point_invalid_statement}\n\n"
+            )
+            raise PublishValidationError(
+                report,
+                title="Invalid Value for Required TyCache Splines Data")
+
+    @classmethod
+    def get_invalid(cls, instance):
+        invalid = []
+        members = instance.data["members"]
+        tyc_attrs = instance.data.get("tyc_attrs", {})
+        if not tyc_attrs:
+            invalid.append(f"No tyCache attributes found in {members}")
+
+        if "tycacheSplines" not in tyc_attrs.keys():
+            invalid.append("Mandatory tyCache Attributes 'tycacheSplines' "
+                           f"is not included in {members}")
+        return invalid
