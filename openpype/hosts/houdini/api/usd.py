f0a9bf89d2e6baed058944d1056ba151d3b41146
Roy Nieterau <roy_nieterau@hotmail.com>
2023-11-20 13:00:26 +0100

    Fix support for `usdrender` node type

--- openpype/hosts/houdini/api/usd.py
+++ openpype/hosts/houdini/api/usd.py
@@ -189,7 +189,7 @@ def get_usd_rop_loppath(node):
     if node_type == "usd":
         return node.parm("loppath").evalAsNode()
 
-    elif node_type in {"usd_rop", "usdrender_rop"}:
+    elif node_type in {"usd_rop", "usdrender_rop", "usdrender"}:
         # Inside Solaris e.g. /stage (not in ROP context)
         # When incoming connection is present it takes it directly
         inputs = node.inputs()
