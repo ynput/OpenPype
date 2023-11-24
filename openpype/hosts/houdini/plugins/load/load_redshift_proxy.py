9ae1e7950915adab798840520455618c32f18cf0
Kayla Man <moonyuet2008@hotmail.com>
2023-11-24 18:35:07 +0800

    replace deprecated self.fname by self.filepath_from_context

--- openpype/hosts/houdini/plugins/load/load_redshift_proxy.py
+++ openpype/hosts/houdini/plugins/load/load_redshift_proxy.py
@@ -1,9 +1,11 @@
 import os
+import re
 from openpype.pipeline import (
     load,
     get_representation_path,
 )
 from openpype.hosts.houdini.api import pipeline
+from openpype.pipeline.load import LoadError
 
 import hou
 
@@ -34,15 +36,16 @@ class RedshiftProxyLoader(load.LoaderPlugin):
         # redshift is not set up or initialized correctly
         if not container.parm("RS_objprop_proxy_enable"):
             container.destroy()
-            raise RuntimeError("Unable to initialize geo node with Redshift "
-                               "attributes. Make sure you have the Redshift "
-                               "plug-in set up correctly for Houdini.")
+            raise LoadError("Unable to initialize geo node with Redshift "
+                            "attributes. Make sure you have the Redshift "
+                            "plug-in set up correctly for Houdini.")
 
         # Enable by default
         container.setParms({
             "RS_objprop_proxy_enable": True,
             "RS_objprop_proxy_file": self.format_path(
-                self.fname, context["representation"])
+                self.filepath_from_context(context),
+                context["representation"])
         })
 
         # Remove the file node, it only loads static meshes
@@ -92,18 +95,16 @@ class RedshiftProxyLoader(load.LoaderPlugin):
     def format_path(path, representation):
         """Format file path correctly for single redshift proxy
         or redshift proxy sequence."""
-        import re
         if not os.path.exists(path):
             raise RuntimeError("Path does not exist: %s" % path)
 
         is_sequence = bool(representation["context"].get("frame"))
         # The path is either a single file or sequence in a folder.
-        if not is_sequence:
-            filename = path
-        else:
+        if is_sequence:
             filename = re.sub(r"(.*)\.(\d+)\.(rs.*)", "\\1.$F4.\\3", path)
-
             filename = os.path.join(path, filename)
+        else:
+            filename = path
 
         filename = os.path.normpath(filename)
         filename = filename.replace("\\", "/")
