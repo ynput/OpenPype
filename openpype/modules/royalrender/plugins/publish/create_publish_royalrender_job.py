618b4b0c6d7b4cd7af858558de0463134d39634c
Ondřej Samohel <ondrej.samohel@ynput.io>
2023-11-27 12:22:11 +0100

    :art: inject submitter environment to the royal render job

--- openpype/modules/royalrender/plugins/publish/create_publish_royalrender_job.py
+++ openpype/modules/royalrender/plugins/publish/create_publish_royalrender_job.py
@@ -205,6 +205,9 @@ class CreatePublishRoyalRenderJob(pyblish.api.InstancePlugin,
         jobs_pre_ids = []
         for job in instance.data["rrJobs"]:  # type: RRJob
             if job.rrEnvList:
+                if len(job.rrEnvList) > 2000:
+                   self.log.warning(("Job environment is too long "
+                                     f"{len(job.rrEnvList)} > 2000"))
                 job_environ.update(
                     dict(RREnvList.parse(job.rrEnvList))
                 )
