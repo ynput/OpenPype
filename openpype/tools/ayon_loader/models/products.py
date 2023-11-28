1777f0efe3cf5ee812622930eab4e9009c3ca532
Petr Kalis <petr.kalis@gmail.com>
2023-11-28 22:09:54 +0100

    Hound

--- openpype/tools/ayon_loader/models/products.py
+++ openpype/tools/ayon_loader/models/products.py
@@ -677,7 +677,7 @@ class ProductsModel:
         representation_ids = [repre["id"] for repre in representations]
         representations_sync_status = (
             self._controller.get_representations_sync_status(
-                project_name,representation_ids)
+                project_name, representation_ids)
         )
         repre_items_by_version_id = collections.defaultdict(dict)
         for representation in representations:
