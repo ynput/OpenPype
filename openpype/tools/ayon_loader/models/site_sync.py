b147ec3304bb9389293fe34a163204417ce52d16
Jakub Trllo <jakub.trllo@gmail.com>
2023-11-30 16:14:45 +0100

    small tweaks in site sync model

--- openpype/tools/ayon_loader/models/site_sync.py
+++ openpype/tools/ayon_loader/models/site_sync.py
@@ -1,13 +1,11 @@
-from collections import defaultdict
+import collections
 
 from openpype.lib import Logger
 from openpype.client.entities import get_representations
 from openpype.client import get_linked_representation_id
-from openpype.tools.ayon_loader.abstract import ActionItem
 from openpype.modules import ModulesManager
-
-from openpype.modules.sync_server.utils import SiteAlreadyPresentError
 from openpype.tools.ayon_utils.models import NestedCacheItem
+from openpype.tools.ayon_loader.abstract import ActionItem
 
 DOWNLOAD_IDENTIFIER = "sitesync.download"
 UPLOAD_IDENTIFIER = "sitesync.upload"
@@ -23,13 +21,21 @@ def _default_version_availability():
 def _default_repre_status():
     return 0.0, 0.0
 
+
 class SiteSyncModel:
+    """Model handling site sync logic.
+
+    Model cares about handling of site sync functionality. All public
+    functions should be possible to call even if site sync is not available.
+    """
+
     lifetime = 60  # In seconds (minute by default)
     status_lifetime = 20
 
     def __init__(self, controller):
         self._controller = controller
 
+        self._site_icons = None
         self._site_sync_enabled_cache = NestedCacheItem(
             levels=1, lifetime=self.lifetime
         )
@@ -49,10 +55,12 @@ class SiteSyncModel:
             default_factory=_default_repre_status,
             lifetime=self.status_lifetime
         )
+
         manager = ModulesManager()
-        self._sitesync_addon = manager.modules_by_name.get("sync_server")
+        self._sitesync_addon = manager.get("sync_server")
 
     def reset(self):
+        self._site_icons = None
         self._site_sync_enabled_cache.reset()
         self._active_site_cache.reset()
         self._remote_site_cache.reset()
@@ -85,9 +93,6 @@ class SiteSyncModel:
                 )
             cache.update_data(enabled)
         return cache.get_data()
-    # TODO cache
-    def get_site_icons(self):
-        return self._sitesync_addon.get_site_icons()
 
     def get_active_site(self, project_name):
         """Active site name for a project.
@@ -126,20 +131,37 @@ class SiteSyncModel:
         return cache.get_data()
 
     def get_active_site_icon_def(self, project_name):
+        """Active site icon definition.
+
+        Args:
+            project_name (Union[str, None]): Name of project.
+
+        Returns:
+            Union[dict[str, Any], None]: Site icon definition.
+        """
+
         if not project_name:
-            return
-        provider = self._sitesync_addon.get_provider_for_site(project_name,
-            self.get_active_site(project_name))
-        return self.get_site_icons().get(provider)
+            return None
+
+        active_site = self.get_active_site(project_name)
+        provider = self._get_provider_for_site(project_name, active_site)
+        return self._get_provider_icon(provider)
 
     def get_remote_site_icon_def(self, project_name):
-        if not project_name:
-            return
-        provider = self._sitesync_addon.get_provider_for_site(
-            project_name,
-            self.get_remote_site(project_name)
-        )
-        return self.get_site_icons().get(provider)
+        """Remote site icon definition.
+
+        Args:
+            project_name (Union[str, None]): Name of project.
+
+        Returns:
+            Union[dict[str, Any], None]: Site icon definition.
+        """
+
+        if not project_name or not self.is_site_sync_enabled(project_name):
+            return None
+        remote_site = self.get_remote_site(project_name)
+        provider = self._get_provider_for_site(project_name, remote_site)
+        return self._get_provider_icon(provider)
 
     def get_version_sync_availability(self, project_name, version_ids):
         """Returns how many representations are available on sites.
@@ -215,37 +237,113 @@ class SiteSyncModel:
                 output[repre_id] = repre_cache.get_data()
         return output
 
-    def get_sitesync_action_items(self, project_name, representation_ids):
+    def get_site_sync_action_items(self, project_name, representation_ids):
+        """
+
+        Args:
+             project_name (str): Project name.
+             representation_ids (Iterable[str]): Representation ids.
+
+        Returns:
+            list[ActionItem]: Actions that can be shown in loader.
+        """
+
         if not self.is_site_sync_enabled(project_name):
             return []
-        repres_status = self.get_representations_sync_state(project_name,
-                                                            representation_ids)
 
-        action_items = []
-        repre_ids_per_identifier = defaultdict(list)
+        repres_status = self.get_representations_sync_status(
+            project_name, representation_ids
+        )
 
+        repre_ids_per_identifier = collections.defaultdict(set)
         for repre_id in representation_ids:
             repre_status = repres_status[repre_id]
             local_status, remote_status = repre_status
 
             if local_status:
-                repre_ids_per_identifier[UPLOAD_IDENTIFIER].append(repre_id)
-                repre_ids_per_identifier[REMOVE_IDENTIFIER].append(repre_id)
-            if remote_status:
-                repre_ids_per_identifier[DOWNLOAD_IDENTIFIER].append(repre_id)
+                repre_ids_per_identifier[UPLOAD_IDENTIFIER].add(repre_id)
+                repre_ids_per_identifier[REMOVE_IDENTIFIER].add(repre_id)
 
+            if remote_status:
+                repre_ids_per_identifier[DOWNLOAD_IDENTIFIER].add(repre_id)
+
+        action_items = []
         for identifier, repre_ids in repre_ids_per_identifier.items():
             if identifier == DOWNLOAD_IDENTIFIER:
                 action_items.append(self._create_download_action_item(
-                    project_name, repre_ids))
-            if identifier == UPLOAD_IDENTIFIER:
+                    project_name, repre_ids
+                ))
+            elif identifier == UPLOAD_IDENTIFIER:
                 action_items.append(self._create_upload_action_item(
-                    project_name, repre_ids))
-            if identifier == REMOVE_IDENTIFIER:
+                    project_name, repre_ids
+                ))
+            elif identifier == REMOVE_IDENTIFIER:
                 action_items.append(self._create_delete_action_item(
-                    project_name, repre_ids))
+                    project_name, repre_ids
+                ))
 
         return action_items
+
+    def is_site_sync_action(self, identifier):
+        """Should be `identifier` handled by SiteSync.
+
+        Args:
+            identifier (str): Action identifier.
+
+        Returns:
+            bool: Should action be handled by SiteSync.
+        """
+
+        return identifier in {
+            UPLOAD_IDENTIFIER,
+            DOWNLOAD_IDENTIFIER,
+            REMOVE_IDENTIFIER,
+        }
+
+    def trigger_action_item(
+        self,
+        identifier,
+        project_name,
+        representation_ids
+    ):
+        """Resets status for site_name or remove local files.
+
+        Args:
+            identifier (str): Action identifier.
+            project_name (str): Project name.
+            representation_ids (Iterable[str]): Representation ids.
+        """
+
+        active_site = self.get_active_site(project_name)
+        remote_site = self.get_remote_site(project_name)
+
+        repre_docs = list(get_representations(
+            project_name, representation_ids=representation_ids
+        ))
+        families_per_repre_id = {
+            item["_id"]: item["context"]["family"]
+            for item in repre_docs
+        }
+
+        for repre_id in representation_ids:
+            family = families_per_repre_id[repre_id]
+            if identifier == DOWNLOAD_IDENTIFIER:
+                self._add_site(
+                    project_name, repre_id, active_site, family
+                )
+
+            elif identifier == UPLOAD_IDENTIFIER:
+                self._add_site(
+                    project_name, repre_id, remote_site, family
+                )
+
+            elif identifier == REMOVE_IDENTIFIER:
+                self._sitesync_addon.remove_site(
+                    project_name,
+                    repre_id,
+                    active_site,
+                    remove_local_files=True
+                )
 
     def _is_site_sync_addon_enabled(self):
         """
@@ -256,6 +354,37 @@ class SiteSyncModel:
         if self._sitesync_addon is None:
             return False
         return self._sitesync_addon.enabled
+
+    def _get_provider_for_site(self, project_name, site_name):
+        """Provider for a site.
+
+        Args:
+            project_name (str): Project name.
+            site_name (str): Site name.
+
+        Returns:
+            Union[str, None]: Provider name.
+        """
+
+        if not self._is_site_sync_addon_enabled():
+            return None
+        return self._sitesync_addon.get_provider_for_site(
+            project_name, site_name
+        )
+
+    def _get_provider_icon(self, provider):
+        """site provider icons.
+
+        Returns:
+            Union[dict[str, Any], None]: Icon of site provider.
+        """
+
+        if not provider:
+            return None
+
+        if self._site_icons is None:
+            self._site_icons = self._sitesync_addon.get_site_icons()
+        return self._site_icons.get(provider)
 
     def _refresh_version_availability(self, project_name, version_ids):
         if not project_name or not version_ids:
@@ -299,7 +428,7 @@ class SiteSyncModel:
             DOWNLOAD_IDENTIFIER,
             "Download",
             "Mark representation for download locally",
-            "download"
+            "fa.download"
         )
 
     def _create_upload_action_item(self, project_name, representation_ids):
@@ -309,25 +438,36 @@ class SiteSyncModel:
             UPLOAD_IDENTIFIER,
             "Upload",
             "Mark representation for upload remotely",
-            "upload"
+            "fa.upload"
         )
 
     def _create_delete_action_item(self, project_name, representation_ids):
-        return self._create_action_item(project_name,
-                                        representation_ids,
-                                        REMOVE_IDENTIFIER,
-                                        "Remove from local",
-                                        "Remove local synchronization",
-                                        "trash")
+        return self._create_action_item(
+            project_name,
+            representation_ids,
+            REMOVE_IDENTIFIER,
+            "Remove from local",
+            "Remove local synchronization",
+            "fa.trash"
+        )
 
-    def _create_action_item(self, project_name, representation_ids,
-                            identifier, label, tooltip, icon_name):
-        icon_name = f"fa.{icon_name}"
+    def _create_action_item(
+        self,
+        project_name,
+        representation_ids,
+        identifier,
+        label,
+        tooltip,
+        icon_name
+    ):
         return ActionItem(
             identifier,
             label,
-            icon={"type": "awesome-font", "name": icon_name,
-                  "color": "#999999"},
+            icon={
+                "type": "awesome-font",
+                "name": icon_name,
+                "color": "#999999"
+            },
             tooltip=tooltip,
             options={},
             order=1,
@@ -338,63 +478,30 @@ class SiteSyncModel:
             representation_ids=representation_ids,
         )
 
-    def is_sitesync_action(self, identifier):
-        """Should be `identifier` handled by SiteSync."""
-        return identifier in {UPLOAD_IDENTIFIER,
-                              DOWNLOAD_IDENTIFIER,
-                              REMOVE_IDENTIFIER}
+    def _add_site(self, project_name, repre_id, site_name, family):
+        self._sitesync_addon.add_site(
+            project_name, repre_id, site_name, force=True
+        )
 
-    def trigger_action_item(
-        self,
-        identifier,
-        project_name,
-        representation_ids
-    ):
-        """Resets status for site_name or remove local files."""
-        active_site = self.get_active_site(project_name)
-        remote_site = self.get_remote_site(project_name)
+        # TODO this should happen in site sync addon
+        if family != "workfile":
+            return
 
-        repre_docs = list(get_representations(
-            project_name, representation_ids=representation_ids
-        ))
-        families_per_repre_id = {item["_id"]: item["context"]["family"]
-                                 for item in repre_docs}
-
-        for repre_id in representation_ids:
-            if identifier == DOWNLOAD_IDENTIFIER:
-                self._add_site(project_name, repre_id,
-                               active_site,
-                               families_per_repre_id[repre_id])
-            if identifier == UPLOAD_IDENTIFIER:
-                self._add_site(project_name, repre_id,
-                               remote_site,
-                               families_per_repre_id[repre_id])
-
-            if identifier == REMOVE_IDENTIFIER:
-                self._sitesync_addon.remove_site(project_name,
-                                                 repre_id,
-                                                 active_site,
-                                                 remove_local_files=True)
-
-    def _add_site(self, project_name, repre_id, site_name,
-                  family):
-        self._sitesync_addon.add_site(project_name,
-                                      repre_id,
-                                      site_name,
-                                      force=True)
-        if family == "workfile":
-            links = get_linked_representation_id(
-                project_name,
-                repre_id=repre_id,
-                link_type="reference"
-            )
-            for link_repre_id in links:
-                try:
-                    print("Adding {} to linked representation: {}".format(
-                        site_name, link_repre_id))
-                    self.sync_server.add_site(project_name, link_repre_id,
-                                              site_name,
-                                              force=False)
-                except SiteAlreadyPresentError:
-                    # do not add/reset working site for references
-                    log.debug("Site present", exc_info=True)
+        links = get_linked_representation_id(
+            project_name,
+            repre_id=repre_id,
+            link_type="reference"
+        )
+        for link_repre_id in links:
+            try:
+                print("Adding {} to linked representation: {}".format(
+                    site_name, link_repre_id))
+                self._sitesync_addon.add_site(
+                    project_name,
+                    link_repre_id,
+                    site_name,
+                    force=False
+                )
+            except Exception:
+                # do not add/reset working site for references
+                log.debug("Site present", exc_info=True)
