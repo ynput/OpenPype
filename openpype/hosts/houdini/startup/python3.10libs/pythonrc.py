071779c148424fb82e73c67cd457c13600d38934
MustafaJafar <mustafataherzaky@outlook.com>
2023-12-05 15:32:08 +0200

    replace OpenPype with AYON

--- openpype/hosts/houdini/startup/python3.10libs/pythonrc.py
+++ openpype/hosts/houdini/startup/python3.10libs/pythonrc.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-"""OpenPype startup script."""
+"""AYON startup script."""
 from openpype.pipeline import install_host
 from openpype.hosts.houdini.api import HoudiniHost
+import os
 
 
 def main():
-    print("Installing OpenPype ...")
+    print("Installing {} ...".format(
+        os.environ.get("AVALON_LABEL") or "AYON"))
     install_host(HoudiniHost())
 
 
