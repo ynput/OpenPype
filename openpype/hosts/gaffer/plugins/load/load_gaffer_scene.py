576c1e028e9ad22b61bb3a3c92fa6d133b446fe4
Roy Nieterau <roy_nieterau@hotmail.com>
2023-12-06 17:25:11 +0100

    Fix class name + reference the exported box

--- openpype/hosts/gaffer/plugins/load/load_gaffer_scene.py
+++ openpype/hosts/gaffer/plugins/load/load_gaffer_scene.py
@@ -8,38 +8,13 @@ from openpype.hosts.gaffer.api.lib import set_node_color
 import Gaffer
 
 
-def import_file(script: Gaffer.ScriptNode, filepath: str):
-
-    with Gaffer.UndoScope(script):
-
-        new_children = []
-
-        def acquire_new_nodes(parent, child):
-            new_children.append(child)
-
-        callback = script.childAddedSignal().connect(
-            acquire_new_nodes, scoped=True
-        )
-
-        script.importFile(str(filepath),
-                          parent=script,
-                          continueOnError=True)
-
-        new_nodes = [c for c in new_children if isinstance(c, Gaffer.Node)]
-        script.selection().clear()
-        script.selection().add(new_nodes)
-
-        del callback
-
-        return new_children
-
-class GafferLoadScene(load.LoaderPlugin):
-    """Import a gaffer scene"""
+class GafferLoadReference(load.LoaderPlugin):
+    """Reference a gaffer scene"""
 
     families = ["gafferScene"]
     representations = ["gfr"]
 
-    label = "Load gaffer scene"
+    label = "Reference Gaffer Scene"
     order = -10
     icon = "code-fork"
     color = "orange"
@@ -50,16 +25,14 @@ class GafferLoadScene(load.LoaderPlugin):
 
         # Import gaffer sccene
         path = self.filepath_from_context(context).replace("\\", "/")
-        new_nodes = import_file(script, path)
 
-        assert len(new_nodes) == 1, "Only one new node allowed"
+        reference = Gaffer.Reference()
+        script.addChild(reference)
+        reference.load(path)
 
-        # Colorize based on family
-        # TODO: Use settings instead
-        node = new_nodes[0]
-        set_node_color(node, (0.533, 0.447, 0.957))
+        set_node_color(reference, (0.533, 0.447, 0.957))
 
-        imprint_container(node,
+        imprint_container(reference,
                           name=name,
                           namespace=namespace,
                           context=context,
@@ -77,17 +50,8 @@ class GafferLoadScene(load.LoaderPlugin):
         # completely and replace it with a new one? For now we do. Preferably
         # however we would have it like a 'reference' so that we can just
         # update the loaded 'box' or 'contents' to the new one.
-        node = container["_node"]
-
-        # Delete self
-        parent = node.parent()
-        parent.removeChild(node)
-
-        new_node = import_file(parent, path)
-
-        # Can we just copy this?
-        for plug in node["user"]:
-            new_node["user"][plug.getName()] = plug
+        node: Gaffer.Reference = container["_node"]
+        node.load(path)
 
         # Update the imprinted representation
         node["user"]["representation"].SetValue(str(representation["_id"]))
