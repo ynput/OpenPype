bb2f3611e40dcc67be23e6051c709f2e33d54c9c
Roy Nieterau <roy_nieterau@hotmail.com>
2023-12-08 11:06:01 +0100

    Allow to import into a box and set default to importing in a `Gaffer.Box`

--- openpype/hosts/gaffer/plugins/load/import_gaffer_scene.py
+++ openpype/hosts/gaffer/plugins/load/import_gaffer_scene.py
@@ -1,5 +1,8 @@
+from openpype.lib import BoolDef
 from openpype.pipeline import load
+
 from openpype.hosts.gaffer.api import get_root
+from openpype.hosts.gaffer.api.lib import make_box
 
 import Gaffer
 
@@ -15,22 +18,37 @@ class GafferImportScene(load.LoaderPlugin):
     icon = "code-fork"
     color = "white"
 
+    options = [
+        BoolDef(
+            "box",
+            label="Import into Box",
+            default=True
+        )
+    ]
+
     def load(self, context, name, namespace, data):
 
         script = get_root()
         path = self.filepath_from_context(context).replace("\\", "/")
 
+        import_to_box = data.get("box", True)
+        if import_to_box:
+            parent = make_box(name, add_input=False, add_output=False)
+            script.addChild(parent)
+        else:
+            parent = script
+
         with Gaffer.UndoScope(script):
             new_children = []
 
-            def get_new_children(parent, child):
+            def get_new_children(_parent, child):
                 """Capture new children from import via `childAddedSignal`"""
                 new_children.append(child)
 
-            callback = script.childAddedSignal().connect(  # noqa
+            callback = parent.childAddedSignal().connect(  # noqa
                 get_new_children, scoped=True
             )
-            script.importFile(path, parent=script, continueOnError=True)
+            script.importFile(path, parent=parent, continueOnError=True)
 
             new_nodes = [child for child in new_children
                          if isinstance(child, Gaffer.Node)]
@@ -38,6 +56,10 @@ class GafferImportScene(load.LoaderPlugin):
             # Select new nodes
             selection = script.selection()
             selection.clear()
-            selection.add(new_nodes)
+
+            if import_to_box:
+                selection.add([parent])
+            else:
+                selection.add(new_nodes)
 
             del callback
