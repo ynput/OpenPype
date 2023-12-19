a15660f6bf86b9a80c9bf3bce2d152bfb97023a9
Jakub Trllo <jakub.trllo@gmail.com>
2023-12-19 18:55:56 +0100

    it is possible to call 'save_registry' only when window is showed up

--- openpype/modules/python_console_interpreter/window/widgets.py
+++ openpype/modules/python_console_interpreter/window/widgets.py
@@ -354,7 +354,7 @@ class PythonInterpreterWidget(QtWidgets.QWidget):
     default_width = 1000
     default_height = 600
 
-    def __init__(self, parent=None):
+    def __init__(self, allow_save_registry=True, parent=None):
         super(PythonInterpreterWidget, self).__init__(parent)
 
         self.setWindowTitle("{} Console".format(
@@ -414,6 +414,8 @@ class PythonInterpreterWidget(QtWidgets.QWidget):
 
         self._first_show = True
         self._splitter_size_ratio = None
+        self._allow_save_registry = allow_save_registry
+        self._should_save_registry = False
 
         self._init_from_registry()
 
@@ -457,6 +459,10 @@ class PythonInterpreterWidget(QtWidgets.QWidget):
             pass
 
     def save_registry(self):
+        # Window was not showed
+        if not self._allow_save_registry or not self._should_save_registry:
+            return
+
         setting_registry = PythonInterpreterRegistry()
 
         setting_registry.set_item("width", self.width())
@@ -651,6 +657,7 @@ class PythonInterpreterWidget(QtWidgets.QWidget):
     def showEvent(self, event):
         self._line_check_timer.start()
         super(PythonInterpreterWidget, self).showEvent(event)
+        self._should_save_registry = True
         # First show setup
         if self._first_show:
             self._first_show = False
@@ -676,5 +683,6 @@ class PythonInterpreterWidget(QtWidgets.QWidget):
 
     def closeEvent(self, event):
         self.save_registry()
+        self._should_save_registry = False
         super(PythonInterpreterWidget, self).closeEvent(event)
         self._line_check_timer.stop()
