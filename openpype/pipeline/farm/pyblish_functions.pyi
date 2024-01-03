f76e7ba12bf720238b59ed3cca361a49b1b218ce
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-01-03 10:34:25 +0100

    :wrench: simple maintenance fixes

--- openpype/pipeline/farm/pyblish_functions.pyi
+++ openpype/pipeline/farm/pyblish_functions.pyi
@@ -1,6 +1,7 @@
 import pyblish.api
 from openpype.pipeline import Anatomy
 from typing import Tuple, Union, List
+from openpype.plugins import publish
 
 
 class TimeData:
@@ -22,3 +23,10 @@ def get_transferable_representations(instance: pyblish.api.Instance) -> list: ..
 def create_skeleton_instance(instance: pyblish.api.Instance, families_transfer: list = ..., instance_transfer: dict = ...) -> dict: ...
 def create_instances_for_aov(instance: pyblish.api.Instance, skeleton: dict, aov_filter: dict) -> List[pyblish.api.Instance]: ...
 def attach_instances_to_subset(attach_to: list, instances: list) -> list: ...
+def prepare_representations(skeleton_data: dict, exp_files: list,
+                            anatomy: Anatomy, aov_filter: dict,
+                            skip_integration_repre_list: list,
+                            do_not_add_review: bool,
+                            context: pyblish.api.Context,
+                            color_managed_plugin: publish.ColormanagedPyblishPluginMixin) -> list: ...  # noqa: E501
+def create_metadata_path(instance: pyblish.api.Instance, anatomy: Anatomy) -> str: ...  # noqa: E501
