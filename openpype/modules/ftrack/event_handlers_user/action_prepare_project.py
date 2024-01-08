9d0fac8adc348f010a82b231e81974e2dba0a702
BenSouchet <contact@bensouchet.dev>
2024-01-08 11:45:46 +0100

    Improve way to bypasss protection to update anatomy attributes to ftrack tracker (quadproduction/issues#275)

--- openpype/modules/ftrack/event_handlers_user/action_prepare_project.py
+++ openpype/modules/ftrack/event_handlers_user/action_prepare_project.py
@@ -406,6 +406,8 @@ class PrepareProjectLocal(BaseAction):
         project_settings = ProjectSettings(project_name)
         project_anatomy_settings = project_settings["project_anatomy"]
         project_anatomy_settings["roots"] = root_data
+        # Add a flag to be able to bypass the enabled protection for the anatomy attrs
+        project_anatomy_settings["bypass_protect_anatomy_attributes"] = True
 
         custom_attribute_values = {}
         attributes_entity = project_anatomy_settings["attributes"]
