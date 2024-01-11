adb7e19c232adad1e9fb122ba5cffdd55970916b
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-01-11 13:21:41 +0100

    Publisher: Window is not always on top (#6107)
    * make publisher a window without always on top
    
    * put publisher window to the top on process
    
    * make sure screenshot window is active
    
    * removed unnecessary variable

--- openpype/tools/publisher/window.py
+++ openpype/tools/publisher/window.py
@@ -42,7 +42,7 @@ from .widgets import (
 )
 
 
-class PublisherWindow(QtWidgets.QDialog):
+class PublisherWindow(QtWidgets.QWidget):
     """Main window of publisher."""
     default_width = 1300
     default_height = 800
@@ -50,7 +50,7 @@ class PublisherWindow(QtWidgets.QDialog):
     publish_footer_spacer = 2
 
     def __init__(self, parent=None, controller=None, reset_on_show=None):
-        super(PublisherWindow, self).__init__(parent)
+        super(PublisherWindow, self).__init__()
 
         self.setObjectName("PublishWindow")
 
@@ -64,17 +64,12 @@ class PublisherWindow(QtWidgets.QDialog):
         if reset_on_show is None:
             reset_on_show = True
 
-        if parent is None:
-            on_top_flag = QtCore.Qt.WindowStaysOnTopHint
-        else:
-            on_top_flag = QtCore.Qt.Dialog
-
         self.setWindowFlags(
-            QtCore.Qt.WindowTitleHint
+            QtCore.Qt.Window
+            | QtCore.Qt.WindowTitleHint
             | QtCore.Qt.WindowMaximizeButtonHint
             | QtCore.Qt.WindowMinimizeButtonHint
             | QtCore.Qt.WindowCloseButtonHint
-            | on_top_flag
         )
 
         if controller is None:
@@ -298,6 +293,12 @@ class PublisherWindow(QtWidgets.QDialog):
         )
         controller.event_system.add_callback(
             "publish.process.stopped", self._on_publish_stop
+        )
+        controller.event_system.add_callback(
+            "publish.process.instance.changed", self._on_instance_change
+        )
+        controller.event_system.add_callback(
+            "publish.process.plugin.changed", self._on_plugin_change
         )
         controller.event_system.add_callback(
             "show.card.message", self._on_overlay_message
@@ -556,6 +557,18 @@ class PublisherWindow(QtWidgets.QDialog):
         if self._reset_on_show:
             self._reset_on_show = False
             self.reset()
+
+    def _make_sure_on_top(self):
+        """Raise window to top and activate it.
+
+        This may not work for some DCCs without Qt.
+        """
+
+        if not self._window_is_visible:
+            self.show()
+
+        self.setWindowState(QtCore.Qt.WindowActive)
+        self.raise_()
 
     def _checks_before_save(self, explicit_save):
         """Save of changes may trigger some issues.
@@ -869,6 +882,12 @@ class PublisherWindow(QtWidgets.QDialog):
         if self._is_on_create_tab():
             self._go_to_publish_tab()
 
+    def _on_instance_change(self):
+        self._make_sure_on_top()
+
+    def _on_plugin_change(self):
+        self._make_sure_on_top()
+
     def _on_publish_validated_change(self, event):
         if event["value"]:
             self._validate_btn.setEnabled(False)
@@ -879,6 +898,7 @@ class PublisherWindow(QtWidgets.QDialog):
             self._comment_input.setText("")
 
     def _on_publish_stop(self):
+        self._make_sure_on_top()
         self._set_publish_overlay_visibility(False)
         self._reset_btn.setEnabled(True)
         self._stop_btn.setEnabled(False)
