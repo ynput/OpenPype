dcf8805cfe2aca6f2615de1a7c18988d0efedae6
Toke Stuart Jepsen <tokejepsen@gmail.com>
2024-01-16 16:53:26 +0000

    Add settings for effect categories

--- server_addon/hiero/server/settings/publish_plugins.py
+++ server_addon/hiero/server/settings/publish_plugins.py
@@ -1,5 +1,7 @@
-from pydantic import Field
-from ayon_server.settings import BaseSettingsModel
+from pydantic import Field, validator
+from ayon_server.settings import (
+    BaseSettingsModel, ensure_unique_names, normalize_name
+)
 
 
 class CollectInstanceVersionModel(BaseSettingsModel):
@@ -7,6 +9,30 @@ class CollectInstanceVersionModel(BaseSettingsModel):
         True,
         title="Enabled"
     )
+
+
+class CollectClipEffectsDefModel(BaseSettingsModel):
+    _layout = "expanded"
+    name: str = Field("", title="Name")
+    effect_classes: list[str] = Field(
+        default_factory=list, title="Effect Classes"
+    )
+
+    @validator("name")
+    def validate_name(cls, value):
+        """Ensure name does not contain weird characters"""
+        return normalize_name(value)
+
+
+class CollectClipEffectsModel(BaseSettingsModel):
+    effect_categories: list[CollectClipEffectsDefModel] = Field(
+        default_factory=list, title="Effect Categories"
+    )
+
+    @validator("effect_categories")
+    def validate_unique_outputs(cls, value):
+        ensure_unique_names(value)
+        return value
 
 
 class ExtractReviewCutUpVideoModel(BaseSettingsModel):
@@ -24,6 +50,10 @@ class PublishPuginsModel(BaseSettingsModel):
     CollectInstanceVersion: CollectInstanceVersionModel = Field(
         default_factory=CollectInstanceVersionModel,
         title="Collect Instance Version"
+    )
+    CollectClipEffects: CollectClipEffectsModel = Field(
+        default_factory=CollectClipEffectsModel,
+        title="Collect Clip Effects"
     )
     """# TODO: enhance settings with host api:
     Rename class name and plugin name
