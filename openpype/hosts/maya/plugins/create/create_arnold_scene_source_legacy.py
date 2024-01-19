846e725363f63270e9c51b719ab8d350055e57d7
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-01-19 16:05:25 +0100

    :recycle: reintroduce old behavior of ass extraction

---
+++ openpype/hosts/maya/plugins/create/create_arnold_scene_source_legacy.py
@@ -0,0 +1,110 @@
+from openpype.hosts.maya.api import (
+    lib,
+    plugin
+)
+from openpype.lib import (
+    NumberDef,
+    BoolDef
+)
+
+
+class CreateArnoldSceneSourceLegacy(plugin.MayaCreator):
+    """Arnold Scene Source"""
+
+    identifier = "io.openpype.creators.maya.legacy.ass"
+    label = "Arnold Scene Source (Legacy)"
+    family = "ass"
+    icon = "cube"
+    settings_name = "CreateAss"
+
+    expandProcedurals = False
+    motionBlur = True
+    motionBlurKeys = 2
+    motionBlurLength = 0.5
+    maskOptions = False
+    maskCamera = False
+    maskLight = False
+    maskShape = False
+    maskShader = False
+    maskOverride = False
+    maskDriver = False
+    maskFilter = False
+    maskColor_manager = False
+    maskOperator = False
+
+    def get_instance_attr_defs(self):
+
+        defs = lib.collect_animation_defs()
+
+        defs.extend([
+            BoolDef("expandProcedural",
+                    label="Expand Procedural",
+                    default=self.expandProcedurals),
+            BoolDef("motionBlur",
+                    label="Motion Blur",
+                    default=self.motionBlur),
+            NumberDef("motionBlurKeys",
+                      label="Motion Blur Keys",
+                      decimals=0,
+                      default=self.motionBlurKeys),
+            NumberDef("motionBlurLength",
+                      label="Motion Blur Length",
+                      decimals=3,
+                      default=self.motionBlurLength),
+
+            # Masks
+            BoolDef("maskOptions",
+                    label="Export Options",
+                    default=self.maskOptions),
+            BoolDef("maskCamera",
+                    label="Export Cameras",
+                    default=self.maskCamera),
+            BoolDef("maskLight",
+                    label="Export Lights",
+                    default=self.maskLight),
+            BoolDef("maskShape",
+                    label="Export Shapes",
+                    default=self.maskShape),
+            BoolDef("maskShader",
+                    label="Export Shaders",
+                    default=self.maskShader),
+            BoolDef("maskOverride",
+                    label="Export Override Nodes",
+                    default=self.maskOverride),
+            BoolDef("maskDriver",
+                    label="Export Drivers",
+                    default=self.maskDriver),
+            BoolDef("maskFilter",
+                    label="Export Filters",
+                    default=self.maskFilter),
+            BoolDef("maskOperator",
+                    label="Export Operators",
+                    default=self.maskOperator),
+            BoolDef("maskColor_manager",
+                    label="Export Color Managers",
+                    default=self.maskColor_manager),
+        ])
+
+        return defs
+
+    def get_publish_families(self):
+        return ["ass.legacy"]
+
+    def create(self, subset_name, instance_data, pre_create_data):
+
+        from maya import cmds
+
+        members = list()
+        if pre_create_data.get("use_selection"):
+            members = cmds.ls(selection=True)
+            pre_create_data["use_selection"] = False
+
+        instance = super(CreateArnoldSceneSourceLegacy, self).create(
+            subset_name, instance_data, pre_create_data
+        )
+
+        instance_node = instance.get("instance_node")
+
+        content = cmds.sets(members, name=instance_node + "_content_SET")
+        proxy = cmds.sets(name=instance_node + "_proxy_SET", empty=True)
+        cmds.sets([content, proxy], forceElement=instance_node)
