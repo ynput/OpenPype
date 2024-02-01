4999550167e3668b7be617c2842cddaee100a9a2
Jack P <jack.p@random42.com>
2024-02-01 13:50:04 +0000

    fix: hound

--- openpype/hosts/max/plugins/publish/save_scene.py
+++ openpype/hosts/max/plugins/publish/save_scene.py
@@ -9,16 +9,15 @@ class SaveCurrentScene(pyblish.api.ContextPlugin):
     order = pyblish.api.ExtractorOrder - 0.49
     hosts = ["max"]
     families = ["maxrender", "workfile"]
-    
+
     def process(self, context):
         host = registered_host()
         current_file = host.get_current_workfile()
 
         assert context.data["currentFile"] == current_file
-    
+
         if host.workfile_has_unsaved_changes():
             self.log.info(f"Saving current file: {current_file}")
             host.save_workfile(current_file)
         else:
             self.log.debug("No unsaved changes, skipping file save..")
-    
