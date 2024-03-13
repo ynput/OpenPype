34359c217180c55e7bd56c53be6cfbf8f327aced
Jakub Jezek <jakubjezek001@gmail.com>
2024-03-13 17:04:05 +0100

    improving compatibility check

--- openpype/pipeline/colorspace.py
+++ openpype/pipeline/colorspace.py
@@ -463,12 +463,12 @@ def compatibility_check():
 
     try:
         import PyOpenColorIO  # noqa: F401
+        # Requirement, introduced in newer ocio version
         config = PyOpenColorIO.GetCurrentConfig()
-        config.getDisplayViewColorSpaceName("sRGB", "sRGB")
-        CachedData.has_compatible_ocio_package = True
+        CachedData.has_compatible_ocio_package = (
+            hasattr(config, "getDisplayViewColorSpaceName")
+        )
     except ImportError:
-        CachedData.has_compatible_ocio_package = False
-    except AttributeError:
         CachedData.has_compatible_ocio_package = False
 
     # compatible
