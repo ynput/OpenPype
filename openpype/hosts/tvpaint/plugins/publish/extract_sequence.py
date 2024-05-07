c6697541679bf9d9e084452fbf18aa2513f9a98e
BenSouchet <contact@bensouchet.dev>
2024-05-07 10:10:04 +0200

    Add detail as a code comment

--- openpype/hosts/tvpaint/plugins/publish/extract_sequence.py
+++ openpype/hosts/tvpaint/plugins/publish/extract_sequence.py
@@ -242,6 +242,8 @@ class ExtractSequence(pyblish.api.Extractor):
             "export_path = \"{}\"".format(
                 first_frame_filepath.replace("\\", "/")
             ),
+            # TvPaint seems to consider software markin as frame 0. If we set a frame start at export,
+            # it will add the frame start to the markin frame and will add an offset for the rest of the export.
             "tv_projectsavesequence '\"'export_path'\"' \"{}\" {} {}".format(
                 export_type, 0, mark_out - mark_in
             )
